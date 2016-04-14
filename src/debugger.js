1164dc67b5bfb5ffedc77bb3e6fcea3df5872444
yann300 <yann.levreau@gmail.com>
2016-04-14 17:14:51 +0200

    indentation

--- src/debugger.js
+++ src/debugger.js
@@ -4,22 +4,22 @@ var VmTraceManager = require('./vmTraceManager');
 var VmTraceBrowser = require('./vmTraceBrowser');
 
 module.exports = React.createClass({
-  getInitialState: function() {
-	 return {vmTrace: null}
-  },
+	getInitialState: function() {
+		return {vmTrace: null}
+	},
 
-  render: function() {
-    return (
-        <div>
-          <h1>Debugger</h1>
-		      <TxBrowser onNewTxRequested={this.retrieveVmTrace} />
-		      <VmTraceBrowser vmTrace={this.state.vmTrace} />
-        </div>
-    );
-  },
+	render: function() {
+		return (
+			<div>
+			<h1>Debugger</h1>
+			<TxBrowser onNewTxRequested={this.retrieveVmTrace} />
+			<VmTraceBrowser vmTrace={this.state.vmTrace} />
+			</div>
+			);
+	},
 
-  retrieveVmTrace: function(blockNumber, txNumber)
-  { 
-	  this.setState({vmTrace: VmTraceManager.retrieveVmTrace(blockNumber, txNumber)});
-  }
+	retrieveVmTrace: function(blockNumber, txNumber)
+	{ 
+		this.setState({vmTrace: VmTraceManager.retrieveVmTrace(blockNumber, txNumber)});
+	}
 });
--- src/vmTraceBrowser.js
+++ src/vmTraceBrowser.js
@@ -2,7 +2,7 @@ var React = require('react');
 var AssemblyItemsBrowser = require('./assemblyItemsBrowser');
 
 module.exports = React.createClass({
-  	render: function() {
-  		return ( <AssemblyItemsBrowser vmTrace={this.props.vmTrace} /> )
+	render: function() {
+		return ( <AssemblyItemsBrowser vmTrace={this.props.vmTrace} /> )
 	}
 });
