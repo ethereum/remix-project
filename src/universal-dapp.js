00f41bf248af20774e1f0396a605d88b01c5fe49
Alex Beregszaszi <alex@rtfs.hu>
2016-06-09 01:09:17 +0100

    UDapp: decode constant responses in web3 mode

--- src/universal-dapp.js
+++ src/universal-dapp.js
@@ -447,6 +447,39 @@ UniversalDApp.prototype.getCallButton = function (args) {
       }
     }
 
+    var decodeResponse = function (response) {
+        // Only decode if there supposed to be fields
+        if (args.abi.outputs.length > 0) {
+          try {
+            var i;
+
+            var outputTypes = [];
+            for (i = 0; i < args.abi.outputs.length; i++) {
+              outputTypes.push(args.abi.outputs[i].type);
+            }
+
+            // decode data
+            var decodedObj = ethJSABI.rawDecode(outputTypes, response);
+
+            // format decoded data
+            decodedObj = ethJSABI.stringify(outputTypes, decodedObj);
+            for (i = 0; i < outputTypes.length; i++) {
+              var name = args.abi.outputs[i].name;
+              if (name.length > 0) {
+                decodedObj[i] = outputTypes[i] + ' ' + name + ': ' + decodedObj[i];
+              } else {
+                decodedObj[i] = outputTypes[i] + ': ' + decodedObj[i];
+              }
+            }
+
+            return getDecodedOutput(decodedObj);
+          } catch (e) {
+            return getDecodedOutput('Failed to decode output: ' + e);
+          }
+        }
+    };
+
+    var decoded;
     self.runTx(data, args, function (err, result) {
       if (err) {
         replaceOutput($result, $('<span/>').text(err).addClass('error'));
@@ -464,37 +497,18 @@ UniversalDApp.prototype.getCallButton = function (args) {
         clearOutput($result);
         $result.append(getReturnOutput(outputObj)).append(getGasUsedOutput(result, result.vm));
 
-        // Only decode if there supposed to be fields
-        if (args.abi.outputs.length > 0) {
-          try {
-            var i;
-
-            var outputTypes = [];
-            for (i = 0; i < args.abi.outputs.length; i++) {
-              outputTypes.push(args.abi.outputs[i].type);
-            }
-
-            // decode data
-            var decodedObj = ethJSABI.rawDecode(outputTypes, result.vm.return);
-
-            // format decoded data
-            decodedObj = ethJSABI.stringify(outputTypes, decodedObj);
-            for (i = 0; i < outputTypes.length; i++) {
-              var name = args.abi.outputs[i].name;
-              if (name.length > 0) {
-                decodedObj[i] = outputTypes[i] + ' ' + name + ': ' + decodedObj[i];
-              } else {
-                decodedObj[i] = outputTypes[i] + ': ' + decodedObj[i];
-              }
-            }
-
-            $result.append(getDecodedOutput(decodedObj));
-          } catch (e) {
-            $result.append(getDecodedOutput('Failed to decode output: ' + e));
-          }
-        }
+	decoded = decodeResponse(result.vm.return);
+	if (decoded) {
+	  $result.append(decoded);
+	}
       } else if (args.abi.constant && !isConstructor) {
-        replaceOutput($result, getReturnOutput(result));
+        clearOutput($result);
+        $result.append(getReturnOutput(result)).append(getGasUsedOutput({}));
+
+	decoded = decodeResponse(ethJSUtil.toBuffer(result));
+	if (decoded) {
+	  $result.append(decoded);
+	}
       } else {
         clearOutput($result);
         $result.append(getReturnOutput(result)).append(getGasUsedOutput(result));
