82e1de3ba9b923dbe33f02fd40915507076e74e9
Dave Hoover <dave.hoover@gmail.com>
2016-06-15 07:50:34 -0400

    Having another go at stable in-browser testing

--- test-browser/new-file-test.js
+++ test-browser/new-file-test.js
@@ -2,7 +2,7 @@ module.exports = {
   'New file test': function (browser) {
     browser
       .url('http://127.0.0.1:8080')
-      .waitForElementVisible('.newFile', 5000)
+      .waitForElementVisible('.newFile', 10000)
       .click('.newFile')
       .assert.containsText('.active', 'Untitled')
       .end();
