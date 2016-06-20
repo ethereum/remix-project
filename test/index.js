2176ad1f27ca4038fff2a5269096ee07b1201e15
yann300 <yann.levreau@gmail.com>
2016-06-20 17:40:19 +0200

    switch to tape

--- test/index.js
+++ test/index.js
@@ -1,12 +1,11 @@
-/* global describe it*/
-var should = require('should')
-
+'use strict'
+var tape = require('tape')
 var init = require('../src/helpers/init')
-describe('index', function () {
-  describe('loadContext', function () {
-    it('web3', function () {
-      var context = init.loadContext()
-      should.exist(context.web3)
-    })
+tape('index', function (t) {
+  t.test('loadContext - web3', function (st) {
+    var context = init.loadContext()
+    st.notEqual(context.web3, undefined)
+    st.notEqual(context.web3, null)
+    st.end()
   })
 })
