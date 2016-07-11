4700d8660bc2f338bfac9c7e8861d00e10b08254
yann300 <yann.levreau@gmail.com>
2016-07-11 10:47:42 +0200

    quick changes

--- ci/browser_tests.sh
+++ ci/browser_tests.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
-SAUCECONNECT_URL="http://saucelabs.com/downloads/sc-4.3.16-linux.tar.gz"
+SAUCECONNECT_URL="https://saucelabs.com/downloads/sc-4.3.16-linux.tar.gz"
 SAUCECONNECT_USERNAME="chriseth"
 SAUCECONNECT_ACCESSKEY="b781828a-9e9c-43d8-89d4-2fbb879595ca"
 SAUCECONNECT_JOBIDENTIFIER="browsersolidity_tests_${TRAVIS_JOB_NUMBER}"
@@ -18,21 +18,10 @@ while [ ! -f $SAUCECONNECT_READYFILE ]; do
   sleep .5
 done
 
-function updateTestExitCode() {
-  if [ $? -eq 1 ]
-  then
-    TEST_EXITCODE=1
-  fi
-}
-
-npm run browser-test-remote-firefox
-updateTestExitCode
-npm run browser-test-remote-chrome
-updateTestExitCode
-npm run browser-test-remote-safari
-updateTestExitCode
-npm run browser-test-remote-ie
-updateTestExitCode
+npm run browser-test-remote-firefox || TEST_EXITCODE=1
+npm run browser-test-remote-chrome || TEST_EXITCODE=1
+npm run browser-test-remote-safari || TEST_EXITCODE=1
+npm run browser-test-remote-ie || TEST_EXITCODE=1
 
 node ci/sauceDisconnect.js $SAUCECONNECT_USERNAME $SAUCECONNECT_ACCESSKEY $SAUCECONNECT_JOBIDENTIFIER
 
