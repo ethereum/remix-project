87a8f3adc2fdc0046ed2463de9db7b0bdbb4e6f0
Alex Beregszaszi <alex@rtfs.hu>
2016-07-27 22:21:17 +0100

    Remove ctrl-f keyboard shortcut

--- src/app/editor.js
+++ src/app/editor.js
@@ -181,8 +181,9 @@ function Editor (loadingFromGist, storage) {
     editor.setSession(sessions[SOL_CACHE_FILE]);
     editor.resize(true);
 
-    // Unmap ctrl-t
+    // Unmap ctrl-t & ctrl-f
     editor.commands.bindKeys({ 'ctrl-t': null });
+    editor.commands.bindKeys({ 'ctrl-f': null });
   }
 }
 
