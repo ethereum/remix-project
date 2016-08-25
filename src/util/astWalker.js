ec4b276ac1f7e6341793b2200cd0ca3c81af2458
yann300 <yann.levreau@gmail.com>
2016-08-25 13:52:38 +0200

    add tests

--- src/util/astWalker.js
+++ src/util/astWalker.js
@@ -1,20 +1,27 @@
 'use strict'
-/*
-  Crawl the given AST through the function walk(ast, callback)
-*/
+/**
+ * Crawl the given AST through the function walk(ast, callback)
+ */
 function AstWalker () {
 }
-/*
-    * visit all the AST nodes
-    *
-    * @param {Object} ast  - AST node
-    * @param {Object or Function} callback  - if (Function) the function will be called for every node.
-    *                                       - if (Object) callback[<Node Type>] will be called for
-    *                                         every node of type <Node Type>. callback["*"] will be called fo all other nodes.
-    *                                         in each case, if the callback returns false it does not descend into children.
-    *                                         If no callback for the current type, children are visited.
-*/
+
+/**
+ * visit all the AST nodes
+ *
+ * @param {Object} ast  - AST node
+ * @param {Object or Function} callback  - if (Function) the function will be called for every node.
+ *                                       - if (Object) callback[<Node Type>] will be called for
+ *                                         every node of type <Node Type>. callback["*"] will be called fo all other nodes.
+ *                                         in each case, if the callback returns false it does not descend into children.
+ *                                         If no callback for the current type, children are visited.
+ */
 AstWalker.prototype.walk = function (ast, callback) {
+  if (callback instanceof Function) {
+    callback = {'*': callback}
+  }
+  if (!('*' in callback)) {
+    callback['*'] = function () { return true }
+  }
   if (manageCallBack(ast, callback) && ast.children && ast.children.length > 0) {
     for (var k in ast.children) {
       var child = ast.children[k]
@@ -24,15 +31,11 @@ AstWalker.prototype.walk = function (ast, callback) {
 }
 
 function manageCallBack (node, callback) {
-  if (callback instanceof Function) {
-    return callback(null, node)
-  } else if (callback instanceof Object) {
-    if (callback[node.name] instanceof Function) {
-      return callback[node.name](null, Function)
-    } else if (callback['*'] instanceof 'Function') {
-      return callback['*'](null, node)
-    } else {
-      return true
-    }
+  if (node.name in callback) {
+    return callback[node.name](node)
+  } else {
+    return callback['*'](node)
   }
 }
+
+module.exports = AstWalker
--- test/tests.js
+++ test/tests.js
@@ -3,3 +3,5 @@ require('./index.js')
 require('./traceManager.js')
 require('./codeManager.js')
 require('./util.js')
+require('./astwalker.js')
+
