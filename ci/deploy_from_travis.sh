0df82b6b316db754171e9f03d9f5b171d24cb32e
Denton Liu <liu.denton+github@gmail.com>
2016-08-29 14:40:05 -0400

    Change {} to `` for code highlighting

--- ci/deploy_from_travis.sh
+++ ci/deploy_from_travis.sh
@@ -9,8 +9,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 git checkout --orphan gh-pages
 git rm --cached -r .
 echo "# Automatic build" > README.md
-echo "Built website from {$SHA}. See https://github.com/ethereum/browser-solidity/ for details." >> README.md
-echo "To use an offline copy, download browser-solidity-$SHA.zip." >> README.md
+echo "Built website from \`$SHA\`. See https://github.com/ethereum/browser-solidity/ for details." >> README.md
+echo "To use an offline copy, download \`browser-solidity-$SHA.zip\`." >> README.md
 # ZIP the whole directory
 zip -r browser-solidity-$SHA.zip $FILES_TO_PACKAGE
 # -f is needed because "build" is part of .gitignore
