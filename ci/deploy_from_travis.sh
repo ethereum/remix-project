000e6dee8712e3d69501793981735df27b214d92
Denton Liu <liu.denton+github@gmail.com>
2016-08-29 14:36:19 -0400

    Revert "Use builtin variable"
    This reverts commit 0a3d3fcce0fcb07d8ab080d8b1fa9548a2757a9c.
    
    This was done because @bobsummerwill brought up in ethereum/solidity#952
    that a longer hash may not be desired.

--- ci/deploy_from_travis.sh
+++ ci/deploy_from_travis.sh
@@ -2,18 +2,20 @@
 
 set -e
 
+SHA=`git rev-parse --verify HEAD`
+
 git config user.name "$COMMIT_AUTHOR"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 git checkout --orphan gh-pages
 git rm --cached -r .
 echo "# Automatic build" > README.md
-echo "Built website from {$TRAVIS_COMMIT}. See https://github.com/ethereum/browser-solidity/ for details." >> README.md
-echo "To use an offline copy, download browser-solidity-$TRAVIS_COMMIT.zip." >> README.md
+echo "Built website from {$SHA}. See https://github.com/ethereum/browser-solidity/ for details." >> README.md
+echo "To use an offline copy, download browser-solidity-$SHA.zip." >> README.md
 # ZIP the whole directory
-zip -r browser-solidity-$TRAVIS_COMMIT.zip $FILES_TO_PACKAGE
+zip -r browser-solidity-$SHA.zip $FILES_TO_PACKAGE
 # -f is needed because "build" is part of .gitignore
-git add -f $FILES_TO_PACKAGE browser-solidity-$TRAVIS_COMMIT.zip
-git commit -m "Built website from {$TRAVIS_COMMIT}."
+git add -f $FILES_TO_PACKAGE browser-solidity-$SHA.zip
+git commit -m "Built website from {$SHA}."
 
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
