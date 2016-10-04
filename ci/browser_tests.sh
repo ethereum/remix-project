08a8db02e4c68361f6a118562c286673f5e9da2a
chriseth <c@ethdev.com>
2016-10-04 11:57:20 +0200

    Clarify saucelabs script

--- ci/browser_tests.sh
+++ ci/browser_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-SAUCECONNECT_URL="https://saucelabs.com/downloads/sc-4.3.16-linux.tar.gz"
+SC_VERSION="4.3.16"
+SAUCECONNECT_URL="https://saucelabs.com/downloads/sc-$SC_VERSION-linux.tar.gz"
 SAUCECONNECT_USERNAME="chriseth"
 SAUCECONNECT_ACCESSKEY="b781828a-9e9c-43d8-89d4-2fbb879595ca"
 SAUCECONNECT_JOBIDENTIFIER="browsersolidity_tests_${TRAVIS_JOB_NUMBER}"
@@ -11,10 +11,10 @@ TEST_EXITCODE=0
 npm run build
 npm run serve &
 
-wget $SAUCECONNECT_URL
-tar -zxvf sc-4.3.16-linux.tar.gz
-./sc-4.3.16-linux/bin/sc -u $SAUCECONNECT_USERNAME -k $SAUCECONNECT_ACCESSKEY -i $SAUCECONNECT_JOBIDENTIFIER --readyfile $SAUCECONNECT_READYFILE &
-while [ ! -f $SAUCECONNECT_READYFILE ]; do
+wget "$SAUCECONNECT_URL"
+tar -zxvf sc-"$SC_VERSION"-linux.tar.gz
+./sc-"$SC_VERSION"-linux/bin/sc -u "$SAUCECONNECT_USERNAME" -k "$SAUCECONNECT_ACCESSKEY" -i "$SAUCECONNECT_JOBIDENTIFIER" --readyfile "$SAUCECONNECT_READYFILE" &
+while [ ! -f "$SAUCECONNECT_READYFILE" ]; do
   sleep .5
 done
 
@@ -23,10 +23,10 @@ npm run browser-test-remote-chrome || TEST_EXITCODE=1
 npm run browser-test-remote-safari || TEST_EXITCODE=1
 npm run browser-test-remote-ie || TEST_EXITCODE=1
 
-node ci/sauceDisconnect.js $SAUCECONNECT_USERNAME $SAUCECONNECT_ACCESSKEY $SAUCECONNECT_JOBIDENTIFIER
+node ci/sauceDisconnect.js "$SAUCECONNECT_USERNAME" "$SAUCECONNECT_ACCESSKEY" "$SAUCECONNECT_JOBIDENTIFIER"
 
-echo $TEST_EXITCODE
-if [ $TEST_EXITCODE -eq 1 ]
+echo "$TEST_EXITCODE"
+if [ "$TEST_EXITCODE" -eq 1 ]
 then
   exit 1
 fi
