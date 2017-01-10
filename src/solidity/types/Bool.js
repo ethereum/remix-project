422b042e32d5d1dadc5f72b69ea0784b7b8f886d
yann300 <yann.levreau@gmail.com>
2017-01-10 13:42:56 +0100

    es6 class function

--- src/solidity/types/Bool.js
+++ src/solidity/types/Bool.js
@@ -6,24 +6,24 @@ class Bool extends ValueType {
   constructor () {
     super(1, 1, 'bool')
   }
-}
 
-Bool.prototype.decodeFromStorage = function (location, storageContent) {
-  var value = util.extractHexValue(location, storageContent, this.storageBytes)
-  return value !== '00'
-}
-
-Bool.prototype.decodeFromStack = function (stackDepth, stack, memory) {
-  if (stack.length - 1 < stackDepth) {
-    return false
-  } else {
-    return util.extractHexByteSlice(stack[stack.length - 1 - stackDepth], this.storageBytes, 0) !== '00'
+  decodeFromStorage (location, storageContent) {
+    var value = util.extractHexValue(location, storageContent, this.storageBytes)
+    return value !== '00'
   }
-}
 
-Bool.prototype.decodeFromMemory = function (offset, memory) {
-  var value = memory.substr(offset, 64)
-  return util.extractHexByteSlice(value, this.storageBytes, 0) !== '00'
+  decodeFromStack (stackDepth, stack, memory) {
+    if (stack.length - 1 < stackDepth) {
+      return false
+    } else {
+      return util.extractHexByteSlice(stack[stack.length - 1 - stackDepth], this.storageBytes, 0) !== '00'
+    }
+  }
+
+  decodeFromMemory (offset, memory) {
+    var value = memory.substr(offset, 64)
+    return util.extractHexByteSlice(value, this.storageBytes, 0) !== '00'
+  }
 }
 
 module.exports = Bool
