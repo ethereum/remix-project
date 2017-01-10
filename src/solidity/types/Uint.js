60ce2bbbe1b7736feb1432552d3445cd7b730fab
yann300 <yann.levreau@gmail.com>
2017-01-10 13:43:41 +0100

    es6 class function

--- src/solidity/types/Uint.js
+++ src/solidity/types/Uint.js
@@ -6,23 +6,23 @@ class Uint extends ValueType {
   constructor (storageBytes) {
     super(1, storageBytes, 'uint')
   }
-}
 
-Uint.prototype.decodeFromStorage = function (location, storageContent) {
-  return util.decodeInt(location, storageContent, this.storageBytes, false)
-}
-
-Uint.prototype.decodeFromStack = function (stackDepth, stack, memory) {
-  if (stackDepth >= stack.length) {
-    return '0'
-  } else {
-    return util.decodeIntFromHex(stack[stack.length - 1 - stackDepth].replace('0x', ''), this.storageBytes, false)
+  decodeFromStorage (location, storageContent) {
+    return util.decodeInt(location, storageContent, this.storageBytes, false)
   }
-}
 
-Uint.prototype.decodeFromMemory = function (offset, memory) {
-  var value = memory.substr(offset, 64)
-  return util.decodeIntFromHex(value, this.storageBytes, false)
+  decodeFromStack (stackDepth, stack, memory) {
+    if (stackDepth >= stack.length) {
+      return '0'
+    } else {
+      return util.decodeIntFromHex(stack[stack.length - 1 - stackDepth].replace('0x', ''), this.storageBytes, false)
+    }
+  }
+
+  decodeFromMemory (offset, memory) {
+    var value = memory.substr(offset, 64)
+    return util.decodeIntFromHex(value, this.storageBytes, false)
+  }
 }
 
 module.exports = Uint
