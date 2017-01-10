b9decda1d7a33aae48845e87e1ab74a34cea7fa1
yann300 <yann.levreau@gmail.com>
2017-01-10 13:43:09 +0100

    es6 class function

--- src/solidity/types/Enum.js
+++ src/solidity/types/Enum.js
@@ -13,30 +13,30 @@ class Enum extends ValueType {
     super(1, storageBytes, 'enum')
     this.enumDef = enumDef
   }
-}
 
-Enum.prototype.decodeFromStorage = function (location, storageContent) {
-  var value = util.extractHexValue(location, storageContent, this.storageBytes)
-  value = parseInt(value, 16)
-  return output(value, this.enumDef)
-}
-
-Enum.prototype.decodeFromStack = function (stackDepth, stack, memory) {
-  var defaultValue = 0
-  if (stack.length - 1 < stackDepth) {
-    defaultValue = 0
-  } else {
-    defaultValue = util.extractHexByteSlice(stack[stack.length - 1 - stackDepth], this.storageBytes, 0)
-    defaultValue = parseInt(defaultValue, 16)
+  decodeFromStorage (location, storageContent) {
+    var value = util.extractHexValue(location, storageContent, this.storageBytes)
+    value = parseInt(value, 16)
+    return output(value, this.enumDef)
   }
-  return output(defaultValue, this.enumDef)
-}
 
-Enum.prototype.decodeFromMemory = function (offset, memory) {
-  var value = memory.substr(offset, 64)
-  value = util.extractHexByteSlice(value, this.storageBytes, 0)
-  value = parseInt(value, 16)
-  return output(value, this.enumDef)
+  decodeFromStack (stackDepth, stack, memory) {
+    var defaultValue = 0
+    if (stack.length - 1 < stackDepth) {
+      defaultValue = 0
+    } else {
+      defaultValue = util.extractHexByteSlice(stack[stack.length - 1 - stackDepth], this.storageBytes, 0)
+      defaultValue = parseInt(defaultValue, 16)
+    }
+    return output(defaultValue, this.enumDef)
+  }
+
+  decodeFromMemory (offset, memory) {
+    var value = memory.substr(offset, 64)
+    value = util.extractHexByteSlice(value, this.storageBytes, 0)
+    value = parseInt(value, 16)
+    return output(value, this.enumDef)
+  }
 }
 
 function output (value, enumDef) {
