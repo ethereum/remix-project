bf9d14524facc9ebc6fe4fe2236e0c8fc486f04e
chriseth <c@ethdev.com>
2017-01-18 11:05:37 +0100

    Also deploy to remix-live repository.

--- ci/deploy_from_travis.sh
+++ ci/deploy_from_travis.sh
@@ -10,20 +10,26 @@ git checkout --orphan gh-pages
 git rm --cached -r .
 echo "# Automatic build" > README.md
 echo "Built website from \`$SHA\`. See https://github.com/ethereum/browser-solidity/ for details." >> README.md
-echo "To use an offline copy, download \`browser-solidity-$SHA.zip\`." >> README.md
+echo "To use an offline copy, download \`remix-$SHA.zip\`." >> README.md
 # ZIP the whole directory
-zip -r browser-solidity-$SHA.zip $FILES_TO_PACKAGE
+zip -r remix-$SHA.zip $FILES_TO_PACKAGE
 # -f is needed because "build" is part of .gitignore
-git add -f $FILES_TO_PACKAGE browser-solidity-$SHA.zip
+git add -f $FILES_TO_PACKAGE remix-$SHA.zip
 git commit -m "Built website from {$SHA}."
 
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+touch deploy_key
+touch deploy_key_remix-live
+chmod 600 deploy_key deploy_key_remix-live
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ci/deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ci/deploy_key_remix-live.enc -out deploy_key_remix-live -d
 eval `ssh-agent -s`
 ssh-add deploy_key
+ssh-add deploy_key_remix-live
 
-git push -f "$PUSH_REPO" gh-pages
+git push -f git@github.com:ethereum/browser-solidity.git gh-pages
+git push -f git@github.com:ethereum/remix-live.git gh-pages
