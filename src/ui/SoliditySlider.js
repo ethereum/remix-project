b5ae84175717877e669436ea4aba964ac671b630
yann300 <yann.levreau@gmail.com>
2017-01-26 17:28:29 +0100

    add setReducedTrace

--- src/ui/SoliditySlider.js
+++ src/ui/SoliditySlider.js
@@ -3,12 +3,18 @@ var Slider = require('./Slider')
 var utils = require('../helpers/util.js')
 
 class SoliditySlider extends Slider {
-  constructor (_traceManager, reducedTraceBySourceLocation) {
+  constructor (_traceManager) {
     super(_traceManager)
-    this.reducedTraceBySourceLocation = reducedTraceBySourceLocation
+    this.reducedTraceBySourceLocation
   }
-  setValue (value) {
-    super.setValue(utils.findLowerBoundValue(value, this.reducedTraceBySourceLocation))
+  setValue (_value) {
+    if (this.reducedTraceBySourceLocation) {
+      super.setValue(utils.findLowerBound(_value, this.reducedTraceBySourceLocation))
+    }
+  }
+  setReducedTrace (_reducedTraceBySourceLocation) {
+    super.init(_reducedTraceBySourceLocation.length)
+    this.reducedTraceBySourceLocation = _reducedTraceBySourceLocation
   }
 }
 
