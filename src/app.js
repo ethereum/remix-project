17a810e011371e9d35bccbd84b5deb8286747bab
Michael Fröwis <mfrowis@localhost.localdomain>
2017-03-14 10:03:16 +0100

    fix editorWindowSize getting negative

--- src/app.js
+++ src/app.js
@@ -462,7 +462,7 @@ var run = function () {
       $('#ghostbar').remove()
       $(document).unbind('mousemove')
       dragging = false
-      delta = (delta<50)?50:delta
+      delta = (delta < 50) ? 50 : delta
       setEditorSize(delta)
       config.set(EDITOR_WINDOW_SIZE, delta)
       reAdjust()
