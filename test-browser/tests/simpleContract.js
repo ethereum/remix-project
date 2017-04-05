0767dcf68745486770f44233747d939c26e65310
yann300 <yann.levreau@gmail.com>
2017-04-05 17:37:31 +0200

    Update simpleContract.js

--- test-browser/tests/simpleContract.js
+++ test-browser/tests/simpleContract.js
@@ -25,6 +25,7 @@ module.exports = {
 function runTests (browser) {
   browser
     .waitForElementVisible('.newFile', 10000)
+    .click('.envView')
   contractHelper.testContracts(browser, sources.sources.Untitled, ['Untitled:test1', 'Untitled:test2'], function () {
     browser.end()
   })
