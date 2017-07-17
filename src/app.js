5fdd77f0462c3b50a394784c636aafff91817902
yann300 <yann.levreau@gmail.com>
2017-07-17 17:33:16 +0200

    standard

--- src/app.js
+++ src/app.js
@@ -753,7 +753,6 @@ var run = function () {
           switchToFile(source)
         }
         this.statementMarker = editor.addMarker(lineColumnPos, 'highlightcode')
-        
         editor.scrollToLine(lineColumnPos.start.line, true, true, function () {})
 
         if (lineColumnPos.start.line === lineColumnPos.end.line) {
