4844ef7bf4fef218d407271e65b7a97ce5a2240f
ninabreznik <ninabreznik@gmail.com>
2017-08-03 03:43:03 +0200

    Style support tab - add info box with links and style chat

--- src/app/support-tab.js
+++ src/app/support-tab.js
@@ -11,22 +11,42 @@ var css = csjs`
     padding: 2%;
     margin-top: 1em;
     padding-bottom: 3em;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
   }
   .chatIframe {
     width: 102%;
-    height: 120%;
-    border: 0;
+    height: 85%;
+    border: 2px dotted ${styles.colors.lightGrey};
     overflow: hidden;
+    transform:
+      scale(0.9);
+  }
+  .infoBox extends ${styles.infoTextBox}{
+
   }
 `
-// transform:
-// translate(-8%, -12%) scale(0.8);
+
+var infoText = yo`
+  <div>
+    Have a question, found a bug or want to propose a feature? Have a look at the
+    <a href='https://github.com/ethereum/browser-solidity/issues'> issues</a> or check out
+    <a href='https://remix.readthedocs.io/en/latest/'> the documentation page</a>.
+    Or join our chat below.
+  </div>
+`
 
 module.exports = supportTab
 
 function supportTab (container, appAPI, events, opts) {
   var el = yo`
     <div class="${css.supportTabView} "id="supportView">
+      <div>
+        <div class="${css.infoBox}">
+          ${infoText}
+        </div>
+      </div>
       <iframe class="${css.chatIframe}" src='https://gitter.im/ethereum/remix/~embed'>
     </div>
   `
