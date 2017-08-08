fe57e4480adb7bdd03b98fe70247f72fe6103ecb
yann300 <yann.levreau@gmail.com>
2017-08-08 11:16:49 +0200

    update debugger tab styling

--- src/app/debugger-tab.js
+++ src/app/debugger-tab.js
@@ -2,16 +2,24 @@ var yo = require('yo-yo')
 
 // -------------- styling ----------------------
 var csjs = require('csjs-inject')
+var styleGuide = require('./style-guide')
+var styles = styleGuide()
 
 var css = csjs`
   .debuggerTabView {
     padding: 2%;
+  }
+  .debugger extends ${styles.displayBox} {
+    margin-bottom: 1%;
   }
 `
 
 module.exports = debuggerTab
 
 function debuggerTab (container, appAPI, events, opts) {
-  var el = yo` <div class="${css.debuggerTabView} "id="debugView"><div id="debugger"></div></div>`
+  var el = yo`
+    <div class="${css.debuggerTabView} "id="debugView">
+      <div id="debugger" class="${css.debugger}"></div>
+    </div>`
   container.appendChild(el)
 }
