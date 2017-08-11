a6c01737018c4cf7632ad23f8fee65e3aeaf115a
ninabreznik <ninabreznik@gmail.com>
2017-08-11 14:42:36 +0100

    Add onclick functions to file-panel, remove file-tab and move all connected APIs etc. to file-panel

--- src/app/panels/file-panel.js
+++ src/app/panels/file-panel.js
@@ -1,11 +1,14 @@
 
 var csjs = require('csjs-inject')
 var yo = require('yo-yo')
-
+var minixhr = require('minixhr')  // simple and small cross-browser XMLHttpRequest (XHR)
+var $ = require('jquery')
 var EventManager = require('ethereum-remix').lib.EventManager
 var FileExplorer = require('../files/file-explorer')
 var modalDialog = require('../ui/modaldialog')
 var modalDialogCustom = require('../ui/modal-dialog-custom')
+var QueryParams = require('./query-params')
+var queryParams = new QueryParams()
 
 module.exports = filepanel
 
@@ -132,10 +135,10 @@ function filepanel (appAPI, filesProvider) {
                 </label>
               </span>
             ` : ''}
-            <span class="${css.gist}">
+            <span class="${css.gist}" title="Publish all open files to an anonymous github gist" onclick=${() => publishToGist(appAPI)}>
               <i class="fa fa-github"></i>
             </span>
-            <span class="${css.copyFiles}">
+            <span class="${css.copyFiles}" title="Copy all files to another instance of Browser-solidity" onclick=${copyFiles}>
               <i class="fa fa-files-o" aria-hidden="true"></i>
             </span>
             <span onclick=${connectToLocalhost} class="${css.connectToLocalhost}">
@@ -273,5 +276,63 @@ function filepanel (appAPI, filesProvider) {
             })
           }})
     }
+  }
+
+  // ------------------ gist publish --------------
+
+  function publishToGist () {
+    if (confirm('Are you sure you want to publish all your files anonymously as a public gist on github.com?')) {
+      appAPI.packageFiles((error, packaged) => {
+        if (error) {
+          console.log(error)
+        } else {
+          var description = 'Created using browser-solidity: Realtime Ethereum Contract Compiler and Runtime. \n Load this file by pasting this gists URL or ID at https://ethereum.github.io/browser-solidity/#version=' + queryParams.get().version + '&optimize=' + queryParams.get().optimize + '&gist='
+          console.log(packaged)
+          minixhr({
+            url: 'https://api.github.com/gists',
+            method: 'POST',
+            data: JSON.stringify({
+              description: description,
+              public: true,
+              files: packaged
+            })
+          }, cb)
+          function cb(data) {
+            if (data instanceof Error) {
+              console.log('fail', data.message)
+              modalDialogCustom.alert('Failed to create gist: ' + (data || 'Unknown transport error'))
+            } else {
+              data = JSON.parse(data)
+              if (data.html_url && confirm('Created a gist at ' + data.html_url + ' Would you like to open it in a new window?')) {
+                window.open(data.html_url, '_blank')
+              }
+            }
+          }
+        }
+      })
+    }
+  }
+
+  // ------------------ copy files --------------
+
+  function copyFiles () {
+    var target = prompt(
+      'To which other browser-solidity instance do you want to copy over all files?',
+      'https://ethereum.github.io/browser-solidity/'
+    )
+    if (target === null) {
+      return
+    }
+    appAPI.packageFiles((error, packaged) => {
+      if (error) {
+        console.log(error)
+      } else {
+        $('<iframe/>', {
+          src: target,
+          style: 'display:none;',
+          load: function () { this.contentWindow.postMessage(['loadFiles', packaged], '*') }
+        }).appendTo('body')
+      }
+    })
   }
 }
