69aea682b56011dc46baed73123b7825ccf44874
yann300 <yann.levreau@gmail.com>
2017-09-28 11:27:11 +0200

    don't confirm if not needed

--- src/app/files/chromeCloudStorageSync.js
+++ src/app/files/chromeCloudStorageSync.js
@@ -19,21 +19,23 @@ module.exports = function (filesProviders) {
         modalDialogCustom.confirm('', 'Overwrite "' + key + '"? Click Ok to overwrite local file with file from cloud. Cancel will push your local file to the cloud.', () => { callback(true) }, () => { callback(false) })
       }
 
-      confirmDialog(function (theResult) {
-        if (typeof resp[key] !== 'undefined' && obj[key] !== resp[key] && theResult) {
-          console.log('Overwriting', key)
-          filesProviders['browser'].set(key, resp[key])
-        } else {
-          console.log('add to obj', obj, key)
-          filesProviders['browser'].get(key, (error, content) => {
-            if (error) {
-              console.log(error)
-            } else {
-              obj[key] = content
-            }
-          })
-        }
-      })
+      if (typeof resp[key] !== 'undefined' && obj[key] !== resp[key]) {
+        confirmDialog((result) => {
+          if (result) {
+            console.log('Overwriting', key)
+            filesProviders['browser'].set(key, resp[key])
+          }
+        })
+      } else {
+        console.log('add to obj', obj, key)
+        filesProviders['browser'].get(key, (error, content) => {
+          if (error) {
+            console.log(error)
+          } else {
+            obj[key] = content
+          }
+        })
+      }
 
       done++
       if (done >= count) {
