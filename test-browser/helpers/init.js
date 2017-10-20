aaf203192b8058c014549a6a82f822efa4fc827b
Zac Kwan <Zaccc123@users.noreply.github.com>
2017-10-20 08:03:08 +0800

    perform a click on autoCompile to enable it

--- test-browser/helpers/init.js
+++ test-browser/helpers/init.js
@@ -2,6 +2,9 @@ module.exports = function (browser, callback) {
   browser
     .url('http://127.0.0.1:8080/#version=builtin')
     .injectScript('test-browser/helpers/applytestmode.js', function () {
-      browser.resizeWindow(2560, 1440, callback)
+      browser.click('#autoCompile')
+      .perform(function () {
+        callback()
+      })
     })
 }
