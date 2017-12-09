8dd759999e1ed9325526cc3af3bf83146185f752
yann300 <yann.levreau@gmail.com>
2017-12-09 15:53:55 +0100

    test library linkage

--- test-browser/tests/units/testRecorder.js
+++ test-browser/tests/units/testRecorder.js
@@ -8,9 +8,10 @@ module.exports = function (browser, callback) {
     .click('#runTabView .runtransaction')
     .clickFunction('getInt - call')
     .clickFunction('getAddress - call')
+    .clickFunction('getFromLib - call')
     .waitForElementPresent('div[class^="contractProperty"] div[class^="value"]')
     .perform(() => {
-      contractHelper.verifyCallReturnValue(browser, '0xec5bee2dbb67da8757091ad3d9526ba3ed2e2137', ['0: uint256: 1', '0: address: 0xca35b7d915458ef540ade6068dfe2f44e8fa733c'], () => { callback() })
+      contractHelper.verifyCallReturnValue(browser, '0xec5bee2dbb67da8757091ad3d9526ba3ed2e2137', ['0: uint256: 1', '1: uint256: 3456', '2: address: 0xca35b7d915458ef540ade6068dfe2f44e8fa733c'], () => { callback() })
     })
   })
 }
@@ -19,46 +20,100 @@ var records = `{
   "accounts": {
     "account{0}": "0xca35b7d915458ef540ade6068dfe2f44e8fa733c"
   },
+  "linkReferences": {
+    "testLib": "created{1512830014773}"
+  },
   "transactions": [
     {
-      "timestamp": 1512661974836,
+      "timestamp": 1512830014773,
       "record": {
         "value": "0",
-        "parameters": [
-          23
-        ],
-        "abi": "0xe8e77626586f73b955364c7b4bbf0bb7f7685ebd40e852b164633a4acbd3244c",
-        "contractName": "test",
-        "bytecode": "6060604052341561000f57600080fd5b6040516020806102098339810160405280805190602001909190505080600081905550506101c7806100426000396000f300606060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680632f30c6f61461005c57806338cc48311461009e57806362738998146100f3575b600080fd5b341561006757600080fd5b61009c600480803590602001909190803573ffffffffffffffffffffffffffffffffffffffff1690602001909190505061011c565b005b34156100a957600080fd5b6100b1610168565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b34156100fe57600080fd5b610106610192565b6040518082815260200191505060405180910390f35b8160008190555080600160006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505050565b6000600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff16905090565b600080549050905600a165627a7a7230582021de204df5493bae860efacfbdea0118cbb3d73760ad48ee0838d07d37fe3c9e0029",
+        "parameters": [],
+        "abi": "0xbc36789e7a1e281436464229828f817d6612f7b477d66591ff96a9e064bcc98a",
+        "contractName": "testLib",
+        "bytecode": "60606040523415600e57600080fd5b60968061001c6000396000f300606060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680636d4ce63c146044575b600080fd5b604a6060565b6040518082815260200191505060405180910390f35b6000610d809050905600a165627a7a7230582022d123b15248b8176151f8d45c2dc132063bcc9bb8d5cd652aea7efae362c8050029",
         "linkReferences": {},
+        "type": "constructor",
+        "from": "account{0}"
+      }
+    },
+    {
+      "timestamp": 1512830015080,
+      "record": {
+        "value": "100",
+        "parameters": [
+          11
+        ],
+        "abi": "0xc41589e7559804ea4a2080dad19d876a024ccb05117835447d72ce08c1d020ec",
+        "contractName": "test",
+        "bytecode": "60606040526040516020806102b183398101604052808051906020019091905050806000819055505061027a806100376000396000f300606060405260043610610062576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680632f30c6f61461006757806338cc48311461009e57806362738998146100f357806387cc10e11461011c575b600080fd5b61009c600480803590602001909190803573ffffffffffffffffffffffffffffffffffffffff16906020019091905050610145565b005b34156100a957600080fd5b6100b1610191565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b34156100fe57600080fd5b6101066101bb565b6040518082815260200191505060405180910390f35b341561012757600080fd5b61012f6101c4565b6040518082815260200191505060405180910390f35b8160008190555080600160006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505050565b6000600160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff16905090565b60008054905090565b600073__browser/ballot.sol:testLib____________636d4ce63c6000604051602001526040518163ffffffff167c010000000000000000000000000000000000000000000000000000000002815260040160206040518083038186803b151561022e57600080fd5b6102c65a03f4151561023f57600080fd5b505050604051805190509050905600a165627a7a72305820e0b2510bb2890a0334bfe5613d96db3e72442e63b514cdeaee8fc2c6bbd19d3a0029",
+        "linkReferences": {
+          "browser/ballot.sol": {
+            "testLib": [
+              {
+                "length": 20,
+                "start": 511
+              }
+            ]
+          }
+        },
         "name": "",
         "type": "constructor",
         "from": "account{0}"
       }
     },
     {
-      "timestamp": 1512661987446,
+      "timestamp": 1512830034180,
       "record": {
-        "value": "0",
+        "value": "1000000000000000000",
         "parameters": [
           1,
           "0xca35b7d915458ef540ade6068dfe2f44e8fa733c"
         ],
-        "to": "created-contract{1512661974836}",
-        "abi": "0xe8e77626586f73b955364c7b4bbf0bb7f7685ebd40e852b164633a4acbd3244c",
+        "to": "created{1512830015080}",
+        "abi": "0xc41589e7559804ea4a2080dad19d876a024ccb05117835447d72ce08c1d020ec",
         "name": "set",
         "type": "function",
         "from": "account{0}"
       }
     }
   ],
-  "linkReferences": {},
   "abis": {
-    "0xe8e77626586f73b955364c7b4bbf0bb7f7685ebd40e852b164633a4acbd3244c": [
+    "0xbc36789e7a1e281436464229828f817d6612f7b477d66591ff96a9e064bcc98a": [
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "get",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ],
+    "0xc41589e7559804ea4a2080dad19d876a024ccb05117835447d72ce08c1d020ec": [
       {
         "constant": true,
         "inputs": [],
         "name": "getInt",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "getFromLib",
         "outputs": [
           {
             "name": "",
@@ -97,8 +152,8 @@ var records = `{
         ],
         "name": "set",
         "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
+        "payable": true,
+        "stateMutability": "payable",
         "type": "function"
       },
       {
@@ -108,8 +163,8 @@ var records = `{
             "type": "uint256"
           }
         ],
-        "payable": false,
-        "stateMutability": "nonpayable",
+        "payable": true,
+        "stateMutability": "payable",
         "type": "constructor"
       }
     ]
