99e4e584c46bd9ae76d7289477026e0fe3feddf9
yann300 <yann.levreau@gmail.com>
2018-01-02 16:55:53 +0100

    test mapping decoding

--- remix-solidity/test/decoder/contracts/mappingStorage.js
+++ remix-solidity/test/decoder/contracts/mappingStorage.js
@@ -5,9 +5,11 @@ module.exports = {
 contract SimpleMappingState {
     uint _num;
     mapping(string => uint) _iBreakSolidityState;
+    mapping(uint => uint) _iBreakSolidityStateInt;
     function updateNum(uint num, string str) public {
         _num = num;
         _iBreakSolidityState[str] = num;
+        _iBreakSolidityStateInt[num] = num;
     }
 }
   `
