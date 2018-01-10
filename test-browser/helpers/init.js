2b97c47a785c2b57f7e3f05f0ff01ee98a1aaf75
yann300 <yann.levreau@gmail.com>
2018-01-10 14:29:41 +0100

    Update init.js

--- test-browser/helpers/init.js
+++ test-browser/helpers/init.js
@@ -2,9 +2,6 @@ module.exports = function (browser, callback) {
   browser
     .url('http://127.0.0.1:8080/#version=builtin')
     .injectScript('test-browser/helpers/applytestmode.js', function () {
-      browser.click('#autoCompile')
-      .perform(function () {
-        callback()
-      })
+      browser.resizeWindow(2560, 1440, callback)
     })
 }
