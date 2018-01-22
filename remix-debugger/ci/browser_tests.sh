23064fb52c639ec26efb22aeba3a7b4b89f47629
Iuri Matias <iuri.matias@gmail.com>
2018-01-22 17:00:05 -0500

    support running browser tests in osx

--- remix-debugger/ci/browser_tests.sh
+++ remix-debugger/ci/browser_tests.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-SC_VERSION="4.4.0"
-SAUCECONNECT_URL="https://saucelabs.com/downloads/sc-$SC_VERSION-linux.tar.gz"
+if test $(uname -s) = "Darwin"
+then
+  OS="osx"
+  FILEFORMAT="zip"
+else
+  OS="linux"
+  FILEFORMAT="tar.gz"
+fi
+SC_VERSION="4.4.11"
+SAUCECONNECT_URL="https://saucelabs.com/downloads/sc-$SC_VERSION-$OS.$FILEFORMAT"
 SAUCECONNECT_USERNAME="yanneth"
 SAUCECONNECT_ACCESSKEY="1f5a4560-b02b-41aa-b52b-f033aad30870"
 SAUCECONNECT_JOBIDENTIFIER="remix_tests_${TRAVIS_JOB_NUMBER}"
@@ -14,8 +22,8 @@ npm run build
 npm run serve &
 
 wget $SAUCECONNECT_URL
-tar -zxvf sc-"$SC_VERSION"-linux.tar.gz
-./sc-"$SC_VERSION"-linux/bin/sc -u $SAUCECONNECT_USERNAME -k $SAUCECONNECT_ACCESSKEY -i $SAUCECONNECT_JOBIDENTIFIER --readyfile $SAUCECONNECT_READYFILE &
+tar -zxvf sc-"$SC_VERSION"-"$OS"."$FILEFORMAT"
+./sc-"$SC_VERSION"-$OS/bin/sc -u $SAUCECONNECT_USERNAME -k $SAUCECONNECT_ACCESSKEY -i $SAUCECONNECT_JOBIDENTIFIER --readyfile $SAUCECONNECT_READYFILE &
 while [ ! -f $SAUCECONNECT_READYFILE ]; do
   sleep .5
 done
