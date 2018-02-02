a23965a2709bb4a6b4b9a8b41c73951fb74aa6a2
Iuri Matias <iuri.matias@gmail.com>
2018-02-02 18:54:43 -0500

    add another describe to the test to reflect the method being tested

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -23,71 +23,73 @@ function compileAndDeploy (filename, callback) {
 }
 
 describe('testRunner', function () {
-  describe('test with beforeAll', function () {
-    let filename = 'tests/examples_1/simple_storage_test.sol'
-    let tests = [], results = {}
+  describe('#runTest', function() {
+    describe('test with beforeAll', function () {
+      let filename = 'tests/examples_1/simple_storage_test.sol'
+      let tests = [], results = {}
 
-    before(function (done) {
-      compileAndDeploy(filename, function (_err, contracts) {
-        var testCallback = function (test) {
-          tests.push(test)
-        }
-        var resultsCallback = function (_err, _results) {
-          results = _results
-          done()
-        }
-        TestRunner.runTest('MyTest', contracts.MyTest, testCallback, resultsCallback)
+      before(function (done) {
+        compileAndDeploy(filename, function (_err, contracts) {
+          var testCallback = function (test) {
+            tests.push(test)
+          }
+          var resultsCallback = function (_err, _results) {
+            results = _results
+            done()
+          }
+          TestRunner.runTest('MyTest', contracts.MyTest, testCallback, resultsCallback)
+        })
+      })
+
+      it('should 1 passing test', function () {
+        assert.equal(results.passingNum, 1)
+      })
+
+      it('should 1 failing test', function () {
+        assert.equal(results.failureNum, 1)
+      })
+
+      it('should returns 3 messages', function () {
+        assert.deepEqual(tests, [
+          { type: 'contract',    value: 'MyTest' },
+          { type: 'testPass',    value: 'Initial value should be100', time: 1 },
+          { type: 'testFailure', value: 'Initial value should be200', time: 1 }
+        ])
       })
     })
 
-    it('should 1 passing test', function () {
-      assert.equal(results.passingNum, 1)
-    })
+    describe('test with beforeEach', function () {
+      let filename = 'tests/examples_2/simple_storage_test.sol'
+      let tests = [], results = {}
 
-    it('should 1 failing test', function () {
-      assert.equal(results.failureNum, 1)
-    })
-
-    it('should returns 3 messages', function () {
-      assert.deepEqual(tests, [
-        { type: 'contract',    value: 'MyTest' },
-        { type: 'testPass',    value: 'Initial value should be100', time: 1 },
-        { type: 'testFailure', value: 'Initial value should be200', time: 1 }
-      ])
-    })
-  })
-
-  describe('test with beforeEach', function () {
-    let filename = 'tests/examples_2/simple_storage_test.sol'
-    let tests = [], results = {}
-
-    before(function (done) {
-      compileAndDeploy(filename, function (_err, contracts) {
-        var testCallback = function (test) {
-          tests.push(test)
-        }
-        var resultsCallback = function (_err, _results) {
-          results = _results
-          done()
-        }
-        TestRunner.runTest('MyTest', contracts.MyTest, testCallback, resultsCallback)
+      before(function (done) {
+        compileAndDeploy(filename, function (_err, contracts) {
+          var testCallback = function (test) {
+            tests.push(test)
+          }
+          var resultsCallback = function (_err, _results) {
+            results = _results
+            done()
+          }
+          TestRunner.runTest('MyTest', contracts.MyTest, testCallback, resultsCallback)
+        })
       })
-    })
 
-    it('should 2 passing tests', function () {
-      assert.equal(results.passingNum, 2)
-    })
+      it('should 2 passing tests', function () {
+        assert.equal(results.passingNum, 2)
+      })
 
-    it('should 0 failing tests', function () {
-      assert.equal(results.failureNum, 0)
-    })
+      it('should 0 failing tests', function () {
+        assert.equal(results.failureNum, 0)
+      })
 
-    it('should returns 3 messages', function () {
-      assert.deepEqual(tests, [
-        { type: 'contract', value: 'MyTest' },
-        { type: 'testPass', value: 'Initial value should be100', time: 1 },
-        { type: 'testPass', value: 'Initial value should be200', time: 1 }
-      ])
+      it('should returns 3 messages', function () {
+        assert.deepEqual(tests, [
+          { type: 'contract', value: 'MyTest' },
+          { type: 'testPass', value: 'Initial value should be100', time: 1 },
+          { type: 'testPass', value: 'Initial value should be200', time: 1 }
+        ])
+      })
     })
   })
 })
