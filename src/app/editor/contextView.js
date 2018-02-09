1ee0e2a8e39c638465795aa5cf23c21faf438649
Iuri Matias <iuri.matias@gmail.com>
2018-02-09 16:09:15 -0500

    move contextView styles to its own file

--- src/app/editor/contextView.js
+++ src/app/editor/contextView.js
@@ -1,57 +1,9 @@
 'use strict'
 var yo = require('yo-yo')
-var csjs = require('csjs-inject')
 var remixLib = require('remix-lib')
-var styleGuide = remixLib.ui.themeChooser
-var styles = styleGuide.chooser()
 var SourceMappingDecoder = remixLib.SourceMappingDecoder
 
-var css = csjs`
-  .contextview            {
-      opacity           : 0.8;
-    }
-  .container              {
-    padding             : 1px 15px;
-  }
-  .line                   {
-    display             : flex;
-    justify-content     : flex-end;
-    align-items         : center;
-    text-overflow       : ellipsis;
-    overflow            : hidden;
-    white-space         : nowrap;
-    color               : ${styles.editor.text_Primary};
-    font-size           : 11px;
-  }
-  .type                   {
-    font-style        : italic;
-    margin-right      : 5px;
-  }
-  .name                   {
-    font-weight       : bold;
-  }
-  .jump                   {
-    cursor            : pointer;
-    margin            : 0 5px;
-    color             : ${styles.editor.icon_Color_Editor};
-  }
-  .jump:hover              {
-    color             : ${styles.editor.icon_HoverColor_Editor};
-  }
-  .referencesnb           {
-    float             : right;
-    margin-left       : 15px;
-  }
-  .gasEstimation {
-    margin-left: 15px;
-    display: flex;
-    align-items: center;
-  }
-  .gasStationIcon {
-    height: 13px;
-    margin-right: 5px;
-  }
-`
+var css = require('./styles/contextView-styles')
 
 /*
   Display information about the current focused code:
