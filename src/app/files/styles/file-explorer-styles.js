cbcae2b50680f03a41aa7ed18f3e4b49f04b3644
Iuri Matias <iuri.matias@gmail.com>
2018-02-09 16:16:05 -0500

    move file-explorer styles to its own file

---
+++ src/app/files/styles/file-explorer-styles.js
@@ -0,0 +1,46 @@
+var csjs = require('csjs-inject')
+var remixLib = require('remix-lib')
+var styleGuide = remixLib.ui.themeChooser
+var styles = styleGuide.chooser()
+
+var css = csjs`
+  .fileexplorer       {
+    box-sizing        : border-box;
+  }
+  input[type="file"] {
+      display: none;
+  }
+  .folder,
+  .file               {
+    font-size         : 14px;
+    cursor            : pointer;
+  }
+  .file               {
+    color             : ${styles.leftPanel.text_Teriary};
+  }
+  .hasFocus           {
+    background-color  : ${styles.leftPanel.backgroundColor_FileExplorer};
+  }
+  .rename             {
+    background-color  : ${styles.leftPanel.backgroundColor_Panel};
+  }
+  .remove             {
+    margin-left       : auto;
+    padding-left      : 5px;
+    padding-right     : 5px;
+  }
+  .activeMode         {
+    display           : flex;
+    width             : 100%;
+    margin-right      : 10px;
+    padding-right     : 19px;
+  }
+  .activeMode > div   {
+    min-width         : 10px;
+  }
+  ul                  {
+    padding           : 0;
+  }
+`
+
+module.exports = css
