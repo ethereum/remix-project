4def8f69760df9427e5ee34d50eb561c86a97247
ninabreznik <ninabreznik@gmail.com>
2018-02-10 23:08:28 +0000

    Restyle transactionActions and unify icons with other icons

--- src/app/tabs/styles/run-tab-styles.js
+++ src/app/tabs/styles/run-tab-styles.js
@@ -109,6 +109,9 @@ var css = csjs`
   .pendingTxsText {
     ${styles.rightPanel.runTab.borderBox_Instance}
     font-style: italic;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
   }
   .item {
     margin-right: 1em;
@@ -141,6 +144,7 @@ var css = csjs`
   .icon {
     cursor: pointer;
     font-size: 12px;
+    cursor: pointer;
     color: ${styles.rightPanel.runTab.icon_Color};
     margin-left: 5px;
   }
@@ -171,13 +175,17 @@ var css = csjs`
   .networkItem {
     margin-right: 5px;
   }
-  .clearinstance {
-    font-size: 20px;
-    cursor: pointer;
-    margin-right: 10px;
-  }
   .transactionActions {
-    float: right;
+    display: flex;
+    width: 50px;
+    justify-content: space-between;
+}
+
+
+
+
+
+
 `
 
 module.exports = css
