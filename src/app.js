6d44d468989bd8058b8a01ed0beb772fc1bfede0
yann300 <yann.levreau@gmail.com>
2018-03-13 11:33:32 +0100

    warn if using https

--- src/app.js
+++ src/app.js
@@ -224,6 +224,10 @@ This instance of Remix you are visiting WILL NOT BE UPDATED.\n
 Please make a backup of your contracts and start using http://remix.ethereum.org`)
   }
 
+  if (window.location.protocol.indexOf('https') === 0) {
+    toolTip('You are using an `https` connection. Please switch to `http` if you are using Remix against an `http Web3 provider` or allow Mixed Content in your browser.')
+  }
+
   function importExternal (url, cb) {
     handleImports.import(url,
       (loadingMsg) => {
