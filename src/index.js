6fa383f0af257d3a41d3e5a1a637f0b89266497e
yann300 <yann.levreau@gmail.com>
2018-03-19 13:03:12 +0100

    add binary && index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  Router: require('./router'),
+  services: {
+    startMistGeth: require('./services/startMistGeth'),
+    startFrontend: require('./services/startFrontend'),
+    autoMine: require('./services/autoMine'),
+    sharedFolder: require('./services/sharedFodler')
+  }
+}
