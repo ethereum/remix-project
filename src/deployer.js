457d3eefc1e07a2ea3fcc0ec5c37181eb6d5b5c0
Iuri Matias <iuri.matias@gmail.com>
2018-03-30 15:08:18 -0400

    add simple provider

--- src/deployer.js
+++ src/deployer.js
@@ -79,10 +79,16 @@ function deployAll (compileResult, web3, callback) {
         let deployObject = contractObject.deploy({arguments: [], data: contractCode})
 
         deployObject.estimateGas().then((gasValue) => {
+          console.dir("got gas estimation " + gasValue);
+          console.dir(accounts);
           deployObject.send({
             from: accounts[0],
             gas: Math.ceil(gasValue * 1.1)
           }).on('receipt', function (receipt) {
+
+            console.dir("==== got the receipt");
+            console.dir(receipt);
+
             contractObject.options.address = receipt.contractAddress
             contractObject.options.from = accounts[0]
             contractObject.options.gas = 5000 * 1000
@@ -91,6 +97,10 @@ function deployAll (compileResult, web3, callback) {
             contracts[contractName] = contractObject
 
             nextEach()
+          }).on('error', function(err) {
+            console.dir("===============");
+            console.dir("error");
+            console.dir(err);
           })
         })
       }, function () {
