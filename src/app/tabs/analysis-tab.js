0483053febaa75cc016c1430ed6491456305a623
serapath <dev@serapath.de>
2018-04-10 18:37:24 +0100

    refactor analysis tab

--- src/app/tabs/analysis-tab.js
+++ src/app/tabs/analysis-tab.js
@@ -1,12 +1,32 @@
 var yo = require('yo-yo')
-var css = require('./styles/analysis-tab-styles')
+var csjs = require('csjs-inject')
+var remixLib = require('remix-lib')
 
-function analysisTab (api = {}, events = {}, opts = {}) {
-  var el = yo`
-    <div class="${css.analysisTabView} "id="staticanalysisView">
-    </div>
-  `
-  return { render () { return el } }
+var EventManager = remixLib.EventManager
+
+module.exports = class AnalysisTab {
+  constructor (opts = { api: {}, events: {} }) {
+    const self = this
+    self.event = new EventManager()
+    self._api = opts.api
+    self._events = opts.events
+    self._view = { el: null }
+    self.data = {}
+    self._components = {}
+  }
+  render () {
+    const self = this
+    if (self._view.el) return self._view.el
+    self._view.el = yo`
+      <div class="${css.analysisTabView} "id="staticanalysisView"></div>`
+    return self._view.el
+  }
 }
-
-module.exports = analysisTab
+const css = csjs`
+  .analysisTabView {
+    padding: 2%;
+    padding-bottom: 3em;
+    display: flex;
+    flex-direction: column;
+  }
+`
