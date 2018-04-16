3c63421fc90d99dcd6807e6d5608d7ad4f7ff895
yann300 <yann.levreau@gmail.com>
2018-04-16 12:40:14 +0200

    fix used key

--- test-browser/plugin/remix.js
+++ test-browser/plugin/remix.js
@@ -9,7 +9,7 @@ window.onload = function () {
   document.querySelector('input#testmessageadd').addEventListener('click', function () {
     window.parent.postMessage(JSON.stringify({
       action: 'request',
-      key: 'editor',
+      key: 'config',
       type: 'setConfig',
       value: [document.getElementById('filename').value, document.getElementById('valuetosend').value],
       id: 34
@@ -19,7 +19,7 @@ window.onload = function () {
   document.querySelector('input#testmessageremove').addEventListener('click', function () {
     window.parent.postMessage(JSON.stringify({
       action: 'request',
-      key: 'editor',
+      key: 'config',
       type: 'removeConfig',
       value: [document.getElementById('filename').value],
       id: 35
@@ -29,7 +29,7 @@ window.onload = function () {
   document.querySelector('input#testmessagerget').addEventListener('click', function () {
     window.parent.postMessage(JSON.stringify({
       action: 'request',
-      key: 'editor',
+      key: 'config',
       type: 'getConfig',
       value: [document.getElementById('filename').value],
       id: 36
