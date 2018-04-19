b0c28a1f85a54da9feca513e886dbac7e25d5bee
yann300 <yann.levreau@gmail.com>
2018-04-19 18:34:11 +0200

    Create index.js

---
+++ embark/index.js
@@ -0,0 +1,16 @@
+var httpServer = require('http-server')
+var remixd = require('remixd')
+
+module.exports = (embark) => {
+  var server = httpServer.createServer({
+    root: __dirname + '/node_modules/remix-ide'
+  })
+
+  embark.registerServiceCheck('Remix IDE', (cb) => {
+    return cb({name: 'Remix IDE (localhost:8080)', status: 'on'})
+  })
+
+  server.listen(8080, '127.0.0.1', function () {})
+  var router = new remixd.Router()
+  router.start(__dirname + '/../../')
+}
