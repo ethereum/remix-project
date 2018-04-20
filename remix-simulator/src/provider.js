949e0fbd1cda35dda20a4204b59b462b915451f4
Iuri Matias <iuri.matias@gmail.com>
2018-04-19 20:50:58 -0400

    fix linting issues

--- remix-simulator/src/provider.js
+++ remix-simulator/src/provider.js
@@ -22,11 +22,11 @@ var Provider = function () {
 
 Provider.prototype.sendAsync = function (payload, callback) {
   const self = this
-  console.dir("payload method is ")
+  console.dir('payload method is ')
   console.dir(payload.method)
 
   if (payload.method === 'eth_accounts') {
-    console.dir('eth_accounts');
+    console.dir('eth_accounts')
     return callback(null, jsonRPCResponse(payload.id, this.accounts.map((x) => x.address)))
   }
   if (payload.method === 'eth_estimateGas') {
@@ -64,39 +64,39 @@ Provider.prototype.sendAsync = function (payload, callback) {
     let address = payload.params[0]
     // let block = payload.params[1]
 
-    callback(null, jsonRPCResponse(payload.id, self.deployedContracts[address] || "0x"))
+    callback(null, jsonRPCResponse(payload.id, self.deployedContracts[address] || '0x'))
   }
   if (payload.method === 'eth_call') {
     processTx(this.accounts, payload, true, callback)
   }
   if (payload.method === 'web3_clientVersion') {
-    callback(null, jsonRPCResponse(payload.id, "Remix Simulator/0.0.1"))
+    callback(null, jsonRPCResponse(payload.id, 'Remix Simulator/0.0.1'))
   }
   if (payload.method === 'shh_version') {
     callback(null, jsonRPCResponse(payload.id, 5))
   }
   if (payload.method === 'eth_getBlockByNumber') {
     let b = {
-      "difficulty": "0x0",
-      "extraData": "0x",
-      "gasLimit": "0x7a1200",
-      "gasUsed": "0x0",
-      "hash": "0xdb731f3622ef37b4da8db36903de029220dba74c41185f8429f916058b86559f",
-      "logsBloom": "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-      "miner": "0x3333333333333333333333333333333333333333",
-      "mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-      "nonce": "0x0000000000000042",
-      "number": "0x0",
-      "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-      "receiptsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-      "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-      "size": "0x1f8",
-      "stateRoot": "0xb7917653f92e62394d2207d0f39a1320ff1cb93d1cee80d3c492627e00b219ff",
-      "timestamp": "0x0",
-      "totalDifficulty": "0x0",
-      "transactions": [],
-      "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-      "uncles": []
+      'difficulty': '0x0',
+      'extraData': '0x',
+      'gasLimit': '0x7a1200',
+      'gasUsed': '0x0',
+      'hash': '0xdb731f3622ef37b4da8db36903de029220dba74c41185f8429f916058b86559f',
+      'logsBloom': '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+      'miner': '0x3333333333333333333333333333333333333333',
+      'mixHash': '0x0000000000000000000000000000000000000000000000000000000000000000',
+      'nonce': '0x0000000000000042',
+      'number': '0x0',
+      'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000000',
+      'receiptsRoot': '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+      'sha3Uncles': '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
+      'size': '0x1f8',
+      'stateRoot': '0xb7917653f92e62394d2207d0f39a1320ff1cb93d1cee80d3c492627e00b219ff',
+      'timestamp': '0x0',
+      'totalDifficulty': '0x0',
+      'transactions': [],
+      'transactionsRoot': '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+      'uncles': []
     }
     callback(null, jsonRPCResponse(payload.id, b))
   }
