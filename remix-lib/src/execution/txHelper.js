15a6509bb2feb1f62c81ce40d5172ce3de9d8d00
yann300 <yann.levreau@gmail.com>
2018-05-09 18:25:38 +0200

    better return

--- remix-lib/src/execution/txHelper.js
+++ remix-lib/src/execution/txHelper.js
@@ -21,7 +21,7 @@ module.exports = {
   },
 
   encodeFunctionId: function (funABI) {
-    if (funABI.type === 'fallback') return ''
+    if (funABI.type === 'fallback') return '0x'
     var abi = new ethers.Interface([funABI])
     abi = abi.functions[funABI.name]
     return abi.sighash
