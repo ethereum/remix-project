66144ce35f2c9df58e1902e6796679cab9772769
serapath <dev@serapath.de>
2018-05-19 01:45:46 -0400

    CompileTab: remove jQuery

--- src/app/ui/renderer.js
+++ src/app/ui/renderer.js
@@ -56,7 +56,7 @@ Renderer.prototype.error = function (message, container, opt) {
   var $pre = $(opt.useSpan ? yo`<span />` : yo`<pre />`).html(message)
 
   var $error = $(yo`<div class="sol ${opt.type}"><div class="close"><i class="fa fa-close"></i></div></div>`).prepend($pre)
-  container.append($error)
+  $(container).append($error)
 
   $error.click((ev) => {
     if (opt.errFile && opt.errLine) {
