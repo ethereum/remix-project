4bb6e5ab36a8f8df7bfebaf27841a9a1ca3dc4f8
Iuri Matias <iuri.matias@gmail.com>
2018-06-25 18:01:19 -0400

    move code analyzer to its own repo

--- remix-solidity/index.js
+++ remix-solidity/index.js
@@ -1,9 +1,7 @@
-var CodeAnalysis = require('./src/analysis/staticAnalysisRunner')
 var Compiler = require('./src/compiler/compiler')
 var CompilerInput = require('./src/compiler/compiler-input')
 
 module.exports = {
-  CodeAnalysis: CodeAnalysis,
   Compiler: Compiler,
   CompilerInput: CompilerInput
 }
