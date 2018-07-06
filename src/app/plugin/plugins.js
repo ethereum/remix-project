fc448ac5205cdbdec98de2e36472872db3d94879
yann300 <yann.levreau@gmail.com>
2018-07-06 15:47:01 +0200

    remove etherscan plugin

--- src/app/plugin/plugins.js
+++ src/app/plugin/plugins.js
@@ -4,9 +4,10 @@ module.exports = {
   'oraclize': {
     url: 'https://remix-plugin.oraclize.it',
     title: 'Oraclize'
-  },
+  }
+  /*
   'etherscan-general': {
     url: 'http://127.0.0.1:8081',
     title: 'Etherscan-general'
-  }
+  }*/
 }
