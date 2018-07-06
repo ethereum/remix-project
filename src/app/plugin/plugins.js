5e07dc98c70ee02b216c84d0cb826839dbd8fb13
yann300 <yann.levreau@gmail.com>
2018-07-06 14:05:17 +0200

    add ether scan network status plugin

--- src/app/plugin/plugins.js
+++ src/app/plugin/plugins.js
@@ -4,5 +4,9 @@ module.exports = {
   'oraclize': {
     url: 'https://remix-plugin.oraclize.it',
     title: 'Oraclize'
+  },
+  'etherscan-general': {
+    url: 'http://127.0.0.1:8081',
+    title: 'Etherscan-general'
   }
 }
