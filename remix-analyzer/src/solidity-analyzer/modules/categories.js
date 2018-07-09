6125e01c8301ea10c6ecfee687d287c930ca5f1b
soad003 <michael.froewis@gmx.at>
2018-07-09 17:18:15 +0200

    Static Analysis: ERC20 decimals type check

--- remix-analyzer/src/solidity-analyzer/modules/categories.js
+++ remix-analyzer/src/solidity-analyzer/modules/categories.js
@@ -1,5 +1,6 @@
 module.exports = {
   SECURITY: {displayName: 'Security', id: 'SEC'},
   GAS: {displayName: 'Gas & Economy', id: 'GAS'},
-  MISC: {displayName: 'Miscellaneous', id: 'MISC'}
+  MISC: {displayName: 'Miscellaneous', id: 'MISC'},
+  ERC: {displayName: 'ERC', id: 'ERC'}
 }
