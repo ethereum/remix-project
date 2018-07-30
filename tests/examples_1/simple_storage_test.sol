cbeda1f3dce69d4de11ffc5433c903dd26185c50
Iuri Matias <iuri.matias@gmail.com>
2018-07-30 17:05:34 -0400

    add test case

--- tests/examples_1/simple_storage_test.sol
+++ tests/examples_1/simple_storage_test.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.7;
-import "./tests.sol";
+import "remix_tests.sol";
 import "./simple_storage.sol";
 
 contract MyTest {
@@ -17,6 +17,15 @@ contract MyTest {
   function initialValueShouldBe200() public constant returns (bool) {
     //return Assert.equal(foo.get(), 200, "initial value is not correct");
     return foo.get() == 200;
+  }
+
+  function shouldTriggerOneFail() public {
+    Assert.equal(uint(1), uint(2), "the test 1 fails");
+    Assert.equal(uint(1), uint(2), "the test 2 fails");
+  }
+
+  function shouldTriggerOnePass() public {
+    Assert.equal(uint(1), uint(1), "the test 3 fails");
   }
 
 }
