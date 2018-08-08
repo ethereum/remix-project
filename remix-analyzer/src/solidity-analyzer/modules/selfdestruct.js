048e1dcdec5c765ad2a73d12ad46af6b430059be
soad003 <michael.froewis@gmx.at>
2018-08-08 11:31:42 +0200

    StaticAnalysis: selfdestruct terminal warning

--- remix-analyzer/src/solidity-analyzer/modules/selfdestruct.js
+++ remix-analyzer/src/solidity-analyzer/modules/selfdestruct.js
@@ -2,25 +2,51 @@ var name = 'Selfdestruct: '
 var desc = 'Be aware of caller contracts.'
 var categories = require('./categories')
 var common = require('./staticAnalysisCommon')
+var AbstractAst = require('./abstractAstView')
 
 function selfdestruct () {
-  this.relevantNodes = []
+  this.abstractAst = new AbstractAst()
+
+  this.visit = this.abstractAst.build_visit(
+    (node) => common.isStatement(node) ||
+              common.isSelfdestructCall(node)
+  )
+
+  this.report = this.abstractAst.build_report(report)
 }
 
-selfdestruct.prototype.visit = function (node) {
-  if (common.isSelfdestructCall(node)) {
-    this.relevantNodes.push(node)
-  }
-}
+selfdestruct.prototype.visit = function () { throw new Error('constantFunctions.js no visit function set upon construction') }
 
-selfdestruct.prototype.report = function () {
-  return this.relevantNodes.map(function (item, i) {
-    return {
-      warning: 'Use of selfdestruct: can block calling contracts unexpectedly. Be especially careful if this contract is planned to be used by other contracts (i.e. library contracts, interactions). Selfdestruction of the callee contract can leave callers in an inoperable state.',
-      location: item.src,
-      more: 'https://paritytech.io/blog/security-alert.html'
-    }
+selfdestruct.prototype.report = function () { throw new Error('constantFunctions.js no report function set upon construction') }
+
+function report (contracts, multipleContractsWithSameName) {
+  var warnings = []
+
+  contracts.forEach((contract) => {
+    contract.functions.forEach((func) => {
+      let hasSelf = false
+      func.relevantNodes.forEach((node) => {
+        if (common.isSelfdestructCall(node)) {
+          warnings.push({
+            warning: 'Use of selfdestruct: can block calling contracts unexpectedly. Be especially careful if this contract is planned to be used by other contracts (i.e. library contracts, interactions). Selfdestruction of the callee contract can leave callers in an inoperable state.',
+            location: node.src,
+            more: 'https://paritytech.io/blog/security-alert.html'
+          })
+          hasSelf = true
+        }
+        if (common.isStatement(node) && hasSelf) {
+          warnings.push({
+            warning: 'Use of selfdestruct: No code after selfdestruct is executed. Selfdestruct is a terminal.',
+            location: node.src,
+            more: 'http://solidity.readthedocs.io/en/develop/introduction-to-smart-contracts.html#self-destruct'
+          })
+          hasSelf = false
+        }
+      })
+    })
   })
+
+  return warnings
 }
 
 module.exports = {
