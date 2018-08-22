1e48cd00c94daaa40998199d0cd54d3b88bc9cac
0mkar <0mkar@protonmail.com>
2018-08-23 00:42:30 +0530

    improve string tests

--- tests/examples_3/simple_string.sol
+++ tests/examples_3/simple_string.sol
@@ -3,11 +3,7 @@ contract SimpleString {
   string public storedData;
 
   function SimpleString() public {
-    storedData = "Hello";
-  }
-
-  function set(string x) public {
-    storedData = x;
+    storedData = "Hello world!";
   }
 
   function get() public view returns (string retVal) {
