810d4783d9b12027cd675162e1eb06d4033b3491
0mkar <0mkar@protonmail.com>
2018-08-28 16:37:02 +0530

    Add winston logger for different verbosity level

--- remix-tests/src/run.js
+++ remix-tests/src/run.js
@@ -2,35 +2,48 @@ const commander = require('commander')
 const Web3 = require('web3')
 const RemixTests = require('./index.js')
 const fs = require('fs')
-const { Signale } = require('signale')
 const Provider = require('remix-simulator').Provider
+const { Log } = require('./logger.js')
+const logger = new Log()
+const log = logger.logger
 require('colors')
 
-// signale configuration
-const options = {
-  types: {
-    greet: {
-      badge: '\n👁',
-      label: '',
-      color: 'yellow'
-    }
+// parse verbosity
+function mapVerbosity (v) {
+  const levels = {
+    0: 'error',
+    1: 'warn',
+    2: 'info',
+    3: 'verbose',
+    4: 'debug',
+    5: 'silly'
   }
+  return levels[v]
 }
-const signale = new Signale(options)
+// get current version
+const pjson = require('../package.json')
+commander
+  .version(pjson.version)
+  .option('-v, --verbose <level>', 'run with verbosity', mapVerbosity)
+  .action(function (filename) {
+    // Console message
+    console.log(('\n\t👁 :: Running remix-tests - Unit testing for solidity :: 👁\t\n').white)
+    // set logger verbosity
+    if (commander.verbose) {
+      logger.setVerbosity(commander.verbose)
+      log.info('verbosity level set to ' + commander.verbose.blue)
+    }
+    let web3 = new Web3()
+    // web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+    web3.setProvider(new Provider())
+    // web3.setProvider(new web3.providers.WebsocketProvider('ws://localhost:8546'))
 
-commander.action(function (filename) {
-  signale.greet(('Running remix-tests: Unit testing for solidity.\n').yellow)
-  let web3 = new Web3()
-  // web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
-  web3.setProvider(new Provider())
-  // web3.setProvider(new web3.providers.WebsocketProvider('ws://localhost:8546'))
-
-  let isDirectory = fs.lstatSync(filename).isDirectory()
-  RemixTests.runTestFiles(filename, isDirectory, web3)
-})
+    let isDirectory = fs.lstatSync(filename).isDirectory()
+    RemixTests.runTestFiles(filename, isDirectory, web3)
+  })
 
 if (!process.argv.slice(2).length) {
-  signale.fatal('Please specify a filename')
+  log.error('Please specify a filename')
   process.exit()
 }
 
