f434e32c7fd52689620dc2f4eca6b8d85ef20f2e
0mkar <0mkar@protonmail.com>
2018-08-28 09:17:12 +0530

    Add initial message

--- remix-tests/src/run.js
+++ remix-tests/src/run.js
@@ -2,11 +2,24 @@ const commander = require('commander')
 const Web3 = require('web3')
 const RemixTests = require('./index.js')
 const fs = require('fs')
-const signale = require('signale')
-
+const { Signale } = require('signale')
 const Provider = require('remix-simulator').Provider
+require('colors')
+
+// signale configuration
+const options = {
+  types: {
+    greet: {
+      badge: '\n👁',
+      label: '',
+      color: 'yellow'
+    }
+  }
+}
+const signale = new Signale(options)
 
 commander.action(function (filename) {
+  signale.greet(('Running remix-tests: Unit testing for solidity.\n').yellow)
   let web3 = new Web3()
   // web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
   web3.setProvider(new Provider())
