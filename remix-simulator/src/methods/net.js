bea9bd9831fff78522c7d6e7f81f2bcf7e289c13
Iuri Matias <iuri.matias@gmail.com>
2018-08-29 13:04:07 -0400

    add test command & fixing linting

--- remix-simulator/src/methods/net.js
+++ remix-simulator/src/methods/net.js
@@ -1,8 +1,8 @@
 
-var Net = function() {
+var Net = function () {
 }
 
-Net.prototype.methods = function() {
+Net.prototype.methods = function () {
   return {
     net_version: this.net_version,
     net_listening: this.net_listening,
@@ -10,16 +10,16 @@ Net.prototype.methods = function() {
   }
 }
 
-Net.prototype.net_version = function(payload, cb) {
+Net.prototype.net_version = function (payload, cb) {
   // should be configured networkId
   cb(null, 1337)
 }
 
-Net.prototype.net_listening = function(payload, cb) {
+Net.prototype.net_listening = function (payload, cb) {
   cb(null, true)
 }
 
-Net.prototype.net_peerCount = function(payload, cb) {
+Net.prototype.net_peerCount = function (payload, cb) {
   cb(null, 0)
 }
 
