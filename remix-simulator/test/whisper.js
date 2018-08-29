9930ab426829369ae213c8ebe430aa70210365b9
Iuri Matias <iuri.matias@gmail.com>
2018-08-29 12:48:55 -0400

    add tests; update web3.js

---
+++ remix-simulator/test/whisper.js
@@ -0,0 +1,19 @@
+var Web3 = require('web3')
+var RemixSim = require('../index.js')
+let web3 = new Web3();
+var assert = require('assert')
+
+describe("Whisper", function() {
+
+  before(function() {
+    let provider = new RemixSim.Provider()
+    web3.setProvider(provider)
+  })
+
+  it("should get correct remix simulator version", async function() {
+    let version = await web3.shh.getVersion()
+    assert.equal(version, 5)
+  })
+
+});
+
