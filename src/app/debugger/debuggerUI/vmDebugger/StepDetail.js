b434d9e9c4f68a8e34e91dc7e1dd2e8192e34c9e
Iuri Matias <iuri.matias@gmail.com>
2018-09-08 16:51:38 -0400

    refactor step detail

--- src/app/debugger/debuggerUI/vmDebugger/StepDetail.js
+++ src/app/debugger/debuggerUI/vmDebugger/StepDetail.js
@@ -9,9 +9,23 @@ function StepDetail (_parentUI, _traceManager) {
 
   this.basicPanel = new DropdownPanel('Step detail', {json: true, displayContentOnly: true})
 
-  this.detail = initDetail()
+  this.detail = {
+    'vm trace step': '-', 'execution step': '-', 'add memory': '', 'gas': '', 'remaining gas': '-', 'loaded address': '-'
+  }
   this.view
   this.init()
+}
+
+StepDetail.prototype.reset = function () {
+  this.detail = {
+    'vm trace step': '-', 'execution step': '-', 'add memory': '', 'gas': '', 'remaining gas': '-', 'loaded address': '-'
+  }
+  this.basicPanel.update(this.detail)
+}
+
+StepDetail.prototype.updateField = function (key, value) {
+  this.detail[key] = value
+  this.basicPanel.update(this.detail)
 }
 
 StepDetail.prototype.render = function () {
@@ -21,81 +35,38 @@ StepDetail.prototype.render = function () {
 StepDetail.prototype.init = function () {
   var self = this
   this.debugger.event.register('traceUnloaded', this, function () {
-    self.detail = initDetail()
-    self.basicPanel.update(self.detail)
+    self.reset()
   })
 
   this.debugger.event.register('newTraceLoaded', this, function () {
-    self.detail = initDetail()
-    self.basicPanel.update(self.detail)
+    self.reset()
   })
 
   this.parentUI.event.register('indexChanged', this, function (index) {
     if (index < 0) return
 
-    self.detail['vm trace step'] = index
+    self.updateField('vm trace step', index)
 
     self.traceManager.getCurrentStep(index, function (error, step) {
-      if (error) {
-        console.log(error)
-        self.detail['execution step'] = '-'
-      } else {
-        self.detail['execution step'] = step
-      }
-      self.basicPanel.update(self.detail)
+      self.updateField('execution step', (error ? '-' : step))
     })
 
     self.traceManager.getMemExpand(index, function (error, addmem) {
-      if (error) {
-        console.log(error)
-        self.detail['add memory'] = '-'
-      } else {
-        self.detail['add memory'] = addmem
-      }
-      self.basicPanel.update(self.detail)
+      self.updateField('add memory', (error ? '-' : addmem))
     })
 
     self.traceManager.getStepCost(index, function (error, gas) {
-      if (error) {
-        console.log(error)
-        self.detail.gas = '-'
-      } else {
-        self.detail.gas = gas
-      }
-      self.basicPanel.update(self.detail)
+      self.updateField('gas', (error ? '-' : gas))
     })
 
     self.traceManager.getCurrentCalledAddressAt(index, function (error, address) {
-      if (error) {
-        console.log(error)
-        self.detail['loaded address'] = '-'
-      } else {
-        self.detail['loaded address'] = address
-      }
-      self.basicPanel.update(self.detail)
+      self.updateField('loaded address', (error ? '-' : address))
     })
 
     self.traceManager.getRemainingGas(index, function (error, remaingas) {
-      if (error) {
-        console.log(error)
-        self.detail['remaining gas'] = '-'
-      } else {
-        self.detail['remaining gas'] = remaingas
-      }
-      self.basicPanel.update(self.detail)
+      self.updateField('remaining gas', (error ? '-' : remaingas))
     })
   })
-}
-
-function initDetail () {
-  return {
-    'vm trace step': '-',
-    'execution step': '-',
-    'add memory': '',
-    'gas': '',
-    'remaining gas': '-',
-    'loaded address': '-'
-  }
 }
 
 module.exports = StepDetail
