13b635a48a96799c77032df2a43786cae09eaf7d
Iuri Matias <iuri.matias@gmail.com>
2018-09-08 17:04:01 -0400

    move step detail logic into caller

--- src/app/debugger/debuggerUI/vmDebugger/StepDetail.js
+++ src/app/debugger/debuggerUI/vmDebugger/StepDetail.js
@@ -13,7 +13,6 @@ function StepDetail (_parentUI, _traceManager) {
     'vm trace step': '-', 'execution step': '-', 'add memory': '', 'gas': '', 'remaining gas': '-', 'loaded address': '-'
   }
   this.view
-  this.init()
 }
 
 StepDetail.prototype.reset = function () {
@@ -30,43 +29,6 @@ StepDetail.prototype.updateField = function (key, value) {
 
 StepDetail.prototype.render = function () {
   return yo`<div id='stepdetail' >${this.basicPanel.render()}</div>`
-}
-
-StepDetail.prototype.init = function () {
-  var self = this
-  this.debugger.event.register('traceUnloaded', this, function () {
-    self.reset()
-  })
-
-  this.debugger.event.register('newTraceLoaded', this, function () {
-    self.reset()
-  })
-
-  this.parentUI.event.register('indexChanged', this, function (index) {
-    if (index < 0) return
-
-    self.updateField('vm trace step', index)
-
-    self.traceManager.getCurrentStep(index, function (error, step) {
-      self.updateField('execution step', (error ? '-' : step))
-    })
-
-    self.traceManager.getMemExpand(index, function (error, addmem) {
-      self.updateField('add memory', (error ? '-' : addmem))
-    })
-
-    self.traceManager.getStepCost(index, function (error, gas) {
-      self.updateField('gas', (error ? '-' : gas))
-    })
-
-    self.traceManager.getCurrentCalledAddressAt(index, function (error, address) {
-      self.updateField('loaded address', (error ? '-' : address))
-    })
-
-    self.traceManager.getRemainingGas(index, function (error, remaingas) {
-      self.updateField('remaining gas', (error ? '-' : remaingas))
-    })
-  })
 }
 
 module.exports = StepDetail
