9a14e0db9c5b98bee6dd9c3830ce8fd72877af63
yann300 <yann.levreau@gmail.com>
2018-09-19 15:51:55 +0200

    add tests

--- remix-lib/test/tests.js
+++ remix-lib/test/tests.js
@@ -3,6 +3,7 @@ require('./eventManager.js')
 require('./sourceMappingDecoder.js')
 require('./util.js')
 require('./txFormat.js')
+require('./txHelper.js')
 
 require('./traceManager.js')
 require('./codeManager.js')
