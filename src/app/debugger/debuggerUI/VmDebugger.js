2bc2af9ed07696c2c400196fdc816a12aacaeb72
Iuri Matias <iuri.matias@gmail.com>
2018-09-19 20:20:54 -0400

    add new FullStorageChanges

--- src/app/debugger/debuggerUI/VmDebugger.js
+++ src/app/debugger/debuggerUI/VmDebugger.js
@@ -13,7 +13,7 @@ var DebuggerSolidityLocals = require('../solidityLocals')
 var SolidityState = require('./vmDebugger/SolidityState')
 var SolidityLocals = require('./vmDebugger/SolidityLocals')
 
-var FullStoragesChangesPanel = require('../remix-debugger/src/ui/FullStoragesChanges')
+var FullStoragesChangesPanel = require('./vmDebugger/FullStoragesChanges')
 var DropdownPanel = require('./vmDebugger/DropdownPanel')
 var remixDebug = require('remix-debug')
 var remixLib = require('remix-lib')
