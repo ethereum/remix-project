98e3a498c1a84734116934b62e14b44af9ecf61d
Iuri Matias <iuri.matias@gmail.com>
2018-10-02 16:57:09 -0400

    move editor out of debugger

--- src/app/debugger/debugger.js
+++ src/app/debugger/debugger.js
@@ -5,9 +5,6 @@ var EventManager = remixLib.EventManager
 var executionContext = require('../../execution-context')
 var globalRegistry = require('../../global/registry')
 
-/**
- * Manage remix and source highlighting
- */
 function Debugger () {
   var self = this
   this.event = new EventManager()
@@ -40,26 +37,10 @@ function Debugger () {
 
   this.debugger.setBreakpointManager(this.breakPointManager)
 
-  self._deps.editor.event.register('breakpointCleared', (fileName, row) => {
-    this.breakPointManager.remove({fileName: fileName, row: row})
-  })
-
-  self._deps.editor.event.register('breakpointAdded', (fileName, row) => {
-    this.breakPointManager.add({fileName: fileName, row: row})
-  })
-
   executionContext.event.register('contextChanged', this, function (context) {
     self.switchProvider(context)
   })
 
-  // unload if a file has changed (but not if tabs were switched)
-  self._deps.editor.event.register('contentChanged', function () {
-    self.debugger.unLoad()
-  })
-
-  //
-  // ====================
-  // listen to events
   this.debugger.event.register('newTraceLoaded', this, function () {
     self.event.trigger('debuggerStatus', [true])
   })
@@ -68,8 +49,6 @@ function Debugger () {
     self.event.trigger('debuggerStatus', [false])
   })
 
-  // ====================
-  // add providers
   this.debugger.addProvider('vm', executionContext.vm())
   this.debugger.addProvider('injected', executionContext.internalWeb3())
   this.debugger.addProvider('web3', executionContext.internalWeb3())
