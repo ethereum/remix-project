bb7d078067ec44923f0d1f89d00134ddb604a444
0mkar <0mkar@protonmail.com>
2018-10-03 13:04:39 +0530

    add forLoopIteratesOverDynamicArray tests

---
+++ remix-analyzer/test/analysis/test-contracts/forLoopIteratesOverDynamicArray.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.22;
+contract forLoopArr {
+    uint[] array;
+    constructor(uint[] _array) {
+        array = _array;
+    }
+
+    function shiftArrItem(uint index) returns(uint[]) {
+        for (uint i = index; i < array.length-1; i++) {
+            array[i] = array[i+1];
+        }
+        return array;
+    }
+}
