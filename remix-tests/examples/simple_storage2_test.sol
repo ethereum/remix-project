97ceaa426d627f3c025262b4e3ac2888cea13ad7
LianaHus <liana@ethdev.com>
2018-11-15 11:21:55 +0100

    - added visibility specifiers - pragma solidity 0.4.* -> 0.5.0

--- remix-tests/examples/simple_storage2_test.sol
+++ remix-tests/examples/simple_storage2_test.sol
@@ -1,16 +1,16 @@
-pragma solidity ^0.4.7;
+ppragma solidity ^0.5.0;
 import "remix_tests.sol";
 import "./simple_storage.sol";
 
 contract MyTest2 {
-  SimpleStorage foo;
-  uint i = 0;
+  SimpleStorage storage foo;
+  uint storage i = 0;
 
-  function beforeAll() {
+  function beforeAll() public {
     foo = new SimpleStorage();
   }
 
-  function beforeEach() {
+  function beforeEach() public {
     if (i == 1) {
       foo.set(200);
     }
