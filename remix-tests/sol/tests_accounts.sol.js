c34b9d88038041d79f51c78f6ede25201b077b22
yann300 <yann.levreau@gmail.com>
2018-11-19 19:16:54 +0100

    Update tests_accounts.sol.js

--- remix-tests/sol/tests_accounts.sol.js
+++ remix-tests/sol/tests_accounts.sol.js
@@ -1,7 +1,7 @@
 module.exports = `pragma solidity ^0.5.0;
 
 library TestsAccounts {
-    function getAccount(uint index) returns (address) {
+    function getAccount(uint index) public returns (address) {
         >accounts<
         return accounts[index];
     }
