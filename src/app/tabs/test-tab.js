8096c527d5f61263e1bba91e48d7cb0b631addad
LianaHus <liana@ethdev.com>
2018-11-21 20:53:20 +0100

    fixes generate tests for solc 0.5.0

--- src/app/tabs/test-tab.js
+++ src/app/tabs/test-tab.js
@@ -193,42 +193,42 @@ module.exports = class TestTab {
   }
 }
 
-var testContractSample = `pragma solidity ^0.4.0;
+var testContractSample = `
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 
 // file name has to end with '_test.sol'
 contract test_1 {
-    
-    function beforeAll() public {
-      // here should instantiate tested contract
-    }
-    
-    function check1() public {
-      // this function is not constant, use 'Assert' to test the contract
-      Assert.equal(uint(2), uint(1), "error message");
-      Assert.equal(uint(2), uint(2), "error message");
-    }
-    
-    function check2() public constant returns (bool) {
-      // this function is constant, use the return value (true or false) to test the contract
-      return true;
-    }
+
+  function beforeAll() public pure {
+    // here should instantiate tested contract
+  }
+
+  function check1() public {
+    // use 'Assert' to test the contract
+    Assert.equal(uint(2), uint(1), "error message");
+    Assert.equal(uint(2), uint(2), "error message");
+  }
+
+  function check2() pure public returns (bool) {
+    // use the return value (true or false) to test the contract
+    return true;
+  }
 }
 
 contract test_2 {
-   
-    function beforeAll() public {
-      // here should instantiate tested contract
-    }
-    
-    function check1() public {
-      // this function is not constant, use 'Assert' to test the contract
-      Assert.equal(uint(2), uint(1), "error message");
-      Assert.equal(uint(2), uint(2), "error message");
-    }
-    
-    function check2() public constant returns (bool) {
-      // this function is constant, use the return value (true or false) to test the contract
-      return true;
-    }
+ 
+  function beforeAll() public pure {
+    // here should instantiate tested contract
+  }
+
+  function check1() public {
+    // use 'Assert' to test the contract
+    Assert.equal(uint(2), uint(1), "error message");
+    Assert.equal(uint(2), uint(2), "error message");
+  }
+
+  function check2() public pure returns (bool) {
+    // use the return value (true or false) to test the contract
+    return true;
+  }
 }`
