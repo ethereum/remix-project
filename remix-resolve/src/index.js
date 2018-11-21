9032e03385489ec3b8dd785a61c91033beaa4f8c
0mkar <0mkar@protonmail.com>
2018-11-21 14:28:47 +0530

    Initialize remix solidity resolve repository

---
+++ remix-resolve/src/index.js
@@ -0,0 +1,12 @@
+/*
+const rr = require('remix-resolve')
+const fileContent = rr.resolve('https://github.com/ethereum/greeter.sol')
+const input = rr.combineSource({ 'greeter.sol': content })
+*/
+const resolve = require('./resolve.js')
+const combineSource = require('./combineSource.js')
+
+module.exports = {
+  resolve: resolve,
+  combineSource: combineSource
+}
