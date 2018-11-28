ed598c0bbed0ba332920a7a929205563601671f8
Iuri Matias <iuri.matias@gmail.com>
2018-11-28 09:46:17 -0500

    add missing return

--- remix-simulator/src/server.js
+++ remix-simulator/src/server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
   provider.sendAsync(req.body, (err, jsonResponse) => {
     if (err) {
-      res.send({error: err})
+      return res.send({error: err})
     }
     res.send(jsonResponse)
   })
@@ -29,7 +29,7 @@ app.ws('/', (ws, req) => {
   ws.on('message', function (msg) {
     provider.sendAsync(JSON.parse(msg), (err, jsonResponse) => {
       if (err) {
-        ws.send({error: err})
+        return ws.send({error: err})
       }
       ws.send(JSON.stringify(jsonResponse))
     })
