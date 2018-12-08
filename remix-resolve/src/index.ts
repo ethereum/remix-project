1d3cb366795d4eb4835cb366693854ba33e9f321
0mkar <0mkar@protonmail.com>
2018-12-08 19:51:45 +0530

    first implementation of resolve with custom handler

--- remix-resolve/src/index.ts
+++ remix-resolve/src/index.ts
@@ -4,5 +4,3 @@ const fileContent = rr.resolve('https://github.com/ethereum/greeter.sol')
 const input = rr.combineSource({ 'greeter.sol': content })
 */
 export * from './resolve'
-export * from './combineSource.js'
-export * from './getFile'
