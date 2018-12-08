c291e6e738b0fbc8f3dab6ae83612d5f9ea809e3
0mkar <0mkar@protonmail.com>
2018-12-08 16:26:27 +0530

    Try to implement resolve function in TS

--- remix-resolve/src/index.ts
+++ remix-resolve/src/index.ts
@@ -3,6 +3,6 @@ const rr = require('remix-resolve')
 const fileContent = rr.resolve('https://github.com/ethereum/greeter.sol')
 const input = rr.combineSource({ 'greeter.sol': content })
 */
-export * from './resolve.js'
+export * from './resolve'
 export * from './combineSource.js'
-export * from './getFile.js'
+export * from './getFile'
