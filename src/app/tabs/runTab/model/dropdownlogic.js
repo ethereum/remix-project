d727f2616bdae8b4661d43fc47119ae978c0ccb2
Iuri Matias <iuri.matias@gmail.com>
2018-12-28 15:56:02 -0500

    move contract creation and transaction run from dropdown view into the dropdown logic module

--- src/app/tabs/runTab/model/dropdownlogic.js
+++ src/app/tabs/runTab/model/dropdownlogic.js
@@ -3,6 +3,7 @@ var remixLib = require('remix-lib')
 var txHelper = remixLib.execution.txHelper
 var executionContext = remixLib.execution.executionContext
 var typeConversion = remixLib.execution.typeConversion
+var txExecution = remixLib.execution.txExecution
 var CompilerAbstract = require('../../../compiler/compiler-abstract')
 var EventManager = remixLib.EventManager
 
@@ -117,6 +118,150 @@ class DropdownLogic {
 
   isVM () {
     return executionContext.isVM()
+  }
+
+  // TODO: check if selectedContract and data can be joined
+  createContract (selectedContract, data, continueCb, promptCb, confirmDialog, modalDialog, finalCb) {
+    if (data) {
+      data.contractName = selectedContract.name
+      data.linkReferences = selectedContract.bytecodeLinkReferences
+      data.contractABI = selectedContract.abi
+    }
+
+    var confirmationCb = (network, tx, gasEstimation, continueTxExecution, cancelCb) => {
+      if (network.name !== 'Main') {
+        return continueTxExecution(null)
+      }
+      var amount = executionContext.web3().fromWei(typeConversion.toInt(tx.value), 'ether')
+
+      // TODO: there is still a UI dependency to remove here, it's still too coupled at this point to remove easily
+      var content = confirmDialog(tx, amount, gasEstimation, this.recorder,
+        (gasPrice, cb) => {
+          let txFeeText, priceStatus
+          // TODO: this try catch feels like an anti pattern, can/should be
+          // removed, but for now keeping the original logic
+          try {
+            var fee = executionContext.web3().toBigNumber(tx.gas).mul(executionContext.web3().toBigNumber(executionContext.web3().toWei(gasPrice.toString(10), 'gwei')))
+            txFeeText = ' ' + executionContext.web3().fromWei(fee.toString(10), 'ether') + ' Ether'
+            priceStatus = true
+          } catch (e) {
+            txFeeText = ' Please fix this issue before sending any transaction. ' + e.message
+            priceStatus = false
+          }
+          cb(txFeeText, priceStatus)
+        },
+        (cb) => {
+          executionContext.web3().eth.getGasPrice((error, gasPrice) => {
+            var warnMessage = ' Please fix this issue before sending any transaction. '
+            if (error) {
+              return cb('Unable to retrieve the current network gas price.' + warnMessage + error)
+            }
+            try {
+              var gasPriceValue = executionContext.web3().fromWei(gasPrice.toString(10), 'gwei')
+              cb(null, gasPriceValue)
+            } catch (e) {
+              cb(warnMessage + e.message, null, false)
+            }
+          })
+        }
+      )
+      modalDialog('Confirm transaction', content,
+        { label: 'Confirm',
+          fn: () => {
+            this.config.setUnpersistedProperty('doNotShowTransactionConfirmationAgain', content.querySelector('input#confirmsetting').checked)
+            // TODO: check if this is check is still valid given the refactor
+            if (!content.gasPriceStatus) {
+              cancelCb('Given gas price is not correct')
+            } else {
+              var gasPrice = executionContext.web3().toWei(content.querySelector('#gasprice').value, 'gwei')
+              continueTxExecution(gasPrice)
+            }
+          }}, {
+            label: 'Cancel',
+            fn: () => {
+              return cancelCb('Transaction canceled by user.')
+            }
+          })
+    }
+
+    this.parentSelf._deps.udapp.createContract(data, confirmationCb, continueCb, promptCb,
+      (error, txResult) => {
+        if (error) {
+          return finalCb(`creation of ${selectedContract.name} errored: ${error}`)
+        }
+        var isVM = executionContext.isVM()
+        if (isVM) {
+          var vmError = txExecution.checkVMError(txResult)
+          if (vmError.error) {
+            return finalCb(vmError.message)
+          }
+        }
+        if (txResult.result.status && txResult.result.status === '0x0') {
+          return finalCb(`creation of ${selectedContract.name} errored: transaction execution failed`)
+        }
+        var address = isVM ? txResult.result.createdAddress : txResult.result.contractAddress
+        finalCb(null, selectedContract, address)
+      }
+    )
+  }
+
+  runTransaction (data, promptCb, modalDialog, confirmDialog, finalCb) {
+    var confirmationCb = (network, tx, gasEstimation, continueTxExecution, cancelCb) => {
+      if (network.name !== 'Main') {
+        return continueTxExecution(null)
+      }
+      var amount = this.fromWei(tx.value, true, 'ether')
+      var content = confirmDialog(tx, amount, gasEstimation, this.parentSelf,
+        (gasPrice, cb) => {
+          let txFeeText, priceStatus
+          // TODO: this try catch feels like an anti pattern, can/should be
+          // removed, but for now keeping the original logic
+          try {
+            var fee = this.calculateFee(tx.gas, gasPrice)
+            txFeeText = ' ' + this.fromWei(fee, false, 'ether') + ' Ether'
+            priceStatus = true
+          } catch (e) {
+            txFeeText = ' Please fix this issue before sending any transaction. ' + e.message
+            priceStatus = false
+          }
+          cb(txFeeText, priceStatus)
+        },
+        (cb) => {
+          this.getGasPrice((error, gasPrice) => {
+            var warnMessage = ' Please fix this issue before sending any transaction. '
+            if (error) {
+              return cb('Unable to retrieve the current network gas price.' + warnMessage + error)
+            }
+            try {
+              var gasPriceValue = this.fromWei(gasPrice, false, 'gwei')
+              cb(null, gasPriceValue)
+            } catch (e) {
+              cb(warnMessage + e.message, null, false)
+            }
+          })
+        }
+      )
+      modalDialog('Confirm transaction', content,
+        { label: 'Confirm',
+          fn: () => {
+            this.parentSelf._deps.config.setUnpersistedProperty('doNotShowTransactionConfirmationAgain', content.querySelector('input#confirmsetting').checked)
+            // TODO: check if this is check is still valid given the refactor
+            if (!content.gasPriceStatus) {
+              cancelCb('Given gas price is not correct')
+            } else {
+              var gasPrice = this.toWei(content.querySelector('#gasprice').value, 'gwei')
+              continueTxExecution(gasPrice)
+            }
+          }}, {
+            label: 'Cancel',
+            fn: () => {
+              return cancelCb('Transaction canceled by user.')
+            }
+          }
+      )
+    }
+
+    this.parentSelf._deps.udapp.runTx(data, confirmationCb, promptCb, finalCb)
   }
 
 }
