257d0efcd778b48206aed3c44f7d41d35a388a86
jeremiG <gendronjeremi@gmail.com>
2019-01-08 09:51:26 -0500

    fix(ui): Apply local CSS variables to existing themes
     - Prevent excessively verbose stylesheet

--- src/app/panels/styles/file-panel-styles.js
+++ src/app/panels/styles/file-panel-styles.js
@@ -82,11 +82,7 @@ var css = csjs`
     bottom            : 0;
     cursor            : col-resize;
     z-index           : 999;
-    border-right      : ${
-      styles.leftPanel.customDragbarBorderRight
-      ? styles.leftPanel.dragbarBorderRight
-      : '2px solid hsla(215, 81%, 79%, .3)'
-    }
+    border-right      : ${styles.leftPanel.dragbarBorderRight};
   }
   .ghostbar           {
     width             : 3px;
