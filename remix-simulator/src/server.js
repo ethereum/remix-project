b64519c81e1513d39c6aa197a6faf413ece172b1
Iuri Matias <iuri.matias@gmail.com>
2019-01-08 11:01:00 -0500

    refactor server code into a module

--- remix-simulator/src/server.js
+++ remix-simulator/src/server.js
@@ -5,35 +5,45 @@ const expressWs = require('express-ws')
 const Provider = require('./provider')
 const log = require('./utils/logs.js')
 
-expressWs(app)
+class Server {
 
-var provider = new Provider()
+  constructor() {
+    this.provider = new Provider()
+  }
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+  start(host, port) {
+    expressWs(app)
 
-app.get('/', (req, res) => {
-  res.send('Welcome to remix-simulator')
-})
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
 
-app.use((req, res) => {
-  provider.sendAsync(req.body, (err, jsonResponse) => {
-    if (err) {
-      return res.send(JSON.stringify({error: err}))
-    }
-    res.send(jsonResponse)
-  })
-})
-
-app.ws('/', (ws, req) => {
-  ws.on('message', function (msg) {
-    provider.sendAsync(JSON.parse(msg), (err, jsonResponse) => {
-      if (err) {
-        return ws.send(JSON.stringify({error: err}))
-      }
-      ws.send(JSON.stringify(jsonResponse))
+    app.get('/', (req, res) => {
+      res.send('Welcome to remix-simulator')
     })
-  })
-})
 
-app.listen(8545, () => log('Remix Simulator listening on port 8545!'))
+    app.use((req, res) => {
+      provider.sendAsync(req.body, (err, jsonResponse) => {
+        if (err) {
+          return res.send(JSON.stringify({error: err}))
+        }
+        res.send(jsonResponse)
+      })
+    })
+
+    app.ws('/', (ws, req) => {
+      ws.on('message', function (msg) {
+        provider.sendAsync(JSON.parse(msg), (err, jsonResponse) => {
+          if (err) {
+            return ws.send(JSON.stringify({error: err}))
+          }
+          ws.send(JSON.stringify(jsonResponse))
+        })
+      })
+    })
+
+    app.listen(8545, () => log('Remix Simulator listening on port 8545!'))
+  }
+
+}
+
+module.exports = Server
