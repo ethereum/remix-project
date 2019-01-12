4cef2ac3ceb478c2524542bfefbe9d33d007a7d1
Iuri Matias <iuri.matias@gmail.com>
2019-01-11 19:22:29 -0500

    remove unused event registration

--- src/app/tabs/analysis-tab.js
+++ src/app/tabs/analysis-tab.js
@@ -20,19 +20,10 @@ class AnalysisTab {
   }
   render () {
     var staticanalysis = new StaticAnalysis()
-
-    staticanalysis.event.register('staticAnaysisWarning', (count) => {
-      if (count <= 0) return
-      const msg = `Static Analysis raised ${count} warning(s) that requires your attention. Check Solidity Static Analysis Module for more information.`
-      const settings = { type: 'staticAnalysisWarning', useSpan: true }
-      this.event.trigger('newStaticAnaysisWarningMessage', [msg, settings])
-    })
-
     this.registry.put({api: staticanalysis, name: 'staticanalysis'})
-    if (this.el) return this.el
-    this.el = yo`
-      <div class="${css.analysisTabView} "id="staticanalysisView">${staticanalysis.render()}</div>`
 
+    if (this.el) return this.el
+    this.el = yo`<div class="${css.analysisTabView} "id="staticanalysisView">${staticanalysis.render()}</div>`
     return this.el
   }
 }
