36248ba584fe021d21d8009f87059225645682bb
Iuri Matias <iuri.matias@gmail.com>
2019-01-14 17:59:12 -0500

    remove unnecessary code & properties from plugin tab

--- src/app/tabs/plugin-tab.js
+++ src/app/tabs/plugin-tab.js
@@ -1,28 +1,22 @@
 var yo = require('yo-yo')
 var css = require('./styles/plugin-tab-styles')
 
-var globalRegistry = require('../../global/registry')
-var EventManager = require('../../lib/events')
-
 class PluginTab {
 
-  constructor (json, localRegistry) {
-    this.event = new EventManager()
-    this._view = { el: null }
+  constructor (json) {
+    this.el = null
     this.data = { json }
-    this._components = {}
-    this._components.registry = localRegistry || globalRegistry
   }
 
   render () {
-    if (this._view.el) return this._view.el
+    if (this.el) return this.el
 
-    this._view.el = yo`
+    this.el = yo`
       <div class="${css.pluginTabView}" id="pluginView">
         <iframe class="${css.iframe}" src="${this.data.json.url}/index.html"></iframe>
       </div>`
 
-    return this._view.el
+    return this.el
   }
 
 }
