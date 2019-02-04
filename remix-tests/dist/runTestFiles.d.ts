03f1967f5096934f94866dfe0d25c9d25b908bfa
Sab94 <asabya@gmail.com>
2019-02-04 15:44:59 +0530

    test migrated to typescript

---
+++ remix-tests/dist/runTestFiles.d.ts
@@ -0,0 +1,2 @@
+declare function runTestFiles(filepath: any, isDirectory: any, web3: any, opts: any): void;
+export = runTestFiles;
---
+++ remix-tests/dist/testRunner.d.ts
@@ -0,0 +1,2 @@
+declare function runTest(testName: any, testObject: any, contractDetails: any, opts: any, testCallback: any, resultsCallback: any): void;
+export = runTest;
