74f2fd5cfcbacffdde37d432477bbb566a05d126
Omkara <0mkar@protonmail.com>
2019-02-05 21:27:11 +0530

    add more typings to src/testRunner.ts

--- remix-tests/tests/testRunner.ts
+++ remix-tests/tests/testRunner.ts
@@ -5,12 +5,7 @@ import { Provider } from 'remix-simulator'
 
 let Compiler = require('../src/compiler.js')
 let Deployer = require('../src/deployer.js')
-import runTest from '../dist/testRunner.js'
-
-interface Results {
-    passingNum: number,
-    failureNum: number,
-}
+import runTest, { ResultsInterface, TestCbInterface, ResultCbInterface } from '../dist/testRunner.js'
 
 function compileAndDeploy (filename: string, callback: Function) {
     let web3: Web3 = new Web3()
@@ -19,7 +14,7 @@ function compileAndDeploy (filename: string, callback: Function) {
     let accounts: object
     async.waterfall([
       function getAccountList(next: Function): void {
-        web3.eth.getAccounts((_err: Error, _accounts: object) => {
+        web3.eth.getAccounts((_err: Error | null | undefined, _accounts: object) => {
           accounts = _accounts
           next(_err)
         })
@@ -45,14 +40,16 @@ function compileAndDeploy (filename: string, callback: Function) {
     describe('#runTest', () => {
       describe('test with beforeAll', () => {
         let filename = 'tests/examples_1/simple_storage_test.sol'
-        let tests:any[] = [], results:Results;
+        let tests:any[] = [], results:ResultsInterface;
 
         before((done) => {
-          compileAndDeploy(filename, (_err: Error, compilationData: object, contracts: any, accounts: object) => {
-            var testCallback = (test: object) => {
+          compileAndDeploy(filename, (_err: Error | null | undefined, compilationData: object, contracts: any, accounts: object) => {
+            var testCallback: TestCbInterface = (err, test) => {
+              if (err) { throw err }
               tests.push(test)
             }
-            var resultsCallback = (_err: Error, _results: Results) => {
+            var resultsCallback: ResultCbInterface = (err, _results) => {
+              if (err) { throw err }
               results = _results
               done()
             }
@@ -78,5 +75,41 @@ function compileAndDeploy (filename: string, callback: Function) {
           ])
         })
       })
+
+      describe('test with beforeEach', function () {
+      let filename = 'tests/examples_2/simple_storage_test.sol'
+      let tests:any[] = [], results:ResultsInterface;
+
+      before(function (done) {
+        compileAndDeploy(filename, function (_err: Error | null | undefined, compilationData: object, contracts: any, accounts: object) {
+          var testCallback: TestCbInterface = (err, test) => {
+            if (err) { throw err }
+            tests.push(test)
+          }
+          var resultsCallback: ResultCbInterface = (err, _results) => {
+            if (err) { throw err }
+            results = _results
+            done()
+          }
+          runTest('MyTest', contracts.MyTest, compilationData[filename]['MyTest'], { accounts }, testCallback, resultsCallback)
+        })
+      })
+
+      it('should 2 passing tests', function () {
+        assert.equal(results.passingNum, 2)
+      })
+
+      it('should 0 failing tests', function () {
+        assert.equal(results.failureNum, 0)
+      })
+
+      it('should returns 3 messages', function () {
+        assert.deepEqual(tests, [
+          { type: 'contract', value: 'MyTest', filename: 'tests/examples_2/simple_storage_test.sol' },
+          { type: 'testPass', value: 'Initial value should be100', time: 1, context: 'MyTest' },
+          { type: 'testPass', value: 'Initial value should be200', time: 1, context: 'MyTest' }
+        ])
+      })
+    })
     })
   })
