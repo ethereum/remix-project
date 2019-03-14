e08a8eabb3686f0d90e3bf154fd24f802ad3dc42
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-03-14 15:50:57 +0100

    Remove `__showing()` in framing service

--- src/framingService.js
+++ src/framingService.js
@@ -1,13 +1,10 @@
 export default {
   start: (appStore, swapPanelApi, verticalIconApi, mainPanelApi, resizeFeature) => {
-    swapPanelApi.event.on('toggle', (moduleName) => {
+    swapPanelApi.event.on('toggle', () => {
       resizeFeature.panel1.clientWidth !== 0 ? resizeFeature.minimize() : resizeFeature.maximise()
     })
-    swapPanelApi.event.on('showing', (moduleName) => {
+    swapPanelApi.event.on('showing', () => {
       resizeFeature.panel1.clientWidth === 0 ? resizeFeature.maximise() : ''
-      var current = appStore.getOne(moduleName)
-      // warn the content that it is being displayed. TODO should probably be done in each view
-      if (current && current.api.__showing) current.api.__showing()
     })
     mainPanelApi.event.on('toggle', () => {
       resizeFeature.maximise()
