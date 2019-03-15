0439761fe9fe6e1a75b7707a846f37d3e561d278
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-03-15 11:25:47 +0100

    use local debug instance instead of api

--- src/app.js
+++ src/app.js
@@ -503,7 +503,7 @@ Please make a backup of your contracts and start using http://remix.ethereum.org
   var txLogger = new TxLogger() // eslint-disable-line
   txLogger.event.register('debuggingRequested', (hash) => {
     if (!appStore.isActive('debugger')) appManager.activateOne('debugger')
-    appStore.getOne('debugger').api.debugger().debug(hash)
+    debug.debugger().debug(hash)
     verticalIconsApi.select('debugger')
   })
 
