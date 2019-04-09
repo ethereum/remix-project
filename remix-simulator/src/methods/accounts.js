f604436155d05ba9453f5904d2dffa496d306146
Iuri Matias <iuri.matias@gmail.com>
2019-04-09 14:05:24 -0400

    add eth_getBalance; use accounts in executionContext vm

--- remix-simulator/src/methods/accounts.js
+++ remix-simulator/src/methods/accounts.js
@@ -1,3 +1,7 @@
+var RemixLib = require('remix-lib')
+var executionContext = RemixLib.execution.executionContext
+var ethJSUtil = require('ethereumjs-util')
+var BN = ethJSUtil.BN
 var Web3 = require('web3')
 
 var Accounts = function () {
@@ -5,23 +9,37 @@ var Accounts = function () {
   // TODO: make it random and/or use remix-libs
   this.accounts = [this.web3.eth.accounts.create(['abcd']), this.web3.eth.accounts.create(['ef12']), this.web3.eth.accounts.create(['ef34'])]
 
-  this.accounts[this.accounts[0].address.toLowerCase()] = this.accounts[0]
-  this.accounts[this.accounts[1].address.toLowerCase()] = this.accounts[1]
-  this.accounts[this.accounts[2].address.toLowerCase()] = this.accounts[2]
+  executionContext.init({get: () => { return true }})
 
-  this.accounts[this.accounts[0].address.toLowerCase()].privateKey = Buffer.from(this.accounts[this.accounts[0].address.toLowerCase()].privateKey.slice(2), 'hex')
-  this.accounts[this.accounts[1].address.toLowerCase()].privateKey = Buffer.from(this.accounts[this.accounts[1].address.toLowerCase()].privateKey.slice(2), 'hex')
-  this.accounts[this.accounts[2].address.toLowerCase()].privateKey = Buffer.from(this.accounts[this.accounts[2].address.toLowerCase()].privateKey.slice(2), 'hex')
+  for (let _account of this.accounts) {
+    executionContext.vm().stateManager.getAccount(Buffer.from(_account.address.toLowerCase().replace("0x", ""), 'hex'), (err, account) => {
+      var balance = '0x56BC75E2D63100000'
+      account.balance = balance || '0xf00000000000000001'
+    })
+  }
 }
 
 Accounts.prototype.methods = function () {
   return {
-    eth_accounts: this.eth_accounts.bind(this)
+    eth_accounts: this.eth_accounts.bind(this),
+    eth_getBalance: this.eth_getBalance.bind(this)
   }
 }
 
 Accounts.prototype.eth_accounts = function (payload, cb) {
   return cb(null, this.accounts.map((x) => x.address))
+}
+
+Accounts.prototype.eth_getBalance = function (payload, cb) {
+  let address = payload.params[0]
+  address = ethJSUtil.stripHexPrefix(address)
+
+  executionContext.vm().stateManager.getAccount(Buffer.from(address, 'hex'), function (err, account) {
+    if (err) {
+      return cb('Account not found')
+    }
+    cb(null, new BN(account.balance).toString(10))
+  })
 }
 
 module.exports = Accounts
