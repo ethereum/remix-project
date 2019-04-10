d1ae60b7291d91749a97431ae6a4c8d22c132332
Iuri Matias <iuri.matias@gmail.com>
2019-04-10 09:12:33 -0400

    add eth_sign & eth_getCompilers

--- remix-simulator/README.md
+++ remix-simulator/README.md
@@ -23,7 +23,7 @@ Implemented:
 * [_] eth_getUncleCountByBlockHash
 * [_] eth_getUncleCountByBlockNumber
 * [~] eth_getCode
-* [_] eth_sign
+* [~] eth_sign
 * [X] eth_sendTransaction
 * [_] eth_sendRawTransaction
 * [X] eth_call
@@ -36,7 +36,7 @@ Implemented:
 * [~] eth_getTransactionReceipt
 * [_] eth_getUncleByBlockHashAndIndex
 * [_] eth_getUncleByBlockNumberAndIndex
-* [_] eth_getCompilers (DEPRECATED)
+* [X] eth_getCompilers (DEPRECATED)
 * [_] eth_compileSolidity (DEPRECATED)
 * [_] eth_compileLLL (DEPRECATED)
 * [_] eth_compileSerpent (DEPRECATED)
@@ -65,4 +65,20 @@ Implemented:
 * [_] shh_uninstallFilter
 * [_] shh_getFilterChanges
 * [_] shh_getMessages
+* [_] bzz_hive (stub)
+* [_] bzz_info (stub)
+* [_] debug_traceTransaction
+* [_] eth_subscribe
+* [_] eth_unsubscribe
+* [_] miner_start
+* [_] miner_stop
+* [_] personal_listAccounts
+* [_] personal_lockAccount
+* [_] personal_newAccount
+* [_] personal_importRawKey
+* [_] personal_unlockAccount
+* [_] personal_sendTransaction
+* [_] rpc_modules
+* [_] web3_clientVersion
+* [_] web3_sha3
 
--- remix-simulator/src/methods/accounts.js
+++ remix-simulator/src/methods/accounts.js
@@ -8,10 +8,13 @@ var Accounts = function () {
   this.web3 = new Web3()
   // TODO: make it random and/or use remix-libs
   this.accounts = [this.web3.eth.accounts.create(['abcd']), this.web3.eth.accounts.create(['ef12']), this.web3.eth.accounts.create(['ef34'])]
+  this.accountsKeys = {}
 
   executionContext.init({get: () => { return true }})
 
   for (let _account of this.accounts) {
+    this.accountsKeys[_account.address.toLowerCase()] = _account.privateKey
+
     executionContext.vm().stateManager.getAccount(Buffer.from(_account.address.toLowerCase().replace("0x", ""), 'hex'), (err, account) => {
       var balance = '0x56BC75E2D63100000'
       account.balance = balance || '0xf00000000000000001'
@@ -22,7 +25,8 @@ var Accounts = function () {
 Accounts.prototype.methods = function () {
   return {
     eth_accounts: this.eth_accounts.bind(this),
-    eth_getBalance: this.eth_getBalance.bind(this)
+    eth_getBalance: this.eth_getBalance.bind(this),
+    eth_sign: this.eth_sign.bind(this)
   }
 }
 
@@ -34,12 +38,24 @@ Accounts.prototype.eth_getBalance = function (payload, cb) {
   let address = payload.params[0]
   address = ethJSUtil.stripHexPrefix(address)
 
-  executionContext.vm().stateManager.getAccount(Buffer.from(address, 'hex'), function (err, account) {
+  executionContext.vm().stateManager.getAccount(Buffer.from(address, 'hex'), (err, account) => {
     if (err) {
       return cb('Account not found')
     }
     cb(null, new BN(account.balance).toString(10))
   })
+}
+
+Accounts.prototype.eth_sign = function (payload, cb) {
+  let address = payload.params[0]
+  let message = payload.params[1]
+
+  let privateKey = this.accountsKeys[address]
+  let account = web3.eth.accounts.privateKeyToAccount(privateKey)
+
+  let data = account.sign(message)
+
+  cb(null, data.signature)
 }
 
 module.exports = Accounts
