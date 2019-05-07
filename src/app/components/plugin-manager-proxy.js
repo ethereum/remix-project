854a934c17b9de7b8975390cb895fe2dce52db34
yann300 <yann.levreau@gmail.com>
2019-05-07 11:50:58 +0200

    fix listening on vyper event

--- src/app/components/plugin-manager-proxy.js
+++ src/app/components/plugin-manager-proxy.js
@@ -9,9 +9,9 @@ class PluginManagerProxy {
   constructor () {
     this.event = new EventManager()
     this._listeners = {}
-    this._listeners['vyper'] = (data) => {
-      registry.get('compilersartefacts').api['__last'] = new CompilerAbstract(data.language, data.result, data.content)
-      this.event.trigger('sendCompilationResult', [data.title, data.content, data.language, data.result])
+    this._listeners['vyper'] = (file, source, languageVersion, data) => {
+      registry.get('compilersartefacts').api['__last'] = new CompilerAbstract(languageVersion, data, source)
+      this.event.trigger('sendCompilationResult', [file, source, languageVersion, data])
     }
     this._listeners['solidity'] = (file, source, languageVersion, data) => {
       registry.get('compilersartefacts').api['__last'] = new CompilerAbstract(languageVersion, data, source)
