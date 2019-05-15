0f837d1981dd7ddcb7ea6e77a3fc3f7159859ff6
yann300 <yann.levreau@gmail.com>
2019-05-15 11:28:35 +0200

    truncate if message too long

--- src/app/ui/tooltip.js
+++ src/app/ui/tooltip.js
@@ -1,5 +1,6 @@
 var yo = require('yo-yo')
 var css = require('./styles/tooltip-styles')
+var modal = require('./modal-dialog-custom')
 
 /**
  * Open a tooltip
@@ -25,9 +26,11 @@ class Toaster {
     opts = defaultOptions(opts)
 
     return new Promise((resolve, reject) => {
+      const shortTooltipText = tooltipText.length > 201 ? tooltipText.substring(0, 200) + '...' : tooltipText
+
       this.tooltip = yo`
     <div class="${css.tooltip} bg-secondary" onmouseenter=${() => { over() }} onmouseleave=${() => { out() }}>
-      <span>${tooltipText}</span>
+      <span>${shortTooltipText}<button class="btn btn-secondary btn-sm" onclick=${() => { modal.alert(tooltipText) }}>show full message</button></span>
       ${action}
     </div>`
       let timeOut = () => {
