19edeec59ea196730c998d3810de79945dfddb36
yann300 <yann.levreau@gmail.com>
2019-05-23 16:41:53 +0200

    bump default solidiy version

--- ci/makeMockCompiler.js
+++ ci/makeMockCompiler.js
@@ -3,7 +3,7 @@
 var fs = require('fs')
 var compiler = require('solc')
 var compilerInput = require('remix-solidity').CompilerInput
-var defaultVersion = 'v0.5.1+commit.c8a2cb62'
+var defaultVersion = 'v0.5.10+commit.5a6ea5b1'
 
 compiler.loadRemoteVersion(defaultVersion, (error, solcSnapshot) => {
   if (error) console.log(error)
@@ -43,6 +43,12 @@ function compile (solcSnapshot, source, optimization, addCompilationResult) {
     input = input.replace(/(\t)|(\n)|(\\n)|( )/g, '')
   } catch (e) {
     console.log(e)
+  }
+  if (result && (result.error || (result.errors && result.errors.length > 0))) {
+    console.log(result.error, result.errors)
+  }
+  if (result) {
+    console.log(result.error, result.errors)
   }
   var ret = {
     key: input,
