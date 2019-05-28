933b562dfee18b4f187fd0909ef7db4ca71ae550
LianaHus <liana@ethdev.com>
2019-05-28 19:36:32 +0200

    added showHome to vertical icon panel

--- src/app/components/vertical-icons.js
+++ src/app/components/vertical-icons.js
@@ -210,12 +210,8 @@ export class VerticalIcons {
   }
 
   updateActivations (name) {
-    if (name === 'home') {
-      this.activateHome()
-    } else {
-      this.removeActive()
-      this.addActive(name)
-    }
+    this.removeActive()
+    this.addActive(name)
   }
 
   onThemeChanged (themeType) {
@@ -227,7 +223,10 @@ export class VerticalIcons {
     }
   }
 
-  activateHome () {
+  /**
+   * Show the home page
+   */
+  showHome () {
     globalRegistry.get('appmanager').api.ensureActivated('home')
   }
 
@@ -236,7 +235,7 @@ export class VerticalIcons {
     <div
       class="${css.homeIcon}"
       onclick="${(e) => {
-        this.activateHome()
+        this.showHome()
       }}"
       plugin="${this.homeProfile.name}" title="${this.homeProfile.displayName}"
     >
