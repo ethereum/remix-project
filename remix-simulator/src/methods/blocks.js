c6f72c4eda97831a49860cb3ead7f58a06680c65
Iuri Matias <iuri.matias@gmail.com>
2019-06-12 07:49:38 -0400

    fix linting issues

--- remix-simulator/src/methods/blocks.js
+++ remix-simulator/src/methods/blocks.js
@@ -1,4 +1,3 @@
-var Web3 = require('web3')
 var RemixLib = require('remix-lib')
 var executionContext = RemixLib.execution.executionContext
 
@@ -25,34 +24,38 @@ Blocks.prototype.methods = function () {
 Blocks.prototype.eth_getBlockByNumber = function (payload, cb) {
   var block = executionContext.blocks[payload.params[0]]
 
+  if (!block) {
+    return cb(new Error('block not found'))
+  }
+
   let b = {
     'number': toHex(block.header.number),
-    "hash": toHex(block.hash()),
+    'hash': toHex(block.hash()),
     'parentHash': toHex(block.header.parentHash),
     'nonce': toHex(block.header.nonce),
-    "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-    "logsBloom": "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331",
-    "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-    "stateRoot": toHex(block.header.stateRoot),
+    'sha3Uncles': '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
+    'logsBloom': '0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331',
+    'transactionsRoot': '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+    'stateRoot': toHex(block.header.stateRoot),
     'miner': this.coinbase,
     'difficulty': toHex(block.header.difficulty),
-    "totalDifficulty": toHex(block.header.totalDifficulty),
+    'totalDifficulty': toHex(block.header.totalDifficulty),
     'extraData': toHex(block.header.extraData),
-    "size": "0x027f07", // 163591
+    'size': '0x027f07', // 163591
     'gasLimit': toHex(block.header.gasLimit),
     'gasUsed': toHex(block.header.gasUsed),
-    "timestamp": toHex(block.header.timestamp),
-    "transactions": block.transactions.map((t) => "0x" + t.hash().toString('hex')),
-    "uncles": []
+    'timestamp': toHex(block.header.timestamp),
+    'transactions': block.transactions.map((t) => '0x' + t.hash().toString('hex')),
+    'uncles': []
   }
 
   cb(null, b)
 }
 
-function toHex(value) {
-  if (!value) return "0x0"
+function toHex (value) {
+  if (!value) return '0x0'
   let v = value.toString('hex')
-  return ((v === "0x" || v === "") ? "0x0" : ("0x" + v))
+  return ((v === '0x' || v === '') ? '0x0' : ('0x' + v))
 }
 
 Blocks.prototype.eth_getBlockByHash = function (payload, cb) {
@@ -60,23 +63,23 @@ Blocks.prototype.eth_getBlockByHash = function (payload, cb) {
 
   let b = {
     'number': toHex(block.header.number),
-    "hash": toHex(block.hash()),
+    'hash': toHex(block.hash()),
     'parentHash': toHex(block.header.parentHash),
     'nonce': toHex(block.header.nonce),
-    "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-    "logsBloom": "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331",
-    "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-    "stateRoot": toHex(block.header.stateRoot),
+    'sha3Uncles': '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
+    'logsBloom': '0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331',
+    'transactionsRoot': '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+    'stateRoot': toHex(block.header.stateRoot),
     'miner': this.coinbase,
     'difficulty': toHex(block.header.difficulty),
-    "totalDifficulty": toHex(block.header.totalDifficulty),
+    'totalDifficulty': toHex(block.header.totalDifficulty),
     'extraData': toHex(block.header.extraData),
-    "size": "0x027f07", // 163591
+    'size': '0x027f07', // 163591
     'gasLimit': toHex(block.header.gasLimit),
     'gasUsed': toHex(block.header.gasUsed),
-    "timestamp": toHex(block.header.timestamp),
-    "transactions": block.transactions.map((t) => "0x" + t.hash().toString('hex')),
-    "uncles": []
+    'timestamp': toHex(block.header.timestamp),
+    'transactions': block.transactions.map((t) => '0x' + t.hash().toString('hex')),
+    'uncles': []
   }
 
   cb(null, b)
