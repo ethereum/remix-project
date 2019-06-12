fea3ce09ba265b5f23cc0810d3c6a5700ce39e03
Rob Stupay <rob@ethereum.org>
2019-06-12 11:41:12 +0200

    update to modal message

--- src/app/files/remixd-handle.js
+++ src/app/files/remixd-handle.js
@@ -97,8 +97,8 @@ function remixdDialog () {
         Before you get started, check out the <a target="_blank" href="https://remix.readthedocs.io/en/latest/remixd.html">Remixd tutorial</a>.
         to find out how to run Remixd.
       </div>
-      <div class=${css.dialogParagraph}>If you have looked at that tutorial and are just looking for the remixd command, <br /> here it is:
-        remixd -s absolute-path-to-the-shared-folder --remix-ide your-remix-ide-URL-instance
+      <div class=${css.dialogParagraph}>If you have looked at that tutorial and are just looking for the remixd command, <br> here it is:
+        <br><b>remixd -s absolute-path-to-the-shared-folder --remix-ide your-remix-ide-URL-instance</b>
       </div>
       <div class=${css.dialogParagraph}>Connection will start a session between <em>${window.location.href}</em> and your local file system <i>ws://127.0.0.1:65520</i>
         so please make sure your system is secured enough (port 65520 neither opened nor forwarded).
