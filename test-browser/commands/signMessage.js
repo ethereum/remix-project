c0d04f9c147fa6f5fb4a625b8f87112169d79f15
yann300 <yann.levreau@gmail.com>
2019-06-20 09:27:51 +0200

    move helpers to nightwatch commands

---
+++ test-browser/commands/signMessage.js
@@ -0,0 +1,41 @@
+const EventEmitter = require('events')
+
+class SelectContract extends EventEmitter {
+  command (msg) {
+    this.api.perform((done) => {
+      signMsg(this.api, msg, (hash, signature) => {
+        done()
+        this.emit('complete')
+      })
+    })
+    return this
+  }
+}
+
+function signMsg (browser, msg, cb) {
+  let hash, signature
+  browser
+      .waitForElementPresent('i[id="remixRunSignMsg"]')
+      .click('i[id="remixRunSignMsg"]')
+      .waitForElementPresent('textarea[id="prompt_text"]')
+      .setValue('textarea[id="prompt_text"]', msg, () => {
+        browser.modalFooterOKClick().perform(
+          (client, done) => {
+            browser.getText('span[id="remixRunSignMsgHash"]', (v) => { hash = v; done() })
+          }
+        )
+        .perform(
+          (client, done) => {
+            browser.getText('span[id="remixRunSignMsgSignature"]', (v) => { signature = v; done() })
+          }
+        )
+        .modalFooterOKClick()
+        .perform(
+          () => {
+            cb(hash, signature)
+          }
+        )
+      })
+}
+
+module.exports = SelectContract
