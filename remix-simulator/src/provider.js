41dd2bb4f8ee6f47b748c0f1259b8505d66c1db5
Iuri Matias <iuri.matias@gmail.com>
2019-07-12 17:42:31 -0400

    add subscription & logs methods; create logmanager to direct subscriptiosn

--- remix-simulator/src/provider.js
+++ remix-simulator/src/provider.js
@@ -1,8 +1,12 @@
+var RemixLib = require('remix-lib')
+var executionContext = RemixLib.execution.executionContext
+
 const log = require('./utils/logs.js')
 const merge = require('merge')
 
 const Accounts = require('./methods/accounts.js')
 const Blocks = require('./methods/blocks.js')
+const Filters = require('./methods/filters.js')
 const Misc = require('./methods/misc.js')
 const Net = require('./methods/net.js')
 const Transactions = require('./methods/transactions.js')
@@ -18,11 +22,16 @@ var Provider = function (options) {
   this.methods = merge(this.methods, this.Accounts.methods())
   this.methods = merge(this.methods, (new Blocks(options)).methods())
   this.methods = merge(this.methods, (new Misc()).methods())
+  this.methods = merge(this.methods, (new Filters()).methods())
   this.methods = merge(this.methods, (new Net()).methods())
   this.methods = merge(this.methods, (this.Transactions.methods()))
   this.methods = merge(this.methods, (new Whisper()).methods())
 
   generateBlock()
+
+  setTimeout(() => {
+    console.dir("hello!")
+  }, 10 * 1000)
 }
 
 Provider.prototype.init = async function () {
@@ -32,14 +41,19 @@ Provider.prototype.init = async function () {
 
 Provider.prototype.sendAsync = function (payload, callback) {
   log.info('payload method is ', payload.method)
+  console.dir(payload)
 
   let method = this.methods[payload.method]
   if (method) {
     return method.call(method, payload, (err, result) => {
       if (err) {
+        console.dir("====== error")
+        console.dir(err)
         return callback(err)
       }
       let response = {'id': payload.id, 'jsonrpc': '2.0', 'result': result}
+      console.dir("response")
+      console.dir(response)
       callback(null, response)
     })
   }
@@ -52,6 +66,10 @@ Provider.prototype.send = function (payload, callback) {
 
 Provider.prototype.isConnected = function () {
   return true
+}
+
+Provider.prototype.on = function (type, cb) {
+  executionContext.logsManager.addListener(type, cb)
 }
 
 module.exports = Provider
