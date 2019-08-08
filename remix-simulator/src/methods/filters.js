4dff40f2c0b359c5f99aa22836d8469f81af644a
Iuri Matias <iuri.matias@gmail.com>
2019-08-08 14:23:52 -0400

    add eth_unsubscribe

--- remix-simulator/src/methods/filters.js
+++ remix-simulator/src/methods/filters.js
@@ -8,7 +8,8 @@ var Filters = function (_options) {
 Filters.prototype.methods = function () {
   return {
     eth_getLogs: this.eth_getLogs.bind(this),
-    eth_subscribe: this.eth_subscribe.bind(this)
+    eth_subscribe: this.eth_subscribe.bind(this),
+    eth_unsubscribe: this.eth_unsubscribe.bind(this)
   }
 }
 
@@ -43,6 +44,17 @@ Filters.prototype.eth_subscribe = function (payload, cb) {
   let subscriptionId = executionContext.logsManager.subscribe(payload.params);
 
   cb(null, subscriptionId)
+}
+
+Filters.prototype.eth_unsubscribe = function (payload, cb) {
+  console.dir("===============================")
+  console.dir("===============================")
+  console.dir("=== eth_unsubscribe")
+  console.dir(payload.params)
+
+  executionContext.logsManager.unsubscribe(payload.params[0])
+
+  cb(null, true)
 }
 
 module.exports = Filters
