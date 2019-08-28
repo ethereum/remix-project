9ef716ff7c1800fa7659ea06391e5da12694774b
Iuri Matias <iuri.matias@gmail.com>
2019-08-28 17:49:06 -0400

    add account tests

--- remix-simulator/test/accounts.js
+++ remix-simulator/test/accounts.js
@@ -10,8 +10,32 @@ describe('Accounts', function () {
     web3.setProvider(provider)
   })
 
-  it('should get a list of accounts', async function () {
-    let accounts = await web3.eth.getAccounts()
-    assert.notEqual(accounts.length, 0)
+  describe('eth_getAccounts', () => {
+    it('should get a list of accounts', async function () {
+      let accounts = await web3.eth.getAccounts()
+      assert.notEqual(accounts.length, 0)
+    })
+  })
+
+  describe('eth_getBalance', () => {
+    it('should get a account balance', async function () {
+      let accounts = await web3.eth.getAccounts()
+      let balance0 = await web3.eth.getBalance(accounts[0])
+      let balance1 = await web3.eth.getBalance(accounts[1])
+      let balance2 = await web3.eth.getBalance(accounts[2])
+
+      assert.deepEqual(balance0, '100000000000000000000')
+      assert.deepEqual(balance1, '100000000000000000000')
+      assert.deepEqual(balance2, '100000000000000000000')
+    })
+  })
+
+  describe('eth_sign', () => {
+    it('should sign payloads', async function () {
+      let accounts = await web3.eth.getAccounts()
+      let signature = await web3.eth.sign('Hello world', accounts[0])
+
+      assert.deepEqual(signature.length, 132)
+    })
   })
 })
