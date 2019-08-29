b63475e28a06aba938d14eeb378a42e07c31e387
yann300 <yann.levreau@gmail.com>
2019-08-29 09:45:55 +0200

    using non builtin version of solidity compiler for browser tests

--- test-browser/helpers/init.js
+++ test-browser/helpers/init.js
@@ -1,6 +1,6 @@
 module.exports = function (browser, callback) {
   browser
-    .url('http://127.0.0.1:8080/#version=builtin')
+    .url('http://127.0.0.1:8080')
     .injectScript('test-browser/helpers/applytestmode.js', function () {
       browser.resizeWindow(2560, 1440, () => {
         initModules(browser, () => {
