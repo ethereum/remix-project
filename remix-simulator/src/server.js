145023da0c946d469fd9602a6ed3d41157341567
yann300 <yann.levreau@gmail.com>
2019-09-03 23:01:21 +0200

    use cors

--- remix-simulator/src/server.js
+++ remix-simulator/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const expressWs = require('express-ws')
@@ -14,6 +15,7 @@ class Server {
   start (host, port) {
     expressWs(app)
 
+    app.use(cors())
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
 
