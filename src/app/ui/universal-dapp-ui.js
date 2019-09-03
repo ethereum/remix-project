fe4d4e3eaf8d82df9ac0feeaf318b7e4707fff6e
yann300 <yann.levreau@gmail.com>
2019-09-03 23:03:37 +0200

    use checksum address in UI

--- src/app/ui/universal-dapp-ui.js
+++ src/app/ui/universal-dapp-ui.js
@@ -59,6 +59,7 @@ UniversalDAppUI.prototype.renderInstance = function (contract, address, contract
 UniversalDAppUI.prototype.renderInstanceFromABI = function (contractABI, address, contractName) {
   var self = this
   address = (address.slice(0, 2) === '0x' ? '' : '0x') + address.toString('hex')
+  address = ethJSUtil.toChecksumAddress(address)
   var instance = yo`<div class="instance ${css.instance} ${css.hidesub}" id="instance${address}"></div>`
   var context = self.udapp.context()
 
