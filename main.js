baec371ad1ed9f7f5fbdb2e37b7b387d55865ea9
yann300 <yann.levreau@gmail.com>
2019-09-17 17:52:28 +0200

    add remixd

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const remixd = require('remixd')
+
 const { app, BrowserWindow } = require('electron')
 const { AppManager, registerPackageProtocol } = require('@philipplgh/electron-app-manager')
 registerPackageProtocol(__dirname + '/cache')
@@ -25,3 +27,13 @@ function createWindow () {
 }
 
 app.on('ready', createWindow)
+
+var folder = process.argv.length > 2 ? process.argv[2] : process.cwd()
+
+var router = new remixd.Router(65520, remixd.services.sharedFolder, { remixIdeUrl: 'package://cd339faeeb58f4c96b9b5ff62556c364.mod' }, (webSocket) => {
+  remixd.services.sharedFolder.setWebSocket(webSocket)
+  remixd.services.sharedFolder.setupNotifications(folder)
+  remixd.services.sharedFolder.sharedFolder(folder, false)
+})
+
+router.start()
