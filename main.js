4052ba84b87b2aed64bf1ddecb30b896e3888aec
yann300 <yann.levreau@gmail.com>
2019-09-17 11:47:27 +0200

    firs commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
