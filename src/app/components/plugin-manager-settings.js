7b548779d8ab91fc2c31038ee2c2f1f3c192bf15
alexcherman <sashacherman@gmail.com>
2019-09-18 17:22:27 +0200

    Change plugin manager styles

--- src/app/components/plugin-manager-settings.js
+++ src/app/components/plugin-manager-settings.js
@@ -11,8 +11,12 @@ const css = csjs`
   align-items: center;
   padding: 5px 20px;
 }
-.permissions button {
-  padding: 2px 5px;
+.permissionsButton {
+  height: 32px;
+  padding: 0 12px;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
   cursor: pointer;
 }
 .permissionForm h4 {
@@ -126,8 +130,8 @@ export class PluginManagerSettings {
 
   render () {
     return yo`
-    <footer class="navbar navbar-light bg-light ${css.permissions}">
-      <button onclick="${() => this.openDialog()}" class="btn btn-info">Settings</button>
+    <footer class="bg-light ${css.permissions} remix-bg-opacity">
+      <button onclick="${() => this.openDialog()}" class="btn btn-primary btn-sm ${css.permissionsButton}">Settings</button>
     </footer>`
   }
 
