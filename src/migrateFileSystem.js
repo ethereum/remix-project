38c934094b7cb744efdabed1273b3b72f65116d7
yann300 <yann.levreau@gmail.com>
2019-09-25 09:50:35 +0200

    added comment

--- src/migrateFileSystem.js
+++ src/migrateFileSystem.js
@@ -1,6 +1,9 @@
 
 import { Storage } from 'remix-lib'
 
+/*
+  Migrating the files to the BrowserFS storage instead or raw localstorage
+*/
 export default (fileProvider) => {
   const fileStorage = new Storage('sol:')
   if (fileStorage.keys().length === 0) return
