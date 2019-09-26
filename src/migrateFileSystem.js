384d4f890e43301e4a853ebdb5119e5777be2776
yann300 <yann.levreau@gmail.com>
2019-09-26 13:40:20 +0200

    do not remove the old file structure

--- src/migrateFileSystem.js
+++ src/migrateFileSystem.js
@@ -6,13 +6,16 @@ import { Storage } from 'remix-lib'
 */
 export default (fileProvider) => {
   const fileStorage = new Storage('sol:')
-  if (fileStorage.keys().length === 0) return
+  const flag = 'status'
+  const fileStorageBrowserFS = new Storage('remix_browserFS_migration:')
+  if (fileStorageBrowserFS.get(flag) === 'done') return
   fileStorage.keys().forEach((path) => {
     if (path !== '.remix.config') {
       const content = fileStorage.get(path)
       fileProvider.set(path, content)
-      fileStorage.remove(path)
+      // fileStorage.remove(path) we don't want to remove it as we are still supporting the old version
       console.log('file migrated', path)
     }
   })
+  fileStorageBrowserFS.set(flag, 'done')
 }
