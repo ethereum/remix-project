f781cffda1a81810a8320e6b67d200c99f7030b5
yann300 <yann.levreau@gmail.com>
2019-09-26 10:11:24 +0200

    remove deprecated options

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ module.exports = {
   Router: require('./router'),
   utils: require('./utils'),
   services: {
-    startMistGeth: require('./services/startMistGeth'),
-    startFrontend: require('./services/startFrontend'),
-    autoMine: require('./services/autoMine'),
     sharedFolder: require('./services/sharedFolder')
   }
 }
--- src/services/startFrontend.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (path, port) {
-  console.log('\x1b[31m%s\x1b[0m', '[ERR] Front end capability is not available anymore')
-  function kill () {}
-  return kill
-}
