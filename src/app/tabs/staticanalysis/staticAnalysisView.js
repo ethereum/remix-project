dd12b66f0a28418a8608a8f61d627bbc212200ae
Liana Husikyan <liana@ethdev.com>
2019-09-27 16:13:54 +0200

    remove !

--- src/app/tabs/staticanalysis/staticAnalysisView.js
+++ src/app/tabs/staticanalysis/staticAnalysisView.js
@@ -104,7 +104,7 @@ staticAnalysisView.prototype.run = function () {
       results.map(function (result, i) {
         result.report.map(function (item, i) {
           var location = ''
-          if (!item.location) {
+          if (item.location) {
             var split = item.location.split(':')
             var file = split[2]
             location = {
