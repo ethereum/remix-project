e232ba2d560401690c9fe84415f4538bb52c0e89
Liana Husikyan <liana@ethdev.com>
2019-09-30 18:00:34 +0200

    new theme

--- src/app/tabs/theme-module.js
+++ src/app/tabs/theme-module.js
@@ -3,7 +3,7 @@ import { EventEmitter } from 'events'
 import * as packageJson from '../../../package.json'
 
 const themes = [
-  {name: 'Dark', quality: 'dark', url: 'https://res.cloudinary.com/dvtmp0niu/raw/upload/v1569488189/custom-dark-theme_uq6li7.css'},
+  {name: 'Dark', quality: 'dark', url: 'https://res.cloudinary.com/dvtmp0niu/raw/upload/v1569856440/remix-dark-theme_dh57sz.css'},
   // switching to the url Todo: remove when the theme is ready
   // {name: 'Dark', quality: 'dark', url: 'assets/css/remix-dark-theme.css'},
   {name: 'Cerulean', quality: 'light', url: 'https://bootswatch.com/4/cerulean/bootstrap.min.css'},
