6b96e1f053229ed31bd9c94254068c539a7aff54
aniket-engg <aniketengg.225@gmail.com>
2019-10-01 20:36:00 +0530

    separate ast blocks

---
+++ remix-analyzer/test/analysis/astBlocks/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  localCall: require('./localCall.json'),
+  thisLocalCall: require('./thisLocalCall.json'),
+  libCall: require('./libCall.json'),
+  externalDirect: require('./externalDirect.json'),
+  superLocal: require('./superLocal.json'),
+  assignment: require('./assignment.json'),
+  inlineAssembly: require('./inlineAssembly.json'),
+  forLoopNode: require('./forLoopNode.json'),
+  whileLoopNode: require('./whileLoopNode.json'),
+  doWhileLoopNode: require('./doWhileLoopNode.json')
+}
