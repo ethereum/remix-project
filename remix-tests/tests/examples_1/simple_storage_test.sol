7c3f2cd1fa274b68be1c642dbfe46854b6faeb6a
0mkar <0mkar@protonmail.com>
2019-10-08 20:04:16 +0530

    update remix-tests unit tests

--- remix-tests/tests/examples_1/simple_storage_test.sol
+++ remix-tests/tests/examples_1/simple_storage_test.sol
@@ -8,22 +8,20 @@ contract MyTest {
     foo = new SimpleStorage();
   }
 
-  function initialValueShouldBe100() public view returns (bool) {
-    //return Assert.equal(foo.get(), 100, "initial value is not correct");
-    return foo.get() == 100;
+  function initialValueShouldBe100() public returns (bool) {
+    return Assert.equal(foo.get(), 100, "initial value is not correct");
   }
 
-  function initialValueShouldBe200() public view returns (bool) {
-    //return Assert.equal(foo.get(), 200, "initial value is not correct");
-    return foo.get() == 200;
+  function initialValueShouldNotBe200() public returns (bool) {
+    return Assert.notEqual(foo.get(), 200, "initial value is not correct");
   }
 
   function shouldTriggerOneFail() public {
-    Assert.equal(uint(1), uint(2), "the test 1 fails");
-    Assert.equal(uint(1), uint(2), "the test 2 fails");
+    Assert.equal(uint(1), uint(2), "uint test 1 fails");
+    Assert.notEqual(uint(1), uint(2), "uint test 2 passes");
   }
 
   function shouldTriggerOnePass() public {
-    Assert.equal(uint(1), uint(1), "the test 3 fails");
+    Assert.equal(uint(1), uint(1), "uint test 3 passes");
   }
 }
--- remix-tests/tests/examples_3/simple_string_test.sol
+++ remix-tests/tests/examples_3/simple_string_test.sol
@@ -8,15 +8,11 @@ contract StringTest {
     foo = new SimpleString();
   }
 
-  function initialValueShouldBeHello() public returns (bool) {
+  function initialValueShouldBeHelloWorld() public returns (bool) {
     return Assert.equal(foo.get(), "Hello world!", "initial value is not correct");
   }
 
   function valueShouldNotBeHelloWordl() public returns (bool) {
-    return Assert.notEqual(foo.get(), "Hello wordl!", "value should not be hello world");
-  }
-
-  function valueShouldBeHelloWorld() public returns (bool) {
-    return Assert.equal(foo.get(), "Hello wordl!", "initial value is not correct");
+    return Assert.notEqual(foo.get(), "Hello wordl!", "value should not be hello wordl");
   }
 }
