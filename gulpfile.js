e7886861de2a95a619fec661fb029da1b57b19e9
aniket-engg <aniketengg.225@gmail.com>
2019-12-03 12:44:19 +0530

    changelog & gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ const promisifyExec = util.promisify(require('child_process').exec);
 
 var packageJSON = require('./package.json');
 
-task('publishTag', function () {
-    exec("git tag v"+ packageJSON.version +"; git push --tags");
+task('publishTag', async function () {
+    await promisifyExec("git tag v"+ packageJSON.version +"; git push --tags");
 });
 
 task('updateChangelog', async function update() {
     let previous_version = process.argv[4];
-    let next_version = packageJSON.version;
+    let next_version = "v" + packageJSON.version;
     await promisifyExec("github-changes -o ethereum -r remix -a --file changes.md --only-pulls --use-commit-body --only-merges --between-tags " + previous_version + "..." + next_version);
     let data = fs.readFileSync(__dirname + '/changes.md', 'utf8') + '\n\n' + fs.readFileSync(__dirname + '/CHANGELOG.md', 'utf8')
     fs.unlinkSync(__dirname + '/CHANGELOG.md');
