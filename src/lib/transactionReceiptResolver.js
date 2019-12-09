08f9bc7de9c48dbc220e2aa78f1c5fd24ebdad49
Iuri Matias <iuri.matias@gmail.com>
2019-12-09 16:58:23 -0500

    pass executionContext as param instead of relying in a global var

--- src/lib/transactionReceiptResolver.js
+++ src/lib/transactionReceiptResolver.js
@@ -1,16 +1,16 @@
 'use strict'
-var executionContext = require('../execution-context')
 
 module.exports = class TransactionReceiptResolver {
-  constructor () {
+  constructor (executionContext) {
     this._transactionReceipts = {}
+    this.executionContext = executionContext
   }
 
   resolve (tx, cb) {
     if (this._transactionReceipts[tx.hash]) {
       return cb(null, this._transactionReceipts[tx.hash])
     }
-    executionContext.web3().eth.getTransactionReceipt(tx.hash, (error, receipt) => {
+    this.executionContext.web3().eth.getTransactionReceipt(tx.hash, (error, receipt) => {
       if (!error) {
         this._transactionReceipts[tx.hash] = receipt
         cb(null, receipt)
