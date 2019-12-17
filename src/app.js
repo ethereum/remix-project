f51f3308e6d02570d9d872be7778655e5caa1318
Iuri Matias <iuri.matias@gmail.com>
2019-12-17 17:44:53 -0500

    add missing executionContext

--- src/app.js
+++ src/app.js
@@ -250,7 +250,7 @@ Please make a backup of your contracts and start using http://remix.ethereum.org
 
   // LAYOUT & SYSTEM VIEWS
   const appPanel = new MainPanel()
-  const mainview = new MainView(editor, appPanel, fileManager, appManager, txlistener, eventsDecoder)
+  const mainview = new MainView(editor, appPanel, fileManager, appManager, txlistener, eventsDecoder, executionContext)
   registry.put({ api: mainview, name: 'mainview' })
 
   appManager.register([
