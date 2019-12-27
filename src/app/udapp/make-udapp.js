46c6f8c62c62d2e43a358159a319b6ad5ad83623
Iuri Matias <iuri.matias@gmail.com>
2019-12-27 16:08:15 -0500

    move txDetailsLink into remix-ide

--- src/app/udapp/make-udapp.js
+++ src/app/udapp/make-udapp.js
@@ -4,11 +4,25 @@ var yo = require('yo-yo')
 var EventsDecoder = remixLib.execution.EventsDecoder
 var TransactionReceiptResolver = require('../../lib/transactionReceiptResolver')
 
+const transactionDetailsLinks = {
+  'Main': 'https://www.etherscan.io/tx/',
+  'Rinkeby': 'https://rinkeby.etherscan.io/tx/',
+  'Ropsten': 'https://ropsten.etherscan.io/tx/',
+  'Kovan': 'https://kovan.etherscan.io/tx/',
+  'Goerli': 'https://goerli.etherscan.io/tx/'
+}
+
+function txDetailsLink (network, hash) {
+  if (transactionDetailsLinks[network]) {
+    return transactionDetailsLinks[network] + hash
+  }
+}
+
 export function makeUdapp (blockchain, udapp, executionContext, compilersArtefacts, logHtmlCallback) {
   // ----------------- UniversalDApp -----------------
   // TODO: to remove when possible
   udapp.event.register('transactionBroadcasted', (txhash, networkName) => {
-    var txLink = executionContext.txDetailsLink(networkName, txhash)
+    var txLink = txDetailsLink(networkName, txhash)
     if (txLink && logHtmlCallback) logHtmlCallback(yo`<a href="${txLink}" target="_blank">${txLink}</a>`)
   })
 
