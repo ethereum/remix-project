ddec15003613578a5c1c0e6104316899379f3789
aniket-engg <aniketengg.225@gmail.com>
2019-12-30 12:38:22 +0530

    basic switch to ts
    types introduced
    
    suggested changes and worker types
    
    more types
    
    more types and code style update
    
    fix to run compilation very first time after plugin activation

--- remix-solidity/index.js
+++
@@ -1,7 +0,0 @@
-const Compiler = require('./src/compiler/compiler')
-const CompilerInput = require('./src/compiler/compiler-input')
-
-module.exports = {
-  Compiler: Compiler,
-  CompilerInput: CompilerInput
-}
