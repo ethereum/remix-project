b53b66f1ae90d0f015ae8a6d59848f36266d8121
Iuri Matias <iuri.matias@gmail.com>
2020-01-02 07:43:56 -0500

    abstract udapp plugin so it maintains backwards compatibilify with the plugin api

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ var CompilerMetadata = require('./app/files/compiler-metadata')
 var CompilerImport = require('./app/compiler/compiler-imports')
 
 const Blockchain = require('./blockchain/blockchain.js')
+const PluginUDapp = require('./blockchain/pluginUDapp.js')
 
 const PluginManagerComponent = require('./app/components/plugin-manager-component')
 const CompilersArtefacts = require('./app/compiler/compiler-artefacts')
@@ -223,6 +224,7 @@ Please make a backup of your contracts and start using http://remix.ethereum.org
   registry.put({api: fileManager, name: 'filemanager'})
 
   const blockchain = new Blockchain(registry.get('config').api)
+  const pluginUdapp = new PluginUDapp(blockchain)
 
   // ----------------- compilation metadata generation servive ----------------------------
   const compilerMetadataGenerator = new CompilerMetadata(blockchain, fileManager, registry.get('config').api)
@@ -295,6 +297,7 @@ Please make a backup of your contracts and start using http://remix.ethereum.org
   )
   const run = new RunTab(
     blockchain,
+    pluginUdapp,
     registry.get('config').api,
     registry.get('filemanager').api,
     registry.get('editor').api,
