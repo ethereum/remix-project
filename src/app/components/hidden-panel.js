dd83ec394b5b7d6b7f148f706819e9afe2cc0db0
yann300 <yann.levreau@gmail.com>
2020-01-10 11:25:47 +0100

    change vm

--- src/app/components/hidden-panel.js
+++ src/app/components/hidden-panel.js
@@ -14,7 +14,7 @@ const profile = {
   displayName: 'Hidden Panel',
   description: '',
   version: packageJson.version,
-  methods: []
+  methods: ['addView', 'removeView']
 }
 
 export class HiddenPanel extends AbstractPanel {
