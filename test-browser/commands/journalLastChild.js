70f893269b688adb2f61c56f3a5edf5e6d383f3d
LianaHus <liana@ethdev.com>
2020-01-22 10:25:41 +0100

    check for numeric type of amount using sendRawTransaction refactoring add check for hexadecimal:wq added ids

--- test-browser/commands/journalLastChild.js
+++ test-browser/commands/journalLastChild.js
@@ -3,10 +3,10 @@ const EventEmitter = require('events')
 class JournalLastChild extends EventEmitter {
   command (val) {
     this.api
-        .waitForElementVisible('#journal div:last-child span.text-info', 10000)
-        .assert.containsText('#journal div:last-child span.text-info', val).perform(() => {
-          this.emit('complete')
-        })
+    .waitForElementVisible('#journal div:last-child span.text-info', 10000)
+    .assert.containsText('#journal div:last-child span.text-info', val).perform(() => {
+      this.emit('complete')
+    })
     return this
   }
 }
