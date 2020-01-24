0a8699ca266d567476dee796b3fc73d9d204f51a
LianaHus <liana@ethdev.com>
2020-01-24 10:43:23 +0100

    added a test

---
+++ test-browser/tests/specialFunctions.js
@@ -0,0 +1,58 @@
+'use strict'
+var init = require('../helpers/init')
+var sauce = require('./sauce')
+
+module.exports = {
+  before: function (browser, done) {
+    init(browser, done)
+  },
+  '@sources': function () {
+    return sources
+  },
+  'Use special functions receive/fullback': function (browser) {
+    browser.waitForElementVisible('#icon-panel', 10000)
+    .testContracts('receiveAndfFallback.sol', sources[0]['browser/Untitled.sol'], ['CheckSpecials'])
+    .clickLaunchIcon('udapp'
+      .selectContract('CheckSpecials')
+      .createContract('')
+      .clickInstance(0)
+      .perform((done) => {
+        browser.getAddressAtPosition(0, (address) => {
+          browser.sendLowLevelTx(address, '0', '0xaa')
+          .journalLastChild('dd')
+          .waitForElementVisible('label[id="deployAndRunLLTxError"]')
+          .assert.containsText('label[id="deployAndRunLLTxError"]', '')
+          .perform(done)
+        })
+      })
+    )
+  },
+  tearDown: sauce
+}
+
+var sources = [
+  {
+    'browser/receiveAndfFallback.sol': {
+      content: `
+        contract CheckSpecials {
+          receive() payable external{}
+          fallback() external {}
+        }
+      `
+    },
+    'browser/receiveOnly.sol': {
+      content: `
+        contract CheckSpecials {
+          receive() payable external{}
+        }
+      `
+    },
+    'browser/fallbackOnly.sol': {
+      content: `
+        contract CheckSpecials {
+          fallback() payable external{}
+        }
+      `
+    }
+  }
+]
