727cf75d8c82ea69cb14b1f765a8b0fbb62c8331
aniket-engg <aniketengg.225@gmail.com>
2020-02-05 15:31:15 +0530

    linting fix

--- remix-debug/src/solidity-decoder/internalCallTree.js
+++ remix-debug/src/solidity-decoder/internalCallTree.js
@@ -199,7 +199,7 @@ function includeVariableDeclaration (tree, step, sourceLocation, scopeId, newLoc
       // so, either this is the direct value, or the offset in memory. That depends on the type.
       if (!error) {
         tree.solidityProxy.contractNameAt(step, (error, contractName) => { // cached
-          if (!error && variableDeclaration.attributes.name != '') {
+          if (!error && variableDeclaration.attributes.name !== '') {
             var states = tree.solidityProxy.extractStatesDefinitions()
             var location = typesUtil.extractLocationFromAstVariable(variableDeclaration)
             location = location === 'default' ? 'storage' : location
