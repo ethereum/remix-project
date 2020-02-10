6a55230caef667b8ea5ee5e3b742a94c88fdbfac
aniket-engg <aniketengg.225@gmail.com>
2020-02-10 15:48:53 +0530

    remix-analyzer: js to ts

---
+++ remix-analyzer/src/solidity-analyzer/modules/assignAndCompare.ts
@@ -0,0 +1,25 @@
+import { MISC } from './categories'
+const common = require('./staticAnalysisCommon')
+import { default as algorithm } from './algorithmCategories'
+
+export class assignAndCompare {
+  warningNodes: any = []
+  name = 'Result not used: '
+  description = 'The result of an operation was not used.'
+  category = MISC
+  algorithm = algorithm.EXACT
+  Module = this
+
+  visit (node) {
+    if (common.isSubScopeWithTopLevelUnAssignedBinOp(node)) common.getUnAssignedTopLevelBinOps(node).forEach((n) => this.warningNodes.push(n))
+  }
+
+  report (compilationResults) {
+    return this.warningNodes.map((item, i) => {
+      return {
+        warning: 'A binary operation yields a value that is not used in the following. This is often caused by confusing assignment (=) and comparison (==).',
+        location: item.src
+      }
+    })
+  }
+}
