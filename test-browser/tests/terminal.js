afbb3593b2d6e8769eb97e1d6a3ed734b0010927
ioedeveloper <daviddisu8@gmail.com>
2020-02-10 15:11:55 +0000

    Added tests to terminal.js and created journalChildIncludes command to check if any child element of journal (console) contains a value.

--- test-browser/tests/terminal.js
+++ test-browser/tests/terminal.js
@@ -4,14 +4,56 @@ var sauce = require('./sauce')
 
 module.exports = {
   before: function (browser, done) {
-    init(browser, done)
+    init(browser, done, 'http://127.0.0.1:8080?plugins=solidity,udapp', false)
   },
-  'SimpleExecutionConsole': function (browser) {
+
+  'Should execution a simple console command': function (browser) {
     browser
     .waitForElementVisible('#terminalCli', 10000)
     .executeScript('1+1')
     .journalLastChild('2')
+  },
+
+  'Should clear console': function (browser) {
+    browser
+    .waitForElementVisible('#terminalCli')
+    .journalChildIncludes('Welcome to Remix')
+    .click('#clearConsole')
+    .assert.containsText('#journal', '')
+  },
+
+  'Should display auto-complete menu': function (browser) {
+    browser
+    .waitForElementVisible('#terminalCli')
+    .click('#terminalCli')
+    .keys('remix.')
+    .assert.visible('div[class^="autoCompleteItem"]')
+  },
+
+  'Should execute remix.help() command': function (browser) {
+    browser
+    .waitForElementVisible('#terminalCli')
+    .executeScript('remix.help()')
+    .journalChildIncludes('remix.call(message: {name, key, payload})')
+    .journalChildIncludes('remix.getFile(path)')
+    .journalChildIncludes('remix.debug(hash)')
+    .journalChildIncludes('remix.loadgist(id)')
+    .journalChildIncludes('remix.loadurl(url)')
+    .journalChildIncludes('remix.setproviderurl(url)')
+    .journalChildIncludes('remix.execute(filepath)')
+    .journalChildIncludes('remix.exeCurrent()')
+    .journalChildIncludes('remix.help()')
+    .journalChildIncludes('remix.debugHelp()')
+  },
+
+  'Should execute remix.debugHelp() command': function (browser) {
+    browser
+    .waitForElementVisible('#terminalCli')
+    .executeScript('remix.debugHelp()')
+    .journalChildIncludes('Here are some examples of scripts that can be run (using remix.exeCurrent() or directly from the console)')
+    .journalChildIncludes('Please see https://www.npmjs.com/package/remix-debug for more informations')
     .end()
   },
+
   tearDown: sauce
 }
