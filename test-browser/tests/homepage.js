cc71c349e7b795bd35997cef5f0d09fd687d8e2f
ioedeveloper <daviddisu8@gmail.com>
2020-02-11 13:29:52 +0000

    Updated unique identifiers to follow naming convention

--- test-browser/tests/homepage.js
+++ test-browser/tests/homepage.js
@@ -7,24 +7,24 @@ module.exports = {
     init(browser, done, 'http://127.0.0.1:8080', false)
   },
   'Loads Icon\'s Panel': function (browser) {
-    browser.waitForElementVisible('div[data-id="icon-panel"]', 10000)
-    .waitForElementVisible('div[data-id="homeIcon"]')
+    browser.waitForElementVisible('div[data-id="remixIdeIconPanel"]', 10000)
+    .waitForElementVisible('div[data-id="remixIdeHomeIcon"]')
     .waitForElementVisible('div[plugin="fileExplorers"]')
     .waitForElementVisible('div[plugin="pluginManager"]')
     .waitForElementVisible('div[plugin="settings"]')
   },
 
   'Loads Side Panel': function (browser) {
-    browser.waitForElementVisible('div[data-id="side-panel"]')
-    .assert.containsText('h6[data-id="swapitTitle"]', 'FILE EXPLORERS')
-    .waitForElementVisible('div[data-id="fileExplorerInit"]')
+    browser.waitForElementVisible('div[data-id="remixIdeSidePanel"]')
+    .assert.containsText('h6[data-id="sidePanelFileExplorerTitle"]', 'FILE EXPLORERS')
+    .waitForElementVisible('div[data-id="sidePanelFileExplorerTreeView"]')
     .waitForElementVisible('li[key="browser/4_Ballot_test.sol"]')
   },
 
   'Loads Main View': function (browser) {
     browser.waitForElementVisible('div[data-id="mainPanelPluginsContainer"]')
-    .waitForElementVisible('div[data-id="homeContainer"]')
-    .waitForElementVisible('div[data-id="hpSections"]')
+    .waitForElementVisible('div[div[data-id="mainPanelHomeContainer"]')
+    .waitForElementVisible('div[data-id="homeContainerHpSections"]')
     .waitForElementVisible('div[data-id="terminalContainerDisplay"]')
   },
 
@@ -35,37 +35,37 @@ module.exports = {
   },
 
   'Toggles Side Panel': function (browser) {
-    browser.waitForElementVisible('div[data-id="side-panel"]')
-    .assert.containsText('h6[data-id="swapitTitle"]', 'FILE EXPLORERS')
+    browser.waitForElementVisible('div[data-id="remixIdeSidePanel"]')
+    .assert.containsText('h6[data-id="sidePanelFileExplorerTitle"]', 'FILE EXPLORERS')
     .clickLaunchIcon('fileExplorers')
-    .assert.hidden('div[data-id="side-panel"]')
+    .assert.hidden('div[data-id="remixIdeSidePanel"]')
     .clickLaunchIcon('fileExplorers')
-    .assert.visible('div[data-id="side-panel"]')
-    .assert.containsText('h6[data-id="swapitTitle"]', 'FILE EXPLORERS')
+    .assert.visible('div[data-id="remixIdeSidePanel"]')
+    .assert.containsText('h6[data-id="sidePanelFileExplorerTitle"]', 'FILE EXPLORERS')
   },
 
   'Toggles Terminal': function (browser) {
-    browser.waitForElementVisible('div[data-id="terminal_container"]')
+    browser.waitForElementVisible('div[data-id="terminalContainer"]')
     .assert.visible('div[data-id="terminalContainerDisplay"]')
-    .click('i[data-id="toggleTerminal"]')
-    .checkElementStyle('div[data-id="toggleTerminalMenu"]', 'height', '35px')
-    .click('i[data-id="toggleTerminal"]')
+    .click('i[data-id="homeContainerTerminalToggle"]')
+    .checkElementStyle('div[data-id="homeContainerTerminalToggleMenu"]', 'height', '35px')
+    .click('i[data-id="homeContainerTerminalToggle"]')
     .assert.visible('div[data-id="terminalContainerDisplay"]')
   },
 
   'Toggles File Explorer Browser': function (browser) {
     browser
-    .waitForElementVisible('div[data-id="fileExplorerInit"]')
+    .waitForElementVisible('div[data-id="sidePanelFileExplorerTreeView"]')
     .assert.visible('ul[key="browser"]')
-    .click('div[data-id="toggle_browser"]')
+    .click('div[data-id="fileExplorerTreeTogglebrowser"]')
     .assert.hidden('ul[key="browser"]')
-    .click('div[data-id="toggle_browser"]')
+    .click('div[data-id="fileExplorerTreeTogglebrowser"]')
     .assert.visible('ul[key="browser"]')
   },
 
   'Switch Tabs using tabs icon': function (browser) {
     browser
-    .waitForElementVisible('div[data-id="fileExplorerInit"]')
+    .waitForElementVisible('div[data-id="sidePanelFileExplorerTreeView"]')
     .switchFile('browser/3_Ballot.sol')
     .assert.containsText('div[title="browser/3_Ballot.sol"]', '3_Ballot.sol')
     .click('span[class^=dropdownCaret]')
