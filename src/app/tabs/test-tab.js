0222afcd2539cc7ef4c84ef25149deaebd287f7f
yann300 <yann.levreau@gmail.com>
2020-02-18 13:12:13 +0100

    fix isActive

--- src/app/tabs/test-tab.js
+++ src/app/tabs/test-tab.js
@@ -225,9 +225,9 @@ module.exports = class TestTab extends ViewPlugin {
     return this.generateFileActionElement
   }
 
-  updateRunAction (currentFile) {
+  async updateRunAction (currentFile) {
     let el = yo`<button id="runTestsTabRunAction" class="${css.runButton} btn btn-primary"  onclick="${this.runTests.bind(this)}">Run Tests</button>`
-    const isSolidityActive = this.appManager.isActive('solidity')
+    const isSolidityActive = await this.appManager.isActive('solidity')
     if (!currentFile || !isSolidityActive) {
       el.setAttribute('disabled', 'disabled')
       if (!currentFile) el.setAttribute('title', 'No file selected')
