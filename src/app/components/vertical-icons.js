367da530e5435c95c46f1a4ea6c0683bb1b72c1e
Liana Husikyan <liana@ethdev.com>
2020-02-26 13:51:05 +0100

    uppercase for first letter of Displayed name

--- src/app/components/vertical-icons.js
+++ src/app/components/vertical-icons.js
@@ -66,6 +66,7 @@ export class VerticalIcons extends Plugin {
    */
   addIcon ({kind, name, icon, displayName, tooltip}) {
     let title = (tooltip || displayName || name)
+    title = title.replace(/^\w/, c => c.toUpperCase());
     this.icons[name] = yo`
       <div
         class="${css.icon}"
