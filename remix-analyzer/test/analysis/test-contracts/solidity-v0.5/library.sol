9fe6fac1375596095ddb6994e83d4d5f9fc4989c
aniket-engg <aniketengg.225@gmail.com>
2020-03-05 19:46:26 +0530

    common unit tests updated

--- remix-analyzer/test/analysis/test-contracts/solidity-v0.5/library.sol
+++ remix-analyzer/test/analysis/test-contracts/solidity-v0.5/library.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.4.9 <0.7.0;
 
-contract Set {
+library Set {
   // We define a new struct datatype that will be used to
   // hold its data in the calling contract.
-//   struct Data { uint flags; }
+  struct Data { mapping(uint => bool) flags; }
 
   // Note that the first parameter is of type "storage
   // reference" and thus only its storage address and not
@@ -11,35 +11,35 @@ contract Set {
   // special feature of library functions.  It is idiomatic
   // to call the first parameter 'self', if the function can
   // be seen as a method of that object.
-  //function insert(Data memory self, uint value) public
-  //    returns (bool)
-  //{
-//       if (self.flags[value])
-//           return false; // already there
-//         self.flags[value] = true;
-
-//       return true;
-//   }
-
-//   function remove(Data memory self, uint value) public
-//       returns (bool)
-//   {
-//       if (!self.flags[value])
-//           return false; // not there
-//       self.flags[value] = false;
-//       return true;
-//   }
-
-  function contains(uint value) public pure
-      returns (uint)
+  function insert(Data storage self, uint value) public
+      returns (bool)
   {
-      return value;
+      if (self.flags[value])
+          return false; // already there
+        self.flags[value] = true;
+
+      return true;
+  }
+
+  function remove(Data storage self, uint value) public
+      returns (bool)
+  {
+      if (!self.flags[value])
+          return false; // not there
+      self.flags[value] = false;
+      return true;
+  }
+
+  function contains(Data storage self, uint value) public
+      returns (bool)
+  {
+      return self.flags[value];
   }
 }
 
 
 contract C {
-    Set x;
+    Set.Data knownValues;
 
     function register(uint value) public {
         // The library functions can be called without a
@@ -47,18 +47,8 @@ contract C {
         // "instance" will be the current contract.
         address payable a;
         a.send(10 wei);
-        //if (!Set.insert(knownValues, value))
-         //   revert();
-    }
-
-    function tests2() public {
-        x = Set(0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c);
-        uint y = x.contains(103);
-        if(y == 103){
-            y++;
-        } else {
-            y--;
-        }
+        if (!Set.insert(knownValues, value))
+            revert();
     }
     // In this contract, we can also directly access knownValues.flags, if we want.
 }
