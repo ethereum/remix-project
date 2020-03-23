eb77463019216d8bcf629f2d15e20fede7126b0b
aniket-engg <aniketengg.225@gmail.com>
2020-03-23 13:45:04 +0530

    more type defs added in tests

--- remix-analyzer/test/analysis/staticAnalysisIssues-test-0.5.0.ts
+++ remix-analyzer/test/analysis/staticAnalysisIssues-test-0.5.0.ts
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 import { default as StatRunner } from '../../dist/src/solidity-analyzer'
 import { CompilationResult, AnalysisReportObj, AnalysisReport } from '../../src/types'
+import { checksEffectsInteraction } from '../../src/solidity-analyzer/modules/'
 import { install, require as requireNPMmodule } from 'npm-install-version'
 install('solc@0.5.0')
 const compiler = requireNPMmodule('solc@0.5.0')
@@ -19,7 +20,7 @@ test('staticAnalysisIssues.functionParameterPassingError', function (t) {
   // https://github.com/ethereum/remix-ide/issues/889#issuecomment-351746474
   t.plan(2)
   const res: CompilationResult = compile('functionParameters.sol')
-  const Module = require('../../dist/src/solidity-analyzer/modules/checksEffectsInteraction').default
+  const Module: any = checksEffectsInteraction
   const statRunner: StatRunner = new StatRunner()
 
   t.doesNotThrow(() => {
