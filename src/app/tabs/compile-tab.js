4ed76c74347f58b2f5bf5f6dd211b0e7c6c568af
ioedeveloper <daviddisu8@gmail.com>
2020-04-03 13:36:27 +0000

    Updated publishToStorage changes after rebase

--- src/app/tabs/compile-tab.js
+++ src/app/tabs/compile-tab.js
@@ -312,52 +312,6 @@ class CompileTab extends ViewPlugin {
     this.selectedContract = contractName
   }
 
-  publish (storage) {
-    if (this.selectedContract) {
-      var contract = this.data.contractsDetails[this.selectedContract]
-
-      if (contract.metadata === undefined || contract.metadata.length === 0) {
-        modalDialogCustom.alert('This contract may be abstract, may not implement an abstract parent\'s methods completely or not invoke an inherited contract\'s constructor correctly.')
-      } else {
-        if (storage === 'swarm') {
-          publishOnSwarm(contract, this.fileManager, function (err, uploaded) {
-            if (err) {
-              try {
-                err = JSON.stringify(err)
-              } catch (e) {}
-              modalDialogCustom.alert(yo`<span>Failed to publish metadata file to swarm, please check the Swarm gateways is available ( swarm-gateways.net ).<br />
-              ${err}</span>`)
-            } else {
-              var result = yo`<div>${uploaded.map((value) => {
-                return yo`<div><b>${value.filename}</b> : <pre>${value.output.url}</pre></div>`
-              })}</div>`
-              modalDialogCustom.alert(`Published ${contract.name}'s Metadata`, yo`<span>Metadata of "${contract.name.toLowerCase()}" was published successfully.<br> <pre>${result}</pre> </span>`)
-            }
-          }, (item) => { // triggered each time there's a new verified publish (means hash correspond)
-            this.fileProvider.addExternal('swarm/' + item.hash, item.content)
-          })
-        } else {
-          publishOnIpfs(contract, this.fileManager, function (err, uploaded) {
-            if (err) {
-              try {
-                err = JSON.stringify(err)
-              } catch (e) {}
-              modalDialogCustom.alert(yo`<span>Failed to publish metadata file to ${storage}, please check the ${storage} gateways is available.<br />
-              ${err}</span>`)
-            } else {
-              var result = yo`<div>${uploaded.map((value) => {
-                return yo`<div><b>${value.filename}</b> : <pre>${value.output.url}</pre></div>`
-              })}</div>`
-              modalDialogCustom.alert(`Published ${contract.name}'s Metadata`, yo`<span>Metadata of "${contract.name.toLowerCase()}" was published successfully.<br> <pre>${result}</pre> </span>`)
-            }
-          }, (item) => { // triggered each time there's a new verified publish (means hash correspond)
-            this.fileProvider.addExternal('ipfs/' + item.hash, item.content)
-          })
-        }
-      }
-    }
-  }
-
   details () {
     const help = {
       'Assembly': 'Assembly opcodes describing the contract including corresponding solidity source code',
