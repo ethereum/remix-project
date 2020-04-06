b69b752ce7f2258fd31a0ddf7dbce522d5e36327
ioedeveloper <daviddisu8@gmail.com>
2020-04-06 16:49:19 +0000

    Connect to Ropsten Test Network

--- src/app.js
+++ src/app.js
@@ -222,8 +222,9 @@ Please make a backup of your contracts and start using http://remix.ethereum.org
     toolTip('You are using an `https` connection. Please switch to `http` if you are using Remix against an `http Web3 provider` or allow Mixed Content in your browser.')
   }
 
+  const hosts = ['127.0.0.1:8080', '192.168.0.101:8080', 'localhost:8080']
   // workaround for Electron support
-  if (!isElectron()) {
+  if (!isElectron() && !hosts.includes(window.location.host)) {
     // Oops! Accidentally trigger refresh or bookmark.
     window.onbeforeunload = function () {
       return 'Are you sure you want to leave?'
