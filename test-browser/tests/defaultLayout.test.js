d53cac7265a56db07737aaa9facdb11f085dd4c5
ioedeveloper <daviddisu8@gmail.com>
2020-04-20 16:34:14 +0000

    Fixed broken writeFile function

--- test-browser/tests/defaultLayout.test.js
+++ test-browser/tests/defaultLayout.test.js
@@ -18,6 +18,7 @@ module.exports = {
     browser.waitForElementVisible('div[data-id="remixIdeSidePanel"]')
     .assert.containsText('h6[data-id="sidePanelSwapitTitle"]', 'FILE EXPLORERS')
     .waitForElementVisible('div[data-id="filePanelFileExplorerTree"]')
+    .pause(1000000)
     .waitForElementVisible('li[key="browser/4_Ballot_test.sol"]')
   },
 
