590892d0b403c27e59ff6ec1a60d71f69ef31ee0
yann300 <yann.levreau@gmail.com>
2020-04-28 13:31:52 +0200

    fix e2e tests

--- test-browser/tests/ballot.js
+++ test-browser/tests/ballot.js
@@ -38,7 +38,7 @@ module.exports = {
     browser.pause(500)
     .click('*[data-id="txLoggerDebugButton0x41fab8ea5b1d9fba5e0a6545ca1a2d62fff518578802c033c2b9a031a01c31b3"]')
     .pause(2000)
-    .clickLaunchIcon('debugger')
+    // .clickLaunchIcon('debugger')
     .click('*[data-id="buttonNavigatorJumpPreviousBreakpoint"]')
     .pause(2000)
     .goToVMTraceStep(79)
