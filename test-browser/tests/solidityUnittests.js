1344395d9ed17c852c6ceee892e4d318cf869d75
LianaHus <liana@ethdev.com>
2020-04-29 13:02:48 +0200

    increased test waiting time

--- test-browser/tests/solidityUnittests.js
+++ test-browser/tests/solidityUnittests.js
@@ -152,7 +152,7 @@ function runTests (browser) {
     .pause(500)
     .scrollAndClick('#runTestsTabRunAction')
     .waitForElementPresent('#solidityUnittestsOutput div[class^="testPass"]')
-    .pause(15000)
+    .pause(20000)
     .assert.containsText('#solidityUnittestsOutput', 'browser/4_Ballot_test.sol')
     .assert.containsText('#solidityUnittestsOutput', '✓ Check winning proposal')
     .assert.containsText('#solidityUnittestsOutput', '✓ Check winnin proposal with return value')
