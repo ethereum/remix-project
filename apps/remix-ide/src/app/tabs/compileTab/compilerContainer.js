df500a0f8ddfc74246e2f2a52f27221a7bf0ece8
LianaHus <liana@ethdev.com>
2020-05-07 20:04:49 +0200

    load wasm builds

--- apps/remix-ide/src/app/tabs/compileTab/compilerContainer.js
+++ apps/remix-ide/src/app/tabs/compileTab/compilerContainer.js
@@ -389,7 +389,7 @@ class CompilerContainer {
     // Workers cannot load js on "file:"-URLs and we get a
     // "Uncaught RangeError: Maximum call stack size exceeded" error on Chromium,
     // resort to non-worker version in that case.
-    if (canUseWorker(this.data.selectedVersion)) {
+    if (this.data.selectedVersion !== 'builtin' && canUseWorker(this.data.selectedVersion)) {
       this.compileTabLogic.compiler.loadVersion(true, url)
       this.setVersionText('(loading using worker)')
     } else {
