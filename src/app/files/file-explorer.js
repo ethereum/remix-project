4fad27d55a765ce256a391cb050c513f45561f0e
ioedeveloper <daviddisu8@gmail.com>
2020-05-13 00:58:58 +0000

    Removed unnecessary comments

--- src/app/files/file-explorer.js
+++ src/app/files/file-explorer.js
@@ -288,7 +288,6 @@ function fileExplorer (localRegistry, files, menuItems) {
               tooltip(`failed to remove file ${key}.`)
             } else {
               const { type } = fileManager.currentFileProvider()
-              console.log('file deleted!')
               self.updatePath(type)
             }
           },
@@ -399,8 +398,6 @@ function fileExplorer (localRegistry, files, menuItems) {
 
 fileExplorer.prototype.updatePath = function (path) {
   this.files.resolveDirectory(path, (error, fileTree) => {
-    console.log('path: ', path)
-    console.log('fileTree: ', fileTree)
     if (error) console.error(error)
     if (!fileTree) return
     var newTree = normalize(path, fileTree)
