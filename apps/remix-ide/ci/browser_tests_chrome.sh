71cd3d300cee1e5d07fe022a5e4eaa538891171b
ioedeveloper <daviddisu8@gmail.com>
2020-05-17 21:17:09 +0000

    Debug ci

--- apps/remix-ide/ci/browser_tests_chrome.sh
+++ apps/remix-ide/ci/browser_tests_chrome.sh
@@ -22,7 +22,7 @@ setupRemixd
 sleep 5
 
 TESTFILES=$(circleci tests glob "./apps/remix-ide/test-browser/tests/**/*.test.js" | circleci tests split --split-by=timings)
-echo TESTFILES
+echo "$TESTFILES"
 npm run nightwatch_local_chrome $TESTFILES
 
 echo "$TEST_EXITCODE"
