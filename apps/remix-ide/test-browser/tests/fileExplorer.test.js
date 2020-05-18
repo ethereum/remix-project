a65a0eb629c19c6f337ec2173a881a23d9dc642d
ioedeveloper <daviddisu8@gmail.com>
2020-05-18 17:38:48 +0000

    Modified fileExplorer test to make use of custom rename

--- apps/remix-ide/test-browser/tests/fileExplorer.test.js
+++ apps/remix-ide/test-browser/tests/fileExplorer.test.js
@@ -29,10 +29,7 @@ module.exports = {
   'Should rename `5_New_contract.sol` to 5_Renamed_Contract.sol': function (browser) {
     browser
     .waitForElementVisible('*[data-id="treeViewLibrowser/5_New_contract.sol"]')
-    .rightClick('[data-path="browser/5_New_contract.sol"]')
-    .click('*[id="menuitemrename"]')
-    .keys('5_Renamed_Contract.sol')
-    .keys(browser.Keys.ENTER)
+    .renameFile('browser/5_New_contract.sol', '5_Renamed_Contract.sol', 'browser/5_Renamed_Contract.sol')
     .waitForElementVisible('*[data-id="treeViewLibrowser/5_Renamed_Contract.sol"]')
   },
 
