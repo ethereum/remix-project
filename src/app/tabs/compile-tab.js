5d7b9bfea58d29674c985cc230714d5b1e7174d8
LianaHus <liana@ethdev.com>
2020-06-02 12:28:12 +0200

    added result indicator for compilation with compiler version

--- src/app/tabs/compile-tab.js
+++ src/app/tabs/compile-tab.js
@@ -116,6 +116,7 @@ class CompileTab extends ViewPlugin {
     this.fileManager.events.on('noFileSelected', this.data.eventHandlers.onNoFileSelected)
 
     this.data.eventHandlers.onCompilationFinished = (success, data, source) => {
+      this._view.errorContainer.appendChild(yo`<span data-id="compilationFinishedWith_${this.getCurrentVersion()}"></span>`)
       if (success) {
         // forwarding the event to the appManager infra
         this.emit('compilationFinished', source.target, source, 'soljson', data)
