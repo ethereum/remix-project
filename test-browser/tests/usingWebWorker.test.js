eb585cc0964733a7e7af795274bee4205a6cff09
LianaHus <liana@ethdev.com>
2020-06-02 10:07:22 +0200

    test improved

--- test-browser/tests/usingWebWorker.test.js
+++ test-browser/tests/usingWebWorker.test.js
@@ -17,12 +17,20 @@ module.exports = {
   'Using Web Worker': function (browser) {
     browser
     .waitForElementVisible('*[data-id="remixIdeIconPanel"]', 10000)
-    .addFile('basic.sol', sources[0]['browser/basic.sol'])
+    .clickLaunchIcon('fileExplorers')
+    .openFile('browser/basic.sol')
+    .clickLaunchIcon('solidity')
+    .execute(() => {
+      document.getElementById('nightlies').checked = true
+    })
     .noWorkerErrorFor('soljson-v0.3.4+commit.7dab890.js')
     .noWorkerErrorFor('soljson-v0.6.5+commit.f956cc89.js')
     .noWorkerErrorFor('soljson-v0.6.8-nightly.2020.5.14+commit.a6d0067b.js')
     .noWorkerErrorFor('soljson-v0.6.0-nightly.2019.12.17+commit.d13438ee.js')
     .noWorkerErrorFor('soljson-v0.4.26+commit.4563c3fc.js')
+    .execute(() => {
+      document.getElementById('nightlies').checked = false
+    })
   },
 
   tearDown: sauce
