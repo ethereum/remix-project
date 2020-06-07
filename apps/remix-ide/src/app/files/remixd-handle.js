4e51d00d05e36b57d3a35ae5adacac3b1584d5d2
ioedeveloper <daviddisu8@gmail.com>
2020-06-07 15:14:58 +0000

    Stabilize Websocket Plugin Changes

--- apps/remix-ide/src/app/files/remixd-handle.js
+++ apps/remix-ide/src/app/files/remixd-handle.js
@@ -1,5 +1,5 @@
 import isElectron from 'is-electron'
-import { Plugin } from '@remixproject/engine'
+import { WebsocketPlugin } from '@remixproject/engine'
 import * as packageJson from '../../../package.json'
 var yo = require('yo-yo')
 var modalDialog = require('../ui/modaldialog')
@@ -20,19 +20,18 @@ var css = csjs`
 
 const profile = {
   name: 'remixd',
-  methods: [],
-  events: [],
   description: 'Using Remixd daemon, allow to access file system',
   kind: 'other',
   version: packageJson.version
 }
 
-export class RemixdHandle extends Plugin {
+export class RemixdHandle extends WebsocketPlugin {
   constructor (fileSystemExplorer, locahostProvider, appManager) {
     super(profile)
     this.fileSystemExplorer = fileSystemExplorer
     this.locahostProvider = locahostProvider
     this.appManager = appManager
+    this.methods = ['get']
   }
 
   deactivate () {
