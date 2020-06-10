c3981ec269353594734851e5f60b051048828226
yann300 <yann.levreau@gmail.com>
2020-06-10 17:11:13 +0200

    publish only built files

--- ci/build_and_publish_docker_images.sh
+++ ci/build_and_publish_docker_images.sh
@@ -4,6 +4,9 @@ set -e
 # If not staging and master branch are existing
 export TAG="$CIRCLE_BRANCH"
 
+mkdir temp_publish_docker
+cp -r $FILES_TO_PACKAGE temp_publish_docker
+
 docker login --username $DOCKER_USER --password $DOCKER_PASS
 docker-compose build
 docker push remixproject/remix-ide:$TAG
