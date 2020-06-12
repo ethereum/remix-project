fce2d132a09bc6faaa6695e05dc5ecf887502766
Edi <edi.sinovcic@gmail.com>
2020-06-12 14:11:43 +0200

    Optimizing docker build

--- ci/build_and_publish_docker_images.sh
+++ ci/build_and_publish_docker_images.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-# If not staging and master branch are existing
 export TAG="$CIRCLE_BRANCH"
 
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+    export TAG="latest";
+fi
+
+rm -rf temp_publish_docker
 mkdir temp_publish_docker
 cp -r $FILES_TO_PACKAGE temp_publish_docker
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-docker-compose build
+docker-compose -f docker-compose.yaml -f build.yaml build
 docker push remixproject/remix-ide:$TAG
