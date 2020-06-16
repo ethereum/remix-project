226a8e43a8cc99bcd57bd78422e3d3a5b23292d2
ioedeveloper <daviddisu8@gmail.com>
2020-06-16 10:27:35 +0100

    done callback

--- apps/remix-ide/test-browser/commands/clickLaunchIcon.js
+++ apps/remix-ide/test-browser/commands/clickLaunchIcon.js
@@ -3,8 +3,8 @@ const EventEmitter = require('events')
 class ClickLaunchIcon extends EventEmitter {
   command (icon) {
     this.api.waitForElementVisible('#icon-panel div[plugin="' + icon + '"]').click('#icon-panel div[plugin="' + icon + '"]').perform((done) => {
-      this.emit('complete')
       done()
+      this.emit('complete')
     })
     return this
   }
