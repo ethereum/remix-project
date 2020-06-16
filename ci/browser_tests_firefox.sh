e94d198f427047fb1a38eb7a219d59fc82ae16e8
ioedeveloper <daviddisu8@gmail.com>
2020-06-16 17:12:21 +0100

    Set test exit code

--- ci/browser_tests_firefox.sh
+++ ci/browser_tests_firefox.sh
@@ -4,11 +4,11 @@ set -e
 
 setupRemixd () {
   mkdir remixdSharedfolder
-  cd apps/remix-ide/contracts
+  cd contracts
   echo 'sharing folder: '
   echo $PWD
-  ../../../node_modules/.bin/remixd -s $PWD --remix-ide http://127.0.0.1:4200 &
-  cd ../../..
+  ./../node_modules/remixd/bin/remixd -s $PWD --remix-ide http://127.0.0.1:8080 &
+  cd ..
 }
 
 BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
@@ -23,7 +23,7 @@ sleep 5
 
 TESTFILES=$(circleci tests glob "./test-browser/tests/**/*.test.js" | circleci tests split --split-by=timings)
 for TESTFILE in $TESTFILES; do
-    ./node_modules/.bin/nightwatch --config nightwatch.js --env chrome $TESTFILE || TEST_EXITCODE=1
+    ./node_modules/.bin/nightwatch --config nightwatch.js --env firefox $TESTFILE || TEST_EXITCODE=1
 done
 
 echo "$TEST_EXITCODE"
