73dcc7ac42206106e9051328b6b9e14c01d89c10
aniket-engg <aniketengg.225@gmail.com>
2020-07-03 17:30:28 +0530

    test working for remix-debug

--- libs/remix-debug/test/decoder/localsTests/structArray.js
+++ libs/remix-debug/test/decoder/localsTests/structArray.js
@@ -31,7 +31,7 @@ module.exports = function (st, vm, privateKey, contractBytecode, compilationResu
             st.fail(error)
           })
           callTree.event.register('callTreeReady', (scopes, scopeStarts) => {
-            helper.decodeLocals(st, 1587, traceManager, callTree, function (locals) {
+            helper.decodeLocals(st, 1622, traceManager, callTree, function (locals) {
               try {
                 st.equals(locals['bytesSimple'].length, '0x14')
                 st.equals(locals['bytesSimple'].value, '0x746573745f7375706572')
