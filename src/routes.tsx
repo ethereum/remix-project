5be8c1127717f38e6686ed9961cac625acff87e1
Solid Studio <solidstudio@labchain.io>
2020-07-04 09:44:41 +0100

    prettier

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,36 +1,31 @@
-import React from "react"
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    RouteProps,
-} from "react-router-dom"
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  RouteProps,
+} from "react-router-dom";
 
-import { ErrorView, HomeView } from "./views"
+import { ErrorView, HomeView } from "./views";
 
 interface Props extends RouteProps {
-    component: any // TODO: new (props: any) => React.Component
-    from: string
+  component: any; // TODO: new (props: any) => React.Component
+  from: string;
 }
 
 const CustomRoute = ({ component: Component, ...rest }: Props) => {
-    return (
-        <Route
-            {...rest}
-            render={(matchProps) => (
-                <Component {...matchProps} />
-            )}
-        />
-    )
-}
+  return (
+    <Route {...rest} render={(matchProps) => <Component {...matchProps} />} />
+  );
+};
 
 export const Routes = () => (
-    <Router>
-        <Switch>
-            <CustomRoute exact path="/" component={HomeView} from="/" />
-            <Route path="/error">
-                <ErrorView />
-            </Route>
-        </Switch>
-    </Router>
-)
+  <Router>
+    <Switch>
+      <CustomRoute exact path="/" component={HomeView} from="/" />
+      <Route path="/error">
+        <ErrorView />
+      </Route>
+    </Switch>
+  </Router>
+);
