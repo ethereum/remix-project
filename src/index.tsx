f1f054a7cd36c4c9aed1196cd546e601911a7d04
Solid Studio <solidstudio@labchain.io>
2020-07-04 09:58:14 +0100

    fix build issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Routes } from "./routes";
+// import { Routes } from "./routes";
 
 ReactDOM.render(
   <React.StrictMode>
