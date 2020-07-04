636358ab71e8a614b8eceb87edd04179603b80a0
Solid Studio <solidstudio@labchain.io>
2020-07-04 09:44:41 +0100

    prettier

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,93 +1,99 @@
-const open = require('open')
+// tslint:disable-next-line
+const open = require("open");
 
-import { getContractDoc, mergeParametersWithDevdoc, getFunctionDocumentation, getContractDocumentation } from './utils';
-import { FunctionDescription } from '@remixproject/plugin';
-import { buildFakeArtifactWithComments, buildFakeABIParameter } from './faker'
+import {
+  getContractDoc,
+  mergeParametersWithDevdoc,
+  getFunctionDocumentation,
+  getContractDocumentation,
+} from "./utils";
+import { FunctionDescription } from "@remixproject/plugin";
+import { buildFakeArtifactWithComments, buildFakeABIParameter } from "./faker";
 
-jest.setTimeout(10000)
+jest.setTimeout(10000);
 
-describe('Publisher tests', () => {
+describe("Publisher tests", () => {
+  describe("getContractDocumentation", () => {
+    test("getContractDocumentation", () => {
+      const result = getContractDocumentation(buildFakeArtifactWithComments());
 
-    describe('getContractDocumentation', () => {
-        test('getContractDocumentation', () => {
-            const result = getContractDocumentation(buildFakeArtifactWithComments())
+      const result2 = {
+        methods: {
+          "age(uint256)": {
+            author: "Mary A. Botanist",
+            details:
+              "The Alexandr N. Tetearing algorithm could increase precision",
+            params: [Object],
+            return: "age in years, rounded up for partial years",
+          },
+        },
+        notice: "You can use this contract for only the most basic simulation",
+        author: "Larry A. Gardner",
+        details:
+          "All function calls are currently implemented without side effects",
+        title: "A simulator for trees",
+      };
 
-            const result2 = {
-                methods:
-                {
-                    'age(uint256)':
-                    {
-                        author: 'Mary A. Botanist',
-                        details:
-                            'The Alexandr N. Tetearing algorithm could increase precision',
-                        params: [Object],
-                        return: 'age in years, rounded up for partial years'
-                    }
-                },
-                notice:
-                    'You can use this contract for only the most basic simulation',
-                author: 'Larry A. Gardner',
-                details:
-                    'All function calls are currently implemented without side effects',
-                title: 'A simulator for trees'
-            }
+      expect(result).toBeDefined();
+    });
+  });
 
-            expect(result).toBeDefined()
-        })
-    })
+  describe("getContractDoc", () => {
+    test("getContractDoc", () => {
+      const template = getContractDoc(
+        "Fallout",
+        buildFakeArtifactWithComments()
+      );
 
-    describe('getContractDoc', () => {
-        test('getContractDoc', () => {
+      console.log("Template", template);
 
-            const template = getContractDoc("Fallout", buildFakeArtifactWithComments());
+      expect(template).toBeDefined();
+    });
+  });
 
-            console.log("Template", template)
+  describe("getFunctionDocumentation", () => {
+    test("getFunctionDocumentation", () => {
+      const abiItem: FunctionDescription = {
+        constant: false,
+        inputs: [],
+        name: "Fal1out",
+        outputs: [],
+        payable: true,
+        stateMutability: "payable",
+        type: "function",
+      };
 
-            expect(template).toBeDefined()
-        })
-    })
+      const result = getFunctionDocumentation(abiItem, {});
 
-    describe('getFunctionDocumentation', () => {
-        test('getFunctionDocumentation', () => {
-            const abiItem: FunctionDescription = {
-                constant: false,
-                inputs: [],
-                name: "Fal1out",
-                outputs: [],
-                payable: true,
-                stateMutability: "payable",
-                type: "function"
-            }
+      expect(result).toBeDefined();
+    });
+  });
 
-            const result = getFunctionDocumentation(abiItem, {})
+  describe("mergeParametersWithDevdoc", () => {
+    test("mergeParametersWithDevdoc", () => {
+      const abiParameters = [buildFakeABIParameter()];
+      const devParams = {};
+      const result = mergeParametersWithDevdoc(abiParameters, devParams);
 
-            expect(result).toBeDefined()
-        })
-    })
+      expect(result.length).toEqual(1);
+    });
 
-    describe('mergeParametersWithDevdoc', () => {
-        test('mergeParametersWithDevdoc', () => {
-            const abiParameters = [buildFakeABIParameter()]
-            const devParams = {}
-            const result = mergeParametersWithDevdoc(abiParameters, devParams)
+    test("mergeParametersWithDevdoc with documentation", () => {
+      const abiParameters = [buildFakeABIParameter()];
+      const devParams = {};
+      const result = mergeParametersWithDevdoc(abiParameters, devParams);
 
-            expect(result.length).toEqual(1)
-        })
+      expect(result.length).toEqual(1);
+    });
+  });
 
-        test('mergeParametersWithDevdoc with documentation', () => {
-            const abiParameters = [buildFakeABIParameter()]
-            const devParams = {}
-            const result = mergeParametersWithDevdoc(abiParameters, devParams)
-
-            expect(result.length).toEqual(1)
-        })
-    })
-
-
-    test.skip('html generation', async () => {
-        await open('https://ipfs.io/ipfs/QmPYQyWyTrUZt3tjiPsEnkRQxedChYUjgEk9zLQ36SfpyW', { app: ['google chrome', '--incognito'] });
-        // start server
-        // generate html
-        // server it
-    })
-})
+  test.skip("html generation", async () => {
+    await open(
+      "https://ipfs.io/ipfs/QmPYQyWyTrUZt3tjiPsEnkRQxedChYUjgEk9zLQ36SfpyW",
+      { app: ["google chrome", "--incognito"] }
+    );
+    // start server
+    // generate html
+    // server it
+  });
+});
