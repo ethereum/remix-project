15b13225ddb5df77f899653e5ff4ee6127486e51
Solid Studio <solidstudio@labchain.io>
2020-07-04 09:34:49 +0100

    first commit

---
+++ src/routes.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    RouteProps,
+} from "react-router-dom"
+
+import { ErrorView, HomeView } from "./views"
+
+interface Props extends RouteProps {
+    component: any // TODO: new (props: any) => React.Component
+    from: string
+}
+
+const CustomRoute = ({ component: Component, ...rest }: Props) => {
+    return (
+        <Route
+            {...rest}
+            render={(matchProps) => (
+                <Component {...matchProps} />
+            )}
+        />
+    )
+}
+
+export const Routes = () => (
+    <Router>
+        <Switch>
+            <CustomRoute exact path="/" component={HomeView} from="/" />
+            <Route path="/error">
+                <ErrorView />
+            </Route>
+        </Switch>
+    </Router>
+)
