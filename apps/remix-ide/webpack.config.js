41098480df5d61981c16b41305c57ca14a9bdb6e
ioedeveloper <daviddisu8@gmail.com>
2020-07-06 14:25:49 +0100

    Added babel loader

--- apps/remix-ide/webpack.config.js
+++ apps/remix-ide/webpack.config.js
@@ -2,9 +2,20 @@ const nxWebpack = require('@nrwl/react/plugins/webpack')
 
 module.exports = config => {
     const nxWebpackConfig = nxWebpack(config)
+    const { module, module: { rules } } = nxWebpackConfig
 
     return {
         ...nxWebpackConfig,
+        module: {
+            ...module,
+            rules: [
+                ...rules,
+                {
+                    test: /\.compiler\.js$/,
+                    use: { loader: 'worker-loader' }
+                }
+            ]
+        },
         node: {
             fs: 'empty',
             tls: 'empty',
