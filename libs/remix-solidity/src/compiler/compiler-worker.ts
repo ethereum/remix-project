cf7d86fa57a9578e84ee5b6a28449c5dc321ec54
ioedeveloper <daviddisu8@gmail.com>
2020-07-10 07:33:26 +0100

    Use remix-solidity build for remix-ide

--- libs/remix-solidity/src/compiler/compiler-worker.ts
+++ libs/remix-solidity/src/compiler/compiler-worker.ts
@@ -1,6 +1,6 @@
 'use strict'
 
-import solc from 'solc/wrapper'
+import * as solc from 'solc/wrapper'
 import { CompilerInput, MessageToWorker } from './types'
 let compileJSON: ((input: CompilerInput) => string) | null = (input) => { return '' }
 const missingInputs: string[] = []
