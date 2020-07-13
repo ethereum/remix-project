eb2f346873dddeec2dda9f2e7a6786aa627a8763
aniket-engg <aniketengg.225@gmail.com>
2020-07-13 14:00:43 +0530

    resolved conflicts

--- libs/remix-analyzer/src/solidity-analyzer/modules/noReturn.ts
+++ libs/remix-analyzer/src/solidity-analyzer/modules/noReturn.ts
@@ -21,13 +21,7 @@ export default class noReturn implements AnalyzerModule {
   )
 
   report: ReportFunction = this.abstractAst.build_report(this._report.bind(this))
-<<<<<<< HEAD:libs/remix-analyzer/src/solidity-analyzer/modules/noReturn.ts
   private _report (contracts: ContractHLAst[], multipleContractsWithSameName: boolean, version: string): ReportObj[] {
-=======
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  private _report (contracts: ContractHLAst[], multipleContractsWithSameName: boolean): ReportObj[] {
->>>>>>> 3ce30ccb299e439bf2f3c0dbb2b69d8476c73c43:remix-analyzer/src/solidity-analyzer/modules/noReturn.ts
     const warnings: ReportObj[] = []
 
     contracts.forEach((contract: ContractHLAst) => {
