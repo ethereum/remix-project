2e791e9954d25612dccbf4f289aa0ce80de060f5
ioedeveloper <daviddisu8@gmail.com>
2020-07-17 14:19:56 +0100

    Serve remix-ide on port 8080

--- apps/remix-ide/test-browser/tests/workspace.test.js
+++ apps/remix-ide/test-browser/tests/workspace.test.js
@@ -4,7 +4,7 @@ const sauce = require('./sauce')
 
 module.exports = {
   before: function (browser, done) {
-    init(browser, done, 'http://127.0.0.1:4200?plugins=solidity,udapp', false)
+    init(browser, done, 'http://127.0.0.1:8080?plugins=solidity,udapp', false)
   },
   'CheckSolidityActivatedAndUDapp': function (browser) {
     browser
