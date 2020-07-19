3dd2bac7e2cd04e31e007a034db6ca615305dbb2
Iuri Matias <iuri.matias@gmail.com>
2020-07-19 10:00:11 -0400

    refactor storageRange and initialMappingsLocation

--- libs/remix-debug/src/storage/storageViewer.js
+++ libs/remix-debug/src/storage/storageViewer.js
@@ -31,13 +31,9 @@ class StorageViewer {
     * @param {Function} - callback - contains a map: [hashedKey] = {key, hashedKey, value}
     */
   storageRange (callback) {
-    this.storageResolver.storageRange(this.context.tx, this.context.stepIndex, this.context.address, (error, storage) => {
-      if (error) {
-        callback(error)
-      } else {
-        callback(null, Object.assign({}, storage, this.storageChanges))
-      }
-    })
+    this.storageResolver.storageRange(this.context.tx, this.context.stepIndex, this.context.address).then((storage) => {
+      callback(null, Object.assign({}, storage, this.storageChanges))
+    }).catch(callback)
   }
 
   /**
@@ -76,15 +72,7 @@ class StorageViewer {
     */
   async initialMappingsLocation (corrections) {
     if (!this.initialMappingsLocationPromise) {
-      this.initialMappingsLocationPromise = new Promise((resolve, reject) => {
-        this.storageResolver.initialPreimagesMappings(this.context.tx, this.context.stepIndex, this.context.address, corrections, (error, initialMappingsLocation) => {
-          if (error) {
-            reject(error)
-          } else {
-            resolve(initialMappingsLocation)
-          }
-        })
-      })
+      this.initialMappingsLocationPromise = this.storageResolver.initialPreimagesMappings(this.context.tx, this.context.stepIndex, this.context.address, corrections)
     }
     return this.initialMappingsLocationPromise
   }
