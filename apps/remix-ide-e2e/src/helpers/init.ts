31dd550714e761a4b54ba17f4ab8115dfb9b9db7
ioedeveloper <daviddisu8@gmail.com>
2020-07-27 11:05:01 +0100

    Add custom commands types

---
+++ apps/remix-ide-e2e/src/helpers/init.ts
@@ -0,0 +1,38 @@
+import { NightwatchBrowser } from "nightwatch"
+
+require('dotenv').config()
+
+module.exports = function (browser: NightwatchBrowser, callback: CallableFunction, url: string, preloadPlugins = true) {
+  browser
+    .url(url || 'http://127.0.0.1:8080')
+    .pause(5000)
+    .switchBrowserTab(0)
+    .injectScript('test-browser/helpers/applytestmode.js', function () {
+      browser.fullscreenWindow(() => {
+        if (preloadPlugins) {
+          console.log('preloadPlugins: ', preloadPlugins)
+          initModules(browser, () => {
+            browser.clickLaunchIcon('solidity')
+            .waitForElementPresent('[for="autoCompile"]')
+            .click('[for="autoCompile"]')
+          })
+        }
+      })
+    })
+    .perform(() => {
+      callback()
+    })
+}
+
+function initModules (browser, callback) {
+  browser.pause(5000)
+    .click('[data-id="verticalIconsKindpluginManager"]')
+    .scrollAndClick('[data-id="pluginManagerComponentActivateButtonsolidityStaticAnalysis"]')
+    .scrollAndClick('[data-id="pluginManagerComponentActivateButtondebugger"]')
+    .scrollAndClick('[data-id="verticalIconsKindfileExplorers"]')
+    .clickLaunchIcon('settings')
+    .setValue('[data-id="settingsTabGistAccessToken"]', process.env.gist_token)
+    .click('[data-id="settingsTabSaveGistToken"]')
+    .click('[data-id="settingsTabThemeFlatly"]') // e2e tests were initially developed with Flatly. Some tests are failing with the default one (Dark), because the dark theme put uppercase everywhere.
+    .perform(() => { callback() })
+}
