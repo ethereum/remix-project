aa46b4eb7fe51a9ac883c767f7edaf47c841b58a
ioedeveloper <daviddisu8@gmail.com>
2020-07-31 17:43:04 +0100

    Added pluginManager tests

---
+++ apps/remix-ide-e2e/src/commands/getInstalledPlugins.ts
@@ -0,0 +1,28 @@
+import { NightwatchBrowser } from "nightwatch"
+import EventEmitter from "events"
+
+class GetInstalledPlugins extends EventEmitter {
+  command (this: NightwatchBrowser, cb: (plugins: string[]) => void): NightwatchBrowser {
+    const browser = this.api
+
+    browser.waitForElementPresent('[plugin]:not([plugin=""]')
+    .perform((done) => {
+      browser.execute(function() {
+        const pluginNames = []
+        const plugins = document.querySelectorAll('[plugin]:not([plugin=""]')
+
+        plugins.forEach(plugin => {
+          pluginNames.push(plugin.getAttribute('plugin'))
+        })
+        return pluginNames
+      }, [], (result) => {
+        done()
+        Array.isArray(result.value) && cb(result.value)
+        this.emit('complete')
+      })
+    })
+    return this
+  }
+}
+
+module.exports = GetInstalledPlugins
