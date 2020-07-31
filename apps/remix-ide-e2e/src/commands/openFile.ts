6c5a8da9a34052b2515a36651044ea9818bf1707
ioedeveloper <daviddisu8@gmail.com>
2020-07-31 10:33:55 +0100

    Added defaultLayout tests

---
+++ apps/remix-ide-e2e/src/commands/openFile.ts
@@ -0,0 +1,27 @@
+import { NightwatchBrowser } from 'nightwatch'
+import EventEmitter from "events"
+
+class OpenFile extends EventEmitter {
+  command (this: NightwatchBrowser, name: string) {
+    this.api.perform((done) => {
+      openFile(this.api, name, () => {
+        done()
+        this.emit('complete')
+      })
+    })
+    return this
+  }
+}
+
+// click on fileExplorer can toggle it. We go through settings to be sure FE is open
+function openFile (browser: NightwatchBrowser, name: string, done: VoidFunction) {
+  browser.clickLaunchIcon('settings').clickLaunchIcon('fileExplorers')
+      .waitForElementVisible('li[key="' + name + '"]')
+      .click('li[key="' + name + '"]')
+      .pause(2000)
+      .perform(() => {
+        done()
+      })
+}
+
+module.exports = OpenFile
