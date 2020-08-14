1665821ab17b81e1ea146fbee2cd799a8e30c889
aniket-engg <aniketengg.225@gmail.com>
2020-08-14 19:51:51 +0530

    ok and equal method tests separated

--- libs/remix-tests/tests/examples_0/assert_equal_test.sol
+++ libs/remix-tests/tests/examples_0/assert_equal_test.sol
@@ -1,14 +1,6 @@
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 
-contract AssertTest {
-
-    function okPassTest() public {
-        Assert.ok(true, "okPassTest fails");
-    }
-
-    function okFailTest() public {
-        Assert.ok(false, "okFailTest fails");
-    }
+contract AssertEqualTest {
 
     function equalUintPassTest() public {
         Assert.equal(uint(1), uint(1), "equalUintPassTest fails");
