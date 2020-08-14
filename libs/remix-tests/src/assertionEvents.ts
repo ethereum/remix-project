41b07ed602a3990412bb036385b8ec8b2f4346fd
aniket-engg <aniketengg.225@gmail.com>
2020-08-14 20:49:49 +0530

    assertion events moved to separate file

---
+++ libs/remix-tests/src/assertionEvents.ts
@@ -0,0 +1,12 @@
+const assertionEvents = [
+    {
+        name: 'AssertionEvent',
+        params: ['bool', 'string']
+    },
+    {
+        name: 'AssertionEventUint',
+        params: ['bool', 'string', 'uint256', 'uint256']
+    }
+]
+
+export default assertionEvents
