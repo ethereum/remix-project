5f21feb02b1f40a4a73a0bcf489eb10715c36efa
aniket-engg <aniketengg.225@gmail.com>
2020-08-20 17:59:25 +0530

    int uint for greater and lesser than

--- libs/remix-tests/tests/examples_0/assert_greaterThan_test.sol
+++ libs/remix-tests/tests/examples_0/assert_greaterThan_test.sol
@@ -25,4 +25,12 @@ contract AssertGreaterThanTest {
     function greaterThanUintIntFailTest() public {
         Assert.greaterThan(uint(1), int(2), "greaterThanUintIntFailTest fails");
     }
+
+    function greaterThanIntUintPassTest() public {
+        Assert.greaterThan(int(10), uint(2), "greaterThanIntUintPassTest passes");
+    }
+
+    function greaterThanIntUintFailTest() public {
+        Assert.greaterThan(int(100), uint(-100), "greaterThanIntUintFailTest fails");
+    }
 }
