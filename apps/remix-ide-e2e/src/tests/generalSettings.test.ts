095147864bcef55f8347140db819092fcb646950
ioedeveloper <daviddisu8@gmail.com>
2020-08-24 11:37:03 +0100

    Fixed generalSettings test

--- apps/remix-ide-e2e/src/tests/generalSettings.test.ts
+++ apps/remix-ide-e2e/src/tests/generalSettings.test.ts
@@ -19,7 +19,6 @@ module.exports = {
   'Should activate `generate contract metadata`': function (browser) {
     browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]', 5000)
     .waitForElementVisible('*[data-id="settingsTabGenerateContractMetadataLabel"]', 5000)
-    .click('*[data-id="settingsTabGenerateContractMetadataLabel"]')
     .click('*[data-id="verticalIconsFileExplorerIcons"]')
     .openFile('browser/3_Ballot.sol')
     .click('*[data-id="verticalIconsKindsolidity"]')
