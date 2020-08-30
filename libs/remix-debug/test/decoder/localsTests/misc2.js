32037978617cd4791bde30af110eb98fd57c4be1
Iuri Matias <iuri.matias@gmail.com>
2020-08-30 09:40:37 -0400

    remove unnecessary extra levels from localTests

--- libs/remix-debug/test/decoder/localsTests/misc2.js
+++ libs/remix-debug/test/decoder/localsTests/misc2.js
@@ -11,53 +11,51 @@ var TraceManager = require('../../../src/trace/traceManager')
 var CodeManager = require('../../../src/code/codeManager')
 
 module.exports = function (st, vm, privateKey, contractBytecode, compilationResult, cb) {
-  vmCall.sendTx(vm, {nonce: 0, privateKey: privateKey}, null, 0, contractBytecode, function (error, txHash) {
+  vmCall.sendTx(vm, { nonce: 0, privateKey: privateKey }, null, 0, contractBytecode, function (error, txHash) {
     if (error) {
-      st.fail(error)
-    } else {
-      vm.web3.eth.getTransaction(txHash, function (error, tx) {
-        if (error) {
-          st.fail(error)
-        } else {
-          tx.to = traceHelper.contractCreationToken('0')
-          var traceManager = new TraceManager({web3: vm.web3})
-          var codeManager = new CodeManager(traceManager)
-          codeManager.clear()
-          var solidityProxy = new SolidityProxy({traceManager, getCode: codeManager.getCode.bind(codeManager)})
-          solidityProxy.reset(compilationResult)
-          var debuggerEvent = new EventManager()
-          var callTree = new InternalCallTree(debuggerEvent, traceManager, solidityProxy, codeManager, { includeLocalVariables: true })
-          callTree.event.register('callTreeBuildFailed', (error) => {
-            st.fail(error)
-          })
-          callTree.event.register('callTreeReady', (scopes, scopeStarts) => {
-            helper.decodeLocals(st, 49, traceManager, callTree, function (locals) {
-              try {
-                st.equals(locals['dynbytes'].value, '0x64796e616d69636279746573')
-                st.equals(locals['smallstring'].value, 'test_test_test')
-                st.equals(Object.keys(locals).length, 2)
-              } catch (e) {
-                st.fail(e.message)
-              }
-            })
-
-            helper.decodeLocals(st, 7, traceManager, callTree, function (locals) {
-              try {
-                // st.equals(Object.keys(locals).length, 0)
-                st.equals(0, 0)
-              } catch (e) {
-                st.fail(e.message)
-              }
-              cb()
-            })
-          })
-          traceManager.resolveTrace(tx).then(() => {
-            debuggerEvent.trigger('newTraceLoaded', [traceManager.trace])
-          }).catch((error) => {
-            st.fail(error)
-          })
-        }
-      })
+      return st.fail(error)
     }
+    vm.web3.eth.getTransaction(txHash, function (error, tx) {
+      if (error) {
+        return st.fail(error)
+      }
+      tx.to = traceHelper.contractCreationToken('0')
+      var traceManager = new TraceManager({ web3: vm.web3 })
+      var codeManager = new CodeManager(traceManager)
+      codeManager.clear()
+      var solidityProxy = new SolidityProxy({ traceManager, getCode: codeManager.getCode.bind(codeManager) })
+      solidityProxy.reset(compilationResult)
+      var debuggerEvent = new EventManager()
+      var callTree = new InternalCallTree(debuggerEvent, traceManager, solidityProxy, codeManager, { includeLocalVariables: true })
+      callTree.event.register('callTreeBuildFailed', (error) => {
+        st.fail(error)
+      })
+      callTree.event.register('callTreeReady', (scopes, scopeStarts) => {
+        helper.decodeLocals(st, 49, traceManager, callTree, function (locals) {
+          try {
+            st.equals(locals['dynbytes'].value, '0x64796e616d69636279746573')
+            st.equals(locals['smallstring'].value, 'test_test_test')
+            st.equals(Object.keys(locals).length, 2)
+          } catch (e) {
+            st.fail(e.message)
+          }
+        })
+
+        helper.decodeLocals(st, 7, traceManager, callTree, function (locals) {
+          try {
+            // st.equals(Object.keys(locals).length, 0)
+            st.equals(0, 0)
+          } catch (e) {
+            st.fail(e.message)
+          }
+          cb()
+        })
+      })
+      traceManager.resolveTrace(tx).then(() => {
+        debuggerEvent.trigger('newTraceLoaded', [traceManager.trace])
+      }).catch((error) => {
+        st.fail(error)
+      })
+    })
   })
 }
