4b667faa877c1ffae9f618b168b8446500667c18
ioedeveloper <daviddisu8@gmail.com>
2020-09-01 15:25:41 +0100

    FullStoragesChanges component

---
+++ libs/debugger-ui/src/lib/vm-debugger/solidity-state.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DropdownPanel from './dropdown-panel'
+
+export const SolidityState = () => {
+    return (
+        <DropdownPanel dropdownName='Solidity State' opts={{ 
+            
+         }} />
+    )
+}
+
+export default SolidityState
