234869fc4f2ce1e96279610ea063698ec526f937
ioedeveloper <daviddisu8@gmail.com>
2020-09-01 16:07:33 +0100

    New folder structure for remix UI

---
+++ libs/remix-ui/tree-view/src/lib/remix-ui-tree-view.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './remix-ui-tree-view.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiTreeViewProps {}
+
+export const RemixUiTreeView = (props: RemixUiTreeViewProps) => {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-tree-view!</h1>
+    </div>
+  );
+};
+
+export default RemixUiTreeView;
