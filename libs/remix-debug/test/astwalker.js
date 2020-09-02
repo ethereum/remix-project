cdfadb254e952df0c83eeaf9e78e7a5f935c01ab
aniket-engg <aniketengg.225@gmail.com>
2020-09-02 20:47:48 +0530

    astwalker tests fixed

--- libs/remix-debug/test/astwalker.js
+++ libs/remix-debug/test/astwalker.js
@@ -8,11 +8,11 @@ tape('ASTWalker', function (t) {
     st.plan(24)
     const astwalker = new AstWalker()
 
-    astwalker.walk(node.ast.legacyAST, function (node) {
-      if (node.name === 'ContractDefinition') {
+    astwalker.walk(node.ast.ast, function (node) {
+      if (node.nodeType === 'ContractDefinition') {
         checkContract(st, node)
       }
-      if (node.name === 'FunctionDefinition') {
+      if (node.nodeType === 'FunctionDefinition') {
         checkSetFunction(st, node)
       }
       return true
@@ -20,42 +20,42 @@ tape('ASTWalker', function (t) {
 
     const callback = {}
     callback.FunctionDefinition = function (node) {
-      st.equal(node.name, 'FunctionDefinition')
-      st.equal(node.attributes.name === 'set' || node.attributes.name === 'get', true)
+      st.equal(node.nodeType, 'FunctionDefinition')
+      st.equal(node.name === 'set' || node.name === 'get', true)
       return true
     }
-    astwalker.walk(node.ast.legacyAST, callback)
+    astwalker.walk(node.ast.ast, callback)
   })
 })
 
 function checkContract (st, node) {
-  st.equal(node.attributes.name, 'test')
-  st.equal(node.children[0].attributes.name, 'x')
-  st.equal(node.children[0].attributes.type, 'int256')
-  st.equal(node.children[1].attributes.name, 'y')
-  st.equal(node.children[1].attributes.type, 'int256')
-  st.equal(node.children[2].name, 'FunctionDefinition')
-  st.equal(node.children[2].attributes.constant, false)
-  st.equal(node.children[2].attributes.name, 'set')
-  st.equal(node.children[2].attributes.public, true)
+  st.equal(node.name, 'test')
+  st.equal(node.nodes[0].name, 'x')
+  st.equal(node.nodes[0].typeDescriptions.typeString, 'int256')
+  st.equal(node.nodes[1].name, 'y')
+  st.equal(node.nodes[1].typeDescriptions.typeString, 'int256')
+  st.equal(node.nodes[2].nodeType, 'FunctionDefinition')
+  st.equal(node.nodes[2].stateMutability, 'nonpayable')
+  st.equal(node.nodes[2].name, 'set')
+  st.equal(node.nodes[2].visibility, 'public')
 }
 
 function checkSetFunction (st, node) {
-  if (node.attributes.name === 'set') {
-    st.equal(node.children[0].name, 'ParameterList')
-    st.equal(node.children[1].name, 'ParameterList')
-    st.equal(node.children[2].name, 'Block')
-    st.equal(node.children[2].children[1].name, 'ExpressionStatement')
-    checkExpressionStatement(st, node.children[2].children[0])
+  if (node.name === 'set') {
+    st.equal(node.parameters.nodeType, 'ParameterList')
+    st.equal(node.returnParameters.nodeType, 'ParameterList')
+    st.equal(node.body.nodeType, 'Block')
+    st.equal(node.body.statements[0].nodeType, 'ExpressionStatement')
+    checkExpressionStatement(st, node.body.statements[0])
   }
 }
 
 function checkExpressionStatement (st, node) {
-  st.equal(node.children[0].name, 'Assignment')
-  st.equal(node.children[0].attributes.operator, '=')
-  st.equal(node.children[0].attributes.type, 'int256')
-  st.equal(node.children[0].children[0].name, 'Identifier')
-  st.equal(node.children[0].children[0].attributes.value, 'x')
-  st.equal(node.children[0].children[1].name, 'Identifier')
-  st.equal(node.children[0].children[1].attributes.value, '_x')
+  st.equal(node.expression.nodeType, 'Assignment')
+  st.equal(node.expression.operator, '=')
+  st.equal(node.expression.typeDescriptions.typeString, 'int256')
+  st.equal(node.expression.leftHandSide.nodeType, 'Identifier')
+  st.equal(node.expression.leftHandSide.name, 'x')
+  st.equal(node.expression.rightHandSide.nodeType, 'Identifier')
+  st.equal(node.expression.rightHandSide.name, '_x')
 }
