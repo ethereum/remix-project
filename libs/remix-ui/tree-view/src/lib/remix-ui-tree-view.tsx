734d67ac0f6ec222b66e71a47abbe403b5ef67ef
ioedeveloper <daviddisu8@gmail.com>
2020-09-03 10:28:06 +0100

    Custom hook

--- libs/remix-ui/tree-view/src/lib/remix-ui-tree-view.tsx
+++ libs/remix-ui/tree-view/src/lib/remix-ui-tree-view.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import './remix-ui-tree-view.css';
+import './remix-ui-tree-view.css'
 
-/* eslint-disable-next-line */
-export interface RemixUiTreeViewProps {}
+export interface RemixUiTreeViewProps {
+  data: []
+}
 
 export const RemixUiTreeView = (props: RemixUiTreeViewProps) => {
   return (
-    <div>
-      <h1>Welcome to remix-ui-tree-view!</h1>
-    </div>
-  );
-};
+    <ul key="" data-id="treeViewUl" className="ul_tv ml-0 px-2">
+      <li key="vm trace step" data-id="treeViewLivm trace step" className="li_tv">
+        <div key="vm trace step" data-id="treeViewDivvm trace step" className="d-flex flex-row align-items-center">
+          <div className="px-1 fas fa-caret-right caret caret_tv_2nzIvs" style={{ visibility: "hidden" }}>
+          </div>
+            <span className="w-100">
+              <div className="d-flex mb-1 flex-row label_item">
+                <label className="small font-weight-bold pr-1 label_key">
+                  vm trace step:
+                </label>
+                <label className="m-0 label_value">-</label>
+              </div>
+            </span>
+          </div>
+        </li>
+        <li key="execution step" data-id="treeViewLiexecution step" className="li_tv">
+          <div key="execution step" data-id="treeViewDivexecution step" className="d-flex flex-row align-items-center">
+            <div className="px-1 fas fa-caret-right caret caret_tv" style={{ visibility: "hidden" }}>
+            </div>
+            <span className="w-100">
+              <div className="d-flex mb-1 flex-row label_item">
+                <label className="small font-weight-bold pr-1 label_key">
+                  execution step:
+                </label>
+                <label className="m-0 label_value">-</label>
+              </div>
+            </span>
+          </div>
+        </li>
+      </ul>)
+}
 
-export default RemixUiTreeView;
+export default RemixUiTreeView
