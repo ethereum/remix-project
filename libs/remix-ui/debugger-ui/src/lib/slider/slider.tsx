10556a73ef1b62da84ddfa2fede91ac61ea64b7c
ioedeveloper <daviddisu8@gmail.com>
2020-09-15 10:25:23 +0100

    Resolved bugs in step manager

--- libs/remix-ui/debugger-ui/src/lib/slider/slider.tsx
+++ libs/remix-ui/debugger-ui/src/lib/slider/slider.tsx
@@ -1,46 +1,26 @@
 import React, { useState, useEffect } from 'react'
 
-export const Slider = ({ stepManager, sliderLength, sliderValue }) => {
+export const Slider = ({ stepManager, sliderValue }) => {
   const [state, setState] = useState({
-    max: null,
-    disabled: true,
-    previousValue: null,
-    currentValue: null
+    currentValue: ''
   })
-
-  useEffect(() => {
-    setState(prevState => {
-      return {
-        ...prevState,
-        max: sliderLength - 1,
-        disabled: (sliderLength === 0)
-      }
-    })
-
-    setValue(0)
-  }, [sliderLength])
 
   useEffect(() => {
     setValue(sliderValue)
   }, [sliderValue])
 
   const setValue = (value) => {
-    setState({ ...state, currentValue: value })
+    if (value === state.currentValue) return
+    setState(prevState => {
+      return { ...prevState, currentValue: value }
+    })
+    stepManager && stepManager.jumpTo(value)
   }
 
   const handleChange = (e) => {
-    console.log('e.target: ', e.target)
     const value = parseInt(e.target.value)
 
-    if (value === state.previousValue) return
-    setState(prevState => {
-      return {
-        ...prevState,
-        previousValue: value,
-        currentValue: value
-      }
-    })
-    stepManager.jumpTo(value)
+    setValue(value)
   }
 
   return (
@@ -50,10 +30,10 @@ export const Slider = ({ stepManager, sliderLength, sliderValue }) => {
             className='w-100 my-0'
             type='range'
             min={0}
-            max={state.max}
-            defaultValue={state.currentValue}
+            max={stepManager ? stepManager.traceLength - 1 : 0}
+            value={state.currentValue}
             onChange={handleChange}
-            disabled={state.disabled}
+            disabled={stepManager ? stepManager.traceLength === 0 : true}
         />
     </div>
   )
