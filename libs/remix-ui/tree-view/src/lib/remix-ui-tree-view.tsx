dca937c96ced0812f7cb4e5e8beeb5507150a94c
ioedeveloper <daviddisu8@gmail.com>
2020-09-16 08:39:47 +0100

    update package-lock.json

--- libs/remix-ui/tree-view/src/lib/remix-ui-tree-view.tsx
+++ libs/remix-ui/tree-view/src/lib/remix-ui-tree-view.tsx
@@ -4,7 +4,7 @@ import { TreeViewProps } from '../types'
 import './remix-ui-tree-view.css'
 
 export const TreeView = (props: TreeViewProps) => {
-  const { children, key, ...otherProps } = props
+  const { children, key = '', ...otherProps } = props
   
   return (
     <ul data-id={`treeViewUl${key}`} className="ul_tv ml-0 px-2" { ...otherProps }>
