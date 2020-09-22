08eb1051031c908b698a0fb7757427ecb5cb18d3
yann300 <yann.levreau@gmail.com>
2020-09-22 11:52:50 +0200

    call plugin using URL params  after page load

--- apps/remix-ide-e2e/src/tests/workspace.test.ts
+++ apps/remix-ide-e2e/src/tests/workspace.test.ts
@@ -5,14 +5,22 @@ import sauce from './sauce'
 
 module.exports = {
   before: function (browser: NightwatchBrowser, done: VoidFunction) {
-    init(browser, done, 'http://127.0.0.1:8080?plugins=solidity,udapp', false)
+    init(browser, done, 'http://127.0.0.1:8080?plugins=solidity,udapp&plugincall=fileManager//open//3_Ballot.sol', false)
   },
 
   'CheckSolidityActivatedAndUDapp': function (browser: NightwatchBrowser) {
     browser
     .waitForElementVisible('#icon-panel', 10000)
     .clickLaunchIcon('solidity')
-    .clickLaunchIcon('udapp')
+    .clickLaunchIcon('udapp')    
+  },
+
+  'Editor should be focused on the 3_Ballot.sol': function (browser: NightwatchBrowser) {
+    browser
+    .pause(5000)
+    .getEditorValue((content) => {
+      browser.assert.ok(content.indexOf('contract Ballot {') != -1, 'content doesn\'t include Ballot contract')
+    })
     .end()
   },
   
