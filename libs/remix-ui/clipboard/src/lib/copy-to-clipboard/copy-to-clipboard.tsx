3f6a267f550ccb93c486c168e22e44ba78389722
ioedeveloper <daviddisu8@gmail.com>
2020-09-25 15:32:24 +0100

    Added tooltip for copy to clipboard

--- libs/remix-ui/clipboard/src/lib/copy-to-clipboard/copy-to-clipboard.tsx
+++ libs/remix-ui/clipboard/src/lib/copy-to-clipboard/copy-to-clipboard.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import copy from 'copy-text-to-clipboard'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 import './copy-to-clipboard.css'
 
-export const CopyToClipboard = ({ content, tip='Copy value to clipboard', icon='fa-copy', ...otherProps }) => {
+export const CopyToClipboard = ({ content, tip='Copy', icon='fa-copy', ...otherProps }) => {
+  const [message, setMessage] = useState(tip)
   const handleClick = () => {
-    if (content) {   // module `copy` keeps last copied thing in the memory, so don't show tooltip if nothing is copied, because nothing was added to memory
+    if (content && content !== '') {   // module `copy` keeps last copied thing in the memory, so don't show tooltip if nothing is copied, because nothing was added to memory
       try {
         if (typeof content !== 'string') {
           content = JSON.stringify(content, null, '\t')
@@ -14,21 +16,28 @@ export const CopyToClipboard = ({ content, tip='Copy value to clipboard', icon='
         console.error(e)
       }
       copy(content)
-      // addTooltip('Copied value to clipboard.')
+      setMessage('Copied')
     } else {
-      // addTooltip('Cannot copy empty content!')
+      setMessage('Cannot copy empty content!')
     }
   }
 
+  const reset = () => {
+    setTimeout(() => setMessage('Copy'), 500)
+  }
+
   return (
-    <i 
-      title={tip}
-      className={`copyIcon far ${icon} p-2`}
-      data-id="copyToClipboardCopyIcon"
-      aria-hidden="true"
-      {...otherProps}
-      onClick={handleClick}
-    ></i>
+    <a href="#" onClick={handleClick} onMouseLeave={reset}>
+      <OverlayTrigger placement="right" overlay={
+        <Tooltip id="overlay-tooltip">
+            { message }
+        </Tooltip>
+      }>
+        <i className={`far ${icon} ml-1 p-2`} data-id="copyToClipboardCopyIcon" aria-hidden="true"
+          {...otherProps}
+        ></i>
+      </OverlayTrigger>
+    </a>
   )
 }
 
