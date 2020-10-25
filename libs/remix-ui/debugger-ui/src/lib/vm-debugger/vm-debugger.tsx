544c665baddb1d688700ea2f892ecd5d9a6b1a6d
ioedeveloper <daviddisu8@gmail.com>
2020-10-25 17:14:22 +0100

    Include registerEvent for vmDebugger

--- libs/remix-ui/debugger-ui/src/lib/vm-debugger/vm-debugger.tsx
+++ libs/remix-ui/debugger-ui/src/lib/vm-debugger/vm-debugger.tsx
@@ -7,7 +7,7 @@ import StoragePanel from './storage-panel'
 import ReturnValuesPanel from './dropdown-panel'
 import FullStoragesChangesPanel from './full-storages-changes'
 
-export const VmDebugger = ({ vmDebuggerLogic, ready }) => {
+export const VmDebugger = ({ vmDebugger: { registerEvent } }) => {
   const [calldataPanel, setCalldataPanel] = useState(null)
   const [memoryPanel, setMemoryPanel] = useState(null)
   const [callStackPanel, setCallStackPanel] = useState(null)
@@ -20,39 +20,35 @@ export const VmDebugger = ({ vmDebuggerLogic, ready }) => {
   const [fullStoragesChangesPanel, setFullStoragesChangesPanel] = useState(null)
 
   useEffect(() => {
-    console.log('vmDebuggerLogic: ', vmDebuggerLogic)
-    if (vmDebuggerLogic) {
-      vmDebuggerLogic.event.register('traceManagerCallDataUpdate', (calldata) => {
-        setCalldataPanel(() => calldata)
+    registerEvent && registerEvent('traceManagerCallDataUpdate', (calldata) => {
+      setCalldataPanel(() => calldata)
+    })
+    registerEvent && registerEvent('traceManagerMemoryUpdate', (calldata) => {
+      setMemoryPanel(() => calldata)
+    })
+    registerEvent && registerEvent('traceManagerCallStackUpdate', (calldata) => {
+      setCallStackPanel(() => calldata)
+    })
+    registerEvent && registerEvent('traceManagerStackUpdate', (calldata) => {
+      setStackPanel(() => calldata)
+    })
+    registerEvent && registerEvent('traceManagerStorageUpdate', (calldata, header) => {
+      setStoragePanel(() => {
+        return { calldata, header }
       })
-      vmDebuggerLogic.event.register('traceManagerMemoryUpdate', (calldata) => {
-        setMemoryPanel(() => calldata)
+    })
+    registerEvent && registerEvent('traceReturnValueUpdate', (calldata) => {
+      setReturnValuesPanel(() => calldata)
+    })
+    registerEvent && registerEvent('traceAddressesUpdate', (calldata) => {
+      setFullStoragesChangesPanel(() => {
+        return {}
       })
-      vmDebuggerLogic.event.register('traceManagerCallStackUpdate', (calldata) => {
-        setCallStackPanel(() => calldata)
-      })
-      vmDebuggerLogic.event.register('traceManagerStackUpdate', (calldata) => {
-        setStackPanel(() => calldata)
-      })
-      vmDebuggerLogic.event.register('traceManagerStorageUpdate', (calldata, header) => {
-        setStoragePanel(() => {
-          return { calldata, header }
-        })
-      })
-      vmDebuggerLogic.event.register('traceReturnValueUpdate', (calldata) => {
-        setReturnValuesPanel(() => calldata)
-      })
-      vmDebuggerLogic.event.register('traceAddressesUpdate', (calldata) => {
-        setFullStoragesChangesPanel(() => {
-          return {}
-        })
-      })
-      vmDebuggerLogic.event.register('traceStorageUpdate', (calldata) => {
-        setFullStoragesChangesPanel(() => calldata)
-      })
-      ready()
-    }
-  }, [vmDebuggerLogic])
+    })
+    registerEvent && registerEvent('traceStorageUpdate', (calldata) => {
+      setFullStoragesChangesPanel(() => calldata)
+    })
+  }, [registerEvent])
 
   return (
     <div id="vmdebugger" className="px-2">
