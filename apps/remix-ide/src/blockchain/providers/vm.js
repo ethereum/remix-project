dbfe530f17ff7c1819254315d8e89dbee202104c
Iuri Matias <iuri.matias@gmail.com>
2020-10-26 15:41:47 -0400

    add call/send methods for other providers

--- apps/remix-ide/src/blockchain/providers/vm.js
+++ apps/remix-ide/src/blockchain/providers/vm.js
@@ -30,9 +30,6 @@ class VMProvider {
     return new Promise((resolve, reject) => {
       // TODO: should use selected account
       this.getAccounts((error, accounts) => {
-        console.dir("accounts")
-        console.dir(accounts)
-        console.dir(params)
         const contract = new this.web3.eth.Contract(abi, address, { from: accounts[0] })
         contract.methods[methodName].apply(contract.methods[methodName], params).send().then(resolve).catch(reject)
       })
@@ -43,8 +40,6 @@ class VMProvider {
     return new Promise((resolve, reject) => {
       // TODO: should use selected account
       this.getAccounts((error, accounts) => {
-        console.dir("accounts")
-        console.dir(accounts)
         const contract = new this.web3.eth.Contract(abi, address, { from: accounts[0] })
         contract.methods[methodName].apply(contract.methods[methodName], params).call().then(resolve).catch(reject)
       })
