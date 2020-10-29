1adf0164a9a7d7ab4f65b97ecb913fad59be0743
aniket-engg <aniketengg.225@gmail.com>
2020-10-29 13:44:14 +0530

    license added in tests libraries

--- libs/remix-tests/sol/tests_accounts.sol.ts
+++ libs/remix-tests/sol/tests_accounts.sol.ts
@@ -1,4 +1,7 @@
-module.exports = `
+module.exports = `// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.22 <0.8.0;
+
 library TestsAccounts {
     function getAccount(uint index) public returns (address) {
         >accounts<
