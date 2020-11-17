025443e00c3d86d76e857f2517256183ec69481b
yann300 <yann.levreau@gmail.com>
2020-11-17 16:16:50 +0100

    standatf

--- apps/remix-ide/src/remixEngine.js
+++ apps/remix-ide/src/remixEngine.js
@@ -1,18 +1,16 @@
-/* global localStorage, fetch */
 import { Engine } from '@remixproject/engine'
 import { EventEmitter } from 'events'
 
 export class RemixEngine extends Engine {
-
   constructor () {
     super()
     this.event = new EventEmitter()
   }
 
   setPluginOption ({ name, kind }) {
-    if (kind === 'provider') return {queueTimeout: 60000 * 2}
-    if (name === 'LearnEth') return {queueTimeout: 60000}
-    return {queueTimeout: 10000}
+    if (kind === 'provider') return { queueTimeout: 60000 * 2 }
+    if (name === 'LearnEth') return { queueTimeout: 60000 }
+    return { queueTimeout: 10000 }
   }
 
   onRegistration (plugin) {
