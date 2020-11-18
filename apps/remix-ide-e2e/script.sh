56b0db8dfa3ac4d7b76cc07e34d6a2f5c6a3762f
ioedeveloper <daviddisu8@gmail.com>
2020-11-18 12:15:21 +0100

    Run e2e tests from bash script

---
+++ apps/remix-ide-e2e/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEST_SCRIPT='node_modules/.bin/nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js';
+
+if [ {$1} != undefined ]
+then 
+    TEST_SCRIPT=${TEST_SCRIPT}' --env '$1;
+else 
+    TEST_SCRIPT=${TEST_SCRIPT}' --env chrome';
+fi
+
+if [ {$2} != undefined ]
+then
+    TEST_SCRIPT=${TEST_SCRIPT}' '$2;
+fi
+
+eval $TEST_SCRIPT;
