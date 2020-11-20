ddabfacc6f8fd776654577b9a5996de5de2a95e8
aniket-engg <aniketengg.225@gmail.com>
2020-11-21 00:16:17 +0530

    provider export improved

--- libs/remix-simulator/test/accounts.ts
+++ libs/remix-simulator/test/accounts.ts
@@ -1,12 +1,12 @@
 /* global describe, before, it */
 import Web3 from 'web3'
-import RemixSim from '../src/index'
+import { Provider } from '../src/index'
 const web3 = new Web3()
 import * as assert from 'assert'
 
 describe('Accounts', () => {
   before(function () {
-    const provider: any = new RemixSim.Provider()
+    const provider: any = new Provider()
     web3.setProvider(provider)
   })
 
