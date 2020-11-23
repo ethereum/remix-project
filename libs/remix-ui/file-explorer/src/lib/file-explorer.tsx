cd08b816dafd1ad9521ab4029f46f0042683dc91
ioedeveloper <daviddisu8@gmail.com>
2020-11-23 08:50:29 +0100

    Scaffold file explorer react app

---
+++ libs/remix-ui/file-explorer/src/lib/file-explorer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './remix-ui-file-explorer.css'
+
+/* eslint-disable-next-line */
+export interface FileExplorerProps {}
+
+export const FileExplorer = (props: FileExplorerProps) => {
+  return (
+    <div>
+      <h1>Welcome to file-explorer!</h1>
+    </div>
+  )
+}
+
+export default FileExplorer
