7013efe83ea248b397253e9f8b9165a5ae32a205
LianaHus <liana@ethdev.com>
2020-11-24 10:43:04 +0100

    adding files

---
+++ libs/remix-ui/modal-dialog/src/lib/modal-dialog-custom.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './modal-dialog-custom.css';
+
+/* eslint-disable-next-line */
+export interface ModalDialogCustomProps {}
+
+export const ModalDialogCustom = (props: ModalDialogCustomProps) => {
+  
+  return (
+    <div>
+      <h1>Welcome to modal-dialog-custom!</h1>
+    </div>
+  );
+};
+
+export default ModalDialogCustom;
