6485814b322fafc19041f08e19a8820e830c3c6a
aniket-engg <aniketengg.225@gmail.com>
2020-11-26 16:24:29 +0530

    evmVersionSelector tested

--- apps/remix-ide/src/app/tabs/compileTab/compilerContainer.js
+++ apps/remix-ide/src/app/tabs/compileTab/compilerContainer.js
@@ -219,7 +219,7 @@ class CompilerContainer {
 
     this._view.evmVersionSelector = yo`
       <select onchange="${this.onchangeEvmVersion.bind(this)}" class="custom-select" id="evmVersionSelector">
-        <option value="default">compiler default</option>
+        <option value="default" selected="selected">compiler default</option>
         <option>istanbul</option>
         <option>petersburg</option>
         <option>constantinople</option>
@@ -230,11 +230,8 @@ class CompilerContainer {
       </select>`
     if (this.compileTabLogic.evmVersion) {
       const s = this._view.evmVersionSelector
-      console.log('s=====>', s)
       let i
       for (i = 0; i < s.options.length; i++) {
-        console.log('options value', s.options[i].value)
-        console.log('this.compileTabLogic.evmVersion', this.compileTabLogic.evmVersion)
         if (s.options[i].value === this.compileTabLogic.evmVersion) {
           break
         }
