c4aa48fea26f953fcb058510226f41df64355916
aniket-engg <aniketengg.225@gmail.com>
2020-11-26 13:31:19 +0530

    test custom compiler

--- apps/remix-ide-e2e/src/tests/url.test.ts
+++ apps/remix-ide-e2e/src/tests/url.test.ts
@@ -25,5 +25,19 @@ module.exports = {
     .verify.elementPresent('[id="optimize"]:checked')
   },
 
+  'Should load using compiler from link passed in remix URL': function (browser: NightwatchBrowser) {
+    browser
+    .url('http://127.0.0.1:8080/#version=https://solidity-blog.s3.eu-central-1.amazonaws.com/data/08preview/soljson.js')
+    .refresh()
+    .pause(5000)
+    .clickLaunchIcon('solidity')
+    .pause(5000)
+    .assert.containsText('#versionSelector option[selected="selected"]', 'custom')
+    // default values
+    .verify.elementPresent('[id="optimize"]:checked')
+    // .assert.containsText('#evmVersionSelector option[selected="selected"]', 'compiler default')
+
+  },
+
   tearDown: sauce
 }
