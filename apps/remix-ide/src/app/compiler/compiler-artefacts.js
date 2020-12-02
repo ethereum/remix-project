f0b124bcb594012dd9a46a9aca410f91c0a161d7
yann300 <yann.levreau@gmail.com>
2020-12-02 09:42:46 +0100

    listen to the optimism compiler

--- apps/remix-ide/src/app/compiler/compiler-artefacts.js
+++ apps/remix-ide/src/app/compiler/compiler-artefacts.js
@@ -46,6 +46,11 @@ module.exports = class CompilerArtefacts extends Plugin {
       this.compilersArtefacts.__last = new CompilerAbstract(languageVersion, data, source)
       saveCompilationPerFileResult(file, source, languageVersion, data)
     })
+
+    this.on('optimism-compiler', 'compilationFinished', (file, source, languageVersion, data) => {
+      this.compilersArtefacts.__last = new CompilerAbstract(languageVersion, data, source)
+      saveCompilationPerFileResult(file, source, languageVersion, data)
+    })
   }
 
   getAllContractDatas () {
