1a6321fca3d770dbee6011881649ab6997e8115c
aniket-engg <aniketengg.225@gmail.com>
2020-12-07 19:09:16 +0530

    fileexplorer and gist tests fix

--- apps/remix-ide-e2e/src/tests/gist.test.ts
+++ apps/remix-ide-e2e/src/tests/gist.test.ts
@@ -26,7 +26,7 @@ module.exports = {
     browser
     .waitForElementVisible('*[data-id="remixIdeIconPanel"]', 10000)
     .clickLaunchIcon('fileExplorers')
-    .rightClick('[data-path="browser/contracts/1_Storage.sol"]')
+    .rightClick('[data-path="browser/readme"]')
     .click('*[id="menuitemcreate folder"]')
     .waitForElementVisible('*[data-id="modalDialogContainer"]')
     .setValue('*[data-id="modalDialogCustomPromptText"]', 'Browser_Tests')
