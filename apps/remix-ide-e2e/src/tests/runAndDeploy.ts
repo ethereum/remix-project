c62bdc17e378027637b32e2a4a310d17e1c403d2
LianaHus <liana@ethdev.com>
2020-12-08 12:59:04 +0100

    updated test: linter fixes prevent 000

--- apps/remix-ide-e2e/src/tests/runAndDeploy.ts
+++ apps/remix-ide-e2e/src/tests/runAndDeploy.ts
@@ -25,15 +25,13 @@ module.exports = {
 
   'Should load run and deploy tab and check value validation': function (browser: NightwatchBrowser) {
     browser.waitForElementPresent('*[data-id="remixIdeSidePanel"]')
-    .clickLaunchIcon('udapp')
-    .waitForElementPresent('*[data-id="sidePanelSwapitTitle"]')
     .assert.containsText('*[data-id="sidePanelSwapitTitle"]', 'DEPLOY & RUN TRANSACTIONS')
     .clearValue('#value')
     .setValue('#value', '0000')
     .assert.containsText('*[data-id="dandrValue"]', '0')
     .clearValue('#value')
     .setValue('#value', '-44')
-    .assert.containsText('*[data-id="dandrValue"]', '44')
+    .assert.containsText('*[data-id="dandrValue"]', '0')
     .clearValue('#value')
     .setValue('#value', '')
     .assert.containsText('*[data-id="dandrValue"]', '0')
