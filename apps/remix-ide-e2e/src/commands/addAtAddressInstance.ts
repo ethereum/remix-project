5568844672fe3ba9617d330a110a44810cad2030
LianaHus <liana@ethdev.com>
2020-12-09 13:20:27 +0100

    using getValue in test

--- apps/remix-ide-e2e/src/commands/addAtAddressInstance.ts
+++ apps/remix-ide-e2e/src/commands/addAtAddressInstance.ts
@@ -16,20 +16,20 @@ class addAtAddressInstance extends EventEmitter {
 function addInstance (browser: NightwatchBrowser, address: string, isValidFormat: boolean, isValidChecksum: boolean, callback: VoidFunction) {
   browser.clickLaunchIcon('udapp').clearValue('.ataddressinput').setValue('.ataddressinput', address, function () {
     browser.click('button[id^="runAndDeployAtAdressButton"]')
-        .execute(function () {
-          const ret = document.querySelector('div[class^="modal-body"] div').innerHTML
-          const modal = document.querySelector('#modal-footer-ok') as HTMLElement
-          
-          modal.click()
-          return ret
-        }, [], function (result) {
-          if (!isValidFormat) {
-            browser.assert.equal(result.value, 'Invalid address.')
-          } else if (!isValidChecksum) {
-            browser.assert.equal(result.value, 'Invalid checksum address.')
-          }
-          callback()
-        })
+      .execute(function () {
+        const ret = document.querySelector('div[class^="modal-body"] div').innerHTML
+        const modal = document.querySelector('#modal-footer-ok') as HTMLElement
+        
+        modal.click()
+        return ret
+      }, [], function (result) {
+        if (!isValidFormat) {
+          browser.assert.equal(result.value, 'Invalid address.')
+        } else if (!isValidChecksum) {
+          browser.assert.equal(result.value, 'Invalid checksum address.')
+        }
+        callback()
+      })
   })
 }
 
