10d145906194aafada711aa6598bf2d485edaeea
aniket-engg <aniketengg.225@gmail.com>
2020-12-11 20:20:53 +0530

    codeResolver and codeUtils updated

--- libs/remix-debug/src/code/codeResolver.ts
+++ libs/remix-debug/src/code/codeResolver.ts
@@ -1,56 +1,61 @@
 'use strict'
 const codeUtils = require('./codeUtils')
 
-function CodeResolver ({getCode}) {
-  this.getCode = getCode
+export class CodeResolver {
 
-  this.bytecodeByAddress = {} // bytes code by contract addesses
-  this.instructionsByAddress = {} // assembly items instructions list by contract addesses
-  this.instructionsIndexByBytesOffset = {} // mapping between bytes offset and instructions index.
-}
-
-CodeResolver.prototype.clear = function () {
-  this.bytecodeByAddress = {}
-  this.instructionsByAddress = {}
-  this.instructionsIndexByBytesOffset = {}
-}
-
-CodeResolver.prototype.resolveCode = async function (address) {
-  const cache = this.getExecutingCodeFromCache(address)
-  if (cache) {
-    return cache
+  getCode
+  bytecodeByAddress
+  instructionsByAddress
+  instructionsIndexByBytesOffset
+  
+  constructor({getCode}) {
+    this.getCode = getCode
+    this.bytecodeByAddress = {} // bytes code by contract addesses
+    this.instructionsByAddress = {} // assembly items instructions list by contract addesses
+    this.instructionsIndexByBytesOffset = {} // mapping between bytes offset and instructions index.
   }
 
-  const code = await this.getCode(address)
-  return this.cacheExecutingCode(address, code)
-}
-
-CodeResolver.prototype.cacheExecutingCode = function (address, hexCode) {
-  const codes = this.formatCode(hexCode)
-  this.bytecodeByAddress[address] = hexCode
-  this.instructionsByAddress[address] = codes.code
-  this.instructionsIndexByBytesOffset[address] = codes.instructionsIndexByBytesOffset
-  return this.getExecutingCodeFromCache(address)
-}
-
-CodeResolver.prototype.formatCode = function (hexCode) {
-  const [code, instructionsIndexByBytesOffset] = codeUtils.nameOpCodes(Buffer.from(hexCode.substring(2), 'hex'))
-  return {code, instructionsIndexByBytesOffset}
-}
-
-CodeResolver.prototype.getExecutingCodeFromCache = function (address) {
-  if (!this.instructionsByAddress[address]) {
-    return null
+  clear () {
+    this.bytecodeByAddress = {}
+    this.instructionsByAddress = {}
+    this.instructionsIndexByBytesOffset = {}
   }
-  return {
-    instructions: this.instructionsByAddress[address],
-    instructionsIndexByBytesOffset: this.instructionsIndexByBytesOffset[address],
-    bytecode: this.bytecodeByAddress[address]
+
+  async resolveCode (address) {
+    const cache = this.getExecutingCodeFromCache(address)
+    if (cache) {
+      return cache
+    }
+
+    const code = await this.getCode(address)
+    return this.cacheExecutingCode(address, code)
+  }
+
+  cacheExecutingCode (address, hexCode) {
+    const codes = this.formatCode(hexCode)
+    this.bytecodeByAddress[address] = hexCode
+    this.instructionsByAddress[address] = codes.code
+    this.instructionsIndexByBytesOffset[address] = codes.instructionsIndexByBytesOffset
+    return this.getExecutingCodeFromCache(address)
+  }
+
+  formatCode (hexCode) {
+    const [code, instructionsIndexByBytesOffset] = codeUtils.nameOpCodes(Buffer.from(hexCode.substring(2), 'hex'))
+    return {code, instructionsIndexByBytesOffset}
+  }
+
+  getExecutingCodeFromCache (address) {
+    if (!this.instructionsByAddress[address]) {
+      return null
+    }
+    return {
+      instructions: this.instructionsByAddress[address],
+      instructionsIndexByBytesOffset: this.instructionsIndexByBytesOffset[address],
+      bytecode: this.bytecodeByAddress[address]
+    }
+  }
+
+  getInstructionIndex (address, pc) {
+    return this.getExecutingCodeFromCache(address).instructionsIndexByBytesOffset[pc]
   }
 }
-
-CodeResolver.prototype.getInstructionIndex = function (address, pc) {
-  return this.getExecutingCodeFromCache(address).instructionsIndexByBytesOffset[pc]
-}
-
-module.exports = CodeResolver
--- libs/remix-debug/src/code/codeUtils.ts
+++ libs/remix-debug/src/code/codeUtils.ts
@@ -1,66 +1,65 @@
 'use strict'
 const opcodes = require('./opcodes')
 
-module.exports = {
-  nameOpCodes: function (raw) {
-    let pushData = ''
-    const codeMap = {}
-    const code = []
+export function nameOpCodes (raw) {
+  let pushData = ''
+  const codeMap = {}
+  const code = []
 
-    for (let i = 0; i < raw.length; i++) {
-      const pc = i
-      const curOpCode = opcodes(raw[pc], false).name
-      codeMap[i] = code.length
-      // no destinations into the middle of PUSH
-      if (curOpCode.slice(0, 4) === 'PUSH') {
-        const jumpNum = raw[pc] - 0x5f
-        pushData = raw.slice(pc + 1, pc + jumpNum + 1)
-        i += jumpNum
-      }
-
-      const data = pushData.toString('hex') !== '' ? ' ' + pushData.toString('hex') : ''
-
-      code.push(this.pad(pc, this.roundLog(raw.length, 10)) + ' ' + curOpCode + data)
-      pushData = ''
+  for (let i = 0; i < raw.length; i++) {
+    const pc = i
+    const curOpCode = opcodes(raw[pc], false).name
+    codeMap[i] = code.length
+    // no destinations into the middle of PUSH
+    if (curOpCode.slice(0, 4) === 'PUSH') {
+      const jumpNum = raw[pc] - 0x5f
+      pushData = raw.slice(pc + 1, pc + jumpNum + 1)
+      i += jumpNum
     }
-    return [ code, codeMap ]
-  },
 
-  /**
-   * Parses code as a list of integers into a list of objects containing
-   * information about the opcode.
-   */
-  parseCode: function (raw) {
-    const code = []
-    for (let i = 0; i < raw.length; i++) {
-      const opcode = opcodes(raw[i], true)
-      if (opcode.name.slice(0, 4) === 'PUSH') {
-        const length = raw[i] - 0x5f
-        opcode.pushData = raw.slice(i + 1, i + length + 1)
-        // in case pushdata extends beyond code
-        if (i + 1 + length > raw.length) {
-          for (let j = opcode.pushData.length; j < length; j++) {
-            opcode.pushData.push(0)
-          }
-        }
-        i += length
-      }
-      code.push(opcode)
-    }
-    return code
-  },
+    const data = pushData.toString() !== '' ? ' ' + pushData.toString() : ''
 
-  pad: function (num, size) {
-    let s = num + ''
-    while (s.length < size) s = '0' + s
-    return s
-  },
-
-  log: function (num, base) {
-    return Math.log(num) / Math.log(base)
-  },
-
-  roundLog: function (num, base) {
-    return Math.ceil(this.log(num, base))
+    code.push(this.pad(pc, this.roundLog(raw.length, 10)) + ' ' + curOpCode + data)
+    pushData = ''
   }
+  return [ code, codeMap ]
 }
+
+/**
+ * Parses code as a list of integers into a list of objects containing
+ * information about the opcode.
+ */
+export function parseCode (raw) {
+  const code = []
+  for (let i = 0; i < raw.length; i++) {
+    const opcode = opcodes(raw[i], true)
+    if (opcode.name.slice(0, 4) === 'PUSH') {
+      const length = raw[i] - 0x5f
+      opcode.pushData = raw.slice(i + 1, i + length + 1)
+      // in case pushdata extends beyond code
+      if (i + 1 + length > raw.length) {
+        for (let j = opcode.pushData.length; j < length; j++) {
+          opcode.pushData.push(0)
+        }
+      }
+      i += length
+    }
+    code.push(opcode)
+  }
+  return code
+}
+
+export function pad (num, size) {
+  let s = num + ''
+  while (s.length < size) s = '0' + s
+  return s
+}
+
+export function log (num, base) {
+  return Math.log(num) / Math.log(base)
+}
+
+export function roundLog (num, base) {
+  return Math.ceil(this.log(num, base))
+}
+
