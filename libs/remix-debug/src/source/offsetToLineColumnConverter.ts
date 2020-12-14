24d41a87611c60e61ea64bcbe57703780524e6b4
aniket-engg <aniketengg.225@gmail.com>
2020-12-14 15:21:52 +0530

    source src updated

--- libs/remix-debug/src/source/offsetToLineColumnConverter.ts
+++ libs/remix-debug/src/source/offsetToLineColumnConverter.ts
@@ -1,33 +1,37 @@
 'use strict'
 const SourceMappingDecoder = require('./sourceMappingDecoder')
 
-function offsetToColumnConverter (compilerEvent) {
-  this.lineBreakPositionsByContent = {}
-  this.sourceMappingDecoder = new SourceMappingDecoder()
-  if (compilerEvent) {
-    compilerEvent.register('compilationFinished', (success, data, source) => {
-      this.clear()
-    })
-  }
-}
+export class OffsetToColumnConverter {
 
-offsetToColumnConverter.prototype.offsetToLineColumn = function (rawLocation, file, sources, asts) {
-  if (!this.lineBreakPositionsByContent[file]) {
-    for (let filename in asts) {
-      const source = asts[filename]
-      // source id was string before. in newer versions it has been changed to an integer so we need to check the type here
-      if (typeof source.id === 'string') source.id = parseInt(source.id, 10)
-      if (source.id === file) {
-        this.lineBreakPositionsByContent[file] = this.sourceMappingDecoder.getLinebreakPositions(sources[filename].content)
-        break
-      }
+  lineBreakPositionsByContent
+  sourceMappingDecoder
+
+  constructor(compilerEvent) {
+    this.lineBreakPositionsByContent = {}
+    this.sourceMappingDecoder = new SourceMappingDecoder()
+    if (compilerEvent) {
+      compilerEvent.register('compilationFinished', (success, data, source) => {
+        this.clear()
+      })
     }
   }
-  return this.sourceMappingDecoder.convertOffsetToLineColumn(rawLocation, this.lineBreakPositionsByContent[file])
-}
 
-offsetToColumnConverter.prototype.clear = function () {
-  this.lineBreakPositionsByContent = {}
-}
+  offsetToLineColumn (rawLocation, file, sources, asts) {
+    if (!this.lineBreakPositionsByContent[file]) {
+      for (let filename in asts) {
+        const source = asts[filename]
+        // source id was string before. in newer versions it has been changed to an integer so we need to check the type here
+        if (typeof source.id === 'string') source.id = parseInt(source.id, 10)
+        if (source.id === file) {
+          this.lineBreakPositionsByContent[file] = this.sourceMappingDecoder.getLinebreakPositions(sources[filename].content)
+          break
+        }
+      }
+    }
+    return this.sourceMappingDecoder.convertOffsetToLineColumn(rawLocation, this.lineBreakPositionsByContent[file])
+  }
 
-module.exports = offsetToColumnConverter
+  clear () {
+    this.lineBreakPositionsByContent = {}
+  }
+}
