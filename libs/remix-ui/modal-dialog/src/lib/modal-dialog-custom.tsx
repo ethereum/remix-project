a4d0f87b6f51ebb623b004444f5e582e83790da7
ioedeveloper <daviddisu8@gmail.com>
2020-12-17 13:47:39 +0100

    Fixed linting

--- libs/remix-ui/modal-dialog/src/lib/modal-dialog-custom.tsx
+++ libs/remix-ui/modal-dialog/src/lib/modal-dialog-custom.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-import './modal-dialog-custom.css';
+import './modal-dialog-custom.css'
 
 /* eslint-disable-next-line */
 export interface ModalDialogCustomProps {}
 
 export const ModalDialogCustom = (props: ModalDialogCustomProps) => {
-  
+
   return (
     <div>
       <h1>Welcome to modal-dialog-custom!</h1>
     </div>
-  );
-};
+  )
+}
 
-export default ModalDialogCustom;
+export default ModalDialogCustom
