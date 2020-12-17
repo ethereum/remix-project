93ae21fce9515ceffdc1844656e1043f3b268927
ioedeveloper <daviddisu8@gmail.com>
2020-12-17 16:09:15 +0100

    Run linting in circleci

--- apps/remix-ide/src/app/ui/modaldialog.js
+++ apps/remix-ide/src/app/ui/modaldialog.js
@@ -130,22 +130,21 @@ module.exports = (title, content, ok, cancel, focusSelector, opts) => {
 
 function html (opts) {
   return yo`
-    <div id="modal-dialog" data-id="modalDialogContainer" data-backdrop="static" data-keyboard="false" class="modal" tabindex="-1" role="dialog">
-      <div id="modal-background" class="modal-dialog" role="document">
-        <div class="modal-content ${css.modalContent} ${opts.class}">
-          <div class="modal-header">
-            <h6 class="modal-title" data-id="modalDialogModalTitle"></h6>
-            <span class="modal-close">
-              <i id="modal-close" title="Close" class="fas fa-times" aria-hidden="true"></i>
-            </span>
-          </div>
-          <div class="modal-body ${css.modalBody}" data-id="modalDialogModalBody"> - </div>
-          <div class="modal-footer" data-id="modalDialogModalFooter" autofocus>
-            <span id="modal-footer-ok" class="${css.modalFooterOk} modal-ok btn btn-sm btn-light" tabindex='5'>OK</span>
-            <span id="modal-footer-cancel" class="${css.modalFooterCancel} modal-cancel btn btn-sm btn-light" tabindex='10' data-dismiss="modal">Cancel</span>
-          </div>
+  <div id="modal-dialog" data-id="modalDialogContainer" data-backdrop="static" data-keyboard="false" class="modal" tabindex="-1" role="dialog">
+    <div id="modal-background" class="modal-dialog" role="document">
+      <div class="modal-content ${css.modalContent} ${opts.class}">
+        <div class="modal-header">
+          <h6 class="modal-title" data-id="modalDialogModalTitle"></h6>
+          <span class="modal-close">
+            <i id="modal-close" title="Close" class="fas fa-times" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div class="modal-body ${css.modalBody}" data-id="modalDialogModalBody"> - </div>
+        <div class="modal-footer" data-id="modalDialogModalFooter" autofocus>
+          <span id="modal-footer-ok" class="${css.modalFooterOk} modal-ok btn btn-sm btn-light" tabindex='5'>OK</span>
+          <span id="modal-footer-cancel" class="${css.modalFooterCancel} modal-cancel btn btn-sm btn-light" tabindex='10' data-dismiss="modal">Cancel</span>
         </div>
       </div>
     </div>
-`
+  </div>`
 }
