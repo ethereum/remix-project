9c7f9b955673e74553a5bbea25a08e857c21cf7e
yann300 <yann.levreau@gmail.com>
2020-12-23 15:01:14 +0100

    pass the address to  "newSourceLocation" listenners

--- libs/remix-ui/debugger-ui/src/lib/debugger-ui.tsx
+++ libs/remix-ui/debugger-ui/src/lib/debugger-ui.tsx
@@ -74,12 +74,11 @@ export const DebuggerUI = (props: DebuggerUIProps) => {
       })
     })
 
-    debuggerInstance.event.register('newSourceLocation', async (lineColumnPos, rawLocation, generatedSources) => {
+    debuggerInstance.event.register('newSourceLocation', async (lineColumnPos, rawLocation, generatedSources, address) => {
       if (!lineColumnPos) return
       const contracts = await debuggerModule.fetchContractAndCompile(
-        currentReceipt.contractAddress || currentReceipt.to,
+        address || currentReceipt.contractAddress || currentReceipt.to,
         currentReceipt)
-
       if (contracts) {
         let path = contracts.getSourceName(rawLocation.file)
         if (!path) {
@@ -153,7 +152,9 @@ export const DebuggerUI = (props: DebuggerUIProps) => {
       offsetToLineColumnConverter: debuggerModule.offsetToLineColumnConverter,
       compilationResult: async (address) => {
         try {
-          return await debuggerModule.fetchContractAndCompile(address, currentReceipt)
+          const ret = await debuggerModule.fetchContractAndCompile(address, currentReceipt)
+          console.log('ok', ret)
+          return ret
         } catch (e) {
           console.error(e)
         }
