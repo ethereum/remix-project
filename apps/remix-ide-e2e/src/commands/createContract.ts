f4ea376a9c8963051180d271127868bc8c09488b
yann300 <yann.levreau@gmail.com>
2021-01-06 11:19:44 +0100

    fix e2e tests

--- apps/remix-ide-e2e/src/commands/createContract.ts
+++ apps/remix-ide-e2e/src/commands/createContract.ts
@@ -15,14 +15,11 @@ class CreateContract extends EventEmitter {
 
 function createContract (browser: NightwatchBrowser, inputParams: string, callback: VoidFunction) {
   if (inputParams) {
-    browser.clickLaunchIcon('settings').clickLaunchIcon('udapp')
-    .setValue('div[class^="contractActionsContainerSingle"] input', inputParams, function () {
+    browser.setValue('div[class^="contractActionsContainerSingle"] input', inputParams, function () {
       browser.click('#runTabView button[class^="instanceButton"]').pause(500).perform(function () { callback() })
     })
   } else {
     browser
-    .clickLaunchIcon('settings')
-    .clickLaunchIcon('udapp')
     .click('#runTabView button[class^="instanceButton"]')
     .pause(500)
     .perform(function () { callback() })
