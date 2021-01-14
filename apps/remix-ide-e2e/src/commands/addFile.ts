95644a377dcf6236cf0d0f3e59a81d82af38df30
ioedeveloper <daviddisu8@gmail.com>
2021-01-14 15:25:04 +0100

    Fix gist test

--- apps/remix-ide-e2e/src/commands/addFile.ts
+++ apps/remix-ide-e2e/src/commands/addFile.ts
@@ -2,9 +2,9 @@ import { NightwatchBrowser, NightwatchContractContent } from 'nightwatch'
 import EventEmitter from "events"
 
 class AddFile extends EventEmitter {
-  command (this: NightwatchBrowser, name: string, content: NightwatchContractContent): NightwatchBrowser {
+  command (this: NightwatchBrowser, name: string, content: NightwatchContractContent, open = true): NightwatchBrowser {
     this.api.perform((done) => {
-      addFile(this.api, name, content, () => {
+      addFile(this.api, name, content, open, () => {
         done()
         this.emit('complete')
       })
@@ -13,13 +13,18 @@ class AddFile extends EventEmitter {
   }
 }
 
-function addFile (browser: NightwatchBrowser, name: string, content: NightwatchContractContent, done: VoidFunction) {
+function addFile (browser: NightwatchBrowser, name: string, content: NightwatchContractContent, open: boolean, done: VoidFunction) {
   browser.clickLaunchIcon('udapp').clickLaunchIcon('fileExplorers').click('.newFile')
     .pause(2000)
     .keys(name)
     .keys(browser.Keys.ENTER)
-    .openFile('browser/' + name)
-    .setEditorValue(content.content)
+    .perform((done) => {
+      if (open) {
+        browser.openFile('browser/' + name)
+          .setEditorValue(content.content)
+      }
+      done()
+    })
     .pause(1000)
     .perform(function () {
       done()
