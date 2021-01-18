3dc161b2230673d7cf50a53a1d7e74548121f1b8
LianaHus <liana@ethdev.com>
2021-01-18 12:54:08 +0100

    style fixes

--- libs/remix-ui/debugger-ui/src/lib/vm-debugger/storage-panel.tsx
+++ libs/remix-ui/debugger-ui/src/lib/vm-debugger/storage-panel.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import DropdownPanel from './dropdown-panel'
 
 export const StoragePanel = ({ calldata, header }) => {
-    return (
-        <div id='storagepanel'>
-            <DropdownPanel dropdownName='Storage' calldata={calldata || {}} header={header} />
-        </div>
-    )
+  return (
+    <div id='storagepanel'>
+      <DropdownPanel dropdownName='Storage' calldata={calldata || {}} header={header} />
+    </div>
+  )
 }
 
 export default StoragePanel
