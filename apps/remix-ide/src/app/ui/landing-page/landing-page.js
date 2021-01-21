f3b71d62f16e1715a4fc29fd57ac9b37cfb1f0af
filip mertens <filip.mertens@ethereum.org>
2021-01-21 12:43:21 +0100

    replace pipeline with learneth
    fix invert learneth

--- apps/remix-ide/src/app/ui/landing-page/landing-page.js
+++ apps/remix-ide/src/app/ui/landing-page/landing-page.js
@@ -206,9 +206,9 @@ export class LandingPage extends ViewPlugin {
 
     const invertNum = (themeQuality === 'dark') ? 1 : 0
     if (this.solEnv.getElementsByTagName('img')[0]) this.solEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
-    if (this.pipelineEnv.getElementsByTagName('img')[0]) this.pipelineEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     if (this.debuggerEnv.getElementsByTagName('img')[0]) this.debuggerEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     if (this.mythXEnv.getElementsByTagName('img')[0]) this.mythXEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
+    if (this.learnEthEnv.getElementsByTagName('img')[0]) this.learnEthEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     if (this.sourcifyEnv.getElementsByTagName('img')[0]) this.sourcifyEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     if (this.moreEnv.getElementsByTagName('img')[0]) this.moreEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     if (this.websiteIcon) this.websiteIcon.style.filter = `invert(${invertNum})`
@@ -259,9 +259,6 @@ export class LandingPage extends ViewPlugin {
       await this.appManager.activatePlugin(['solidity', 'udapp', 'solidityStaticAnalysis', 'solidityUnitTesting'])
       this.verticalIcons.select('solidity')
     }
-    const startPipeline = () => {
-      this.appManager.activatePlugin(['solidity', 'pipeline', 'udapp'])
-    }
     const startDebugger = async () => {
       await this.appManager.activatePlugin('debugger')
       this.verticalIcons.select('debugger')
@@ -269,6 +266,10 @@ export class LandingPage extends ViewPlugin {
     const startMythX = async () => {
       await this.appManager.activatePlugin(['solidity', 'mythx'])
       this.verticalIcons.select('mythx')
+    }
+    const startLearnEth = async () => {
+      await this.appManager.activatePlugin(['solidity', 'LearnEth'])
+      this.verticalIcons.select('LearnEth')
     }
     const startSourceVerify = async () => {
       await this.appManager.activatePlugin(['solidity', 'source-verification'])
@@ -294,8 +295,8 @@ export class LandingPage extends ViewPlugin {
     globalRegistry.get('themeModule').api.events.on('themeChanged', (theme) => {
       globalRegistry.get('themeModule').api.fixInvert(document.getElementById('remixLogo'))
       globalRegistry.get('themeModule').api.fixInvert(document.getElementById('solidityLogo'))
-      globalRegistry.get('themeModule').api.fixInvert(document.getElementById('pipelineLogo'))
       globalRegistry.get('themeModule').api.fixInvert(document.getElementById('debuggerLogo'))
+      globalRegistry.get('themeModule').api.fixInvert(document.getElementById('learnEthLogo'))
       globalRegistry.get('themeModule').api.fixInvert(document.getElementById('workshopLogo'))
       globalRegistry.get('themeModule').api.fixInvert(document.getElementById('moreLogo'))
     })
@@ -316,9 +317,9 @@ export class LandingPage extends ViewPlugin {
     // main
     this.solEnv = createLargeButton('assets/img/solidityLogo.webp', 'solidityLogo', 'Solidity', startSolidity)
     // Featured
-    this.pipelineEnv = createLargeButton('assets/img/pipelineLogo.webp', 'pipelineLogo', 'Pipeline', startPipeline)
     this.debuggerEnv = createLargeButton('assets/img/debuggerLogo.webp', 'debuggerLogo', 'Debugger', startDebugger)
     this.mythXEnv = createLargeButton('assets/img/mythxLogo.webp', 'mythxLogo', 'MythX', startMythX)
+    this.learnEthEnv = createLargeButton('assets/img/learnEthLogo.webp', 'learnEthLogo', 'LearnEth', startLearnEth)
     this.sourcifyEnv = createLargeButton('assets/img/sourcifyLogo.webp', 'sourcifyLogo', 'Sourcify', startSourceVerify)
     this.moreEnv = createLargeButton('assets/img/moreLogo.webp', 'moreLogo', 'More', startPluginManager)
     this.websiteIcon = yo`<img id='remixHhomeWebsite' class="mr-1 ${css.image}" src="${profile.icon}"></img>`
@@ -326,9 +327,9 @@ export class LandingPage extends ViewPlugin {
     const themeQuality = globalRegistry.get('themeModule').api.currentTheme().quality
     const invertNum = (themeQuality === 'dark') ? 1 : 0
     this.solEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
-    this.pipelineEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     this.debuggerEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     this.mythXEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
+    this.learnEthEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     this.sourcifyEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     this.moreEnv.getElementsByTagName('img')[0].style.filter = `invert(${invertNum})`
     this.websiteIcon.style.filter = `invert(${invertNum})`
@@ -359,7 +360,7 @@ export class LandingPage extends ViewPlugin {
                   <h4>Featured Plugins</h4>
                   <div class="d-flex flex-row pt-2">
                     ${this.solEnv}
-                    ${this.pipelineEnv}
+                    ${this.learnEthEnv}
                     ${this.mythXEnv}
                     ${this.sourcifyEnv}
                     ${this.debuggerEnv}
