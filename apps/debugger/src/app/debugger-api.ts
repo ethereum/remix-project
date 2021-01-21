3cabe3af2920072382046c990b3c1a785fe45684
lianahus <liana@ethereum.org>
2021-01-21 15:37:10 +0100

    added bleach to the lib

--- apps/debugger/src/app/debugger-api.ts
+++ apps/debugger/src/app/debugger-api.ts
@@ -6,10 +6,10 @@ import type { CompilationResult } from '@remix-project/remix-solidity-ts'
 export const DebuggerApiMixin = (Base) => class extends Base {
   initDebuggerApi () {
     this.debugHash = null
-        
+
     const self = this
     this.web3Provider = {
-      sendAsync(payload, callback) {
+      sendAsync (payload, callback) {
         self.call('web3Provider', 'sendAsync', payload)
           .then(result => callback(null, result))
           .catch(e => callback(e))
