0485a1407e6ce1d9ef65d76c652e4557b22f2f96
aniket-engg <aniketengg.225@gmail.com>
2021-02-03 20:20:00 +0530

    fix tests for imports from openzepplin

--- apps/remix-ide-e2e/src/tests/debugger.test.ts
+++ apps/remix-ide-e2e/src/tests/debugger.test.ts
@@ -92,7 +92,7 @@ module.exports = {
     .waitForElementVisible('#stepdetail')
     .goToVMTraceStep(10)
     .getEditorValue((content) => {
-      browser.assert.ok(content.indexOf(`constructor (string memory name_, string memory symbol_) public {
+      browser.assert.ok(content.indexOf(`constructor (string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
         _decimals = 18;
