68a4fc0e06a35200298e24cbcc60d01edd979ae3
Iuri Matias <iuri.matias@gmail.com>
2021-02-07 14:29:11 -0500

    move settings of deploy tab to react

--- libs/remix-ui/run-tab/src/lib/settings/value-selector.tsx
+++ libs/remix-ui/run-tab/src/lib/settings/value-selector.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react' // eslint-disable-line
 /* eslint-disable-next-line */
 import './value-selector.css'
 
 export const ValueSelector = () => {
-  const [gasValue, setGasValue] = useState("0");
+  const [gasValue, setGasValue] = useState('0')
   return (
     <div className="remixui_crow">
-      <label className="remixui_settingsLabel" data-id="remixDRValueLabel">Value</label>
+      <label className="remixui_settingsLabel">Value</label>
       <div className="remixui_gasValueContainer">
-        <input type="text" data-id="dandrValue" className="form-control remixui_gasNval remixui_col2" id="value" value={gasValue} onChange={(e) => setGasValue((e.target as HTMLInputElement).value)} title="Enter the value and choose the unit" />
+        <input type="text" className="form-control remixui_gasNval remixui_col2" id="value" value={gasValue} onChange={(e) => setGasValue((e.target as HTMLInputElement).value)} title="Enter the value and choose the unit" />
         <select name="unit" className="form-control p-1 remixui_gasNvalUnit remixui_col2_2 custom-select" id="unit">
           <option data-unit="wei">wei</option>
           <option data-unit="gwei">gwei</option>
