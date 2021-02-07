ea735f316dcbb867dad7ae78c91135d5a742db45
Iuri Matias <iuri.matias@gmail.com>
2021-02-07 14:29:11 -0500

    move settings of deploy tab to react

---
+++ libs/remix-ui/run-tab/src/lib/settings/value-selector.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+/* eslint-disable-next-line */
+import './value-selector.css'
+
+export const ValueSelector = () => {
+  const [gasValue, setGasValue] = useState("0");
+  return (
+    <div className="remixui_crow">
+      <label className="remixui_settingsLabel">Value</label>
+      <div className="remixui_gasValueContainer">
+        <input type="text" className="form-control remixui_gasNval remixui_col2" id="value" value={gasValue} onChange={(e) => setGasValue((e.target as HTMLInputElement).value)} title="Enter the value and choose the unit" />
+        <select name="unit" className="form-control p-1 remixui_gasNvalUnit remixui_col2_2 custom-select" id="unit">
+          <option data-unit="wei">wei</option>
+          <option data-unit="gwei">gwei</option>
+          <option data-unit="finney">finney</option>
+          <option data-unit="ether">ether</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default ValueSelector
