6518d3565789cc29e9f388af39d724524b08b70c
aniket-engg <aniketengg.225@gmail.com>
2021-02-11 19:26:26 +0530

    remix-solidity linting fixed

--- libs/remix-solidity/src/compiler/compiler-worker.ts
+++ libs/remix-solidity/src/compiler/compiler-worker.ts
@@ -12,39 +12,39 @@ export default function (self) { // eslint-disable-line @typescript-eslint/expli
     const data: MessageToWorker = e.data
     switch (data.cmd) {
       case 'loadVersion':
-        {
-          delete self.Module
-          // NOTE: workaround some browsers?
-          self.Module = undefined
-          compileJSON = null
-          //importScripts() method of synchronously imports one or more scripts into the worker's scope
-          self.importScripts(data.data)
-          const compiler: solc = solc(self.Module)
-          compileJSON = (input) => {
-            try {
-              const missingInputsCallback = (path) => {
-                missingInputs.push(path)
-                return { 'error': 'Deferred import' }
-              }
-              return compiler.compile(input, { import: missingInputsCallback })
-            } catch (exception) {
-              return JSON.stringify({ error: 'Uncaught JavaScript exception:\n' + exception })
+      {
+        delete self.Module
+        // NOTE: workaround some browsers?
+        self.Module = undefined
+        compileJSON = null
+        // importScripts() method of synchronously imports one or more scripts into the worker's scope
+        self.importScripts(data.data)
+        const compiler: solc = solc(self.Module)
+        compileJSON = (input) => {
+          try {
+            const missingInputsCallback = (path) => {
+              missingInputs.push(path)
+              return { error: 'Deferred import' }
             }
+            return compiler.compile(input, { import: missingInputsCallback })
+          } catch (exception) {
+            return JSON.stringify({ error: 'Uncaught JavaScript exception:\n' + exception })
           }
-          self.postMessage({
-            cmd: 'versionLoaded',
-            data: compiler.version()
-          })
-          break
         }
-        
+        self.postMessage({
+          cmd: 'versionLoaded',
+          data: compiler.version()
+        })
+        break
+      }
+
       case 'compile':
         missingInputs.length = 0
-        if(data.input && compileJSON) {
+        if (data.input && compileJSON) {
           self.postMessage({
-            cmd: 'compiled', 
-            job: data.job, 
-            data: compileJSON(data.input), 
+            cmd: 'compiled',
+            job: data.job,
+            data: compileJSON(data.input),
             missingInputs: missingInputs
           })
         }
