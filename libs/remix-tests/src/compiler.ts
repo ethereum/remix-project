a6a31d6daafa73bad98e5369af9bcb17e3ce46de
aniket-engg <aniketengg.225@gmail.com>
2021-02-11 19:43:58 +0530

    linting for remix-tests fixed

--- libs/remix-tests/src/compiler.ts
+++ libs/remix-tests/src/compiler.ts
@@ -2,26 +2,26 @@ import fs from './fileSystem'
 import async from 'async'
 import path from 'path'
 import Log from './logger'
-const logger = new Log()
-const log = logger.logger
 import { Compiler as RemixCompiler } from '@remix-project/remix-solidity'
 import { SrcIfc, CompilerConfiguration, CompilationErrors } from './types'
+const logger = new Log()
+const log = logger.logger
 
 function regexIndexOf (inputString: string, regex: RegExp, startpos = 0) {
-    const indexOf = inputString.substring(startpos).search(regex)
-    return (indexOf >= 0) ? (indexOf + (startpos)) : indexOf
+  const indexOf = inputString.substring(startpos).search(regex)
+  return (indexOf >= 0) ? (indexOf + (startpos)) : indexOf
 }
 
 function writeTestAccountsContract (accounts: string[]) {
-    const testAccountContract = require('../sol/tests_accounts.sol')
-    let body = `address[${accounts.length}] memory accounts;`
-    if (!accounts.length) body += ';'
-    else {
-        accounts.map((address, index) => {
-            body += `\naccounts[${index}] = ${address};\n`
-        })
-    }
-    return testAccountContract.replace('>accounts<', body)
+  const testAccountContract = require('../sol/tests_accounts.sol')
+  let body = `address[${accounts.length}] memory accounts;`
+  if (!accounts.length) body += ';'
+  else {
+    accounts.map((address, index) => {
+      body += `\naccounts[${index}] = ${address};\n`
+    })
+  }
+  return testAccountContract.replace('>accounts<', body)
 }
 
 /**
@@ -29,47 +29,46 @@ function writeTestAccountsContract (accounts: string[]) {
  * @param path file path to check
  */
 
-function isRemixTestFile(path: string) {
-    return ['tests.sol', 'remix_tests.sol', 'remix_accounts.sol'].some(name => path.includes(name))
+function isRemixTestFile (path: string) {
+  return ['tests.sol', 'remix_tests.sol', 'remix_accounts.sol'].some(name => path.includes(name))
 }
 
 /**
  * @dev Process file to prepare sources object to be passed in solc compiler input
- * 
+ *
  * See: https://solidity.readthedocs.io/en/latest/using-the-compiler.html#input-description
- * 
+ *
  * @param filePath path of file to process
- * @param sources existing 'sources' object in which keys are the "global" names of the source files and 
+ * @param sources existing 'sources' object in which keys are the "global" names of the source files and
  *                value is object containing content of corresponding file with under key 'content'
  * @param isRoot True, If file is a root test contract file which is getting processed, not an imported file
  */
 
-function processFile(filePath: string, sources: SrcIfc, isRoot = false) {
-    const importRegEx = /import ['"](.+?)['"];/g
-    let group: RegExpExecArray| null = null
-    const isFileAlreadyInSources: boolean = Object.keys(sources).includes(filePath)
+function processFile (filePath: string, sources: SrcIfc, isRoot = false) {
+  const importRegEx = /import ['"](.+?)['"];/g
+  let group: RegExpExecArray| null = null
+  const isFileAlreadyInSources: boolean = Object.keys(sources).includes(filePath)
 
-    // Return if file is a remix test file or already processed
-    if(isRemixTestFile(filePath) || isFileAlreadyInSources)
-        return
+  // Return if file is a remix test file or already processed
+  if (isRemixTestFile(filePath) || isFileAlreadyInSources) { return }
 
-    let content: string = fs.readFileSync(filePath, { encoding: 'utf-8' })
-    const testFileImportRegEx = /^(import)\s['"](remix_tests.sol|tests.sol)['"];/gm
+  let content: string = fs.readFileSync(filePath, { encoding: 'utf-8' })
+  const testFileImportRegEx = /^(import)\s['"](remix_tests.sol|tests.sol)['"];/gm
 
-    // import 'remix_tests.sol', if file is a root test contract file and doesn't already have it 
-    if (isRoot && filePath.endsWith('_test.sol') && regexIndexOf(content, testFileImportRegEx) < 0) {
-        const includeTestLibs = '\nimport \'remix_tests.sol\';\n'
-        content = includeTestLibs.concat(content)
-    }
-    sources[filePath] = {content}
-    importRegEx.exec('') // Resetting state of RegEx
+  // import 'remix_tests.sol', if file is a root test contract file and doesn't already have it
+  if (isRoot && filePath.endsWith('_test.sol') && regexIndexOf(content, testFileImportRegEx) < 0) {
+    const includeTestLibs = '\nimport \'remix_tests.sol\';\n'
+    content = includeTestLibs.concat(content)
+  }
+  sources[filePath] = { content }
+  importRegEx.exec('') // Resetting state of RegEx
 
-    // Process each 'import' in file content
-    while ((group = importRegEx.exec(content))) {
-        const importedFile: string = group[1]
-        const importedFilePath: string = path.join(path.dirname(filePath), importedFile)
-        processFile(importedFilePath, sources)
-    }
+  // Process each 'import' in file content
+  while ((group = importRegEx.exec(content))) {
+    const importedFile: string = group[1]
+    const importedFilePath: string = path.join(path.dirname(filePath), importedFile)
+    processFile(importedFilePath, sources)
+  }
 }
 
 const userAgent = (typeof (navigator) !== 'undefined') && navigator.userAgent ? navigator.userAgent.toLowerCase() : '-'
@@ -81,88 +80,86 @@ const isBrowser = !(typeof (window) === 'undefined' || userAgent.indexOf(' elect
  * @param isDirectory True, if path is a directory
  * @param opts Options
  * @param cb Callback
- * 
+ *
  * TODO: replace this with remix's own compiler code
  */
 
-export function compileFileOrFiles(filename: string, isDirectory: boolean, opts: any, compilerConfig: CompilerConfiguration, cb): void {
-    let compiler: any
-    const accounts: string[] = opts.accounts || []
-    const sources: SrcIfc = {
-        'tests.sol': { content: require('../sol/tests.sol') },
-        'remix_tests.sol': { content: require('../sol/tests.sol') },
-        'remix_accounts.sol': { content: writeTestAccountsContract(accounts) }
-    }
-    const filepath: string = (isDirectory ? filename : path.dirname(filename))
-    try {
-        if(!isDirectory && fs.existsSync(filename)) {
-            if (filename.split('.').pop() === 'sol') {
-                processFile(filename, sources, true)
-            } else {
-                throw new Error('Not a solidity file')
-            }
-        } else {
-            // walkSync only if it is a directory
-            let testFileCount = 0
-            fs.walkSync(filepath, (foundpath: string) => {
-                // only process .sol files
-                if (foundpath.split('.').pop() === 'sol' && foundpath.endsWith('_test.sol')) {
-                    testFileCount++
-                    processFile(foundpath, sources, true)
-                }
-            })
-            if(testFileCount > 0) {
-                log.info(`${testFileCount} Solidity test file${testFileCount===1?'':'s'} found`)
-            }
-            else {
-                log.error(`No Solidity test file found. Make sure your test file ends with '_test.sol'`)
-                process.exit()
-            }
+export function compileFileOrFiles (filename: string, isDirectory: boolean, opts: any, compilerConfig: CompilerConfiguration, cb): void {
+  let compiler: any
+  const accounts: string[] = opts.accounts || []
+  const sources: SrcIfc = {
+    'tests.sol': { content: require('../sol/tests.sol') },
+    'remix_tests.sol': { content: require('../sol/tests.sol') },
+    'remix_accounts.sol': { content: writeTestAccountsContract(accounts) }
+  }
+  const filepath: string = (isDirectory ? filename : path.dirname(filename))
+  try {
+    if (!isDirectory && fs.existsSync(filename)) {
+      if (filename.split('.').pop() === 'sol') {
+        processFile(filename, sources, true)
+      } else {
+        throw new Error('Not a solidity file')
+      }
+    } else {
+      // walkSync only if it is a directory
+      let testFileCount = 0
+      fs.walkSync(filepath, (foundpath: string) => {
+        // only process .sol files
+        if (foundpath.split('.').pop() === 'sol' && foundpath.endsWith('_test.sol')) {
+          testFileCount++
+          processFile(foundpath, sources, true)
         }
-        
-    } catch (e) { // eslint-disable-line no-useless-catch
-        throw e
-    } finally {
-        async.waterfall([
-            function loadCompiler(next) {
-                compiler = new RemixCompiler()
-                if(compilerConfig) {
-                    const {currentCompilerUrl, evmVersion, optimize, runs} = compilerConfig
-                    evmVersion ? compiler.set('evmVersion', evmVersion) : null
-                    optimize ? compiler.set('optimize', optimize) : null
-                    runs ? compiler.set('runs', runs) : null
-                    if(currentCompilerUrl) {
-                        compiler.loadRemoteVersion(currentCompilerUrl)
-                        compiler.event.register('compilerLoaded', this, function (version) {
-                            next()
-                        })
-                    } else {
-                        compiler.onInternalCompilerLoaded()
-                        next()
-                    }
-                } else {
-                    compiler.onInternalCompilerLoaded()
-                    next()
-                }
-            },
-            function doCompilation(next) {
-                // @ts-ignore
-                compiler.event.register('compilationFinished', this, (success, data, source) => {
-                    next(null, data)
-                })
-                compiler.compile(sources, filepath)
-            }
-        ], function (err: Error | null | undefined, result: any) {
-            const error: Error[] = []
-            if (result.error) error.push(result.error)
-            const errors = (result.errors || error).filter((e) => e.type === 'Error' || e.severity === 'error')
-            if (errors.length > 0) {
-                if (!isBrowser) require('signale').fatal(errors)
-                return cb(new CompilationErrors(errors))
-            }
-            cb(err, result.contracts, result.sources) //return callback with contract details & ASTs
-        })
+      })
+      if (testFileCount > 0) {
+        log.info(`${testFileCount} Solidity test file${testFileCount === 1 ? '' : 's'} found`)
+      } else {
+        log.error('No Solidity test file found. Make sure your test file ends with \'_test.sol\'')
+        process.exit()
+      }
     }
+  } catch (e) { // eslint-disable-line no-useless-catch
+    throw e
+  } finally {
+    async.waterfall([
+      function loadCompiler (next) {
+        compiler = new RemixCompiler()
+        if (compilerConfig) {
+          const { currentCompilerUrl, evmVersion, optimize, runs } = compilerConfig
+          if (evmVersion) compiler.set('evmVersion', evmVersion)
+          if (optimize) compiler.set('optimize', optimize)
+          if (runs) compiler.set('runs', runs)
+          if (currentCompilerUrl) {
+            compiler.loadRemoteVersion(currentCompilerUrl)
+            compiler.event.register('compilerLoaded', this, function (version) {
+              next()
+            })
+          } else {
+            compiler.onInternalCompilerLoaded()
+            next()
+          }
+        } else {
+          compiler.onInternalCompilerLoaded()
+          next()
+        }
+      },
+      function doCompilation (next) {
+        // @ts-ignore
+        compiler.event.register('compilationFinished', this, (success, data, source) => {
+          next(null, data)
+        })
+        compiler.compile(sources, filepath)
+      }
+    ], function (err: Error | null | undefined, result: any) {
+      const error: Error[] = []
+      if (result.error) error.push(result.error)
+      const errors = (result.errors || error).filter((e) => e.type === 'Error' || e.severity === 'error')
+      if (errors.length > 0) {
+        if (!isBrowser) require('signale').fatal(errors)
+        return cb(new CompilationErrors(errors))
+      }
+      cb(err, result.contracts, result.sources) // return callback with contract details & ASTs
+    })
+  }
 }
 
 /**
@@ -173,53 +170,53 @@ export function compileFileOrFiles(filename: string, isDirectory: boolean, opts:
  * @param opts Options
  * @param cb Callback
  */
-export function compileContractSources(sources: SrcIfc, compilerConfig: CompilerConfiguration, importFileCb: any, opts: any, cb): void {
-    let compiler, filepath: string
-    const accounts: string[] = opts.accounts || []
-    // Iterate over sources keys. Inject test libraries. Inject test library import statements.
-    if (!('remix_tests.sol' in sources) && !('tests.sol' in sources)) {
-        sources['tests.sol'] = { content: require('../sol/tests.sol.js') }
-        sources['remix_tests.sol'] = { content: require('../sol/tests.sol.js') }
-        sources['remix_accounts.sol'] = { content: writeTestAccountsContract(accounts) }
-    }
-    const testFileImportRegEx = /^(import)\s['"](remix_tests.sol|tests.sol)['"];/gm
+export function compileContractSources (sources: SrcIfc, compilerConfig: CompilerConfiguration, importFileCb: any, opts: any, cb): void {
+  let compiler, filepath: string
+  const accounts: string[] = opts.accounts || []
+  // Iterate over sources keys. Inject test libraries. Inject test library import statements.
+  if (!('remix_tests.sol' in sources) && !('tests.sol' in sources)) {
+    sources['tests.sol'] = { content: require('../sol/tests.sol.js') }
+    sources['remix_tests.sol'] = { content: require('../sol/tests.sol.js') }
+    sources['remix_accounts.sol'] = { content: writeTestAccountsContract(accounts) }
+  }
+  const testFileImportRegEx = /^(import)\s['"](remix_tests.sol|tests.sol)['"];/gm
 
-    const includeTestLibs = '\nimport \'remix_tests.sol\';\n'
-    for (const file in sources) {
-        const c: string = sources[file].content
-        if (file.endsWith('_test.sol') && c && regexIndexOf(c, testFileImportRegEx) < 0) {
-            sources[file].content = includeTestLibs.concat(c)
-        }
+  const includeTestLibs = '\nimport \'remix_tests.sol\';\n'
+  for (const file in sources) {
+    const c: string = sources[file].content
+    if (file.endsWith('_test.sol') && c && regexIndexOf(c, testFileImportRegEx) < 0) {
+      sources[file].content = includeTestLibs.concat(c)
     }
+  }
 
-    async.waterfall([
-        function loadCompiler (next) {
-            const {currentCompilerUrl, evmVersion, optimize, runs, usingWorker} = compilerConfig
-            compiler = new RemixCompiler(importFileCb)
-            compiler.set('evmVersion', evmVersion)
-            compiler.set('optimize', optimize)
-            compiler.set('runs', runs)
-            compiler.loadVersion(usingWorker, currentCompilerUrl)
-            // @ts-ignore
-            compiler.event.register('compilerLoaded', this, (version) => {
-                next()
-            })
-        },
-        function doCompilation (next) {
-            // @ts-ignore
-            compiler.event.register('compilationFinished', this, (success, data, source) => {
-                next(null, data)
-            })
-            compiler.compile(sources, filepath)
-        }
-    ], function (err: Error | null | undefined , result: any) {
-        const error: Error[] = []
-        if (result.error) error.push(result.error)
-        const errors = (result.errors || error).filter((e) => e.type === 'Error' || e.severity === 'error')
-        if (errors.length > 0) {
-            if (!isBrowser) require('signale').fatal(errors)
-            return cb(new CompilationErrors(errors))
-        }
-        cb(err, result.contracts, result.sources) // return callback with contract details & ASTs
-    })
+  async.waterfall([
+    function loadCompiler (next) {
+      const { currentCompilerUrl, evmVersion, optimize, runs, usingWorker } = compilerConfig
+      compiler = new RemixCompiler(importFileCb)
+      compiler.set('evmVersion', evmVersion)
+      compiler.set('optimize', optimize)
+      compiler.set('runs', runs)
+      compiler.loadVersion(usingWorker, currentCompilerUrl)
+      // @ts-ignore
+      compiler.event.register('compilerLoaded', this, (version) => {
+        next()
+      })
+    },
+    function doCompilation (next) {
+      // @ts-ignore
+      compiler.event.register('compilationFinished', this, (success, data, source) => {
+        next(null, data)
+      })
+      compiler.compile(sources, filepath)
+    }
+  ], function (err: Error | null | undefined, result: any) {
+    const error: Error[] = []
+    if (result.error) error.push(result.error)
+    const errors = (result.errors || error).filter((e) => e.type === 'Error' || e.severity === 'error')
+    if (errors.length > 0) {
+      if (!isBrowser) require('signale').fatal(errors)
+      return cb(new CompilationErrors(errors))
+    }
+    cb(err, result.contracts, result.sources) // return callback with contract details & ASTs
+  })
 }
--- libs/remix-tests/src/run.ts
+++ libs/remix-tests/src/run.ts
@@ -7,29 +7,29 @@ import fs from './fileSystem'
 import { Provider } from '@remix-project/remix-simulator'
 import { CompilerConfiguration } from './types'
 import Log from './logger'
+import colors from 'colors'
 const logger = new Log()
 const log = logger.logger
-import colors from 'colors'
 
 // parse verbosity
 function mapVerbosity (v: number) {
-    const levels = {
-        0: 'error',
-        1: 'warn',
-        2: 'info',
-        3: 'verbose',
-        4: 'debug',
-        5: 'silly'
-    }
-    return levels[v]
+  const levels = {
+    0: 'error',
+    1: 'warn',
+    2: 'info',
+    3: 'verbose',
+    4: 'debug',
+    5: 'silly'
+  }
+  return levels[v]
 }
 
 function mapOptimize (v: string) {
-        const optimize = {
-                'true': true,
-                'false': false
-        }
-        return optimize[v];
+  const optimize = {
+    true: true,
+    false: false
+  }
+  return optimize[v]
 }
 
 const version = require('../package.json').version
@@ -37,92 +37,91 @@ const version = require('../package.json').version
 commander.version(version)
 
 commander.command('version').description('output the version number').action(function () {
-    console.log(version)
+  console.log(version)
 })
 
 commander.command('help').description('output usage information').action(function () {
-    commander.help()
+  commander.help()
 })
 
 // get current version
 commander
-    .option('-c, --compiler <string>', 'set compiler version (e.g: 0.6.1, 0.7.1 etc)')
-    .option('-e, --evm <string>', 'set EVM version (e.g: petersburg, istanbul etc)')
-    .option('-o, --optimize <bool>', 'enable/disable optimization', mapOptimize)
-    .option('-r, --runs <number>', 'set runs (e.g: 150, 250 etc)')
-    .option('-v, --verbose <level>', 'set verbosity level (0 to 5)', mapVerbosity)
-    .action(async (testsPath) => {
+  .option('-c, --compiler <string>', 'set compiler version (e.g: 0.6.1, 0.7.1 etc)')
+  .option('-e, --evm <string>', 'set EVM version (e.g: petersburg, istanbul etc)')
+  .option('-o, --optimize <bool>', 'enable/disable optimization', mapOptimize)
+  .option('-r, --runs <number>', 'set runs (e.g: 150, 250 etc)')
+  .option('-v, --verbose <level>', 'set verbosity level (0 to 5)', mapVerbosity)
+  .action(async (testsPath) => {
+    // Check if path exists
+    if (!fs.existsSync(testsPath)) {
+      log.error(testsPath + ' not found')
+      process.exit(1)
+    }
 
-        // Check if path exists
-        if (!fs.existsSync(testsPath)) {
-            log.error(testsPath + ' not found')
-            process.exit(1)
-        }
+    // Check if path is for a directory
+    const isDirectory = fs.lstatSync(testsPath).isDirectory()
 
-        // Check if path is for a directory
-        const isDirectory = fs.lstatSync(testsPath).isDirectory()
+    // If path is for a file, file name must have `_test.sol` suffix
+    if (!isDirectory && !testsPath.endsWith('_test.sol')) {
+      log.error('Test filename should end with "_test.sol"')
+      process.exit()
+    }
 
-        // If path is for a file, file name must have `_test.sol` suffix
-        if(!isDirectory && !testsPath.endsWith('_test.sol')) {
-            log.error('Test filename should end with "_test.sol"')
-            process.exit()
-        }
+    // Console message
+    console.log(colors.white('\n\t👁\t:: Running remix-tests - Unit testing for solidity ::\t👁\n'))
 
-        // Console message
-        console.log(colors.white('\n\t👁\t:: Running remix-tests - Unit testing for solidity ::\t👁\n'))
-        
-        // Set logger verbosity
-        if (commander.verbose) {
-            logger.setVerbosity(commander.verbose)
-            log.info('verbosity level set to ' + commander.verbose.blue)
-        }
+    // Set logger verbosity
+    if (commander.verbose) {
+      logger.setVerbosity(commander.verbose)
+      log.info('verbosity level set to ' + commander.verbose.blue)
+    }
 
-        let compilerConfig = {} as CompilerConfiguration
-        if (commander.compiler) {
-            const compVersion = commander.compiler
-            const baseURL = 'https://binaries.soliditylang.org/wasm/'
-            const response: AxiosResponse = await axios.get(baseURL + 'list.json')
-            const { releases, latestRelease } = response.data
-            const compString = releases[compVersion]
-            if(!compString) {
-                log.error(`No compiler found in releases with version ${compVersion}`)
-                process.exit()
-            } else {
-                compilerConfig.currentCompilerUrl = compString.replace('soljson-', '').replace('.js', '')
-                log.info(`Compiler version set to ${compVersion}. Latest version is ${latestRelease}`)
-            }
-        }
+    const compilerConfig = {} as CompilerConfiguration
+    if (commander.compiler) {
+      const compVersion = commander.compiler
+      const baseURL = 'https://binaries.soliditylang.org/wasm/'
+      const response: AxiosResponse = await axios.get(baseURL + 'list.json')
+      const { releases, latestRelease } = response.data
+      const compString = releases[compVersion]
+      if (!compString) {
+        log.error(`No compiler found in releases with version ${compVersion}`)
+        process.exit()
+      } else {
+        compilerConfig.currentCompilerUrl = compString.replace('soljson-', '').replace('.js', '')
+        log.info(`Compiler version set to ${compVersion}. Latest version is ${latestRelease}`)
+      }
+    }
 
-        if (commander.evm) {
-            compilerConfig.evmVersion = commander.evm
-            log.info(`EVM set to ${compilerConfig.evmVersion}`)
-        }
+    if (commander.evm) {
+      compilerConfig.evmVersion = commander.evm
+      log.info(`EVM set to ${compilerConfig.evmVersion}`)
+    }
 
-        if (commander.optimize) {
-            compilerConfig.optimize = commander.optimize
-            log.info(`Optimization is ${compilerConfig.optimize ? 'enabled' : 'disabled'}`)
-        }
+    if (commander.optimize) {
+      compilerConfig.optimize = commander.optimize
+      log.info(`Optimization is ${compilerConfig.optimize ? 'enabled' : 'disabled'}`)
+    }
 
-        if (commander.runs) {
-            if(!commander.optimize) {
-                log.error(`Optimization should be enabled for runs`)
-                process.exit()
-            }
-            compilerConfig.runs = commander.runs
-            log.info(`Runs set to ${compilerConfig.runs}`)
-        }
+    if (commander.runs) {
+      if (!commander.optimize) {
+        log.error('Optimization should be enabled for runs')
+        process.exit()
+      }
+      compilerConfig.runs = commander.runs
+      log.info(`Runs set to ${compilerConfig.runs}`)
+    }
 
-        const web3 = new Web3()
-        const provider: any = new Provider()
-        await provider.init()
-        web3.setProvider(provider)
+    const web3 = new Web3()
+    const provider: any = new Provider()
+    await provider.init()
+    web3.setProvider(provider)
 
-        runTestFiles(path.resolve(testsPath), isDirectory, web3, compilerConfig)
-    })
+    runTestFiles(path.resolve(testsPath), isDirectory, web3, compilerConfig)
+  })
 
 if (!process.argv.slice(2).length) {
-    log.error('Please specify a file or directory path')
-    process.exit()
+  log.error('Please specify a file or directory path')
+  process.exit()
 }
 
 commander.parse(process.argv)
