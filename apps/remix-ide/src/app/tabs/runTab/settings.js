1430dfded1b26c591df70360d3d27fdae2bc980b
Marius van der Wijden <m.vanderwijden@live.de>
2021-02-15 12:49:15 +0100

    Update settings.js

--- apps/remix-ide/src/app/tabs/runTab/settings.js
+++ apps/remix-ide/src/app/tabs/runTab/settings.js
@@ -236,13 +236,13 @@ class SettingsUI {
     return yo`
       <div class="">
         Note: To use Geth & https://remix.ethereum.org, configure it to allow requests from Remix:(see <a href="https://geth.ethereum.org/docs/rpc/server" target="_blank">Geth Docs on rpc server</a>)
-        <div class="border p-1">geth --rpc --rpccorsdomain https://remix.ethereum.org</div>
+        <div class="border p-1">geth --http --http.corsdomain https://remix.ethereum.org</div>
         <br>
         To run Remix & a local Geth test node, use this command: (see <a href="https://geth.ethereum.org/getting-started/dev-mode" target="_blank">Geth Docs on Dev mode</a>)
-        <div class="border p-1">geth --rpc --rpccorsdomain="${window.origin}" --rpcapi web3,eth,debug,personal,net --vmdebug --datadir ${thePath} --dev console</div>
+        <div class="border p-1">geth --http --http.corsdomain="${window.origin}" --http.api web3,eth,debug,personal,net --vmdebug --datadir ${thePath} --dev console</div>
         <br>
         <br> 
-        <b>WARNING:</b> It is not safe to use the --rpccorsdomain flag with a wildcard: <b>--rpccorsdomain *</b>
+        <b>WARNING:</b> It is not safe to use the --http.corsdomain flag with a wildcard: <b>--http.corsdomain *</b>
         <br>
         <br>For more info: <a href="https://remix-ide.readthedocs.io/en/latest/run.html#more-about-web3-provider" target="_blank">Remix Docs on Web3 Provider</a>
         <br>
