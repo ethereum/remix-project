dbd4b5e43a1c210c6f9e74421968b2413c01f525
aniket-engg <aniketengg.225@gmail.com>
2021-02-17 23:51:33 +0530

    transactionExecution e2e tests fixed

--- apps/remix-ide-e2e/src/tests/transactionExecution.test.ts
+++ apps/remix-ide-e2e/src/tests/transactionExecution.test.ts
@@ -178,7 +178,7 @@ const sources = [
         _a = msg.sender;
     }
 
-    function retunValues2 () public returns (byte _b, bytes2 _b2, bytes3 _b3, bytes memory _blit, bytes5 _b5, bytes6 _b6, string memory _str, bytes7 _b7, bytes22 _b22, bytes32 _b32)  {
+    function retunValues2 () public returns (bytes1 _b, bytes2 _b2, bytes3 _b3, bytes memory _blit, bytes5 _b5, bytes6 _b6, string memory _str, bytes7 _b7, bytes22 _b22, bytes32 _b32)  {
         _b = 0x12;
         _b2 = 0x1223;
         _b5 = hex"043245";
