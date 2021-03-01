71b3a30ef55e98087ad6d276029563b8f431444b
yann300 <yann.levreau@gmail.com>
2021-03-01 12:06:22 +0100

    linting

--- apps/remix-ide/src/app/panels/file-panel.js
+++ apps/remix-ide/src/app/panels/file-panel.js
@@ -66,7 +66,7 @@ module.exports = class Filepanel extends ViewPlugin {
   }
 
   render () {
-    this.initWorkspace().then(() => this.getWorkspaces()).catch(console.error)    
+    this.initWorkspace().then(() => this.getWorkspaces()).catch(console.error)
     return this.el
   }
 
