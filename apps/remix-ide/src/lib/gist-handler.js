37d9c377e392fc9ff46e38d235e65655b8ef811e
yann300 <yann.levreau@gmail.com>
2021-03-03 09:37:53 +0100

    ensure alert has a title

--- apps/remix-ide/src/lib/gist-handler.js
+++ apps/remix-ide/src/lib/gist-handler.js
@@ -20,7 +20,7 @@ function GistHandler (_window) {
           if (gistId) {
             cb(gistId)
           } else {
-            modalDialogCustom.alert('Error while loading gist. Please provide a valid Gist ID or URL.')
+            modalDialogCustom.alert('Gist load error', 'Error while loading gist. Please provide a valid Gist ID or URL.')
           }
         }
       })
@@ -49,7 +49,7 @@ function GistHandler (_window) {
         json: true
       }, async (error, response, data = {}) => {
         if (error || !data.files) {
-          modalDialogCustom.alert(`Gist load error: ${error || data.message}`)
+          modalDialogCustom.alert('Gist load error', error || data.message)
           return
         }
         const obj = {}
@@ -61,7 +61,7 @@ function GistHandler (_window) {
             const provider = fileManager.getProvider('workspace')
             provider.lastLoadedGistId = gistId
           } else {
-            modalDialogCustom.alert(errorLoadingFile.message || errorLoadingFile)
+            modalDialogCustom.alert('Gist load error', errorLoadingFile.message || errorLoadingFile)
           }
         })
       })
