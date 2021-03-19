5d3865ef49726e32b2b88189f507be98df026e3f
tizah <davidzagi93@gmail.com>
2021-03-19 15:11:34 +0100

    build: build and run the remix-project and created static-analyser using nx console

---
+++ libs/remix-ui/static-analyser/README.md
@@ -0,0 +1,7 @@
+# static-analyser
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test static-analyser` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/remix-ui/static-analyser/src/lib/static-analyser.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './static-analyser.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiStaticAnalyserProps {}
+
+export const RemixUiStaticAnalyser = (props: RemixUiStaticAnalyserProps) => {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-static-analyser!</h1>
+    </div>
+  );
+};
+
+export default RemixUiStaticAnalyser;
