62df9243ca8f6ebe9c12e0ac43a5ee4ffae7ab86
ioedeveloper <daviddisu8@gmail.com>
2021-03-26 16:08:29 +0100

    Wait for content timeout

--- apps/remix-ide-e2e/src/commands/addFile.ts
+++ apps/remix-ide-e2e/src/commands/addFile.ts
@@ -18,11 +18,11 @@ function addFile (browser: NightwatchBrowser, name: string, content: NightwatchC
     .clickLaunchIcon('fileExplorers')
     .click('li[data-id="treeViewLitreeViewItemREADME.txt"]') // focus on root directory
     .click('.newFile')
-    .waitForElementVisible('*[data-id="treeViewLitreeViewItem/blank"]')
+    .waitForElementVisible('*[data-id="treeViewLitreeViewItem/blank"]', 60000)
     .sendKeys('*[data-id="treeViewLitreeViewItem/blank"] .remixui_items', name)
     .sendKeys('*[data-id="treeViewLitreeViewItem/blank"] .remixui_items', browser.Keys.ENTER)
     .pause(2000)
-    .waitForElementVisible(`li[data-id="treeViewLitreeViewItem${name}"]`)
+    .waitForElementVisible(`li[data-id="treeViewLitreeViewItem${name}"]`, 60000)
     .setEditorValue(content.content)
     .pause(1000)
     .perform(function () {
