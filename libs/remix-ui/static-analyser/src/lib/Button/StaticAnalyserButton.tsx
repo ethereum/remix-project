8112f60705ba3170623ed4f495bb692b62e2aac7
tizah <davidzagi93@gmail.com>
2021-04-10 02:06:33 +0100

    refactor: added custom checkbox for static analyser plugin

---
+++ libs/remix-ui/static-analyser/src/lib/Button/StaticAnalyserButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react'  //eslint-disable-line
+
+interface StaticAnalyserButtonProps {
+  onClick: (event) => void
+  buttonText: string,
+  disabled?: boolean
+}
+
+const StaticAnalyserButton = ({
+  onClick,
+  buttonText,
+  disabled
+}: StaticAnalyserButtonProps) => {
+  return (
+    <div className="remixui-button-container">
+      <button className="btn btn-sm w-31 btn-primary" onClick={onClick} disabled={disabled}>
+        {buttonText}
+      </button>
+    </div>
+  )
+}
+
+export default StaticAnalyserButton
