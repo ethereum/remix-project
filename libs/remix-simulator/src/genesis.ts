922697c359a5db578dc6a427020902a5a366a863
yann300 <yann.levreau@gmail.com>
2021-04-21 10:11:04 +0200

    using fromBlockData

--- libs/remix-simulator/src/genesis.ts
+++ libs/remix-simulator/src/genesis.ts
@@ -1,15 +1,16 @@
-import { Block, BlockHeader } from '@ethereumjs/block'
+import { Block } from '@ethereumjs/block'
 import { BN } from 'ethereumjs-util'
 
 export function generateBlock (executionContext) {
-  const header: BlockHeader = BlockHeader.fromHeaderData({
-    timestamp: (new Date().getTime() / 1000 | 0),
-    number: 0,
-    coinbase: '0x0e9281e9c6a0808672eaba6bd1220e144c9bb07a',
-    difficulty: new BN('69762765929000', 10),
-    gasLimit: new BN('8000000').imuln(1)
-  })
-  const block: Block = new Block(header, [], [])
+  const block: Block = Block.fromBlockData({
+    header: {
+      timestamp: (new Date().getTime() / 1000 | 0),
+      number: 0,
+      coinbase: '0x0e9281e9c6a0808672eaba6bd1220e144c9bb07a',
+      difficulty: new BN('69762765929000', 10),
+      gasLimit: new BN('8000000').imuln(1)
+    }
+  }, { common: executionContext.vmObject().common })
 
   executionContext.vm().runBlock({ block: block, generate: true, skipBlockValidation: true, skipBalance: false }).then(() => {
     executionContext.addBlock(block)
