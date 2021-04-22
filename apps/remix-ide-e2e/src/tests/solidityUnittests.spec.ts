908d897f9fb78e28bbecc56269798ea72d21ecf9
lianahus <liana@ethereum.org>
2021-04-22 09:24:29 +0200

    fixing test

--- apps/remix-ide-e2e/src/tests/solidityUnittests.spec.ts
+++ apps/remix-ide-e2e/src/tests/solidityUnittests.spec.ts
@@ -156,6 +156,7 @@ function runTests (browser: NightwatchBrowser) {
     .pause(500)
     .clearValue('*[data-id="uiPathInput"]')
     .setValue('*[data-id="uiPathInput"]', 'tests')
+    .click('*[data-id="testTabGenerateTestFolder"]')
     .pause(2000)
     .scrollAndClick('#runTestsTabRunAction')
     .waitForElementVisible('*[data-id="testTabSolidityUnitTestsOutputheader"]', 120000)
