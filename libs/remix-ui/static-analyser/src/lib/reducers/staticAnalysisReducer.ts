f664226c59df6a0952563ee93782175196b7de09
tizah <davidzagi@yahoo.com>
2021-04-27 17:30:40 +0100

    refactored static analysis to improve performance

--- libs/remix-ui/static-analyser/src/lib/reducers/staticAnalysisReducer.ts
+++ libs/remix-ui/static-analyser/src/lib/reducers/staticAnalysisReducer.ts
@@ -1,65 +1,22 @@
-import remixLib from '@remix-project/remix-lib'
-import * as _ from 'lodash'
-const StaticAnalysisRunner = require('@remix-project/remix-analyzer').CodeAnalysis
 
-const utils = remixLib.util
-
-const runner = new StaticAnalysisRunner()
-
-const preProcessModules = (arr: any) => {
-  return arr.map((Item, i) => {
-    const itemObj = new Item()
-    itemObj._index = i
-    itemObj.categoryDisplayName = itemObj.category.displayName
-    itemObj.categoryId = itemObj.category.id
-    return itemObj
-  })
+export const initialState = {
+  file: null,
+  source: null,
+  languageVersion: null,
+  data: null
 }
-
-const groupedModules = utils.groupBy(
-  preProcessModules(runner.modules()),
-  'categoryId'
-)
-
-const getIndex = (modules, array) => {
-  Object.values(modules).map((value: {_index}) => {
-    if (Array.isArray(value)) {
-      value.forEach((x) => {
-        array.push(x._index.toString())
-      })
-    } else {
-      array.push(value._index.toString())
-    }
-  })
-}
-const groupedModuleIndex = (modules) => {
-  const indexOfCategory = []
-  if (!_.isEmpty(modules)) {
-    getIndex(modules, indexOfCategory)
-  }
-  return indexOfCategory
-}
-
-export const initialState = { categoryIndex: [] }
 
 export const analysisReducer = (state, action) => {
   switch (action.type) {
-    case 'initialize':
-      return { ...state, categoryIndex: groupedModuleIndex(groupedModules) }
-    case 'uncheck':
+    case 'compilationFinished':
       return {
         ...state,
-        categoryIndex: state.categoryIndex.filter((el) => {
-          return !action.payload.includes(el)
-        })
+        file: action.payload.file,
+        source: action.payload.source,
+        languageVersion: action.payload.languageVersion,
+        data: action.payload.data
       }
-    case 'check':
-      return { ...state, categoryIndex: _.uniq([...state.categoryIndex, ...action.payload]) }
-    case 'uncheckSingle':
-      return { ...state, categoryIndex: state.categoryIndex.filter(val => val !== action.payload) }
-    case 'checkSingle':
-      return { ...state, categoryIndex: _.uniq([...state.categoryIndex, action.payload]) }
     default:
-      return { ...state, categoryIndex: groupedModuleIndex(groupedModules) }
+      return initialState
   }
 }
