f48693991116ea5ada1f0566531e620a37915299
tizah <davidzagi@yahoo.com>
2021-04-27 17:30:40 +0100

    refactored static analysis to improve performance

--- libs/remix-ui/static-analyser/src/lib/actions/staticAnalysisActions.ts
+++ libs/remix-ui/static-analyser/src/lib/actions/staticAnalysisActions.ts
@@ -1,24 +1,14 @@
-import React, { useState } from 'react' //eslint-disable-line
+import React from 'react' //eslint-disable-line
 
-export const compilation = (analysisModule, state, run) => {
-//   const setCompilationResult = async (data, source, file) => {
-//     await setResult({ lastCompilationResult: data, lastCompilationSource: source, currentFile: file })
-//   }
+export const compilation = (analysisModule, dispatch) => {
   if (analysisModule) {
     analysisModule.on(
       'solidity',
       'compilationFinished',
       (file, source, languageVersion, data) => {
         if (languageVersion.indexOf('soljson') !== 0) return
-        setCompilationResult(data, source, file)
-        if (state.categoryIndex.length > 0) {
-          run(data, source, file)
-        }
+        dispatch({ type: 'compilationFinished', payload: { file, source, languageVersion, data } })
       }
     )
   }
-}
-
-export const setCompilationResult = async (data, source, file) => {
-  return await { data, source, file }
 }
