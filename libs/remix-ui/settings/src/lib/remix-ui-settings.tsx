ec782ad2475f1c1567e81344c1e6bb875789e3f6
tizah <davidzagi@yahoo.com>
2021-05-07 12:31:33 +0100

    feat: task configuration and creating of setting-tab in react

---
+++ libs/remix-ui/settings/src/lib/remix-ui-settings.tsx
@@ -0,0 +1,17 @@
+import React from 'react' // eslint-disable-line
+
+import './remix-ui-settings.css'
+
+/* eslint-disable-next-line */
+export interface RemixUiSettingsProps {
+}
+
+export const RemixUiSettings = (props: RemixUiSettingsProps) => {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-settings!</h1>
+    </div>
+  )
+}
+
+export default RemixUiSettings
