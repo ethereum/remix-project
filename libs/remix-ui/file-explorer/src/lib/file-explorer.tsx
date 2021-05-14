16f539627b147f436b98c7ce65003841a7531163
filip mertens <filip.mertens@ethereum.org>
2021-05-14 17:42:26 +0200

    use shiftkey

--- libs/remix-ui/file-explorer/src/lib/file-explorer.tsx
+++ libs/remix-ui/file-explorer/src/lib/file-explorer.tsx
@@ -228,7 +228,7 @@ export const FileExplorer = (props: FileExplorerProps) => {
 
   useEffect(() => {
     const keyPressHandler = (e: KeyboardEvent) => {
-      if (e.key === 'Shift') {
+      if (e.shiftKey) {
         setState(prevState => {
           return { ...prevState, ctrlKey: true }
         })
@@ -236,7 +236,7 @@ export const FileExplorer = (props: FileExplorerProps) => {
     }
 
     const keyUpHandler = (e: KeyboardEvent) => {
-      if (e.key === 'Shift') {
+      if (!e.shiftKey) {
         setState(prevState => {
           return { ...prevState, ctrlKey: false }
         })
