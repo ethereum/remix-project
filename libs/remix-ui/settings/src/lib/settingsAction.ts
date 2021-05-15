d462a9be7a74e2172789aafb55aef5ff80750cda
tizah <davidzagi@yahoo.com>
2021-05-15 11:33:39 +0100

    saving state on the settings tab

--- libs/remix-ui/settings/src/lib/settingsAction.ts
+++ libs/remix-ui/settings/src/lib/settingsAction.ts
@@ -9,31 +9,28 @@ declare global {
 const _paq = window._paq = window._paq || [] //eslint-disable-line
 
 export const generateContractMetadat = (element, event, dispatch) => {
-  const checked = element.config.set('settings/generate-contract-metadata')
-  console.log({ checked }, ' checked in general contract metadata')
-  element.config.set('settings/generate-contract-metadata', !event.target.checked)
+  element.config.set('settings/generate-contract-metadata', event.target.checked)
   dispatch({ type: 'contractMetadata', payload: { name: event.target.name, isChecked: event.target.checked, textClass: event.target.checked ? textDark : textSecondary } })
 }
 
 export const etherumVM = (element, event, dispatch) => {
-  element.config.set('settings/always-use-vm', !event.target.checked)
+  element.config.set('settings/always-use-vm', event.target.checked)
   dispatch({ type: 'ethereumVM', payload: { name: event.target.name, isChecked: event.target.checked, textClass: event.target.checked ? textDark : textSecondary } })
 }
 
 export const textWrapEventAction = (element, event, dispatch) => {
-  console.log({ element })
-  element.config.set('settings/text-wrap', !event.target.checked)
+  element.config.set('settings/text-wrap', event.target.checked)
   element.editor.resize(!event.target.checked)
   dispatch({ type: 'textWrap', payload: { name: event.target.name, isChecked: event.target.checked, textClass: event.target.checked ? textDark : textSecondary } })
 }
 
 export const personal = (element, event, dispatch) => {
-  element.config.set('settings/personal-mode', !event.target.checked)
+  element.config.set('settings/personal-mode', event.target.checked)
   dispatch({ type: 'personal', payload: { name: event.target.name, isChecked: event.target.checked, textClass: event.target.checked ? textDark : textSecondary } })
 }
 
 export const useMatomoAnalytics = (element, event, dispatch) => {
-  element.config.set('settings/matomo-analytics', !event.target.checked)
+  element.config.set('settings/matomo-analytics', event.target.checked)
   dispatch({ type: 'useMatomoAnalytics', payload: { name: event.target.name, isChecked: event.target.checked, textClass: event.target.checked ? textDark : textSecondary } })
   if (event.target.checked) {
     _paq.push(['forgetUserOptOut'])
