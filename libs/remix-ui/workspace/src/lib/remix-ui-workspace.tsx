a346fad5d9de40f23c8aca6821aea6b4ed198b5c
aniket-engg <aniketengg.225@gmail.com>
2021-05-17 13:55:59 +0530

    switch to previous workspace from localhost

--- libs/remix-ui/workspace/src/lib/remix-ui-workspace.tsx
+++ libs/remix-ui/workspace/src/lib/remix-ui-workspace.tsx
@@ -91,6 +91,7 @@ export const Workspace = (props: WorkspaceProps) => {
 
   const localhostDisconnect = () => {
     if (state.currentWorkspace === LOCALHOST) setWorkspace(props.workspaces.length > 0 ? props.workspaces[0] : NO_WORKSPACE)
+    else setWorkspace(state.currentWorkspace) // Useful to switch to last selcted workspace when remixd is disconnected
   }
   props.localhost.event.unregister('disconnected', localhostDisconnect)
   props.localhost.event.register('disconnected', localhostDisconnect)
@@ -272,6 +273,7 @@ export const Workspace = (props: WorkspaceProps) => {
 
   const remixdExplorer = {
     hide: async () => {
+      // If 'connect to localhost' is clicked from home tab, mode is not 'localhost'
       if (props.fileManager.mode === 'localhost') {
         await setWorkspace(NO_WORKSPACE)
         props.fileManager.setMode('browser')
@@ -279,6 +281,7 @@ export const Workspace = (props: WorkspaceProps) => {
           return { ...prevState, hideRemixdExplorer: true, loadingLocalhost: false }
         })
       } else {
+        // Hide spinner in file explorer
         setState(prevState => {
           return { ...prevState, loadingLocalhost: false }
         })
