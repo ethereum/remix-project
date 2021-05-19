f68eecfb8b4261ebbc4ef85c80f8ca6177c9e82f
yann300 <yann.levreau@gmail.com>
2021-05-19 12:31:38 +0200

    refactor unitest

---
+++ libs/remix-debug/test/decoder/contracts/calldata.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  contract: `
+contract calldataLocal {
+    constructor () public {     
+    }
+    
+    function level11(uint8[1] calldata foo, string[2][1] calldata boo) public {
+      
+    }
+  }
+`}
