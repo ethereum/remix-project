40a8f83630acbe140709efbbfe88f4ea1574d584
yann300 <yann.levreau@gmail.com>
2021-05-19 21:58:29 +0200

    ensure calldata locals are propery tested

--- libs/remix-debug/test/decoder/contracts/calldata.ts
+++ libs/remix-debug/test/decoder/contracts/calldata.ts
@@ -2,12 +2,13 @@
 
 module.exports = {
   contract: `
-contract calldataLocal {
+  pragma experimental ABIEncoderV2;
+  contract calldataLocal {
     constructor () public {     
     }
     
     function level11(uint8[1] calldata foo, string[2][1] calldata boo) public {
-      
+      uint p = 45;
     }
   }
 `}
