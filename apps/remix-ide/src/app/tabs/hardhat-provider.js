ec05ea91f6d30058ab722d6153f6d39f62c90e59
aniket-engg <aniketengg.225@gmail.com>
2021-05-21 17:04:12 +0530

    set execution context for hardhat provider

--- apps/remix-ide/src/app/tabs/hardhat-provider.js
+++ apps/remix-ide/src/app/tabs/hardhat-provider.js
@@ -1,8 +1,5 @@
 import * as packageJson from '../../../../../package.json'
 import { Plugin } from '@remixproject/engine'
-const yo = require('yo-yo')
-const modalDialogCustom = require('../ui/modal-dialog-custom')
-import Web3 from 'web3'
 
 const profile = {
   name: 'hardhat-provider',
@@ -14,38 +11,23 @@ const profile = {
 }
 
 export default class HardhatProvider extends Plugin {
-    constructor () {
-      super(profile)
-      this.provider = null
-    }
+  constructor () {
+    super(profile)
+    this.provider = null
+  }
 
-    hardhatProviderDialogBody () {
-      return yo`
-        <div class="">
-          Hardhat Provider Endpoint
-        </div>
-      `
-    }
-
-    sendAsync (data) {
-      console.log('Inside sendAsync - modalDialogCustom', modalDialogCustom)
-      modalDialogCustom.prompt('Hardhat node request', this.hardhatProviderDialogBody(), 'http://127.0.0.1:8545', (target) => {
-        console.log('target--->', target)
-        this.provider = new Web3.providers.HttpProvider(target)
-        return new Promise((resolve, reject) => {
-          console.log('inside sendAsync promise')
-          if (this.provider) {
-            this.provider[this.provider.sendAsync ? 'sendAsync' : 'send'](data, (error, message) => {
-              if (error) return reject(error)
-              resolve(message)
-            })
-          } else {
-            resolve({"jsonrpc": "2.0", "result": [], "id": data.id})
-          }
+  sendAsync (data) {
+    return new Promise((resolve, reject) => {
+      if (this.provider) {
+        this.provider[this.provider.sendAsync ? 'sendAsync' : 'send'](data, (error, message) => {
+          if (error) return reject(error)
+          resolve(message)
         })
-      })
-        
+      } else {
+        resolve({ jsonrpc: '2.0', result: [], id: data.id })
       }
+    })
+  }
 }
 
 module.exports = HardhatProvider
