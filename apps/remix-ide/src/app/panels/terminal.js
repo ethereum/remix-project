9c6f81ebb041194c6ac3fd40b88373c08f04ab13
aniket-engg <aniketengg.225@gmail.com>
2021-05-21 20:00:34 +0530

    remix-plugin version updated and terminal API used

--- apps/remix-ide/src/app/panels/terminal.js
+++ apps/remix-ide/src/app/panels/terminal.js
@@ -93,9 +93,6 @@ class Terminal extends Plugin {
   }
 
   onActivation () {
-    this.on('solidity-logic', 'hardhatCompilationFinished', (msg) => {
-      this.commands.log(msg)
-    })
     this.on('scriptRunner', 'log', (msg) => {
       this.commands.log.apply(this.commands, msg.data)
     })
