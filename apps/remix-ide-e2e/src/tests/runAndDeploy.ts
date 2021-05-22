04f0f3b53d654eab66016bc05716c8946e261853
tizah <davidzagi@yahoo.com>
2021-05-22 19:30:38 +0100

    adding a pause to a test in runAndDeploy

--- apps/remix-ide-e2e/src/tests/runAndDeploy.ts
+++ apps/remix-ide-e2e/src/tests/runAndDeploy.ts
@@ -33,6 +33,7 @@ module.exports = {
   'Should sign message using account key': function (browser: NightwatchBrowser) {
     browser.waitForElementPresent('*[data-id="settingsRemixRunSignMsg"]')
       .click('select[id="selectExEnvOptions"] option[value="vm"]')
+      .pause(2000)
       .click('*[data-id="settingsRemixRunSignMsg"]')
       .pause(2000)
       .waitForElementPresent('*[data-id="modalDialogCustomPromptText"]')
