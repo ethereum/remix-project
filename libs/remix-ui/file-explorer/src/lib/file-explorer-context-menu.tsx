82e6fb7aa3c6826f3c12c4ee435dec4402aab3e3
ioedeveloper <daviddisu8@gmail.com>
2021-05-28 12:54:49 +0100

    Refactor delete all for multi-select

--- libs/remix-ui/file-explorer/src/lib/file-explorer-context-menu.tsx
+++ libs/remix-ui/file-explorer/src/lib/file-explorer-context-menu.tsx
@@ -6,6 +6,7 @@ import './css/file-explorer-context-menu.css'
 export const FileExplorerContextMenu = (props: FileExplorerContextMenuProps) => {
   const { actions, createNewFile, createNewFolder, deletePath, renamePath, hideContextMenu, pushChangesToGist, publishFileToGist, publishFolderToGist, copy, paste, runScript, emit, pageX, pageY, path, type, focus, ...otherProps } = props
   const contextMenuRef = useRef(null)
+
   useEffect(() => {
     contextMenuRef.current.focus()
   }, [])
@@ -21,43 +22,24 @@ export const FileExplorerContextMenu = (props: FileExplorerContextMenuProps) => 
     }
   }, [pageX, pageY])
 
-  const filterItem = (item: action) => {
-    /**
-     * if there are multiple elements focused we need to take this and all conditions must be met plus the action must be set to 'multi'
-     * for example : 'downloadAsZip' with type ['file','folder','multi'] will work on files and folders when multiple are selected
-    **/
-    const nonRootFocus = focus.filter((el) => { return !(el.key === '' && el.type === 'folder') })
-    if (nonRootFocus.length > 1) {
-      for (const element of nonRootFocus) {
-        if (!itemMatchesCondition(item, element.type, element.key)) return false
-      }
-      return (item.type.includes('multi'))
-    } else {
-      return itemMatchesCondition(item, type, path)
-    }
-  }
-
-  const itemMatchesCondition = (item: action, itemType: string, itemPath: string) => {
-    if (item.type && Array.isArray(item.type) && (item.type.findIndex(name => name === itemType) !== -1)) return true
-    else if (item.path && Array.isArray(item.path) && (item.path.findIndex(key => key === itemPath) !== -1)) return true
-    else if (item.extension && Array.isArray(item.extension) && (item.extension.findIndex(ext => itemPath.endsWith(ext)) !== -1)) return true
-    else if (item.pattern && Array.isArray(item.pattern) && (item.pattern.filter(value => itemPath.match(new RegExp(value))).length > 0)) return true
+  const itemMatchesCondition = (item: action) => {
+    if (item.type && Array.isArray(item.type) && (item.type.findIndex(name => name === type) !== -1)) return true
+    else if (item.path && Array.isArray(item.path) && (item.path.findIndex(key => key === path) !== -1)) return true
+    else if (item.extension && Array.isArray(item.extension) && (item.extension.findIndex(ext => path.endsWith(ext)) !== -1)) return true
+    else if (item.pattern && Array.isArray(item.pattern) && (item.pattern.filter(value => path.match(new RegExp(value))).length > 0)) return true
     else return false
   }
 
   const getPath = () => {
-    const nonRootFocus = focus.filter((el) => { return !(el.key === '' && el.type === 'folder') })
-    if (nonRootFocus.length > 1) {
-      return nonRootFocus.map((element) => { return element.key })
+    if (focus.length > 1) {
+      return focus.map((element) => element.key)
     } else {
       return path
     }
   }
 
   const menu = () => {
-    return actions.filter(item => {
-      return filterItem(item)
-    }).map((item, index) => {
+    return actions.filter(item => itemMatchesCondition(item)).map((item, index) => {
       return <li
         id={`menuitem${item.name.toLowerCase()}`}
         key={index}
@@ -75,7 +57,7 @@ export const FileExplorerContextMenu = (props: FileExplorerContextMenuProps) => 
               renamePath(path, type)
               break
             case 'Delete':
-              deletePath(getPath())
+              deletePath(path)
               break
             case 'Push changes to gist':
               pushChangesToGist(path, type)
@@ -94,6 +76,9 @@ export const FileExplorerContextMenu = (props: FileExplorerContextMenuProps) => 
               break
             case 'Paste':
               paste(path, type)
+              break
+            case 'Delete All':
+              deletePath(getPath())
               break
             default:
               emit && emit(item.id, getPath())
