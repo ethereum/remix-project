dc598fe9bbc0d9045dd0f85560497ad8384227ef
yann300 <yann.levreau@gmail.com>
2021-06-23 23:11:37 +0200

    keep track of stop/revert/return indexes and outofgad indexes

--- libs/remix-debug/src/debugger/VmDebugger.ts
+++ libs/remix-debug/src/debugger/VmDebugger.ts
@@ -59,8 +59,8 @@ export class VmDebuggerLogic {
   }
 
   listenToCodeManagerEvents () {
-    this._codeManager.event.register('changed', (code, address, index, nextIndex) => {
-      this.event.trigger('codeManagerChanged', [code, address, index, nextIndex])
+    this._codeManager.event.register('changed', (code, address, index, nextIndex, returnInstructionIndexes, outOfGasInstructionIndexes) => {
+      this.event.trigger('codeManagerChanged', [code, address, index, nextIndex, returnInstructionIndexes, outOfGasInstructionIndexes])
     })
   }
 
