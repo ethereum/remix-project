81e1ebe7c13dacb06080ecb3e2f87033ce9a6fb7
joseph izang <joeizang@hotmail.com>
2021-06-29 12:26:42 +0100

    create skeleton of plugin manager in react

---
+++ libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './remix-ui-plugin-manager.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiPluginManagerProps {}
+
+export const RemixUiPluginManager = (props: RemixUiPluginManagerProps) => {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-plugin-manager!</h1>
+    </div>
+  );
+};
+
+export default RemixUiPluginManager;
