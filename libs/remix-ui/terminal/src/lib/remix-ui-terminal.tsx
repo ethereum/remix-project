e8befa822611da12c2da7032aee3b0129b570d5b
tizah <davidzagi@yahoo.com>
2021-06-29 12:41:39 +0100

    ese to master nitial setup for moving terminal to react

---
+++ libs/remix-ui/terminal/src/lib/remix-ui-terminal.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './remix-ui-terminal.css'
+
+/* eslint-disable-next-line */
+export interface RemixUiTerminalProps {}
+
+export const RemixUiTerminal = (props: RemixUiTerminalProps) => {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-terminal!</h1>
+    </div>
+  )
+}
+
+export default RemixUiTerminal;
