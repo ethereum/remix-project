64b16fa17e9d5aec6f8887aab6a872c42be9dd92
Aniket-Engg <aniketengg.225@gmail.com>
2021-07-01 11:37:52 +0530

    array added to useeffect

--- libs/remix-ui/static-analyser/src/lib/remix-ui-static-analyser.tsx
+++ libs/remix-ui/static-analyser/src/lib/remix-ui-static-analyser.tsx
@@ -89,7 +89,7 @@ export const RemixUiStaticAnalyser = (props: RemixUiStaticAnalyserProps) => {
       if (currentWorkspace && currentWorkspace.isLocalhost === true) setShowSlither('visible')
     })
     return () => { }
-  })
+  }, [props.analysisModule])
 
   const message = (name, warning, more, fileName, locationString) : string => {
     return (`
