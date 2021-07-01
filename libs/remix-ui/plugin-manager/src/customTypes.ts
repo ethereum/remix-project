65d140cb3d14b872473854c16678005fa6dc68de
joseph izang <joeizang@hotmail.com>
2021-07-01 13:03:47 +0100

    add listgroupitem component

--- libs/remix-ui/plugin-manager/src/customTypes.ts
+++ libs/remix-ui/plugin-manager/src/customTypes.ts
@@ -5,3 +5,16 @@ export type PluginManagerSettings = {
   settings: () => HTMLElement
   render: () => HTMLElement
 }
+
+export type Profile = {
+  name: 'pluginManager',
+  displayName: 'Plugin manager',
+  methods: [],
+  events: [],
+  icon: 'assets/img/pluginManager.webp',
+  description: 'Start/stop services, modules and plugins',
+  kind: 'settings',
+  location: 'sidePanel',
+  documentation: 'https://remix-ide.readthedocs.io/en/latest/plugin_manager.html',
+  version: string
+}
