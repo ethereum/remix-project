ffb2e863c02b9555b943b6ae04009e54ff1d4958
joseph izang <joeizang@hotmail.com>
2021-07-01 13:03:47 +0100

    add listgroupitem component

--- libs/remix-ui/plugin-manager/src/lib/components/rootView.tsx
+++ libs/remix-ui/plugin-manager/src/lib/components/rootView.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
+import { Profile } from '../../customTypes'
 import ActiveTile from './activeTile'
+import ListGroupItem from './listGroupItem'
 
-interface RowViewProps {
+interface RootViewProps {
   localPluginButtonText: string
-
+  activeProfiles: Profile[]
+  inactiveProfiles?: Profile[]
+  filterPlugins: () => void
+  openLocalPlugins: () => void
+  renderItem: (profile: Profile) => void
 }
 
-function RootView () {
+function RootView ({
+  localPluginButtonText,
+  activeProfiles,
+  inactiveProfiles,
+  filterPlugins,
+  openLocalPlugins,
+  renderItem
+}: RootViewProps) {
   return (
     <div id="pluginManager" data-id="pluginManagerComponentPluginManager">
       <header className="form-group pluginSearch plugins-header py-3 px-4 border-bottom" data-id="pluginManagerComponentPluginManagerHeader">
         <input type="text" className="form-control" placeholder="Search" data-id="pluginManagerComponentSearchInput" />
-        <button className="btn btn-secondary text-dark border-0" data-id="pluginManagerComponentPluginSearchButton">Connect to a Local Plugin</button>
+        <button className="btn btn-secondary text-dark border-0" data-id="pluginManagerComponentPluginSearchButton">{localPluginButtonText}</button>
       </header>
       <section data-id="pluginManagerComponentPluginManagerSection">
         <ActiveTile />
-        <div className="list-group list-group-flush plugins-list-group" data-id="pluginManagerComponentActiveTile">
-
-        </div>
+        <ListGroupItem
+          activeProfiles={activeProfiles}
+          inactiveProfiles={inactiveProfiles}
+          renderItem={renderItem}
+        />
       </section>
     </div>
   )
--- libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
+++ libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
-
-import './remix-ui-plugin-manager.css';
+import './remix-ui-plugin-manager.css'
 
 /* eslint-disable-next-line */
 export interface RemixUiPluginManagerProps {
-  name: 'pluginManager',
-  displayName: 'Plugin manager',
-  methods: [],
-  events: [],
-  icon: 'assets/img/pluginManager.webp',
-  description: 'Start/stop services, modules and plugins',
-  kind: 'settings',
-  location: 'sidePanel',
-  documentation: 'https://remix-ide.readthedocs.io/en/latest/plugin_manager.html',
-  version: string
+
 }
 
 export const RemixUiPluginManager = (props: RemixUiPluginManagerProps) => {
@@ -21,7 +11,7 @@ export const RemixUiPluginManager = (props: RemixUiPluginManagerProps) => {
     <div>
       <h1>Welcome to remix-ui-plugin-manager!</h1>
     </div>
-  );
-};
+  )
+}
 
-export default RemixUiPluginManager;
+export default RemixUiPluginManager
