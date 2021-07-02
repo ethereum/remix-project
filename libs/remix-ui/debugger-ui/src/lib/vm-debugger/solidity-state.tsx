a8aaf5ce2176ce5f8f4f6ceb1d557de678f6a29e
filip mertens <filip.mertens@ethereum.org>
2021-07-02 15:00:11 +0200

    fix debugger crash

--- libs/remix-ui/debugger-ui/src/lib/vm-debugger/solidity-state.tsx
+++ libs/remix-ui/debugger-ui/src/lib/vm-debugger/solidity-state.tsx
@@ -5,32 +5,36 @@ import { ExtractData } from '../../types' // eslint-disable-line
 
 export const SolidityState = ({ calldata, message }) => {
   const formatSelf = (key: string, data: ExtractData) => {
-    let color = 'var(--primary)'
-    if (data.isArray || data.isStruct || data.isMapping) {
-      color = 'var(--info)'
-    } else if (
-      data.type.indexOf('uint') === 0 ||
+    try {
+      let color = 'var(--primary)'
+      if (data.isArray || data.isStruct || data.isMapping) {
+        color = 'var(--info)'
+      } else if (
+        data.type.indexOf('uint') === 0 ||
             data.type.indexOf('int') === 0 ||
             data.type.indexOf('bool') === 0 ||
             data.type.indexOf('enum') === 0
-    ) {
-      color = 'var(--green)'
-    } else if (data.type === 'string') {
-      color = 'var(--teal)'
+      ) {
+        color = 'var(--green)'
+      } else if (data.type === 'string') {
+        color = 'var(--teal)'
         } else if (data.self == 0x0) { // eslint-disable-line
-      color = 'var(--gray)'
+        color = 'var(--gray)'
+      }
+      return (
+        <label className='mb-0' style={{ color: data.isProperty ? 'var(--info)' : '', whiteSpace: 'pre-wrap' }}>
+          {' ' + key}:
+          <label className='mb-0' style={{ color }}>
+            {' ' + data.self}
+          </label>
+          <label style={{ fontStyle: 'italic' }}>
+            {data.isProperty || !data.type ? '' : ' ' + data.type}
+          </label>
+        </label>
+      )
+    } catch (e) {
+      return (<></>)
     }
-    return (
-      <label className='mb-0' style={{ color: data.isProperty ? 'var(--info)' : '', whiteSpace: 'pre-wrap' }}>
-        {' ' + key}:
-        <label className='mb-0' style={{ color }}>
-          {' ' + data.self}
-        </label>
-        <label style={{ fontStyle: 'italic' }}>
-          {data.isProperty || !data.type ? '' : ' ' + data.type}
-        </label>
-      </label>
-    )
   }
 
   return (
