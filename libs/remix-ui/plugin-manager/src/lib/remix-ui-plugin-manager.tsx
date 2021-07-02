8e25c52c5e5fb17ade9fa6fd63062591f364d838
joseph izang <joeizang@hotmail.com>
2021-07-02 11:55:05 +0100

    trying to import react component

--- libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
+++ libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Profile } from '../customTypes'
 import './remix-ui-plugin-manager.css'
 
 /* eslint-disable-next-line */
 export interface RemixUiPluginManagerProps {
-
+  activatePlugin: (name: string) => void
+  deActivatePlugin: (name: string) => void
+  isActive: () => void
+  openLocalPlugin: () => Promise<void>
+  filterPlugins: () => void
+  reRender: () => void
+  profile: Profile
 }
 
 export const RemixUiPluginManager = (props: RemixUiPluginManagerProps) => {
