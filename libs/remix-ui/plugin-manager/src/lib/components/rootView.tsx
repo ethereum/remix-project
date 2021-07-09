f16029edb57ebcec08b617bd8a9dcfd599ad36ff
joseph izang <joeizang@hotmail.com>
2021-07-09 13:43:40 +0100

    finish refactoring of visual implementation of local plugin form

--- libs/remix-ui/plugin-manager/src/lib/components/rootView.tsx
+++ libs/remix-ui/plugin-manager/src/lib/components/rootView.tsx
@@ -1,7 +1,10 @@
-import React, { Fragment, useContext } from 'react'
+import React, { FormEvent, Fragment, useContext, useState } from 'react'
 import { PluginManagerContext } from '../contexts/pluginmanagercontext'
 import ModuleHeading from './moduleHeading'
 import PluginCard from './pluginCard'
+import { ModalDialog } from '@remix-ui/modal-dialog'
+import LocalPluginForm from './localPluginForm'
+import { FormStateProps } from '../../types'
 
 interface RootViewProps {
   localPluginButtonText: string
@@ -9,33 +12,62 @@ interface RootViewProps {
 
 function RootView ({ localPluginButtonText }: RootViewProps) {
   const { actives, inactives } = useContext(PluginManagerContext)
+  const [visible, setVisible] = useState<boolean>(true)
+
+  const openModal = () => {
+    setVisible(false)
+  }
+
+  const closeModal = () => setVisible(true)
+
+  const handleSubmit = (evt: FormEvent, formData: FormStateProps) => {
+    console.log('Data submitted from the form!!!: ', formData)
+    evt.preventDefault()
+    closeModal()
+  }
+
   return (
-    <div id="pluginManager" data-id="pluginManagerComponentPluginManager">
-      <header className="form-group remixui_pluginSearch plugins-header py-3 px-4 border-bottom" data-id="pluginManagerComponentPluginManagerHeader">
-        <input type="text" className="form-control" placeholder="Search" data-id="pluginManagerComponentSearchInput" />
-        <button className="remixui_pluginSearchButton btn bg-transparent text-dark border-0 mt-2 text-underline" data-id="pluginManagerComponentPluginSearchButton">
+    <Fragment>
+      <ModalDialog
+        handleHide={closeModal}
+        hide={visible}
+        title="Local Plugin"
+        okLabel="OK"
+        cancelLabel="Cancel"
+        cancelFn={closeModal}
+      >
+        <LocalPluginForm
+          formSubmitHandler={handleSubmit}
+        />
+      </ModalDialog>
+
+      <div id="pluginManager" data-id="pluginManagerComponentPluginManager">
+        <header className="form-group remixui_pluginSearch plugins-header py-3 px-4 border-bottom" data-id="pluginManagerComponentPluginManagerHeader">
+          <input type="text" className="form-control" placeholder="Search" data-id="pluginManagerComponentSearchInput" />
+          <button onClick={openModal} className="remixui_pluginSearchButton btn bg-transparent text-dark border-0 mt-2 text-underline" data-id="pluginManagerComponentPluginSearchButton">
           Connect to a Local Plugin
-        </button>
-      </header>
-      <section data-id="pluginManagerComponentPluginManagerSection">
-        {actives.length === 0 ? (
-          <Fragment>
-            <ModuleHeading headingLabel="Active Modules"/>
-            {actives.map((profile) => (
-              <PluginCard profile={profile}/>
-            ))}
-          </Fragment>
-        ) : null }
-        {inactives.length === 0 ? (
-          <Fragment>
-            <ModuleHeading headingLabel="Inactive Modules"/>
-            {inactives.map((profile) => (
-              <PluginCard profile={profile}/>
-            ))}
-          </Fragment>
-        ) : null}
-      </section>
-    </div>
+          </button>
+        </header>
+        <section data-id="pluginManagerComponentPluginManagerSection">
+          {actives.length === 0 ? (
+            <Fragment>
+              <ModuleHeading headingLabel="Active Modules"/>
+              {actives.map((profile) => (
+                <PluginCard profile={profile}/>
+              ))}
+            </Fragment>
+          ) : null }
+          {inactives.length === 0 ? (
+            <Fragment>
+              <ModuleHeading headingLabel="Inactive Modules"/>
+              {inactives.map((profile) => (
+                <PluginCard profile={profile}/>
+              ))}
+            </Fragment>
+          ) : null}
+        </section>
+      </div>
+    </Fragment>
   )
 }
 
