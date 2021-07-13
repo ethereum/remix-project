a11beb3f619f7735912dfb52b1049b07bee1a49c
joseph izang <joeizang@hotmail.com>
2021-07-13 17:01:43 +0100

    add proposed changes to remixappManager class to expose encapsulated functionality

--- libs/remix-ui/plugin-manager/src/lib/components/button.tsx
+++ libs/remix-ui/plugin-manager/src/lib/components/button.tsx
@@ -3,17 +3,25 @@ import { PluginManagerContext } from '../contexts/pluginmanagercontext'
 
 interface ButtonProps {
   buttonText: 'Activate' | 'Deactivate'
-
+  pluginName: string
 }
 
-function Button ({ buttonText }: ButtonProps) {
-  const { profile, deActivatePlugin, activatePlugin } = useContext(PluginManagerContext)
-  const dataId = `pluginManagerComponentDeactivateButton${profile.name}`
+function Button ({ buttonText, pluginName }: ButtonProps) {
+  const { appManager, _paq } = useContext(PluginManagerContext)
+  const dataId = `pluginManagerComponentDeactivateButton${pluginName}`
   const [needToDeactivate] = useState('btn btn-secondary btn-sm')
   const [needToActivate] = useState('btn btn-success btn-sm')
   return (
     <button
-      onClick={buttonText === 'Activate' ? () => activatePlugin(profile.name) : () => deActivatePlugin(profile.name)}
+      onClick={buttonText === 'Activate' ? () => {
+        appManager.turnPluginOn(pluginName)
+        _paq.push(['trackEvent', 'manager', 'activate', pluginName])
+        buttonText = 'Deactivate'
+      } : () => {
+        appManager.turnPluginOff(pluginName)
+        _paq.push(['trackEvent', 'manager', 'deactivate', pluginName])
+        buttonText = 'Activate'
+      }}
       className={buttonText === 'Activate' ? needToActivate : needToDeactivate}
       data-id={dataId}
     >
