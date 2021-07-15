a865db43b323c2aca5f638e6eea430516a3d5ab1
joseph izang <joeizang@hotmail.com>
2021-07-15 10:24:09 +0100

    pass one large prop pluginComponent to maintain context

--- libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
+++ libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
@@ -5,7 +5,7 @@ import PluginManagerContextProvider from './contexts/pluginmanagercontext'
 import './remix-ui-plugin-manager.css'
 
 export const RemixUiPluginManager = (props: RemixUiPluginManagerProps) => {
-  console.log('current state of appmanager', props.appManager)
+  console.log('current state of appmanager', props.pluginComponent)
   console.log('The state of props ', props)
 
   return (
