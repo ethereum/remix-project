f3643f7a92a7438ccb4908ad66cd6c56329a784e
lianahus <liana@ethereum.org>
2021-07-16 14:48:42 +0200

    checking for isFile

--- apps/remix-ide/src/app/panels/tab-proxy.js
+++ apps/remix-ide/src/app/panels/tab-proxy.js
@@ -44,7 +44,8 @@ export class TabProxy extends Plugin {
 
     fileManager.events.on('fileRemoved', (name) => {
       const workspace = this.fileManager.currentWorkspace()
-
+      console.log('wotk= ', workspace)
+      console.log('mode= ', this.fileManager.mode)
       workspace ? this.removeTab(workspace + '/' + name) : this.removeTab(this.fileManager.mode + '/' + name)
     })
 
