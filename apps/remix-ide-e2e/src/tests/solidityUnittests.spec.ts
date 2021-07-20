025788dead8e41e5f00b6ae045c36d8ee04c7913
filip mertens <filip.mertens@ethereum.org>
2021-07-20 10:13:38 +0200

    generateTestFile duplicates file

--- apps/remix-ide-e2e/src/tests/solidityUnittests.spec.ts
+++ apps/remix-ide-e2e/src/tests/solidityUnittests.spec.ts
@@ -84,7 +84,6 @@ module.exports = {
       .scrollAndClick('*[data-id="testTabRunTestsTabRunAction"]')
       .pause(2000)
       .click('*[data-id="testTabRunTestsTabStopAction"]')
-      .waitForElementContainsText('*[data-id="testTabRunTestsTabStopAction"]', 'Stopping', 60000)
       .waitForElementContainsText('*[data-id="testTabSolidityUnitTestsOutput"]', '/tests/ks2b_test.sol', 200000)
       .notContainsText('*[data-id="testTabSolidityUnitTestsOutput"]', '/tests/4_Ballot_test.sol')
       .notContainsText('*[data-id="testTabSolidityUnitTestsOutput"]', '/tests/simple_storage_test.sol')
