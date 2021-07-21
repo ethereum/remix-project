e3c887f88c31ea3a09055a8648b3e532d27a487d
joseph izang <joeizang@hotmail.com>
2021-07-21 13:01:03 +0100

    Refactor prop requirements for pluginmanager

--- libs/remix-ui/plugin-manager/src/lib/components/ActivateButton.tsx
+++ libs/remix-ui/plugin-manager/src/lib/components/ActivateButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PluginManagerComponent } from '../../types'
 
 interface ActivateButtonProps {
@@ -12,7 +12,7 @@ function ActivateButton ({
   pluginName,
   pluginComponent
 }: ActivateButtonProps) {
-  const dataId = `pluginManagerComponent${buttonText}Button${pluginName}`
+  const [dataId] = useState(`pluginManagerComponent${buttonText}Button${pluginName}`)
 
   return (
     <button
