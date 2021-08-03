1582a43939b77451e0ccc1134207693debb3ed9e
joseph izang <joeizang@hotmail.com>
2021-08-03 17:04:57 +0100

    fix all but 2 issues to have 100% passing tests

---
+++ libs/remix-ui/plugin-manager/src/lib/components/ActivePluginCardContainer.tsx
@@ -0,0 +1,48 @@
+import { Profile } from '@remixproject/plugin-utils'
+import React, { Fragment, useEffect, useState } from 'react'
+import { PluginManagerComponent } from '../../types'
+import ActivePluginCard from './ActivePluginCard'
+import ModuleHeading from './moduleHeading'
+
+interface ActivePluginCardContainerProps {
+  pluginComponent: PluginManagerComponent
+}
+function ActivePluginCardContainer ({ pluginComponent }: ActivePluginCardContainerProps) {
+  const [activeProfiles, setActiveProfiles] = useState<Profile[]>()
+  const [inactiveProfiles, setinactiveProfiles] = useState<Profile[]>([])
+  const deactivatePlugin = (pluginName: string) => {
+    pluginComponent.deactivateP(pluginName)
+  }
+
+  useEffect(() => {
+    const savedActiveProfiles = JSON.parse(localStorage.getItem('newActivePlugins'))
+    if (savedActiveProfiles === null) {
+      localStorage.setItem('newActivePlugins', '[]')
+    }
+    if (pluginComponent.activePlugins && pluginComponent.activePlugins.length > 0) {
+      setActiveProfiles(pluginComponent.activePlugins)
+    } else if (savedActiveProfiles && savedActiveProfiles.length > 0 && pluginComponent.activePlugins.length === 0) {
+      setActiveProfiles(savedActiveProfiles)
+    }
+  }, [pluginComponent, pluginComponent.activePlugins])
+  return (
+    <Fragment>
+      {(activeProfiles && activeProfiles.length) ? <ModuleHeading headingLabel="Active Modules" count={activeProfiles.length} /> : null}
+      {activeProfiles && activeProfiles.map(profile => (
+        <ActivePluginCard
+          buttonText="Deactivate"
+          profile={profile}
+          deactivatePlugin={deactivatePlugin}
+          key={profile.name}
+          setInactivePlugins={setinactiveProfiles}
+          inactivePlugins={inactiveProfiles}
+          activePlugins={activeProfiles}
+          setActivePlugins={setActiveProfiles}
+        />
+      ))
+      }
+    </Fragment>
+  )
+}
+
+export default ActivePluginCardContainer
--- libs/remix-ui/plugin-manager/src/pluginManagerStateMachine.ts
+++ libs/remix-ui/plugin-manager/src/pluginManagerStateMachine.ts
@@ -1,7 +1,7 @@
 import { Profile } from '@remixproject/plugin-utils'
 import { PluginManagerComponent } from './types'
 
-const defaultActivatedPlugins = [
+export const defaultActivatedPlugins = [
   'manager',
   'contentImport',
   'theme',
@@ -27,111 +27,75 @@ const defaultActivatedPlugins = [
   'udapp'
 ]
 
-/**
- * Compares default enabled plugins of remix ide
- * and tracks newly activated plugins and manages
- * their state by persisting in local storage
- * @param newPlugin Profile of a Plugin
- * @param pluginComponent PluginManagerComponent Instance
- */
-export async function PersistActivatedPlugin (pluginComponent: PluginManagerComponent, newPlugin: Profile) {
-  const persisted = localStorage.getItem('newActivePlugins')
-  const activatedPlugins: Profile[] = JSON.parse(persisted)
+// /**
+//  * Compares default enabled plugins of remix ide
+//  * and tracks newly activated plugins and manages
+//  * their state by persisting in local storage
+//  * @param newPlugin Profile of a Plugin
+//  * @param pluginComponent PluginManagerComponent Instance
+//  */
+// export async function PersistActivatedPlugin (pluginComponent: PluginManagerComponent, newPlugin: Profile) {
+//   const persisted = localStorage.getItem('newActivePlugins')
+//   const activatedPlugins: Profile[] = JSON.parse(persisted)
 
-  const newlyActivatedPlugins: Array<Profile> = []
-  const defaultActivated = defaultActivatedPlugins.includes(newPlugin.name) === false
-  if (newPlugin) {
-    if (defaultActivated) {
-      // if this is true then we are sure that the profile name is in localStorage/workspace
-      if (activatedPlugins && activatedPlugins.length && !activatedPlugins.includes(newPlugin)) {
-        await FetchAndPersistPlugin(pluginComponent, newPlugin, activatedPlugins)
-        localStorage.setItem('newActivePlugins', JSON.stringify(activatedPlugins))
-      } else {
-        await FetchAndPersistPlugin(pluginComponent, newPlugin, newlyActivatedPlugins)
-        localStorage.setItem('newActivePlugins', JSON.stringify(newlyActivatedPlugins))
-      }
-    }
-  }
-}
-
-/**
- * Update the list of inactive plugin profiles filtering based on a predetermined
- * filter pipeline
- * @param deactivatedPlugin plugin profile to be deactivated
- * @param pluginComponent Plugin manager class which is the context for all operations
- * @returns {Array} array of inactives
- */
-export async function UpdateInactivePluginList (deactivatedPlugin: Profile, pluginComponent: PluginManagerComponent) {
-  const activated: Profile[] = JSON.parse(window.localStorage.getItem('newActivePlugins'))
-  const isFiltered = (profile) => (profile.displayName ? profile.displayName : profile.name)
-    .toLowerCase().includes(deactivatedPlugin.name)
-  const isNotActivated = (profile) => activated.includes(profile)
-  const isNotRequired = (profile) => !pluginComponent.appManager.isRequired(profile.name)
-  const isNotDependent = (profile) => !pluginComponent.appManager.isDependent(profile.name)
-  const isNotHome = (profile) => profile.name !== 'home'
-  const sortByName = (profileA, profileB) => {
-    const nameA = ((profileA.displayName) ? profileA.displayName : profileA.name).toUpperCase()
-    const nameB = ((profileB.displayName) ? profileB.displayName : profileB.name).toUpperCase()
-    return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0
-  }
-  const tempArray = pluginComponent.appManager.getAll()
-    .filter(isFiltered)
-    .filter(isNotActivated)
-    .filter(isNotRequired)
-    .filter(isNotDependent)
-    .filter(isNotHome)
-    .sort(sortByName)
-  return tempArray
-}
-
-// export function GetNewlyActivatedPlugins (pluginComponent: PluginManagerComponent) {
-//   const profiles: Profile[] = JSON.parse(window.localStorage.getItem('newActivePlugins'))
-//   let isValid: boolean = false
-//   // eslint-disable-next-line no-debugger
-//   debugger
-//   pluginComponent.activeProfiles.forEach(profileName => {
-//     isValid = profiles.some(profile => profile.name === profileName)
-//   })
-//   if (isValid) {
-//     return profiles
-//   } else {
-//     profiles.forEach(profile => {
-//       if (!pluginComponent.activeProfiles.includes(profile.name)) {
-//         RemoveActivatedPlugin(profile.name)
+//   const newlyActivatedPlugins: Array<Profile> = []
+//   const defaultActivated = defaultActivatedPlugins.includes(newPlugin.name) === false
+//   if (newPlugin) {
+//     if (defaultActivated) {
+//       // if this is true then we are sure that the profile name is in localStorage/workspace
+//       if (activatedPlugins && activatedPlugins.length && !activatedPlugins.includes(newPlugin)) {
+//         await FetchAndPersistPlugin(pluginComponent, newPlugin, activatedPlugins)
+//         localStorage.setItem('newActivePlugins', JSON.stringify(activatedPlugins))
+//       } else {
+//         await FetchAndPersistPlugin(pluginComponent, newPlugin, newlyActivatedPlugins)
+//         localStorage.setItem('newActivePlugins', JSON.stringify(newlyActivatedPlugins))
 //       }
-//     })
-//     const newProfiles = JSON.parse(window.localStorage.getItem('newActivePlugins'))
-//     return newProfiles
+//     }
 //   }
 // }
 
-async function FetchAndPersistPlugin (pluginComponent: PluginManagerComponent, newPlugin: Profile<any>, newlyActivatedPlugins: Profile<any>[]) {
-  try {
-    const targetProfile = await pluginComponent.appManager.getProfile(newPlugin.name)
-    if (targetProfile !== null || targetProfile !== undefined) newlyActivatedPlugins.push(targetProfile)
-  } catch {
-    throw new Error('Could not fetch and persist target profile!')
-  }
+export function populateActivePlugins (pluginComponent: PluginManagerComponent) {
+  const activePluginNames = pluginComponent.activeProfiles
+  const filteredNames = []
+  const workspace: string[] = JSON.parse(localStorage.getItem('workspace'))
+  defaultActivatedPlugins.forEach(defaultName => {
+    if (workspace.includes(defaultName) === false) {
+      filteredNames.push(defaultName)
+    }
+  })
+  // filteredNames = activePluginNames.concat(defaultActivatedPlugins)
+  // const newArray = [...new Set(filteredNames)]
+  console.log('Here are the plugin names that should be shown!!', filteredNames)
+  // console.log('Here are the distinct profile names!!', newArray)
 }
 
-/**
- * Remove a deactivated plugin from persistence (localStorage)
- * @param pluginName Name of plugin profile to be removed
- */
-export function RemoveActivatedPlugin (pluginName: string) {
-  const getWorkspacePlugins = JSON.parse(localStorage.getItem('newActivePlugins'))
-  const removeExisting = getWorkspacePlugins.filter(target => target.name !== pluginName)
-  localStorage.setItem('newActivePlugins', JSON.stringify(removeExisting))
-}
+// async function FetchAndPersistPlugin (pluginComponent: PluginManagerComponent, newPlugin: Profile<any>, newlyActivatedPlugins: Profile<any>[]) {
+//   try {
+//     const targetProfile = await pluginComponent.appManager.getProfile(newPlugin.name)
+//     if (targetProfile !== null || targetProfile !== undefined) newlyActivatedPlugins.push(targetProfile)
+//   } catch {
+//     throw new Error('Could not fetch and persist target profile!')
+//   }
+// }
 
-/**
- * Gets the latest list of inactive plugin profiles and persist them
- * in local storage
- * @param inactivesList Array of inactive plugin profiles
- * @returns {void}
- */
-export function PersistNewInactivesState (inactivesList: Profile[]) {
-  if (inactivesList && inactivesList.length) {
-    localStorage.setItem('updatedInactives', JSON.stringify(inactivesList))
-  }
-}
+// /**
+//  * Remove a deactivated plugin from persistence (localStorage)
+//  * @param pluginName Name of plugin profile to be removed
+//  */
+// export function RemoveActivatedPlugin (pluginName: string) {
+//   const getWorkspacePlugins = JSON.parse(localStorage.getItem('newActivePlugins'))
+//   const removeExisting = getWorkspacePlugins.filter(target => target.name !== pluginName)
+//   localStorage.setItem('newActivePlugins', JSON.stringify(removeExisting))
+// }
+
+// /**
+//  * Gets the latest list of inactive plugin profiles and persist them
+//  * in local storage
+//  * @param inactivesList Array of inactive plugin profiles
+//  * @returns {void}
+//  */
+// export function PersistNewInactivesState (inactivesList: Profile[]) {
+//   if (inactivesList && inactivesList.length) {
+//     localStorage.setItem('updatedInactives', JSON.stringify(inactivesList))
+//   }
+// }
