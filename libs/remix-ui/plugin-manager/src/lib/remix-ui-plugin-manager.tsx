fe42fd99859de71f01706748633ad7cffc55c05b
joseph izang <joeizang@hotmail.com>
2021-08-05 16:50:51 +0100

    trying manual plumbing for localplugin creation

--- libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
+++ libs/remix-ui/plugin-manager/src/lib/remix-ui-plugin-manager.tsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import { PluginManagerComponent, RemixUiPluginManagerProps } from '../types'
+import { RemixUiPluginManagerProps } from '../types'
 import ActivePluginCardContainer from './components/ActivePluginCardContainer'
 import InactivePluginCardContainer from './components/InactivePluginCardContainer'
 import RootView from './components/rootView'
 import './remix-ui-plugin-manager.css'
-
-export function getSolidity (pluginComponent: PluginManagerComponent) {
-  const fetchSolidity = async () => {
-    const solidity = await pluginComponent.appManager.getProfile('solidity')
-    const solidityLogic = await pluginComponent.appManager.getProfile('solidity-logic')
-    return [solidity, solidityLogic]
-  }
-  const materializeFetch = fetchSolidity()
-  return materializeFetch
-}
-
-export function getWorkspacePluginNames () {
-  const workspace: string[] = JSON.parse(localStorage.getItem('workspace'))
-  return workspace
-}
 
 export const RemixUiPluginManager = ({ pluginComponent }: RemixUiPluginManagerProps) => {
   return (
