f814fe7fec6f6e9ce638cb9de12f4f75b9e139e4
joseph izang <joeizang@hotmail.com>
2021-08-09 13:07:24 +0100

    fixes for localplugin toast

---
+++ libs/remix-ui/plugin-manager/src/pluginManagerReducer.ts
@@ -0,0 +1,12 @@
+
+export type localPluginReducerActionType = {
+  type: 'show' | 'close',
+  payload?: any
+}
+
+export function localPluginToastReducer (currentState: string, toastAction: localPluginReducerActionType) {
+  switch (toastAction.type) {
+    case 'show':
+      return `Cannot create Plugin : ${toastAction.payload!}`
+  }
+}
