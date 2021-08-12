a5a8888a9e544b869b52bb36436f295cf4acaa32
yann300 <yann.levreau@gmail.com>
2021-08-12 10:49:39 +0200

    make sure hardhat can be activated when using remixd

--- apps/remix-ide/src/app/tabs/compile-tab.js
+++ apps/remix-ide/src/app/tabs/compile-tab.js
@@ -108,13 +108,21 @@ class CompileTab extends ViewPlugin {
       this.call('editor', 'clearAnnotations')
     }
 
-    this.on('filePanel', 'setWorkspace', (workspace) => {
+    const resetView = (isLocalhost) => {
       this.compileTabLogic.isHardhatProject().then((result) => {
-        if (result && workspace.isLocalhost) this.isHardHatProject = true
+        if (result && isLocalhost) this.isHardHatProject = true
         else this.isHardHatProject = false
         this.renderComponent()
       })
       this.resetResults()
+    }
+
+    this.on('filePanel', 'setWorkspace', (workspace) => {
+      resetView(workspace.isLocalhost)
+    })
+
+    this.on('remixd', 'rootFolderChanged', () => {
+      resetView(true)
     })
 
     this.compileTabLogic.event.on('startingCompilation', this.data.eventHandlers.onStartingCompilation)
