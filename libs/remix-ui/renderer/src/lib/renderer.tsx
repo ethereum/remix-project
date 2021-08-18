5c7f13c6a8bccf7e2992545e206aa08d56da72da
lianahus <liana@ethereum.org>
2021-08-18 20:07:19 +0200

    fixed the error parsing

--- libs/remix-ui/renderer/src/lib/renderer.tsx
+++ libs/remix-ui/renderer/src/lib/renderer.tsx
@@ -29,15 +29,9 @@ export const Renderer = ({ message, opt = {}, plugin }: RendererProps) => {
     // ^ e.g:
     // browser/gm.sol: Warning: Source file does not specify required compiler version! Consider adding "pragma solidity ^0.6.12
     // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.2.0/contracts/introspection/IERC1820Registry.sol:3:1: ParserError: Source file requires different compiler version (current compiler is 0.7.4+commit.3f05b770.Emscripten.clang) - note that nightly builds are considered to be strictly less than the released version
-    let positionDetails = getPositionDetails(text)
+    const positionDetails = getPositionDetails(text)
     const options = opt
 
-    if (!positionDetails.errFile || (opt.errorType && opt.errorType === positionDetails.errFile)) {
-      // Updated error reported includes '-->' before file details
-      const errorDetails = text.split('-->')
-      // errorDetails[1] will have file details
-      if (errorDetails.length > 1) positionDetails = getPositionDetails(errorDetails[1])
-    }
     options.errLine = positionDetails.errLine
     options.errCol = positionDetails.errCol
     options.errFile = positionDetails.errFile.trim()
@@ -61,6 +55,8 @@ export const Renderer = ({ message, opt = {}, plugin }: RendererProps) => {
 
     // To handle some compiler warning without location like SPDX license warning etc
     if (!msg.includes(':')) return { errLine: -1, errCol: -1, errFile: msg }
+
+    msg = msg.split('-->')[1].trim()
 
     // extract line / column
     let pos = msg.match(/^(.*?):([0-9]*?):([0-9]*?)?/)
