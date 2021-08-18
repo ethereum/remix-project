b9d36b4997868dc84e40acc6d7cbf0aaae2b860a
aniket-engg <aniketengg.225@gmail.com>
2021-08-18 19:09:51 +0530

    unit tests for hhLogs

--- libs/remix-tests/tests/examples_0/assert_ok_test.sol
+++ libs/remix-tests/tests/examples_0/assert_ok_test.sol
@@ -1,12 +1,17 @@
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 
+
+import "./hardhat/console.sol";
+
 contract AssertOkTest {
 
     function okPassTest() public {
+        console.log("AssertOkTest", "okPassTest");
         Assert.ok(true, "okPassTest passes");
     }
 
     function okFailTest() public {
+        console.log("AssertOkTest", "okFailTest");
         Assert.ok(false, "okFailTest fails");
     }
 }
