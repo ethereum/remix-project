95e89e7b73bc5f577fdf746121f18024ea5f68f3
yann300 <yann.levreau@gmail.com>
2021-08-19 15:34:43 +0200

    ensure API is public

--- apps/solidity-compiler/src/app/compiler.ts
+++ apps/solidity-compiler/src/app/compiler.ts
@@ -2,6 +2,19 @@ import { PluginClient } from "@remixproject/plugin";
 import { createClient } from "@remixproject/plugin-webview";
 import { CompilerApiMixin } from './compiler-api'
 
+const profile = {
+  name: 'solidity',
+  displayName: 'Solidity compiler',
+  icon: 'assets/img/solidity.webp',
+  description: 'Compile solidity contracts',
+  kind: 'compiler',
+  permission: true,
+  location: 'sidePanel',
+  documentation: 'https://remix-ide.readthedocs.io/en/latest/solidity_editor.html',
+  version: '0.0.1',
+  methods: ['getCompilationResult', 'compile', 'compileWithParameters', 'setCompilerConfig', 'compileFile']
+}
+
 export interface ConfigurationSettings {
   version: string,
   evmVersion: string,
