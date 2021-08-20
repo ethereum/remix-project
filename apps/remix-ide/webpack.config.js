90e378e65ac9ae5fd7937835c00b8cfaa0ce1111
ioedeveloper <daviddisu8@gmail.com>
2021-08-20 16:00:22 +0100

    Enable react production build

--- apps/remix-ide/webpack.config.js
+++ apps/remix-ide/webpack.config.js
@@ -1,17 +1,27 @@
 const nxWebpack = require('@nrwl/react/plugins/webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = config => {
-    const nxWebpackConfig = nxWebpack(config)
-
-    return {
-        ...nxWebpackConfig,
-        node: {
-            fs: 'empty',
-            tls: 'empty',
-            readline: 'empty',
-            net: 'empty',
-            module: 'empty',
-            child_process: 'empty'
-        }
+  const nxWebpackConfig = nxWebpack(config)
+  const productionConfig = process.env.NODE_ENV === 'production' ? {
+    mode: 'production',
+    devtool: 'source-map',
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()]
     }
+  } : {}
+
+  return {
+    ...nxWebpackConfig,
+    ...productionConfig,
+    node: {
+      fs: 'empty',
+      tls: 'empty',
+      readline: 'empty',
+      net: 'empty',
+      module: 'empty',
+      child_process: 'empty'
+    }
+  }
 }
