587969bd8e97fb04c90c42ad38db09135b656bb2
ioedeveloper <daviddisu8@gmail.com>
2021-08-20 17:22:46 +0100

    Setup localhost events

--- libs/remix-ui/workspace/src/lib/contexts/index.ts
+++ libs/remix-ui/workspace/src/lib/contexts/index.ts
@@ -5,6 +5,5 @@ export const FileSystemContext = createContext<{
   fs: BrowserState,
   modal:(title: string, message: string | JSX.Element, okLabel: string, okFn: () => void, cancelLabel?: string, cancelFn?: () => void) => void,
   dispatchInitWorkspace:() => void,
-  dispatchInitLocalhost:() => void,
   dispatchFetchDirectory:(path: string) => void
     }>(null)
