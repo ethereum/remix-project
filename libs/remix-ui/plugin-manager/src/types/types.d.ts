069c84219041fc08f5435b0781b8e88be0ef2b7c
joseph izang <joeizang@hotmail.com>
2021-08-20 13:46:27 +0100

    changes based on comments from @ioedeveloper

--- libs/remix-ui/plugin-manager/src/types/types.d.ts
+++ libs/remix-ui/plugin-manager/src/types/types.d.ts
@@ -5,23 +5,6 @@ import { Engine } from '@remixproject/engine/lib/engine'
 import { PluginBase, Profile } from '@remixproject/plugin-utils'
 import { IframePlugin, ViewPlugin, WebsocketPlugin } from '@remixproject/engine-web'
 /* eslint-disable camelcase */
-declare module 'yo-yo'{
-  interface yo_yo {
-    (strings:string[], ...values:any[]):HTMLElement;
-    update(element:HTMLElement, element2:HTMLElement);
-  }
-  var yo:yo_yo
-  export = yo;
-}
-
-declare module 'dom-css'{
- interface dom_css{
-   (element:HTMLElement, css:any):void;
- }
-
- var css:dom_css
- export = css;
-}
 
 interface SetPluginOptionType {
   queueTimeout: number
