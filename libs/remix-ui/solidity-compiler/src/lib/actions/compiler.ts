70c682eb3a89e640b6a626c07949fc28d37ed874
yann300 <yann.levreau@gmail.com>
2021-08-23 18:36:19 +0200

    fix iframe plugin api

--- libs/remix-ui/solidity-compiler/src/lib/actions/compiler.ts
+++ libs/remix-ui/solidity-compiler/src/lib/actions/compiler.ts
@@ -26,7 +26,7 @@ export const resetCompilerMode = () => (dispatch: React.Dispatch<any>) => {
   })
 }
 
-export const listenToEvents = (compileTabLogic: CompileTabLogic, api) => (dispatch: React.Dispatch<any>) => {
+export const listenToEvents = (compileTabLogic, api) => (dispatch: React.Dispatch<any>) => {
   api.onSessionSwitched = () => {
     dispatch(setEditorMode('sessionSwitched'))
   }
