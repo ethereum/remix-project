2ceb41d88b844d3cef73854a1ee451a582f373a2
yann300 <yann.levreau@gmail.com>
2021-08-23 18:36:19 +0200

    fix iframe plugin api

--- libs/remix-ui/solidity-compiler/src/lib/types/index.ts
+++ libs/remix-ui/solidity-compiler/src/lib/types/index.ts
@@ -1,27 +1,41 @@
 export type onCurrentFileChanged = (fileName: string) => void
 
 export interface SolidityCompilerProps {
-  plugin: {
-    currentFile: string
-    contractMap: {
-      file: string
-    } | Record<string, any>
-    compileErrors: any,
-    compileTabLogic: any,
-    contractsDetails: Record<string, any>,
-    contentImport: any,
-    call: (...args) => void
-    on: (...args) => void,
-    setSelectedVersion: (value: string) => void,
-    configurationSettings: ConfigurationSettings,
-    getConfiguration: (value: string) => string,
-    setConfiguration: (name: string, value: string) => void,
-    onCurrentFileChanged: (fileName: string) => void,
-    onResetResults: () => void,
-    onSetWorkspace: (workspace: any) => void,
-    onNoFileSelected: () => void,
-    onCompilationFinished: (contractsDetails: any, contractMap: any) => void
-  },
+  plugin: ICompilerApi
+}
+
+export interface ICompilerApi {
+  currentFile: string
+  contractMap: {
+    file: string
+  } | Record<string, any>
+  compileErrors: any
+  compileTabLogic: any
+  contractsDetails: Record<string, any>
+  configurationSettings: ConfigurationSettings
+
+  setHardHatCompilation: (value: boolean) => void
+  getParameters: () => any
+  setParameters: (params) => void
+  getConfiguration: (value: string) => string
+  setConfiguration: (name: string, value: string) => void
+  getFileManagerMode: () => string
+  setCompilerConfig: (settings: any) => void
+
+  getCompilationResult: () => any
+
+  onCurrentFileChanged: (fileName: string) => void
+  onResetResults: () => void,
+  onSetWorkspace: (isLocalhost: boolean) => void
+  onNoFileSelected: () => void
+  onCompilationFinished: (contractsDetails: any, contractMap: any) => void
+  onSessionSwitched: () => void
+  onContentChanged: () => void
+
+  fileExists: (file: string) => Promise<boolean>
+  writeFile: (file: string, content: string) => Promise<void>
+  readFile: (file: string) => Promise<string>
+  open: (file: string) => void
 }
 
 export interface CompilerContainerProps {
