8728f1f992f5564dcbe941dd51b646401dea057a
aniket-engg <aniketengg.225@gmail.com>
2021-08-24 12:15:16 +0530

    activate blockchain plugin

--- apps/remix-ide/src/app.js
+++ apps/remix-ide/src/app.js
@@ -482,7 +482,7 @@ Please make a backup of your contracts and start using http://remix.ethereum.org
   await appManager.activatePlugin(['sidePanel']) // activating  host plugin separately
   await appManager.activatePlugin(['home'])
   await appManager.activatePlugin(['settings'])
-  await appManager.activatePlugin(['hiddenPanel', 'filePanel', 'pluginManager', 'contextualListener', 'terminal', 'fetchAndCompile', 'contentImport'])
+  await appManager.activatePlugin(['hiddenPanel', 'filePanel', 'pluginManager', 'contextualListener', 'terminal', 'blockchain', 'fetchAndCompile', 'contentImport'])
   await appManager.registerContextMenuItems()
   // Set workspace after initial activation
   if (Array.isArray(workspace)) {
