9988cdfeb4e550927984b3b1a37e9329b09ed1be
davidzagi93@gmail.com <davidzagi93@gmail.com>
2021-08-28 14:37:33 +0100

    adding a pause to Deploy n and use Ballot with external web3

--- apps/remix-ide-e2e/src/tests/ballot.test.ts
+++ apps/remix-ide-e2e/src/tests/ballot.test.ts
@@ -91,6 +91,7 @@ module.exports = {
       .clickInstance(0)
       .click('*[data-id="terminalClearConsole"]')
       .clickFunction('delegate - transact (not payable)', { types: 'address to', values: '0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c' })
+      .pause(5000)
       .journalLastChildIncludes('Ballot.delegate(address)')
       .journalLastChildIncludes('data: 0x5c1...a733c')
   },
