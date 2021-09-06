711e37aca54f79c9ee53c860801e0de78a1535e5
Joseph Izang <joeizang@hotmail.com>
2021-09-06 20:08:41 +0100

    scaffold vertical-icons react library

---
+++ libs/remix-ui/vertical-icons/src/lib/remix-ui-vertical-icons.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './remix-ui-vertical-icons.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiVerticalIconsProps {}
+
+export const RemixUiVerticalIcons = (props: RemixUiVerticalIconsProps) => {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-vertical-icons!</h1>
+    </div>
+  );
+};
+
+export default RemixUiVerticalIcons;
