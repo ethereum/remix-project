a8f64d85e7acd0b17a4952fb90e30dd40ae33604
yann300 <yann.levreau@gmail.com>
2021-09-07 14:45:54 +0200

    refactor ICompilerAPI

--- libs/remix-ui/solidity-compiler/src/lib/solidity-compiler.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/solidity-compiler.tsx
@@ -9,7 +9,7 @@ import { Renderer } from '@remix-ui/renderer' // eslint-disable-line
 import './css/style.css'
 
 export const SolidityCompiler = (props: SolidityCompilerProps) => {
-  const { plugin, plugin: { currentFile, compileTabLogic, contractsDetails, contractMap, compileErrors, configurationSettings } } = props
+  const { api, api: { currentFile, compileTabLogic, contractsDetails, contractMap, compileErrors, configurationSettings } } = props
   const [state, setState] = useState({
     isHardhatProject: false,
     currentFile,
@@ -32,32 +32,32 @@ export const SolidityCompiler = (props: SolidityCompilerProps) => {
   })
   const [currentVersion, setCurrentVersion] = useState('')
 
-  plugin.onCurrentFileChanged = (currentFile: string) => {
+  api.onCurrentFileChanged = (currentFile: string) => {
     setState(prevState => {
       return { ...prevState, currentFile }
     })
   }
 
-  plugin.onResetResults = () => {
+  api.onResetResults = () => {
     setState(prevState => {
       return { ...prevState, currentFile: '', contractsDetails: {}, contractMap: {} }
     })
   }
 
-  plugin.onSetWorkspace = async (isLocalhost: boolean) => {
+  api.onSetWorkspace = async (isLocalhost: boolean) => {
     const isHardhat = isLocalhost && await compileTabLogic.isHardhatProject()
     setState(prevState => {
       return { ...prevState, currentFile, isHardhatProject: isHardhat }
     })
   }
 
-  plugin.onNoFileSelected = () => {
+  api.onNoFileSelected = () => {
     setState(prevState => {
       return { ...prevState, currentFile: '' }
     })
   }
 
-  plugin.onCompilationFinished = (contractsDetails: any, contractMap: any) => {
+  api.onCompilationFinished = (contractsDetails: any, contractMap: any) => {
     setState(prevState => {
       return { ...prevState, contractsDetails, contractMap }
     })
@@ -71,7 +71,7 @@ export const SolidityCompiler = (props: SolidityCompilerProps) => {
 
   const updateCurrentVersion = (value) => {
     setCurrentVersion(value)
-    plugin.setParameters({ version: value })
+    api.setCompilerParameters({ version: value })
   }
 
   const modal = async (title: string, message: string | JSX.Element, okLabel: string, okFn: () => void, cancelLabel?: string, cancelFn?: () => void) => {
@@ -111,19 +111,19 @@ export const SolidityCompiler = (props: SolidityCompilerProps) => {
   return (
     <>
       <div id="compileTabView">
-        <CompilerContainer api={plugin} isHardhatProject={state.isHardhatProject} compileTabLogic={compileTabLogic} tooltip={toast} modal={modal} compiledFileName={currentFile} updateCurrentVersion={updateCurrentVersion} configurationSettings={configurationSettings} />
-        <ContractSelection api={plugin} contractMap={contractMap} contractsDetails={contractsDetails} modal={modal} />
+        <CompilerContainer api={api} isHardhatProject={state.isHardhatProject} compileTabLogic={compileTabLogic} tooltip={toast} modal={modal} compiledFileName={currentFile} updateCurrentVersion={updateCurrentVersion} configurationSettings={configurationSettings} />
+        <ContractSelection api={api} contractMap={contractMap} contractsDetails={contractsDetails} modal={modal} />
         <div className="remixui_errorBlobs p-4" data-id="compiledErrors">
           <span data-id={`compilationFinishedWith_${currentVersion}`}></span>
-          { compileErrors.error && <Renderer message={compileErrors.error.formattedMessage || compileErrors.error} plugin={plugin} opt={{ type: compileErrors.error.severity || 'error', errorType: compileErrors.error.type }} /> }
+          { compileErrors.error && <Renderer message={compileErrors.error.formattedMessage || compileErrors.error} plugin={api} opt={{ type: compileErrors.error.severity || 'error', errorType: compileErrors.error.type }} /> }
           { compileErrors.error && (compileErrors.error.mode === 'panic') && modal('Error', panicMessage(compileErrors.error.formattedMessage), 'Close', null) }
           { compileErrors.errors && compileErrors.errors.length && compileErrors.errors.map((err, index) => {
-            if (plugin.getConfiguration('hideWarnings')) {
+            if (api.getAppParameter('hideWarnings')) {
               if (err.severity !== 'warning') {
-                return <Renderer key={index} message={err.formattedMessage} plugin={plugin} opt={{ type: err.severity, errorType: err.type }} />
+                return <Renderer key={index} message={err.formattedMessage} plugin={api} opt={{ type: err.severity, errorType: err.type }} />
               }
             } else {
-              return <Renderer key={index} message={err.formattedMessage} plugin={plugin} opt={{ type: err.severity, errorType: err.type }} />
+              return <Renderer key={index} message={err.formattedMessage} plugin={api} opt={{ type: err.severity, errorType: err.type }} />
             }
           }) }
         </div>
