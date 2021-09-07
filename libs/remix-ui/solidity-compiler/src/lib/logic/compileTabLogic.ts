ece00bd39401e2c55ac7618a9b65ccf33d9a0b55
yann300 <yann.levreau@gmail.com>
2021-09-07 14:45:54 +0200

    refactor ICompilerAPI

--- libs/remix-ui/solidity-compiler/src/lib/logic/compileTabLogic.ts
+++ libs/remix-ui/solidity-compiler/src/lib/logic/compileTabLogic.ts
@@ -1,3 +1,5 @@
+import { ICompilerApi } from '@remix-project/remix-lib-ts'
+
 const Compiler = require('@remix-project/remix-solidity').Compiler
 const EventEmitter = require('events')
 
@@ -8,7 +10,7 @@ declare global {
 }
 const _paq = (window._paq = window._paq || []); //eslint-disable-line
 
-export class CompileTab {
+export class CompileTabLogic {
   public compiler
   public optimize
   public runs
@@ -16,49 +18,45 @@ export class CompileTab {
   public compilerImport
   public event
 
-  constructor (public api, public contentImport) {
+  constructor (public api: ICompilerApi, public contentImport) {
     this.event = new EventEmitter()
     this.compiler = new Compiler((url, cb) => api.resolveContentAndSave(url).then((result) => cb(null, result)).catch((error) => cb(error.message)))
   }
 
-  init() {
-    this.optimize = this.api.getCompilerParameters().optimize;
-    this.api.setCompilerParameters({ optimize: this.optimize });
-    this.compiler.set('optimize', this.optimize);
+  init () {
+    this.optimize = this.api.getCompilerParameters().optimize
+    this.api.setCompilerParameters({ optimize: this.optimize })
+    this.compiler.set('optimize', this.optimize)
 
-    this.runs = this.api.getCompilerParameters().runs;
-    this.runs = this.runs && this.runs !== 'undefined' ? this.runs : 200;
-    this.api.setCompilerParameters({ runs: this.runs });
-    this.compiler.set('runs', this.runs);
+    this.runs = this.api.getCompilerParameters().runs
+    this.runs = this.runs && this.runs !== 'undefined' ? this.runs : 200
+    this.api.setCompilerParameters({ runs: this.runs })
+    this.compiler.set('runs', this.runs)
 
-    this.evmVersion = this.api.getCompilerParameters().evmVersion;
-    if (
-      this.evmVersion === 'undefined' ||
-      this.evmVersion === 'null' ||
-      !this.evmVersion
-    ) {
-      this.evmVersion = null;
+    this.evmVersion = this.api.getCompilerParameters().evmVersion
+    if (this.evmVersion === 'undefined' || this.evmVersion === 'null' || !this.evmVersion) {
+      this.evmVersion = null
     }
-    this.api.setCompilerParameters({ evmVersion: this.evmVersion });
-    this.compiler.set('evmVersion', this.evmVersion);
+    this.api.setCompilerParameters({ evmVersion: this.evmVersion })
+    this.compiler.set('evmVersion', this.evmVersion)
   }
 
-  setOptimize(newOptimizeValue) {
-    this.optimize = newOptimizeValue;
-    this.api.setCompilerParameters({ optimize: this.optimize });
-    this.compiler.set('optimize', this.optimize);
+  setOptimize (newOptimizeValue) {
+    this.optimize = newOptimizeValue
+    this.api.setCompilerParameters({ optimize: this.optimize })
+    this.compiler.set('optimize', this.optimize)
   }
 
-  setRuns(runs) {
-    this.runs = runs;
-    this.api.setCompilerParameters({ runs: this.runs });
-    this.compiler.set('runs', this.runs);
+  setRuns (runs) {
+    this.runs = runs
+    this.api.setCompilerParameters({ runs: this.runs })
+    this.compiler.set('runs', this.runs)
   }
 
-  setEvmVersion(newEvmVersion) {
-    this.evmVersion = newEvmVersion;
-    this.api.setCompilerParameters({ evmVersion: this.evmVersion });
-    this.compiler.set('evmVersion', this.evmVersion);
+  setEvmVersion (newEvmVersion) {
+    this.evmVersion = newEvmVersion
+    this.api.setCompilerParameters({ evmVersion: this.evmVersion })
+    this.compiler.set('evmVersion', this.evmVersion)
   }
 
   getCompilerState() {
@@ -128,7 +126,7 @@ export class CompileTab {
       // TODO readd saving current file
       // this.api.saveCurrentFile()
       this.event.emit('removeAnnotations')
-      var currentFile = this.api.getConfiguration('currentFile')
+      var currentFile = this.api.getAppParameter('currentFile')
       return this.compileFile(currentFile)
     } catch (err) {
       console.error(err);
