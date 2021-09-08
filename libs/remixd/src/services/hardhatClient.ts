55ba295bd1855fe54aa4d9f5e2e57872b929f9c6
aniket-engg <aniketengg.225@gmail.com>
2021-09-08 19:24:44 +0530

    hardhat compilation with error and result fixed

--- libs/remixd/src/services/hardhatClient.ts
+++ libs/remixd/src/services/hardhatClient.ts
@@ -1,49 +1,50 @@
-import * as WS from 'ws' // eslint-disable-line
-import { PluginClient } from '@remixproject/plugin'
-const { spawn } = require('child_process')
+import * as WS from 'ws'; // eslint-disable-line
+import { PluginClient } from '@remixproject/plugin';
+const { spawn } = require('child_process');
 
 export class HardhatClient extends PluginClient {
-  methods: Array<string>
-  websocket: WS
-  currentSharedFolder: string
+  methods: Array<string>;
+  websocket: WS;
+  currentSharedFolder: string;
 
-  constructor (private readOnly = false) {
-    super()
-    this.methods = ['compile']
+  constructor(private readOnly = false) {
+    super();
+    this.methods = ['compile'];
   }
 
-  setWebSocket (websocket: WS): void {
-    this.websocket = websocket
+  setWebSocket(websocket: WS): void {
+    this.websocket = websocket;
   }
 
-  sharedFolder (currentSharedFolder: string): void {
-    this.currentSharedFolder = currentSharedFolder
+  sharedFolder(currentSharedFolder: string): void {
+    this.currentSharedFolder = currentSharedFolder;
   }
 
-  compile (configPath: string) {
+  compile(configPath: string) {
     return new Promise((resolve, reject) => {
       if (this.readOnly) {
-        const errMsg = '[Hardhat Compilation]: Cannot compile in read-only mode'
-        return reject(new Error(errMsg))
+        const errMsg =
+          '[Hardhat Compilation]: Cannot compile in read-only mode';
+        return reject(new Error(errMsg));
       }
-      const cmd = `npx hardhat compile --config ${configPath}`
-      const options = { cwd: this.currentSharedFolder, shell: true }
-      const child = spawn(cmd, options)
-      let result = ''
-      let error = ''
-      child.stdout.on('data', (data) => {
-        const msg = `[Hardhat Compilation]: ${data.toString()}`
-        console.log('\x1b[32m%s\x1b[0m', msg)
-        result += msg + '\n'
-      })
-      child.stderr.on('data', (err) => {
-        error += `[Hardhat Compilation]: ${err.toString()} \n`
-      })
+      const cmd = `npx hardhat compile --config ${configPath}`;
+      const options = { cwd: this.currentSharedFolder, shell: true };
+      const child = spawn(cmd, options);
+      let result = '';
+      let error = '';
+      child.stdout.on('data', data => {
+        const msg = `[Hardhat Compilation]: ${data.toString()}`;
+        console.log('\x1b[32m%s\x1b[0m', msg);
+        result += msg + '\n';
+      });
+      child.stderr.on('data', err => {
+        error += `[Hardhat Compilation]: ${err.toString()} \n`;
+      });
       child.on('close', () => {
-        if (error && result) resolve(error + result)
-        else if (error) reject(error)
-        else resolve(result)
-      })
-    })
+        if (error && result) resolve(error + result);
+        else if (error) reject(error);
+        else resolve(result);
+      });
+    });
   }
 }
