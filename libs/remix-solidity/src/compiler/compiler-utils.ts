7a8671726d6114c726dcb30d8a437bb26465943e
yann300 <yann.levreau@gmail.com>
2021-09-08 10:50:21 +0200

    fix references & wrong rebasing

--- libs/remix-solidity/src/compiler/compiler-utils.ts
+++ libs/remix-solidity/src/compiler/compiler-utils.ts
@@ -12,7 +12,7 @@ export const pathToURL = {}
  * @param version is the version of compiler with or without 'soljson-v' prefix and .js postfix
  */
 export function urlFromVersion (version) {
-  let url  
+  let url
   if (version === 'builtin') {
     let location: string | Location = window.document.location
     let path = location.pathname
