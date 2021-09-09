9c8b17890bdf00e643afb3c33557f1e8ef4533c1
davidzagi93@gmail.com <davidzagi93@gmail.com>
2021-09-09 02:38:41 +0100

    added waitForElementVisible in workspace test

--- apps/remix-ide-e2e/src/tests/workspace.test.ts
+++ apps/remix-ide-e2e/src/tests/workspace.test.ts
@@ -48,7 +48,8 @@ module.exports = {
       .waitForElementVisible('*[data-id="modalDialogCustomPromptTextCreate"]')
       // eslint-disable-next-line dot-notation
       .execute(function () { document.querySelector('*[data-id="modalDialogCustomPromptTextCreate"]')['value'] = 'workspace_name_1' })
-      .click('[data-id="workspacesModalDialogModalDialogModalFooter-react"] > span')
+      .waitForElementVisible('span[data-id="workspacesModalDialog-modal-footer-ok-react"]')
+      .click('span[data-id="workspacesModalDialog-modal-footer-ok-react"]')
       .waitForElementVisible('*[data-id="treeViewLitreeViewItemtests"]')
       .pause(5000)
       .waitForElementNotPresent('*[data-id="treeViewLitreeViewItemtest.sol"]')
