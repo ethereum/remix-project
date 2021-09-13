15a38e468c1ddb9ab79bb1b8a297d16be8eb0b49
yann300 <yann.levreau@gmail.com>
2021-09-13 17:30:43 +0200

    fix initiallizing currentFile

--- apps/remix-ide/src/app/tabs/compile-tab.js
+++ apps/remix-ide/src/app/tabs/compile-tab.js
@@ -114,7 +114,6 @@ class CompileTab extends CompilerApiMixin(ViewPlugin) { // implements ICompilerA
   }
 
   async onActivation () {
-    this.currentFile = await this.call('fileManager', 'file')
     super.onActivation()
     this.call('filePanel', 'registerContextMenuItem', {
       id: 'solidity',
