4a96300a7fd7d3be325320a889d5eae66e6fec1d
bunsenstraat <filip.mertens@ethereum.org>
2021-09-14 14:40:09 +0200

    rm console

--- apps/remix-ide/src/app/files/fileManager.js
+++ apps/remix-ide/src/app/files/fileManager.js
@@ -726,7 +726,6 @@ class FileManager extends Plugin {
 
   syncEditor (path) {
     var currentFile = this._deps.config.get('currentFile')
-    console.log(currentFile, path)
     if (path !== currentFile) return
     var provider = this.fileProviderOf(currentFile)
     if (provider) {
