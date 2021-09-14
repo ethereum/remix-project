41f662da2df3f41db799c9061e93fce90b50b624
yann300 <yann.levreau@gmail.com>
2021-09-14 11:50:49 +0200

    create remix editor react lib

---
+++ libs/remix-ui/editor/src/lib/monaco-editor/monaco-editor.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './monaco-editor.css';
+
+/* eslint-disable-next-line */
+export interface MonacoEditorProps {}
+
+export const MonacoEditor = (props: MonacoEditorProps) => {
+  return (
+    <div>
+      <h1>Welcome to monaco-editor!</h1>
+    </div>
+  );
+};
+
+export default MonacoEditor;
