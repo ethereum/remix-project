452233a31cfd0c8064665894a87c656183353575
ioedeveloper <daviddisu8@gmail.com>
2021-09-16 16:13:13 +0100

    create new file with plugin call

--- libs/remix-ui/helper/src/lib/remix-ui-helper.ts
+++ libs/remix-ui/helper/src/lib/remix-ui-helper.ts
@@ -19,3 +19,25 @@ export const checkSpecialChars = (name: string) => {
 export const checkSlash = (name: string) => {
   return name.match(/\//) != null
 }
+
+export const createNonClashingNameAsync = async (name, fileManager, prefix = '') => {
+  if (!name) name = 'Undefined'
+  let counter
+  let ext = 'sol'
+  const reg = /(.*)\.([^.]+)/g
+  const split = reg.exec(name)
+  if (split) {
+    name = split[1]
+    ext = split[2]
+  }
+  let exist = true
+
+  do {
+    const isDuplicate = await fileManager.exists(name + counter + prefix + '.' + ext)
+
+    if (isDuplicate) counter = (counter | 0) + 1
+    else exist = false
+  } while (exist)
+
+  return name + counter + prefix + '.' + ext
+}
