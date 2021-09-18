dd954fc68cb6b77d4e5cf998c90cc879d9968dc5
ioedeveloper <daviddisu8@gmail.com>
2021-09-18 17:13:10 +0100

    Export remaining file-explorer functions to actions

--- libs/remix-ui/helper/src/lib/remix-ui-helper.ts
+++ libs/remix-ui/helper/src/lib/remix-ui-helper.ts
@@ -20,9 +20,9 @@ export const checkSlash = (name: string) => {
   return name.match(/\//) != null
 }
 
-export const createNonClashingNameAsync = async (name, fileManager, prefix = '') => {
+export const createNonClashingNameAsync = async (name: string, fileManager, prefix = '') => {
   if (!name) name = 'Undefined'
-  let counter
+  let _counter
   let ext = 'sol'
   const reg = /(.*)\.([^.]+)/g
   const split = reg.exec(name)
@@ -33,11 +33,30 @@ export const createNonClashingNameAsync = async (name, fileManager, prefix = '')
   let exist = true
 
   do {
-    const isDuplicate = await fileManager.exists(name + counter + prefix + '.' + ext)
+    const isDuplicate = await fileManager.exists(name + _counter + prefix + '.' + ext)
 
-    if (isDuplicate) counter = (counter | 0) + 1
+    if (isDuplicate) _counter = (_counter | 0) + 1
     else exist = false
   } while (exist)
+  const counter = _counter || ''
 
   return name + counter + prefix + '.' + ext
+}
+
+export const joinPath = (...paths) => {
+  paths = paths.filter((value) => value !== '').map((path) => path.replace(/^\/|\/$/g, '')) // remove first and last slash)
+  if (paths.length === 1) return paths[0]
+  return paths.join('/')
+}
+
+export const getPathIcon = (path: string) => {
+  return path.endsWith('.txt')
+    ? 'far fa-file-alt' : path.endsWith('.md')
+      ? 'far fa-file-alt' : path.endsWith('.sol')
+        ? 'fak fa-solidity-mono' : path.endsWith('.js')
+          ? 'fab fa-js' : path.endsWith('.json')
+            ? 'fas fa-brackets-curly' : path.endsWith('.vy')
+              ? 'fak fa-vyper-mono' : path.endsWith('.lex')
+                ? 'fak fa-lexon' : path.endsWith('.contract')
+                  ? 'fab fa-ethereum' : 'far fa-file'
 }
