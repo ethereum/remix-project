a6bb2d8de249c091b4e06e125ccc06927462b9bd
yann300 <yann.levreau@gmail.com>
2021-09-21 13:52:57 +0200

    monaco editor

--- apps/remix-ide-e2e/src/tests/recorder.spec.ts
+++ apps/remix-ide-e2e/src/tests/recorder.spec.ts
@@ -129,7 +129,7 @@ contract t2est {
 
 const records = `{
   "accounts": {
-    "account{2}": "0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c"
+    "account{10}": "0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c"
   },
   "linkReferences": {
     "testLib": "created{1512830014773}"
@@ -146,7 +146,7 @@ const records = `{
         "linkReferences": {},
         "inputs": "()",  
         "type": "constructor",
-        "from": "account{2}"
+        "from": "account{10}"
       }
     },
     {
@@ -172,7 +172,7 @@ const records = `{
         "name": "",
         "type": "constructor",
         "inputs": "(uint256)",
-        "from": "account{2}"
+        "from": "account{10}"
       }
     },
     {
@@ -188,7 +188,7 @@ const records = `{
         "name": "set",
         "inputs": "(uint256,address)",
         "type": "function",
-        "from": "account{2}"
+        "from": "account{10}"
       }
     }
   ],
@@ -287,7 +287,7 @@ const records = `{
 
 const scenario = {
   accounts: {
-    'account{2}': '0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c'
+    'account{10}': '0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c'
   },
   linkReferences: {},
   transactions: [
@@ -305,7 +305,7 @@ const scenario = {
         name: '',
         type: 'constructor',
         inputs: '(uint256)',
-        from: 'account{2}'
+        from: 'account{10}'
       }
     },
     {
@@ -320,7 +320,7 @@ const scenario = {
         name: 'set',
         inputs: '(uint256)',
         type: 'function',
-        from: 'account{2}'
+        from: 'account{10}'
       }
     }
   ],
