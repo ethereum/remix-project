c13301fda9e56381792585d0aa3c7dab38b7dbc3
ioedeveloper <daviddisu8@gmail.com>
2021-09-22 17:14:57 +0100

    finalize child components

---
+++ libs/remix-ui/workspace/src/lib/components/file-label.tsx
@@ -0,0 +1,59 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useEffect, useRef } from 'react'
+import { FileType } from '../types'
+
+export interface FileLabelProps {
+  file: FileType,
+  focusEdit: {
+    element: string
+    type: string
+    isNew: boolean
+    lastEdit: string
+  }
+  editModeOff: (content: string) => void
+}
+
+export const FileLabel = (props: FileLabelProps) => {
+  const { file, focusEdit, editModeOff } = props
+  const isEditable = focusEdit.element === file.path
+  const labelRef = useRef(null)
+
+  useEffect(() => {
+    if (isEditable) {
+      setTimeout(() => {
+        labelRef.current.focus()
+      }, 0)
+    }
+  }, [isEditable])
+
+  const handleEditInput = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.which === 13) {
+      event.preventDefault()
+      editModeOff(labelRef.current.innerText)
+    }
+  }
+
+  const handleEditBlur = (event: React.SyntheticEvent) => {
+    event.stopPropagation()
+    editModeOff(labelRef.current.innerText)
+  }
+
+  return (
+    <div
+      className='remixui_items d-inline-block w-100'
+      ref={labelRef}
+      suppressContentEditableWarning={true}
+      contentEditable={isEditable}
+      onKeyDown={handleEditInput}
+      onBlur={handleEditBlur}
+    >
+      <span
+        title={file.path}
+        className={'remixui_label ' + (file.isDirectory ? 'folder' : 'remixui_leaf')}
+        data-path={file.path}
+      >
+        { file.name }
+      </span>
+    </div>
+  )
+}
