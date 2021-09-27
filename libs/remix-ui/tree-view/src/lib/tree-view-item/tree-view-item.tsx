aac36a32661af7520dba6967506d3f179d01bf56
filip mertens <filip.mertens@ethereum.org>
2021-09-27 16:43:57 +0200

    rm log

--- libs/remix-ui/tree-view/src/lib/tree-view-item/tree-view-item.tsx
+++ libs/remix-ui/tree-view/src/lib/tree-view-item/tree-view-item.tsx
@@ -16,7 +16,7 @@ export const TreeViewItem = (props: TreeViewItemProps) => {
       <div key={`treeViewDiv${id}`} data-id={`treeViewDiv${id}`} className={`d-flex flex-row align-items-center ${labelClass}`} onClick={() => !controlBehaviour && setIsExpanded(!isExpanded)}>
         { children ? <div className={isExpanded ? `px-1 ${iconY} caret caret_tv` : `px-1 ${iconX} caret caret_tv`} style={{ visibility: children ? 'visible' : 'hidden' }}></div> : icon ? <div className={`pr-3 pl-1 ${icon} caret caret_tv`}></div> : null }
         <span className='w-100 pl-1'>
-          { label } {`treeViewLi${id}`}
+          { label }
         </span>
       </div>
       { isExpanded ? children : null }
