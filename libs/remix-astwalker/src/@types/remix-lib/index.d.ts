5835d96224f4efeb28d112f6034384cbb3aaca96
Admin <aniketengg.225@gmail.com>
2021-09-28 00:12:07 +0530

    astwalker linting fix

--- libs/remix-astwalker/src/@types/remix-lib/index.d.ts
+++ libs/remix-astwalker/src/@types/remix-lib/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for the things we need from remix-lib
 
-declare module "remix-lib" {
+declare module 'remix-lib' {
   export module util {
     export function findLowerBound(target: number, array: Array<number>): number;
   }
