bfc8ea720ee1190d644ad7b342a278e8a3d7230b
bunsenstraat <bunsenstraat@gmail.com>
2021-09-29 10:39:51 +0200

    Update compile-tab.js

--- apps/remix-ide/src/app/tabs/compile-tab.js
+++ apps/remix-ide/src/app/tabs/compile-tab.js
@@ -124,7 +124,9 @@ class CompileTab extends CompilerApiMixin(ViewPlugin) { // implements ICompilerA
       path: [],
       pattern: []
     })
-    this.currentFile = await this.call('fileManager', 'file')
+    try {
+      this.currentFile = await this.call('fileManager', 'file')
+    } catch (error) {}
   }
 
   getCompilerParameters () {
