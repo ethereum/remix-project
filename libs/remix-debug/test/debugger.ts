bee8df3b7f1fc24a173b185b54db0d6db0b0fb14
yann300 <yann.levreau@gmail.com>
2021-09-29 09:39:43 +0200

    refactor and fix remix-debug tests

--- libs/remix-debug/test/debugger.ts
+++ libs/remix-debug/test/debugger.ts
@@ -7,7 +7,6 @@ import { BreakpointManager } from '../src/code/breakpointManager'
 
 var compiler = require('solc')
 var vmCall = require('./vmCall')
-var remixLib = require('@remix-project/remix-lib')
 
 var ballot = `pragma solidity >=0.4.22 <0.8.0;
 
@@ -151,19 +150,17 @@ contract Ballot {
 `;
 
 (async () => {
-  var privateKey = Buffer.from('dae9801649ba2d95a21e688b56f77905e5667c44ce868ec83f82e838712a2c7a', 'hex')
-  var vm = await vmCall.initVM(privateKey)
+  var privateKey = Buffer.from('503f38a9c967ed597e47fe25643985f032b072db8075426a92110f82df48dfcb', 'hex')
   var output = compiler.compile(compilerInput(ballot))
   output = JSON.parse(output)
-  var web3VM = new remixLib.vm.Web3VMProvider()
-  web3VM.setVM(vm)
   const param = '0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000148656c6c6f20576f726c64210000000000000000000000000000000000000000'
-  vmCall.sendTx(vm, {nonce: 0, privateKey: privateKey}, null, 0, output.contracts['test.sol']['Ballot'].evm.bytecode.object + param, (error, txHash) => {
-    console.log(error, txHash)
+  const web3 = await vmCall.getWeb3()
+  vmCall.sendTx(web3, {nonce: 0, privateKey: privateKey}, null, 0, output.contracts['test.sol']['Ballot'].evm.bytecode.object + param, (error, hash) => {
+    console.log(error, hash)
     if (error) {
       throw error
     } else {
-      web3VM.eth.getTransaction(txHash, (error, tx) => {
+      web3.eth.getTransaction(hash, (error, tx) => {
         if (error) {
           throw error
         } else {
@@ -171,7 +168,7 @@ contract Ballot {
             compilationResult: function () {
               return { data: output }
             },
-            web3: web3VM
+            web3: web3
           })
   
           debugManager.callTree.event.register('callTreeReady', () => {
@@ -230,7 +227,7 @@ function testDebugging (debugManager) {
       var storageView = debugManager.storageViewAt(196, address)
 
       storageView.storageRange().then((storage) => {
-        t.equal(JSON.stringify(storage), JSON.stringify({ '0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563': { key: '0x0000000000000000000000000000000000000000000000000000000000000000', value: '0x0000000000000000000000004b0897b0513fdc7c541b6d9d7e929c4e5364d2db' } }))
+        t.equal(JSON.stringify(storage), JSON.stringify({ '0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563': { key: '0x0000000000000000000000000000000000000000000000000000000000000000', value: '0x0000000000000000000000005b38da6a701c568545dcfcb03fcb875f56beddc4' } }))
       }).catch((error) => {
         if (error) return t.end(error)
       })
@@ -245,7 +242,7 @@ function testDebugging (debugManager) {
       const state = await debugManager.extractStateAt(312)
       const decodedState = await debugManager.decodeStateAt(312, state)
       console.log(decodedState)
-      t.equal(decodedState['chairperson'].value, '0x4B0897B0513FDC7C541B6D9D7E929C4E5364D2DB')
+      t.equal(decodedState['chairperson'].value, '0x5B38DA6A701C568545DCFCB03FCB875F56BEDDC4')
       t.equal(decodedState['chairperson'].type, 'address')
       t.equal(decodedState['proposals'].value[0].value.voteCount.value, '0')
       t.equal(decodedState['proposals'].value[0].value.voteCount.type, 'uint256')
@@ -259,13 +256,15 @@ function testDebugging (debugManager) {
 
   tape('traceManager.decodeLocalsAt', async (t) => {
     t.plan(1)
-    const tested = JSON.parse('{"proposalNames":{"value":[{"value":"0x48656C6C6F20576F726C64210000000000000000000000000000000000000000","type":"bytes32"}],"length":"0x1","type":"bytes32[]","cursor":1,"hasNext":false},"p":{"value":"45","type":"uint256"},"addressLocal":{"value":"0x4B0897B0513FDC7C541B6D9D7E929C4E5364D2DB","type":"address"},"i":{"value":"2","type":"uint256"},"proposalsLocals":{"value":[{"value":{"name":{"value":"0x48656C6C6F20576F726C64210000000000000000000000000000000000000000","type":"bytes32"},"voteCount":{"value":"0","type":"uint256"}},"type":"struct Ballot.Proposal"}],"length":"0x1","type":"struct Ballot.Proposal[]"}}')
+    const tested = JSON.parse('{"proposalNames":{"value":[{"value":"0x48656C6C6F20576F726C64210000000000000000000000000000000000000000","type":"bytes32"}],"length":"0x1","type":"bytes32[]","cursor":1,"hasNext":false},"p":{"value":"45","type":"uint256"},"addressLocal":{"value":"0x5B38DA6A701C568545DCFCB03FCB875F56BEDDC4","type":"address"},"i":{"value":"2","type":"uint256"},"proposalsLocals":{"value":[{"value":{"name":{"value":"0x48656C6C6F20576F726C64210000000000000000000000000000000000000000","type":"bytes32"},"voteCount":{"value":"0","type":"uint256"}},"type":"struct Ballot.Proposal"}],"length":"0x1","type":"struct Ballot.Proposal[]"}}')
     try {
       const address = debugManager.traceManager.getCurrentCalledAddressAt(327)
       const location = await debugManager.sourceLocationFromVMTraceIndex(address, 327)
       debugManager.decodeLocalsAt(327, location, (error, decodedlocals) => {
         if (error) return t.end(error)
-        t.ok(deepequal(decodedlocals, tested), `locals does not match. expected: ${JSON.stringify(tested)} - current: ${decodedlocals}`)
+        const res = deepequal(decodedlocals, tested)
+        console.log(res)
+        t.ok(res, `test if locals does match. expected: ${JSON.stringify(tested)} - current: ${JSON.stringify(decodedlocals)}`)
       })
     } catch (error) {
       return t.end(error)
