ca9c49cf2608bcc742ac0f134ec2e5667cc33994
Aniket-Engg <aniketengg.225@gmail.com>
2021-10-08 12:30:10 +0530

    enable back remix-tests CLI unit tests

--- libs/remix-tests/tests/testRunner.cli.spec.ts
+++ libs/remix-tests/tests/testRunner.cli.spec.ts
@@ -40,7 +40,7 @@ Commands:
       })
 
     test('remix-tests running a test file', () => {
-      const res = spawnSync(executablePath, [resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, [resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim()).toMatch(/:: Running remix-tests - Unit testing for solidity ::/)
       expect(res.stdout.toString().trim()).toMatch(/creation of library remix_tests.sol:Assert pending.../)
@@ -55,7 +55,7 @@ Commands:
     })
 
     test('remix-tests running a test file with custom compiler version', () => {
-      const res = spawnSync(executablePath, ['--compiler', '0.7.4', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--compiler', '0.7.4', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('Compiler version set to 0.7.4. Latest version is')).toBeTruthy()
       expect(res.stdout.toString().trim().includes('Loading remote solc version v0.7.4+commit.3f05b770 ...')).toBeTruthy()
@@ -69,13 +69,13 @@ Commands:
     })
 
     test('remix-tests running a test file with unavailable custom compiler version (should fail)', () => {
-      const res = spawnSync(executablePath, ['--compiler', '1.10.4', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--compiler', '1.10.4', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('No compiler found in releases with version 1.10.4')).toBeTruthy()
     })
 
     test('remix-tests running a test file with custom EVM', () => {
-      const res = spawnSync(executablePath, ['--evm', 'petersburg', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--evm', 'petersburg', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('EVM set to petersburg')).toBeTruthy()
       expect(res.stdout.toString().trim()).toMatch(/:: Running remix-tests - Unit testing for solidity ::/)
@@ -88,7 +88,7 @@ Commands:
     })
 
     test('remix-tests running a test file by enabling optimization', () => {
-      const res = spawnSync(executablePath, ['--optimize', 'true', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--optimize', 'true', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('Optimization is enabled')).toBeTruthy()
       expect(res.stdout.toString().trim()).toMatch(/:: Running remix-tests - Unit testing for solidity ::/)
@@ -101,7 +101,7 @@ Commands:
     })
 
     test('remix-tests running a test file by enabling optimization and setting runs', () => {
-      const res = spawnSync(executablePath, ['--optimize', 'true', '--runs', '300', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--optimize', 'true', '--runs', '300', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('Optimization is enabled')).toBeTruthy()
       expect(res.stdout.toString().trim().includes('Runs set to 300')).toBeTruthy()
@@ -115,13 +115,13 @@ Commands:
     })
 
     test('remix-tests running a test file without enabling optimization and setting runs (should fail)', () => {
-      const res = spawnSync(executablePath, ['--runs', '300', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--runs', '300', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('Optimization should be enabled for runs')).toBeTruthy()
     })
 
     test('remix-tests running a test file with all options', () => {
-      const res = spawnSync(executablePath, ['--compiler', '0.7.5', '--evm', 'istanbul', '--optimize', 'true', '--runs', '250', resolve(__dirname + '/examples_0/assert_ok_test.sol')])
+      const res = spawnSync(executablePath, ['--compiler', '0.7.5', '--evm', 'istanbul', '--optimize', 'true', '--runs', '250', resolve(__dirname + '/examples_0/assert_ok_without_console_test.sol')])
       // match initial lines
       expect(res.stdout.toString().trim().includes('Compiler version set to 0.7.5. Latest version is')).toBeTruthy()
       expect(res.stdout.toString().trim().includes('Loading remote solc version v0.7.5+commit.eb77ed08 ...')).toBeTruthy()
