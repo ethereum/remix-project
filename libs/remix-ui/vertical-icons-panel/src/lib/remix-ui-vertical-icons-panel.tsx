8a8e147d87750cdd497497f2831601d6de79a99e
Joseph Izang <josephizang@gmail.com>
2021-10-11 11:46:09 +0100

    create vertical icons panel react lib

---
+++ libs/remix-ui/vertical-icons-panel/src/lib/remix-ui-vertical-icons-panel.tsx
@@ -0,0 +1,16 @@
+import './remix-ui-vertical-icons-panel.module.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiVerticalIconsPanelProps {}
+
+export function RemixUiVerticalIconsPanel(
+  props: RemixUiVerticalIconsPanelProps
+) {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-vertical-icons-panel!</h1>
+    </div>
+  );
+}
+
+export default RemixUiVerticalIconsPanel;
