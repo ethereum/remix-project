92c7f37ff58ca9bbae9cd988151db31856f8df2c
filip mertens <filip.mertens@ethereum.org>
2021-10-13 19:15:50 +0200

    run tests

--- apps/remix-ide-e2e/src/local-plugin/.testcaferc.js
+++ apps/remix-ide-e2e/src/local-plugin/.testcaferc.js
@@ -2,7 +2,7 @@ let os = require("os");
 
 module.exports = {
     skipJsErrors: true,
-    browsers: ['chrome'],
+    browsers: ['chrome  -incognito'],
     screenshots: {
         "path": "/tmp/artifacts",
         "takeOnFails": true,
