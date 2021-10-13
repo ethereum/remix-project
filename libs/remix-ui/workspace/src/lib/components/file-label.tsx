e50047c2f34ac795ef57d687a23a646e750ddd59
ioedeveloper <daviddisu8@gmail.com>
2021-10-13 19:03:49 +0100

    Fix linting, remove comments and rename workspaceStatus

--- libs/remix-ui/workspace/src/lib/components/file-label.tsx
+++ libs/remix-ui/workspace/src/lib/components/file-label.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line no-use-before-define
 import React, { useEffect, useRef, useState } from 'react'
 import { FileType } from '../types'
 
