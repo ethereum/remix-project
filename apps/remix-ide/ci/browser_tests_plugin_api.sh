4f277528992119aae591da8778e3c04460946633
bunsenstraat <filip.mertens@ethereum.org>
2021-10-16 15:48:56 +0200

    ci cnfig

--- apps/remix-ide/ci/browser_tests_plugin_api.sh
+++ apps/remix-ide/ci/browser_tests_plugin_api.sh
@@ -8,7 +8,10 @@ TEST_EXITCODE=0
 
 npm run serve &
 npx nx serve remix-ide-e2e-src-local-plugin &
-sleep 5 &
+
+sleep 5
+
+npm run build:e2e
 npm run nightwatch_local_pluginApi || TEST_EXITCODE=1
 
 echo "$TEST_EXITCODE"
