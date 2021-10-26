cade37cb49123cae13c2f08bad3cc7bbae1f2ea4
bunsenstraat <filip.mertens@ethereum.org>
2021-10-26 11:58:10 +0200

    rm plugin manager from ci

--- apps/remix-ide-e2e/src/local-plugin/src/app/logger.tsx
+++ apps/remix-ide-e2e/src/local-plugin/src/app/logger.tsx
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 interface loggerProps {
-  append: boolean,
   log: any,
   id: string
 }
 
 export const Logger: React.FC<loggerProps> = (props) => {
-  const [value, setValue] = useState<string>('')
-
-  useEffect(() => {
-    setValue(value => {
-      const addValue = typeof props.log === 'string' ? props.log : JSON.stringify(props.log)
-      return props.append ? `${value} ${addValue}` : addValue
-    })
-  }, [props])
-
-  return (<div id={props.id} className="jumbotron overflow-auto text-break mb-1 p-2">{value}</div>)
+  return (<div id={props.id} className="jumbotron overflow-auto text-break mb-1 p-2">{props.log}</div>)
 }
--- apps/remix-ide-e2e/src/tests/pluginManager.test.ts
+++ apps/remix-ide-e2e/src/tests/pluginManager.test.ts
@@ -12,13 +12,6 @@ const testData = {
   pluginUrl: 'https://zokrates.github.io/zokrates-remix-plugin/'
 }
 
-const localPluginData = {
-  pluginName: 'localPlugin',
-  pluginDisplayName: 'Local Plugin',
-  pluginCanActivate: 'LearnEth',
-  pluginUrl: 'http://localhost:2020'
-}
-
 module.exports = {
   before: function (browser: NightwatchBrowser, done: VoidFunction) {
     init(browser, done, 'http://127.0.0.1:8080', false)
@@ -76,44 +69,6 @@ module.exports = {
       .waitForElementVisible('*[data-id="pluginManagerComponentActivateButtonvyper"]', 60000)
   },
 
-  /*
-  'Should grant plugin permission (ZOKRATES)': function (browser) {
-    browser.waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-    .click('*[data-id="pluginManagerPermissionsButton"]')
-    .waitForElementVisible('*[data-id="pluginManagerSettingsPermissionForm"]')
-    .assert.containsText('*[data-id="pluginManagerSettingsPermissionForm"]', 'No Permission requested yet')
-    .modalFooterOKClick()
-    .click('*[data-id="verticalIconsFileExplorerIcons"]')
-    .openFile('3_Ballot.sol')
-    .click('*[plugin="ZoKrates"]')
-    .pause(5000)
-    .frame(0)
-    .useXpath().click("//span[text()='Compile']")
-    .pause(2000)
-    .frameParent()
-    .useCss().waitForElementVisible('*[data-id="modalDialogContainer"]')
-    .assert.containsText('*[data-id="permissionHandlerMessage"]', 'ZOKRATES" WOULD LIKE TO ACCESS "FILE MANAGER" :')
-    .pause(2000)
-    .click('*[data-id="permissionHandlerRememberChoice"]')
-    .pause(2000)
-    .modalFooterOKClick()
-  },
-
-  'Should revert plugin permission (ZOKRATES)': function (browser) {
-    browser.waitForElementVisible('*[data-id="verticalIconsSettingsIcons"]')
-    .click('*[data-id="verticalIconsSettingsIcons"]')
-    .waitForElementVisible('*[data-id="pluginManagerPermissionsButton"]')
-    .click('*[data-id="pluginManagerPermissionsButton"]')
-    .waitForElementVisible('*[data-id="modalDialogContainer"]')
-    .click('*[data-id="pluginManagerSettingsPermissionForm"]')
-    .pause(2000)
-    .click('*[data-id="pluginManagerSettingsClearAllPermission"]')
-    .pause(2000)
-    .assert.containsText('*[data-id="pluginManagerSettingsPermissionForm"]', 'No Permission requested yet')
-    .modalFooterOKClick()
-  },
-  */
-
   'Should connect a local plugin': function (browser: NightwatchBrowser) {
     browser.waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
       .execute(function () {
@@ -130,8 +85,6 @@ module.exports = {
       .click('*[data-id="localPluginRadioButtonsidePanel"]')
       .click('*[data-id="pluginManagerLocalPluginModalDialogModalDialogModalFooter-react"]')
       .click('*[data-id="pluginManagerLocalPluginModalDialog-modal-footer-ok-react')
-      // .modalFooterOKClick()
-      // .waitForElementVisible('*[data-id="pluginManagerComponentDeactivateButtonremixIde"]', 60000)
   },
 
   'Should display error message for creating already existing plugin': function (browser: NightwatchBrowser) {
@@ -163,9 +116,8 @@ module.exports = {
           .waitForElementVisible('*[data-id="remixIdeSidePanel"]')
           .pause(3000)
           .perform((done) => {
-            // const filtered = plugins.filter(plugin => plugin !== 'testremixIde') // remove this when localplugin bug is resolved
             plugins.forEach(plugin => {
-              if ((plugin !== testData.pluginName) && plugin !== localPluginData.pluginName) {
+              if (plugin !== testData.pluginName) {
                 browser.waitForElementVisible(`[plugin="${plugin}"`)
               }
             })
