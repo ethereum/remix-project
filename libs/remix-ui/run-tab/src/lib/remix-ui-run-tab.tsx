d972e39c5065641ab2b57d29d5e6cc6734ac5350
ioedeveloper <daviddisu8@gmail.com>
2021-11-01 14:26:57 +0100

    Setup udapp library

---
+++ libs/remix-ui/run-tab/src/lib/remix-ui-run-tab.tsx
@@ -0,0 +1,14 @@
+import './remix-ui-run-tab.module.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiRunTabProps {}
+
+export function RemixUiRunTab(props: RemixUiRunTabProps) {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-run-tab!</h1>
+    </div>
+  );
+}
+
+export default RemixUiRunTab;
