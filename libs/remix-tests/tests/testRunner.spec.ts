7b8bdd45bfae9231e27ec6376e461cb88e6e9f69
Aniket-Engg <aniketengg.225@gmail.com>
2021-11-08 12:52:10 +0530

    accounts passed for unit tests

--- libs/remix-tests/tests/testRunner.spec.ts
+++ libs/remix-tests/tests/testRunner.spec.ts
@@ -67,7 +67,7 @@ async function compileAndDeploy(filename: string, callback: Function) {
       }
       try {
         compilationData = compilationResult
-        deployAll(compilationResult, web3, null, false, null, next)
+        deployAll(compilationResult, web3, accounts, false, null, next)
       } catch (e) {
         throw e
       }
