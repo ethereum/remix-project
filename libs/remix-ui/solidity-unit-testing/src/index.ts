5b002f17ad733eb2f1a191ce53d9eafa656acd79
Aniket-Engg <aniketengg.225@gmail.com>
2021-11-10 13:45:14 +0530

    SUT in React

---
+++ libs/remix-ui/solidity-unit-testing/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/remix-ui-solidity-unit-testing';
---
+++ libs/remix-ui/solidity-unit-testing/src/lib/remix-ui-solidity-unit-testing.tsx
@@ -0,0 +1,16 @@
+import './remix-ui-solidity-unit-testing.module.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiSolidityUnitTestingProps {}
+
+export function RemixUiSolidityUnitTesting(
+  props: RemixUiSolidityUnitTestingProps
+) {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-solidity-unit-testing!</h1>
+    </div>
+  );
+}
+
+export default RemixUiSolidityUnitTesting;
