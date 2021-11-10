ec5ceb8638413cb03617f6419c6eea5802c49471
filip mertens <filip.mertens@ethereum.org>
2021-11-10 14:27:41 +0100

    fix timing

--- apps/remix-ide-e2e/src/tests/workspace.test.ts
+++ apps/remix-ide-e2e/src/tests/workspace.test.ts
@@ -19,7 +19,7 @@ module.exports = {
     browser
       .pause(5000)
       .refresh()
-      .pause(20000)
+      .waitForElementVisible('#editorView', 30000)
       .getEditorValue((content) => {
         browser.assert.ok(content.indexOf('contract Ballot {') !== -1, 'content doesn\'t include Ballot contract')
       })
