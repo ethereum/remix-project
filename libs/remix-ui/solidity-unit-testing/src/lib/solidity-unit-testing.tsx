325e418d58382a7a51234d69e8a6703092880d63
Aniket-Engg <aniketengg.225@gmail.com>
2021-11-11 11:04:49 +0530

    setup update

--- libs/remix-ui/solidity-unit-testing/src/lib/solidity-unit-testing.tsx
+++ libs/remix-ui/solidity-unit-testing/src/lib/solidity-unit-testing.tsx
@@ -1,14 +1,15 @@
-import './remix-ui-solidity-unit-testing.module.css';
+import React, { useState } from 'react' // eslint-disable-line
 
 /* eslint-disable-next-line */
 export interface SolidityUnitTestingProps {}
 
 export const SolidityUnitTesting = (props: SolidityUnitTestingProps) => {
+  console.log('props---->', props)
   return (
     <div>
       <h1>Welcome to remix-ui-solidity-unit-testing!</h1>
     </div>
-  );
+  )
 }
 
 export default SolidityUnitTesting
