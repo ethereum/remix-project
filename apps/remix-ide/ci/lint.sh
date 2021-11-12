7b05fbcb96c5eff7051e41412dc3dfef514812a2
filip mertens <filip.mertens@ethereum.org>
2021-11-12 18:46:14 +0100

    build e2e

--- apps/remix-ide/ci/lint.sh
+++ apps/remix-ide/ci/lint.sh
@@ -6,8 +6,11 @@ BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
 echo "$BUILD_ID"
 TEST_EXITCODE=0
 
+npm run build:e2e
 KEYS=$(jq -r '.projects | keys' workspace.json  | tr -d '[],"')
 TESTFILES=$(circleci tests glob "dist/apps/remix-ide-e2e/src/tests/**/*.test.js") 
+echo $TESTFILES
+TESTFILES=$(circleci tests glob "dist/apps/remix-ide-e2e/src/tests/**/*.test.js" | circleci tests split --split-by=timings)
 echo $TESTFILES
 
 echo "$TEST_EXITCODE"
