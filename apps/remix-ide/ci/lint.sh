65422a0efc7ccff4e332c69252b4bf64654b9d1f
filip mertens <filip.mertens@ethereum.org>
2021-11-12 18:41:39 +0100

    doitlint

--- apps/remix-ide/ci/lint.sh
+++ apps/remix-ide/ci/lint.sh
@@ -7,8 +7,7 @@ echo "$BUILD_ID"
 TEST_EXITCODE=0
 
 KEYS=$(jq -r '.projects | keys' workspace.json  | tr -d '[],"')
-
-TESTFILES=$($KEYS | circleci tests split)
+TESTFILES=$(circleci tests glob "dist/apps/remix-ide-e2e/src/tests/**/*.test.js") 
 echo $TESTFILES
 
 echo "$TEST_EXITCODE"
