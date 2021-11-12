045646c70b606229aa6ef2e1ee8d7cd316430bd6
filip mertens <filip.mertens@ethereum.org>
2021-11-12 14:15:26 +0100

    lint again

--- apps/remix-ide/ci/lint.sh
+++ apps/remix-ide/ci/lint.sh
@@ -6,10 +6,9 @@ BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
 echo "$BUILD_ID"
 TEST_EXITCODE=0
 
-nx run remix-core-plugin:lint
-nx run remix-ui-settings:lint
-nx run remix-ui-renderer:lint
-nx run solidity-compiler:lint
+npm run lint  || TEST_EXITCODE=1
+npm run lint:libs  || TEST_EXITCODE=1
+npm run lint remix-ide-e2e || TEST_EXITCODE=1
 
 echo "$TEST_EXITCODE"
 if [ "$TEST_EXITCODE" -eq 1 ]
