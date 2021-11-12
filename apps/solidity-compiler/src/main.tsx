aa93da8418e96b67f669e00f6a47c4e43116d4ad
filip mertens <filip.mertens@ethereum.org>
2021-11-12 20:23:44 +0100

    fix linting

--- apps/solidity-compiler/src/main.tsx
+++ apps/solidity-compiler/src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './app/app';
+import App from './app/app'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
