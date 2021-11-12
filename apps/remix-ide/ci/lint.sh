44aebc9e6e1a952ac84dbccc8266a1cdb5385e5d
filip mertens <filip.mertens@ethereum.org>
2021-11-12 19:10:32 +0100

    try again

--- apps/remix-ide/ci/lint.sh
+++ apps/remix-ide/ci/lint.sh
@@ -10,22 +10,12 @@ npm run build:e2e
 KEYS=$(jq -r '.projects | keys' workspace.json  | tr -d '[],"')
 # add .js to every key
 KEYS=$(echo $KEYS | sed 's/\(.*\)/\1.js/')
-TESTFILES=$(circleci tests glob "dist/apps/remix-ide-e2e/src/tests/**/*.test.js") 
-echo $TESTFILES
-TESTFILES=$(circleci tests glob "dist/apps/remix-ide-e2e/src/tests/**/*.test.js" | circleci tests split --split-by=timings)
-echo $TESTFILES
-TESTFILES=$(echo $KEYS | circleci tests split)
-echo $TESTFILES
-KEYS="test1.js test2.js"
+# keys to array
+KEYS=($KEYS)
+
 TESTFILES=$(echo $KEYS | circleci tests split)
 echo $TESTFILES
 
-for row in $TESTFILES; do
-    if [ "$row" != "debugger" ]
-    then
-        nx lint ${row} || TEST_EXITCODE=1
-    fi
-done
 
 echo "$TEST_EXITCODE"
 if [ "$TEST_EXITCODE" -eq 1 ]
