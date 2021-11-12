a972e6065c347f7213ead1bb0a59b8e8eba0e45f
filip mertens <filip.mertens@ethereum.org>
2021-11-12 12:30:16 +0100

    lint sh

---
+++ apps/remix-ide/ci/lint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
+echo "$BUILD_ID"
+TEST_EXITCODE=0
+
+npm run lint  || TEST_EXITCODE=1
+npm run lint:libs  || TEST_EXITCODE=1
+npm run lint remix-ide-e2e || TEST_EXITCODE=1
+
+echo "$TEST_EXITCODE"
+if [ "$TEST_EXITCODE" -eq 1 ]
+then
+  exit 1
+fi
