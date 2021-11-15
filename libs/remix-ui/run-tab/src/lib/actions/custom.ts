9e75970ff751f030a20022179e10ce77118d080c
ioedeveloper <daviddisu8@gmail.com>
2021-11-15 17:01:44 +0100

    setup environment list and events

--- libs/remix-ui/run-tab/src/lib/actions/custom.ts
+++ libs/remix-ui/run-tab/src/lib/actions/custom.ts
@@ -3,7 +3,7 @@ import React from 'react'
 import * as ethJSUtil from 'ethereumjs-util'
 import Web3 from 'web3'
 import { shortenAddress } from '@remix-ui/helper'
-import { fetchAccountsListFailed, fetchAccountsListRequest, fetchAccountsListSuccess, setGasLimit, setSelectedAccount, setSendUnit, setSendValue } from './payload'
+import { addProvider, fetchAccountsListFailed, fetchAccountsListRequest, fetchAccountsListSuccess, removeProvider, setExecutionEnvironment, setGasLimit, setNetworkName, setSelectedAccount, setSendUnit, setSendValue } from './payload'
 import { runTabInitialState, runTabReducer } from '../reducers/runTab'
 
 export function useRunTabPlugin (plugin) {
@@ -19,6 +19,26 @@ export function useRunTabPlugin (plugin) {
       if (error) return
       updateAccountBalances()
     })
+
+    plugin.blockchain.event.register('contextChanged', (context, silent) => {
+      setFinalContext()
+    })
+
+    plugin.blockchain.event.register('networkStatus', ({ error, network }) => {
+      if (error) {
+        const netUI = 'can\'t detect network '
+        setNetworkNameFromProvider(netUI)
+
+        return
+      }
+      const networkProvider = plugin.networkModule.getNetworkProvider.bind(plugin.networkModule)
+      const netUI = (networkProvider() !== 'vm') ? `${network.name} (${network.id || '-'}) network` : ''
+
+      setNetworkNameFromProvider(netUI)
+    })
+
+    plugin.blockchain.event.register('addProvider', provider => addExternalProvider(provider))
+    plugin.blockchain.event.register('removeProvider', name => removeExternalProvider(name))
 
     plugin.blockchain.resetAndInit(plugin.config, {
       getAddress: (cb) => {
@@ -42,7 +62,6 @@ export function useRunTabPlugin (plugin) {
         }
       }
     })
-    console.log('called: reset and init')
   }
 
   const updateAccountBalances = () => {
@@ -56,6 +75,7 @@ export function useRunTabPlugin (plugin) {
         accounts[value] = updated
       })
     })
+    dispatch(fetchAccountsListSuccess(accounts))
   }
 
   const fillAccountsList = async () => {
@@ -64,7 +84,18 @@ export function useRunTabPlugin (plugin) {
       const promise = plugin.blockchain.getAccounts()
 
       promise.then((accounts: string[]) => {
-        dispatch(fetchAccountsListSuccess(accounts))
+        const loadedAccounts = {}
+
+        if (!accounts) accounts = []
+        accounts.forEach((account) => {
+          plugin.blockchain.getBalanceInEther(account, (err, balance) => {
+            if (err) return
+            const updated = shortenAddress(account, balance)
+
+            loadedAccounts[account] = updated
+          })
+        })
+        dispatch(fetchAccountsListSuccess(loadedAccounts))
       }).catch((e) => {
         dispatch(fetchAccountsListFailed(e.message))
       })
@@ -85,5 +116,72 @@ export function useRunTabPlugin (plugin) {
     dispatch(setGasLimit(value))
   }
 
-  return { runTab, setupEvents, fillAccountsList, setAccount, setUnit, setGasFee }
+  const addPluginProvider = (profile) => {
+    if (profile.kind === 'provider') {
+      ((profile, app) => {
+        const web3Provider = {
+          async sendAsync (payload, callback) {
+            try {
+              const result = await app.call(profile.name, 'sendAsync', payload)
+              callback(null, result)
+            } catch (e) {
+              callback(e)
+            }
+          }
+        }
+        app.blockchain.addProvider({ name: profile.displayName, provider: web3Provider })
+      })(profile, plugin)
+    }
+  }
+
+  const removePluginProvider = (profile) => {
+    if (profile.kind === 'provider') plugin.blockchain.removeProvider(profile.displayName)
+  }
+
+  const setFinalContext = () => {
+    // set the final context. Cause it is possible that this is not the one we've originaly selected
+    const value = _getProviderDropdownValue()
+
+    setExecEnv(value)
+    // this.event.trigger('clearInstance', [])
+  }
+
+  const _getProviderDropdownValue = (): string => {
+    const provider = plugin.blockchain.getProvider()
+    const fork = plugin.blockchain.getCurrentFork()
+
+    return provider === 'vm' ? provider + '-' + fork : provider
+  }
+
+  const setExecEnv = (env: string) => {
+    dispatch(setExecutionEnvironment(env))
+  }
+
+  const setNetworkNameFromProvider = (networkName: string) => {
+    dispatch(setNetworkName(networkName))
+  }
+
+  const addExternalProvider = (network) => {
+    dispatch(addProvider(network))
+    // addTooltip(yo`<span><b>${network.name}</b> provider added</span>`)
+  }
+
+  const removeExternalProvider = (name) => {
+    dispatch(removeProvider(name))
+  }
+
+  const setExecutionContext = (executionContext: { context: string, fork: string }) => {
+    plugin.blockchain.changeExecutionContext(executionContext, () => {
+      // modalDialogCustom.prompt('External node request', this.web3ProviderDialogBody(), 'http://127.0.0.1:8545', (target) => {
+      //   this.blockchain.setProviderFromEndpoint(target, context, (alertMsg) => {
+      // if (alertMsg) addTooltip(alertMsg)
+      // setFinalContext()
+      // })
+      // }, this.setFinalContext.bind(this))
+    // }, (alertMsg) => {
+      // addTooltip(alertMsg)
+    }, setFinalContext())
+  }
+
+  return { runTab, setupEvents, fillAccountsList, setAccount, setUnit, setGasFee, addPluginProvider, removePluginProvider, setExecEnv, setFinalContext, setExecutionContext }
 }
