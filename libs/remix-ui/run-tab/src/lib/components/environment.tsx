67f5a226f290de88c689a7363622f0d94be7b82b
ioedeveloper <daviddisu8@gmail.com>
2021-11-16 15:06:09 +0100

    Added modal to runTab

--- libs/remix-ui/run-tab/src/lib/components/environment.tsx
+++ libs/remix-ui/run-tab/src/lib/components/environment.tsx
@@ -3,47 +3,14 @@ import React from 'react'
 import { EnvironmentProps } from '../types'
 
 export function EnvironmentUI (props: EnvironmentProps) {
-  // setDropdown (selectExEnv) {
-  //   this.selectExEnv = selectExEnv
-
-  //   const addProvider = (network) => {
-  //     selectExEnv.appendChild(yo`<option
-  //       title="provider name: ${network.name}"
-  //       value="${network.name}"
-  //       name="executionContext"
-  //     >
-  //       ${network.name}
-  //     </option>`)
-  //     addTooltip(yo`<span><b>${network.name}</b> provider added</span>`)
-  //   }
-
-  //   const removeProvider = (name) => {
-  //     var env = selectExEnv.querySelector(`option[value="${name}"]`)
-  //     if (env) {
-  //       selectExEnv.removeChild(env)
-  //       addTooltip(yo`<span><b>${name}</b> provider removed</span>`)
-  //     }
-  //   }
-  //   this.blockchain.event.register('addProvider', provider => addProvider(provider))
-  //   this.blockchain.event.register('removeProvider', name => removeProvider(name))
-
-  //   selectExEnv.addEventListener('change', (event) => {
-  //     const provider = selectExEnv.options[selectExEnv.selectedIndex]
-  //     const fork = provider.getAttribute('fork') // can be undefined if connected to an external source (web3 provider / injected)
-  //     let context = provider.value
-  //     context = context.startsWith('vm') ? 'vm' : context // context has to be 'vm', 'web3' or 'injected'
-  //     this.setExecutionContext({ context, fork })
-  //   })
-
-  //   selectExEnv.value = this._getProviderDropdownValue()
-  // }
-
   const handleChangeExEnv = (env: string) => {
-    props.setExecEnv(env)
-    const fork = provider.getAttribute('fork') // can be undefined if connected to an external source (web3 provider / injected)
+    const provider = props.providers.providerList.find(exEnv => exEnv.value === env)
+    const fork = provider.fork // can be undefined if connected to an external source (web3 provider / injected)
     let context = provider.value
+
     context = context.startsWith('vm') ? 'vm' : context // context has to be 'vm', 'web3' or 'injected'
-    this.setExecutionContext({ context, fork })
+    props.setExecutionContext({ context, fork })
+    props.setExecEnv(env)
   }
 
   return (
@@ -54,10 +21,10 @@ export function EnvironmentUI (props: EnvironmentProps) {
       <div className="udapp_environment">
         <select id="selectExEnvOptions" data-id="settingsSelectEnvOptions" className="form-control udapp_select custom-select" value={props.selectedEnv} onChange={(e) => { handleChangeExEnv(e.target.value) }}>
           {
-            props.providerList.map((provider) =>
-              <option id={provider.id} data-id={provider.dataId}
+            props.providers.providerList.map((provider, index) =>
+              <option id={provider.id} key={index} data-id={provider.dataId}
                 title={provider.title}
-                value={provider.value}> Web3 Provider
+                value={provider.value}> { provider.content }
               </option>
             )
           }
