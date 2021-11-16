bc3a186ac01e6137c72e19eaac304647828ded6d
bunsenstraat <filip.mertens@ethereum.org>
2021-11-16 17:03:32 +0100

    testing groups

---
+++ apps/remix-ide-e2e/src/tests/generalSettings.test.ts_group1.test.ts
@@ -0,0 +1,6 @@
+'use strict'
+import * as test from './generalSettings.test'
+import buildGroupTest from '../helpers/buildgrouptest'
+const group = 'group1'
+
+module.exports = buildGroupTest(group, test)
