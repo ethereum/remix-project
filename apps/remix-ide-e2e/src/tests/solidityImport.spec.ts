c9a0651cf6030c0f2fee6ab93bddaa13ba34968a
filip mertens <filip.mertens@ethereum.org>
2021-11-16 19:41:22 +0100

    reset lock

--- apps/remix-ide-e2e/src/tests/solidityImport.spec.ts
+++ apps/remix-ide-e2e/src/tests/solidityImport.spec.ts
@@ -38,7 +38,7 @@ module.exports = {
       .verifyContracts(['test7', 'ERC20'], { wait: 10000 })
   },
 
-  'Test Github Import - from other branch': function (browser: NightwatchBrowser) {
+  'Test Github Import - from other branch #group2': function (browser: NightwatchBrowser) {
     browser
       .setSolidityCompilerVersion('soljson-v0.5.0+commit.1d4f565a.js') // switch back to 0.5.0 : release-v2.3.0 branch is not solidity 0.6 compliant
       .addFile('Untitled5.sol', sources[4]['Untitled5.sol'])
@@ -46,7 +46,7 @@ module.exports = {
       .verifyContracts(['test8', 'ERC20', 'SafeMath'], { wait: 10000 })
   },
 
-  'Test Github Import - no branch specified': function (browser: NightwatchBrowser) {
+  'Test Github Import - no branch specified #group2': function (browser: NightwatchBrowser) {
     browser
       .setSolidityCompilerVersion('soljson-v0.8.0+commit.c7dfd78e.js') // open-zeppelin moved to pragma ^0.8.0 (master branch)
       .clickLaunchIcon('filePanel')
@@ -56,7 +56,7 @@ module.exports = {
       .verifyContracts(['test10', 'ERC20'], { wait: 10000 })
   },
 
-  'Test Github Import - raw URL': function (browser: NightwatchBrowser) {
+  'Test Github Import - raw URL #group2': function (browser: NightwatchBrowser) {
     browser
       .clickLaunchIcon('filePanel')
       .click('li[data-id="treeViewLitreeViewItemREADME.txt"')
@@ -65,7 +65,7 @@ module.exports = {
       .verifyContracts(['test11', 'ERC20'], { wait: 10000 })
   },
 
-  'Test switch to a github import from a solidity warning': function (browser: NightwatchBrowser) {
+  'Test switch to a github import from a solidity warning #group2': function (browser: NightwatchBrowser) {
     browser
       .setSolidityCompilerVersion('soljson-v0.7.4+commit.3f05b770.js')
       .clickLaunchIcon('filePanel')
@@ -81,7 +81,7 @@ module.exports = {
       })
   },
 
-  'Test NPM Import (with unpkg.com)': function (browser: NightwatchBrowser) {
+  'Test NPM Import (with unpkg.com) #group2': function (browser: NightwatchBrowser) {
     browser
       .setSolidityCompilerVersion('soljson-v0.8.7+commit.e28d00a7.js')
       .clickLaunchIcon('filePanel')
