e8b4199bd1efd451374a66763c6ecca888fe7c48
filip mertens <filip.mertens@ethereum.org>
2021-11-16 19:15:37 +0100

    reset

--- apps/remix-ide-e2e/src/tests/generalSettings.test.ts_group1.test.ts
+++
@@ -1,6 +0,0 @@
-'use strict'
-import * as test from './generalSettings.test'
-import buildGroupTest from '../helpers/buildgrouptest'
-const group = 'group1'
-
-module.exports = buildGroupTest(group, test)
