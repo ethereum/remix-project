559704c6032195d9e6037dc66fd5d139e4c99f1e
filip mertens <filip.mertens@ethereum.org>
2021-11-17 17:15:54 +0100

    verbose

--- apps/remix-ide-e2e/nightwatch.ts
+++ apps/remix-ide-e2e/nightwatch.ts
@@ -64,7 +64,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         'goog:chromeOptions': {
-          args: ['window-size=2560,1440', 'start-fullscreen', '--no-sandbox', '--headless'],
+          args: ['window-size=2560,1440', 'start-fullscreen', '--no-sandbox', '--headless', '--verbose'],
           extensions: [metamaskExtension]
         }
       }
