a7f5993cf2e1d54d2108f703f1a10b26c795da80
ioedeveloper <daviddisu8@gmail.com>
2021-11-19 14:00:32 +0100

    Removed custom hook.

--- libs/remix-ui/run-tab/src/lib/components/settingsUI.tsx
+++ libs/remix-ui/run-tab/src/lib/components/settingsUI.tsx
@@ -87,7 +87,7 @@ export function SettingsUI (props: SettingsProps) {
 
   return (
     <div className="udapp_settings">
-      <EnvironmentUI setExecEnv={props.setExecEnv} selectedEnv={props.selectExEnv} providers={props.providers} setExecutionContext={props.setExecutionContext} />
+      <EnvironmentUI setWeb3Endpoint={props.setWeb3Endpoint} selectedEnv={props.selectExEnv} providers={props.providers} setExecutionContext={props.setExecutionContext} externalEndpoint={props.externalEndpoint} />
       <NetworkUI networkName={props.networkName} />
       <AccountUI personalMode={props.personalMode} selectExEnv={props.selectExEnv} accounts={props.accounts} setAccount={props.setAccount} />
       <GasPriceUI gasLimit={props.gasLimit} setGasFee={props.setGasFee} />
