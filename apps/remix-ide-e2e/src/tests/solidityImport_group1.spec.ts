86f2f6babcea7cbdbc5a1a7ca7e130a1140ac6f0
bunsenstraat <filip.mertens@ethereum.org>
2021-11-21 13:23:43 +0100

    group script

--- apps/remix-ide-e2e/src/tests/solidityImport_group1.spec.ts
+++ apps/remix-ide-e2e/src/tests/solidityImport_group1.spec.ts
@@ -1,5 +1,5 @@
 'use strict'
-import * as test from './solidityImport.spec'
+import * as test from './solidityImport.test'
 import buildGroupTest from '../helpers/buildgrouptest'
 const group = 'group1'
 
