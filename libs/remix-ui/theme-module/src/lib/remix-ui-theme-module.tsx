77024b4783e958d67439969b8d98855f536a6081
Joseph Izang <josephizang@gmail.com>
2021-11-25 13:47:23 +0100

    theme module react lib

---
+++ libs/remix-ui/theme-module/src/lib/remix-ui-theme-module.tsx
@@ -0,0 +1,14 @@
+import './remix-ui-theme-module.module.css';
+
+/* eslint-disable-next-line */
+export interface RemixUiThemeModuleProps {}
+
+export function RemixUiThemeModule(props: RemixUiThemeModuleProps) {
+  return (
+    <div>
+      <h1>Welcome to remix-ui-theme-module!</h1>
+    </div>
+  );
+}
+
+export default RemixUiThemeModule;
