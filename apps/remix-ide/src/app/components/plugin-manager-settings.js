985d988592031138399458bdc8ae4ba2c8c98979
bunsenstraat <filip.mertens@ethereum.org>
2021-11-25 12:12:05 +0100

    remove yo from plugin man settings

--- apps/remix-ide/src/app/components/plugin-manager-settings.js
+++ apps/remix-ide/src/app/components/plugin-manager-settings.js
@@ -86,57 +86,6 @@ export class PluginManagerSettings {
     yo.update(this.currentSetting, this.settings())
   }
 
-  settings () {
-    const permissionByToPlugin = (toPlugin, funcObj) => {
-      const permissionByMethod = (methodName, fromPlugins) => {
-        const togglePermission = (fromPlugin) => {
-          this.permissions[toPlugin][methodName][fromPlugin].allow = !this.permissions[toPlugin][methodName][fromPlugin].allow
-        }
-        return Object.keys(fromPlugins).map(fromName => {
-          const fromPluginPermission = fromPlugins[fromName]
-          const checkbox = fromPluginPermission.allow
-            ? yo`<input onchange=${() => togglePermission(fromName)} class="mr-2" type="checkbox" checked id="permission-checkbox-${toPlugin}-${methodName}-${toPlugin}" aria-describedby="module ${fromPluginPermission} asks permission for ${methodName}" />`
-            : yo`<input onchange=${() => togglePermission(fromName)} class="mr-2" type="checkbox" id="permission-checkbox-${toPlugin}-${methodName}-${toPlugin}" aria-describedby="module ${fromPluginPermission} asks permission for ${methodName}" />`
-          return yo`
-            <div class="form-group ${css.permissionKey}">
-              <div class="${css.checkbox}">
-                ${checkbox}
-                <label for="permission-checkbox-${toPlugin}-${methodName}-${toPlugin}" data-id="permission-label-${toPlugin}-${methodName}-${toPlugin}">Allow <u>${fromName}</u> to call <u>${methodName}</u></label>
-              </div>
-              <i onclick="${() => this.clearPersmission(fromName, toPlugin, methodName)}" class="fa fa-trash-alt" data-id="pluginManagerSettingsRemovePermission-${toPlugin}-${methodName}-${toPlugin}"></i>
-            </div>
-          `
-        })
-      }
-
-      const permissionsByFunctions = Object
-        .keys(funcObj)
-        .map(methodName => permissionByMethod(methodName, funcObj[methodName]))
-
-      return yo`
-      <div border p-2>
-        <div class="pb-2 ${css.permissionKey}">
-          <h3>${toPlugin} permissions:</h3>
-          <i onclick="${() => this.clearAllPersmission(toPlugin)}" class="far fa-trash-alt" data-id="pluginManagerSettingsClearAllPermission-${toPlugin}"></i>
-        </div>
-        ${permissionsByFunctions}
-      </div>`
-    }
-
-    const byToPlugin = Object
-      .keys(this.permissions)
-      .map(toPlugin => permissionByToPlugin(toPlugin, this.permissions[toPlugin]))
-
-    const title = byToPlugin.length === 0
-      ? yo`<h4>No Permission requested yet.</h4>`
-      : yo`<h4>Current Permission settings</h4>`
-
-    return yo`<form class="${css.permissionForm}" data-id="pluginManagerSettingsPermissionForm">
-      ${title}
-      <hr/>
-      ${byToPlugin}
-    </form>`
-  }
 
   render () {
     return yo`
