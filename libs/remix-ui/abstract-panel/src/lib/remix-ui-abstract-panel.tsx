bf0cfa6491478a3a340cbeb7a8e38dafb07f5e51
davidzagi93@gmail.com <davidzagi93@gmail.com>
2021-11-30 12:23:36 +0100

    update to abstract panel

--- libs/remix-ui/abstract-panel/src/lib/remix-ui-abstract-panel.tsx
+++ libs/remix-ui/abstract-panel/src/lib/remix-ui-abstract-panel.tsx
@@ -2,12 +2,16 @@ import React from 'react' // eslint-disable-line
 import './remix-ui-abstract-panel.module.css';
 
 /* eslint-disable-next-line */
-export interface RemixUiAbstractPanelProps {}
+export interface RemixUiAbstractPanelProps {
+  reactView: any
+  plugin: any
+}
 
 export function RemixUiAbstractPanel(props: RemixUiAbstractPanelProps) {
   return (
     <div id="plugins" className="plugins">
-      
+      {console.log({ view: props.plugin })}
+      good
     </div>
   );
 }
