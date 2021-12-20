1ba79bdb9095d8668a8e710215222e1a2abb6153
filip mertens <filip.mertens@ethereum.org>
2021-12-20 17:44:00 +0100

    move stuff

--- apps/remix-ide/src/index.tsx
+++ apps/remix-ide/src/index.tsx
@@ -1,9 +1,16 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import AppComponent from './app'
+// eslint-disable-next-line no-unused-vars
+import { RemixApp } from '@remix-ui/app'
+
+const appComponent = new AppComponent()
+appComponent.run()
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <RemixApp app={appComponent}></RemixApp>
+  </React.StrictMode>,
   document.getElementById('root')
 )
