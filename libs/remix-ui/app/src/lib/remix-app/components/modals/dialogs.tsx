764944fde1f00c4e5b46e3cf283a147ac83dee81
filip mertens <filip.mertens@ethereum.org>
2021-12-22 11:02:50 +0100

    rm cons

--- libs/remix-ui/app/src/lib/remix-app/components/modals/dialogs.tsx
+++ libs/remix-ui/app/src/lib/remix-app/components/modals/dialogs.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useDialogDispatchers, useDialogs } from '../../context/provider'
 import { Toaster } from '@remix-ui/toaster'
 import ModalWrapper from './modal-wrapper'
-import { ModalTypes } from '../../types'
 
 const AppDialogs = () => {
-  const { modal, toast, alert, handleHideModal, handleToaster } = useDialogDispatchers()
+  const { handleHideModal, handleToaster } = useDialogDispatchers()
   const { focusModal, focusToaster } = useDialogs()
 
   return (
