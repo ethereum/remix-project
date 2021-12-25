901d8f5908be7a6c9d7db2112829367a47f46eaf
bunsenstraat <filip.mertens@ethereum.org>
2021-12-25 11:11:05 +0100

    lint fix

--- libs/remix-ui/panel/src/lib/remix-ui-panel.tsx
+++ libs/remix-ui/panel/src/lib/remix-ui-panel.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react' // eslint-disable-line
-import './panel.css';
-import RemixUIPanelHeader from './panel-header';
-import RemixUIPanelPlugin from './panel-plugin';
-import { PluginRecord } from './types';
+import './panel.css'
+import RemixUIPanelHeader from './panel-header'
+import RemixUIPanelPlugin from './panel-plugin'
+import { PluginRecord } from './types'
 
 /* eslint-disable-next-line */
 export interface RemixPanelProps {
@@ -10,19 +10,18 @@ export interface RemixPanelProps {
   header: JSX.Element
 }
 
-export function RemixPanel(props: RemixPanelProps) {
-
+export function RemixPanel (props: RemixPanelProps) {
   return (
     <div className='panel plugin-manager'>
       {props.header}
       <div className="pluginsContainer">
         {Object.values(props.plugins).map((pluginRecord) => {
-            return <RemixUIPanelPlugin key={pluginRecord.profile.name} pluginRecord={pluginRecord} />
+          return <RemixUIPanelPlugin key={pluginRecord.profile.name} pluginRecord={pluginRecord} />
         })}
       </div>
     </div>
 
-  );
+  )
 }
 
-export default RemixPanel;
+export default RemixPanel
