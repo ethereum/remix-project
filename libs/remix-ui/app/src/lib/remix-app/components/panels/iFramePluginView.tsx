c186c964d042760ad3b2343d638d4810aa649c85
bunsenstraat <filip.mertens@ethereum.org>
2021-12-27 13:51:07 +0100

    start main

--- libs/remix-ui/app/src/lib/remix-app/components/panels/iFramePluginView.tsx
+++ libs/remix-ui/app/src/lib/remix-app/components/panels/iFramePluginView.tsx
@@ -10,6 +10,7 @@ const IFramePluginView = (props: IFramePluginViewProps) => {
   const [loading, isLoading] = useState<boolean>(true)
 
   const loaded = () => {
+    console.log(props.plugin.profile.name + "loaded")
     props.plugin.shake(ref.current)
     isLoading(false)
   }
