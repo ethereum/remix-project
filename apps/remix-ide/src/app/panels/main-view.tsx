ca586c3b3036557ee1441a60e8e44262757a34c9
bunsenstraat <filip.mertens@ethereum.org>
2021-12-27 14:20:05 +0100

    start mainview

--- apps/remix-ide/src/app/panels/main-view.tsx
+++ apps/remix-ide/src/app/panels/main-view.tsx
@@ -1,8 +1,23 @@
+import { EditorContextListener } from '@remix-project/core-plugin'
 import { Plugin } from '@remixproject/engine'
-
+import { MainPanel } from '../components/main-panel'
+const EventManager = require('../../lib/events')
 export class MainView {
   fileManager: Plugin
+  event: any
+  tabProxy: Plugin
+  editor: Plugin
+  mainPanel: MainPanel
+  terminal: Plugin
+  appManager: Plugin
+  contextualListener: EditorContextListener
   constructor (contextualListener, editor, mainPanel, fileManager, appManager, terminal) {
     this.fileManager = fileManager
+    this.event = new EventManager()
+    this.editor = editor
+    this.terminal = terminal
+    this.appManager = appManager
+    this.mainPanel = mainPanel
+    this.contextualListener = contextualListener
   }
 }
