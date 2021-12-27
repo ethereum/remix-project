1edd28d9c97e56e483f4617dd839998ac2e44230
Joseph Izang <josephizang@gmail.com>
2021-12-27 15:54:08 +0100

    fix linting errors

--- libs/remix-ui/vertical-icons-panel/src/lib/reducers/verticalScrollReducer.ts
+++ libs/remix-ui/vertical-icons-panel/src/lib/reducers/verticalScrollReducer.ts
@@ -4,14 +4,14 @@ export type actionType = {
   payload: any
 }
 
-export function verticalScrollReducer(prevState: any, actionPayload: actionType) {
+export function verticalScrollReducer (prevState: any, actionPayload: actionType) {
   if (actionPayload.type === 'resize') {
-    actionPayload.payload.scrollHeight > actionPayload.payload.clientHeight
+    // actionPayload.payload.scrollHeight > actionPayload.payload.clientHeight
     console.log(`values being checked are ${actionPayload.payload.scrollHeight} > ${actionPayload.payload.clientHeight}`)
     const newvals = actionPayload.payload
     return { ...newvals }
   } else if (actionPayload.type === 'other') {
-    actionPayload.payload.scrollHeight > actionPayload.payload.clientHeight
+    // actionPayload.payload.scrollHeight > actionPayload.payload.clientHeight
   }
   return prevState
 }
