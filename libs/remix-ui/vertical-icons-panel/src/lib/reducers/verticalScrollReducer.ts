16e9b9ba1faef66abf05afcc1b9d44148810e74c
Joseph Izang <josephizang@gmail.com>
2021-12-28 02:52:15 +0100

    scroll parameters now dependent on state

--- libs/remix-ui/vertical-icons-panel/src/lib/reducers/verticalScrollReducer.ts
+++ libs/remix-ui/vertical-icons-panel/src/lib/reducers/verticalScrollReducer.ts
@@ -6,12 +6,10 @@ export type actionType = {
 
 export function verticalScrollReducer (prevState: any, actionPayload: actionType) {
   if (actionPayload.type === 'resize') {
-    // actionPayload.payload.scrollHeight > actionPayload.payload.clientHeight
     console.log(`values being checked are ${actionPayload.payload.scrollHeight} > ${actionPayload.payload.clientHeight}`)
-    const newvals = actionPayload.payload
-    return { ...newvals }
-  } else if (actionPayload.type === 'other') {
-    // actionPayload.payload.scrollHeight > actionPayload.payload.clientHeight
+    let { scrollHeight, clientHeight, scrollState } = actionPayload.payload
+    if (scrollHeight > clientHeight) scrollState = true
+    return { scrollHeight, clientHeight, scrollState }
   }
   return prevState
 }
