e5ce6e7c614147c8bf5f4c8f243147e17a6033c9
Joseph Izang <josephizang@gmail.com>
2021-12-28 02:54:18 +0100

    remove console log in reducer

--- libs/remix-ui/vertical-icons-panel/src/lib/reducers/verticalScrollReducer.ts
+++ libs/remix-ui/vertical-icons-panel/src/lib/reducers/verticalScrollReducer.ts
@@ -6,7 +6,6 @@ export type actionType = {
 
 export function verticalScrollReducer (prevState: any, actionPayload: actionType) {
   if (actionPayload.type === 'resize') {
-    console.log(`values being checked are ${actionPayload.payload.scrollHeight} > ${actionPayload.payload.clientHeight}`)
     let { scrollHeight, clientHeight, scrollState } = actionPayload.payload
     if (scrollHeight > clientHeight) scrollState = true
     return { scrollHeight, clientHeight, scrollState }
