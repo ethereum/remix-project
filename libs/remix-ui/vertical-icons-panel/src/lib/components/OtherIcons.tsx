27422c50d83b78159e105de538a15f3efd810f0d
Joseph Izang <josephizang@gmail.com>
2021-12-30 01:49:19 +0100

    react to statusChanged event

--- libs/remix-ui/vertical-icons-panel/src/lib/components/OtherIcons.tsx
+++ libs/remix-ui/vertical-icons-panel/src/lib/components/OtherIcons.tsx
@@ -33,14 +33,15 @@ function OtherIcons ({ verticalIconsPlugin, itemContextAction, addActive, remove
     Object.keys(verticalIconsPlugin.targetProfileForChange)
       .filter(customFilter)
       .forEach(p =>
-        verticalIconsPlugin.on(verticalIconsPlugin.targetProfileForChange[p].name, 'statusChanged', (iconStatus: IconStatus) => {
+        verticalIconsPlugin.on(verticalIconsPlugin.targetProfileForChange[p].name, 'statusChanged', (evt: Event, iconStatus: IconStatus) => {
           const action: IconBadgeReducerAction = {
             type: verticalIconsPlugin.targetProfileForChange[p].name,
             payload: { status: iconStatus, verticalIconPlugin: verticalIconsPlugin }
           }
+          console.log('check the event payload', { evt })
           dispatchStatusUpdate(action)
         }))
-  }, [])
+  }, [verticalIconsPlugin.targetProfileForChange, Object.keys(verticalIconsPlugin.targetProfileForChange).length])
   return (
     <div id="otherIcons">
       {
