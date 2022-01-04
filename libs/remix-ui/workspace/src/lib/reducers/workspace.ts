fc2280e2cea08d04521aacdcd226bb6506508ff3
aniket-engg <aniketengg.225@gmail.com>
2022-01-04 13:59:53 +0530

    do not merge files for localhost workspace

--- libs/remix-ui/workspace/src/lib/reducers/workspace.ts
+++ libs/remix-ui/workspace/src/lib/reducers/workspace.ts
@@ -674,8 +674,6 @@ const fetchDirectoryContent = (state: BrowserState, payload: { fileTree, path: s
   } else {
     if (payload.path === state.mode || payload.path === '/') {
       let files = normalize(payload.fileTree, payload.path, payload.type)
-
-      files = _.merge(files, state[state.mode].files[state.mode])
       if (deletePath) delete files[deletePath]
       return { [state.mode]: files }
     } else {
