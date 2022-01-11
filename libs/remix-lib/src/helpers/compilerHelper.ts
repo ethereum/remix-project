7be12dd1b2942c7f63e2b9a03587d2f7e2fb0991
yann300 <yann.levreau@gmail.com>
2022-01-11 15:23:14 +0100

    fix references

--- libs/remix-lib/src/helpers/compilerHelper.ts
+++ libs/remix-lib/src/helpers/compilerHelper.ts
@@ -1,5 +1,3 @@
-import {Language} from "@remix-project/remix-solidity-ts";
-
 export function compilerInput (contracts) {
   return JSON.stringify({
     language: 'Solidity',
@@ -21,14 +19,4 @@ export function compilerInput (contracts) {
       }
     }
   })
-}
-
-export const Languages = ['Solidity', 'Yul']
-
-export function getValidLanguage (val: string): Language {
-  if (val !== undefined && val !== null && val) {
-    const lang = val.slice(0, 1).toUpperCase() + val.slice(1).toLowerCase()
-    return Languages.indexOf(lang) > -1 ? lang as Language : null
-  }
-  return null
 }
