5fd0ef1a9eb78b6776ac3a246c73c9287fe255a5
lianahus <liana@ethereum.org>
2022-01-20 12:37:54 +0100

    icon alignment fixed

--- libs/remix-ui/vertical-icons-panel/src/lib/remix-ui-vertical-icons-panel.tsx
+++ libs/remix-ui/vertical-icons-panel/src/lib/remix-ui-vertical-icons-panel.tsx
@@ -109,19 +109,19 @@ const RemixUiVerticalIconsPanel = ({
             itemContextAction={itemContextAction}
           />
         </div>
-            <div>
-              { scrollableRef.current && scrollableRef.current.scrollHeight > scrollableRef.current.clientHeight ? (<Chevron
-                divElementRef={scrollableRef}
-                direction='down'
-                cssRule={'fa fa-chevron-down remixui_icon-chevron my-0'}
-              />) : null }
-              <IconList
-                theme={theme}
-                icons={icons.filter((p) => p.profile.name === 'settings' || p.profile.name === 'pluginManager')}
-                verticalIconsPlugin={verticalIconsPlugin}
-                itemContextAction={itemContextAction}
-              />
-            </div>
+        <div className="remixui_default-icons-container border-0">
+          { scrollableRef.current && scrollableRef.current.scrollHeight > scrollableRef.current.clientHeight ? (<Chevron
+            divElementRef={scrollableRef}
+            direction='down'
+            cssRule={'fa fa-chevron-down remixui_icon-chevron my-0'}
+          />) : null }
+          <IconList
+            theme={theme}
+            icons={icons.filter((p) => p.profile.name === 'settings' || p.profile.name === 'pluginManager')}
+            verticalIconsPlugin={verticalIconsPlugin}
+            itemContextAction={itemContextAction}
+          />
+        </div>
       </div>
     </div>
   )
