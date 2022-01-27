031dea192dc7b570713285273093ecf356a3792a
aniket-engg <aniketengg.225@gmail.com>
2022-01-27 13:30:39 +0530

    splash screen before app loading

--- libs/remix-ui/app/src/index.ts
+++ libs/remix-ui/app/src/index.ts
@@ -1,4 +1,4 @@
-export { default as default } from './lib/remix-app/remix-app'
+export { default as RemixApp } from './lib/remix-app/remix-app'
 export { dispatchModalContext } from './lib/remix-app/context/context'
 export { ModalProvider } from './lib/remix-app/context/provider'
 export { AppModal } from './lib/remix-app/interface/index'
