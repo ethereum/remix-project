ec8d340aaaafb10788929bcda6fac7f6be77e9f5
lianahus <liana@ethereum.org>
2022-02-18 12:06:04 +0100

    style

--- libs/remix-ui/terminal/src/lib/terminalWelcome.tsx
+++ libs/remix-ui/terminal/src/lib/terminalWelcome.tsx
@@ -3,7 +3,7 @@ import React from 'react' // eslint-disable-line
 const TerminalWelcomeMessage = ({ packageJson }) => {
   return (
     <div className="remix_ui_terminal_block px-4 " data-id="block_null">
-      <div> - Welcome to Remix {packageJson} - </div><br />
+      <div className="remix_ui_terminal_welcome"> Welcome to Remix {packageJson} </div><br />
       <div>You can use this terminal to: </div>
       <ul className='ml-0 mr-4'>
         <li>Check transactions details and start debugging.</li>
