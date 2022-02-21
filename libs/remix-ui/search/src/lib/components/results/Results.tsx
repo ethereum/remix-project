e7e7f879196dc7b5c7170d019c7cd4292010aeab
filip mertens <filip.mertens@ethereum.org>
2022-02-21 13:56:43 +0100

    clean up

--- libs/remix-ui/search/src/lib/components/results/Results.tsx
+++ libs/remix-ui/search/src/lib/components/results/Results.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { SearchContext } from '../../context/context'
 import { ResultItem } from './ResultItem'
 
