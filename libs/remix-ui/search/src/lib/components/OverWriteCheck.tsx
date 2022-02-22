c15e7cd9d8cd8dacf15f972f90846baf0cac9591
filip mertens <filip.mertens@ethereum.org>
2022-02-22 11:52:57 +0100

    cleanup

--- libs/remix-ui/search/src/lib/components/OverWriteCheck.tsx
+++ libs/remix-ui/search/src/lib/components/OverWriteCheck.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext } from 'react'
 import { SearchContext } from '../context/context'
 
 export const OverWriteCheck = props => {
