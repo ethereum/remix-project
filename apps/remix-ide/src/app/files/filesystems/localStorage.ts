41f441748a8832b34b8159bf02e5788c3a386cf9
bunsenstraat <filip.mertens@ethereum.org>
2022-02-27 16:57:12 +0100

    move to components

---
+++ apps/remix-ide/src/app/files/filesystems/localStorage.ts
@@ -0,0 +1,58 @@
+import { fileSystem } from "../fileSystem";
+
+export class localStorageFS extends fileSystem {
+
+    constructor() {
+        super()
+        this.name = 'localstorage'
+    }
+    load = async () => {
+        const me = this
+        return new Promise((resolve, reject) => {
+            try {
+                // eslint-disable-next-line no-undef
+                BrowserFS.install(window)
+                // eslint-disable-next-line no-undef
+                BrowserFS.configure({
+                    fs: 'LocalStorage'
+                }, async function (e) {
+                    if (e) {
+                        console.log('BROWSEFS Error: ' + e)
+                        reject(e)
+                    } else {
+                        me.fs = { ...window.require('fs') }
+                        me.fsCallBack = window.require('fs')
+                        me.fs.readdir = me.fs.readdirSync
+                        me.fs.readFile = me.fs.readFileSync
+                        me.fs.writeFile = me.fs.writeFileSync
+                        me.fs.stat = me.fs.statSync
+                        me.fs.unlink = me.fs.unlinkSync
+                        me.fs.rmdir = me.fs.rmdirSync
+                        me.fs.mkdir = me.fs.mkdirSync
+                        me.fs.rename = me.fs.renameSync
+                        me.fs.exists = me.fs.existsSync
+                        me.loaded = true
+                        resolve(true)
+                    }
+                })
+            } catch (e) {
+                console.log('BrowserFS is not ready!')
+                reject(e)
+            }
+        })
+    }
+
+    test = async () => {
+        return new Promise((resolve, reject) => {
+            const test = 'test';
+            try {
+                localStorage.setItem(test, test);
+                localStorage.removeItem(test);
+                resolve(true)
+            } catch(e) {
+                reject(e)
+            }
+        })
+    }
+
+}
