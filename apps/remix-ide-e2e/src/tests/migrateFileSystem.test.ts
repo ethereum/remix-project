be8dba8e3f4769d084c4b2c2f8ca0f5efc516ed7
filip mertens <filip.mertens@ethereum.org>
2022-02-28 13:07:45 +0100

    more testing

--- apps/remix-ide-e2e/src/tests/migrateFileSystem.test.ts
+++ apps/remix-ide-e2e/src/tests/migrateFileSystem.test.ts
@@ -36,7 +36,7 @@ module.exports = {
         browser.assert.ok(content.includes('function retrieve() public view returns (uint256){'))
       })
   },
-  'Should load the testmigration url with local storage enablbed #group3': function (browser: NightwatchBrowser) {
+  'Should load the testmigration url with local storage anabled #group3': function (browser: NightwatchBrowser) {
     browser.url('http://127.0.0.1:8080?e2e_testmigration=true&e2e_testmigration_fallback=true')
       .pause(6000)
       .switchBrowserTab(0)
@@ -46,7 +46,17 @@ module.exports = {
       .waitForElementVisible('[id="remixTourSkipbtn"]')
       .click('[id="remixTourSkipbtn"]')
   },
-  'should have localstorage storage in terminal #group2 #group3': function (browser: NightwatchBrowser) {
+  'Should generate error in migration by deleting indexedDB and falling back to local storage with test #group5': function (browser: NightwatchBrowser) {
+    browser.url('http://127.0.0.1:8080?e2e_testmigration=true')
+      .pause(6000)
+      .switchBrowserTab(0)
+      .maximizeWindow().execute(('delete window.indexedDB'))
+      .waitForElementVisible('*[data-id="skipbackup-btn"]', 5000)
+      .click('*[data-id="skipbackup-btn"]')
+      .waitForElementVisible('[id="remixTourSkipbtn"]')
+      .click('[id="remixTourSkipbtn"]')
+  },
+  'should have localstorage storage in terminal #group2 #group3 #group5': function (browser: NightwatchBrowser) {
     browser.assert.containsText('*[data-id="terminalJournal"]', 'localstorage')
   },
   'Should have README file with TEST README as content #group1 #group3': function (browser: NightwatchBrowser) {
@@ -58,12 +68,13 @@ module.exports = {
         browser.assert.equal(content, 'TEST README')
       })
   },
-  'Should have a workspace_test #group1 #group3': function (browser: NightwatchBrowser) {
+  // these are test data entries
+  'Should have a workspace_test #group1 #group3 #group5': function (browser: NightwatchBrowser) {
     browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]', 5000)
       .click('*[data-id="workspacesSelect"] option[value="workspace_test"]')
       .waitForElementVisible('*[data-id="treeViewLitreeViewItemtest_contracts"]')
   },
-  'Should have a sol file with test data #group1 #group3': function (browser: NightwatchBrowser) {
+  'Should have a sol file with test data #group1 #group3 #group5': function (browser: NightwatchBrowser) {
     browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]', 5000)
       .click('*[data-id="treeViewLitreeViewItemtest_contracts"]')
       .openFile('test_contracts/1_Storage.sol')
@@ -71,7 +82,7 @@ module.exports = {
         browser.assert.equal(content, 'testing')
       })
   },
-  'Should have a artifacts file with JSON test data #group1 #group3': function (browser: NightwatchBrowser) {
+  'Should have a artifacts file with JSON test data #group1 #group3 #group5': function (browser: NightwatchBrowser) {
     browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]', 5000)
       .click('*[data-id="treeViewLitreeViewItemtest_contracts/artifacts"]')
       .openFile('test_contracts/artifacts/Storage_metadata.json')
@@ -80,10 +91,11 @@ module.exports = {
         browser.assert.equal(metadata.test, 'data')
       })
   },
-  'Should have a empty workspace #group1 #group3': function (browser: NightwatchBrowser) {
+  'Should have a empty workspace #group1 #group3 #group5': function (browser: NightwatchBrowser) {
     browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]', 5000)
       .click('*[data-id="workspacesSelect"] option[value="emptyspace"]')
   },
+  // end of test data entries
   'Should load with all storage blocked #group4': function (browser: NightwatchBrowser) {
     browser.url('http://127.0.0.1:8080?e2e_testblock_storage=true')
       .pause(6000)
@@ -91,4 +103,5 @@ module.exports = {
       .maximizeWindow()
       .assert.containsText('.alert-warning', 'Your browser does not support')
   },
+
 }
