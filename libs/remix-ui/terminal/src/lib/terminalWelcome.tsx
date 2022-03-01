f8ca4aa38e016e54a5cc38f48ad9e6f048f25d38
filip mertens <filip.mertens@ethereum.org>
2022-03-01 16:21:12 +0100

    tracking events

--- libs/remix-ui/terminal/src/lib/terminalWelcome.tsx
+++ libs/remix-ui/terminal/src/lib/terminalWelcome.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect } from 'react' // eslint-disable-line
 
 const TerminalWelcomeMessage = ({ packageJson, storage }) => {
-
-  useEffect(() => {
-    console.log(storage)
-  }, [
-    storage
-  ])
   return (
     <div className="remix_ui_terminal_block px-4 " data-id="block_null">
       <div className="remix_ui_terminal_welcome"> Welcome to Remix {packageJson} </div><br />
-      <div className="">Your files are stored in {(window as any).remixFileSystem.name}, {storage && storage.usage} used</div><br />
+      <div className="">Your files are stored in {(window as any).remixFileSystem.name}, {storage} used</div><br />
       <div>You can use this terminal to: </div>
       <ul className='ml-0 mr-4'>
         <li>Check transactions details and start debugging.</li>
