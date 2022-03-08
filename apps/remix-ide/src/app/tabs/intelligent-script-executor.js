b2f18795b5d05188960793b1a7da591a5a523e54
yann300 <yann.levreau@gmail.com>
2022-03-08 20:21:11 +0100

    listen on artefactsUpdated instead

--- apps/remix-ide/src/app/tabs/intelligent-script-executor.js
+++ apps/remix-ide/src/app/tabs/intelligent-script-executor.js
@@ -16,20 +16,18 @@ export class IntelligentScriptExecutor extends Plugin {
   }
 
   onActivation () {
-    this.on('solidity', 'compilationFinished', async (file, source, languageVersion, data, input, version) => {
-      const currentFile = await this.call('fileManager', 'file') 
-      if (data && data.sources[currentFile] &&
-        data.sources[currentFile].ast && data.sources[currentFile].ast.nodes && data.sources[currentFile].ast.nodes.length) {
-          const nodes = data.sources[currentFile].ast.nodes
-          for (let node of nodes) {
-            if (node.documentation && node.documentation.text && node.documentation.text.startsWith('@custom:dev-run-script')) {
-              const text = node.documentation.text.replace('@custom:dev-run-script', '').trim()
-              await this.call('terminal', 'log', `running ${text} ...`)
-              const content = await this.call('fileManager', 'readFile', text)
-              await this.call('udapp', 'clearAllInstances')
-              await this.call('scriptRunner', 'execute', content)
-            }
-          }
+    let listen = false
+    setTimeout(() => {
+      listen = true
+    }, 500)
+    this.on('compilerMetadata', 'artefactsUpdated', async (fileName, contract) => {
+      if (!listen) return
+      if (contract.object && contract.object.devdoc['custom:dev-run-script']) {
+        const text = contract.object.devdoc['custom:dev-run-script']
+        await this.call('terminal', 'log', `running ${text} ...`)
+        const content = await this.call('fileManager', 'readFile', text)
+        await this.call('udapp', 'clearAllInstances')
+        await this.call('scriptRunner', 'execute', content)
       }      
     })
   }
