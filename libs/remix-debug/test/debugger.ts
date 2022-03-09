9bdcac2206a102dfce2b6579aaa0ea7023ea2652
yann300 <yann.levreau@gmail.com>
2022-03-09 15:27:13 +0100

    fix unit test

--- libs/remix-debug/test/debugger.ts
+++ libs/remix-debug/test/debugger.ts
@@ -277,7 +277,7 @@ function testDebugging (debugManager) {
       return sourceMappingDecoder.convertOffsetToLineColumn(rawLocation, sourceMappingDecoder.getLinebreakPositions(ballot))
     }})
 
-    breakPointManager.add({fileName: 'test.sol', row: 38})
+    breakPointManager.add({fileName: 0, row: 38})
 
     breakPointManager.event.register('breakpointHit', function (sourceLocation, step) {
       console.log('breakpointHit')
