e8bdb01d2b6bd57fd1086abd9faa11f4c41518bd
aniket-engg <aniketengg.225@gmail.com>
2022-03-10 13:31:20 +0530

    pause

--- apps/remix-ide-e2e/src/tests/terminal.test.ts
+++ apps/remix-ide-e2e/src/tests/terminal.test.ts
@@ -104,7 +104,7 @@ module.exports = {
       .click('[data-id="treeViewDivtreeViewItemcontracts"]')
       .openFile('contracts/2_Owner.sol')
       .clickLaunchIcon('solidity')
-      .click('*[data-id="compilerContainerCompileBtn"]').pause(20000) // compile Owner
+      .click('*[data-id="compilerContainerCompileBtn"]').pause(5000) // compile Owner
       .executeScript('remix.execute(\'deployWithEthersJs.js\')')
       .waitForElementContainsText('*[data-id="terminalJournal"]', 'Contract Address:', 60000)
       .waitForElementContainsText('*[data-id="terminalJournal"]', '0xd9145CCE52D386f254917e481eB44e9943F39138', 60000)
@@ -127,9 +127,9 @@ module.exports = {
       .openFile('contracts/1_Storage.sol')
       .clickLaunchIcon('solidity')
       .click('*[data-id="compilerContainerCompileBtn"]')
-      .pause(10000) // compile Storage
+      .pause(2000) // compile Storage
       .executeScript('remix.execute(\'scripts/storage.test.js\')')
-      .pause(5000)
+      .pause(1000)
       .waitForElementContainsText('*[data-id="terminalJournal"]', 'Running tests....')
       .waitForElementContainsText('*[data-id="terminalJournal"]', 'storage contract Address:')
       .waitForElementContainsText('*[data-id="terminalJournal"]', '✓ test initial value')
