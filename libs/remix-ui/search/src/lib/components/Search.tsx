c7666b10f1bed547e1f00234249aad3dd8beeb29
filip mertens <filip.mertens@ethereum.org>
2022-03-11 13:17:00 +0100

    fixes

--- libs/remix-ui/search/src/lib/components/Search.tsx
+++ libs/remix-ui/search/src/lib/components/Search.tsx
@@ -7,6 +7,7 @@ import { Include } from './Include'
 import { Exclude } from './Exclude'
 import { Replace } from './Replace'
 import { OverWriteCheck } from './OverWriteCheck'
+import { FindContainer } from './FindContainer'
 
 export const SearchTab = props => {
 
@@ -16,8 +17,7 @@ return (
     <>
     <div className="search_plugin_search_tab pl-2 pr-2">
       <SearchProvider plugin={plugin}>
-        <Find></Find>
-        <Replace></Replace>
+        <FindContainer></FindContainer>
         <Include></Include>
         <Exclude></Exclude>
         <OverWriteCheck></OverWriteCheck>
