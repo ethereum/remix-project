76278dca0525321222349797ea7fc1d4a4782a3e
yann300 <yann.levreau@gmail.com>
2022-03-15 16:53:08 +0100

    fix tests

--- apps/remix-ide-e2e/src/commands/openFile.ts
+++ apps/remix-ide-e2e/src/commands/openFile.ts
@@ -15,7 +15,7 @@ class OpenFile extends EventEmitter {
 
 // click on fileExplorer can toggle it. We go through settings to be sure FE is open
 function openFile (browser: NightwatchBrowser, name: string, done: VoidFunction) {
-  browser.clickLaunchIcon('settings').clickLaunchIcon('filePanel')
+  browser.clickLaunchIcon('settings').pause(500).clickLaunchIcon('filePanel')
     .waitForElementVisible('li[data-id="treeViewLitreeViewItem' + name + '"', 60000)
     .click('li[data-id="treeViewLitreeViewItem' + name + '"')
     .pause(2000)
