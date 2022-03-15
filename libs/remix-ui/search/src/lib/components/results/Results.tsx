684a7b263be91aa150a7a95c9c8ec89d26b1df8e
bunsenstraat <filip.mertens@ethereum.org>
2022-03-15 12:41:08 +0100

    merge

--- libs/remix-ui/search/src/lib/components/results/Results.tsx
+++ libs/remix-ui/search/src/lib/components/results/Results.tsx
@@ -6,8 +6,8 @@ export const Results = () => {
   const { state } = useContext(SearchContext)
   return (
     <div data-id='search_results' className='mt-2'>
-      {state.find ? <div className='search_plugin_result_count_number badge badge-pill badge-secondary'>{state.count} results</div>: null}
-      {state.find && state.count >= state.maxResults? <div className='alert alert-warning mt-1'>The result set only contains a subset of all matches<br></br>Please narrow down your search.</div>: null}
+      {state.find ? <div className='search_plugin_result_count_number badge badge-pill badge-secondary'>showing {state.count} results {state.fileCount} in files</div>: null}
+      {state.find && state.clipped? <div className='alert alert-warning mt-1'>The result set only shows a subset of all matches<br></br>Please narrow down your search.</div>: null}
       {state.searchResults &&
         state.searchResults.map((result, index) => {
           return index <state.maxFiles? <ResultItem key={index} file={result} />: null
