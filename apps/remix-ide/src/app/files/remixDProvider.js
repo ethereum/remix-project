da7aeecc4178d08ebf47cd24e3589af1db1f177f
David Disu <daviddisu8@gmail.com>
2022-03-16 11:28:59 +0100

    Prevent paste in within ancestor folder

--- apps/remix-ide/src/app/files/remixDProvider.js
+++ apps/remix-ide/src/app/files/remixDProvider.js
@@ -1,5 +1,6 @@
 'use strict'
 const FileProvider = require('./fileProvider')
+const pathModule = require('path')
 
 module.exports = class RemixDProvider extends FileProvider {
   constructor (appManager) {
@@ -206,5 +207,13 @@ module.exports = class RemixDProvider extends FileProvider {
     const unprefixedpath = this.removePrefix(path)
     if (!this._isReady) throw new Error('provider not ready')
     return await this._appManager.call('remixd', 'isFile', { path: unprefixedpath })
+  }
+
+  isSubDirectory (parent, child) {
+    if (!parent) return false
+    if (parent === child) return true
+    const relative = pathModule.relative(parent, child)
+
+    return !!relative && relative.split(pathModule.sep)[0] !== '..'
   }
 }
