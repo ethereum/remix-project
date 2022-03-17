d2612ebed53a2f56811a8cb4759debe3d5a3a8c8
yann300 <yann.levreau@gmail.com>
2022-03-17 12:13:28 +0100

    improve script executor

---
+++ apps/remix-ide/src/app/tabs/intelligent-script-executor.ts
@@ -0,0 +1,60 @@
+import { Plugin } from '@remixproject/engine'
+import * as packageJson from '../../../../../package.json'
+
+export const profile = {
+  name: 'intelligentScriptExecutor',
+  displayName: 'Intelligent Script Executor',
+  description: 'after each compilation, run the script defined in Natspec.',
+  methods: [],
+  version: packageJson.version,
+  kind: 'none'
+}
+
+type listener = (event: KeyboardEvent) => void
+
+export class IntelligentScriptExecutor extends Plugin {
+  executionListener: listener
+  targetFileName: string
+
+  constructor () {
+    super(profile)
+    this.executionListener = async (e) => {
+      // ctrl+e or command+e
+      const file = await this.call('fileManager', 'file')
+      if ((e.metaKey || e.ctrlKey) && e.keyCode === 69 && file !== '') {
+        if (file.endsWith('.sol')) {
+          e.preventDefault()
+          this.targetFileName = file
+          await this.call('solidity', 'compile', file)
+        } else if (file.endsWith('.js')) {
+          e.preventDefault()
+          this.runScript(file)
+        }
+      }
+    }
+  }
+
+  async runScript (fileName) {
+    await this.call('terminal', 'log', `running ${fileName} ...`)
+    const content = await this.call('fileManager', 'readFile', fileName)
+    await this.call('udapp', 'clearAllInstances')
+    await this.call('scriptRunner', 'execute', content)
+  }
+
+  onActivation () {
+    window.document.addEventListener('keydown', this.executionListener)
+
+    this.on('compilerMetadata', 'artefactsUpdated', async (fileName, contract) => {
+      if (this.targetFileName === contract.file && contract.object && contract.object.devdoc['custom:dev-run-script']) {
+        const file = contract.object.devdoc['custom:dev-run-script']
+        if (file) this.runScript(file)
+      }
+      this.targetFileName = null
+    })
+  }
+
+  onDeactivation () {
+    window.document.removeEventListener('keydown', this.executionListener)
+    this.off('compilerMetadata', 'artefactsUpdated')
+  }
+}
