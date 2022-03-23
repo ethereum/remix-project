7870b310bb98589aef96c56a7a92453a1e3fed4c
lianahus <liana@ethereum.org>
2022-03-23 11:06:47 +0100

    fixed recorder card

--- libs/remix-ui/workspace/src/lib/remix-ui-workspace.tsx
+++ libs/remix-ui/workspace/src/lib/remix-ui-workspace.tsx
@@ -228,9 +228,9 @@ export function Workspace () {
             </div>
           </header>
         </div>
-        <div className='remixui_fileExplorerTree'>
-          <div>
-            <div className='pl-2 remixui_treeview' data-id='filePanelFileExplorerTree'>
+        <div className='h-100 remixui_fileExplorerTree'>
+          <div className='h-100'>
+            <div className='pl-2 h-100 remixui_treeview' data-id='filePanelFileExplorerTree'>
               { (global.fs.mode === 'browser') && (currentWorkspace !== NO_WORKSPACE) &&
                   <FileExplorer
                     name={currentWorkspace}
