16852a0366db9955e9d8822ffc67c7f4b7bd076b
yann300 <yann.levreau@gmail.com>
2022-03-28 11:44:57 +0200

    remove comment

--- libs/remix-ui/editor/src/lib/remix-ui-editor.tsx
+++ libs/remix-ui/editor/src/lib/remix-ui-editor.tsx
@@ -4,7 +4,6 @@ import Editor, { loader } from '@monaco-editor/react'
 import { reducerActions, reducerListener, initialState } from './actions/editor'
 import { language, conf } from './syntax'
 import { cairoLang, cairoConf } from './cairoSyntax'
-// import { ethersDTS, test } from './types'
 
 import './remix-ui-editor.css'
 import { loadTypes } from './web-types'
