3c4ec22c56150321db5c02e057a4907ecfc83ae1
yann300 <yann.levreau@gmail.com>
2022-03-28 18:26:40 +0200

    remove using localhost

--- apps/remix-ide/src/remixAppManager.js
+++ apps/remix-ide/src/remixAppManager.js
@@ -132,7 +132,6 @@ export class RemixAppManager extends PluginManager {
       }
     }
     return plugins.map(plugin => {
-      if (plugin.name === 'scriptRunner') plugin.url = 'http://127.0.0.1:8081'
       return new IframePlugin(plugin)
       // return new IframeReactPlugin(plugin)
     })
