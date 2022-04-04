2ecb46c33f2d3bf38c3adb3e7ac64aca2d67c647
Aniket-Engg <aniketengg.225@gmail.com>
2022-04-04 17:58:42 +0530

    truffle compile in remixd

--- apps/remix-ide/src/app/panels/file-panel.js
+++ apps/remix-ide/src/app/panels/file-panel.js
@@ -7,6 +7,7 @@ import Registry from '../state/registry'
 import { RemixdHandle } from '../plugins/remixd-handle'
 const { GitHandle } = require('../files/git-handle.js')
 const { HardhatHandle } = require('../files/hardhat-handle.js')
+const { TruffleHandle } = require('../files/truffle-handle.js')
 const { SlitherHandle } = require('../files/slither-handle.js')
 
 /*
@@ -51,6 +52,7 @@ module.exports = class Filepanel extends ViewPlugin {
     this.remixdHandle = new RemixdHandle(this.fileProviders.localhost, appManager)
     this.gitHandle = new GitHandle()
     this.hardhatHandle = new HardhatHandle()
+    this.truffleHandle = new TruffleHandle()
     this.slitherHandle = new SlitherHandle()
     this.workspaces = []
     this.appManager = appManager
