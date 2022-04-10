51e1c6b0f849edfd968477568fed464e8eca8740
filip mertens <filip.mertens@ethereum.org>
2022-04-10 13:34:46 +0200

    invalidate cache

---
+++ apps/remix-ide/src/assets/js/loader.js
@@ -0,0 +1,22 @@
+createScriptTag = function(url, type) {
+  var script = document.createElement('script');
+  script.src = url;
+  script.type = type;
+  script.async = false;
+  script.defer = true;
+  document.getElementsByTagName('head')[0].appendChild(script);
+};
+
+fetch('assets/version.json').then(response => {
+  response.text().then(function(data) {
+    console.log(data);
+    const version = JSON.parse(data).version;
+    console.log(`Loading Remix ${version}`);
+    createScriptTag(`main.${version}.js`, 'text/javascript');
+    createScriptTag(`polyfills.${version}.js`, 'module');
+    if (version.includes('dev')) {
+      createScriptTag(`vendor.${version}.js`, 'module');
+      createScriptTag(`runtime.js`, 'module');
+    }
+  });
+});
