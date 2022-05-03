58bcd7f20526851ecdde07f0a31ecb4d2d63faa1
filip mertens <filip.mertens@ethereum.org>
2022-05-03 17:44:26 +0200

    flaky tests

---
+++ apps/remix-ide/ci/flaky.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
+echo "$BUILD_ID"
+TEST_EXITCODE=0
+
+npm run ganache-cli &
+npm run serve:production &
+echo 'sharing folder: ' $PWD '/apps/remix-ide/contracts' &
+npm run remixd &
+
+sleep 5
+
+npm run build:e2e
+
+TESTFILES=$(grep -IRiL "\'@disabled\': \?true" "dist/apps/remix-ide-e2e/src/tests" | grep "\.flaky" | sort | circleci tests split )
+for TESTFILE in $TESTFILES; do
+    npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js $TESTFILE --env=$1  || TEST_EXITCODE=1
+done
+
+echo "$TEST_EXITCODE"
+if [ "$TEST_EXITCODE" -eq 1 ]
+then
+  exit 1
+fi
