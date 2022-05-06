ddf5ef485a13eb9623a21fa064afabd51cade67c
Aniket-Engg <aniketengg.225@gmail.com>
2022-05-06 14:12:47 +0530

    default template in dir

---
+++ libs/remix-ui/workspace/src/lib/templates/remixDefault/index.ts
@@ -0,0 +1,13 @@
+export default {
+    'contracts/1_Storage.sol': require('./contracts/1_Storage.sol').default,
+    'contracts/2_Owner.sol': require('./contracts/2_Owner.sol').default,
+    'contracts/3_Ballot.sol': require('./contracts/3_Ballot.sol').default,
+    'scripts/deploy_with_ethers.ts': require('./scripts/deploy_with_ethers.ts').default,
+    'scripts/deploy_with_web3.ts': require('./scripts/deploy_with_web3.ts').default,
+    'scripts/ethers.ts': require('./scripts/ethers.ts').default,
+    'scripts/web3.ts': require('./scripts/web3.ts').default,
+    'tests/Ballot_test.sol': require('./tests/Ballot_test.sol').default,
+    'tests/storage.test.js': require('./tests/storage.test.js').default,
+    'README.txt': require('./README.txt').default,
+
+}
