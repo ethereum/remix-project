0a51ed95fc68f73e1a0f6d8eeea68f25e501c783
Aniket-Engg <aniketengg.225@gmail.com>
2022-05-10 11:35:13 +0530

    erc20 template

---
+++ libs/remix-ws-templates/erc20/index.js
@@ -0,0 +1,8 @@
+export default {
+    'contracts/SampleERC20.sol': require('./contracts/SampleERC20.sol').default,
+    'scripts/deploy_with_ethers.ts': require('./scripts/deploy_with_ethers.ts').default,
+    'scripts/deploy_with_web3.ts': require('./scripts/deploy_with_web3.ts').default,
+    'scripts/ethers.ts': require('./scripts/ethers.ts').default,
+    'scripts/web3.ts': require('./scripts/web3.ts').default,
+    'tests/SampleERC20_test.sol': require('./tests/SampleERC20_test.sol').default
+}
--- libs/remix-ws-templates/index.js
+++ libs/remix-ws-templates/index.js
@@ -1,3 +1,3 @@
 export { default as remixDefault } from './remixDefault'
-// export { default as erc20 } from './erc20'
+export { default as erc20 } from './erc20'
 // export { default as blank } from './blank'
