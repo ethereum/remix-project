15fc4da344917ddc1f19b4bc16c50862d12bb1fd
Aniket-Engg <aniketengg.225@gmail.com>
2022-05-11 13:52:18 +0530

    raw loader for templates

--- libs/remix-ws-templates/src/templates/remixDefault/index.ts
+++ libs/remix-ws-templates/src/templates/remixDefault/index.ts
@@ -1,13 +1,24 @@
-export default {
-    'contracts/1_Storage.sol': require('./contracts/1_Storage.sol').default,
-    'contracts/2_Owner.sol': require('./contracts/2_Owner.sol').default,
-    'contracts/3_Ballot.sol': require('./contracts/3_Ballot.sol').default,
-    'scripts/deploy_with_ethers.ts': require('./scripts/deploy_with_ethers.ts').default,
-    'scripts/deploy_with_web3.ts': require('./scripts/deploy_with_web3.ts').default,
-    'scripts/ethers.ts': require('./scripts/ethers.ts').default,
-    'scripts/web3.ts': require('./scripts/web3.ts').default,
-    'tests/Ballot_test.sol': require('./tests/Ballot_test.sol').default,
-    'tests/storage.test.js': require('./tests/storage.test.js').default,
-    'README.txt': require('./README.txt').default,
-
+export default async () => {
+    return {
+        // @ts-ignore
+        'contracts/1_Storage.sol': (await import('raw-loader!./contracts/1_Storage.sol')).default,
+        // @ts-ignore
+        'contracts/2_Owner.sol': (await import('raw-loader!./contracts/2_Owner.sol')).default,
+        // @ts-ignore
+        'contracts/3_Ballot.sol': (await import('raw-loader!./contracts/3_Ballot.sol')).default,
+        // @ts-ignore
+        'scripts/deploy_with_ethers.ts': (await import('!!raw-loader!./scripts/deploy_with_ethers.ts')).default,
+        // @ts-ignore
+        'scripts/deploy_with_web3.ts': (await import('!!raw-loader!./scripts/deploy_with_web3.ts')).default,
+        // @ts-ignore
+        'scripts/ethers.ts': (await import('!!raw-loader!./scripts/ethers.ts')).default,
+        // @ts-ignore
+        'scripts/web3.ts': (await import('!!raw-loader!./scripts/web3.ts')).default,
+        // @ts-ignore
+        'tests/Ballot_test.sol': (await import('raw-loader!./tests/Ballot_test.sol')).default,
+        // @ts-ignore
+        'tests/storage.test.js': (await import('!!raw-loader!./tests/storage.test.js')).default,
+        // @ts-ignore
+        'README.txt': (await import('raw-loader!./README.txt')).default,
+    }
 }
