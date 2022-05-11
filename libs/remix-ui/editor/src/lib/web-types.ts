d2a33890c278ae489a6177b65ab9929817ed2a88
yann300 <yann.levreau@gmail.com>
2022-05-11 12:59:28 +0200

    fix remix type autocomplete

--- libs/remix-ui/editor/src/lib/web-types.ts
+++ libs/remix-ui/editor/src/lib/web-types.ts
@@ -1,3 +1,5 @@
+import { remixTypes } from './remix-plugin-types'
+
 export const loadTypes = async (monaco) => {
     // ethers.js
 
@@ -162,20 +164,12 @@ export const loadTypes = async (monaco) => {
     // @ts-ignore
     const indexWeb3Util = await import('raw-loader!web3-utils/types/index.d.ts')
     monaco.languages.typescript.typescriptDefaults.addExtraLib(indexWeb3Util.default, `file:///node_modules/@types/web3-utils/index.d.ts`)
-
-
-    /*
     // remix
-   
-    // @ts-ignore
-    const indexRemixApi = await import('./remix-plugin-types')
-    monaco.languages.typescript.typescriptDefaults.addExtraLib(indexRemixApi.types)
-    */
-   
-    monaco.languages.typescript.typescriptDefaults.addExtraLib(`
-            import { PluginClient } from 'packages/plugin/core/src/lib/client'
-            declare const remix: PluginClient
-        `)
-
+    let indexRemixApi = remixTypes + `\n
+    declare global {
+        const remix: PluginClient;
+    }
+    `
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(indexRemixApi)
     console.log('loaded monaco types')
 }
