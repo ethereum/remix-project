99a5ea88b5cc7d2eccc9c21b02d0e6d61ec2e134
Aniket-Engg <aniketengg.225@gmail.com>
2022-05-16 18:56:08 +0530

    updated natspec position

--- libs/remix-ws-templates/src/templates/ozerc20/contracts/SampleERC20.sol
+++ libs/remix-ws-templates/src/templates/ozerc20/contracts/SampleERC20.sol
@@ -2,12 +2,12 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 /**
  * @title SampleERC20
  * @dev Create a sample ERC20 standard token
  */
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SampleERC20 is ERC20 {
 
