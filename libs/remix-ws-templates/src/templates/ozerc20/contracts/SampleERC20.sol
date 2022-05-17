fb79defc4ffaeea0da7716ef84a3758339f3fe82
yann300 <yann.levreau@gmail.com>
2022-05-17 13:48:11 +0200

    remove live in contract template

--- libs/remix-ws-templates/src/templates/ozerc20/contracts/SampleERC20.sol
+++ libs/remix-ws-templates/src/templates/ozerc20/contracts/SampleERC20.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @title SampleERC20
  * @dev Create a sample ERC20 standard token
  */
-
 contract SampleERC20 is ERC20 {
 
     constructor(string memory tokenName, string memory tokenSymbol) ERC20(tokenName, tokenSymbol) {}
