1044ba8eef10fd4e83345335bd1ef021cff82ae6
Aniket-Engg <aniketengg.225@gmail.com>
2022-05-19 18:52:33 +0530

    oz721 renaminng

--- libs/remix-ws-templates/src/templates/ozerc721/index.ts
+++ libs/remix-ws-templates/src/templates/ozerc721/index.ts
@@ -7,9 +7,9 @@ export default async () => {
         // @ts-ignore
         'scripts/deploy_with_web3.ts': (await import('!!raw-loader!./scripts/deploy_with_web3.ts')).default,
         // @ts-ignore
-        'scripts/ethers.ts': (await import('!!raw-loader!./scripts/ethers.ts')).default,
+        'scripts/ethers-lib.ts': (await import('!!raw-loader!./scripts/ethers-lib.ts')).default,
         // @ts-ignore
-        'scripts/web3.ts': (await import('!!raw-loader!./scripts/web3.ts')).default,
+        'scripts/web3-lib.ts': (await import('!!raw-loader!./scripts/web3-lib.ts')).default,
         // @ts-ignore
         'tests/SampleERC721_test.sol': (await import('raw-loader!./tests/SampleERC721_test.sol')).default
     }
