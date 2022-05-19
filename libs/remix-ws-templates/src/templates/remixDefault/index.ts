7db1e058ae44c358a072b759785f2047f833ab60
Aniket-Engg <aniketengg.225@gmail.com>
2022-05-19 19:29:25 +0530

    e2e fix

--- libs/remix-ws-templates/src/templates/remixDefault/index.ts
+++ libs/remix-ws-templates/src/templates/remixDefault/index.ts
@@ -11,9 +11,9 @@ export default async () => {
         // @ts-ignore
         'scripts/deploy_with_web3.ts': (await import('!!raw-loader!./scripts/deploy_with_web3.ts')).default,
         // @ts-ignore
-        'scripts/ethers-lib.ts': (await import('raw-loader!./scripts/ethers-lib.ts')).default,
+        'scripts/ethers-lib.ts': (await import('!!raw-loader!./scripts/ethers-lib.ts')).default,
         // @ts-ignore
-        'scripts/web3-lib.ts': (await import('raw-loader!./scripts/web3-lib.ts')).default,
+        'scripts/web3-lib.ts': (await import('!!raw-loader!./scripts/web3-lib.ts')).default,
         // @ts-ignore
         'tests/Ballot_test.sol': (await import('raw-loader!./tests/Ballot_test.sol')).default,
         // @ts-ignore
