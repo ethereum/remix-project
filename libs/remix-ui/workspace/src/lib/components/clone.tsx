25b61d65744d64a70ab0300c7d29da9cae639dfd
David Disu <daviddisu8@gmail.com>
2022-06-14 17:12:04 +0100

    Modify UI to support username and email on settings tab

--- libs/remix-ui/workspace/src/lib/components/clone.tsx
+++ libs/remix-ui/workspace/src/lib/components/clone.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 
 const popover = (
-  <Popover id="popover-basic" className='bg-light'>
+  <Popover id="popover-basic" className='bg-light border-secondary'>
     <Popover.Title as="h3" className='bg-dark border-0'>Clone workspace</Popover.Title>
       <Popover.Content>
         <div className="remixui_cloneContainer">
@@ -17,7 +17,7 @@ const popover = (
 )
 
 export const CloneWorkspace = () => (
-  <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+  <OverlayTrigger trigger="click" placement={"bottom-end"} overlay={popover}>
     <i className="fas fa-cloud-download remixui_menuicon pr-0"></i>
   </OverlayTrigger>
 )
