c395b34772a546b3641d972206a6ce1324e27361
Aniket-Engg <aniketengg.225@gmail.com>
2022-06-15 14:56:30 +0530

    detect etherscan route

--- apps/remix-ide/src/app/tabs/theme-module.js
+++ apps/remix-ide/src/app/tabs/theme-module.js
@@ -37,7 +37,8 @@ export class ThemeModule extends Plugin {
     themes.map((theme) => {
       this.themes[theme.name.toLocaleLowerCase()] = {
        ...theme,
-        url: window.location.origin + window.location.pathname + theme.url
+        // url: window.location.origin + window.location.pathname + theme.url
+        url: window.location.origin + '/' + theme.url
       }
     })
     this._paq = _paq
