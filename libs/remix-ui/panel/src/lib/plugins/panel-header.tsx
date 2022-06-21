5c795e4c491836bd059d97233a379425b86dcfd2
lianahus <liana@ethereum.org>
2022-06-21 12:06:11 +0200

    show verified icon for plugins

--- libs/remix-ui/panel/src/lib/plugins/panel-header.tsx
+++ libs/remix-ui/panel/src/lib/plugins/panel-header.tsx
@@ -20,7 +20,10 @@ const RemixUIPanelHeader = (props: RemixPanelProps) => {
 
   return (
     <header className='swapitHeader'><h6 data-id='sidePanelSwapitTitle'>{plugin?.profile.displayName || plugin?.profile.name}</h6>
-      {plugin?.profile.documentation ? (<a href={plugin.profile.documentation} className="titleInfo mb-2" title="link to documentation" target="_blank" rel="noreferrer"><i aria-hidden="true" className="fas fa-book"></i></a>) : ''}
+      <div className="flex flex-row">
+        {plugin?.profile.verified ? (<i aria-hidden="true" className="text-success fas fa-check" title="Verified by Remix"></i>) : '' }
+        {plugin?.profile.documentation ? (<a href={plugin.profile.documentation} className="titleInfo mb-2" title="link to documentation" target="_blank" rel="noreferrer"><i aria-hidden="true" className="fas fa-book"></i></a>) : ''}
+      </div>
     </header>)
 }
 
