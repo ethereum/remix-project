491113a0fb1e95dc4b472b71a0fc0182c3452fca
Aniket-Engg <aniketengg.225@gmail.com>
2022-07-01 12:59:27 +0530

    install slither as postinstall script

--- libs/remixd/src/services/slitherClient.ts
+++ libs/remixd/src/services/slitherClient.ts
@@ -14,7 +14,7 @@ export class SlitherClient extends PluginClient {
 
   constructor (private readOnly = false) {
     super()
-    this.methods = ['analyse', 'install']
+    this.methods = ['analyse']
   }
 
   setWebSocket (websocket: WS): void {
@@ -23,33 +23,6 @@ export class SlitherClient extends PluginClient {
 
   sharedFolder (currentSharedFolder: string): void {
     this.currentSharedFolder = currentSharedFolder
-  }
-
-  install (): void {
-    try {
-      const solcVersion = '0.8.15'
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: requires Python3.6+ (pip3) to be installed on your system`)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: solc-select will be installed along with Slither to set different solc compiler versions.`)
-      const options = { cwd: this.currentSharedFolder, shell: true }
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: checking pip3 availability ...`)
-      const pip3OP = execSync('pip3 --version', options)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: pip3 found: ${pip3OP.toString()}`)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: installing slither...`)
-      const slitherOP = execSync('pip3 install slither-analyzer', options)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: slither installation output: ${slitherOP.toString()}`)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: installing solc-select...`)
-      const solcSelectOP = execSync('pip3 install solc-select', options)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: solc-select installation output: ${solcSelectOP.toString()}`)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: installing solc ${solcVersion}...`)
-      const solcInstallOP = execSync(`solc-select install ${solcVersion}`, options)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: solc installation output: ${solcInstallOP.toString()}`)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: setting solc version to ${solcVersion}...`)
-      const solcUseOP = execSync(`solc-select use ${solcVersion}`, options)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: solc setting installation output: ${solcUseOP.toString()}`)
-      console.log('\x1b[32m%s\x1b[0m', `[Slither Installation]: Slither is ready to use!`)
-    } catch (err) {
-      console.log('\x1b[31m%s\x1b[0m', `[Slither Installation]: Error occured: ${err}`)
-    }
   }
 
   mapNpmDepsDir (list) {
