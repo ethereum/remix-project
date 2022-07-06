f6c7d1e524473c22d9d56c49434df63d25d2cd1b
David Disu <daviddisu8@gmail.com>
2022-07-06 14:15:09 +0100

    Fixed found issues with clone UI

--- libs/remix-ui/settings/src/lib/github-settings.tsx
+++ libs/remix-ui/settings/src/lib/github-settings.tsx
@@ -46,8 +46,8 @@ export function GithubSettings (props: GithubSettingsProps) {
   return (
     <div className="border-top">
       <div className="card-body pt-3 pb-2">
-        <h6 className="card-title">GitHub Access Token</h6>
-        <p className="mb-1">Manage the access token used to publish to Gist and retrieve GitHub contents.</p>
+        <h6 className="card-title">GitHub Credentials</h6>
+        <p className="mb-1">Manage your GitHub credentials used to publish to Gist and retrieve GitHub contents.</p>
         <p className="">Go to github token page (link below) to create a new token and save it in Remix. Make sure this token has only \'create gist\' permission.</p>
         <p className="mb-1"><a className="text-primary" target="_blank" href="https://github.com/settings/tokens">https://github.com/settings/tokens</a></p>
         <div>
