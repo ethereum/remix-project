d07984ee9695a5553b60eaefa5f24aa8921b83ad
yann300 <yann.levreau@gmail.com>
2022-07-11 12:08:39 +0200

    fix flaky test

--- apps/remix-ide-e2e/src/commands/clickFunction.ts
+++ apps/remix-ide-e2e/src/commands/clickFunction.ts
@@ -4,8 +4,18 @@ import EventEmitter from 'events'
 class ClickFunction extends EventEmitter {
   command (this: NightwatchBrowser, fnFullName: string, expectedInput?: NightwatchClickFunctionExpectedInput): NightwatchBrowser {
     this.api.waitForElementPresent('.instance button[title="' + fnFullName + '"]')
-      .scrollInto('#runTabView input[title="' + expectedInput.types + '"]')
-      .setValue('#runTabView input[title="' + expectedInput.types + '"]', expectedInput.values, _ => _)
+      .perform(function (client, done) {
+        client.execute(function () {
+          document.querySelector('#runTabView').scrollTop = document.querySelector('#runTabView').scrollHeight
+        }, [], function () {
+          if (expectedInput) {
+            client
+            .scrollInto('#runTabView input[title="' + expectedInput.types + '"]')
+            .setValue('#runTabView input[title="' + expectedInput.types + '"]', expectedInput.values, _ => _)
+          }
+          done()
+        })
+      })
       .scrollAndClick('.instance button[title="' + fnFullName + '"]')
       .pause(2000)
       .perform(() => {
