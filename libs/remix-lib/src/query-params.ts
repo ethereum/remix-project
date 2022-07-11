b8ff702296998d4446b7e683cf3c72ca181020ae
Aniket-Engg <aniketengg.225@gmail.com>
2022-07-11 17:29:46 +0530

    remove slash

--- libs/remix-lib/src/query-params.ts
+++ libs/remix-lib/src/query-params.ts
@@ -17,9 +17,7 @@ export class QueryParams  {
   }
 
   get () {
-    let qs 
-    if (window.location.hash.startsWith('#/')) qs = window.location.hash.substr(2)
-    else qs = window.location.hash.substr(1)
+    const qs = window.location.hash.substr(1)
 
     if (window.location.search.length > 0) {
       // use legacy query params instead of hash
