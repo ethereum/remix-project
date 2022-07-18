41532b1a5df913395ded5a2bfc581d9d55699f21
lianahus <liana@ethereum.org>
2022-07-18 10:54:54 +0200

    bold

--- apps/remix-ide/src/app/tabs/hardhat-provider.tsx
+++ apps/remix-ide/src/app/tabs/hardhat-provider.tsx
@@ -23,7 +23,7 @@ export class HardhatProvider extends AbstractProvider {
   body (): JSX.Element {
     return (
       <div> Note: To run Hardhat network node on your system, go to hardhat project folder and run command:
-        <div className="p-1 pl-3">npx hardhat node</div>       
+        <div className="p-1 pl-3"><b>npx hardhat node</b></div>       
         <div className="pt-2 pb-4">
           For more info, visit: <a href="https://hardhat.org/getting-started/#connecting-a-wallet-or-dapp-to-hardhat-network" target="_blank">Hardhat Documentation</a>
         </div>
