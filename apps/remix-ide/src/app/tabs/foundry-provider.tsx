f888f30f253f01d03ced13b95ecd93f61839f8bd
lianahus <liana@ethereum.org>
2022-07-18 10:26:12 +0200

    style fixes in Foundry modal

--- apps/remix-ide/src/app/tabs/foundry-provider.tsx
+++ apps/remix-ide/src/app/tabs/foundry-provider.tsx
@@ -1,9 +1,5 @@
 import * as packageJson from '../../../../../package.json'
-import { Plugin } from '@remixproject/engine'
-import { AppModal, AlertModal, ModalTypes } from '@remix-ui/app'
 import React from 'react' // eslint-disable-line
-import { Blockchain } from '../../blockchain/blockchain'
-import { ethers } from 'ethers'
 import { AbstractProvider } from './abstract-provider'
 
 const profile = {
@@ -22,10 +18,12 @@ export class FoundryProvider extends AbstractProvider {
 
   body (): JSX.Element {
     return (
-      <div> Note: To run Anvil on your system, run
-        <div className="border p-1">curl -L https://foundry.paradigm.xyz | bash</div> 
-        <div className="border p-1">anvil</div>       
-        For more info, visit: <a href="https://github.com/foundry-rs/foundry" target="_blank">Foundry Documentation</a>
+      <div> Note: To run Anvil on your system, run:
+        <div className="p-1 pl-3"><b>curl -L https://foundry.paradigm.xyz | bash</b></div>
+        <div className="p-1 pl-3"><b>anvil</b></div>
+        <div className="pt-2 pb-4">
+          For more info, visit: <a href="https://github.com/foundry-rs/foundry" target="_blank">Foundry Documentation</a>
+        </div>
         <div>Anvil JSON-RPC Endpoint:</div>
       </div>
     )
