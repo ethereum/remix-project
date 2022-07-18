508309f8550af963d2d4e9a371fd7b8cda02e4b6
filip mertens <filip.mertens@ethereum.org>
2022-07-18 13:40:45 +0200

    flaky tx listener

--- apps/remix-ide-e2e/src/tests/remixd.test.ts
+++ apps/remix-ide-e2e/src/tests/remixd.test.ts
@@ -74,7 +74,7 @@ module.exports = {
       .setSolidityCompilerVersion('soljson-v0.5.0+commit.1d4f565a.js')
       .testContracts('test_import_node_modules.sol', sources[3]['test_import_node_modules.sol'], ['SafeMath'])
   },
-  'Import from node_modules and reference a github import #group2 #flaky': function (browser) {
+  'Import from node_modules and reference a github import #group2': function (browser) {
     browser.waitForElementVisible('#icon-panel', 2000)
       .clickLaunchIcon('filePanel')
       .addFile('test_import_node_modules_with_github_import.sol', sources[4]['test_import_node_modules_with_github_import.sol'])
