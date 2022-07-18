0e318c5e1071a44fe352ecdf895584336507b05b
filip mertens <filip.mertens@ethereum.org>
2022-07-18 10:22:16 +0200

    update nightwatch

---
+++ apps/remix-ide-e2e/src/commands/executeScriptInTerminal.ts
@@ -0,0 +1,20 @@
+import { NightwatchBrowser } from 'nightwatch'
+import EventEmitter from 'events'
+
+class ExecuteScriptInTerminal extends EventEmitter {
+  command (this: NightwatchBrowser, script: string): NightwatchBrowser {
+    this.api
+      .clearEditableContent('*[data-id="terminalCliInput"]')
+      .click('*[data-id="terminalCli"]')
+      .setValue('*[data-id="terminalCliInput"]', [this.api.Keys.CONTROL, 'a', this.api.Keys.DELETE])
+      .sendKeys('*[data-id="terminalCliInput"]', script)
+      .sendKeys('*[data-id="terminalCliInput"]', this.api.Keys.ENTER)
+      .sendKeys('*[data-id="terminalCliInput"]', this.api.Keys.ENTER)
+      .perform(() => {
+        this.emit('complete')
+      })
+    return this
+  }
+}
+
+module.exports = ExecuteScriptInTerminal
