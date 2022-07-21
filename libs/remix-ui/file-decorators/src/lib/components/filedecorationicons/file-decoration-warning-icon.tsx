9181e8ddaf09fd613a827f2839524b37e476c8a6
filip mertens <filip.mertens@ethereum.org>
2022-07-21 15:39:23 +0200

    add tests

--- libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-warning-icon.tsx
+++ libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-warning-icon.tsx
@@ -5,11 +5,7 @@ import { fileDecoration } from '../../types'
 const FileDecorationWarningIcon = (props: {
     fileDecoration: fileDecoration
 }) => {
-<<<<<<< HEAD
-    return <><span data-id={`file-decoration-warning-${props.fileDecoration.path}`} className={`${props.fileDecoration.fileStateIconClass} text-warning pr-2`}>{props.fileDecoration.text}</span></>
-=======
     return <><span className={`${props.fileDecoration.fileStateIconClass} text-warning pr-2`}>{props.fileDecoration.text}</span></>
->>>>>>> 43bc1038a (add test)
 }
 
 export default FileDecorationWarningIcon
