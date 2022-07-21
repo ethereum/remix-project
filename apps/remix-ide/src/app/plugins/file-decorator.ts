241453dc88f49ccf095630070f63391a203b0c86
Seth Samuel <sethsamuel03@gmail.com>
2022-07-21 15:39:23 +0200

    add tests

--- apps/remix-ide/src/app/plugins/file-decorator.ts
+++ apps/remix-ide/src/app/plugins/file-decorator.ts
@@ -8,11 +8,7 @@ import { fileDecoration } from '@remix-ui/file-decorators'
 const profile = {
     name: 'fileDecorator',
     desciption: 'Keeps decorators of the files',
-<<<<<<< HEAD
     methods: ['setFileDecorators', 'clearFileDecorators', 'clearAllFileDecorators'],
-=======
-    methods: ['setFileDecorators', 'clearFileDecorators'],
->>>>>>> 43bc1038a (add test)
     events: ['fileDecoratorsChanged'],
     version: '0.0.1'
 }
@@ -22,25 +18,11 @@ export class FileDecorator extends Plugin {
     constructor() {
         super(profile)
     }
-<<<<<<< HEAD
-
-    onActivation(): void {
-        this.on('filePanel', 'setWorkspace', async () => {
-            await this.clearAllFileDecorators()
-        })
-    }
-
-=======
->>>>>>> 43bc1038a (add test)
     /**
      * 
      * @param fileStates Array of file states
      */
     async setFileDecorators(fileStates: fileDecoration[] | fileDecoration) {
-<<<<<<< HEAD
-        const { from } = this.currentRequest
-        const workspace = await this.call('filePanel', 'getCurrentWorkspace')
-=======
         const workspace = await this.call('filePanel', 'getCurrentWorkspace')
         function sortByPath( a: fileDecoration, b: fileDecoration ) {
             if ( a.path < b.path ){
@@ -52,49 +34,20 @@ export class FileDecorator extends Plugin {
             return 0;
           }
 
->>>>>>> 43bc1038a (add test)
         const fileStatesPayload = Array.isArray(fileStates) ? fileStates : [fileStates]
         // clear all file states in the previous state of this owner on the files called
         fileStatesPayload.forEach((state) => {
             state.workspace = workspace
-<<<<<<< HEAD
             state.owner = from
         })
         const filteredState = this._fileStates.filter((state) => {
             const index = fileStatesPayload.findIndex((payloadFileState: fileDecoration) => {
                 return from == state.owner && payloadFileState.path == state.path
-=======
-        })
-        const filteredState = this._fileStates.filter((state) => {
-            const index = fileStatesPayload.findIndex((payloadFileState: fileDecoration) => {
-                return payloadFileState.owner == state.owner && payloadFileState.path == state.path
->>>>>>> 43bc1038a (add test)
             })
             return index == -1
         })
         const newState = [...filteredState, ...fileStatesPayload].sort(sortByPath)
-<<<<<<< HEAD
-
-=======
         
->>>>>>> 43bc1038a (add test)
-        if (!deepequal(newState, this._fileStates)) {
-            this._fileStates = newState
-            this.emit('fileDecoratorsChanged', this._fileStates)
-        }
-    }
-
-<<<<<<< HEAD
-    async clearFileDecorators(path?: string) {
-        const { from } = this.currentRequest
-        if (!from) return
-
-        const filteredState = this._fileStates.filter((state) => {
-            if(state.owner != from) return true
-            if(path && state.path != path) return true
-        })
-        const newState = [...filteredState].sort(sortByPath)
-
         if (!deepequal(newState, this._fileStates)) {
             this._fileStates = newState
             this.emit('fileDecoratorsChanged', this._fileStates)
@@ -102,24 +55,8 @@ export class FileDecorator extends Plugin {
 
     }
 
-    async clearAllFileDecorators() {
-        this._fileStates = []
-        this.emit('fileDecoratorsChanged', [])
-    }
-}
-
-const sortByPath = (a: fileDecoration, b: fileDecoration) => {
-    if (a.path < b.path) {
-        return -1;
-    }
-    if (a.path > b.path) {
-        return 1;
-    }
-    return 0;
-=======
     async clearFileDecorators() {
         this._fileStates = []
         this.emit('fileDecoratorsChanged', [])
     }
->>>>>>> 43bc1038a (add test)
 }
--- libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-error-icon.tsx
+++ libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-error-icon.tsx
@@ -7,11 +7,7 @@ const FileDecorationErrorIcon = (props: {
     fileDecoration: fileDecoration
 }) => {
     return <>
-<<<<<<< HEAD
-        <span data-id={`file-decoration-error-${props.fileDecoration.path}`} className={`${props.fileDecoration.fileStateIconClass} text-danger pr-2`}>{props.fileDecoration.text}</span>
-=======
         <span className={`${props.fileDecoration.fileStateIconClass} text-danger pr-2`}>{props.fileDecoration.text}</span>
->>>>>>> 43bc1038a (add test)
     </>
 }
 
