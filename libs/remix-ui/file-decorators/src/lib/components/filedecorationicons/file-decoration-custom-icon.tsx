4bff17e989a97ca9d92d2eccd82676701275fcbb
filip mertens <filip.mertens@ethereum.org>
2022-07-21 15:39:23 +0200

    add tests

--- libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-custom-icon.tsx
+++ libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-custom-icon.tsx
@@ -5,11 +5,7 @@ import { fileDecoration } from '../../types'
 const FileDecorationCustomIcon = (props: {
     fileDecoration: fileDecoration
 }) => {
-<<<<<<< HEAD
-    return <><span data-id={`file-decoration-custom-${props.fileDecoration.path}`} className={`${props.fileDecoration.fileStateIconClass} pr-2`}>
-=======
     return <><span className={`${props.fileDecoration.fileStateIconClass}pr-2`}>
->>>>>>> 43bc1038a (add test)
         {props.fileDecoration.fileStateIcon}
     </span></>
 }
