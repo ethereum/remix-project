4cae17a9f84971a4eaab5c3c67faaef4cfce9f5d
bunsenstraat <filip.mertens@ethereum.org>
2022-08-01 16:47:07 +0200

    nightly flaky tests

---
+++ apps/remix-ide/ci/nightly.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
+echo "$BUILD_ID"
+TEST_EXITCODE=0
+
+yarn run ganache-cli &
+yarn run serve:production &
+echo 'sharing folder: ' $PWD '/apps/remix-ide/contracts' &
+yarn run remixd &
+
+sleep 5
+
+yarn run build:e2e
+
+TESTFILES=$(grep -IRiL "\'@disabled\': \?true" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | sort | circleci tests split )
+for TESTFILE in $TESTFILES; do
+    for i in 1 2 3 4 5 6 7 8 9 10
+    do
+        npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js $TESTFILE --env=$1  || TEST_EXITCODE=1
+    done
+done
+
+echo "$TEST_EXITCODE"
+if [ "$TEST_EXITCODE" -eq 1 ]
+then
+  exit 1
+fi
