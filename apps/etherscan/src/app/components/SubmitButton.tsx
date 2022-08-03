45d08527282a32ec877876319b783ef201dcc2bf
yann300 <yann.levreau@gmail.com>
2022-08-03 12:24:20 +0200

    add etherscan plugin

--- apps/etherscan/src/app/components/SubmitButton.tsx
+++ apps/etherscan/src/app/components/SubmitButton.tsx
@@ -3,17 +3,26 @@ import React from "react"
 interface Props {
   text: string
   isSubmitting?: boolean
+<<<<<<< HEAD
   dataId?: string
+=======
+>>>>>>> e02014ca4 (add etherscan plugin)
 }
 
 export const SubmitButton: React.FC<Props> = ({
   text,
+<<<<<<< HEAD
   dataId,
+=======
+>>>>>>> e02014ca4 (add etherscan plugin)
   isSubmitting = false,
 }) => {
   return (
     <button
+<<<<<<< HEAD
       data-id={dataId}
+=======
+>>>>>>> e02014ca4 (add etherscan plugin)
       style={{ padding: "0.25rem 0.4rem", marginRight: "0.5em" }}
       type="submit"
       className="btn btn-primary"
@@ -29,7 +38,11 @@ export const SubmitButton: React.FC<Props> = ({
             aria-hidden="true"
             style={{ marginRight: "0.3em" }}
           />
+<<<<<<< HEAD
           Verifying... Please wait
+=======
+          Verifying...Please wait
+>>>>>>> e02014ca4 (add etherscan plugin)
         </div>
       )}
     </button>
