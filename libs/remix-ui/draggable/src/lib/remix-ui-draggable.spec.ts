29c7887959a2b34fd06b7941e2408e27b1ef6767
iamsethsamuel <sethsamuel03@gmail.com>
2022-08-03 13:17:45 +0100

    drag and drop is now a library

---
+++ libs/remix-ui/draggable/src/lib/remix-ui-draggable.spec.ts
@@ -0,0 +1,7 @@
+import { remixUiDraggable } from './remix-ui-draggable';
+
+describe('remixUiDraggable', () => {
+    it('should work', () => {
+        expect(remixUiDraggable()).toEqual('remix-ui-draggable');
+    })
+})
