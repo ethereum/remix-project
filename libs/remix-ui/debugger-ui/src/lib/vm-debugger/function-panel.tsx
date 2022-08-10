9e5410e7c42ec273a1b62d0b48daeaf5872a798f
yann300 <yann.levreau@gmail.com>
2022-08-10 12:24:06 +0200

    grid view for the debugger

--- libs/remix-ui/debugger-ui/src/lib/vm-debugger/function-panel.tsx
+++ libs/remix-ui/debugger-ui/src/lib/vm-debugger/function-panel.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react' // eslint-disable-line
 import DropdownPanel from './dropdown-panel' // eslint-disable-line
 import { default as deepequal } from 'deep-equal' // eslint-disable-line
 
-export const FunctionPanel = ({ data }) => {
+export const FunctionPanel = ({ data, className }) => {
   const [calldata, setCalldata] = useState(null)
 
   useEffect(() => {
@@ -10,7 +10,7 @@ export const FunctionPanel = ({ data }) => {
   }, [data])
 
   return (
-    <div id='FunctionPanel' data-id='functionPanel'>
+    <div id='FunctionPanel' className={className} data-id='functionPanel'>
       <DropdownPanel dropdownName='Function Stack' calldata={calldata || {}} />
     </div>
   )
