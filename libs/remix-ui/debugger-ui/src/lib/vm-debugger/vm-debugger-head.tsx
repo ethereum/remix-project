33cbad0884d340c6bc504c162df7bc4318126ccd
yann300 <yann.levreau@gmail.com>
2022-08-11 10:44:59 +0200

    fix UI grid

--- libs/remix-ui/debugger-ui/src/lib/vm-debugger/vm-debugger-head.tsx
+++ libs/remix-ui/debugger-ui/src/lib/vm-debugger/vm-debugger-head.tsx
@@ -98,17 +98,15 @@ export const VmDebuggerHead = ({ vmDebugger: { registerEvent, triggerEvent } }) 
   }, [registerEvent])
 
   return (
-    <div id='vmheadView' className="mt-1 px-0">
-      <div className='d-flex flex-column'>
-        <div className='w-100 row align-items-center'>
-          <FunctionPanel className="col" data={functionPanel} />
-          <SolidityLocals className="col"  data={solidityLocals.calldata} message={solidityLocals.message} registerEvent={registerEvent} triggerEvent={triggerEvent} />
-          <SolidityState className="col" calldata={solidityState.calldata} message={solidityState.message} />
-        </div>
-        <div className='w-100 row align-items-center'>
-          <div className='col'><CodeListView registerEvent={registerEvent} /></div>
-          <div className='col'><StepDetail stepDetail={stepDetail} /></div>
-        </div>
+    <div id='vmheadView' className="mt-1 px-0 d-flex">
+      <div className='d-flex flex-column pr-2' style={{ flex:1 }}>
+        <FunctionPanel className="" data={functionPanel} />
+        <SolidityLocals className=""  data={solidityLocals.calldata} message={solidityLocals.message} registerEvent={registerEvent} triggerEvent={triggerEvent} />
+        <CodeListView registerEvent={registerEvent} />
+      </div>
+      <div className='d-flex flex-column pl-2' style={{ flex:1 }}>
+        <SolidityState className="" calldata={solidityState.calldata} message={solidityState.message} />     
+        <StepDetail stepDetail={stepDetail} />
       </div>
     </div>
   )
