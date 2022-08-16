4acba9be0324e2ab653ea46e406b71379c23d290
Seth Samuel <sethsamuel03@gmail.com>
2022-08-16 16:27:27 +0100

    app building locally

--- libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-tooltip.tsx
+++ libs/remix-ui/file-decorators/src/lib/components/filedecorationicons/file-decoration-tooltip.tsx
@@ -9,15 +9,9 @@ const FileDecorationTooltip = (props: {
 },
 ) => {
     const getComments = function (fileDecoration: fileDecoration) {
-<<<<<<< HEAD
-        if (fileDecoration.comment) {
-            const comments = Array.isArray(fileDecoration.comment) ? fileDecoration.comment : [fileDecoration.comment]
-            return comments.map((comment, index) => {
-=======
         if (fileDecoration.commment) {
             const commments = Array.isArray(fileDecoration.commment) ? fileDecoration.commment : [fileDecoration.commment]
             return commments.map((comment, index) => {
->>>>>>> 43bc1038a (add test)
                 return <div key={index}>{comment}<br></br></div>
             })
         }
