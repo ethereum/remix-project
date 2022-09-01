ee969f84b3a08e88d63ee5258456429b381a7452
bunsenstraat <filip.mertens@ethereum.org>
2022-09-01 09:39:07 +0200

    unused var

--- apps/remix-ide-e2e/src/tests/terminal.test.ts
+++ apps/remix-ide-e2e/src/tests/terminal.test.ts
@@ -284,40 +284,6 @@ module.exports = {
 }
 
 
-const testStorageForLiveMode = `// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
-
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
-contract Storage {
-
-    uint256 number;
-
-    constructor () {
-        number = 350;
-    }
-
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
-    }
-
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
-        return number;
-    }
-}`
-
 const asyncAwait = `
   var p = function () {
     return new Promise(function (resolve, reject)  {
