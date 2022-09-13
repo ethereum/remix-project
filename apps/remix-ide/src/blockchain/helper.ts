ad2d7b09bb6e5d2b9efda52542ab64ed07a2bb1e
yann300 <yann.levreau@gmail.com>
2022-09-13 19:01:21 +0200

    add sepolia

--- apps/remix-ide/src/blockchain/helper.ts
+++ apps/remix-ide/src/blockchain/helper.ts
@@ -3,7 +3,8 @@ const transactionDetailsLinks = {
     Rinkeby: 'https://rinkeby.etherscan.io/tx/',
     Ropsten: 'https://ropsten.etherscan.io/tx/',
     Kovan: 'https://kovan.etherscan.io/tx/',
-    Goerli: 'https://goerli.etherscan.io/tx/'
+    Goerli: 'https://goerli.etherscan.io/tx/',
+    Sepolia: 'https://sepolia.etherscan.io/tx/'
   }
   
   export function etherScanLink (network: string, hash: string): string {
