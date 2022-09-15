648b7164e018f057884cd9ce44d8513b574b4c87
Joseph Izang <josephizang@gmail.com>
2022-09-15 02:05:23 +0100

    fix e2e firefox

--- apps/remix-ide-e2e/src/commands/testConstantFunction.ts
+++ apps/remix-ide-e2e/src/commands/testConstantFunction.ts
@@ -15,13 +15,13 @@ class TestConstantFunction extends EventEmitter {
 }
 
 function testConstantFunction (browser: NightwatchBrowser, address: string, fnFullName: string, expectedInput: NightwatchTestConstantFunctionExpectedInput, expectedOutput: string, cb: VoidFunction) {
-  browser.waitForElementPresent('.instance button[title="' + fnFullName + '"]').perform(function (client, done) {
+  browser.waitForElementPresent('.instance button[data-title="' + fnFullName + '"]').perform(function (client, done) {
     client.execute(function () {
       document.querySelector('#runTabView').scrollTop = document.querySelector('#runTabView').scrollHeight
     }, [], function () {
       if (expectedInput) {
-        client.waitForElementPresent('#runTabView input[title="' + expectedInput.types + '"]')
-          .setValue('#runTabView input[title="' + expectedInput.types + '"]', expectedInput.values)
+        client.waitForElementPresent('#runTabView input[data-title="' + expectedInput.types + '"]')
+          .setValue('#runTabView input[data-title="' + expectedInput.types + '"]', expectedInput.values)
       }
       done()
     })
