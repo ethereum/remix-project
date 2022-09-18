03e432eacff7c9f45bc337eb7882a6a4bd5933ba
filip mertens <filip.mertens@ethereum.org>
2022-09-18 12:22:24 +0200

    rm incompatible version

--- apps/remix-ide-e2e/src/tests/usingWebWorker.test.ts
+++ apps/remix-ide-e2e/src/tests/usingWebWorker.test.ts
@@ -35,7 +35,6 @@ module.exports = {
       .clickLaunchIcon('solidity')
       .waitForElementVisible('[data-id="compilerNightliesBuild"]')
       .click('[data-id="compilerNightliesBuild"]')
-      .noWorkerErrorFor('soljson-v0.3.4+commit.7dab8902.js')
       .noWorkerErrorFor('soljson-v0.6.5+commit.f956cc89.js')
       .noWorkerErrorFor('soljson-v0.6.8-nightly.2020.5.14+commit.a6d0067b.js')
       .noWorkerErrorFor('soljson-v0.6.0-nightly.2019.12.17+commit.d13438ee.js')
