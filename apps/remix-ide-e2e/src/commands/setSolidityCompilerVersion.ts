3fe115ca3ea5faacdb348d181648960a1f085cd8
filip mertens <filip.mertens@ethereum.org>
2022-09-18 12:46:13 +0200

    more assertions

--- apps/remix-ide-e2e/src/commands/setSolidityCompilerVersion.ts
+++ apps/remix-ide-e2e/src/commands/setSolidityCompilerVersion.ts
@@ -4,6 +4,14 @@ import EventEmitter from 'events'
 class SetSolidityCompilerVersion extends EventEmitter {
   command (this: NightwatchBrowser, version: string): NightwatchBrowser {
     this.api
+      .waitForElementVisible({
+        selector: "//*[@id='versionSelector']",
+        locateStrategy: 'xpath'
+      })
+      .waitForElementPresent({
+        selector: `//option[@value='${version}']`,
+        locateStrategy: 'xpath'
+      })
       .click(`#compileTabView #versionSelector [value="${version}"]`)
       .pause(5000)
       .saveScreenshot(`./reports/screenshots/${version}.png`)
