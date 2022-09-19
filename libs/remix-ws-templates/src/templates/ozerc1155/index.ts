c606a69a1279f490ee0f165d0d430b61d5a3a819
Aniket-Engg <aniketengg.225@gmail.com>
2022-09-19 15:01:40 +0530

    added oz ERC1155 template

---
+++ libs/remix-ws-templates/src/templates/ozerc1155/index.ts
@@ -0,0 +1,20 @@
+import { erc1155 } from '@openzeppelin/wizard';
+
+export default async (opts) => {
+    let filesObj = {
+        'contracts/MyToken.sol': erc1155.print({ ...erc1155.defaults, upgradeable: opts.upgradeable}),
+        // @ts-ignore
+        'scripts/deploy_with_ethers.ts': (await import('!!raw-loader!./scripts/deploy_with_ethers.ts')).default,
+        // @ts-ignore
+        'scripts/deploy_with_web3.ts': (await import('!!raw-loader!./scripts/deploy_with_web3.ts')).default,
+        // @ts-ignore
+        'scripts/ethers-lib.ts': (await import('!!raw-loader!./scripts/ethers-lib.ts')).default,
+        // @ts-ignore
+        'scripts/web3-lib.ts': (await import('!!raw-loader!./scripts/web3-lib.ts')).default,
+    }
+    // If no options is selected, opts.upgradeable will be undefined
+    // We do not show test file for upgradeable contract
+    // @ts-ignore
+    if (opts.upgradeable === undefined || !opts.upgradeable) filesObj['tests/MyToken_test.sol'] = (await import('raw-loader!./tests/MyToken_test.sol')).default
+    return filesObj
+}
