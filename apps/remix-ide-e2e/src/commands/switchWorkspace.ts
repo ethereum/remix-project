868d0e55077dc59e2b058cf78315639120096177
filip mertens <filip.mertens@ethereum.org>
2022-09-19 09:36:10 +0200

    assert

--- apps/remix-ide-e2e/src/commands/switchWorkspace.ts
+++ apps/remix-ide-e2e/src/commands/switchWorkspace.ts
@@ -3,12 +3,13 @@ import EventEmitter from 'events'
 
 class switchWorkspace extends EventEmitter {
   command (this: NightwatchBrowser, workspaceName: string): NightwatchBrowser {
-    this.api.waitForElementVisible('[data-id="workspacesSelect"]')
+    this.api
+    .waitForElementVisible('[data-id="workspacesSelect"]')
+    .waitForElementPresent(`[data-id="dropdown-item-${workspaceName}"]`)
     .click('[data-id="workspacesSelect"]')
     .waitForElementVisible(`[data-id="dropdown-item-${workspaceName}"]`)
-    .pause(2000)
     .click(`[data-id="dropdown-item-${workspaceName}"]`)
-    .pause(3000)
+    .pause(4000)
     .perform((done) => {
       done()
       this.emit('complete')
