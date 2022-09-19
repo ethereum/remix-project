e4f8340109d6076e6d2bbc210b7dc92d1d3b1cb1
Aniket-Engg <aniketengg.225@gmail.com>
2022-09-19 18:19:47 +0530

    remove test file for upgradable contracts

--- libs/remix-ws-templates/src/templates/ozerc1155/index.ts
+++ libs/remix-ws-templates/src/templates/ozerc1155/index.ts
@@ -10,7 +10,7 @@ export default async (opts) => {
         // @ts-ignore
         'scripts/ethers-lib.ts': (await import('!!raw-loader!./scripts/ethers-lib.ts')).default,
         // @ts-ignore
-        'scripts/web3-lib.ts': (await import('!!raw-loader!./scripts/web3-lib.ts')).default,
+        'scripts/web3-lib.ts': (await import('!!raw-loader!./scripts/web3-lib.ts')).default
     }
     // If no options is selected, opts.upgradeable will be undefined
     // We do not show test file for upgradeable contract
