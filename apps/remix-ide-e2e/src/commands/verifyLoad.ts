e29e939dbf3cb50160fc1dcf14a65a64973330cc
filip mertens <filip.mertens@ethereum.org>
2022-09-19 14:59:06 +0200

    longer timeout

--- apps/remix-ide-e2e/src/commands/verifyLoad.ts
+++ apps/remix-ide-e2e/src/commands/verifyLoad.ts
@@ -6,18 +6,22 @@ class VerifyLoad extends EventEmitter {
         browser.waitForElementPresent({
             selector: "//span[@data-id='typesloaded']",
             locateStrategy: 'xpath',
+            timeout: 60000
         })
         .waitForElementPresent({
             selector: "//span[@data-id='editorloaded']",
             locateStrategy: 'xpath',
+            timeout: 60000
         })
         .waitForElementPresent({
             selector: "//span[@data-id='workspaceloaded']",
             locateStrategy: 'xpath',
+            timeout: 60000
         })
         .waitForElementPresent({
             selector: "//span[@data-id='apploaded']",
             locateStrategy: 'xpath',
+            timeout: 60000
         })
         .perform((done) => {
             done()
