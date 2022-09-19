368a935f8e94fd22402ca4219028e3d87e47de27
filip mertens <filip.mertens@ethereum.org>
2022-09-19 09:09:41 +0200

    flaky

--- apps/remix-ide-e2e/src/tests/url.test.ts
+++ apps/remix-ide-e2e/src/tests/url.test.ts
@@ -156,7 +156,7 @@ module.exports = {
       })
   },
 
-  'Should select deploy with proxy option from URL params #group2': function (browser: NightwatchBrowser) {
+  'Should select deploy with proxy option from URL params #group2 #flaky': function (browser: NightwatchBrowser) {
     browser
       .url('http://127.0.0.1:8080/#optimize=false&runs=200&deployProxy=true')
       .refresh()
