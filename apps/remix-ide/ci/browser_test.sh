3bcef975efada801afcfa5d0948256939841fc86
filip mertens <filip.mertens@ethereum.org>
2022-09-20 19:22:27 +0200

    syntax

--- apps/remix-ide/ci/browser_test.sh
+++ apps/remix-ide/ci/browser_test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | grep -E "\b[${2}]"
+
 BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
 echo "$BUILD_ID"
 TEST_EXITCODE=0
@@ -14,8 +16,8 @@ yarn run remixd &
 sleep 5
 
 yarn run build:e2e
-grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | grep -E '\b[$2]'
-TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | grep -E '\b[$2]' | circleci tests split --split-by=timings )
+
+TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | grep -E "\b[$2]" | circleci tests split --split-by=timings )
 for TESTFILE in $TESTFILES; do
     npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js dist/apps/remix-ide-e2e/src/tests/${TESTFILE}.test.js --env=$1  || TEST_EXITCODE=1
 done
