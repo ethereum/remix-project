dcb619d81ed2241401d0acf25e7ceaf46e6e1e91
filip mertens <filip.mertens@ethereum.org>
2022-09-20 19:05:41 +0200

    syntax

--- apps/remix-ide/ci/browser_test.sh
+++ apps/remix-ide/ci/browser_test.sh
@@ -14,7 +14,7 @@ yarn run remixd &
 sleep 5
 
 yarn run build:e2e
-grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | | grep-E '\b[$2]'
+grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | grep-E '\b[$2]'
 TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep "\.spec\|\.test" | xargs -I {} basename {} .test.js | grep-E '\b[$2]' | circleci tests split --split-by=timings )
 for TESTFILE in $TESTFILES; do
     npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js dist/apps/remix-ide-e2e/src/tests/${TESTFILE}.test.js --env=$1  || TEST_EXITCODE=1
