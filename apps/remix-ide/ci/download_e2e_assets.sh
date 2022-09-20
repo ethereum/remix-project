e179d51b68a2eb375954a9b1fb99a97d4282f107
filip mertens <filip.mertens@ethereum.org>
2022-09-20 09:49:52 +0200

    assets after build

--- apps/remix-ide/ci/download_e2e_assets.sh
+++ apps/remix-ide/ci/download_e2e_assets.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.7+commit.e28d00a7.js -O ./apps/remix-ide/src/assets/js/soljson-v0.8.7+commit.e28d00a7.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.16+commit.07a7930e.js -O ./apps/remix-ide/src/assets/js/soljson-v0.8.16+commit.07a7930e.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.15+commit.e14f2714.js -O ./apps/remix-ide/src/assets/js/soljson-v0.8.15+commit.e14f2714.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.7.4+commit.3f05b770.js -O ./apps/remix-ide/src/assets/js/soljson-v0.7.4+commit.3f05b770.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.4.14+commit.c2215d46.js -O ./apps/remix-ide/src/assets/js/soljson-v0.4.14+commit.c2215d46.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.8+commit.0bbfe453.js -O ./apps/remix-ide/src/assets/js/soljson-v0.6.8+commit.0bbfe453.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.1+commit.df193b15.js -O ./apps/remix-ide/src/assets/js/soljson-v0.8.1+commit.df193b15.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.5+commit.f956cc89.js -O ./apps/remix-ide/src/assets/js/soljson-v0.6.5+commit.f956cc89.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.8-nightly.2020.5.14+commit.a6d0067b.js -O ./apps/remix-ide/src/assets/js/soljson-v0.6.8-nightly.2020.5.14+commit.a6d0067b.js
-wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.0-nightly.2019.12.17+commit.d13438ee.js -O ./apps/remix-ide/src/assets/js/soljson-v0.6.0-nightly.2019.12.17+commit.d13438ee.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.7+commit.e28d00a7.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.8.7+commit.e28d00a7.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.16+commit.07a7930e.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.8.16+commit.07a7930e.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.15+commit.e14f2714.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.8.15+commit.e14f2714.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.7.4+commit.3f05b770.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.7.4+commit.3f05b770.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.4.14+commit.c2215d46.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.4.14+commit.c2215d46.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.8+commit.0bbfe453.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.6.8+commit.0bbfe453.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.8.1+commit.df193b15.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.8.1+commit.df193b15.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.5+commit.f956cc89.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.6.5+commit.f956cc89.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.8-nightly.2020.5.14+commit.a6d0067b.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.6.8-nightly.2020.5.14+commit.a6d0067b.js
+wget --no-check-certificate https://binaries.soliditylang.org/bin/soljson-v0.6.0-nightly.2019.12.17+commit.d13438ee.js -O ./dist/apps/remix-ide/assets/js/soljson-v0.6.0-nightly.2019.12.17+commit.d13438ee.js
