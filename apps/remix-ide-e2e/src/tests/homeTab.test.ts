81af656ac742e743cc561afc9057dd6db67986dd
Aniket-Engg <aniketengg.225@gmail.com>
2022-09-27 19:12:09 +0530

    new file creation e2e

--- apps/remix-ide-e2e/src/tests/homeTab.test.ts
+++ apps/remix-ide-e2e/src/tests/homeTab.test.ts
@@ -12,6 +12,12 @@ describe('HomeTab e2e test', function () {
         browser
         .waitForElementVisible('*[data-id="homeTabNewFile"]')
         .click('*[data-id="homeTabNewFile"]')
+        // click again to make it work
+        .click('*[data-id="homeTabNewFile"]')
+        .waitForElementVisible('li[data-id="treeViewLitreeViewItem//blank"]')
+        .sendKeys('li[data-id="treeViewLitreeViewItem//blank"]', 'newTestFile')
+        .sendKeys('li[data-id="treeViewLitreeViewItem//blank"]', browser.Keys.ENTER)
+        .waitForElementVisible('li[data-id="treeViewLitreeViewItemnewTestFile.sol"]')
     })
 
     after(browser => browser.end());
