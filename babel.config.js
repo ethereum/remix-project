65f069e9af7b1169baae4b58ad8741445df12cf5
yann300 <yann.levreau@gmail.com>
2022-09-27 09:45:11 +0200

    simulator in a web worker

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
         "babel-plugin-replace-ts-export-assignment", 
         "@babel/plugin-transform-modules-commonjs", 
         "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-nullish-coalescing-operator",
     ["@babel/plugin-proposal-private-property-in-object", { "loose": false }],
     ["@babel/plugin-proposal-private-methods", { "loose": false }]]
 }
