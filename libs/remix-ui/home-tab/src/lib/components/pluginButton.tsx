f67bdcdab792158b3757e02d50826353425d24c6
lianahus <liana@ethereum.org>
2022-09-28 15:16:31 +0200

    scrollelement for Featured plugins

--- libs/remix-ui/home-tab/src/lib/components/pluginButton.tsx
+++ libs/remix-ui/home-tab/src/lib/components/pluginButton.tsx
@@ -1,29 +1,43 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useContext } from 'react'
 import { ThemeContext } from '../themeContext'
-
+import { OverlayTrigger, Tooltip } from 'react-bootstrap' // eslint-disable-line
 interface PluginButtonProps {
   imgPath: string,
   envID: string,
   envText: string,
   callback: any,
-  l2?: boolean
+  l2?: boolean,
+  description: string,
+  remixMaintained?: boolean
 }
 
-function PluginButton ({ imgPath, envID, envText, callback, l2 }: PluginButtonProps) {
+function PluginButton ({ imgPath, envID, envText, callback, l2, description, remixMaintained }: PluginButtonProps) {
   const themeFilter = useContext(ThemeContext)
 
   return (
     <div>
       <button
-        className="btn border-secondary d-flex mr-3 text-nowrap justify-content-center flex-column align-items-center remixui_home_envButton"
+        className="btn border-secondary d-flex mr-3 text-nowrap justify-content-center align-items-center remixui_home_envButton"
         data-id={'landingPageStart' + envText}
         onClick={() => callback()}
       >
-        <img className="m-2 align-self-center remixui_home_envLogo" id={envID} src={imgPath} alt="" style={ { filter: themeFilter.filter } } />
-        <label className="text-uppercase text-dark remixui_home_cursorStyle">{envText}</label>
+        <img className="px-2 m-2 align-self-center remixui_home_envLogo" id={envID} src={imgPath} alt="" style={ { filter: themeFilter.filter } } />
+        <div className="h-100 d-flex flex-column">
+          <label className="text-uppercase text-dark remixui_home_cursorStyle">{envText}</label>
+          <div className="remixui_home_envLogoDescription">{description}</div>
+        </div>
       </button>
       { l2 && <label className="bg-light mx-1 px-1 mb-0 mx-2 position-relative remixui_home_l2Label">L2</label> }
+      { remixMaintained &&
+          <OverlayTrigger placement="bottom" overlay={
+            <Tooltip id="overlay-tooltip-run-script">
+              <span>Maintained by Remix</span>
+            </Tooltip>
+          }>
+            <i className="bg-light text-success mx-1 px-1 mb-0 mx-2 position-relative remixui_home_maintainedLabel fas fa-check"></i>
+          </OverlayTrigger>
+      }
 
     </div>
   )
