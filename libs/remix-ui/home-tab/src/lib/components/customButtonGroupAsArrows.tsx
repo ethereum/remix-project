a235e221be5df88ce61b9d9585e336b077ed86a7
lianahus <liana@ethereum.org>
2022-10-08 00:29:16 +0200

    Featured secction

---
+++ libs/remix-ui/home-tab/src/lib/components/customButtonGroupAsArrows.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState, useRef } from 'react'
+
+interface  CustomButtonGroupAsArrowsProps {}
+  
+function CustomButtonGroupAsArrows ({ next, previous }) {
+    return (
+        <div
+            style={{
+            textAlign: "center",
+            }}
+        >
+            <h4>These buttons can be positioned anywhere you want on the screen</h4>
+            <button onClick={previous}>Prev</button>
+            <button onClick={next}>Next</button>
+        </div>
+        )
+    }
+}
+
+export default CustomButtonGroupAsArrows
--- libs/remix-ui/home-tab/src/lib/components/homeTabFeaturedPlugins.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeaturedPlugins.tsx
@@ -23,6 +23,21 @@ function HomeTabFeaturedPlugins ({plugin}: HomeTabFeaturedPluginsProps) {
     themeQuality: themes.light,
   })
 
+  const carouselRef = useRef(null)
+
+  // Todo doesn't work
+  useEffect(() => {
+      window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  const handleScroll = (e) => {
+    console.log("e = ", e)   
+  }
+
+
   const startSolidity = async () => {
     await plugin.appManager.activatePlugin(['solidity', 'udapp', 'solidityStaticAnalysis', 'solidityUnitTesting'])
     plugin.verticalIcons.select('solidity')
@@ -40,43 +55,46 @@ function HomeTabFeaturedPlugins ({plugin}: HomeTabFeaturedPluginsProps) {
   }
   const startLearnEth = async () => {
     await plugin.appManager.activatePlugin(['solidity', 'LearnEth', 'solidityUnitTesting'])
-    plugin.verticalIcons.select('LearnEth')
+    plugin.verticalIcons.select('learnEth')
     _paq.push(['trackEvent', 'pluginManager', 'userActivate', 'learnEth'])
   }
   const startSourceVerify = async () => {
     await plugin.appManager.activatePlugin(['solidity', 'sourcify'])
     plugin.verticalIcons.select('sourcify')
     _paq.push(['trackEvent', 'pluginManager', 'userActivate', 'sourcify'])
-  }
-  const startPluginManager = async () => {
-    plugin.verticalIcons.select('pluginManager')
+  }  
+  const startSolidityUnitTesting = async () => {
+    await plugin.appManager.activatePlugin(['solidity', 'solidityUnitTesting'])
+    plugin.verticalIcons.select('solidityUnitTesting')
+    _paq.push(['trackEvent', 'pluginManager', 'userActivate', 'solidityUnitTesting'])
   }
   
-  return (
+    return (
     <div className="pl-2 w-100" id="hTFeaturedPlugins">
       <label className="pl-2" style={{fontSize: "1.2rem"}}>Featured Plugins</label>
       <div className="w-100 d-flex flex-column">
         <ThemeContext.Provider value={ state.themeQuality }>
           <Carousel 
+            ref={carouselRef}
+            focusOnSelect
             customButtonGroup={<CustomNavButtons next={undefined} previous={undefined} goToSlide={undefined} />}
             arrows={false}
             swipeable={false}
             draggable={true}
             showDots={false}
-            responsive={{ desktop: { breakpoint: { max: 3000, min: 1024 }, items: 4} }}
+            responsive={{ desktop: { breakpoint: { max: 3000, min: 1024 }, items: 5} }}
             renderButtonGroupOutside={true}
             ssr={true} // means to render carousel on server-side.
-            infinite={false}
             keyBoardControl={true}
             containerClass="carousel-container"
             deviceType={"desktop"}
-            itemClass="carousel-item-padding-10-px"
+            itemClass="w-100"
           >
             <PluginButton imgPath="assets/img/solidityLogo.webp" envID="solidityLogo" envText="Solidity" description="Compile, test and analyse smart contract." remixMaintained={true} callback={() => startSolidity()} />
             <PluginButton imgPath="assets/img/starkNetLogo.webp" envID="starkNetLogo" envText="StarkNet" description="Compile and deploy contracts with Cairo, a native language for StarkNet." l2={true} callback={() => startStarkNet()} />
             <PluginButton imgPath="assets/img/solhintLogo.webp" envID="solhintLogo" envText="Solhint linter" description="Solhint is an open source project for linting Solidity code." callback={() => startSolhint()} />
             <PluginButton imgPath="assets/img/sourcifyNewLogo.webp" envID="sourcifyLogo" envText="Sourcify" description="Solidity contract and metadata verification service." callback={() => startSourceVerify()} />
-            <PluginButton imgPath="assets/img/moreLogo.webp" envID="moreLogo" envText="Plugin Manager" description="Discover more plugins." callback={startPluginManager} />
+            <PluginButton imgPath="assets/img/unitTesting.webp" envID="sUTLogo" envText="Solidity unit testing" description="Write and run unit tests for your contracts in Solidity." callback={() => startSolidityUnitTesting()} />
           </Carousel>
         </ThemeContext.Provider>
       </div>
