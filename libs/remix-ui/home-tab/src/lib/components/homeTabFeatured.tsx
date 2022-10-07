23ee18fe17938f20bc8e3ad1e881d256a5ee4da8
lianahus <liana@ethereum.org>
2022-10-08 00:29:16 +0200

    Featured secction

--- libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
@@ -5,7 +5,7 @@ import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 import CustomNavButtons from './customNavButtons'
 
-function HomeTabFeatured () {
+function HomeTabFeatured() {
   const [state, setState] = useState<{
     themeQuality: { filter: string, name: string },
   }>({
@@ -27,36 +27,52 @@ function HomeTabFeatured () {
 
   return (
     <div className="pt-4 pl-2" id="hTFeaturedeSection">
-      <label style={{fontSize: "1.2rem"}}>Featured</label>
-      <div className=" mb-4">
-        <div className="w-100 d-flex flex-column" style={{height: "230px"}}>
-          <ThemeContext.Provider value={ state.themeQuality }>
-            <Carousel 
+      <label style={{ fontSize: "1.2rem" }}>Featured</label>
+      <div className="mb-4">
+        <div className="w-100 d-flex flex-column" style={{ height: "230px" }}>
+          <ThemeContext.Provider value={state.themeQuality}>
+            <Carousel
               customButtonGroup={<CustomNavButtons next={undefined} previous={undefined} goToSlide={undefined} />}
               arrows={false}
               swipeable={false}
               draggable={true}
               showDots={true}
-              responsive={{ desktop: { breakpoint: { max: 3000, min: 1024 }, items: 1} }}
+              responsive={{ desktop: { breakpoint: { max: 2000, min: 1024 }, items: 1 } }}
               renderDotsOutside={true}
               ssr={true} // means to render carousel on server-side.
               infinite={true}
+              centerMode={false}
               autoPlay={true}
               keyBoardControl={true}
               containerClass="border carousel-container"
+              sliderClass="px-2 h-100 justify-content-between"
               deviceType={"desktop"}
               itemClass="p-2 carousel-item-padding-10-px"
-              autoPlaySpeed={10000}
+              autoPlaySpeed={15000}
               dotListClass="position-relative mt-2"
             >
-              <div>
-                <img src={"assets/img/solidityLogo.webp"} style={{width: "300px", height:"200px"}} alt="" ></img>
-              </div><div>
-                <img src={"assets/img/bgRemi.webp"} alt="" ></img>
-              </div><div>
-                <img src={"assets/img/homeStickers.png"} alt="" ></img>blablabla
-              </div><div>
-                <img src={"assets/img/solidityLogo.webp"} alt="" ></img>blablabla
+              <div className="d-flex">
+                <img src={"assets/img/bgRemi.webp"} style={{ flex: "1", height: "200px", maxWidth: "max-content"}} alt="" ></img>
+                <div className="h6 w-50 p-4" style={{ flex: "1"}}>
+                  <h5>JUMP INTO WEB3</h5>
+                  <span>The Remix Project is a rich toolset which can be used for the entire journey of contract development by users of any knowledge level, and as a learning lab for teaching and experimenting with Ethereum.</span>
+                </div>
+              </div>
+              <div className="d-flex">
+                <img src={"/assets/img/remixRewardUser.webp"} style={{ flex: "1", height: "200px", maxWidth: "max-content" }} alt="" ></img>
+                <div className="h6 p-4" style={{ flex: "1"}}>
+                  <h5>REMIX REWARDS</h5>
+                  <p style={{fontStyle: 'italic'}}>NFTs for our users!</p>
+                  <span>Remix Project rewards contributors, beta testers, and UX research participants with NFTs deployed on Optimism. Remix Reward holders are able to mint a second “Remixer” user NFT badge to give to any other user of their choice</span>
+                </div>
+              </div>
+              <div className="d-flex">
+                <img src={"/assets/img/remixRewardBetaTester.webp"} style={{ flex: "1", height: "200px", maxWidth: "max-content" }} alt="" ></img>
+                <div className="h6 p-4" style={{ flex: "1"}}>
+                  <h5>BETA TESTING</h5>
+                  <p style={{fontStyle: 'italic'}}>Our community supports us.</p>
+                  <span>You can join Beta Testing before each release of Remix IDE. Help us test now and get a handle on new features!</span>
+                </div>
               </div>
             </Carousel>
           </ThemeContext.Provider>
--- libs/remix-ui/home-tab/src/lib/components/pluginButton.tsx
+++ libs/remix-ui/home-tab/src/lib/components/pluginButton.tsx
@@ -16,26 +16,26 @@ function PluginButton ({ imgPath, envID, envText, callback, l2, description, rem
   const themeFilter = useContext(ThemeContext)
 
   return (
-    <div>
+    <div className="d-flex remixui_home_envButton">
       <button
-        className="btn border-secondary d-flex mr-3 text-nowrap justify-content-center align-items-center remixui_home_envButton"
+        className="btn border-secondary d-flex text-nowrap justify-content-center align-items-center mr-2 remixui_home_envButton"
         data-id={'landingPageStart' + envText}
         onClick={() => callback()}
       >
-        <img className="px-2 m-2 align-self-center remixui_home_envLogo" id={envID} src={imgPath} alt="" style={ { filter: themeFilter.filter } } />
+        <img className="px-2 align-self-center remixui_home_envLogo" id={envID} src={imgPath} alt="" style={ { filter: themeFilter.filter } } />
         <div className="h-100 d-flex flex-column">
           <label className="text-uppercase text-dark remixui_home_cursorStyle">{envText}</label>
           <div className="remixui_home_envLogoDescription">{description}</div>
         </div>
       </button>
-      { l2 && <label className="bg-light mx-1 px-1 mb-0 mx-2 position-relative remixui_home_l2Label">L2</label> }
+      { l2 && <label className="bg-light mx-1 px-1 mb-0 mx-2 position-absolute remixui_home_l2Label">L2</label> }
       { remixMaintained &&
           <OverlayTrigger placement="bottom" overlay={
             <Tooltip id="overlay-tooltip-run-script">
               <span>Maintained by Remix</span>
             </Tooltip>
           }>
-            <i className="bg-light text-success mx-1 px-1 mb-0 mx-2 position-relative remixui_home_maintainedLabel fas fa-check"></i>
+            <i className="bg-light text-success mx-1 px-1 mb-0 mx-2 position-absolute remixui_home_maintainedLabel fas fa-check"></i>
           </OverlayTrigger>
       }
 
