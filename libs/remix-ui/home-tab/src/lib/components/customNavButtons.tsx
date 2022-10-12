96762f02715f2d84d86f621d804083e009a815df
Aniket-Engg <aniketengg.225@gmail.com>
2022-10-12 18:35:44 +0530

    arrows centered

--- libs/remix-ui/home-tab/src/lib/components/customNavButtons.tsx
+++ libs/remix-ui/home-tab/src/lib/components/customNavButtons.tsx
@@ -5,10 +5,10 @@ const CustomNavButtons = ({ next, previous, goToSlide, ...rest }) => {
   const { carouselState: { currentSlide, totalItems } } = rest
   return (
     <div className="mt-1 d-flex justify-content-end carousel-button-group">
-      <button className={currentSlide === 0 ? 'disable py-0 border btn' : 'py-0 border btn'} onClick={() => previous()}>
+      <button className={currentSlide === 0 ? 'disable py-1 border btn' : 'py-1 border btn'} onClick={() => previous()}>
         <i className="fas fa-angle-left"></i>
       </button>
-      <button className={currentSlide + 1 === totalItems ? 'disable py-0 border btn' : 'py-0 border btn'} onClick={() => {
+      <button className={currentSlide + 1 === totalItems ? 'disable py-1 border btn' : 'py-1 border btn'} onClick={() => {
           if (currentSlide + 1 < totalItems) goToSlide(currentSlide + 1)
         }} >
         <i className="fas fa-angle-right"></i>
