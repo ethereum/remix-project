4d12e0ecd4670be455dc8d547dc34b9d1de5dcd2
lianahus <liana@ethereum.org>
2022-10-19 10:34:49 +0200

    scroll functionality for FP and GS sections in home tab

--- libs/remix-ui/home-tab/src/lib/components/customNavButtons.tsx
+++ libs/remix-ui/home-tab/src/lib/components/customNavButtons.tsx
@@ -2,15 +2,24 @@
 import React from 'react'
 
 const CustomNavButtons = ({ next, previous, goToSlide, ...rest }) => {
-  const { carouselState: { currentSlide, totalItems } } = rest
+  const { carouselState: { currentSlide, totalItems, itemWidth, containerWidth } } = rest
   return (
     <div className="mt-1 d-flex justify-content-end carousel-button-group">
-      <button className={currentSlide === 0 ? 'disable py-1 border btn' : 'py-1 border btn'} onClick={() => previous()}>
+      <button
+        className={currentSlide === 0 ? 'disable py-1 border btn' : 'py-1 border btn'}
+        disabled={currentSlide === 0}
+        onClick={() => previous()}
+      >
         <i className="fas fa-angle-left"></i>
       </button>
-      <button className={currentSlide + 1 === totalItems ? 'disable py-1 border btn' : 'py-1 border btn'} onClick={() => {
-          if (currentSlide + 1 < totalItems) goToSlide(currentSlide + 1)
-        }} >
+      <button
+        className={ 
+          ((totalItems - currentSlide) * itemWidth + 5) < containerWidth ? 'disable py-1 border btn' : 'py-1 border btn'}
+        onClick={() => {
+            if (currentSlide + 1 < totalItems) goToSlide(currentSlide + 1)
+        }}
+        disabled ={((totalItems - currentSlide) * itemWidth + 5) < containerWidth}
+      >
         <i className="fas fa-angle-right"></i>
       </button>
     </div>
