9a5952ed6a26fbafa3d56f868f45c3c9550a1a4c
filip mertens <filip.mertens@ethereum.org>
2022-10-25 14:56:49 +0200

    add react app

---
+++ apps/remix-react/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./app.module.css";
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="remix-react" />
+      <div />
+    </>
+  );
+}
+
+export default App;
