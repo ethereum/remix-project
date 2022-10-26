4bdeeca1d251a86af6eab1d441d4f00e419be33a
Aniket-Engg <aniketengg.225@gmail.com>
2022-10-26 19:52:01 +0530

    fixes #2995

--- libs/remix-ui/run-tab/src/lib/components/contractDropdownUI.tsx
+++ libs/remix-ui/run-tab/src/lib/components/contractDropdownUI.tsx
@@ -256,7 +256,7 @@ export function ContractDropdownUI(props: ContractDropdownProps) {
       <div className='d-flex justify-content-between'>
         <div className="d-flex justify-content-between align-items-end">
           <label className="udapp_settingsLabel pr-1">Contract</label>
-          <div className="d-flex">{compilerName && compilerName !== '' && <label className="text-capitalize" style={{ maxHeight: '0.6rem', lineHeight: '1rem' }} data-id="udappCompiledBy">(Compiled by {compilerName})</label>}</div>
+          <div className="d-flex">{compilerName && compilerName !== '' && <label style={{ maxHeight: '0.6rem', lineHeight: '1rem' }} data-id="udappCompiledBy">(Compiled by <span className="text-capitalize"> {compilerName}</span>)</label>}</div>
         </div>
         {props.remixdActivated ?
           <OverlayTrigger placement={'right'} overlay={
