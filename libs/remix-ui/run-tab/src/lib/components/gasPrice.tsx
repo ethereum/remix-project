ca2676bbcd92f393ca02b9df96e1d556c6b155f1
Joseph Izang <josephizang@gmail.com>
2022-10-27 22:39:57 +0100

    apply custom tooltip to run-tab

--- libs/remix-ui/run-tab/src/lib/components/gasPrice.tsx
+++ libs/remix-ui/run-tab/src/lib/components/gasPrice.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-use-before-define
+import { CustomTooltip } from '@remix-ui/helper'
 import React from 'react'
-import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { GasPriceProps } from '../types'
 
 export function GasPriceUI (props: GasPriceProps) {
@@ -11,13 +11,14 @@ export function GasPriceUI (props: GasPriceProps) {
   return (
     <div className="udapp_crow">
       <label className="udapp_settingsLabel">Gas limit</label>
-      <OverlayTrigger placement={'right-end'} overlay={
-          <Tooltip className="text-nowrap" id="remixGasPriceTooltip">
-            <span>{"The default gas limit is 3M. Adjust as needed."}</span>
-          </Tooltip>
-      }>
+      <CustomTooltip
+        placement={'right-end'}
+        tooltipClasses="text-nowrap"
+        tooltipId="remixGasPriceTooltip"
+        tooltipText={"The default gas limit is 3M. Adjust as needed."}
+      >
         <input type="number" className="form-control udapp_gasNval udapp_col2" id="gasLimit" value={props.gasLimit} onChange={handleGasLimit} />
-      </OverlayTrigger>
+      </CustomTooltip>
     </div>
   )
 }
