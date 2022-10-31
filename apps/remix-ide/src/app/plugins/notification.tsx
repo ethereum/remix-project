f8e0c30f86ee4ed1b62bcd0113b504bbf4f47385
filip mertens <filip.mertens@ethereum.org>
2022-10-31 15:18:01 +0100

    fix imports

--- apps/remix-ide/src/app/plugins/notification.tsx
+++ apps/remix-ide/src/app/plugins/notification.tsx
@@ -1,8 +1,8 @@
 import { Plugin } from '@remixproject/engine'
 import { LibraryProfile, MethodApi, StatusEvents } from '@remixproject/plugin-utils'
 import { AppModal } from '@remix-ui/app'
-import { AlertModal } from 'libs/remix-ui/app/src/lib/remix-app/interface'
-import { dispatchModalInterface } from 'libs/remix-ui/app/src/lib/remix-app/context/context'
+import { AlertModal } from '@remix-ui/app'
+import { dispatchModalInterface } from '@remix-ui/app'
 
 interface INotificationApi {
   events: StatusEvents,
