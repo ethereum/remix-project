a11797586d4e0e87c6c8fef1646f88be1c243b90
filip mertens <filip.mertens@ethereum.org>
2022-10-31 15:15:48 +0100

    add id to toast

--- libs/remix-ui/app/src/lib/remix-app/context/provider.tsx
+++ libs/remix-ui/app/src/lib/remix-app/context/provider.tsx
@@ -36,10 +36,10 @@ export const ModalProvider = ({ children = [], reducer = modalReducer, initialSt
     })
   }
 
-  const toast = (message: string | JSX.Element) => {
+  const toast = (message: string | JSX.Element, uid?: string | number) => {
     dispatch({
       type: modalActionTypes.setToast,
-      payload: message
+      payload: { message, uid }
     })
   }
 
