6dc1640311bab6dc89d76ca6f9d04289b08aa4f9
Joseph Izang <josephizang@gmail.com>
2022-11-04 12:40:39 +0100

    fix bad overwrite of proxy feature

--- libs/remix-ui/run-tab/src/lib/components/contractGUI.tsx
+++ libs/remix-ui/run-tab/src/lib/components/contractGUI.tsx
@@ -557,30 +557,11 @@ export function ContractGUI (props: ContractGUIProps) {
                   <label className="mt-2 text-left d-block">
                     Proxy Address :
                   </label>
-                  <CustomTooltip
-                    tooltipText="Enter previously deployed proxy address on the selected network"
-                    placement="left"
-                    tooltipId="ERC1967AddressInputTooltip"
-                    tooltipClasses="text-nowrap"
-                  >
-                    <input
-                      style={{ height: 32 }}
-                      className="form-control udapp_input"
-                      data-id="ERC1967AddressInput"
-                      placeholder="proxy address"
-                      onChange={handleSetProxyAddress}
-                    />
-                  </CustomTooltip>
-                </div>
-               : (
-                <span
-                  className="text-capitalize"
-                  data-id="lastDeployedERC1967Address"
-                  style={{ fontSize: ".8em" }}
-                >
-                  {proxyAddress || "No proxy address available"}
-                </span>
-              )}
+                  <input style={{ height: 32 }} className="form-control udapp_input" data-id="ERC1967AddressInput" placeholder='proxy address' title='Enter previously deployed proxy address on the selected network' onChange={handleSetProxyAddress} onBlur={() => validateProxyAddress(proxyAddress) } />
+                  { proxyAddressError && <span className='text-lowercase' data-id="errorMsgProxyAddress" style={{ fontSize: '.8em' }}>{ proxyAddressError }</span> }
+                </div> :
+                <span className='text-capitalize' data-id="lastDeployedERC1967Address" style={{ fontSize: '.8em' }}>{ proxyAddress || proxyAddressError }</span>
+              }
             </div>
           </div>
         </>
