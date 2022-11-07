40ad95ddfe8c81a3103a25107e7c20e34edce2c7
yann300 <yann.levreau@gmail.com>
2022-11-07 10:30:06 +0100

    better comment

--- apps/etherscan/src/app/utils/scripts.ts
+++ apps/etherscan/src/app/utils/scripts.ts
@@ -2,7 +2,7 @@ export const verifyScript = `
 /**
  * @param {string} apikey - etherscan api key.
  * @param {string} contractAddress - Address of the contract to verify.
- * @param {string} contractArguments - Parameters used in the contract constructor during the initial deployment.
+ * @param {string} contractArguments - Parameters used in the contract constructor during the initial deployment. It should be the hex encoded value.
  * @param {string} contractName - Name of the contract
  * @param {string} contractFile - File where the contract is located
  * @returns {{ guid, status, message, succeed }} verification result
