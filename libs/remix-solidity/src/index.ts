54fa5030d36a02fc61517ed86bb19302c7753fd7
yann300 <yann.levreau@gmail.com>
2022-11-07 10:08:00 +0100

    better scoping of exported artefact

--- libs/remix-solidity/src/index.ts
+++ libs/remix-solidity/src/index.ts
@@ -1,7 +1,7 @@
 export { Compiler } from './compiler/compiler'
 export { compile } from './compiler/compiler-helpers'
 export { default as CompilerInput, getValidLanguage } from './compiler/compiler-input'
-export * from './compiler/compiler-abstract'
+export { CompilerAbstract } from './compiler/compiler-abstract'
 export * from './compiler/types'
 export { promisedMiniXhr, pathToURL, baseURLBin, baseURLWasm, canUseWorker, urlFromVersion } from './compiler/compiler-utils'
 export { default as helper } from './compiler/helper'
