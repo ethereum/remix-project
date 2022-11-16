d29daa191fb267da5e316b2605b52d883309bf8d
Aniket-Engg <aniketengg.225@gmail.com>
2022-11-16 17:21:46 +0530

    template files added

---
+++ libs/remix-ws-templates/src/templates/gnosisSafeMultisig/contracts/MultisigWallet.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+import "https://github.com/safe-global/safe-contracts/blob/main/contracts/GnosisSafe.sol";
+
+contract MultisigWallet is GnosisSafe {}
