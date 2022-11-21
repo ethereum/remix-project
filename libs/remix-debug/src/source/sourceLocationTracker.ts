42fe88df8669e1311f7887be6127024fe456e2e0
yann300 <yann.levreau@gmail.com>
2022-11-21 16:51:45 +0100

    remove need of using extractValidSourceLocation

--- libs/remix-debug/src/source/sourceLocationTracker.ts
+++ libs/remix-debug/src/source/sourceLocationTracker.ts
@@ -87,11 +87,15 @@ export class SourceLocationTracker {
       (map.file > amountOfSources - 1) this indicates the current file index exceed the total number of files.
                                               this happens when generated sources should not be considered.
     */
-    while (vmtraceStepIndex >= 0 && (map.file === -1 || map.file > amountOfSources - 1)) {
+    while (vmtraceStepIndex >= 0 && this.isInvalidSourceLocation(map, amountOfSources)) {
       map = await this.getSourceLocationFromVMTraceIndex(address, vmtraceStepIndex, contracts)
       vmtraceStepIndex = vmtraceStepIndex - 1
     }
     return map
+  }
+
+  isInvalidSourceLocation (sourceLocation, amountOfSources) {
+    return sourceLocation.file === -1 || sourceLocation.file > amountOfSources - 1
   }
 
   async getValidSourceLocationFromVMTraceIndexFromCache (address: string, vmtraceStepIndex: number, contracts: any, cache: Map<number, any>) {
