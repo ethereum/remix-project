544623109b00ae4bc14844e643513e455c660f45
Joseph Izang <josephizang@gmail.com>
2022-11-22 14:00:25 +0100

    simplify rearrangement code

--- libs/remix-ui/solidity-compiler/src/lib/logic/compileTabLogic.ts
+++ libs/remix-ui/solidity-compiler/src/lib/logic/compileTabLogic.ts
@@ -24,7 +24,7 @@ export class CompileTabLogic {
   constructor (public api: ICompilerApi, public contentImport) {
     this.event = new EventEmitter()
     this.compiler = new Compiler((url, cb) => api.resolveContentAndSave(url).then((result) => cb(null, result)).catch((error) => cb(error.message)))
-    this.evmVersions = ['default', 'berlin', 'london', 'istanbul', 'petersburg', 'constantinople', 'byzantium', 'spuriousDragon', 'tangerineWhistle', 'homestead']
+    this.evmVersions = ['default', 'london', 'berlin', 'istanbul', 'petersburg', 'constantinople', 'byzantium', 'spuriousDragon', 'tangerineWhistle', 'homestead']
   }
 
   init () {
