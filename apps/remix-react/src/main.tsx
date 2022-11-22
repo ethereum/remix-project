4e8e6992787e91cdb281cd41a5c6ce55cac03807
filip mertens <filip.mertens@ethereum.org>
2022-11-22 10:54:12 +0100

    rm react app

--- apps/remix-react/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM from "react-dom/client";
-import {  TransactionDebugger } from '@remix-project/remix-debug'
-
-import App from "./app/app";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
