5279df9b67e4c27e80cf8b21e30678d95e112fb5
Joseph Izang <josephizang@gmail.com>
2022-12-08 17:29:36 +0100

    fix text and translations

--- libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
@@ -36,22 +36,22 @@ function HomeTabFeatured() {
               dotListClass="position-relative mt-2"
             >
               <div className="mx-1 px-1 d-flex">
+                <img src={"/assets/img/remixRewardUser.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                <div className="h6 p-4" style={{ flex: "1" }}>
+                  <h5><FormattedMessage id='home.solidityDevSurveyHeader' /></h5>
+                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.solidityDevSurvey1' /></p>
+                  <p>
+                    <FormattedMessage id='home.solidityDevSurvey' />
+                  </p>
+                  <a className="remixui_home_text" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'soliditySurvey'])} href="https://blog.soliditylang.org/2022/12/07/solidity-developer-survey-2022-announcement/"><FormattedMessage id='home.surveyLink' /></a>
+                </div>
+              </div>
+              <div className="mx-1 px-1 d-flex">
                 <img src={"assets/img/bgRemi.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
                 <div className="h6 w-50 p-4" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.jumpIntoWeb3' /></h5>
                   <p><FormattedMessage id='home.jumpIntoWeb3Text'/></p>
                   <a className="remixui_home_text" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'jumpIntoWeb3'])} target="__blank" href="https://remix-project.org"><FormattedMessage id='home.more' /></a>
-                </div>
-              </div>
-              <div className="mx-1 px-1 d-flex">
-                <img src={"/assets/img/remixRewardUser.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
-                <div className="h6 p-4" style={{ flex: "1" }}>
-                  <h5><FormattedMessage id='home.solidityDevSurveyHeader' /></h5>
-                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.solidityDevSurvey' /></p>
-                  <p>
-                    {/* <FormattedMessage id='' /> */}
-                  </p>
-                  <a className="remixui_home_text" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'soliditySurvey'])} href="https://blog.soliditylang.org/2022/12/07/solidity-developer-survey-2022-announcement/"><FormattedMessage id='home.solidityDevSurvey1' /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
