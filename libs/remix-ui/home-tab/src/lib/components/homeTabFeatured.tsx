e0d31bc917cd03e60f76984b427cfc383f34b7fd
Joseph Izang <josephizang@gmail.com>
2022-12-12 15:27:29 +0100

    removed faulty chinese translation

--- libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
@@ -33,43 +33,45 @@ function HomeTabFeatured() {
               sliderClass="h-100 justify-content-between"
               deviceType={"desktop"}
               itemClass=""
-              autoPlaySpeed={15000}
+              autoPlaySpeed={150000}
               dotListClass="position-relative mt-2"
             >
               <div className="mx-1 px-1 d-flex">
                 <img className="mt-2 pb-1" src={"/assets/img/solidity.webp"} style={{ flex: "1", height: "150px", maxWidth: "150px", paddingTop: 2, paddingBottom: 2, filter: themeFilter.filter }} alt="" ></img>
-                <div className="h6 w-50 pt-2" style={{ flex: "1" }}>
+                <div className="h6 w-50 p-4" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.solidityDevSurveyHeader' /></h5>
-                  <p className="remixui_home_carouselText">
+                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.solidityDevSurvey1' /></p>
+                  <div style={{ fontSize: 'medium' }}>
                     <FormattedMessage id='home.solidityDevSurvey' />
-                  </p>
-                  <a className="remixui_home_text btn btn-primary text-decoration-none mb-3" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'soliditySurvey'])} href="https://blog.soliditylang.org/2022/12/07/solidity-developer-survey-2022-announcement/"><FormattedMessage id='home.surveyLink' /></a>
+                  </div>
+                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'soliditySurvey'])} href="https://blog.soliditylang.org/2022/12/07/solidity-developer-survey-2022-announcement/"><FormattedMessage id='home.surveyLink' /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
                 <img src={"assets/img/bgRemi.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
                 <div className="h6 w-50 p-4" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.jumpIntoWeb3' /></h5>
-                  <p><FormattedMessage id='home.jumpIntoWeb3Text'/></p>
-                  <a className="remixui_home_text" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'jumpIntoWeb3'])} target="__blank" href="https://remix-project.org"><FormattedMessage id='home.more' /></a>
+                  
+                  <div><FormattedMessage id='home.jumpIntoWeb3Text'/></div>
+                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'jumpIntoWeb3'])} target="__blank" href="https://remix-project.org"><FormattedMessage id='home.more' /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
                 <img src={"/assets/img/remixRewardUser.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
                 <div className="h6 w-50 p-4" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.remixRewards' /></h5>
-                  {/* <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.remixRewardsText1' /></p> */}
-                  <p><FormattedMessage id='home.remixRewardsText2' /></p>
-                  <a className="remixui_home_text" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'remixRewards'])} href="https://rewards.remix.ethereum.eth.limo"><FormattedMessage id='home.more' /></a>
+                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.remixRewardsText1' /></p>
+                  <div><FormattedMessage id='home.remixRewardsText2' /></div>
+                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'remixRewards'])} href="https://rewards.remix.ethereum.eth.limo"><FormattedMessage id='home.more' /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
                 <img src={"/assets/img/remixRewardBetaTester.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
                 <div className="h6 w-50 p-4" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.betaTesting' /></h5>
-                  {/* <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.betaTestingText1' /></p> */}
-                  <p><FormattedMessage id='home.betaTestingText2' /></p>
-                  <a className="remixui_home_text" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'betatesting'])} target="__blank" href="https://rewards.remix.ethereum.eth.limo"><FormattedMessage id='home.more' /></a>
+                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.betaTestingText1' /></p>
+                  <div><FormattedMessage id='home.betaTestingText2' /></div>
+                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'betatesting'])} target="__blank" href="https://rewards.remix.ethereum.eth.limo"><FormattedMessage id='home.more' /></a>
                 </div>
               </div>
             </Carousel>
