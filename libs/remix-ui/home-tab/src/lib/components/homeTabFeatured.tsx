2c3d34b681f964e933536742db77043c66438bcf
Joseph Izang <josephizang@Josephs-MacBook-Pro.local>
2022-12-12 10:35:09 +0100

    add fixes as per review comments

--- libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
@@ -37,19 +37,18 @@ function HomeTabFeatured() {
               dotListClass="position-relative mt-2"
             >
               <div className="mx-1 px-1 d-flex">
-                <img src={"/assets/img/solidity.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px", paddingTop: 2, paddingBottom: 2 }} alt="" ></img>
-                <div className="h6 p-4" style={{ flex: "1" }}>
+                <img className="mt-1 pb-1" src={"/assets/img/solidity.webp"} style={{ flex: "1", height: "160px", maxWidth: "170px", paddingTop: 2, paddingBottom: 2, filter: themeFilter.filter }} alt="" ></img>
+                <div className="h6 pt-2" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.solidityDevSurveyHeader' /></h5>
-                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.solidityDevSurvey1' /></p>
                   <p>
                     <FormattedMessage id='home.solidityDevSurvey' />
                   </p>
-                  <a className="remixui_home_text" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'soliditySurvey'])} href="https://blog.soliditylang.org/2022/12/07/solidity-developer-survey-2022-announcement/"><FormattedMessage id='home.surveyLink' /></a>
+                  <a className="remixui_home_text btn btn-primary text-decoration-none mb-3" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'soliditySurvey'])} href="https://blog.soliditylang.org/2022/12/07/solidity-developer-survey-2022-announcement/"><FormattedMessage id='home.surveyLink' /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
                 <img src={"assets/img/bgRemi.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
-                <div className="h6 p-4" style={{ flex: "1" }}>
+                <div className="h6 w-50 p-4" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.jumpIntoWeb3' /></h5>
                   <p><FormattedMessage id='home.jumpIntoWeb3Text'/></p>
                   <a className="remixui_home_text" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'jumpIntoWeb3'])} target="__blank" href="https://remix-project.org"><FormattedMessage id='home.more' /></a>
