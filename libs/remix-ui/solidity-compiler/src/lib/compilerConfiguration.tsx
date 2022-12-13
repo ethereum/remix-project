4061f681b46fb0da535769cdee6e0ef8729ba2d6
yann300 <yann.levreau@gmail.com>
2022-12-13 11:08:42 +0100

    Update compilerConfiguration.tsx

--- libs/remix-ui/solidity-compiler/src/lib/compilerConfiguration.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/compilerConfiguration.tsx
@@ -12,7 +12,7 @@ export const configFileContent = `
 			"*": ["abi", "metadata", "devdoc", "userdoc", "storageLayout", "evm.legacyAssembly", "evm.bytecode", "evm.deployedBytecode", "evm.methodIdentifiers", "evm.gasEstimates", "evm.assembly"]
 			}
 		},
-		"evmVersion": "byzantium"
+		"evmVersion": null
 	}
 }
 `
