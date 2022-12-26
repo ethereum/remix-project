c0b3245b77791d23e0a16417440392d6857b4ffe
Joseph Izang <josephizang@gmail.com>
2022-12-26 21:36:31 +0100

    fix e2e test

--- libs/remix-ui/solidity-compiler/src/lib/contract-selection.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/contract-selection.tsx
@@ -238,6 +238,7 @@ export const ContractSelection = (props: ContractSelectionProps) => {
     const contractListConfirm = contractList.some(x => x.file === currentFile)
     return confirmNodeType && contractListConfirm
   }
+  
   return (
     // define swarm logo
     <>
