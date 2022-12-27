ff135480dfa34c58a0c476c333e256f2a6b0421a
Joseph Izang <josephizang@gmail.com>
2022-12-27 11:09:46 +0100

    fix e2e

--- apps/remix-ide-e2e/src/commands/clickLaunchIcon.ts
+++ apps/remix-ide-e2e/src/commands/clickLaunchIcon.ts
@@ -3,14 +3,9 @@ import EventEmitter from 'events'
 
 class ClickLaunchIcon extends EventEmitter {
   command (this: NightwatchBrowser, icon: string): NightwatchBrowser {
-    console.log(icon)
     this.api
-    .useXpath()
-    .assert.visible(`//*[@id="verticalIconsKind${icon}"]`)
-    // .waitForElementVisible('#icon-panel div[plugin="' + icon + '"]')
-    .click(`//*[@id="verticalIconsKind${icon}"]`)
-    .useCss()
-    // .click('#icon-panel div[plugin="' + icon + '"]')
+    .waitForElementVisible('#icon-panel div[plugin="' + icon + '"]')
+    .click('#icon-panel div[plugin="' + icon + '"]')
     .perform((done) => {
       done()
       this.emit('complete')
