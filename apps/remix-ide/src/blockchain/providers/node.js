61347f4d3edfe8d3b568770f846e5897ae31171f
yann300 <yann.levreau@gmail.com>
2023-01-05 12:13:30 +0100

    stop using require with ethereumjs/ deps

--- apps/remix-ide/src/blockchain/providers/node.js
+++ apps/remix-ide/src/blockchain/providers/node.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3')
-const { hashPersonalMessage } = require('@ethereumjs/util')
+import { hashPersonalMessage } from '@ethereumjs/util'
 const Personal = require('web3-eth-personal')
 
 class NodeProvider {
