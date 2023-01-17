3a6b36337d8d1609c6555c30b059ff4087df0844
Aniket-Engg <aniketengg.225@gmail.com>
2023-01-17 18:28:41 +0530

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -6,5 +6,8 @@ module.exports = {
         "@babel/plugin-proposal-class-properties",
         "@babel/plugin-proposal-nullish-coalescing-operator",
     ["@babel/plugin-proposal-private-property-in-object", { "loose": false }],
-    ["@babel/plugin-proposal-private-methods", { "loose": false }]]
+    ["@babel/plugin-proposal-private-methods", { "loose": false }],
+    ["@babel/plugin-transform-runtime", {
+        "regenerator": true
+      }]]
 }
