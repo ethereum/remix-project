a6a28dde7c2d673fc681e4b001ed3a3668521f96
Joseph Izang <josephizang@gmail.com>
2023-01-20 23:29:54 +0100

    add solidity-uml-gen react lib

---
+++ libs/remix-ui/solidity-uml-gen/src/lib/solidity-uml-gen.tsx
@@ -0,0 +1,15 @@
+
+export interface RemixUiSolidityUmlGenProps {
+  plugin: any
+}
+
+function RemixUiSolidityUmlGen ({ plugin }: RemixUiSolidityUmlGenProps) {
+
+  return (
+    <div>
+      <h1>Solidity 2 UML Generator View!</h1>
+    </div>
+  )
+}
+
+export default RemixUiSolidityUmlGen
