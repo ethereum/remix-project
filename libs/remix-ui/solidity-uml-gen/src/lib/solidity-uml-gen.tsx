7f7713f8751c5513f861908425c9387a7672813a
Joseph Izang <josephizang@gmail.com>
2023-01-21 01:30:15 +0100

    add react viewer to solidityumlgen react lib

--- libs/remix-ui/solidity-uml-gen/src/lib/solidity-uml-gen.tsx
+++ libs/remix-ui/solidity-uml-gen/src/lib/solidity-uml-gen.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Viewer from 'react-viewer'
 export interface RemixUiSolidityUmlGenProps {
   plugin: any
 }
 
 export function RemixUiSolidityUmlGen ({ plugin }: RemixUiSolidityUmlGenProps) {
-
+  const [showViewer, setShowViewer] = useState(false)
+  const [svgPayload, setSVGPayload] = useState('')
   return (
     <div>
       <h1>Solidity 2 UML Generator View!</h1>
+      <Viewer
+          visible={showViewer}
+          rotatable={false}
+          loop={false}
+          noClose={false}
+          onClose={() => setShowViewer(false)}
+          noFooter={true}
+          showTotal={false}
+          changeable={false}
+          zoomSpeed={0.2}
+          minScale={1}
+          images={[{src: `data:image/svg+xml;base64,${btoa(svgPayload)}`}]}
+        />
     </div>
   )
 }
