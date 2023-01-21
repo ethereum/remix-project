6a03e8fba4d786a02a3c5f067541226b2423a8ae
filip mertens <filip.mertens@ethereum.org>
2023-01-21 11:49:48 +0100

    git config

--- apps/remix-ide/ci/remixd_test.zsh
+++ apps/remix-ide/ci/remixd_test.zsh
@@ -6,6 +6,9 @@ BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
 echo "$BUILD_ID"
 TEST_EXITCODE=0
 
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
 cd ..
 echo $SHELL
 ls -la .
