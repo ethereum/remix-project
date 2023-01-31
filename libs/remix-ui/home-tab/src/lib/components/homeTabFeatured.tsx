d162735a6a24918f248460824892eeca29b55755
lianahus <liana@ethereum.org>
2023-01-31 19:05:50 +0100

    added youtube page to home tab

--- libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
@@ -42,16 +42,30 @@ function HomeTabFeatured() {
                   <h5><FormattedMessage id='home.jumpIntoWeb3' /></h5>
                   
                   <div><FormattedMessage id='home.jumpIntoWeb3Text'/></div>
-                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'jumpIntoWeb3'])} target="__blank" href="https://remix-project.org"><FormattedMessage id='home.more' /></a>
+                  <a
+                    className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3"
+                    onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'jumpIntoWeb3'])}
+                    target="__blank"
+                    href="https://remix-project.org"><FormattedMessage
+                    id='home.jumpIntoWeb3More'
+                  /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
-                <img src={"/assets/img/remixRewardUser_small.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                <a href="https://www.youtube.com/@EthereumRemix/videos"  target="__blank">
+                  <img src={"/assets/img/YouTubeLogo.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                </a>
                 <div className="h6 w-50 p-4" style={{ flex: "1" }}>
-                  <h5><FormattedMessage id='home.remixRewards' /></h5>
-                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.remixRewardsText1' /></p>
-                  <div><FormattedMessage id='home.remixRewardsText2' /></div>
-                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" target="__blank" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'remixRewards'])} href="https://rewards.remix.ethereum.eth.limo"><FormattedMessage id='home.more' /></a>
+                  <h5><FormattedMessage id='home.remixYouTube' /></h5>
+                  <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.remixYouTubeText1' /></p>
+                  <div><FormattedMessage id='home.remixYouTubeText2' /></div>
+                  <a
+                    className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3"
+                    onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'youTubeMore'])}
+                    target="__blank"
+                    href="https://www.youtube.com/@EthereumRemix/videos"><FormattedMessage
+                    id='home.remixYouTubeMore'
+                  /></a>
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
@@ -60,7 +74,13 @@ function HomeTabFeatured() {
                   <h5><FormattedMessage id='home.betaTesting' /></h5>
                   <p style={{ fontStyle: 'italic' }}><FormattedMessage id='home.betaTestingText1' /></p>
                   <div><FormattedMessage id='home.betaTestingText2' /></div>
-                  <a className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3" onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'betatesting'])} target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLSd0WsJnKbeJo-BGrnf7WijxAdmE4PnC_Z4M0IApbBfHLHZdsQ/viewform"><FormattedMessage id='home.more' /></a>
+                  <a
+                    className="remixui_home_text btn btn-secondary mt-2 text-decoration-none mb-3"
+                    onClick={() => _paq.push(['trackEvent', 'hometab', 'featuredSection', 'betatesting'])}
+                    target="__blank"
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSd0WsJnKbeJo-BGrnf7WijxAdmE4PnC_Z4M0IApbBfHLHZdsQ/viewform"><FormattedMessage
+                    id='home.betaTestingMore'
+                  /></a>
                 </div>
               </div>
             </Carousel>
