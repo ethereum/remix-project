d7aeca11a1fe801f5c388d702fee5eaceb7ee7ae
filip mertens <filip.mertens@ethereum.org>
2023-02-04 13:06:05 +0100

    testing

--- apps/remix-ide/webpack.config.js
+++ apps/remix-ide/webpack.config.js
@@ -1,9 +1,10 @@
-const nxWebpack = require('@nrwl/react/plugins/webpack')
-const CopyPlugin = require("copy-webpack-plugin");
+const { composePlugins, withNx } = require('@nrwl/webpack');
+const { withReact } = require('@nrwl/react');
 const webpack = require('webpack')
+const CopyPlugin = require("copy-webpack-plugin");
 const version = require('../../package.json').version
 const fs = require('fs')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require("terser-webpack-plugin");
 
 const versionData = {
   version: version,
@@ -13,71 +14,80 @@ const versionData = {
 
 fs.writeFileSync('./apps/remix-ide/src/assets/version.json', JSON.stringify(versionData))
 
-module.exports = config => {
-  const nxWebpackConfig = nxWebpack(config)
-  const webpackConfig = {
-    ...nxWebpackConfig,
-    resolve: {
-      ...nxWebpackConfig.resolve,
-      fallback: {
-        ...nxWebpackConfig.resolve.fallback,
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "path": require.resolve("path-browserify"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "constants": require.resolve("constants-browserify"),
-        "os": false, //require.resolve("os-browserify/browser"),
-        "timers": false, // require.resolve("timers-browserify"),
-        "zlib": require.resolve("browserify-zlib"),
-        "fs": false,
-        "module": false,
-        "tls": false,
-        "net": false,
-        "readline": false,
-        "child_process": false,
-        "buffer": require.resolve("buffer/"),
-        "vm": require.resolve('vm-browserify'),
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), withReact(), (config) => {
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
+
+  // add fallback for node modules
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "path": require.resolve("path-browserify"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "constants": require.resolve("constants-browserify"),
+    "os": false, //require.resolve("os-browserify/browser"),
+    "timers": false, // require.resolve("timers-browserify"),
+    "zlib": require.resolve("browserify-zlib"),
+    "fs": false,
+    "module": false,
+    "tls": false,
+    "net": false,
+    "readline": false,
+    "child_process": false,
+    "buffer": require.resolve("buffer/"),
+    "vm": require.resolve('vm-browserify'),
+  }
+
+  // add externals
+  config.externals = {
+    ...config.externals,
+    solc: 'solc',
+  }
+
+  // add public path
+  config.output.publicPath = '/'
+
+  // set filename
+  config.output.filename = `[name].${versionData.version}.${versionData.timestamp}.js`
+  config.output.chunkFilename = `[name].${versionData.version}.${versionData.timestamp}.js`
+
+  // add plugin
+  // add copy plugin
+  config.plugins.push(
+    new CopyPlugin({
+      patterns: [
+        { from: '../../node_modules/monaco-editor/dev/vs', to: 'assets/js/monaco-editor/dev/vs' }
+      ].filter(Boolean)
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      url: ['url', 'URL'],
+      process: 'process/browser',
+    })
+  )
+
+  // set minimizer
+  config.optimization.minimizer = [
+    new TerserPlugin({
+      parallel: true,
+      terserOptions: {
+        ecma: 6,
+        compress: {
+          drop_console: true,
+        },
+        output: {
+          comments: false,
+        },
       },
-    },
-    externals: {
-      ...nxWebpackConfig.externals,
-      solc: 'solc',
-    },
+      extractComments: false,
+    }),
+  ];
 
-    output: {
-      ...nxWebpackConfig.output,
-      publicPath: '/',
-      filename: `[name].${versionData.version}.${versionData.timestamp}.js`,
-      chunkFilename: `[name].${versionData.version}.${versionData.timestamp}.js`,
-    },
-    plugins: [
-      ...nxWebpackConfig.plugins,
-      //new BundleAnalyzerPlugin({
-      //  analyzerMode: 'static'
-      //}),
-      new CopyPlugin({
-        patterns: [
-          { from: '../../node_modules/monaco-editor/dev/vs', to: 'assets/js/monaco-editor/dev/vs' }
-        ].filter(Boolean)
-      }),
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-        url: ['url', 'URL'],
-        process: 'process/browser',
-      }),
-    ]
-  }
+  console.log(config)
 
-  webpackConfig.output.chunkLoadTimeout = 600000
 
-  if (process.env.NODE_ENV === 'production') {
-    return {
-      ...webpackConfig,
-      mode: 'production',
-      devtool: 'source-map',
-    }
-  } else {
-    return webpackConfig
-  }
-}
+  return config;
+});
