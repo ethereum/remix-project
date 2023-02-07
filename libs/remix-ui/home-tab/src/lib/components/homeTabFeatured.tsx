e749ff0b607c069c02e3d30b24557a71dd14fd70
lianahus <liana@ethereum.org>
2023-02-07 12:17:54 +0100

    made images on Featured clickable

--- libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFeatured.tsx
@@ -37,7 +37,9 @@ function HomeTabFeatured() {
               dotListClass="position-relative mt-2"
             >
               <div className="mx-1 px-1 d-flex">
-                <img src={"assets/img/bgRemi_small.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                <a href="https://remix-project.org" target="__blank">
+                  <img src={"assets/img/bgRemi_small.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                </a>
                 <div className="h6 w-50 p-2 pl-4  align-self-center" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.jumpIntoWeb3' /></h5>
                   <div style={{ fontSize: '0.8rem' }} className="mb-3"><FormattedMessage id='home.jumpIntoWeb3Text'/></div>
@@ -52,7 +54,7 @@ function HomeTabFeatured() {
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
-                <a href="https://www.youtube.com/@EthereumRemix/videos"  target="__blank">
+                <a href="https://www.youtube.com/@EthereumRemix/videos" target="__blank">
                   <img src={"/assets/img/YouTubeLogo.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
                 </a>
                 <div className="h6 w-50 p-2 pl-4  align-self-center" style={{ flex: "1" }}>
@@ -70,7 +72,9 @@ function HomeTabFeatured() {
                 </div>
               </div>
               <div className="mx-1 px-1 d-flex">
-                <img src={"/assets/img/remixRewardBetaTester_small.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSd0WsJnKbeJo-BGrnf7WijxAdmE4PnC_Z4M0IApbBfHLHZdsQ/viewform" target="__blank">
+                  <img src={"/assets/img/remixRewardBetaTester_small.webp"} style={{ flex: "1", height: "170px", maxWidth: "170px" }} alt="" ></img>
+                </a>
                 <div className="h6 w-50 p-2 pl-4  align-self-center" style={{ flex: "1" }}>
                   <h5><FormattedMessage id='home.betaTesting' /></h5>
                   <p style={{ fontStyle: 'italic', fontSize: '1rem' }}><FormattedMessage id='home.betaTestingText1' /></p>
