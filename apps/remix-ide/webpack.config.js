d345a4eb8d182e1b4c7e6841d098bc4332403810
filip mertens <filip.mertens@ethereum.org>
2023-02-15 15:03:38 +0100

    fix build

--- apps/remix-ide/webpack.config.js
+++ apps/remix-ide/webpack.config.js
@@ -4,6 +4,8 @@ const webpack = require('webpack')
 const version = require('../../package.json').version
 const fs = require('fs')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require("terser-webpack-plugin")
+
 
 const versionData = {
   version: version,
@@ -14,7 +16,13 @@ const versionData = {
 fs.writeFileSync('./apps/remix-ide/src/assets/version.json', JSON.stringify(versionData))
 
 module.exports = config => {
+  console.log("GET WEBPACK CONFIG")
   const nxWebpackConfig = nxWebpack(config)
+  // remove mini-css-extract-plugin injected by nx
+  nxWebpackConfig.plugins = nxWebpackConfig.plugins.filter(plugin => {
+    return plugin.constructor.name !== 'MiniCssExtractPlugin'
+  })
+  
   const webpackConfig = {
     ...nxWebpackConfig,
     resolve: {
@@ -58,7 +66,7 @@ module.exports = config => {
       //}),
       new CopyPlugin({
         patterns: [
-          { from: '../../node_modules/monaco-editor/dev/vs', to: 'assets/js/monaco-editor/dev/vs' }
+          { from: '../../node_modules/monaco-editor/dev/vs', to: 'assets/js/monaco-editor/dev/vs' },
         ].filter(Boolean)
       }),
       new webpack.ProvidePlugin({
@@ -66,18 +74,36 @@ module.exports = config => {
         url: ['url', 'URL'],
         process: 'process/browser',
       }),
+      // new HtmlWebpackPlugin({
+      //   template: 'src/index.html',
+      //   filename: 'index.html',
+      //   minify: false,
+      // }),
+      // new HtmlWebpackSkipAssetsPlugin({
+      //   // exclude all javascript files
+      //   skipAssets: [/.*\.js/],
+      // })
     ]
   }
-
+  webpackConfig.ignoreWarnings = [/Failed to parse source map/] // ignore source-map-loader warnings
   webpackConfig.output.chunkLoadTimeout = 600000
 
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV)
   if (process.env.NODE_ENV === 'production') {
-    return {
+
+    const config = {
       ...webpackConfig,
       mode: 'production',
       devtool: 'source-map',
+      optimization: {
+        minimize: false,
+        mangleExports: false,
+      },
     }
+    console.log('config', config.plugins)
+    return config
   } else {
+    console.log('config dev')
     return webpackConfig
   }
 }
