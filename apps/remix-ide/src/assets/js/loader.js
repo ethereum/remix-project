cf9b7c3db8901757ba7d4cfac150884ea9450b62
filip mertens <filip.mertens@ethereum.org>
2023-02-16 12:50:26 +0100

    double load fix

--- apps/remix-ide/src/assets/js/loader.js
+++ apps/remix-ide/src/assets/js/loader.js
@@ -32,13 +32,6 @@ if (domains[window.location.hostname]) {
   })()
 }
 
-createScriptTag = function (url, type) {
-  var script = document.createElement('script');
-  script.src = url;
-  script.type = type;
-  document.getElementsByTagName('head')[0].appendChild(script);
-};
-
 function isElectron() {
   // Renderer process
   if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
@@ -63,11 +56,5 @@ fetch(versionUrl, { cache: "no-store" }).then(response => {
   response.text().then(function (data) {
     const version = JSON.parse(data);
     console.log(`Loading Remix ${version.version}`);
-    createScriptTag(`polyfills.${version.version}.${version.timestamp}.js`, 'module');
-    if (version.mode === 'development') {
-      createScriptTag(`vendor.${version.version}.${version.timestamp}.js`, 'module');
-      createScriptTag(`runtime.${version.version}.${version.timestamp}.js`, 'module');
-    }
-    createScriptTag(`main.${version.version}.${version.timestamp}.js`, 'text/javascript');
   });
 });
