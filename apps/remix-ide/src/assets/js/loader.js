449831f4892f4026add643e2e0cc5714ec424d08
filip mertens <filip.mertens@ethereum.org>
2023-02-16 10:23:05 +0100

    fix build

--- apps/remix-ide/src/assets/js/loader.js
+++ apps/remix-ide/src/assets/js/loader.js
@@ -32,12 +32,7 @@ if (domains[window.location.hostname]) {
   })()
 }
 
-createScriptTag = function (url, type) {
-  var script = document.createElement('script');
-  script.src = url;
-  script.type = type;
-  document.getElementsByTagName('head')[0].appendChild(script);
-};
+
 
 function isElectron() {
   // Renderer process
@@ -61,13 +56,8 @@ function isElectron() {
 const versionUrl = 'assets/version.json'
 fetch(versionUrl, { cache: "no-store" }).then(response => {
   response.text().then(function (data) {
+
     const version = JSON.parse(data);
     console.log(`Loading Remix ${version.version}`);
-    createScriptTag(`polyfills.${version.version}.${version.timestamp}.js`, 'module');
-    if (version.mode === 'development') {
-      createScriptTag(`vendor.${version.version}.${version.timestamp}.js`, 'module');
-      createScriptTag(`runtime.${version.version}.${version.timestamp}.js`, 'module');
-    }
-    createScriptTag(`main.${version.version}.${version.timestamp}.js`, 'text/javascript');
   });
 });
