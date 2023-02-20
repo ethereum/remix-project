eeca72360e76e6a6ae9e925c47a80e8aba1daf7b
Joseph Izang <josephizang@gmail.com>
2023-02-20 13:49:56 +0100

    add functions to download uml in two formats

--- libs/remix-ui/solidity-uml-gen/src/lib/components/UmlDownload.tsx
+++ libs/remix-ui/solidity-uml-gen/src/lib/components/UmlDownload.tsx
@@ -2,16 +2,25 @@ import React, { Fragment } from 'react'
 import { Dropdown } from 'react-bootstrap'
 
 export default function UmlDownload() {
+
+  const downloadAsPng = () => {
+    // convert serialized svg to png and download
+
+  }
+
+  const downloadAsPdf = () => {
+    // convert serialized svg to pdf and download
+  }
   return(
     <Fragment>
       <Dropdown>
         <Dropdown.Toggle className="badge badge-info remixui_no-shadow p-2 rounded-circle mr-2">
-        <i className="far fa-arrow-to-bottom align-item-center d-flex justify-content-center"></i>
+          <i className="far fa-arrow-to-bottom align-item-center d-flex justify-content-center"></i>
         </Dropdown.Toggle>
         <Dropdown.Menu>
-          <Dropdown.Item>Download as PNG</Dropdown.Item>
+          <Dropdown.Item onClick={downloadAsPng}>Download as PNG</Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item>Download as PDF</Dropdown.Item>
+          <Dropdown.Item onClick={downloadAsPdf}>Download as PDF</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </Fragment>
