11b63030389c5cf085cfd0a6d9e014f37c890052
filip mertens <filip.mertens@ethereum.org>
2023-02-23 08:16:28 +0100

    rm flaky

--- apps/remix-ide-e2e/src/tests/solidityImport.test.ts
+++ apps/remix-ide-e2e/src/tests/solidityImport.test.ts
@@ -24,7 +24,7 @@ module.exports = {
       .pause(1000)
   },
 
-  'Test Failed Import #group1 #flaky': function (browser: NightwatchBrowser) {
+  'Test Failed Import #group1': function (browser: NightwatchBrowser) {
     browser.addFile('Untitled3.sol', sources[2]['Untitled3.sol'])
       .clickLaunchIcon('solidity')
       .click('[data-id="compilerContainerCompileBtn"]')
