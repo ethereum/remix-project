432ca2d129acefcf8d9cbbd7e197f64516bd3183
filip mertens <filip.mertens@ethereum.org>
2023-02-27 12:07:28 +0100

    png

--- apps/remix-ide-e2e/src/tests/remixd.test.ts
+++ apps/remix-ide-e2e/src/tests/remixd.test.ts
@@ -316,7 +316,7 @@ function testImportFromRemixd(browser: NightwatchBrowser, callback: VoidFunction
 }
 
 async function spawnRemixd(path: string): Promise<ChildProcess> {
-  const remixd = spawn('yarn run remixd', [`-s ${path} | tee ./reports/screenshots/errors.txt`], { cwd: process.cwd(), shell: true, detached: true })
+  const remixd = spawn('yarn run remixd', [`-s ${path} | tee ./reports/screenshots/errors.png`], { cwd: process.cwd(), shell: true, detached: true })
   return new Promise((resolve, reject) => {
     remixd.stdout.on('data', function (data) {
       if(
