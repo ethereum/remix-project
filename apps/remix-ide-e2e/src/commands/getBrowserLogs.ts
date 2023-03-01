9bf6e5dda22e47288565aa0e27ce640865a089a6
filip mertens <filip.mertens@ethereum.org>
2023-03-01 18:02:53 +0100

    update log

--- apps/remix-ide-e2e/src/commands/getBrowserLogs.ts
+++ apps/remix-ide-e2e/src/commands/getBrowserLogs.ts
@@ -3,16 +3,22 @@ import EventEmitter from 'events'
 
 class GetBrowserLogs extends EventEmitter {
     command(this: NightwatchBrowser): NightwatchBrowser {
-        this.api.getLog('browser', function (logs) {
-            if (logs && Array.isArray(logs)) {
-                logs.forEach(function (log) {
-                    console.log(log)
+        if (this.api.isChrome) {
+            this.api.getLog('browser', function (logs) {
+                if (logs && Array.isArray(logs)) {
+                    logs.forEach(function (log) {
+                        console.log(log)
+                    }
+                    )
                 }
-                )
             }
-        }).perform(() => {
+            ).perform(() => {
+                this.emit('complete')
+            })
+        } else {
+            console.log('Browser logs are only available in Chrome')
             this.emit('complete')
-        })
+        }
         return this
     }
 }
