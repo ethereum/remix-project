3f41d34cd1ce1935498a23632d6f79ea80b366fd
Joseph Izang <josephizang@gmail.com>
2023-03-03 17:55:01 +0100

    add required artifacts to have app work. remove redundancies

--- apps/remixdocgen/src/app/routes.tsx
+++ apps/remixdocgen/src/app/routes.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   RouteProps,
 } from "react-router-dom";
 
-import { ErrorView, HomeView } from "./views";
+
 
 interface Props extends RouteProps {
   component: any; // TODO: new (props: any) => React.Component
@@ -15,17 +14,11 @@ interface Props extends RouteProps {
 
 const CustomRoute = ({ component: Component, ...rest }: Props) => {
   return (
-    <Route {...rest} render={(matchProps) => <Component {...matchProps} />} />
+    <></>
   );
 };
 
 export const Routes = () => (
   <Router>
-    <Switch>
-      <CustomRoute exact path="/" component={HomeView} from="/" />
-      <Route path="/error">
-        <ErrorView />
-      </Route>
-    </Switch>
   </Router>
 );
--- apps/remixdocgen/src/app/views/index.ts
+++ apps/remixdocgen/src/app/views/index.ts
@@ -1,2 +1,1 @@
-export { HomeView } from "./HomeView";
-export { ErrorView } from "./ErrorView";
+
