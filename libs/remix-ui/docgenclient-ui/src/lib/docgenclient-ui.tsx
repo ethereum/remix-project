c6a1e4436dff68b0a25bd99cdd513ab2e22831db
Joseph Izang <josephizang@gmail.com>
2023-03-04 23:22:25 +0100

    test text

--- libs/remix-ui/docgenclient-ui/src/lib/docgenclient-ui.tsx
+++ libs/remix-ui/docgenclient-ui/src/lib/docgenclient-ui.tsx
@@ -5,6 +5,7 @@ export function RemixUiDocgenClient() {
   return (
     <div>
       <h1 className="h1">DocgenClientUI</h1>
+      <p className="p">This is a plugin that allows you to generate documentation for your smart contracts.</p>
     </div>
   )
 }
