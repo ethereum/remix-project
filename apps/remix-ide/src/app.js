414af5e8c986219e761b91597d1c50f9e4bbf195
Joseph Izang <josephizang@gmail.com>
2023-03-08 11:59:26 +0100

    fix issues

--- apps/remix-ide/src/app.js
+++ apps/remix-ide/src/app.js
@@ -413,7 +413,7 @@ class AppComponent {
     await this.appManager.activatePlugin(['settings', 'config'])
     await this.appManager.activatePlugin(['hiddenPanel', 'pluginManager', 'codeParser', 'codeFormatter', 'fileDecorator', 'terminal', 'blockchain', 'fetchAndCompile', 'contentImport', 'gistHandler'])
     await this.appManager.activatePlugin(['settings'])
-    await this.appManager.activatePlugin(['walkthrough', 'storage', 'search', 'compileAndRun', 'recorder', 'docgen'])
+    await this.appManager.activatePlugin(['walkthrough', 'storage', 'search', 'compileAndRun', 'recorder'])
 
     this.appManager.on(
       'filePanel',
