78525d3279c44d710b4b916e4458754fee8f534b
filip mertens <filip.mertens@ethereum.org>
2023-03-08 16:18:32 +0100

    refactor

--- apps/remixdocgen/src/app/docgen-client.ts
+++ apps/remixdocgen/src/app/docgen-client.ts
@@ -1,22 +1,72 @@
 import { PluginClient } from '@remixproject/plugin'
+import { CompilationResult, SourceWithTarget } from '@remixproject/plugin-api'
 import { createClient } from '@remixproject/plugin-webview'
-import { DocGen } from './plugins/docgen'
+import EventEmitter from 'events'
+import { Config, defaults } from './docgen/config'
+import { Build, buildSite } from './docgen/site'
+import { loadTemplates } from './docgen/templates'
+import {SolcInput, SolcOutput} from 'solidity-ast/solc'
+import { render } from './docgen/render'
 
 export class DocGenClient extends PluginClient {
-  private client
-  private docgen
+  private currentTheme
+  public eventEmitter: EventEmitter
+  private build: Build
   
   constructor() {
     super()
-    this.client = createClient(this)
+    this.eventEmitter = new EventEmitter()
+    createClient(this)
     // this.docgen = new DocGen()
     this.methods = ['generateDocs', 'publishDocs']
+    this.onload().then(async () => {
+      console.log('docgen client loaded')
+      await this.setListeners()
+    })
   }
-  loadClient() {
-    this.client.onload()
+
+  async setListeners() {
+    this.currentTheme = await this.call('theme', 'currentTheme');
+    this.on("theme", "themeChanged", (theme: any) => {
+      this.currentTheme = theme
+      this.eventEmitter.emit('themeChanged', this.currentTheme)
+    });
+    this.eventEmitter.emit('themeChanged', this.currentTheme)
+
+    this.on('solidity', 'compilationFinished', (fileName: string, source: SourceWithTarget, languageVersion: string, data: CompilationResult) => {
+      console.log('docgen client compilationFinished', data, source)
+      const input: SolcInput = {
+        sources: source.sources
+      }
+      const output: SolcOutput = {
+        sources: data.sources as any
+      }
+      this.build = {
+        input: input,
+        output: output
+      }
+
+      this.eventEmitter.emit('compilationFinished', this.build, fileName)
+
+    })
   }
+
+  async docgen(builds: Build[], userConfig?: Config): Promise<void> {
+    const config = { ...defaults, ...userConfig }
+    const templates = await loadTemplates(config.theme, config.root, config.templates)
+    const site = buildSite(builds, config, templates.properties ?? {})
+    const renderedSite = render(site, templates, config.collapseNewlines)
+    const docs: string[] = []
+    for (const { id, contents } of renderedSite) {
+      await this.call('fileManager', 'setFile', id, contents)
+      docs.push(id)
+    }
+    this.eventEmitter.emit('docsGenerated', docs)
+  }
+
   async generateDocs() {
     console.log('docgen client generateDocs')
+    this.docgen([this.build])
   }
 
   async publishDocs() {
