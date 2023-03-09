3ab83bf38b812b25fb48c7ca09c1f2dbf6f26cee
filip mertens <filip.mertens@ethereum.org>
2023-03-09 15:29:37 +0100

    readme

--- apps/remix-ide/ci/deploy_from_travis_remix-live.sh
+++ apps/remix-ide/ci/deploy_from_travis_remix-live.sh
@@ -8,6 +8,11 @@ cd dist/apps/remix-ide
 git init
 git config user.name "$COMMIT_AUTHOR"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+echo "# Automatic build" > README.md
+echo "Built website from \`$SHA\`. See https://github.com/ethereum/remix-ide/ for details." >> README.md
+echo "To use an offline copy, download \`remix-$SHA.zip\`." >> README.md
+
 zip -r remix-$SHA.zip .
 git add .
 git commit -m "Built website from {$SHA}."
