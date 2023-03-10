cb427843cd5db89527dc3a359c89482f6811c7fd
filip mertens <filip.mertens@ethereum.org>
2023-03-10 17:03:27 +0100

    addfile

--- apps/remix-ide-e2e/src/commands/addFile.ts
+++ apps/remix-ide-e2e/src/commands/addFile.ts
@@ -45,8 +45,8 @@ function addFile(browser: NightwatchBrowser, name: string, content: NightwatchCo
       } else {
         browser.click('[data-id="fileExplorerNewFilecreateNewFile"]')
           .waitForElementPresent({
-            selector: '//*[@data-id="/blank"]',
-            locateStrategy: 'xpath',
+            selector: '*[@data-id="/blank"] .remixui_items',
+            locateStrategy: 'css selector'
           })
           .sendKeys('*[data-id$="/blank"] .remixui_items', name)
           .sendKeys('*[data-id$="/blank"] .remixui_items', browser.Keys.ENTER)
