3792f470b56f50a3ce521ab3121c5ec70999b3de
filip mertens <filip.mertens@ethereum.org>
2023-03-10 15:38:55 +0100

    see opts

--- apps/remix-ide-e2e/src/commands/logTime.ts
+++ apps/remix-ide-e2e/src/commands/logTime.ts
@@ -4,7 +4,7 @@ import EventEmitter from 'events'
 class LogTime extends EventEmitter {
   command (this: NightwatchBrowser, msg?: string): NightwatchBrowser {
     this.api.perform((done) => {
-      console.log('time: ',msg, new Date().toLocaleTimeString())
+      console.log('time: ',msg || '', new Date().toLocaleTimeString())
       done()
       this.emit('complete')
     })
--- apps/remix-ide-e2e/src/commands/verifyContracts.ts
+++ apps/remix-ide-e2e/src/commands/verifyContracts.ts
@@ -2,7 +2,7 @@ import { NightwatchBrowser } from 'nightwatch'
 import EventEmitter from 'events'
 
 class VerifyContracts extends EventEmitter {
-  command (this: NightwatchBrowser, compiledContractNames: string[], opts = { wait: 1000, version: null, runs: '200' }): NightwatchBrowser {
+  command(this: NightwatchBrowser, compiledContractNames: string[], opts = { wait: 1000, version: null, runs: '200' }): NightwatchBrowser {
     this.api.perform((done) => {
       verifyContracts(this.api, compiledContractNames, opts, () => {
         done()
@@ -13,21 +13,21 @@ class VerifyContracts extends EventEmitter {
   }
 }
 
-function verifyContracts (browser: NightwatchBrowser, compiledContractNames: string[], opts: { wait: number, version?: string, runs?: string }, callback: VoidFunction) {
+function verifyContracts(browser: NightwatchBrowser, compiledContractNames: string[], opts: { wait: number, version?: string, runs?: string }, callback: VoidFunction) {
   browser
+    .perform(() => {
+      console.log(opts)
+    })
     .clickLaunchIcon('solidity')
     .pause(opts.wait)
-    .pause(5000)
     .waitForElementPresent('*[data-id="compiledContracts"] option', 60000)
     .perform(async (done) => {
       if (opts.version) {
         browser
           .click('*[data-id="compilation-details"]')
           .waitForElementVisible('*[data-id="remixui_treeviewitem_metadata"]')
-          .pause(2000)
           .click('*[data-id="remixui_treeviewitem_metadata"]')
           .waitForElementVisible('*[data-id="treeViewDivtreeViewItemcompiler"]')
-          .pause(2000)
           .click('*[data-id="treeViewDivtreeViewItemcompiler"]')
           .waitForElementVisible('*[data-id="treeViewLiversion"]')
           .assert.containsText('*[data-id="treeViewLiversion"]', `${opts.version}`)
@@ -38,25 +38,23 @@ function verifyContracts (browser: NightwatchBrowser, compiledContractNames: str
           })
       } if (opts.runs) {
         browser
-        .click('*[data-id="compilation-details"]')
-        .waitForElementVisible('*[data-id="remixui_treeviewitem_metadata"]')
-        .pause(2000)
-        .click('*[data-id="remixui_treeviewitem_metadata"]')
-        .waitForElementVisible('*[data-id="treeViewDivtreeViewItemsettings"]')
-        .pause(2000)
-        .click('*[data-id="treeViewDivtreeViewItemsettings"]')
-        .waitForElementVisible('*[data-id="treeViewDivtreeViewItemoptimizer"]')
-        .click('*[data-id="treeViewDivtreeViewItemoptimizer"]')
-        .waitForElementVisible('*[data-id="treeViewDivruns"]')
-        .assert.containsText('*[data-id="treeViewDivruns"]', `${opts.runs}`)
-        .click('[data-id="workspacesModalDialog-modal-footer-ok-react"]')
-        .perform(() => {
-          done()
-          callback()
-        })
+          .click('*[data-id="compilation-details"]')
+          .waitForElementVisible('*[data-id="remixui_treeviewitem_metadata"]')
+          .click('*[data-id="remixui_treeviewitem_metadata"]')
+          .waitForElementVisible('*[data-id="treeViewDivtreeViewItemsettings"]')
+          .click('*[data-id="treeViewDivtreeViewItemsettings"]')
+          .waitForElementVisible('*[data-id="treeViewDivtreeViewItemoptimizer"]')
+          .click('*[data-id="treeViewDivtreeViewItemoptimizer"]')
+          .waitForElementVisible('*[data-id="treeViewDivruns"]')
+          .assert.textContains('*[data-id="treeViewDivruns"]', `${opts.runs}`)
+          .click('[data-id="workspacesModalDialog-modal-footer-ok-react"]')
+          .perform(() => {
+            done()
+            callback()
+          })
       } else {
         for (const index in compiledContractNames) {
-         await browser.waitForElementContainsText('[data-id="compiledContracts"]', compiledContractNames[index], 60000)
+          await browser.waitForElementContainsText('[data-id="compiledContracts"]', compiledContractNames[index], 60000)
         }
         done()
         callback()
