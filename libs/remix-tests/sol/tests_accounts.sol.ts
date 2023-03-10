12f36183d7f07dd734a034b046300425ee8e8bee
yann300 <yann.levreau@gmail.com>
2023-03-10 23:51:11 +0100

    update the accounts lib each time there's a test run

--- libs/remix-tests/sol/tests_accounts.sol.ts
+++ libs/remix-tests/sol/tests_accounts.sol.ts
@@ -5,7 +5,6 @@ pragma solidity >=0.4.22 <0.9.0;
 library TestsAccounts {
     function getAccount(uint index) pure public returns (address) {
         >accounts<
-        return accounts[index];
     }
 }
 `
