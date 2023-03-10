37ee6abf1584e41158d57726860f028adba949ec
filip mertens <filip.mertens@ethereum.org>
2023-03-10 15:20:45 +0100

    log time

--- apps/remix-ide-e2e/src/commands/logTime.ts
+++ apps/remix-ide-e2e/src/commands/logTime.ts
@@ -3,7 +3,7 @@ import EventEmitter from 'events'
 
 class LogTime extends EventEmitter {
   command (this: NightwatchBrowser, msg?: string): NightwatchBrowser {
-    this.perform((done) => {
+    this.api.perform((done) => {
       console.log('time: ',msg, new Date().toLocaleTimeString())
       done()
       this.emit('complete')
