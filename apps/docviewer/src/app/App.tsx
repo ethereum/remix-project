999eaa1f72340b433f048b8e3e8ae7073abb283f
Joseph Izang <josephizang@gmail.com>
2023-03-13 23:30:02 +0100

    add docviewer plugin to show generated documentation

---
+++ apps/docviewer/src/app/App.tsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <>
+      <h1>Documentation Viewer</h1>
+      <p>
+        Show documentation of compiled contracts.
+      </p>
+    </>
+  )
+}
