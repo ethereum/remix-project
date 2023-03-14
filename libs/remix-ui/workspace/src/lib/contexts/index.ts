2417529a742226c1a194860351ad2c6fa3e80f52
Aniket-Engg <aniketengg.225@gmail.com>
2023-03-14 20:14:27 +0530

    download workspace files and folders

--- libs/remix-ui/workspace/src/lib/contexts/index.ts
+++ libs/remix-ui/workspace/src/lib/contexts/index.ts
@@ -33,6 +33,7 @@ export const FileSystemContext = createContext<{
   dispatchHandleClickFile: (path: string, type: 'file' | 'folder' | 'gist') => Promise<void>
   dispatchHandleExpandPath: (paths: string[]) => Promise<void>,
   dispatchHandleDownloadFiles: () => Promise<void>,
+  dispatchHandleDownloadWorkspace: () => Promise<void>,
   dispatchHandleRestoreBackup: () => Promise<void>
   dispatchCloneRepository: (url: string) => Promise<void>,
   dispatchMoveFile: (src: string, dest: string) => Promise<void>,
