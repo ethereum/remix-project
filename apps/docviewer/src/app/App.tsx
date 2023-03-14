5cf3fa980cb0a370971cbbd392e8a193062d717b
Joseph Izang <josephizang@gmail.com>
2023-03-14 02:13:37 +0100

    create new plugin for doc viewer. cleanup

--- apps/docviewer/src/app/App.tsx
+++ apps/docviewer/src/app/App.tsx
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from "react"
+import { DocViewer } from "./docviewer"
+
+const client = new DocViewer()
 
 export default function App() {
+  const [contents, setContents] = useState('')
+  useEffect(() => {
+    client.eventEmitter.on('contentsReady', (fileContents: string) => {
+      console.log('contentsReady', fileContents)
+      setContents(fileContents)
+    })
+
+  }, [])
   return (
     <>
       <h1>Documentation Viewer</h1>
       <p>
         Show documentation of compiled contracts.
+      </p>
+      <p>
+        {contents && contents.length}
       </p>
     </>
   )
