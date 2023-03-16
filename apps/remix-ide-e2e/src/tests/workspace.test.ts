9c4634d0fd990aa244f702c88f2a0658b22451a8
Aniket-Engg <aniketengg.225@gmail.com>
2023-03-16 19:05:18 +0530

    rename ws e2e

--- apps/remix-ide-e2e/src/tests/workspace.test.ts
+++ apps/remix-ide-e2e/src/tests/workspace.test.ts
@@ -413,9 +413,9 @@ module.exports = {
         selector: '//i[@data-id="workspaceDropdownMenuIcon"]',
         locateStrategy: 'xpath',
       })
-      .click('//*[@id="workspacesMenuDropdown"]/span/i')
-      .waitForElementVisible('//*[@id="workspacesMenuDropdown"]/div/ul')
-      .click('//*[@id="workspacesMenuDropdown"]/div/ul/a[3]') // rename workspace_name
+      .click('*[data-id="workspacesMenuDropdown"]')
+      .waitForElementVisible('*[data-id="wsdropdownMenu"]')
+      .click('*[data-id="workspacerename"]') // rename workspace_name
       .useCss()
       .waitForElementVisible('*[data-id="treeViewLitreeViewItemtests"]')
       .waitForElementVisible('*[data-id="modalDialogCustomPromptTextRename"]')
