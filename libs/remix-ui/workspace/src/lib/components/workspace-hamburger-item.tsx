0ba8d8c94c07073d2fa95e353e4aa1ab1a4adf2a
lianahus <liana@ethereum.org>
2023-03-16 14:53:41 +0100

    corrected the position of tooltips

--- libs/remix-ui/workspace/src/lib/components/workspace-hamburger-item.tsx
+++ libs/remix-ui/workspace/src/lib/components/workspace-hamburger-item.tsx
@@ -19,7 +19,7 @@ export function HamburgerMenuItem (props: HamburgerMenuItemProps) {
         <>
           <Dropdown.Item>
             <CustomTooltip
-              placement="right-start"
+              placement="right"
               tooltipId={uid + "Tooltip"}
               tooltipClasses="text-nowrap"
               tooltipText={<FormattedMessage id={'filePanel.workspace.' + props.kind} />}
