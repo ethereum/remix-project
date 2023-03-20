47d49bf8f76f9da929c86d78ad9877c4576ec745
filip mertens <filip.mertens@ethereum.org>
2023-03-20 13:20:42 +0100

    var

--- apps/remix-ide/ci/publish_plugin.sh
+++ apps/remix-ide/ci/publish_plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-HASH=sha1sum persist/plugin-$1.zip
+HASH=$(sha1sum persist/plugin-$1.zip)
 npx ts-node apps/remix-ide/ci/publish_plugin.ts $1 $HASH alpha
 
 git clone git@github.com:ethereum/remix-plugins-directory.git
