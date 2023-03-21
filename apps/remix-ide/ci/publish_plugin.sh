384f2f5ea636826a69581de83103ddb46ba0a429
filip mertens <filip.mertens@ethereum.org>
2023-03-21 14:19:53 +0100

    env

--- apps/remix-ide/ci/publish_plugin.sh
+++ apps/remix-ide/ci/publish_plugin.sh
@@ -11,6 +11,7 @@ git clone git@github.com:ethereum/remix-plugins-directory.git
 npx ts-node apps/remix-ide/ci/publish_plugin.ts $1 $HASH alpha
 
 cd remix-plugins-directory
+git checkout -b plugin-$1-$HASH
 git config user.name "$COMMIT_AUTHOR"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 git status
