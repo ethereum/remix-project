f9137f928e412bb9cabe5dda52c651b4dfca3145
filip mertens <filip.mertens@ethereum.org>
2023-03-21 13:54:31 +0100

    read the file

--- apps/remix-ide/ci/publish_plugin.ts
+++ apps/remix-ide/ci/publish_plugin.ts
@@ -90,6 +90,8 @@ import * as child_process from 'child_process'
   // write the profile.json file
   fs.writeFileSync(plugin_directory_path + pluginName + '/profile.json', JSON.stringify(plugin, null, 2))
 
+  // read the profile.json file
+  console.log(fs.readFileSync(plugin_directory_path + pluginName + '/profile.json'))
 
   // create pull request
   const promisifyExec = util.promisify(child_process.exec)
