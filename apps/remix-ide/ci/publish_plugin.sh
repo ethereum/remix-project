f72ec3646956a007da207b67844f400477ede23b
filip mertens <filip.mertens@ethereum.org>
2023-03-21 14:24:10 +0100

    try ts

--- apps/remix-ide/ci/publish_plugin.sh
+++ apps/remix-ide/ci/publish_plugin.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git config --global user.name "$COMMIT_AUTHOR"
+git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
 HASH=$(sha1sum persist/plugin-$1.zip | cut -d " " -f 1)
 # unzip persist/plugin-$1.zip
@@ -10,12 +12,4 @@ HASH=$(sha1sum persist/plugin-$1.zip | cut -d " " -f 1)
 git clone git@github.com:ethereum/remix-plugins-directory.git
 npx ts-node apps/remix-ide/ci/publish_plugin.ts $1 $HASH alpha
 
-cd remix-plugins-directory
-git checkout -b plugin-$1-$HASH
-git config user.name "$COMMIT_AUTHOR"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-git status
-git add .
-git commit -m "Publish plugin $1"
-git status
 
