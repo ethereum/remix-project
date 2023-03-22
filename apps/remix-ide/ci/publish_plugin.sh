7f0660479fe61eafb4ddfd2e40fd22f2f5bac0ad
filip mertens <filip.mertens@ethereum.org>
2023-03-22 12:33:45 +0100

    gh

--- apps/remix-ide/ci/publish_plugin.sh
+++ apps/remix-ide/ci/publish_plugin.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt update
-
 sudo apt install gh
 
 git config --global user.name "$COMMIT_AUTHOR"
