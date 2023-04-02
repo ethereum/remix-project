285cb5f17ba6c4d22be03e88c794d020fa2c80a0
Joseph Izang <josephizang@gmail.com>
2023-04-02 02:02:20 +0100

    fix docgen bug

--- apps/doc-gen/src/app/docgen/utils/normalizeContractPath.ts
+++ apps/doc-gen/src/app/docgen/utils/normalizeContractPath.ts
@@ -1,6 +1,7 @@
-export function normalizeContractPath(contractPath: string): string {
-  let paths = contractPath.split('/')
-  let filename = paths[paths.length - 1].split('.')[0]
+
+export function normalizeContractPath(contractPath: string, returnPath?: boolean): string | string[]{
+  const paths = contractPath.split('/')
+  const filename = paths[paths.length - 1].split('.')[0]
   let folders = ''
   for (let i = 0; i < paths.length - 1; i++) {
     if(i !== paths.length -1) {
@@ -8,9 +9,9 @@ export function normalizeContractPath(contractPath: string): string {
     }
   }
   const resultingPath = `${folders}${filename}`
-  // cleanup variables
-  paths = null
-  filename = null
-  folders = null
+  if(returnPath) {
+  return [folders,resultingPath]
+  } else {
   return resultingPath
+  }
 }
