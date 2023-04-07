52c3b684e84773175b8a432eb46f8659c3e7f1a4
ioedeveloper <daviddisu8@gmail.com>
2023-04-07 15:22:59 +0100

    Implement sendAsync api with newly discovered functions from wagmi codebas

--- apps/wallet-connect/src/services/RemixClient.ts
+++ apps/wallet-connect/src/services/RemixClient.ts
@@ -1,9 +1,13 @@
 import { PluginClient } from '@remixproject/plugin'
 import { createClient } from '@remixproject/plugin-webview'
-import EthereumProvider from '@walletconnect/ethereum-provider'
+import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createClient as wagmiCreateClient } from 'wagmi'
+import { arbitrum, mainnet, polygon, optimism, Chain, goerli, sepolia } from 'wagmi/chains'
+import { PROJECT_ID } from './constant'
 
 export class RemixClient extends PluginClient {
-    #walletConnectClient: EthereumProvider
+    wagmiClient
+    chains: Chain[]
 
     constructor() {
         super()
@@ -12,26 +16,43 @@ export class RemixClient extends PluginClient {
         this.onload()
     }
 
-    init () {
-        console.log('initialzing...')
+    async init() {
+        try {
+            this.chains = [arbitrum, mainnet, polygon, optimism, goerli, sepolia]
+          
+            const { provider } = configureChains(this.chains, [w3mProvider({ projectId: PROJECT_ID })])
+            this.wagmiClient = wagmiCreateClient({
+              autoConnect: false,
+              connectors: w3mConnectors({ projectId: PROJECT_ID, version: 1, chains: this.chains }),
+              provider
+            })
+        } catch (e) {
+            return console.error("Could not get a wallet connection", e)
+        }
     }
 
-    set walletConnectClient (value: EthereumProvider) {
-        this.#walletConnectClient = value
-    }
-
-    sendAsync = (data: { id: string, method: string, params: any[] }) => {
+    sendAsync = (data: { method: string, params: string, id: string }) => {
         return new Promise((resolve, reject) => {
-            if (this.#walletConnectClient) {
-                if (data.method === 'eth_chainId') data.method = 'eth_chainId'
-                this.#walletConnectClient.sendAsync(data, (error, message) => {
-                    console.log('method: ', data.method)
-                    if (error) return reject(error)
-                    console.log('message: ', message)
-                    resolve({"jsonrpc": "2.0", "result": message, "id": data.id})
-                })
+            if (this.wagmiClient) {
+                if (this.wagmiClient.data && this.wagmiClient.data.provider && this.wagmiClient.data.provider.sendAsync) {
+                    this.wagmiClient.data.provider.sendAsync(data, (error, message) => {
+                        if (error) return reject(error)
+                        resolve(message)
+                    })
+                } else if (this.wagmiClient.data && this.wagmiClient.data.provider && this.wagmiClient.data.provider.jsonRpcFetchFunc) {
+                    this.wagmiClient.data.provider.jsonRpcFetchFunc(data.method, data.params).then((message) => {
+                        resolve({"jsonrpc": "2.0", "result": message, "id": data.id})
+                    }).catch((error) => {
+                        reject(error)
+                    })
+                } else {
+                    this.wagmiClient.provider.send(data.method, data.params).then((message) => {
+                        resolve({"jsonrpc": "2.0", "result": message, "id": data.id})
+                    }).catch((error) => {
+                        reject(error)
+                    })
+                }
             } else {
-                console.error('Remix Client is not connected to WalletConnect Client.')
                 resolve({"jsonrpc": "2.0", "result": [], "id": data.id})
             }
         })
