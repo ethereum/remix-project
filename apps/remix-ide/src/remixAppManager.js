6767d21d89e841bef109f1dddc75908b831d31a5
ioedeveloper <daviddisu8@gmail.com>
2023-04-13 14:21:48 +0100

    Rename plugin name

--- apps/remix-ide/src/remixAppManager.js
+++ apps/remix-ide/src/remixAppManager.js
@@ -17,7 +17,7 @@ const requiredModules = [ // services + layout views + system views
 // dependentModules shouldn't be manually activated (e.g hardhat is activated by remixd)
 const dependentModules = ['foundry', 'hardhat', 'truffle', 'slither']
 
-const loadLocalPlugins = ["doc-gen", "doc-viewer", "etherscan", "vyper", "wallet-connect"]
+const loadLocalPlugins = ["doc-gen", "doc-viewer", "etherscan", "vyper", "walletconnect"]
 
 const sensitiveCalls = {
   'fileManager': ['writeFile', 'copyFile', 'rename', 'copyDir'],
