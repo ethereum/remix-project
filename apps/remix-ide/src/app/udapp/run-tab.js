977dcd88121e908d6c8fe76ae8131a4b499ded34
yann300 <yann.levreau@gmail.com>
2023-04-18 14:30:29 +0200

    add shanghai vm

--- apps/remix-ide/src/app/udapp/run-tab.js
+++ apps/remix-ide/src/app/udapp/run-tab.js
@@ -151,6 +151,7 @@ export class RunTab extends ViewPlugin {
     
     // VM
     const titleVM = 'Execution environment is local to Remix.  Data is only saved to browser memory and will vanish upon reload.'
+    await addProvider('vm-shanghai', 'Remix VM (Shanghai)', false, true, 'shanghai', 'settingsVMShanghaiMode', titleVM)
     await addProvider('vm-merge', 'Remix VM (Merge)', false, true, 'merge', 'settingsVMMergeMode', titleVM)
     await addProvider('vm-london', 'Remix VM (London)', false, true, 'london', 'settingsVMLondonMode', titleVM)
     await addProvider('vm-berlin', 'Remix VM (Berlin)', false, true, 'berlin', 'settingsVMBerlinMode', titleVM)
