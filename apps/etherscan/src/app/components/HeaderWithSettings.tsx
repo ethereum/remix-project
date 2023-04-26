7727a3c1a0f88be73137d28cb5a0cf3835bd900c
Aniket-Engg <aniketengg.225@gmail.com>
2023-04-26 12:33:58 +0530

    font awesome icons for navigation

--- apps/etherscan/src/app/components/HeaderWithSettings.tsx
+++ apps/etherscan/src/app/components/HeaderWithSettings.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { NavLink } from "react-router-dom"
+import { CustomTooltip } from '@remix-ui/helper';
 import { AppContext } from "../AppContext"
 import { ThemeType } from "../types"
 
@@ -17,37 +18,14 @@ interface IconProps {
 
 const HomeIcon: React.FC<IconProps> = ({ from, themeType }: IconProps) => {
   return (
-    <NavLink
-      data-id="home"
-      data-toggle="tooltip"
-      data-placement="top"
-      title="Home"
-      to={{
-        pathname: "/"
-      }}
-      state={ from }
-      style={isActive => {
-        return {
-          ...(isActive ? getStyleFilterIcon(themeType) : {}), ...{ marginRight: "0.4em" }
-        }
-      }}
-    >
-      <svg
-        style={{ filter: "invert(0.5)" }}
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        className="bi bi-house-door-fill"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
+      <NavLink
+        data-id="home"
+        to="/"
+        state={ from }
+        style={{ marginRight: "0.4em" }}
       >
-        <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z" />
-        <path
-          fillRule="evenodd"
-          d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
-        />
-      </svg>
-    </NavLink>
+        <i className="fas fa-home"></i>
+      </NavLink>
   )
 }
 
