3b5dd850a3de8a03e99a9157f0ecef9fb2bcb155
filip mertens <filip.mertens@ethereum.org>
2023-05-01 15:38:41 +0200

    install jq

--- apps/remix-ide/ci/downloadsoljson.sh
+++ apps/remix-ide/ci/downloadsoljson.sh
@@ -8,13 +8,26 @@ then
     exit
 fi
 
+# install jq if not installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    echo "installing jq"
+    sudo apt-get install jq
+fi
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    exit
+fi
+
 # download https://binaries.soliditylang.org/wasm/list.json as json
 curl https://binaries.soliditylang.org/wasm/list.json > list.json
 # get the latest version
 version=$(cat list.json | jq -r '.latestRelease')
 echo $version
 # the value of releases with key $version
-url=$(cat list.json | npx jq -r '.releases[]' | grep "$version+")
+url=$(cat list.json | jq -r '.releases[]' | grep "$version+")
 echo $url
 # set path to the url
 path="https://binaries.soliditylang.org/bin/$url"
