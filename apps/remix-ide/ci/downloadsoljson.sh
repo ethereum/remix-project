863d69ab74030888eeeb4ad32d237222a52a10b0
filip mertens <filip.mertens@ethereum.org>
2023-05-02 13:44:05 +0200

    optimize

--- apps/remix-ide/ci/downloadsoljson.sh
+++ apps/remix-ide/ci/downloadsoljson.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Downloading latest soljson.js from https://binaries.soliditylang.org/wasm/list.json"
 set -e
 # check if curl is installed
 if ! command -v curl &> /dev/null
@@ -10,7 +11,7 @@ fi
 
 
 # download https://binaries.soliditylang.org/wasm/list.json as json
-curl https://binaries.soliditylang.org/wasm/list.json > list.json
+curl -s https://binaries.soliditylang.org/wasm/list.json > list.json
 # get the latest version without jq
 latest=$(grep 'latestRelease' list.json | cut -d '"' -f 4)
 echo "latest version: $latest"
@@ -20,7 +21,7 @@ echo "url: $url"
 path="https://binaries.soliditylang.org/bin/$url"
 echo "path: $path"
 # download the file to ./apps/remix-ide/src/assets/js/soljson.js
-curl $path > ./apps/remix-ide/src/assets/js/soljson.js
+curl -s $path > ./apps/remix-ide/src/assets/js/soljson.js
 # if directory ./apps/remix-ide/src/assets/js/soljson does not exist, create it
 if [ ! -d "./apps/remix-ide/src/assets/js/soljson" ]; then
     mkdir ./apps/remix-ide/src/assets/js/soljson
