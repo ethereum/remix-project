51176a3b1de1c8e0eebf5cb073fadf32d21ccd8e
Joseph Izang <josephizang@gmail.com>
2023-05-02 18:29:59 +0100

    add sol2uml test skeleton

---
+++ apps/remix-ide-e2e/src/tests/sol2uml.test.ts
@@ -0,0 +1,9 @@
+'use strict'
+import { NightwatchBrowser } from 'nightwatch'
+import init from '../helpers/init'
+
+module.exports = {
+  before: function (browser: NightwatchBrowser, done: VoidFunction) { 
+    init(browser, done)
+   }
+}
