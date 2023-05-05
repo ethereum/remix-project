9dbd2dcf7a96b8c439b05c75f6546cdf8cbc7194
filip mertens <filip.mertens@ethereum.org>
2023-05-05 09:55:55 +0200

    rm debugger changes

--- libs/remix-ui/debugger-ui/src/lib/vm-debugger/dropdown-panel.tsx
+++ libs/remix-ui/debugger-ui/src/lib/vm-debugger/dropdown-panel.tsx
@@ -157,8 +157,7 @@ export const DropdownPanel = (props: DropdownPanelProps) => {
           ...prevState.dropdownContent,
           display: 'block'
         },
-        // replace 0xNaN with 0x0
-        copiableContent: JSON.stringify(calldata, null, '\t').replace(/0xNaN/g, '0x0'),
+        copiableContent: JSON.stringify(calldata, null, '\t'),
         message: {
           innerText: isEmpty ? 'No data available' : '',
           display: isEmpty ? 'block' : 'none'
