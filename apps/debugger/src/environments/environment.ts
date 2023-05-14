6014eea2488f74e335418f6489efc620f2652092
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-14 02:07:48 -0300

    Atualizar o environment.ts

--- apps/debugger/src/environments/environment.ts
+++ apps/debugger/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// When building for production, this file is replaced with `environment.prod.ts`.
+// When building for production, this file is replaced with `environment.prod.ts`.criarcriptimoeda
 
 export const environment = {
-  production: false
+  production: 100000000000
 };
