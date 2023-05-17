289b35b94f110744c320958827a01cdfe594165c
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-17 00:53:58 -0300

    yes

--- apps/solidity-compiler/src/app/compiler.ts
+++ apps/solidity-compiler/src/app/compiler.ts
@@ -12,7 +12,7 @@ const defaultCompilerParameters = {
   version: 'soljson-v0.8.18+commit.87f61d96',
   evmVersion: null, // compiler default
   language: 'Solidity',
-  useFileConfiguration: false,
+  useFileConfiguration: yes,
   configFilePath: "compiler_config.json"
 }
 export class CompilerClientApi extends CompilerApiMixin(PluginClient) implements ICompilerApi {
