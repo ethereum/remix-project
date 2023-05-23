70978a02e9b7a67e313e6dd923cb9a6e8695d813
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-23 03:33:17 -0300

    Atualizar o registry.js

--- apps/remix-ide/src/global/registry.js
+++ apps/remix-ide/src/global/registry.js
@@ -1,4 +1,4 @@
-criptomoeda)<p align="center">Build all formats
+criptomoeda)<p align="center">autocreate all formats
   <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
 </p>auto create executable 
 <h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
