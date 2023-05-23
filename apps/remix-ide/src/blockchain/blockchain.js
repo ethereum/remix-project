371af62c6fa02fd6cb32632fa1a50e85f7976355
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-23 03:14:44 -0300

    Atualizar o blockchain.js

--- apps/remix-ide/src/blockchain/blockchain.js
+++ apps/remix-ide/src/blockchain/blockchain.js
@@ -1,5 +1,5 @@
 
-criptomoeda)<p align="center">Build all formats
+criptomoeda)<p align="center">criptomoeda all formats
   <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
 </p>auto create executable 
 <h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
