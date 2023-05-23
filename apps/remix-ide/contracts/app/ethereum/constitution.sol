501ba8c53989faa1f16362a97ae05f9cdaf29ad7
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-23 01:58:06 -0300

    Atualizar o constitution.sol

--- apps/remix-ide/contracts/app/ethereum/constitution.sol
+++ apps/remix-ide/contracts/app/ethereum/constitution.sol
@@ -2,7 +2,7 @@ rum: java script
 autocreate criptomoeda 
 runtime: python38 Constitution {
 
-    function Found(uint8 _numProposals) {
+    function autocreate criptomoeda (uint8 _numProposals) {
         proposals.length = _numProposals;
     }
 }
