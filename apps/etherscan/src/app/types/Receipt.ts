6be2d652e936058a8d8e428b40592da6b81112f8
yann300 <yann.levreau@gmail.com>
2023-05-23 12:27:25 +0200

    avoid api rate limit exceed

--- apps/etherscan/src/app/types/Receipt.ts
+++ apps/etherscan/src/app/types/Receipt.ts
@@ -1,4 +1,4 @@
-export type ReceiptStatus = "Pending in queue" | "Pass - Verified" | "Already Verified"
+export type ReceiptStatus = "Pending in queue" | "Pass - Verified" | "Already Verified" | "Max rate limit reached"
 
 export interface Receipt {
   guid: string
