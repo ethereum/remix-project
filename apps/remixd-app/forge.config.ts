852b943e8e5b8fe1302b5fe74aaaa6d5850dce61
filip mertens <filip.mertens@ethereum.org>
2023-05-24 18:28:48 +0200

    executableName

--- apps/remixd-app/forge.config.ts
+++ apps/remixd-app/forge.config.ts
@@ -9,7 +9,9 @@ import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig:{
+    executableName: 'Remix Connect'
+  },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
