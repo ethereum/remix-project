be4762f79a132e37c12acb241414767d87a45373
filip mertens <filip.mertens@ethereum.org>
2023-05-26 10:58:04 +0200

    edge version

--- apps/remixd-app/selenium.config.js
+++ apps/remixd-app/selenium.config.js
@@ -5,6 +5,11 @@ module.exports = {
       version: '112.0.5615.28',
       arch: process.arch,
       baseURL: 'https://chromedriver.storage.googleapis.com'
+    },
+    chromiumedge: {
+      version: '112.0.1722.39',
+      arch: process.arch,
+      baseURL: 'https://msedgedriver.azureedge.net/'
     }
   }
 }
