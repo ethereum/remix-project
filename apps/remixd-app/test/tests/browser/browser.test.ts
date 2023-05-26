8073bc09bb2c6512c488293d5fb5f320f4a4c57b
filip mertens <filip.mertens@ethereum.org>
2023-05-26 13:22:07 +0200

    restore timing

--- apps/remixd-app/test/tests/browser/browser.test.ts
+++ apps/remixd-app/test/tests/browser/browser.test.ts
@@ -23,8 +23,9 @@ module.exports = {
       .clickLaunchIcon('pluginManager')
       .scrollAndClick('#pluginManager *[data-id="pluginManagerComponentActivateButtonremixd"]')
       .waitForElementVisible('*[data-id="remixdConnect-modal-footer-ok-react"]', 2000)
-      .pause(1000)
+      .pause(2000)
       .click('*[data-id="remixdConnect-modal-footer-ok-react"]')
+      .pause(5000)
       .clickLaunchIcon('filePanel')
       .end()
   },
