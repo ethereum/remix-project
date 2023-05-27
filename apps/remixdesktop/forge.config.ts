94c19ac6d44d7d633e053c14d1f1ba9aef4ee8e0
filip mertens <filip.mertens@ethereum.org>
2023-05-27 14:56:07 +0200

    prepackage

--- apps/remixdesktop/forge.config.ts
+++ apps/remixdesktop/forge.config.ts
@@ -7,6 +7,7 @@ import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
+import copy from 'recursive-copy';
 import fs from 'fs';
 
 const config: ForgeConfig = {
@@ -23,8 +24,18 @@ const config: ForgeConfig = {
     postStart: async (appProcess: any) => {
       console.log('We should start the app here');
       // list files in .webpack/renderer
-      const files = fs.readdirSync('./.webpack/renderer/main_window');
-      console.log(files);
+      //const files = fs.readdirSync('./.webpack/renderer/main_window');
+      //console.log(files);
+    },
+    prePackage: async (forgeConfig, options) => {
+      console.log('We should package the app here', options, forgeConfig);
+      // remove all files in .webpack/renderer
+      fs.rmdirSync('./.webpack/renderer/main_window', { recursive: true });
+
+      await copy('/Volumes/bunsen/code/rmproject2/remix-project/dist/apps/remix-ide', './.webpack/renderer/main_window');
+    },
+    postPackage: async (forgeConfig, options) => {
+      console.info('Packages built at:', options.outputPaths);
     }
   },
   plugins: [
@@ -36,7 +47,9 @@ const config: ForgeConfig = {
           return middlewares;
         }
       },
-      renderer: {
+      devContentSecurityPolicy: // allow all content for now
+      "default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';"
+      ,renderer: {
         config: rendererConfig,
         entryPoints: [
           {
