b4c9657e9b966ee6a1fffb02c0cb6bfcd84e5e06
bunsenstraat <filip.mertens@ethereum.org>
2023-05-28 10:56:08 +0200

    new app

--- apps/remixdesktop/webpack.config.js
+++ apps/remixdesktop/webpack.config.js
@@ -1,49 +1,10 @@
 const { composePlugins, withNx } = require('@nrwl/webpack')
-const { withReact } = require('@nrwl/react')
 const webpack = require('webpack')
-const CopyPlugin = require("copy-webpack-plugin")
-const version = require('../../package.json').version
-const fs = require('fs')
 const TerserPlugin = require("terser-webpack-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
-const axios = require('axios')
-
-const versionData = {
-  version: version,
-  timestamp: Date.now(),
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-}
-
-const loadLocalSolJson = async () => {
-  // execute apps/remix-ide/ci/downloadsoljson.sh
-  //const child = require('child_process').execSync('bash ' + __dirname + '/ci/downloadsoljson.sh', { encoding: 'utf8', cwd: process.cwd(), shell: true })
-  // show output
-  //console.log(child)
-}
-
-fs.writeFileSync(__dirname + '/src/assets/version.json', JSON.stringify(versionData))
-
-loadLocalSolJson()
-
-const project = fs.readFileSync(__dirname + '/project.json', 'utf8')
-
-const implicitDependencies = JSON.parse(project).implicitDependencies
-
-const copyPatterns = implicitDependencies.map((dep) => {
-  try {
-    fs.statSync(__dirname + `/../../dist/apps/${dep}`).isDirectory()
-    return { from: __dirname + `/../../dist/apps/${dep}`, to: `plugins/${dep}` }
-  }
-  catch (e) {
-    console.log('error', e)
-    return false
-  }
-})
-
-console.log('Copying plugins... ', copyPatterns)
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), withReact(), (config) => {
+module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
 
@@ -68,7 +29,7 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
     "buffer": require.resolve("buffer/"),
     "vm": require.resolve('vm-browserify'),
   }
-
+  
 
   // add externals
   config.externals = {
@@ -79,18 +40,10 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
   // add public path
   config.output.publicPath = '/'
 
-  // set filename
-  config.output.filename = `[name].${versionData.version}.${versionData.timestamp}.js`
-  config.output.chunkFilename = `[name].${versionData.version}.${versionData.timestamp}.js`
+
 
   // add copy & provide plugin
   config.plugins.push(
-    new CopyPlugin({
-      patterns: [
-        { from: '../../node_modules/monaco-editor/min/vs', to: 'assets/js/monaco-editor/min/vs' },
-        ...copyPatterns
-      ].filter(Boolean)
-    }),
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
       url: ['url', 'URL'],
@@ -105,7 +58,8 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
     enforce: "pre"
   })
 
-  config.ignoreWarnings = [/Failed to parse source map/, /require function/] // ignore source-map-loader warnings & AST warnings
+  config.ignoreWarnings = [/Failed to parse source map/] // ignore source-map-loader warnings
+
 
   // set minimizer
   config.optimization.minimizer = [
@@ -128,8 +82,5 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
     ignored: /node_modules/
   }
 
-  console.log('config', config)
   return config;
 });
-
-
