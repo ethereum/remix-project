7fa090e5c3eb2fc1d6ac8029a78d350e845d37b7
Joseph Izang <josephizang@gmail.com>
2023-05-29 23:29:38 +0100

    work on badges. next events

---
+++ libs/remix-ui/static-analyser/src/lib/components/BasicTitle.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type BasicTitleProps = {
+  warningStateEntries: any
+}
+
+function calculateWarningStateEntries(entries: any[][]) {
+  let warninglength = 0
+  entries.forEach((entry) => {
+    warninglength += entry[1].length
+  })
+  return warninglength
+}
+
+export default function BasicTitle(props: BasicTitleProps) {
+  return (
+    <span className="rounded-circle">Basic{props.warningStateEntries.length > 0 ? <i className="badge badge-info rounded-circle ml-2">{calculateWarningStateEntries(props.warningStateEntries)}</i>: null}
+    </span>
+  )
+}
