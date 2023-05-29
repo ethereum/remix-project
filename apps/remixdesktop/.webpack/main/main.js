c65465b056400d7e87fce7d27bba996df9d23e85
bunsenstraat <filip.mertens@ethereum.org>
2023-05-29 09:52:36 +0200

    rm trash

--- apps/remixdesktop/.webpack/main/main.js
+++ apps/remixdesktop/.webpack/main/main.js
@@ -1,7 +1,7 @@
 "use strict";
 (self["webpackChunk"] = self["webpackChunk"] || []).push([["main"],{
 
-/***/ 4117:
+/***/ 117:
 /***/ (() => {
 
 
@@ -11,6 +11,6 @@
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__(4117));
+/******/ var __webpack_exports__ = (__webpack_exec__(117));
 /******/ }
 ]);
