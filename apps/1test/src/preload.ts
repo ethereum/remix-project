87f65eeba73346b3b18f70e237a8eef3707781ea
filip mertens <filip.mertens@ethereum.org>
2023-06-01 10:41:22 +0200

    refactor

--- apps/1test/src/preload.ts
+++ apps/1test/src/preload.ts
@@ -4,12 +4,21 @@ import { contextBridge, ipcRenderer } from 'electron'
 
 console.log('preload.ts')
 
-contextBridge.exposeInMainWorld('api', {
+/* preload script needs statically defined API for each plugin */
+
+const exposedPLugins = ['fs', 'git']
+
+contextBridge.exposeInMainWorld('electronAPI', {
   activatePlugin: (name: string) => {
     return ipcRenderer.invoke('engine:activatePlugin', name)
   },
-  receiveFromFS: (cb:any) => ipcRenderer.on('fs:send', cb),
-  sendToFS:  (message: Partial<Message>) => ipcRenderer.send('fs:on', message),
-  receiveFromGit: (cb:any) => ipcRenderer.on('git:send', cb),
-  sendToGit: (message: Partial<Message>) => ipcRenderer.send('git:on', message)
+  
+  plugins: exposedPLugins.map(name => {
+    return {
+      name,
+      activate: () => ipcRenderer.invoke('engine:activatePlugin', name),
+      on: (cb:any) => ipcRenderer.on(`${name}:send`, cb),
+      send: (message: Partial<Message>) => ipcRenderer.send(`${name}:on`, message)
+    }
+  })
 })
