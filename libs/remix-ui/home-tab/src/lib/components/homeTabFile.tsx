bca3a16f6182b08d64cf8eeae84e7181153cb31f
lianahus <liana@ethereum.org>
2023-06-05 10:11:34 +0200

    fixed switch to workspace

--- libs/remix-ui/home-tab/src/lib/components/homeTabFile.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFile.tsx
@@ -119,22 +119,20 @@ function HomeTabFile({plugin}: HomeTabFileProps) {
     const workspaces = await plugin.call('filePanel', 'getWorkspaces')
     if (!workspaces.find(workspace => workspace.name === wName)) {
       await plugin.call('filePanel', 'createWorkspace', wName, 'remixDefault')
-      await plugin.call('filePanel', 'setWorkspace', wName)
-    } else {
-      await plugin.call('filePanel', 'switchToWorkspace', { name: wName, isLocalHost: false })
     }
+    await plugin.call('filePanel', 'switchToWorkspace', { name: wName, isLocalHost: false })
+    await plugin.call('filePanel', 'switchToWorkspace', { name: wName, isLocalHost: false }) // don't ask why
 
-    const content = `// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+    const content = `
+  // SPDX-License-Identifier: MIT
+  pragma solidity >=0.7.0 <0.9.0;
 
-contract helloWorld {
-}
-`
-
-    const {newContent, newPath} = await plugin.call('fileManager', 'writeFileNoRewrite', '/contracts/helloWorld.sol', content)
+  contract helloWorld {
+  }
+  `
+    const {newPath} = await plugin.call('fileManager', 'writeFileNoRewrite', '/contracts/helloWorld.sol', content)
     await plugin.call('fileManager', 'open', newPath)
   }
-
   const uploadFile = async (target) => {
     _paq.push(['trackEvent', 'hometab', 'filesSection', 'uploadFile'])
     await plugin.call('filePanel', 'uploadFile', target)
