966903d617007bc55450b9bfaf27d28b8a657aec
Joseph Izang <josephizang@gmail.com>
2023-06-07 14:50:50 +0100

    fix error counting for basic tab

--- libs/remix-ui/static-analyser/src/lib/components/BasicTitle.tsx
+++ libs/remix-ui/static-analyser/src/lib/components/BasicTitle.tsx
@@ -29,9 +29,24 @@ export function calculateWarningStateEntries(entries: [string, any][]) {
   return { length: warninglength, errors }
 }
 
+function determineLength(warningStateEntries: [string, any][]) {
+  let length = 0
+  warningStateEntries.forEach((entry) => {
+    length += entry[1].filter(x => x.options.type === 'warning').length
+  })
+  return length
+}
+
 export function BasicTitle(props: BasicTitleProps) {
+  determineLength(props.warningStateEntries)
   return (
-    <span>Basic{props.warningStateEntries.length > 0 && !props.hideWarnings ? <i data-id="StaticAnalysisErrorCount" className="badge badge-info rounded-circle ml-1">{calculateWarningStateEntries(props.warningStateEntries).length}</i>: null}
+    <span>Remix{props.warningStateEntries.length > 0 && !props.hideWarnings ? <i data-id="StaticAnalysisErrorCount" className="badge badge-info rounded-circle ml-1">{calculateWarningStateEntries(props.warningStateEntries).length}</i>: (
+      <i className="badge badge-info rounded-circle ml-1">
+        {
+          calculateWarningStateEntries(props.warningStateEntries).errors.length
+        }
+      </i>
+    )}
     </span>
   )
 }
