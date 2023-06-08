13e79b23c3506175388f3a4af8b6e861ccc5246c
Joseph Izang <josephizang@gmail.com>
2023-06-08 16:17:39 +0100

    add id attribute to button

--- libs/remix-ui/static-analyser/src/lib/Button/StaticAnalyserButton.tsx
+++ libs/remix-ui/static-analyser/src/lib/Button/StaticAnalyserButton.tsx
@@ -20,7 +20,7 @@ const StaticAnalyserButton = ({
   const newclassList = disabled && classList.length > 0 ? `${classList} disabled` :
   classList.length === 0 && disabled ? `${defaultStyle} disabled` : classList.length > 0 ? `${classList}` : defaultStyle
   return (
-    <button className={newclassList} disabled={disabled} onClick={onClick}>
+    <button id="staticAnalysisRunBtn" className={newclassList} disabled={disabled} onClick={onClick}>
       <CustomTooltip
         placement="right"
         tooltipId="ssaRunButtonTooltip"
