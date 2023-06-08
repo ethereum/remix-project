ad387542440e2b93a6ce63c74d02754ce50665e4
Aniket-Engg <aniketengg.225@gmail.com>
2023-06-08 13:20:31 +0530

    optional in TS

--- apps/etherscan/src/app/utils/scripts.ts
+++ apps/etherscan/src/app/utils/scripts.ts
@@ -10,7 +10,7 @@ export const verifyScript = `
  * @param {string} expectedImplAddress - Implementation contract address, in case of proxy contract verification (optional)
  * @returns {{ guid, status, message, succeed }} verification result
  */
-export const verify = async (apikey: string, contractAddress: string, contractArguments: string, contractName: string, contractFile: string, chainRef: number | string, isProxyContract: boolean, expectedImplAddress: string) => {
+export const verify = async (apikey: string, contractAddress: string, contractArguments: string, contractName: string, contractFile: string, chainRef?: number | string, isProxyContract?: boolean, expectedImplAddress?: string) => {
     const compilationResultParam = await remix.call('compilerArtefacts' as any, 'getCompilerAbstract', contractFile)
     console.log('verifying.. ' + contractName)
     // update apiKey and chainRef to verify contract on multiple networks
