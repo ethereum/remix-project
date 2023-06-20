5fec997575f469c986fb1700fea6f24a6695e0a8
yann300 <yann.levreau@gmail.com>
2023-06-20 12:01:01 +0200

    basic gpt impl

---
+++ apps/remix-ide/src/app/plugins/openaigpt.tsx
@@ -0,0 +1,37 @@
+import { Plugin } from '@remixproject/engine'
+import { OpenAIApi, CreateChatCompletionResponse } from 'openai'
+
+const _paq = window._paq = window._paq || []
+
+const profile = {
+  name: 'openaigpt',
+  displayName: 'openaigpt',
+  description: 'openaigpt',
+  methods: ['message'],
+  events: [],
+  maintainedBy: 'Remix',
+}
+
+export class OpenAIGpt extends Plugin {
+  openai: OpenAIApi
+
+  constructor() {
+    super(profile)
+  }
+
+  async message(prompt): Promise<CreateChatCompletionResponse> {
+    this.call('terminal', 'log', 'Waiting for GPT answer...')
+    const result = await (await fetch('https://openai-gpt.remixproject.org', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({prompt})
+    })).json()
+    
+    console.log(result)
+    this.call('terminal', 'log', { type: 'typewriterlog', value: result.choices[0].message.content })
+    return result.data
+  }
+}
--- libs/remix-ui/renderer/src/lib/renderer.tsx
+++ libs/remix-ui/renderer/src/lib/renderer.tsx
@@ -67,6 +67,21 @@ export const Renderer = ({message, opt = {}, plugin}: RendererProps) => {
     }
   }
 
+  const askGtp = async () => {
+    try {
+      const content = await plugin.call('fileManager', 'readFile', editorOptions.errFile)
+      const message = `
+      solidity file: ${content}
+      error message: ${messageText}
+      explain which line the error occured, the reason and how to fix it.
+      `
+      await plugin.call('openaigpt', 'message', message)      
+    } catch (err) {
+      console.error('unable to askGtp')
+      console.error(err)
+    }
+  }
+
   return (
     <>
       {messageText && !close && (
@@ -82,6 +97,7 @@ export const Renderer = ({message, opt = {}, plugin}: RendererProps) => {
             <i className="fas fa-times"></i>
           </div>
           <CopyToClipboard content={messageText} className={` p-0 m-0 far fa-copy ${classList}`} direction={'top'} />
+          <span onClick={() => { askGtp() }}>ASK GPT</span>
         </div>
       )}
     </>
