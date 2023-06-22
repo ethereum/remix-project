3660c8bc336b665dbeea6ea296b0c8569dd8cfe6
filip mertens <filip.mertens@ethereum.org>
2023-06-22 08:44:51 +0200

    fix menu

--- apps/remixdesktop/src/menus/edit.ts
+++ apps/remixdesktop/src/menus/edit.ts
@@ -6,11 +6,6 @@ export default (
 ) => {
   const submenu: MenuItemConstructorOptions[] = [
     {
-      label: 'Cut',
-      accelerator: commandKeys['editor:cut'],
-      enabled: false
-    },
-    {
       role: 'copy',
       command: 'editor:copy',
       accelerator: commandKeys['editor:copy'],
@@ -20,7 +15,22 @@ export default (
       role: 'paste',
       accelerator: commandKeys['editor:paste'],
       registerAccelerator: true
-    }
+    },
+    {
+      role: 'cut',
+      accelerator: commandKeys['editor:cut'],
+      registerAccelerator: true 
+    },
+    {
+      role: 'selectAll',
+      accelerator: commandKeys['editor:selectall'],
+      registerAccelerator: true
+    },
+    {
+      role: 'undo',
+      accelerator: commandKeys['editor:undo'],
+      registerAccelerator: true
+    },
   ];
 
   if (process.platform !== 'darwin') {
