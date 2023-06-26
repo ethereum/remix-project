371cb0dec6b845deb262edb65673aea288a31cd1
yann300 <yann.levreau@gmail.com>
2023-06-26 12:50:32 +0200

    open files upon creation

--- libs/remix-ws-templates/src/script-templates/contract-deployer/index.ts
+++ libs/remix-ws-templates/src/script-templates/contract-deployer/index.ts
@@ -3,9 +3,13 @@ export const contractDeployerScripts = async (plugin) => {
         'scripts/contract-deployer/create2-factory-deploy.ts' ,
         // @ts-ignore
         (await import('!!raw-loader!./create2-factory-deploy.ts')).default)
+
+    await plugin.call('fileManager', 'open', 'scripts/contract-deployer/create2-factory-deploy.ts')
     
     await plugin.call('fileManager', 'writeFile', 
         'scripts/contract-deployer/basic-contract-deploy.ts' ,
         // @ts-ignore
         (await import('!!raw-loader!./basic-contract-deploy.ts')).default)
+
+    await plugin.call('fileManager', 'open', 'scripts/contract-deployer/basic-contract-deploy.ts')
 }
