c7ce0c3889e2d6a23389fe5951822bbf330cc26d
bunsenstraat <bunsenstraat@gmail.com>
2023-07-04 17:35:16 +0200

    Delete remix_accounts.sol

--- apps/remix-ide/contracts/.deps/remix-tests/remix_accounts.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.4.22 <0.9.0;
-
-library TestsAccounts {
-    function getAccount(uint index) pure public returns (address) {
-        return address(0);
-    }
-}
