d169033bf1106d2addc7eda0fcf4d0a2a5019160
bunsenstraat <bunsenstraat@gmail.com>
2023-07-06 13:58:48 +0200

    split

--- apps/remix-ide/ci/remixd_windows.sh
+++ apps/remix-ide/ci/remixd_windows.sh
@@ -7,7 +7,7 @@ npx http-server -p 9090 --cors='*' ./node_modules &
 yarn run serve:production &
 sleep 5
 
-TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep remixd)
+TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep remixd | circleci tests split)
 for TESTFILE in $TESTFILES; do
     npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js ${TESTFILE} --env=edge || npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js ${TESTFILE} --env=edge || TEST_EXITCODE=1
 done
