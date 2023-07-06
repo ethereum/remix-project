8d039b4bc1be2935ee4cca9224e1d11802ab2a66
bunsenstraat <bunsenstraat@gmail.com>
2023-07-06 13:33:16 +0200

    script

--- apps/remix-ide/ci/remixd_windows.sh
+++ apps/remix-ide/ci/remixd_windows.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
-BUILD_ID=${CIRCLE_BUILD_NUM:-${TRAVIS_JOB_NUMBER}}
-echo "$BUILD_ID"
 TEST_EXITCODE=0
 yarn run ganache-cli &
 npx http-server -p 9090 --cors='*' ./node_modules &
 yarn run serve:production &
 sleep 5
 
-npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js dist/apps/remix-ide-e2e/src/tests/remixd_group1.test.js --env=edge
+TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep remixd)
+for TESTFILE in $TESTFILES; do
+    npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js ${TESTFILE} --env=edge || npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js ${TESTFILE} --env=edge || TEST_EXITCODE=1
+done
 
 echo "$TEST_EXITCODE"
 if [ "$TEST_EXITCODE" -eq 1 ]
