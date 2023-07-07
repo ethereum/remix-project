9be770212fe7096c04f6f0d707b39202ad7665fc
filip mertens <filip.mertens@ethereum.org>
2023-07-07 13:16:51 +0200

    chrome

--- apps/remix-ide/ci/remixd_windows.sh
+++ apps/remix-ide/ci/remixd_windows.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
 # we don't know which EDGE is installed, could be 112 or 114, it's unclear why the image is not consistent
 set -e
-EDGE_VERSION=$(powershell '(Get-Item "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe").VersionInfo.ProductVersion')
 
-echo "Edge version is $EDGE_VERSION"
-
-if [[ $EDGE_VERSION == *"112"* ]]; then
-  echo "Edge 112 is installed"
-  yarn run ganache-cli &
-  npx http-server -p 9090 --cors='*' ./node_modules &
-  yarn run serve:production &
-  yarn run selenium:edge112 &
-  sleep 5
-
-elif [[ $EDGE_VERSION == *"114"* ]]; then
-  echo "Edge 114 is installed"
-  yarn run ganache-cli &
-  npx http-server -p 9090 --cors='*' ./node_modules &
-  yarn run serve:production &
-  yarn run selenium:edge114 &
-  sleep 5
-else
-  echo "Edge version is not 112 or 114"
-  exit 1
-fi
+yarn run ganache-cli &
+npx http-server -p 9090 --cors='*' ./node_modules &
+yarn run serve:production &
+sleep 5
 
 TEST_EXITCODE=0
 
@@ -43,11 +25,11 @@ npx -y kill-port 4444
 
 if [ "$TEST_EXITCODE" -eq 1 ]
 then
-
-  echo "Tests failed"
-  exit 1
-
+    
+    echo "Tests failed"
+    exit 1
+    
 else
-  echo "Tests passed"
-  exit 0
+    echo "Tests passed"
+    exit 0
 fi
