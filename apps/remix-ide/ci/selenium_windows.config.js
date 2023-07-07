d26e4e18f62945e2a3448923f3d5dcd06591ff26
filip mertens <filip.mertens@ethereum.org>
2023-07-07 13:26:59 +0200

    rm edge

--- apps/remix-ide/ci/selenium_windows.config.js
+++ apps/remix-ide/ci/selenium_windows.config.js
@@ -5,11 +5,6 @@ module.exports = {
         version: '114.0.5735.16',
         arch: process.arch,
         baseURL: 'https://chromedriver.storage.googleapis.com'
-      },
-      chromiumedge: {
-        version: '112.0.1722.39',
-        arch: process.arch,
-        baseURL: 'https://msedgedriver.azureedge.net/'
       }
     }
   }
