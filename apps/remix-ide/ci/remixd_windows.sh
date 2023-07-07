a99d4200d3dd557adb04a1d40b484d9fec57156f
filip mertens <filip.mertens@ethereum.org>
2023-07-07 11:41:16 +0200

    script

--- apps/remix-ide/ci/remixd_windows.sh
+++ apps/remix-ide/ci/remixd_windows.sh
@@ -5,7 +5,7 @@ TEST_EXITCODE=0
 yarn run ganache-cli &
 npx http-server -p 9090 --cors='*' ./node_modules &
 yarn run serve:production &
-yarn run selenium:edge114 &
+yarn run selenium:edge112 &
 sleep 5
 
 TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep remixd | circleci tests split)
@@ -24,37 +24,8 @@ npx -y kill-port 4444
 if [ "$TEST_EXITCODE" -eq 1 ]
 then
 
-  # if 114 fails, try 112 
-  echo "Tests failed Retry"
-
-  TEST_EXITCODE=0
-  yarn run ganache-cli &
-  npx http-server -p 9090 --cors='*' ./node_modules &
-  yarn run serve:production &
-  yarn run selenium:edge112 &
-  sleep 5
-
-  TESTFILES=$(grep -IRiL "@disabled" "dist/apps/remix-ide-e2e/src/tests" | grep remixd | circleci tests split)
-  for TESTFILE in $TESTFILES; do
-      npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch.js ${TESTFILE} --env=edge || TEST_EXITCODE=1
-  done
-
-  echo "$TEST_EXITCODE"
-
-  # kill process
-  npx -y kill-port 8080
-  npx -y kill-port 8545
-  npx -y kill-port 9090
-  npx -y kill-port 4444
-
-  if [ "$TEST_EXITCODE" -eq 1 ]
-  then
-    echo "Tests failed"
-    exit 1
-  else
-    echo "Tests passed"
-    exit 0
-  fi
+  echo "Tests failed"
+  exit 1
 
 else
   echo "Tests passed"
