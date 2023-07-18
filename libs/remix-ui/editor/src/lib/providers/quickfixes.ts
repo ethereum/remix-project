ab6fe57405b65f58b1e46860867b74abf9cde6b2
Aniket-Engg <aniketengg.225@gmail.com>
2023-07-18 19:56:00 +0530

    add SPDX license quickfix

---
+++ libs/remix-ui/editor/src/lib/providers/quickfixes.ts
@@ -0,0 +1,6 @@
+export default {
+    "Warning: SPDX license identifier not provided in source file. Before publishing, consider adding a comment containing \"SPDX-License-Identifier: <SPDX-License>\" to each source file. Use \"SPDX-License-Identifier: UNLICENSED\" for non-open-source code. Please see https://spdx.org for more information.\n--> contracts/2_Owner.sol\n\n": {
+        "title": "Add open-source license",
+        "message": "// SPDX-License-Identifier: GPL-3.0"
+    }
+}
