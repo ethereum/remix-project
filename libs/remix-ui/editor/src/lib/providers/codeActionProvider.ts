236fee5470d30d4c335ac9c54b8c4fde954143c5
bunsenstraat <filip.mertens@ethereum.org>
2023-07-24 12:26:20 +0200

    fix lint

--- libs/remix-ui/editor/src/lib/providers/codeActionProvider.ts
+++ libs/remix-ui/editor/src/lib/providers/codeActionProvider.ts
@@ -4,45 +4,45 @@ import { EditorUIProps } from "../remix-ui-editor"
 import { default as fixes } from "./quickfixes"
 
 export class RemixCodeActionProvider implements monaco.languages.CodeActionProvider {
-    props: EditorUIProps
-    monaco: Monaco
-    constructor(props: any, monaco: any) {
-        this.props = props
-        this.monaco = monaco
-    }
+  props: EditorUIProps
+  monaco: Monaco
+  constructor(props: any, monaco: any) {
+    this.props = props
+    this.monaco = monaco
+  }
 
-    async provideCodeActions (
-        model /**ITextModel*/,
-        range /**Range*/,
-        context /**CodeActionContext*/,
-        token /**CancellationToken*/
-    ) {
+  async provideCodeActions (
+    model /**ITextModel*/,
+    range /**Range*/,
+    context /**CodeActionContext*/,
+    token /**CancellationToken*/
+  ) {
 
-        const actions = context.markers.map(error => {
-            const errStrings = Object.keys(fixes)
-            const errStr = errStrings.find(es => error.message.includes(es))
-            const fix = fixes[errStr]
-            return {
-                title: fix.title,
-                diagnostics: [error],
-                kind: "quickfix",
-                edit: {
-                    edits: [
-                        {
-                            resource: model.uri,
-                            edit: {
-                                range: fix.range || error,
-                                text: fix.message
-                            }
-                        }
-                    ]
-                },
-                isPreferred: true
+    const actions = context.markers.map(error => {
+      const errStrings = Object.keys(fixes)
+      const errStr = errStrings.find(es => error.message.includes(es))
+      const fix = fixes[errStr]
+      return {
+        title: fix.title,
+        diagnostics: [error],
+        kind: "quickfix",
+        edit: {
+          edits: [
+            {
+              resource: model.uri,
+              edit: {
+                range: fix.range || error,
+                text: fix.message
+              }
             }
-        })
-        return {
-            actions: actions,
-            dispose: () => {}
-        }
+          ]
+        },
+        isPreferred: true
+      }
+    })
+    return {
+      actions: actions,
+      dispose: () => {}
     }
+  }
 }
