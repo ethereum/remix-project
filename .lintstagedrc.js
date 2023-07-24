76142f0fd472baaebb51972f8f4a1eb220283078
Joseph Izang <josephizang@gmail.com>
2023-07-24 16:34:08 +0100

    add packages. add config prettier and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "*.{js,ts,tsx}": ["prettier --write", "eslint --fix"],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  singleAttributePerLine: false,
+}
