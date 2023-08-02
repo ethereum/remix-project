647b819f7e388e6275e4b4414650765032e7ab4d
aniket-engg <aniketengg.225@gmail.com>
2023-08-02 13:17:55 +0530

    set default solidity version to 0.8.21

--- libs/remix-ui/solidity-compiler/src/lib/compiler-container.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/compiler-container.tsx
@@ -51,7 +51,7 @@ export const CompilerContainer = (props: CompilerContainerProps) => {
     customVersions: [],
     compilerLicense: null,
     selectedVersion: null,
-    defaultVersion: 'soljson-v0.8.18+commit.87f61d96.js', // this default version is defined: in makeMockCompiler (for browser test)
+    defaultVersion: 'soljson-v0.8.21+commit.d9974bed.js', // this default version is defined: in makeMockCompiler (for browser test)
     runs: '',
     compiledFileName: '',
     includeNightlies: false,
