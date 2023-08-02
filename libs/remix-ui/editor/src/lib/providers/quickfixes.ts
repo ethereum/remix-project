4eabf7be3954c3eb53cb4ee5e95e278c4f5a97a6
aniket-engg <aniketengg.225@gmail.com>
2023-08-02 12:18:31 +0530

    id and comments

--- libs/remix-ui/editor/src/lib/providers/quickfixes.ts
+++ libs/remix-ui/editor/src/lib/providers/quickfixes.ts
@@ -1,10 +1,12 @@
 export default {
   "Warning: SPDX license identifier not provided in source file. Before publishing, consider adding a comment containing \"SPDX-License-Identifier: <SPDX-License>\" to each source file. Use \"SPDX-License-Identifier: UNLICENSED\" for non-open-source code. Please see https://spdx.org for more information.": {
+    "id": 1,
     "title": "Add open-source license",
     "message": "// SPDX-License-Identifier: GPL-3.0",
     "nodeType": "SourceUnit"
   },
   "Warning: Source file does not specify required compiler version! Consider adding" : {
+    "id": 2,
     "title": "Add pragma line",
     "message": "pragma solidity ^0.*.*;",
     "nodeType": "PragmaDirective",
@@ -16,11 +18,13 @@ export default {
     }
   },
   "SyntaxError: No visibility specified. Did you intend to add \"public\"": {
+    "id": 3,
     "title": "Add visibility 'public'",
     "message": "public ",
     "nodeType": "FunctionDefinition"
   },
   "Warning: Function state mutability can be restricted to view": {
+    "id": 4,
     "title": "Add mutability 'view'",
     "message": "view ",
     "nodeType": "FunctionDefinition"
