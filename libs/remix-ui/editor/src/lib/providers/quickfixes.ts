42f745461049f94f3b667a237439a375bdb3be58
aniket-engg <aniketengg.225@gmail.com>
2023-08-08 15:10:24 +0530

    quick fixes as array and refactoring

--- libs/remix-ui/editor/src/lib/providers/quickfixes.ts
+++ libs/remix-ui/editor/src/lib/providers/quickfixes.ts
@@ -10,7 +10,7 @@ export default {
     "title": "Add non-open-source license",
     "message": "// SPDX-License-Identifier: UNLICENSED"
   }],
-  "Warning: Source file does not specify required compiler version! Consider adding" : {
+  "Warning: Source file does not specify required compiler version! Consider adding" : [{
     "id": 2,
     "title": "Add Solidity pragma",
     "message": "pragma solidity ^0.*.*;",
@@ -21,23 +21,23 @@ export default {
       startColumn: 1,
       endColumn: 1
     }
-  },
-  "SyntaxError: No visibility specified. Did you intend to add \"public\"": {
+  }],
+  "SyntaxError: No visibility specified. Did you intend to add \"public\"": [{
     "id": 3,
     "title": "Add visibility 'public'",
     "message": "public ",
     "nodeType": "FunctionDefinition"
-  },
-  "Warning: Function state mutability can be restricted to view": {
+  }],
+  "Warning: Function state mutability can be restricted to view": [{
     "id": 4,
     "title": "Add mutability 'view'",
     "message": "view ",
     "nodeType": "FunctionDefinition"
-  },
-  "Warning: Function state mutability can be restricted to pure": {
+  }],
+  "Warning: Function state mutability can be restricted to pure": [{
     "id": 5,
     "title": "Add mutability 'pure'",
     "message": "pure ",
     "nodeType": "FunctionDefinition"
-  }
+  }]
 }
