fda62978b9f616611597f1baeb11f5c2d5d95d25
drafish <xwlyy1991@163.com>
2023-08-14 10:00:28 +0800

    patch en locale messages for udapp and filePanel

--- apps/remix-ide/src/app/providers/hardhat-provider.tsx
+++ apps/remix-ide/src/app/providers/hardhat-provider.tsx
@@ -1,5 +1,6 @@
 import * as packageJson from '../../../../../package.json'
 import React from 'react' // eslint-disable-line
+import {FormattedMessage} from 'react-intl'
 import {AbstractProvider} from './abstract-provider'
 
 const profile = {
@@ -20,15 +21,21 @@ export class HardhatProvider extends AbstractProvider {
     return (
       <div>
         {' '}
-        Note: To run Hardhat network node on your system, go to hardhat project folder and run command:
+        <FormattedMessage id="udapp.hardhatProviderText1" />
         <div className="p-1 pl-3">
           <b>npx hardhat node</b>
         </div>
         <div className="pt-2 pb-4">
-          For more info, visit:{' '}
-          <a href="https://hardhat.org/getting-started/#connecting-a-wallet-or-dapp-to-hardhat-network" target="_blank">
-            Hardhat Documentation
-          </a>
+          <FormattedMessage
+            id="udapp.hardhatProviderText2"
+            values={{
+              a: (chunks) => (
+                <a href="https://hardhat.org/getting-started/#connecting-a-wallet-or-dapp-to-hardhat-network" target="_blank">
+                  {chunks}
+                </a>
+              )
+            }}
+          />
         </div>
         <div>Hardhat JSON-RPC Endpoint:</div>
       </div>
