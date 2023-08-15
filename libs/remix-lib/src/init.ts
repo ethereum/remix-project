28ca150d1ca039c525fd64b89553a30340b504cd
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-08-14 22:03:50 -0400

    fix lint

--- libs/remix-lib/src/init.ts
+++ libs/remix-lib/src/init.ts
@@ -20,8 +20,8 @@ class Web3DebugPlugin extends Web3PluginBase {
       method: 'debug_preimage',
       params: [key]
     })
-    .then(result => cb(null, result))
-    .catch(error => cb(error))
+      .then(result => cb(null, result))
+      .catch(error => cb(error))
   }
 
   public traceTransaction(txHash, options, cb) {
@@ -29,8 +29,8 @@ class Web3DebugPlugin extends Web3PluginBase {
       method: 'debug_traceTransaction',
       params: [txHash, options]
     })
-    .then(result => cb(null, result))
-    .catch(error => cb(error))
+      .then(result => cb(null, result))
+      .catch(error => cb(error))
   }
 
   public storageRangeAt(txBlockHash, txIndex, address, start, maxSize, cb) {
@@ -38,7 +38,7 @@ class Web3DebugPlugin extends Web3PluginBase {
       method: 'debug_storageRangeAt',
       params: [txBlockHash, txIndex, address, start, maxSize]
     })
-    .then(result => cb(null, result))
-    .catch(error => cb(error))
+      .then(result => cb(null, result))
+      .catch(error => cb(error))
   }
 }
