76f9db54102fd55696f53a0a1363ec8d4c7d4315
aniket-engg <aniketengg.225@gmail.com>
2023-08-23 12:13:16 +0530

    fix e2e

--- apps/remix-ide-e2e/src/commands/checkAnnotations.ts
+++ apps/remix-ide-e2e/src/commands/checkAnnotations.ts
@@ -2,8 +2,8 @@ import EventEmitter from 'events'
 import {NightwatchBrowser} from 'nightwatch'
 
 class checkAnnotations extends EventEmitter {
-  command(this: NightwatchBrowser, type: string, line: number): NightwatchBrowser {
-    this.api.assert.containsText(`.glyph-margin-widgets .${type}`, line.toString()).perform(() => this.emit('complete'))
+  command(this: NightwatchBrowser, type: string): NightwatchBrowser {
+    this.api.waitForElementPresent(`.glyph-margin-widgets .${type}`).perform(() => this.emit('complete'))
     return this
   }
 }
