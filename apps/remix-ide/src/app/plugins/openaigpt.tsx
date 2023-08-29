fde9c403dd9b256f5f61d59fbd725065955de634
yann300 <yann.levreau@gmail.com>
2023-08-29 15:45:41 +0200

    fix call to openai

--- apps/remix-ide/src/app/plugins/openaigpt.tsx
+++ apps/remix-ide/src/app/plugins/openaigpt.tsx
@@ -1,7 +1,7 @@
 import { Plugin } from '@remixproject/engine'
-import { OpenAIApi, CreateChatCompletionResponse } from 'openai'
+import { CreateChatCompletionResponse } from 'openai'
 
-const _paq = window._paq = window._paq || []
+const _paq = (window._paq = window._paq || [])
 
 const profile = {
   name: 'openaigpt',
@@ -13,23 +13,23 @@ const profile = {
 }
 
 export class OpenAIGpt extends Plugin {
-  openai: OpenAIApi
-
   constructor() {
     super(profile)
   }
 
   async message(prompt): Promise<CreateChatCompletionResponse> {
     this.call('terminal', 'log', 'Waiting for GPT answer...')
-    const result = await (await fetch('https://openai-gpt.remixproject.org', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({prompt})
-    })).json()
-    
+    const result = await (
+      await fetch('https://openai-gpt.remixproject.org', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt }),
+      })
+    ).json()
+
     console.log(result)
     this.call('terminal', 'log', { type: 'typewritersuccess', value: result.choices[0].message.content })
     return result.data
