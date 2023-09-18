20598a48bc211b6009ef2a8d48af87f1c140c83a
Joseph Izang <josephizang@gmail.com>
2023-09-18 09:51:32 +0100

    simplify selector for addFile command

--- apps/remix-ide-e2e/src/commands/addFile.ts
+++ apps/remix-ide-e2e/src/commands/addFile.ts
@@ -43,7 +43,8 @@ function addFile(browser: NightwatchBrowser, name: string, content: NightwatchCo
             done()
           })
       } else {
-        browser.click('span[data-id="fileExplorerNewFilecreateNewFile"]')
+        // browser.click('span[data-id="fileExplorerNewFilecreateNewFile"]')
+        browser.click('#fileExplorerMenuContainer > span#createNewFile')
           .waitForElementContainsText('*[data-id$="/blank"]', '', 60000)
           .sendKeys('*[data-id$="/blank"] .remixui_items', name)
           .sendKeys('*[data-id$="/blank"] .remixui_items', browser.Keys.ENTER)
