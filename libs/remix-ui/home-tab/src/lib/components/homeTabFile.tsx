cd31f6d7c64b5e75a2246f5a2a37c593a334e2e4
lianahus <liana@ethereum.org>
2023-09-21 11:19:19 +0200

    adding tooltips to home buttons

--- libs/remix-ui/home-tab/src/lib/components/homeTabFile.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabFile.tsx
@@ -244,44 +244,37 @@ function HomeTabFile({ plugin }: HomeTabFileProps) {
         </label>
         <div className="d-flex flex-column">
           <div className="d-flex flex-row">
-            <button className="btn btn-primary text-nowrap p-2 mr-2 border my-1" data-id="homeTabStartCoding" style={{ width: 'fit-content' }} onClick={() => startCoding()}>
-              <FormattedMessage id="home.startCoding" />
-            </button>
-            <label className="btn text-nowrap p-2 mr-2 border my-1" style={{ width: 'fit-content', cursor: 'pointer' }} htmlFor="openFileInput">
-              <FormattedMessage id="home.openFile" />
-            </label>
-            <input
-              title="open file"
-              type="file"
-              id="openFileInput"
-              onChange={(event) => {
-                event.stopPropagation()
-                plugin.verticalIcons.select('filePanel')
-                uploadFile(event.target)
-              }}
-              multiple
-            />
             <CustomTooltip
               placement={'top'}
               tooltipId="overlay-tooltip"
               tooltipClasses="text-nowrap"
-              tooltipText={<FormattedMessage id='home.openFile' />}
+              tooltipText={<FormattedMessage id='home.startCodingPlayground' />}
+              tooltipTextClasses="border bg-light text-dark p-1 pr-3"
+            >
+              <button className="btn btn-primary text-nowrap p-2 mr-2 border my-1" data-id="homeTabStartCoding" style={{ width: 'fit-content' }} onClick={() => startCoding()}>
+                <FormattedMessage id="home.startCoding" />
+              </button>
+            </CustomTooltip>
+            <CustomTooltip
+              placement={'top'}
+              tooltipId="overlay-tooltip"
+              tooltipClasses="text-nowrap"
+              tooltipText={<FormattedMessage id='home.openFileTooltip' />}
               tooltipTextClasses="border bg-light text-dark p-1 pr-3"
             >
               <span>
-                <label className="btn p-2 mr-2 border my-1" style={{width: 'fit-content', cursor: 'pointer'}} htmlFor="openFileInput">
-                  <FormattedMessage id='home.openFile' />
+                <label className="btn text-nowrap p-2 mr-2 border my-1" style={{ width: 'fit-content', cursor: 'pointer' }} htmlFor="openFileInput">
+                  <FormattedMessage id="home.openFile" />
                 </label>
                 <input
                   title="open file"
                   type="file"
-                  id="openFileInput" 
-                  onChange={
-                    (event) => {
-                      event.stopPropagation()
-                      plugin.verticalIcons.select('filePanel')
-                      uploadFile(event.target)
-                    }}
+                  id="openFileInput"
+                  onChange={(event) => {
+                    event.stopPropagation()
+                    plugin.verticalIcons.select('filePanel')
+                    uploadFile(event.target)
+                  }}
                   multiple
                 />
               </span>
