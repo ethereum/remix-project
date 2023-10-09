f53d80725d67681767754322173015970e60e721
filip mertens <filip.mertens@ethereum.org>
2023-10-09 08:33:59 +0200

    solhint

--- apps/solhint/src/main.tsx
+++ apps/solhint/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import App from './app/app'
+import App from './app/App';
 
 const container = document.getElementById('root');
 
