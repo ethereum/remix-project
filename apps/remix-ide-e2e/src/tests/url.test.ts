baffa7cbd531f8d6997f0042eeb9931a3c91c5ee
filip mertens <filip.mertens@ethereum.org>
2023-10-10 07:36:10 +0200

    url test

--- apps/remix-ide-e2e/src/tests/url.test.ts
+++ apps/remix-ide-e2e/src/tests/url.test.ts
@@ -10,11 +10,11 @@ const sources = [
     'myTokenV1.sol': {
       content: `
       // SPDX-License-Identifier: MIT
-      pragma solidity ^0.8.4;
+      pragma solidity ^0.8.20;
       
       import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-      import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
       import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+      import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
       import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
       
       contract MyToken is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
@@ -23,9 +23,9 @@ const sources = [
               _disableInitializers();
           }
       
-          function initialize() initializer public {
+          function initialize(address initialOwner) initializer public {
               __ERC721_init("MyToken", "MTK");
-              __Ownable_init();
+              __Ownable_init(initialOwner);
               __UUPSUpgradeable_init();
           }
       
