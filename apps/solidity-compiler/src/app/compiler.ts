5f5267f028e97f9ad09ad671039830dd14218473
aniket-engg <aniketengg.225@gmail.com>
2023-10-11 13:33:16 +0530

    Set default solidity to 0.8.21

--- apps/solidity-compiler/src/app/compiler.ts
+++ apps/solidity-compiler/src/app/compiler.ts
@@ -9,7 +9,7 @@ import { CompileTabLogic } from '@remix-ui/solidity-compiler'
 const defaultCompilerParameters = {
   runs: '200',
   optimize: false,
-  version: 'soljson-v0.8.18+commit.87f61d96',
+  version: 'soljson-v0.8.21+commit.d9974bed',
   evmVersion: null, // compiler default
   language: 'Solidity',
   useFileConfiguration: false,
