6329c4de8128d13e235524312c26a64343ed22f0
filip mertens <filip.mertens@ethereum.org>
2023-10-30 14:13:44 +0100

    wait longer

--- apps/remix-ide-e2e/src/tests/transactionExecution.test.ts
+++ apps/remix-ide-e2e/src/tests/transactionExecution.test.ts
@@ -237,7 +237,11 @@ module.exports = {
       .setSolidityCompilerVersion('soljson-v0.8.17+commit.8df45f5f.js')
       .clickLaunchIcon('udapp')
       .switchEnvironment('vm-mainnet-fork')
-      .waitForElementPresent('select[data-id="runTabSelectAccount"] option[value="0xdD870fA1b7C4700F2BD7f44238821C26f7392148"]') // wait for the udapp to load the list of accounts
+      .waitForElementPresent({
+        locateStrategy: 'css selector',
+        selector: 'select[data-id="runTabSelectAccount"] option[value="0xdD870fA1b7C4700F2BD7f44238821C26f7392148"]',
+        timeout: 240000
+      }) // wait for the udapp to load the list of accounts
       .selectContract('MyResolver')
       .createContract('')
       .clickInstance(0)
