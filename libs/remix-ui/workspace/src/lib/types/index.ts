feb7e861f6fc88a1e4c1b2f7dbb60fc76cf109e7
ioedeveloper <daviddisu8@gmail.com>
2023-10-31 12:24:35 +0100

    Activate circom plugin after template selected

--- libs/remix-ui/workspace/src/lib/types/index.ts
+++ libs/remix-ui/workspace/src/lib/types/index.ts
@@ -17,7 +17,7 @@ export interface JSONStandardInput {
   };
 }
 export type MenuItems = action[]
-export type WorkspaceTemplate = 'gist-template' | 'code-template' | 'remixDefault' | 'blank' | 'ozerc20' | 'zeroxErc20' | 'ozerc721'
+export type WorkspaceTemplate = 'gist-template' | 'code-template' | 'remixDefault' | 'blank' | 'ozerc20' | 'zeroxErc20' | 'ozerc721' | 'semaphore'
 export interface WorkspaceProps {
   plugin: FilePanelType
 }
