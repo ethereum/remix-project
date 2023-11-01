2491b719f115c8dc1c15df0220386e7463dc6c96
Aniket <30843294+Aniket-Engg@users.noreply.github.com>
2023-11-01 18:08:03 +0530

    Update txExecution.ts

--- libs/remix-lib/src/execution/txExecution.ts
+++ libs/remix-lib/src/execution/txExecution.ts
@@ -82,7 +82,7 @@ export function checkError (execResult, compiledContracts) {
   const exceptionError = execResult.errorMessage || ''
   const error = `Error occured: ${execResult.errorMessage}.\n`
   let msg = ''
-  if (exceptionError === errorCode.INVALID_OPCODE) {
+  if (exceptionError.includes(errorCode.INVALID_OPCODE)) {
     msg = '\t\n\tThe execution might have thrown OR the EVM version used by the selected environment is not compatible with the compiler EVM version.\n'
     ret.error = true
   } else if (exceptionError === errorCode.OUT_OF_GAS) {
