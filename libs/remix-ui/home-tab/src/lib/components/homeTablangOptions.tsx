cb0f0c19418bd2f9da1d48776b53ef34285435d9
Joseph Izang <josephizang@gmail.com>
2023-11-03 14:11:12 +0100

    fix dropdown issues

--- libs/remix-ui/home-tab/src/lib/components/homeTablangOptions.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTablangOptions.tsx
@@ -26,19 +26,25 @@ export function LanguageOptions({ plugin }: { plugin: any }) {
 
   return (
     <>
-      <div className="d-flex align-items-center justify-content-end mr-2">
-        <DropdownButton title={langOptions} id="langdropdown" size="sm">
-          {['EN', 'ES', 'FR', 'ZH'].map(lang => (
-            <DropdownItem as={'span'} onClick={() =>
-            {
-              changeLanguage(lang.toLowerCase())
-              setLangOptions(lang)
-            }}
-            >
-              {lang}
-            </DropdownItem>
-          ))}
-        </DropdownButton>
+      <div className={langOptions !== 'fr' ? `d-flex align-items-center justify-content-end mr-2` : `d-flex align-items-center justify-content-end mr-3`}>
+        <Dropdown>
+          <Dropdown.Toggle title={langOptions} id="languagedropdown" size="sm">
+            {langOptions}
+          </Dropdown.Toggle>
+          <Dropdown.Menu className="dropdown-menu langSelector" style={{ minWidth: '2rem', backgroundColor: 'var(--body-bg)'}}>
+            {['EN', 'ES', 'FR', 'IT', 'ZH'].map(lang => (
+              <DropdownItem as={'span'} className="p-2" onClick={() =>
+              {
+                changeLanguage(lang.toLowerCase())
+                setLangOptions(lang)
+              }}
+              style={{ color: 'var(--white)'}}
+              >
+                {lang}
+              </DropdownItem>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     </>
   )
