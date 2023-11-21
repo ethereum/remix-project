2d95fdb3cac946310a3878d8fe2c033cb56eb7ac
ioedeveloper <daviddisu8@gmail.com>
2023-11-21 11:49:24 +0100

    Modify path before checking if path exists

--- apps/circuit-compiler/src/app/services/circomPluginClient.ts
+++ apps/circuit-compiler/src/app/services/circomPluginClient.ts
@@ -228,7 +228,7 @@ export class CircomPluginClient extends PluginClient {
       includes.map(async (include) => {
         // fix for endless recursive includes
         let dependencyContent = ''
-        let path = include
+        let path = include.replace(/(\.\.\/)+/g, '')
         // @ts-ignore
         const pathExists = await this.call('fileManager', 'exists', path)
 
