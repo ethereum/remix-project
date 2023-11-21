00b31af001d3b68d392eb8f5ccd5d37717f6e266
yann300 <yann.levreau@gmail.com>
2023-11-21 15:21:05 +0100

    add e2e

---
+++ apps/remix-ide-e2e/src/commands/waitForElementNotContainsText.ts
@@ -0,0 +1,28 @@
+import { NightwatchBrowser } from 'nightwatch'
+import EventEmitter from 'events'
+
+class WaitForElementNotContainsText extends EventEmitter {
+  command (this: NightwatchBrowser, id: string, value: string, timeout = 10000): NightwatchBrowser {
+    let waitId // eslint-disable-line
+    let currentValue
+    const runid = setInterval(() => {
+      this.api.getText(id, (result) => {
+        currentValue = result.value
+        if (typeof result.value === 'string' && result.value.indexOf(value) !== -1) {
+          clearInterval(runid)
+          clearTimeout(waitId)
+          this.api.assert.ok(false, `WaitForElementContainsText ${id} contains ${value} . It should not`)
+          this.emit('complete')
+        }
+      })
+    }, 200)
+
+    waitId = setTimeout(() => {
+      clearInterval(runid)
+      this.api.assert.fail(`TimeoutError: An error occurred while running .waitForElementContainsText() command on ${id} after ${timeout} milliseconds. expected: ${value} - got: ${currentValue}`)
+    }, timeout)
+    return this
+  }
+}
+
+module.exports = WaitForElementNotContainsText
