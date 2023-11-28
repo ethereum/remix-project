c7e9442ffdfe88a345056adea6a2cf81cb98be43
yann300 <yann.levreau@gmail.com>
2023-11-28 12:53:06 +0100

    use gpt from the terminal

--- apps/remix-ide/src/remixEngine.js
+++ apps/remix-ide/src/remixEngine.js
@@ -22,6 +22,7 @@ export class RemixEngine extends Engine {
     if (name === 'udapp') return { queueTimeout: 60000 * 4 }
     if (name === 'circuit-compiler') return { queueTimeout: 60000 * 4 }
     if (name === 'filePanel') return { queueTimeout: 60000 * 20 }
+    if (name === 'openaigpt') return { queueTimeout: 60000 * 2 }
     return { queueTimeout: 10000 }
   }
 
