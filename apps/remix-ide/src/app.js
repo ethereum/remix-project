45304b940a70ee538df18aadad99cf612de14062
Joseph Izang <josephizang@gmail.com>
2023-12-18 12:17:37 +0100

    fix app.js

--- apps/remix-ide/src/app.js
+++ apps/remix-ide/src/app.js
@@ -138,8 +138,6 @@ class AppComponent {
 
     this.walkthroughService = new WalkthroughService(appManager)
 
-    this.walkthroughService = new WalkthroughService(appManager, !this.showMatamo || !this.showEnter)
-
     const hosts = ['127.0.0.1:8080', '192.168.0.101:8080', 'localhost:8080']
     // workaround for Electron support
     if (!isElectron() && !hosts.includes(window.location.host)) {
