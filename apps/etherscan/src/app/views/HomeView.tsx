0ea3a763acbe1987df632d21b0654158a3b3cd1a
yann300 <yann.levreau@gmail.com>
2023-12-20 13:19:52 +0100

    do not use AppContext.Consumer

--- apps/etherscan/src/app/views/HomeView.tsx
+++ apps/etherscan/src/app/views/HomeView.tsx
@@ -8,28 +8,24 @@ import {Receipt} from '../types'
 import {VerifyView} from './VerifyView'
 
 export const HomeView: React.FC<React.PropsWithChildren> = () => {
-  return (
-    <AppContext.Consumer>
-      {({apiKey, clientInstance, setReceipts, receipts, contracts, networkName}) => {
-        return !apiKey ? (
-          <Navigate
-            to={{
-              pathname: '/settings'
-            }}
-          />
-        ) : (
-          <VerifyView
-            contracts={contracts}
-            client={clientInstance}
-            apiKey={apiKey}
-            onVerifiedContract={(receipt: Receipt) => {
-              const newReceipts = [...receipts, receipt]
-              setReceipts(newReceipts)
-            }}
-            networkName={networkName}
-          />
-        )
+  const context = React.useContext(AppContext)
+  
+  return !context.apiKey ? (
+    <Navigate
+      to={{
+        pathname: '/settings'
       }}
-    </AppContext.Consumer>
+    />
+  ) : (
+    <VerifyView
+      contracts={context.contracts}
+      client={context.clientInstance}
+      apiKey={context.apiKey}
+      onVerifiedContract={(receipt: Receipt) => {
+        const newReceipts = [...context.receipts, receipt]
+        context.setReceipts(newReceipts)
+      }}
+      networkName={context.networkName}
+    />
   )
 }
