42bcd5c657e21ac1a2f8365e76c8a20a3ecbcbe2
aniket-engg <aniketengg.225@gmail.com>
2023-12-28 16:28:16 +0530

    log

--- gulpfile.js
+++ gulpfile.js
@@ -65,10 +65,10 @@ task('syncLibVersions', async function () {
 });
 
 async function setBranchHead(branchName, head) {
-   await promisifyExec(`git checkout ${branchName}`);
-   await promisifyExec(`git pull origin ${branchName}`);
+   console.log(await promisifyExec(`git checkout ${branchName}`));
+   console.log(await promisifyExec(`git pull origin ${branchName}`));
    console.log(`pull done for ${branchName}`)
-   await promisifyExec(`git reset --hard ${head}`);
+   console.log(await promisifyExec(`git reset --hard ${head}`));
    await promisifyExec(`git push -f origin ${branchName}`);
 }
 
