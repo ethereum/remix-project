7aa26947337b36d1bd1f8e8c57bc45f1655a76b1
filip mertens <filip.mertens@ethereum.org>
2024-01-05 06:53:47 +0100

    clipboard

--- apps/remix-ide-e2e/src/helpers/init.ts
+++ apps/remix-ide-e2e/src/helpers/init.ts
@@ -24,6 +24,7 @@ export default function (browser: NightwatchBrowser, callback: VoidFunction, url
         .perform(done())
     })
     .verifyLoad()
+    
     .perform(() => {
       if (hideToolTips) {
         browser.execute(function () { // hide tooltips
