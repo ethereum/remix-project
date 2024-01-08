9df7c9a05d60be5c85cbe467fc60e5f6f8a2179f
yann300 <yann.levreau@gmail.com>
2024-01-08 12:51:49 +0100

    add overflow-hidden

--- libs/remix-ui/helper/src/lib/components/custom-dropdown.tsx
+++ libs/remix-ui/helper/src/lib/components/custom-dropdown.tsx
@@ -29,7 +29,7 @@ export const CustomToggle = React.forwardRef(
       className={className.replace('dropdown-toggle', '')}
     >
       <div className="d-flex">
-        <div className="mr-auto text-nowrap">{children}</div>
+        <div className="mr-auto text-nowrap overflow-hidden">{children}</div>
         {icon && (
           <div className="pr-1">
             <i className={`${icon} pr-1`}></i>
