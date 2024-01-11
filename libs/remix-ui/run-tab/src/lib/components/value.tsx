c1edbca18dd4435772cd96f9d54cb5e01cc52203
yann300 <yann.levreau@gmail.com>
2024-01-11 10:30:12 +0100

    Fix updating value

--- libs/remix-ui/run-tab/src/lib/components/value.tsx
+++ libs/remix-ui/run-tab/src/lib/components/value.tsx
@@ -6,13 +6,26 @@ import {CustomTooltip, isNumeric} from '@remix-ui/helper'
 import {ValueProps} from '../types'
 
 export function ValueUI(props: ValueProps) {
-  const inputValue = useRef<HTMLInputElement>({} as HTMLInputElement)  
+  const [sendValue, setSendValue] = useState<string>(props.sendValue)
+  const inputValue = useRef<HTMLInputElement>({} as HTMLInputElement)
 
   useEffect(() => {
-    if (props.sendValue !== inputValue.current.value) {
-      inputValue.current.value = props.sendValue
+    sendValue !== props.sendValue && props.setSendValue(sendValue)
+  }, [sendValue])
+
+  useEffect(() => {
+    if(props.sendValue !== sendValue) {
+      setSendValue(props.sendValue)
     }
   },[props.sendValue])
+
+  const validateInputKey = (e) => {
+    // preventing not numeric keys
+    // preventing 000 case
+    if (!isNumeric(e.key) || (e.key === '0' && !parseInt(inputValue.current.value) && inputValue.current.value.length > 0)) {
+      e.preventDefault()
+    }
+  }
 
   const validateValue = (e) => {
     const value = e.target.value
@@ -20,28 +33,22 @@ export function ValueUI(props: ValueProps) {
     if (!value) {
       // assign 0 if given value is
       // - empty
-      inputValue.current.value = '0'
-      props.setSendValue('0')
-      return
+      return setSendValue('0')
     }
 
     let v
     try {
       v = new BN(value, 10)
-      props.setSendValue(v.toString(10))
+      setSendValue(v.toString(10))
     } catch (e) {
       // assign 0 if given value is
       // - not valid (for ex 4345-54)
       // - contains only '0's (for ex 0000) copy past or edit
-      inputValue.current.value = '0'
-      props.setSendValue('0')
+      setSendValue('0')
     }
 
     // if giveen value is negative(possible with copy-pasting) set to 0
-    if (v.lt(0)) {
-      inputValue.current.value = '0'
-      props.setSendValue('0')
-    }
+    if (v.lt(0)) setSendValue('0')
   }
 
   return (
@@ -60,8 +67,9 @@ export function ValueUI(props: ValueProps) {
             className="form-control udapp_gasNval udapp_col2"
             id="value"
             data-id="dandrValue"
+            onKeyPress={validateInputKey}
             onChange={validateValue}
-            value={props.sendValue}
+            value={sendValue}
           />
         </CustomTooltip>
 
