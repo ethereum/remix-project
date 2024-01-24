0e2aa0e91ad9b3a39f3bbdbff168f0cfc009b828
Glitch <glitch.txs@gmail.com>
2024-01-24 09:54:59 -0300

    remove wagmi v2 add ethers v5 because of compat issues

--- apps/walletconnect/src/types.ts
+++ apps/walletconnect/src/types.ts
@@ -4,9 +4,10 @@ export interface RequestArguments {
   readonly id?: string
 }
 
-export interface EIP1193Provider {
-	request: <T>(args: RequestArguments) => Promise<T>
-  sendAsync: <T>(args: RequestArguments, callback:(error, response)=>void) => Promise<T>
-	on: (event: string, listener: (event: unknown) => void) => void
-	removeListener: (event: string, listener: (event: unknown) => void) => void
+export type Chain = {
+  chainId: number
+  name: string
+  currency: string
+  explorerUrl: string
+  rpcUrl: string
 }
