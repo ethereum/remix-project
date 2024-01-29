1d656eb02829acd665a85933cec417734b19eda1
Rim Rakhimov <rimrakhimov@gmail.com>
2024-01-29 11:55:02 +0400

    Remove warning about unknown host

--- libs/remix-ui/app/src/lib/remix-app/components/modals/origin-warning.tsx
+++ libs/remix-ui/app/src/lib/remix-app/components/modals/origin-warning.tsx
@@ -15,15 +15,6 @@ const OriginWarning = () => {
       (window.location.hostname === 'ethereum.github.io' && window.location.pathname.indexOf('/remix-live-alpha') === 0)
     ) {
       setContent('Welcome to the Remix alpha instance. Please use it to try out latest features. But use preferably https://remix.ethereum.org for any production work.')
-    } else if (
-      window.location.protocol.indexOf('http') === 0 &&
-      window.location.hostname !== 'remix.ethereum.org' &&
-      window.location.hostname !== 'localhost' &&
-      window.location.hostname !== '127.0.0.1'
-    ) {
-      setContent(`The Remix IDE has moved to http://remix.ethereum.org.\n
-      This instance of Remix you are visiting WILL NOT BE UPDATED.\n
-      Please make a backup of your contracts and start using http://remix.ethereum.org`)
     }
   }, [])
 
