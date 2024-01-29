d8be373c20c6b91a5e2bccbba3823dc41648bd12
bunsenstraat <filip@give.it>
2024-01-29 09:36:19 +0100

    auto update

--- apps/remix-ide/src/app/plugins/electron/appUpdaterPlugin.ts
+++ apps/remix-ide/src/app/plugins/electron/appUpdaterPlugin.ts
@@ -11,4 +11,45 @@ export class appUpdaterPlugin extends ElectronPlugin {
         console.log('appUpdaterPlugin')
         super(profile)
     }
+
+    onActivation(): void {
+        this.on('appUpdater', 'askForUpdate', () => {
+            console.log('askForUpdate')
+            const upgradeModal = {
+                id: 'confirmUpdate',
+                title: 'An update is available',
+                message: `A new version of Remix Desktop is available. Do you want to update?`,
+                modalType: 'modal',
+                okLabel: 'Yes',
+                cancelLabel: 'No',
+                okFn: () => {
+                    this.call('appUpdater', 'download')
+                },
+                cancelFn: () => {
+
+                },
+                hideFn: () => null
+            }
+            this.call('notification', 'modal', upgradeModal)
+        })
+        this.on('appUpdater', 'downloadReady', () => {
+            console.log('downloadReady')
+            const upgradeModal = {
+                id: 'confirmInstall',
+                title: 'An update is ready to install',
+                message: `A new version of Remix Desktop is ready to install. Do you want to install it now? This will close Remix Desktop.`,
+                modalType: 'modal',
+                okLabel: 'Yes',
+                cancelLabel: 'No',
+                okFn: () => {
+                    this.call('appUpdater', 'install')
+                },
+                cancelFn: () => {
+
+                },
+                hideFn: () => null
+            }
+            this.call('notification', 'modal', upgradeModal)
+        })
+    }
 }
--- apps/remixdesktop/src/plugins/appUpdater.ts
+++ apps/remixdesktop/src/plugins/appUpdater.ts
@@ -9,18 +9,65 @@ const profile = {
 }
 
 export class AppUpdaterPlugin extends ElectronBasePlugin {
+  clients: AppUpdaterPluginClient[] = []
   constructor() {
     console.log('AppUpdaterPlugin')
     super(profile, clientProfile, AppUpdaterPluginClient)
     this.methods = [...super.methods]
+
+    autoUpdater.autoDownload = false
+    autoUpdater.on('checking-for-update', () => {
+      console.log('Checking for update...');
+      this.sendToLog('Checking for update...')
+    })
+    autoUpdater.on('update-available', (info: any) => {
+      console.log('Update available.', info);
+      this.sendToLog('Update available.')
+      for (const client of this.clients) {
+        client.askForUpdate()
+      }
+    })
+    autoUpdater.on('update-not-available', () => {
+      console.log('Update not available.');
+      this.sendToLog('Update not available.')
+
+    })
+    autoUpdater.on('error', (err) => {
+      console.log('Error in auto-updater. ' + err);
+      this.sendToLog('Error in auto-updater. ' + err)
+    })
+    autoUpdater.on('download-progress', (progressObj) => {
+      let log_message = "Download speed: " + progressObj.bytesPerSecond;
+      log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+      log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+      console.log(log_message);
+      this.sendToLog(log_message)
+    })
+    autoUpdater.on('update-downloaded', (info) => {
+      console.log('Update downloaded');
+      this.sendToLog('Update downloaded')
+      for(const client of this.clients) {
+        client.downloadReady()
+      }
+    })
   }
+
+  async sendToLog(message: string): Promise<void> {
+    for (const client of this.clients) {
+      client.call('terminal', 'log', {
+        type: 'log',
+        value: message,
+      })
+    }
+  }
+
 }
 
 const clientProfile: Profile = {
   name: 'appUpdater',
   displayName: 'appUpdater',
   description: 'appUpdater',
-  methods: ['checkForUpdates'],
+  methods: ['checkForUpdates', 'download', 'install'],
 }
 
 class AppUpdaterPluginClient extends ElectronBasePluginClient {
@@ -34,53 +81,31 @@ class AppUpdaterPluginClient extends ElectronBasePluginClient {
     this.onload(async () => {
       console.log('onload', 'appUpdaterPluginClient')
       this.emit('loaded')
-      
-      autoUpdater.on('checking-for-update', () => {
-        console.log('Checking for update...');
-        this.call('terminal', 'log', {
-          type: 'log',
-          value: 'Checking for update...',
-        })
-      })
-      autoUpdater.on('update-available', (info: any) => {
-        console.log('Update available.', info);
-        this.call('terminal', 'log', {
-          type: 'log',
-          value: 'Update available.',
-        })
-       })
-       autoUpdater.on('update-not-available', () => {
-         console.log('Update not available.');
-          this.call('terminal', 'log', {
-            type: 'log',
-            value: 'Update not available.',
-          })
-       })
-       autoUpdater.on('error', (err) => {
-         console.log('Error in auto-updater. ' + err);
-          this.call('terminal', 'log', {
-            type: 'log',
-            value: 'Error in auto-updater. ' + err,
-          })
-       })
-       autoUpdater.on('update-downloaded', (info) => {
-         console.log('Update downloaded');
-          this.call('terminal', 'log', {
-            type: 'log',
-            value: 'Update downloaded',
-          })
-          autoUpdater.quitAndInstall();
-       })
-       await this.checkForUpdates()
+      await this.checkForUpdates()
     })
-
   }
- 
+
+  async askForUpdate(): Promise<void> {
+    this.emit('askForUpdate')
+  }
+
+  async downloadReady(): Promise<void> {
+    this.emit('downloadReady')
+  }
+
+  async download(): Promise<void> {
+    autoUpdater.downloadUpdate()
+  }
+
+  async install(): Promise<void> {
+    autoUpdater.quitAndInstall()
+  }
+
   async checkForUpdates(): Promise<void> {
     console.log('checkForUpdates')
     this.call('terminal', 'log', {
       type: 'log',
-      value: 'Checking for updates...' + autoUpdater.currentVersion,
+      value: 'Remix Desktop version: ' + autoUpdater.currentVersion,
     })
     autoUpdater.checkForUpdates()
   }
