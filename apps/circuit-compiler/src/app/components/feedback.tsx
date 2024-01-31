24416dcc55a6c0d26ee589ec10e4fce98daf2aca
ioedeveloper <daviddisu8@gmail.com>
2024-01-31 14:27:39 +0100

    Fix copy cotent and stop click propagation

--- apps/circuit-compiler/src/app/components/feedback.tsx
+++ apps/circuit-compiler/src/app/components/feedback.tsx
@@ -45,8 +45,7 @@ export function CompilerFeedback ({ feedback, filePathToId, hideWarnings, openEr
                   <RenderIf condition={response.type === 'Error'}>
                     <div className={`circuit_feedback ${response.type.toLowerCase()} alert alert-danger`} data-id="circuit_feedback">
                       <FeedbackAlert
-                        message={response.message}
-                        location={ response.labels[0] ? response.labels[0].message + ` ${filePathToId[response.labels[0].file_id]}:${response.labels[0].range.start}:${response.labels[0].range.end}` : null}
+                        message={response.message + (response.labels[0] ? ": " + response.labels[0].message + ` ${filePathToId[response.labels[0].file_id]}:${response.labels[0].range.start}:${response.labels[0].range.end}` : '')}
                         askGPT={ () => handleAskGPT(response) } />
                     </div>
                   </RenderIf>
@@ -54,7 +53,6 @@ export function CompilerFeedback ({ feedback, filePathToId, hideWarnings, openEr
                     <div className={`circuit_feedback ${response.type.toLowerCase()} alert alert-warning`} data-id="circuit_feedback">
                       <FeedbackAlert
                         message={response.message}
-                        location={null}
                         askGPT={() => { handleAskGPT(response) }} />
                     </div>
                   </RenderIf>
