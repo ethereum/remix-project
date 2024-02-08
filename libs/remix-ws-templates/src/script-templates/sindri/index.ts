cf622f3bf0b0e6793a4fa9cde1982b9f64bbc767
Evan Sangaline <esangaline@gmail.com>
2024-02-08 14:21:25 -0600

    Add `.sindriignore` when adding Sindri scripts.

--- libs/remix-ws-templates/src/script-templates/sindri/index.ts
+++ libs/remix-ws-templates/src/script-templates/sindri/index.ts
@@ -4,6 +4,21 @@ export const sindriScripts = async (plugin) => {
     // @ts-ignore
     (await import('!!raw-loader!./sindri.ts')).default)
 
+  // Only write out the `.sindriignore` file if it doesn't already exist.
+  let sindriIgnoreExists: boolean
+  try {
+    await plugin.call('fileManager', 'readFile', './.sindriignore')
+    sindriIgnoreExists = true
+  } catch {
+    sindriIgnoreExists = false
+  }
+  if (!sindriIgnoreExists) {
+    await plugin.call('fileManager', 'writeFile',
+      '.sindriignore',
+      // @ts-ignore
+      (await import('raw-loader!./.sindriignore')).default)
+  }
+
   // Only write out the `sindri.json` file if it doesn't already exist.
   let sindriJsonExists: boolean
   try {
