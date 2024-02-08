ba45e114b4511bccfce69f627b075bf8b424c852
Evan Sangaline <esangaline@gmail.com>
2024-02-08 14:18:01 -0600

    Don't overwrite `sindri.json` if it exists.

--- libs/remix-ws-templates/src/script-templates/sindri/index.ts
+++ libs/remix-ws-templates/src/script-templates/sindri/index.ts
@@ -1,15 +1,23 @@
 export const sindriScripts = async (plugin) => {
-    await plugin.call('fileManager', 'writeFile', 
-      'scripts/sindri/sindri.ts' ,
-      // @ts-ignore
-      (await import('!!raw-loader!./sindri.ts')).default)
+  await plugin.call('fileManager', 'writeFile',
+    'scripts/sindri/sindri.ts' ,
+    // @ts-ignore
+    (await import('!!raw-loader!./sindri.ts')).default)
 
-    
-    await plugin.call('fileManager', 'writeFile', 
-      'sindri.json' ,
+  // Only write out the `sindri.json` file if it doesn't already exist.
+  let sindriJsonExists: boolean
+  try {
+    await plugin.call('fileManager', 'readFile', './fake.json')
+    sindriJsonExists = true
+  } catch {
+    sindriJsonExists = false
+  }
+  if (!sindriJsonExists) {
+    await plugin.call('fileManager', 'writeFile',
+      'sindri.json',
       // @ts-ignore
       (await import('raw-loader!./sindri.conf')).default)
-      
-      
-    await plugin.call('fileManager', 'open', 'scripts/sindri/sindri.ts')
   }
+
+  await plugin.call('fileManager', 'open', 'scripts/sindri/sindri.ts')
+}
