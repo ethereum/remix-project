f31c688a897b4fbf805f9df2a5033cd6216d4e38
bunsenstraat <bunsenstraat@gmail.com>
2024-02-22 07:57:18 +0000

    lint

--- libs/remix-ui/xterm/src/lib/reducer/index.ts
+++ libs/remix-ui/xterm/src/lib/reducer/index.ts
@@ -10,60 +10,60 @@ export const xTerminInitialState: xTerminalUiState = {
 
 export const xtermReducer = (state = xTerminInitialState, action: Actions) => {
   switch (action.type) {
-    case 'ENABLE_TERMINALS':
-      return {
-        ...state,
-        terminalsEnabled: true
-      }
-    case 'DISABLE_TERMINALS':
-      return {
-        ...state,
-        terminalsEnabled: false
-      }
-    case 'ADD_TERMINAL':
-      return {
-        ...state,
-        terminals: [...state.terminals, action.payload]
-      }
-    case 'HIDE_TERMINAL':
-      return {
-        ...state,
-        terminals: state.terminals.map(terminal => terminal.pid === action.payload ? { ...terminal, hidden: true } : terminal)
-      }
-    case 'SHOW_TERMINAL':
-      return {
-        ...state,
-        terminals: state.terminals.map(terminal => terminal.pid === action.payload ? { ...terminal, hidden: false } : terminal)
-      }
-    case 'HIDE_ALL_TERMINALS':
-      return {
-        ...state,
-        terminals: state.terminals.map(terminal => ({ ...terminal, hidden: true }))
-      }
-    case 'REMOVE_TERMINAL':
-      const removed = state.terminals.filter(xtermState => xtermState.pid !== action.payload)
-      if (removed.length > 0)
-            removed[removed.length - 1].hidden = false
-      return {
-        ...state,
-        terminals: removed
-      }
-    case 'ADD_SHELLS':
-      return {
-        ...state,
-        shells: action.payload
-      }
-    case 'SHOW_OUTPUT':
-      return {
-        ...state,
-        showOutput: action.payload
-      }
-    case 'SET_WORKING_DIR':
-      return {
-        ...state,
-        workingDir: action.payload
-      }
-    default:
-      return state
+  case 'ENABLE_TERMINALS':
+    return {
+      ...state,
+      terminalsEnabled: true
+    }
+  case 'DISABLE_TERMINALS':
+    return {
+      ...state,
+      terminalsEnabled: false
+    }
+  case 'ADD_TERMINAL':
+    return {
+      ...state,
+      terminals: [...state.terminals, action.payload]
+    }
+  case 'HIDE_TERMINAL':
+    return {
+      ...state,
+      terminals: state.terminals.map(terminal => terminal.pid === action.payload ? { ...terminal, hidden: true } : terminal)
+    }
+  case 'SHOW_TERMINAL':
+    return {
+      ...state,
+      terminals: state.terminals.map(terminal => terminal.pid === action.payload ? { ...terminal, hidden: false } : terminal)
+    }
+  case 'HIDE_ALL_TERMINALS':
+    return {
+      ...state,
+      terminals: state.terminals.map(terminal => ({ ...terminal, hidden: true }))
+    }
+  case 'REMOVE_TERMINAL':
+    const removed = state.terminals.filter(xtermState => xtermState.pid !== action.payload)
+    if (removed.length > 0)
+      removed[removed.length - 1].hidden = false
+    return {
+      ...state,
+      terminals: removed
+    }
+  case 'ADD_SHELLS':
+    return {
+      ...state,
+      shells: action.payload
+    }
+  case 'SHOW_OUTPUT':
+    return {
+      ...state,
+      showOutput: action.payload
+    }
+  case 'SET_WORKING_DIR':
+    return {
+      ...state,
+      workingDir: action.payload
+    }
+  default:
+    return state
   }
 }
