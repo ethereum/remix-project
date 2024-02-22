dafe866cd4e52617e7f5384b8020cd4f77a24ccd
aniket-engg <aniketengg.225@gmail.com>
2024-02-22 15:39:30 +0530

    show saved on

--- libs/remix-ui/run-tab/src/lib/actions/actions.ts
+++ libs/remix-ui/run-tab/src/lib/actions/actions.ts
@@ -70,7 +70,7 @@ export const addInstance = (dispatch: React.Dispatch<any>, instance: { contractD
   dispatch(addNewInstance(instance))
 }
 
-export const addSavedInstance = (dispatch: React.Dispatch<any>, instance: { contractData?: ContractData, address: string, name: string, abi?: any, decodedResponse?: Record<number, any> }) => {
+export const addSavedInstance = (dispatch: React.Dispatch<any>, instance: { contractData?: ContractData, address: string, name: string, abi?: any, decodedResponse?: Record<number, any>, savedOn?: number }) => {
   instance.decodedResponse = {}
   dispatch(addNewSavedInstance(instance))
 }
