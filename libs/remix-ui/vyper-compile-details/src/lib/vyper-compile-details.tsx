675066c11c37a7d23e34bfeb775d362a17e8b7ba
Joseph Izang <josephizang@gmail.com>
2024-02-23 23:54:09 +0100

    clean console log

--- libs/remix-ui/vyper-compile-details/src/lib/vyper-compile-details.tsx
+++ libs/remix-ui/vyper-compile-details/src/lib/vyper-compile-details.tsx
@@ -11,7 +11,6 @@ interface RemixUiVyperCompileDetailsProps {
 
 export function RemixUiVyperCompileDetails({ payload, theme, themeStyle }: RemixUiVyperCompileDetailsProps) {
   const compileResult = payload['compileResult'] ?? {}
-  console.log('compileResult', compileResult, payload)
   const bcode = compileResult.bytecode ? compileResult.bytecode.object : ''
   const runtimeBcode = compileResult.runtimeBytecode ? compileResult.runtimeBytecode.object : ''
   const ir = compileResult.ir
