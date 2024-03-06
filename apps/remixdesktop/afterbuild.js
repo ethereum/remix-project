023f2b61cfb63c553c03e5aec6349a0e1122a016
filip mertens <filip.mertens@ethereum.org>
2024-03-06 13:00:27 +0100

    add zips

--- apps/remixdesktop/afterbuild.js
+++ apps/remixdesktop/afterbuild.js
@@ -9,7 +9,7 @@ exports.default = async function afterbuild(context) {
   console.log('AFTER BUILD', context);
 
   const artifactPaths = context.artifactPaths
-  const dmgs = artifactPaths.filter((dmg) => dmg.endsWith('.dmg')).map((dmg) => `'${dmg}'`)
+  const dmgs = artifactPaths.filter((dmg) => dmg.endsWith('.dmg') || dmg.endsWith('.zip')).map((dmg) => `'${dmg}'`)
   fs.writeFileSync('dmgs.json', JSON.stringify({ dmgs }, null, 2))
   
 
