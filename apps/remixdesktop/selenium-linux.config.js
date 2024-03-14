3523a5fc19f0fc244fb4a61c0bbdaaf11f325f88
bunsenstraat <filip@give.it>
2024-03-14 10:22:39 +0100

    fix linux testing

--- apps/remixdesktop/selenium-linux.config.js
+++ apps/remixdesktop/selenium-linux.config.js
@@ -2,14 +2,9 @@ module.exports = {
     baseURL: 'https://selenium-release.storage.googleapis.com',
     drivers: {
       chrome: {
-        version: '114.0.1788.0',
+        version: '116.0.5845.228',
         arch: process.arch,
         baseURL: 'https://chromedriver.storage.googleapis.com'
-      },
-      chromiumedge: {
-        version: '114.0.1788.0',
-        arch: process.arch,
-        baseURL: 'https://msedgedriver.azureedge.net/'
       }
     }
   }
