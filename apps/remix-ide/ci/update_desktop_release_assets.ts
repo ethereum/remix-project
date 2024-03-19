ea26819fd9f600a38f5c0b4e249f7c29202e78ce
bunsenstraat <filip.mertens@ethereum.org>
2024-03-19 07:47:24 +0100

    bunsenstraat

--- apps/remix-ide/ci/update_desktop_release_assets.ts
+++ apps/remix-ide/ci/update_desktop_release_assets.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 import YAML from 'yaml'
 import crypto from 'crypto'
 
-const owner = 'ethereum'
+const owner = 'bunsenstraat'
 const repo = 'remix-desktop'
 const headers = {
   'X-GitHub-Api-Version': '2022-11-28',
