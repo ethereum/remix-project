4074a49aa810122c4ab4696cc01a8bbafda1e587
aniket-engg <aniketengg.225@gmail.com>
2024-04-02 21:07:10 +0530

    check initial text

--- apps/remix-ide-e2e/src/tests/pinned_contracts.test.ts
+++ apps/remix-ide-e2e/src/tests/pinned_contracts.test.ts
@@ -7,4 +7,10 @@ module.exports = {
   before: function (browser: NightwatchBrowser, done: VoidFunction) {
     init(browser, done)
   },
+  'Should show text in pinned contracts section #group1': function (browser: NightwatchBrowser) {
+    browser
+      .clickLaunchIcon('udapp')
+      .assert.elementPresent('*[data-id="NoPinnedInstanceText"]')
+      .assert.textContains('*[data-id="NoPinnedInstanceText"]', 'No pinned contracts found for selected workspace & network')
+  }
 }
