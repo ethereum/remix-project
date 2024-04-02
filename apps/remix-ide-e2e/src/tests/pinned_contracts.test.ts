e8f6a8e83eecf1735329a7675e1d1c69cf41283f
aniket-engg <aniketengg.225@gmail.com>
2024-04-02 14:20:08 +0530

    pinned contracts e2e

---
+++ apps/remix-ide-e2e/src/tests/pinned_contracts.test.ts
@@ -0,0 +1,10 @@
+'use strict'
+import { NightwatchBrowser } from 'nightwatch'
+import init from '../helpers/init'
+
+module.exports = {
+  '@disabled': true,
+  before: function (browser: NightwatchBrowser, done: VoidFunction) {
+    init(browser, done)
+  },
+}
