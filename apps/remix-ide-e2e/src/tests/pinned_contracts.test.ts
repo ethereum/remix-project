0af10acb88b6ca1da59a935b477c587b6dfdf72b
aniket-engg <aniketengg.225@gmail.com>
2024-04-03 14:13:32 +0530

    pinning e2e

--- apps/remix-ide-e2e/src/tests/pinned_contracts.test.ts
+++ apps/remix-ide-e2e/src/tests/pinned_contracts.test.ts
@@ -12,5 +12,19 @@ module.exports = {
       .clickLaunchIcon('udapp')
       .assert.elementPresent('*[data-id="NoPinnedInstanceText"]')
       .assert.textContains('*[data-id="NoPinnedInstanceText"]', 'No pinned contracts found for selected workspace & network')
-  }
+  },
+  'Deploy & pin contract #group1': function (browser: NightwatchBrowser) {
+    browser
+      .clickLaunchIcon('filePanel')
+      .waitForElementVisible('*[data-id="treeViewLitreeViewItemcontracts"]')
+      .click('*[data-id="treeViewLitreeViewItemcontracts]')
+      .click('*[data-id="treeViewLitreeViewItemcontracts/1_Storage.sol"]')
+      .clickLaunchIcon('udapp')
+      .click('*[data-id="Deploy - transact (not payable)"]')
+      .waitForElementPresent('*[data-shared="universalDappUiInstance"]')
+      .click('*[data-id="universalDappUiUdappPin"]')
+      .assert.elementPresent('*[data-id="deployAndRunNoInstanceText"]')
+      .assert.textContains('*[data-id="deployAndRunNoInstanceText"]', 'Currently you have no unpinned contracts to interact with.')
+      .assert.elementNotPresent('*[data-id="NoPinnedInstanceText"]')
+  },
 }
