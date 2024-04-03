7e362256f0cd0c29fd8d7dd02357738592712cdf
yann300 <yann.levreau@gmail.com>
2024-04-03 23:21:05 +0200

    fix chrome driver

--- apps/remix-ide-e2e/nightwatch.ts
+++ apps/remix-ide-e2e/nightwatch.ts
@@ -37,7 +37,7 @@ module.exports = {
           args: [
             'window-size=2560,1440',
             '--no-sandbox',
-            '--headless',
+            '--headless=new',
             '--verbose',
             '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
           ],
