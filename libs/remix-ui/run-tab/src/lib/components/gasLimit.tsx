0058a45cd8c67fde8e49d8c36ed8281eae649419
yann300 <yann.levreau@gmail.com>
2024-04-10 15:18:02 +0200

    fix gasLimit default and allow still using gas estimate

---
+++ libs/remix-ui/run-tab/src/lib/components/gasLimit.tsx
@@ -0,0 +1,44 @@
+// eslint-disable-next-line no-use-before-define
+import {CustomTooltip} from '@remix-ui/helper'
+import React, { useEffect, useRef } from 'react'
+import {FormattedMessage} from 'react-intl'
+import {GasPriceProps} from '../types'
+
+const defaultGasLimit = 500000
+export function GasLimitUI(props: GasPriceProps) {
+  const auto = useRef(true)
+  const inputComponent = useRef<HTMLInputElement>(null)
+  const currentGasLimit = useRef(defaultGasLimit)
+
+  useEffect(() => {
+    handleGasLimitAuto(true)
+  }, [])
+
+  const handleGasLimit = (e) => {
+    props.setGasFee(e.target.value)
+  }
+
+  const handleGasLimitAuto = (checked) => {
+    auto.current = checked
+    if (checked) {
+      currentGasLimit.current = parseInt(inputComponent.current.value)
+      props.setGasFee(0)
+    } else {
+      props.setGasFee(currentGasLimit.current)
+    }    
+  }
+
+  return (
+    <div className="udapp_crow">
+      <label className="udapp_settingsLabel">
+        <FormattedMessage id="udapp.gasLimit" />
+      </label>
+      <div className='custom-control custom-checkbox udapp_col2 udapp_gasNval'>
+        <input type="checkbox" className="" id="gasLimitAuto" checked={auto.current} onChange={(e) => { handleGasLimitAuto(e.target.checked) } } />
+        <CustomTooltip placement={'right'} tooltipClasses="text-nowrap" tooltipId="remixGasPriceTooltip" tooltipText={<FormattedMessage id="udapp.tooltipText4" />}>
+          <input type="number" ref={inputComponent} disabled={auto.current} className="form-control" id="gasLimit" value={props.gasLimit === 0 ? currentGasLimit.current : props.gasLimit} onChange={handleGasLimit} />        
+        </CustomTooltip>        
+      </div>      
+    </div>
+  )
+}
