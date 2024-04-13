97a0fb7ef7c8dfc50e4ff73accc6a1efedb3ae63
bunsenstraat <filip@give.it>
2024-04-13 11:50:20 +0200

    mv scripts

--- chrome.sh
+++ chrome.sh
@@ -1,6 +1,5 @@
         google-chrome --version > version.txt
         VERSION=$(grep -Eo '[0-9]+\.' < version.txt | head -1 | cut -d'.' -f1)
         echo "CHROME DRIVER INSTALL $VERSION"
-        yarn add -D chromedriver@$VERSION
+        yarn add -D chromedriver@$VERSION geckodriver
         rm version.txt
-        yarn add -D geckodriver
