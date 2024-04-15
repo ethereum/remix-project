ecd8bea1013b50d925f5ecb4556d0d79cc9227e0
ioedeveloper <daviddisu8@gmail.com>
2024-04-15 14:43:40 +0100

    Update IPFS urls

--- libs/remix-ws-templates/src/templates/rln/scripts/plonk/plonk_trusted_setup.ts
+++ libs/remix-ws-templates/src/templates/rln/scripts/plonk/plonk_trusted_setup.ts
@@ -6,7 +6,7 @@ const snarkjs = require('snarkjs');
     // @ts-ignore
     await remix.call('circuit-compiler', 'generateR1cs', 'circuits/rln.circom');
 
-    const ptau_final = "https://ipfs-cluster.ethdevops.io/ipfs/QmTiT4eiYz5KF7gQrDsgfCSTRv3wBPYJ4bRN1MmTRshpnW";
+    const ptau_final = "https://ipfs-cluster.ethdevops.io/ipfs/QmciCq5JcZQyTLvC9GRanrLBi82ZmSriq1Fr5zANkGHebf";
     // @ts-ignore
     const r1csBuffer = await remix.call('fileManager', 'readFile', 'circuits/.bin/rln.r1cs', { encoding: null });
     // @ts-ignore
