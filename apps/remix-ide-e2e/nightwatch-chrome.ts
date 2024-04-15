9d69b9434c24ee09606a1d2d5c9155b38f3571a1
bunsenstraat <bunsenstraat@gmail.com>
2024-04-15 13:00:26 +0200

    Update nightwatch-chrome.ts

--- apps/remix-ide-e2e/nightwatch-chrome.ts
+++ apps/remix-ide-e2e/nightwatch-chrome.ts
@@ -56,8 +56,7 @@ module.exports = {
         'javascriptEnabled': true,
         'acceptSslCerts': true,
         'goog:chromeOptions': {
-          args: ['window-size=2560,1440', '--no-sandbox', '--verbose'],
-          extensions: [metamaskExtension]
+          args: ['window-size=2560,1440', '--no-sandbox', '--verbose']
         }
       }
     },
