36a7accb7597e1d7cdf56c4f708b08f2ef86ae84
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-04-19 10:28:37 +0200

    added timer to avoid rate limiter

--- apps/remix-endpoints/test/solcoder.spec.ts
+++ apps/remix-endpoints/test/solcoder.spec.ts
@@ -33,7 +33,7 @@ describe('solcoder', () => {
       timeout: 10000,  // total request time including connection setup
     };
 
-    const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
+	const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
     console.log(response.data);
     // TODO: add more assertions
     chai.expect(response.data.data[0]).not.to.be.empty;
@@ -41,125 +41,132 @@ describe('solcoder', () => {
   });
 
   it("Generate code from user comment", async () =>{
-    const postData = {
+    setTimeout( async () => {
+      const postData = {
       data: [
         `// SPDX-License-Identifier: GPL-3.0
-		pragma solidity >=0.8.2 <0.9.0;
-		contract Storage {
-			/// function \`StringEncode\` for encoding a string with abi.encode
-			`,
+        pragma solidity >=0.8.2 <0.9.0;
+        contract Storage {
+          /// function \`StringEncode\` for encoding a string with abi.encode
+          `,
         "code_completion",
-		"",
+        "",
         false,
         50,
         0.9,
         0.92,
         50
       ]
-    };
+      };
 
-    const config = {
+      const config = {
       headers: {
         'Content-Type': 'application/json'
       },
       timeout: 10000,  // total request time including connection setup
-    };
-
-    const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
-    console.log(response.data);
-	// TODO: add more assertions
-	
-    chai.expect(response.data.data[0]).not.to.be.empty;
-    chai.expect(response.data.data[0]).to.contains('function')
-    chai.expect(response.data.data[0]).to.contains('StringEncode')
-    chai.expect(response.data.is_generating).to.equal(false);
+      };
+      
+      const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
+      console.log(response.data);
+      // TODO: add more assertions
+      
+      chai.expect(response.data.data[0]).not.to.be.empty;
+      chai.expect(response.data.data[0]).to.contains('function')
+      chai.expect(response.data.data[0]).to.contains('StringEncode')
+      chai.expect(response.data.is_generating).to.equal(false);
+    }, 11000)
   });
 
+
   it("Explain user code using context", async () =>{
-    const postData = {
-		data: [
-		  `function store(uint256 num) public {
-			  number = num;
-		  }`,
-		  "code_generation",
-		  false,
-		  20,
-		  0.9,
-		  0.92,
-		  50,
-		  `/ SPDX-License-Identifier: GPL-3.0
-  
-		  pragma solidity >=0.8.2 <0.9.0;
-		  
-		  /**
-		   * @title Storage
-		   * @dev Store & retrieve value in a variable
-		   * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
-		   */
-		  contract Storage {
-		  
-			  uint256 number;
-		  
-			  /**
-			   * @dev Store value in variable
-			   * @param num value to store
-			   */
-			  function store(uint256 num) public {
-				  number = num;
-			  }
-
-			  /**
-			   * @dev Return value 
-			   * @return value of 'number'
-			   */
-			  function retrieve() public view returns (uint256){
-				  return number;
-			  }
-		  }`
-		]
-	  };
-  
-	  const config = {
-		headers: {
-		  'Content-Type': 'application/json'
-		},
-		timeout: 10000,  // total request time including connection setup
-	  };
-  
-	  const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
-	  console.log(response.data);
-	  // TODO: add more assertions
-	  chai.expect(response.data.data[0]).not.to.be.empty;
-	  chai.expect(response.data.is_generating).to.equal(false);
-	});
-
-  it("Explain user code without using any context", async () =>{
-    const postData = {
+    setTimeout( async () => {
+      const postData = {
       data: [
         `function store(uint256 num) public {
-			number = num;
-		}`,
-        "code_explaining",
+          number = num;
+        }`,
+        "code_generation",
         false,
         20,
         0.9,
         0.92,
         50,
-		""
+        `/ SPDX-License-Identifier: GPL-3.0
+    
+        pragma solidity >=0.8.2 <0.9.0;
+        
+        /**
+         * @title Storage
+         * @dev Store & retrieve value in a variable
+         * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+         */
+        contract Storage {
+        
+          uint256 number;
+        
+          /**
+           * @dev Store value in variable
+           * @param num value to store
+           */
+          function store(uint256 num) public {
+            number = num;
+          }
+
+          /**
+           * @dev Return value 
+           * @return value of 'number'
+           */
+          function retrieve() public view returns (uint256){
+            return number;
+          }
+        }`
       ]
     };
 
     const config = {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      timeout: 10000,  // total request time including connection setup
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    timeout: 10000,  // total request time including connection setup
     };
 
     const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
     console.log(response.data);
+    // TODO: add more assertions
     chai.expect(response.data.data[0]).not.to.be.empty;
     chai.expect(response.data.is_generating).to.equal(false);
+    }, 22000)
+  });
+
+  it("Explain user code without using any context", async () =>{
+    setTimeout( async () => {
+      const postData = {
+        data: [
+          `function store(uint256 num) public {
+        number = num;
+      }`,
+          "code_explaining",
+          false,
+          20,
+          0.9,
+          0.92,
+          50,
+      ""
+        ]
+      };
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        timeout: 10000,  // total request time including connection setup
+      };
+    
+    const response = await axiosInstance.post(`${API_URL}/solcoder/`, postData, config)
+      console.log(response.data);
+      chai.expect(response.data.data[0]).not.to.be.empty;
+      chai.expect(response.data.is_generating).to.equal(false);
+    }, 33000)
   });
 
 })
