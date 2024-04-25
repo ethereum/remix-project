90b057779f72a2b8be1fec9d541271c2ec80a5b3
filip mertens <filip.mertens@ethereum.org>
2024-04-25 08:35:28 +0200

    test script

--- apps/remixdesktop/run_ci_test.sh
+++ apps/remixdesktop/run_ci_test.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -e
-
+TEST_EXITCODE=0
 yarn run build:e2e && node ./splice_tests.js
 TESTFILES=$(node ./splice_tests.js | circleci tests split --split-by=timings)
 for TESTFILE in $TESTFILES; do
     yarn run test --test ./build-e2e/remixdesktop/test/tests/app/${TESTFILE} || TEST_EXITCODE=1
 done
+
+if [ "$CIRCLE_NODE_INDEX" -eq 0 ]; then
+  yarn test:isogit
+elif [ "$CIRCLE_NODE_INDEX" -eq 1 ]; then
+  yarn test:offline
+fi
 
 echo "$TEST_EXITCODE"
 if [ "$TEST_EXITCODE" -eq 1 ]
