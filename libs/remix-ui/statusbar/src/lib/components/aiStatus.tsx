3abeda0fc1743cd16f5fc02e0f474e49b8844e61
Joseph Izang <josephizang@gmail.com>
2024-05-10 13:22:58 +0200

    add components for statusbar and style

---
+++ libs/remix-ui/statusbar/src/lib/components/aiStatus.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AIStatus() {
+  return (
+    <div className="d-flex flex-row p-1 text-white justify-content-center align-items-center">
+      <span className="fa-regular fa-microchip-ai ml-1"></span>
+      <span className="small mx-1">Remix Copilot</span>
+    </div>
+  )
+}
