2871478623fb4344d4ffd1f134157ce067c24f48
lianahus <liana@ethereum.org>
2024-05-28 18:25:27 +0200

    fixes in Grid view and updates for remix guide

--- libs/remix-ui/grid-view/src/lib/filtersContext.tsx
+++ libs/remix-ui/grid-view/src/lib/filtersContext.tsx
@@ -3,9 +3,10 @@ import React, { createContext, useState, useContext } from 'react';
 interface FilterContextType {
   showUntagged: boolean
   showPin: boolean
-  keyValueMap: Record<string, { enabled: boolean; color: string; }>;
+  keyValueMap: Record<string, { enabled: boolean; color: string; }>
   updateValue: (key: string, enabled: boolean, color: string) => void
   addValue: (key: string, enabled: boolean, color: string) => void
+  filter: string
 }
 const FiltersContext = createContext<FilterContextType>({
   showUntagged: false,
@@ -13,6 +14,7 @@ const FiltersContext = createContext<FilterContextType>({
   keyValueMap: {},
   updateValue: () => {},
   addValue: () => {},
+  filter: ""
 });
 
 export default FiltersContext
