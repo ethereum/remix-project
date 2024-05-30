a504053f2d678060c4cf5c90f8ed06be5f2eefdd
Joseph Izang <josephizang@gmail.com>
2024-05-30 15:58:29 +0100

    basic tooltip to sections

--- libs/remix-ui/statusbar/src/lib/components/scamAlertStatus.tsx
+++ libs/remix-ui/statusbar/src/lib/components/scamAlertStatus.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { ExtendedRefs, ReferenceType } from '@floating-ui/react'
+import { CustomTooltip } from '@remix-ui/helper'
 
 export interface ScamAlertStatusProps {
   refs: ExtendedRefs<ReferenceType>
@@ -11,12 +12,16 @@ export default function ScamAlertStatus ({ refs, getReferenceProps }: ScamAlertS
 
   return (
     <>
-      <div className="mr-2 d-flex align-items-center justify-content-center" id="hTScamAlertSection" ref={refs.setReference} {...getReferenceProps()}>
-        <span className="pr-2 far fa-exclamation-triangle text-danger"></span>
-        <span className="text-white font-semibold small">
-          <FormattedMessage id="home.scamAlert" />
-        </span>
-      </div>
+      <CustomTooltip
+        tooltipText={"Scam Alerts"}
+      >
+        <div className="mr-2 d-flex align-items-center justify-content-center" id="hTScamAlertSection" ref={refs.setReference} {...getReferenceProps()}>
+          <span className="pr-2 far fa-exclamation-triangle text-danger"></span>
+          <span className="text-white font-semibold small">
+            <FormattedMessage id="home.scamAlert" />
+          </span>
+        </div>
+      </CustomTooltip>
     </>
   )
 }
