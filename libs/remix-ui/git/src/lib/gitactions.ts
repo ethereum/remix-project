a403c9f9811cdb1461063ea3602d379ed1024afa
filip mertens <filip.mertens@ethereum.org>
2024-06-04 10:46:06 +0200

    ifx bug

--- libs/remix-ui/git/src/lib/gitactions.ts
+++ libs/remix-ui/git/src/lib/gitactions.ts
@@ -218,6 +218,9 @@ export const addall = async (files: fileStatusResult[]) => {
 
 export const add = async (filepath: addInput) => {
   try {
+    if(typeof filepath.filepath === "string") {
+      filepath.filepath = removeSlash(filepath.filepath)
+    }
     await plugin.call('dgitApi', 'add', filepath);
     sendToGitLog({
       type: 'success',
