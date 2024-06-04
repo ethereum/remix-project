a563e18b657d7625d5539807be685cab4618530a
Joseph Izang <josephizang@gmail.com>
2024-06-04 12:30:55 +0100

    add pause

--- apps/remix-ide-e2e/src/tests/homeTab.test.ts
+++ apps/remix-ide-e2e/src/tests/homeTab.test.ts
@@ -49,6 +49,7 @@ module.exports = {
       .waitForElementVisible('*[data-id$="fileExplorerTreeItemInput"]')
       .sendKeys('*[data-id$="fileExplorerTreeItemInput"]', 'HometabNewFile.txt')
       .sendKeys('*[data-id$="fileExplorerTreeItemInput"]', browser.Keys.ENTER)
+      .pause(2000)
       .waitForElementVisible('*[data-id="treeViewLitreeViewItemHometabNewFile.txt"]', 7000)
   }
 }
