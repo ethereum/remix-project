923924164c01a80702ad2ed232f0424e8846aacf
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-06-04 11:43:00 +0200

    fixed capital letters on ask

--- libs/remix-ui/renderer/src/lib/renderer.tsx
+++ libs/remix-ui/renderer/src/lib/renderer.tsx
@@ -111,7 +111,7 @@ export const Renderer = ({ message, opt = {}, plugin }: RendererProps) => {
               onClick={() => { askGtp() }}
               style={{ borderColor: "var(--ai)" }}
             >
-              ASK Remix AI
+              Ask Remix AI
             </span>
 
           </div>
