2ee3fce8823019990b22b96b87af9317272c9930
aniket-engg <aniketengg.225@gmail.com>
2024-06-06 14:05:43 +0530

    show remediation

--- libs/remix-ui/run-tab/src/lib/components/solScanTable.tsx
+++ libs/remix-ui/run-tab/src/lib/components/solScanTable.tsx
@@ -13,14 +13,15 @@ export function SolScanTable(props: SolScanTableProps) {
   return (
     <>
     <p>Scanning successful! <b>{scanDetails.length} warnings </b> found for file: <b>{fileName}</b></p>
-    <p>See the warning details below. For more details, <a href="https://solidityscan.com/signup">Sign up with SolidityScan</a></p>
+    <p>See the warning details below. For more details, <a href="https://solidityscan.com/signup" target='blank'>Sign up with SolidityScan</a></p>
     <table className="table table-bordered table-hover">
       <thead>
         <tr>
-          <td scope="col">ID</td>
           <td scope="col">NAME</td>
           <td scope="col">SEVERITY</td>
+          <td scope="col">CONFIDENCE</td>
           <td scope="col">DESCRIPTION</td>
+          <td scope="col">REMEDIATION</td>
         </tr>
       </thead>
       <tbody>
@@ -28,10 +29,11 @@ export function SolScanTable(props: SolScanTableProps) {
           Array.from(scanDetails, (template) => {
             return (
               <tr key={template.template_details.issue_id}>
-                <td scope="col">{template.template_details.issue_id}</td>
                 <td scope="col">{template.template_details.issue_name}</td>
                 <td scope="col">{template.template_details.issue_severity}</td>
+                <td scope="col">{template.template_details.issue_confidence}</td>
                 <td scope="col">{parse(template.template_details.static_issue_description)}</td>
+                <td scope="col">{template.template_details.issue_remediation ? parse(template.template_details.issue_remediation) : 'Not Available' }</td>
               </tr>
             )
           })
