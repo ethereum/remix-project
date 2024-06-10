14337c1cca818b4d45b483b6c753f5749e6f6f58
aniket-engg <aniketengg.225@gmail.com>
2024-06-10 17:17:52 +0530

    linting fix

--- libs/remix-ui/run-tab/src/lib/components/solScanTable.tsx
+++ libs/remix-ui/run-tab/src/lib/components/solScanTable.tsx
@@ -17,11 +17,11 @@ export function SolScanTable(props: SolScanTableProps) {
       <table className="table table-bordered table-hover">
         <thead>
           <tr>
-            <td scope="col" style={{wordBreak: "keep-all"}}>NAME</td>
-            <td scope="col" style={{wordBreak: "keep-all"}}>SEVERITY</td>
-            <td scope="col" style={{wordBreak: "keep-all"}}>CONFIDENCE</td>
-            <td scope="col" style={{wordBreak: "keep-all"}}>DESCRIPTION</td>
-            <td scope="col" style={{wordBreak: "keep-all"}}>REMEDIATION</td>
+            <td scope="col" style={{ wordBreak: "keep-all" }}>NAME</td>
+            <td scope="col" style={{ wordBreak: "keep-all" }}>SEVERITY</td>
+            <td scope="col" style={{ wordBreak: "keep-all" }}>CONFIDENCE</td>
+            <td scope="col" style={{ wordBreak: "keep-all" }}>DESCRIPTION</td>
+            <td scope="col" style={{ wordBreak: "keep-all" }}>REMEDIATION</td>
           </tr>
         </thead>
         <tbody>
