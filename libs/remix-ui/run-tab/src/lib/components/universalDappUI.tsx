d8d78d4169280f1251b6145b293e54c727480d8c
lianahus <liana@ethereum.org>
2024-06-11 10:40:01 +0200

    added proper link for solidityscan

--- libs/remix-ui/run-tab/src/lib/components/universalDappUI.tsx
+++ libs/remix-ui/run-tab/src/lib/components/universalDappUI.tsx
@@ -295,7 +295,6 @@ export function UniversalDappUI(props: UdappProps) {
           }
           await props.plugin.call('notification', 'modal', modal)
         }
-
       })
     }
   }
@@ -305,7 +304,10 @@ export function UniversalDappUI(props: UdappProps) {
     const modal: AppModal = {
       id: 'SolidityScanPermissionHandler',
       title: <FormattedMessage id="udapp.solScan.modalTitle" />,
-      message: <FormattedMessage id="udapp.solScan.modalMessage" />,
+      message: <div className='d-flex flex-column'>
+        <FormattedMessage id="udapp.solScan.modalMessage" />
+        <a href={'https://solidityscan.com'} target="_blank" >Learn more</a>
+      </div>,
       okLabel: <FormattedMessage id="udapp.solScan.modalOkLabel" />,
       okFn: handleScanContinue,
       cancelLabel: <FormattedMessage id="udapp.solScan.modalCancelLabel" />
