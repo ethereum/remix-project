429ecde2a1f31c792fa1acdcdfacf1a7d8e605a8
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-06-12 13:52:15 +0200

    Add ThemeType

--- apps/contract-verification/src/app/app.tsx
+++ apps/contract-verification/src/app/app.tsx
@@ -3,10 +3,18 @@ import React, {useState, useEffect, useRef} from 'react'
 import {AppContext} from './AppContext'
 import DisplayRoutes from './routes'
 
+import {ThemeType} from './types'
+
 import './App.css'
 
 const App = () => {
-  return <DisplayRoutes />
+  const [themeType, setThemeType] = useState<ThemeType>('dark')
+
+  return (
+    <AppContext.Provider value={{themeType, setThemeType}}>
+      <DisplayRoutes />
+    </AppContext.Provider>
+  )
 }
 
 export default App
