a37322cbce7cfbd8e53accc17be73b2ef07779e5
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-06-12 14:02:08 +0200

    Add pluginClient to fix unstyled Plugin
    Only after adding the PluginClient (that imports @remixproject/plugin-webview) the
    contract-verification-plugin started to inherit Remix styles

--- apps/contract-verification/src/app/app.tsx
+++ apps/contract-verification/src/app/app.tsx
@@ -1,11 +1,15 @@
 import React, {useState, useEffect, useRef} from 'react'
 
+import {ContractVerificationPluginClient} from './ContractVerificationPluginClient'
+
 import {AppContext} from './AppContext'
 import DisplayRoutes from './routes'
-
+import {CustomTooltip} from '@remix-ui/helper'
 import {ThemeType} from './types'
 
 import './App.css'
+
+const plugin = new ContractVerificationPluginClient()
 
 const App = () => {
   const [themeType, setThemeType] = useState<ThemeType>('dark')
