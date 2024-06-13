0386b7aeb3c1c4106d063be17d728af81c9e03dd
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-06-13 08:55:00 +0200

    Add Verify form items. Fetch and format chains form chainid.network/chains.json

---
+++ apps/contract-verification/src/app/components/Input/Dropdown/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+interface DropdownItem {
+  value: string
+  text: string
+}
+
+interface DropdownProps {
+  label: string
+  items: DropdownItem[]
+  id: string
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({label, items, id}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{label}</label>
+      <select className="form-control custom-select pr-4" id={id}>
+        {items.map((item, index) => (
+          <option value={item.value} key={index}>
+            {item.text}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
