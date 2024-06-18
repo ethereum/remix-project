45d55d6de4cc024716d0af636e416540e929119e
yann300 <yann.levreau@gmail.com>
2024-06-18 21:45:14 +0200

    fix test

--- apps/remix-ide-e2e/src/tests/url.test.ts
+++ apps/remix-ide-e2e/src/tests/url.test.ts
@@ -342,9 +342,10 @@ module.exports = {
     browser
       .url('http://127.0.0.1:8080/#ghfolder=https://github.com/ethereum/remix-project/tree/master/apps/remix-ide/contracts/hardhat')
       .refreshPage()
+      .waitForElementVisible('*[data-id="treeViewLitreeViewItemcontracts"]', 40000)
       .currentWorkspaceIs('code-sample')
       .openFile('contracts')
-      .openFile('Lock.sol')
+      .openFile('contracts/Lock.sol')
       .getEditorValue((content) => {
         browser.assert.ok(content.indexOf('contract Lock {') !== -1, 'content does contain "contract Lock {"')
       })      
