b3f23d03b2c0b85b27cccbea6902c4a426f45fdf
Joseph Izang <josephizang@gmail.com>
2024-06-20 08:41:50 +0100

    pointer correction

--- libs/remix-ui/statusbar/src/lib/components/aiStatus.tsx
+++ libs/remix-ui/statusbar/src/lib/components/aiStatus.tsx
@@ -35,7 +35,7 @@ export default function AIStatus(props: AIStatusProps) {
     <CustomTooltip
       tooltipText={copilotActive ? "Remix Copilot activated" : "Remix Copilot disabled."}
     >
-      <div className="d-flex flex-row pr-2 text-white justify-content-center align-items-center">
+      <div className="d-flex flex-row pr-2 text-white justify-content-center align-items-center remixui_statusbar_aistatus">
         <span className={copilotActive === false ? "fa-regular fa-microchip-ai ml-1 text-white" : "fa-regular fa-microchip-ai ml-1"}></span>
         <span className={copilotActive === false ? "small mx-1 text-white semi-bold" : "small mx-1 semi-bold" }>Remix Copilot</span>
       </div>
