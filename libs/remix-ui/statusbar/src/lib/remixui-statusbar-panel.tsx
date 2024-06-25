a02b181516b45e45a38a419bd07aa1a8b303609d
Your Name <you@example.com>
2024-06-25 07:17:51 +0200

    try set window size

--- libs/remix-ui/statusbar/src/lib/remixui-statusbar-panel.tsx
+++ libs/remix-ui/statusbar/src/lib/remixui-statusbar-panel.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import GitStatus from './components/gitStatus'
 import AIStatus from './components/aiStatus'
 import ScamAlertStatus from './components/scamAlertStatus'
 import ScamDetails from './components/scamDetails'
 import { FloatingFocusManager, autoUpdate, flip, offset, shift, size, useClick, useDismiss, useFloating, useInteractions, useRole } from '@floating-ui/react'
 import axios from 'axios'
+import { appPlatformTypes, platformContext } from '@remix-ui/app'
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { StatusBar } from 'apps/remix-ide/src/app/components/status-bar'
 
@@ -22,6 +23,7 @@ export function RemixUIStatusBar({ statusBarPlugin }: RemixUIStatusBarProps) {
   const [scamAlerts, setScamAlerts] = useState<ScamAlert[]>([])
   const [gitBranchName, setGitBranchName] = useState('')
   const [isAiActive, setIsAiActive] = useState(false)
+  const platform = useContext(platformContext)
   const { refs, context, floatingStyles } = useFloating({
     open: showScamDetails,
     onOpenChange: setShowScamDetails,
@@ -73,7 +75,8 @@ export function RemixUIStatusBar({ statusBarPlugin }: RemixUIStatusBarProps) {
       )}
       <div className="d-flex remixui_statusbar_height flex-row bg-primary justify-content-between align-items-center">
         <div className="remixui_statusbar remixui_statusbar_gitstatus">
-          <GitStatus plugin={statusBarPlugin} gitBranchName={gitBranchName} setGitBranchName={setGitBranchName} />
+          {platform == appPlatformTypes.desktop ? <></> :
+            <GitStatus plugin={statusBarPlugin} gitBranchName={gitBranchName} setGitBranchName={setGitBranchName} />}
         </div>
         <div className="remixui_statusbar"></div>
         <div className="remixui_statusbar d-flex flex-row">
