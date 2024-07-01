8ba75251e6dd5d1dd7e69f310fc31636ba690710
Manuel Wedler <manuel@wedler.dev>
2024-07-01 18:06:57 +0200

    Make receipts view prettier

--- apps/contract-verification/src/app/views/ReceiptsView.tsx
+++ apps/contract-verification/src/app/views/ReceiptsView.tsx
@@ -5,15 +5,23 @@ import { AccordionReceipt } from '../components/AccordionReceipt'
 import { SubmittedContract } from '../types/VerificationTypes'
 
 export const ReceiptsView = () => {
+  const { chains } = React.useContext(AppContext)
   // const {submittedContracts} = React.useContext(AppContext);
+
+  const getChainName = (chainId: string) => {
+    return chains.find((chain) => chain.chainId === parseInt(chainId))?.name ?? 'Unknown Chain'
+  }
 
   const submittedContracts = example as unknown as Record<string,SubmittedContract>
   console.log('submittedContracts', submittedContracts)
   return (
-    <>
+    <div className="accordion" id="receiptsAccordion">
       {Object.values(submittedContracts).map((contract, index) => (
-        <AccordionReceipt contract={contract} index={index} />
+        <AccordionReceipt contract={contract} index={index} chainName={getChainName(contract.chainId)} />
       ))}
-    </>
+      {Object.values(submittedContracts).map((contract, index) => (
+        <AccordionReceipt contract={contract} index={index+1} chainName={getChainName(contract.chainId)} />
+      ))}
+    </div>
   )
 }
