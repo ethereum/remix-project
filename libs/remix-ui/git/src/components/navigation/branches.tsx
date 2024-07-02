f60342dfeda76658c3b41d1deb474995485b9b44
Joseph Izang <josephizang@gmail.com>
2024-07-02 13:29:50 +0100

    replace svg carets with i tags and css classes. fix padding

--- libs/remix-ui/git/src/components/navigation/branches.tsx
+++ libs/remix-ui/git/src/components/navigation/branches.tsx
@@ -20,10 +20,8 @@ export const BranchesNavigation = ({ eventKey, activePanel, callback }) => {
     <>
       <div className={'d-flex justify-content-between pt-1 ' + (activePanel === eventKey? 'bg-light': '')}>
         <span data-id='branches-panel' onClick={()=>handleClick()} role={'button'} className='nav d-flex justify-content-start align-items-center w-75'>
-          {
-            activePanel === eventKey ? <FontAwesomeIcon className='' icon={faCaretDown}></FontAwesomeIcon> : <FontAwesomeIcon className='' icon={faCaretRight}></FontAwesomeIcon>
-          }
-          <label className="pl-1 nav form-check-label">BRANCHES</label>
+          <i className={ activePanel !== eventKey ? "fa fa-caret-right" : "fa fa-caret-down" }></i>
+          <label className="pl-2 nav form-check-label">BRANCHES</label>
           <LoaderIndicator></LoaderIndicator>
         </span>
       </div>
