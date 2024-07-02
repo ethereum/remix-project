a0c29948edcb12f09d846e102633814e1a39e8b3
Joseph Izang <josephizang@gmail.com>
2024-07-02 15:59:24 +0100

    add spacing and moved fetch repo button

--- libs/remix-ui/git/src/components/panels/remotes.tsx
+++ libs/remix-ui/git/src/components/panels/remotes.tsx
@@ -30,9 +30,11 @@ export const Remotes = (props: RemotesProps) => {
 
   return (
     <>
-      <div data-id="remotes-panel-content">
+      <div data-id="remotes-panel-content" className="d-flex flex-column">
+        <RemotesImport plugin={props.plugin} />
+        <hr className="mt-0 border border-2" />
         {context.remotes && context.remotes.length ?
-          <>
+          <div>
 
             {context.remotes && context.remotes.map((remote, index) => {
 
@@ -40,18 +42,17 @@ export const Remotes = (props: RemotesProps) => {
                 <Remoteselect key={index} remote={remote}></Remoteselect>
               );
             })}
-          </> : <>No remotes</>}
-        <hr></hr>
+          </div> : <div>
+            <label className="text-uppercase">No remotes</label>
+          </div>}
 
-        <input placeholder="remote name" name='remotename' onChange={e => onRemoteNameChange(e.target.value)} value={remoteName} className="form-control mb-2" type="text" id="remotename" />
-        <input placeholder="remote url" name='remoteurl' onChange={e => onUrlChange(e.target.value)} value={url} className="form-control" type="text" id="remoteurl" />
+        <input placeholder="remote name" name='remotename' onChange={e => onRemoteNameChange(e.target.value)} value={remoteName} className="form-control mb-3" type="text" id="remotename" />
+        <input placeholder="remote url" name='remoteurl' onChange={e => onUrlChange(e.target.value)} value={url} className="form-control mb-3" type="text" id="remoteurl" />
 
         <button disabled={(remoteName && url) ? false : true} className='btn btn-primary mt-1 w-100' onClick={async () => {
           addRemote();
         }}>add remote</button>
-        <hr />
-        <RemotesImport plugin={props.plugin} />
-        <hr />
+        <hr className="mt-0 border border-2" />
       </div>
     </>)
 }
