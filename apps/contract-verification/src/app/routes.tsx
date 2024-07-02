2a42111e422f68776f8213b3b74da015191c7c8f
Manuel Wedler <manuel@wedler.dev>
2024-07-02 16:17:03 +0200

    Add Lookup view

--- apps/contract-verification/src/app/routes.tsx
+++ apps/contract-verification/src/app/routes.tsx
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { VerifyView } from './views'
 import { DefaultLayout } from './layouts'
 import { ReceiptsView } from './views/ReceiptsView'
+import { LookupView } from './views/LookupView'
 
 const DisplayRoutes = () => (
   <Router>
@@ -22,6 +23,15 @@ const DisplayRoutes = () => (
         element={
           <DefaultLayout from="/" title="Receipts" description="Check the verification statuses of contracts submitted for verification">
             <ReceiptsView />
+          </DefaultLayout>
+        }
+      />
+
+      <Route
+        path="/lookup"
+        element={
+          <DefaultLayout from="/" title="Lookup" description="Search for verified contracts and download them to Remix">
+            <LookupView />
           </DefaultLayout>
         }
       />
