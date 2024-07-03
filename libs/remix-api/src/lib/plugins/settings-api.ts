324158cd238df7e6b05e504cc552a71824855c59
bunsenstraat <filip.mertens@ethereum.org>
2024-07-03 08:23:44 +0200

    fix plugin api & statusbar

--- libs/remix-api/src/lib/plugins/settings-api.ts
+++ libs/remix-api/src/lib/plugins/settings-api.ts
@@ -3,6 +3,8 @@ import { StatusEvents } from '@remixproject/plugin-utils'
 export interface ISettings {
   events: {
     configChanged: () => void,
+    copilotChoiceUpdated: (isChecked: boolean) => void,
+    copilotChoiceChanged: (isChecked: boolean) => void,
   } & StatusEvents
   methods: {
     getGithubAccessToken(): string
