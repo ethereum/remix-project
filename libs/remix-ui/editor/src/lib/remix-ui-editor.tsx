0a18abe432e643dcc648dd9da3e4a4fc08dcae39
yann300 <yann.levreau@gmail.com>
2024-07-16 11:57:58 +0200

    linting

--- libs/remix-ui/editor/src/lib/remix-ui-editor.tsx
+++ libs/remix-ui/editor/src/lib/remix-ui-editor.tsx
@@ -697,7 +697,7 @@ export const EditorUI = (props: EditorUIProps) => {
             </div>
           ),
         }
-        props.plugin.call('notification', 'alert', modalContent)        
+        props.plugin.call('notification', 'alert', modalContent)
         pasteCodeRef.current = true
         _paq.push(['trackEvent', 'editor', 'onDidPaste', 'more_than_10_lines'])
       }
