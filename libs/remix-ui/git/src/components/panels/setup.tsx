777658c886aadd62a838dd472278737ae24bd8cc
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-07-16 13:40:38 +0530

    Linting fix

--- libs/remix-ui/git/src/components/panels/setup.tsx
+++ libs/remix-ui/git/src/components/panels/setup.tsx
@@ -1,32 +1,34 @@
-import React, {useEffect, useState} from 'react'
-import {GetDeviceCode} from '../github/devicecode'
-import {GitHubCredentials} from './githubcredentials'
+import React, { useState } from 'react'
+import { GetDeviceCode } from '../github/devicecode'
+import { GitHubCredentials } from './githubcredentials'
 
 export const Setup = () => {
   const [screen, setScreen] = useState(0)
 
-  if (screen === 0) {
-    return (
-      <div className="px-3">
-        <h6>SETUP</h6>
-        <div>
-          <div className="mt-1 mb-2">To ensure that your commits are properly attributed in Git, you need to configure a username and email address. These will be used to identify the author of the commit.</div>
-          <GetDeviceCode></GetDeviceCode>
-          <hr></hr>
-          <GitHubCredentials></GitHubCredentials>
+  return (
+    <>
+      {screen === 0 ? (
+        <div className="px-3">
+          <h6>SETUP</h6>
+          <div>
+            <div className="mt-1 mb-2">
+              To ensure that your commits are properly attributed in Git, you need to configure a username and email address. These will be used to identify the author of the commit.
+            </div>
+            <GetDeviceCode />
+            <hr />
+            <GitHubCredentials />
+          </div>
         </div>
-      </div>
-    )
-  } else if (screen === 1) {
-    return (
-      <div className="px-3">
-        <h5>SETUP</h5>
-        <h6>Step 2</h6>
-        <div>
-          To ensure that your commits are properly attributed in Git, you need to configure your username and email address.
-          <GitHubCredentials></GitHubCredentials>
+      ) : (
+        <div className="px-3">
+          <h5>SETUP</h5>
+          <h6>Step 2</h6>
+          <div>
+            To ensure that your commits are properly attributed in Git, you need to configure your username and email address.
+            <GitHubCredentials />
+          </div>
         </div>
-      </div>
-    )
-  }
+      )}
+    </>
+  )
 }
