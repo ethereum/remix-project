7b2ef742749e5c141c0e970fc439eac240561c50
Manuel Wedler <manuel@wedler.dev>
2024-07-24 11:22:47 +0200

    Remove unused code

--- apps/contract-verification/src/app/Receipts/props.ts
+++
@@ -1,9 +0,0 @@
-import { EtherscanVerifier } from '../Verifiers/EtherscanVerifier'
-import { SourcifyVerifier } from '../Verifiers/SourcifyVerifier'
-
-export interface ReceiptProps {
-  verifyPromise: Promise<any>
-  address: string
-  chainId: string
-  verifier: EtherscanVerifier | SourcifyVerifier
-}
