52593f53b5678f00120e735ad1fb2416c9cc181e
Manuel Wedler <manuel@wedler.dev>
2024-07-25 12:04:35 +0200

    Show message on ReceiptsView if no contracts were submitted

--- apps/contract-verification/src/app/views/ReceiptsView.tsx
+++ apps/contract-verification/src/app/views/ReceiptsView.tsx
@@ -4,12 +4,11 @@ import { AppContext } from '../AppContext'
 
 export const ReceiptsView = () => {
   const { submittedContracts } = useContext(AppContext)
+  const contracts = Object.values(submittedContracts).reverse()
 
   return (
     <div className="accordion" id="receiptsAccordion">
-      {Object.values(submittedContracts).reverse().map((contract, index) => (
-        <AccordionReceipt contract={contract} index={index} />
-      ))}
+      {contracts.length > 0 ? contracts.map((contract, index) => <AccordionReceipt contract={contract} index={index} />) : <div className="text-center mt-5">No contracts submitted for verification</div>}
     </div>
   )
 }
