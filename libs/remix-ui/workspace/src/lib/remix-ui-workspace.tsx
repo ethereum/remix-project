29512291dd40d92dbc37058278f47b15143fff70
Joseph Izang <josephizang@gmail.com>
2024-09-04 00:46:11 +0100

    create blank template code and markup

--- libs/remix-ui/workspace/src/lib/remix-ui-workspace.tsx
+++ libs/remix-ui/workspace/src/lib/remix-ui-workspace.tsx
@@ -41,6 +41,8 @@ export function Workspace() {
   const transparentRadioRef = useRef()
   const uupsRadioRef = useRef()
   const global = useContext(FileSystemContext)
+  const [counter, setCounter] = useState(0)
+
   const workspaceRenameInput = useRef()
   const intl = useIntl()
   const cloneUrlRef = useRef<HTMLInputElement>()
@@ -335,12 +337,69 @@ export function Workspace() {
     )
   }
 
-  const createBlankWorkspace = () => {
+  const createModalMessage = async (
+    defaultName: string,
+    gitConfigNotSet: boolean,
+    onChangeTemplateName: (name: string) => void,
+    onChangeInitGit: (name: string) => void) => {
+
+    return (
+      <>
+        <label id="wsName" className="form-check-label" style={{ fontWeight: 'bolder' }}>
+          <FormattedMessage id="filePanel.workspaceName" />
+        </label>
+        <input
+          type="text"
+          data-id="modalDialogCustomPromptTextCreate"
+          defaultValue={defaultName}
+          className="form-control"
+          onChange={(e) => onChangeTemplateName(e.target.value)}
+          onInput={(e) => onChangeTemplateName((e.target as any).value)}
+        />
+        <div className="d-flex py-2 align-items-center custom-control custom-checkbox">
+          <input
+            id="initGitRepository"
+            data-id="initGitRepository"
+            className="form-check-input custom-control-input"
+            type="checkbox"
+            disabled={gitConfigNotSet}
+            onChange={(e) => onChangeInitGit(e.target.value)}
+            onInput={(e) => onChangeInitGit((e.target as any).value)}
+          />
+          <label
+            htmlFor="initGitRepository"
+            data-id="initGitRepositoryLabel"
+            className="m-0 form-check-label custom-control-label udapp_checkboxAlign"
+            title={window._intl.formatMessage({ id: 'filePanel.initGitRepoTitle' })}
+          >
+            <FormattedMessage id="filePanel.initGitRepositoryLabel" />
+          </label>
+        </div>
+        {gitConfigNotSet ? (
+          <div className="text-warning">
+            <FormattedMessage id="filePanel.initGitRepositoryWarning" />
+          </div>
+        ) : (
+          <></>
+        )}
+      </>
+    )
+  }
+  const createBlankWorkspace = async () => {
+    const username = await global.plugin.call('settings', 'get', 'settings/github-user-name')
+    const email = await global.plugin.call('settings', 'get', 'settings/github-email')
+    const gitNotSet = !username || !email
+    const defaultName = await global.plugin.call('filePanel', 'getAvailableWorkspaceName', 'blank')
+    let workspace = defaultName
+    let gitInit = false
+    setCounter((previous) => {
+      return previous + 1
+    })
     global.modal(
       intl.formatMessage({ id: 'filePanel.workspace.createBlank' }),
-      <></>,
+      await createModalMessage('blank - 1', gitNotSet, (value) => { workspace = value }, (value) => gitInit = false),
       intl.formatMessage({ id: 'filePanel.ok' }),
-      () => global.dispatchCreateWorkspace('blank_workspace', 'blank', false),
+      () => global.dispatchCreateWorkspace(`blank - ${counter}`, 'blank', false),
       intl.formatMessage({ id: 'filePanel.cancel' })
     )
   }
