7af5547113101613589d4d669e71f491482b7d5b
yann300 <yann.levreau@gmail.com>
2024-09-03 11:35:43 +0200

    update web3.js import

--- apps/remix-ide/src/blockchain/providers/injected.ts
+++ apps/remix-ide/src/blockchain/providers/injected.ts
@@ -1,4 +1,4 @@
-import Web3 from 'web3'
+import { Web3 } from 'web3'
 import { hashPersonalMessage, isHexString, bytesToHex } from '@ethereumjs/util'
 import { ExecutionContext } from '../execution-context'
 
