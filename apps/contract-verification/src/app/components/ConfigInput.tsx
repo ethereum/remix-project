1773aa2c73bb4b65dffd2008d81260d87f9a2122
Manuel Wedler <manuel@wedler.dev>
2024-09-11 17:57:02 +0200

    Make settings view prettier

--- apps/contract-verification/src/app/components/ConfigInput.tsx
+++ apps/contract-verification/src/app/components/ConfigInput.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { CustomTooltip } from '@remix-ui/helper'
 
 interface ConfigInputProps {
   label: string
@@ -28,17 +29,40 @@ export const ConfigInput: React.FC<ConfigInputProps> = ({ label, id, secret, ini
     saveResult(value)
   }
 
+  const handleCancel = () => {
+    setEnabled(false)
+    setValue(initialValue)
+  }
+
   return (
-    <div className="form-group">
-      <label htmlFor={id}>{label}</label>
-      <input type={secret ? 'password' : 'text'} className="form-control mb-2" id={id} placeholder={`Add ${label}`} value={value} onChange={(e) => setValue(e.target.value)} disabled={!enabled} />
+    <div className="form-group small mb-0">
+      <label className='mt-3' htmlFor={id}>{label}</label>
       <div className="d-flex flex-row justify-content-start">
-        <button type="button" className="btn btn-secondary mr-3" disabled={enabled} onClick={handleChange}>
-          Change
-        </button>
-        <button type="button" className="btn btn-secondary" disabled={!enabled} onClick={handleSave}>
-          Save
-        </button>
+        <input
+          type={secret ? 'password' : 'text'}
+          className={`form-control small w-100 ${!enabled ? 'bg-transparent pl-0 border-0' : ''}`}
+          id={id}
+          placeholder={`Add ${label}`}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!enabled}
+        />
+
+        { enabled ? (
+          <>
+            <button type="button" className="btn btn-primary btn-sm ml-2" onClick={handleSave}>
+              Save
+            </button>
+            <button type="button" className="btn btn-secondary btn-sm ml-2" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <CustomTooltip tooltipText={`Edit ${label}`}>
+            <button type="button" className="btn btn-sm fas fa-pen my-1" style={{ height: '100%' }} disabled={enabled} onClick={handleChange}>
+            </button>
+          </CustomTooltip>
+        )}
       </div>
     </div>
   )
