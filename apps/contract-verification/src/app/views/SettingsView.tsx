937368d65e0755497840c0a3a8846894b5b84688
Manuel Wedler <manuel@wedler.dev>
2024-09-11 17:57:02 +0200

    Make settings view prettier

--- apps/contract-verification/src/app/views/SettingsView.tsx
+++ apps/contract-verification/src/app/views/SettingsView.tsx
@@ -32,19 +32,19 @@ export const SettingsView = () => {
 
       {selectedChain && (
         <div>
-          <div className="pt-2">
-            <span className="font-weight-bold">Sourcify</span>
+          <div className="p-2 my-2 border">
+            <span className="font-weight-bold">Sourcify - {selectedChain.name}</span>
             <ConfigInput label="API URL" id="sourcify-api-url" secret={false} initialValue={chainSettings.verifiers['Sourcify']?.apiUrl ?? ''} saveResult={(result) => handleChange('Sourcify', 'apiUrl', result)} />
             <ConfigInput label="Repo URL" id="sourcify-explorer-url" secret={false} initialValue={chainSettings.verifiers['Sourcify']?.explorerUrl ?? ''} saveResult={(result) => handleChange('Sourcify', 'explorerUrl', result)} />
           </div>
-          <div className="pt-2">
-            <span className="font-weight-bold">Etherscan</span>
+          <div className="p-2 my-2 border">
+            <span className="font-weight-bold">Etherscan - {selectedChain.name}</span>
             <ConfigInput label="API Key" id="etherscan-api-key" secret={true} initialValue={chainSettings.verifiers['Etherscan']?.apiKey ?? ''} saveResult={(result) => handleChange('Etherscan', 'apiKey', result)} />
             <ConfigInput label="API URL" id="etherscan-api-url" secret={false} initialValue={chainSettings.verifiers['Etherscan']?.apiUrl ?? ''} saveResult={(result) => handleChange('Etherscan', 'apiUrl', result)} />
             <ConfigInput label="Explorer URL" id="etherscan-explorer-url" secret={false} initialValue={chainSettings.verifiers['Etherscan']?.explorerUrl ?? ''} saveResult={(result) => handleChange('Etherscan', 'explorerUrl', result)} />
           </div>
-          <div className="pt-2">
-            <span className="font-weight-bold">Blockscout</span>
+          <div className="p-2 my-2 border">
+            <span className="font-weight-bold">Blockscout - {selectedChain.name}</span>
             <ConfigInput label="Instance URL" id="blockscout-api-url" secret={false} initialValue={chainSettings.verifiers['Blockscout']?.apiUrl ?? ''} saveResult={(result) => handleChange('Blockscout', 'apiUrl', result)} />
           </div>
         </div>
