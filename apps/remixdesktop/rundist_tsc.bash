579aa953bd26006f1b42d30842e94e837be3eba7
filip mertens <filip.mertens@ethereum.org>
2024-09-16 11:40:43 +0200

    include modules

--- apps/remixdesktop/rundist_tsc.bash
+++ apps/remixdesktop/rundist_tsc.bash
@@ -5,13 +5,13 @@ version=$(awk -F'"' '/"version":/ {print $4}' package.json)
 
 # Determine the command to run based on the version
 if [[ $version == *"beta"* ]]; then
-  command="yarn esbuild -c beta.json"
+  command="yarn tscbuild -c beta.json"
 elif [[ $version == *"alpha"* ]]; then
-  command="yarn esbuild -c alpha.json"
+  command="yarn tscbuild -c alpha.json"
 elif [[ $version == *"insiders"* ]]; then
-  command="yarn esbuild -c insiders.json"
+  command="yarn tscbuild -c insiders.json"
 else
-  command="yarn esbuild -c latest.json"
+  command="yarn tscbuild -c latest.json"
 fi
 
 # Append any arguments passed in CLI
