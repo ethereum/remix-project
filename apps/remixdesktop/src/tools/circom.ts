1fa2f274be1e3a1005142d9b7687449a0526a800
ioedeveloper <daviddisu8@gmail.com>
2024-09-17 16:31:34 +0100

    Complete download compiler UI

--- apps/remixdesktop/src/tools/circom.ts
+++ apps/remixdesktop/src/tools/circom.ts
@@ -35,56 +35,70 @@ async function downloadFile(url: string, dest: string) {
   })
 }
 
-export function getInstallationPath(version = 'latest') {
+export function getInstallationPath(version) {
   switch (process.platform) {
   case 'win32':
-    return path.join(app.getPath('temp'), version, 'circom-windows-amd64.exe')
+    return path.join(app.getPath('temp'), 'circom-download', version, 'circom-windows-amd64.exe')
 
   case 'darwin':
-    return path.join(app.getAppPath(), version, 'circom-macos-amd64')
+    return path.join(app.getAppPath(), 'circom-download', version, 'circom-macos-amd64')
 
   case 'linux':
-    return path.join(app.getAppPath(), version, 'circom-linux-amd64')
+    return path.join(app.getAppPath(), 'circom-download', version, 'circom-linux-amd64')
   }
 }
 
-function getInstallationUrl(version = 'latest') {
+export function getInstallationUrl(version) {
   switch (process.platform) {
   case 'win32':
-    return `https://github.com/iden3/circom/releases/${version}/download/circom-windows-amd64.exe`
+    return version === 'latest' ? 'https://github.com/iden3/circom/releases/latest/download/circom-windows-amd64.exe' : `https://github.com/iden3/circom/releases/download/${version}/circom-windows-amd64.exe`
 
   case 'darwin':
-    return `https://github.com/iden3/circom/releases/${version}/download/circom-macos-amd64`
+    return version === 'latest' ? 'https://github.com/iden3/circom/releases/latest/download/circom-macos-amd64' : `https://github.com/iden3/circom/releases/download/${version}/circom-macos-amd64`
 
   case 'linux':
-    return `https://github.com/iden3/circom/releases/${version}/download/circom-linux-amd64`
+    return version === 'latest' ? 'https://github.com/iden3/circom/releases/latest/download/circom-linux-amd64' : `https://github.com/iden3/circom/releases/download/${version}/circom-linux-amd64`
+  }
+}
+
+export function getLogInputSignalsPath() {
+  switch (process.platform) {
+  case 'win32':
+    return path.join(app.getPath('temp'), 'log_input_signals.txt')
+
+  case 'darwin':
+    return path.join(app.getAppPath(), 'log_input_signals.txt')
+
+  case 'linux':
+    return path.join(app.getAppPath(), 'log_input_signals.txt')
   }
 }
 
 export const circomCli = {
-  async installCircom () {
+  async installCircom (version) {
+    const installationPath = getInstallationPath(version)
+    const installationUrl = getInstallationUrl(version)
+
+    if (!existsSync(path.dirname(installationPath))) fs.mkdirSync(path.dirname(installationPath), { recursive: true })
     try {
-      const installationPath = getInstallationPath()
-      console.log('downloading circom to ', installationPath)
-      const installationUrl = getInstallationUrl()
       await downloadFile(installationUrl, installationPath)
-      console.log('downloading done')
     } catch (e) {
-      console.error(e)
+      fs.rmSync(installationPath)
+      throw new Error('Download failed!')
     }
   },
 
-  async isCircomInstalled () {
+  async isCircomInstalled (version) {
     try {
-      const installationPath = getInstallationPath()
+      const installationPath = getInstallationPath(version)
       return existsSync(installationPath)
     } catch (e) {
       return false
     }
   },
 
-  async run (filePath: string, options?: Record<string, string>) {
-    const installationPath = getInstallationPath()
+  async run (filePath: string, version: string, options?: Record<string, string>) {
+    const installationPath = getInstallationPath(version)
     const cmd = `${installationPath} ${filePath} ${Object.keys(options || {}).map((key) => options[key] ? `--${key} ${options[key]}` : `--${key}`).join(' ')}`
 
     return await execAsync(cmd)
