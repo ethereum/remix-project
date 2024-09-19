21ba0159ad2bd030f3e780f1666ded1aa6bf2469
Manuel Wedler <manuel@wedler.dev>
2024-09-19 11:13:15 +0200

    Fix NavMenu for smaller screens

--- apps/contract-verification/src/app/layouts/Default.tsx
+++ apps/contract-verification/src/app/layouts/Default.tsx
@@ -10,9 +10,9 @@ interface Props {
 
 export const DefaultLayout = ({ children, title, description }: PropsWithChildren<Props>) => {
   return (
-    <div>
+    <div className="d-flex flex-column h-100">
       <NavMenu />
-      <div className="py-4 px-3" style={{ marginTop: '40px' }}>
+      <div className="py-4 px-3 flex-grow-1" style={{ overflowY: 'auto' }}>
         <div>
           <p className="text-center" style={{ fontSize: '0.8rem' }}>
             {description}
