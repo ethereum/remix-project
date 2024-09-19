ba4cecaa36b0ffa86922597f66820c57c034d895
Manuel Wedler <manuel@wedler.dev>
2024-09-19 11:13:15 +0200

    Fix NavMenu for smaller screens

--- apps/contract-verification/src/app/components/NavMenu.tsx
+++ apps/contract-verification/src/app/components/NavMenu.tsx
@@ -9,7 +9,7 @@ interface NavItemProps {
 
 const NavItem: React.FC<NavItemProps> = ({ to, icon, title }) => {
   return (
-    <NavLink to={to} className={({ isActive }) => 'text-decoration-none d-flex flex-column justify-content-center px-2 ' + (isActive ? 'bg-light' : 'bg-transparent')}>
+    <NavLink to={to} className={({ isActive }) => 'text-decoration-none d-flex flex-column justify-content-center p-2 small ' + (isActive ? 'bg-light' : 'bg-transparent')}>
       <span>
         <span>{icon}</span>
         <span className="ml-2">{title}</span>
@@ -20,7 +20,7 @@ const NavItem: React.FC<NavItemProps> = ({ to, icon, title }) => {
 
 export const NavMenu = () => {
   return (
-    <nav className="d-flex flex-row justify-start fixed-top w-100" style={{ height: '40px' }}>
+    <nav className="d-flex flex-row justify-start w-100">
       <NavItem to="/" icon={<i className="fas fa-home"></i>} title="Verify" />
       <NavItem to="/receipts" icon={<i className="fas fa-receipt"></i>} title="Receipts" />
       <NavItem to="/lookup" icon={<i className="fas fa-search"></i>} title="Lookup" />
