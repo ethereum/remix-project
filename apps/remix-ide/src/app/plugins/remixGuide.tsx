588615ad3fd00d774461bfd6e70d457c588c2b05
Liana Husikyan <liana@ethereum.org>
2024-10-28 18:44:50 +0100

    Update remixGuide.tsx

--- apps/remix-ide/src/app/plugins/remixGuide.tsx
+++ apps/remix-ide/src/app/plugins/remixGuide.tsx
@@ -8,7 +8,6 @@ import { RemixUIGridSection } from '@remix-ui/remix-ui-grid-section'
 import { RemixUIGridCell } from '@remix-ui/remix-ui-grid-cell'
 import * as Data from './remixGuideData.json'
 import './remixGuide.css'
-import Icon from 'libs/remix-ui/vertical-icons-panel/src/lib/components/Icon'
 //@ts-ignore
 const _paq = (window._paq = window._paq || [])
 
