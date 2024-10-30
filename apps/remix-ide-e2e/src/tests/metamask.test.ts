d0815c692a424cc6d39b1cf808a2a3bfdc44b77e
bunsenstraat <filip.mertens@ethereum.org>
2024-10-30 11:19:30 +0100

    send voter

--- apps/remix-ide-e2e/src/tests/metamask.test.ts
+++ apps/remix-ide-e2e/src/tests/metamask.test.ts
@@ -274,7 +274,7 @@ const tests = {
       .waitForElementPresent('*[data-id="universalDappUiContractActionWrapper"]', 60000)
       .clearConsole()
       .clickInstance(0)
-      .clickFunction('giveRightToVote - transact (not payable)', { types: 'address to', values: '"0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db"' })
+      .clickFunction('giveRightToVote - transact (not payable)', { types: 'address voter', values: '"0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db"' })
       .saveScreenshot('./reports/screenshots/metamask_6.png')
       .pause(5000)
       .perform((done) => { // call delegate
