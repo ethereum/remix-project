439504f2e9697c7fac937549111a3b56ee0c2a5f
bunsenstraat <filip.mertens@ethereum.org>
2024-10-30 11:05:58 +0100

    pause

--- apps/remix-ide-e2e/src/tests/metamask.test.ts
+++ apps/remix-ide-e2e/src/tests/metamask.test.ts
@@ -276,14 +276,14 @@ const tests = {
       .clickInstance(0)
       .clickFunction('giveRightToVote - transact (not payable)', { types: 'address to', values: '"0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db"' })
       .saveScreenshot('./reports/screenshots/metamask_6.png')
-      .pause()
+      .pause(5000)
       .perform((done) => { // call delegate
         browser.switchBrowserWindow(extension_url, 'MetaMask', (browser) => {
           browser
             .maximizeWindow()
             .hideMetaMaskPopup()
             
-            .pause()
+            .pause(5000)
             .saveScreenshot('./reports/screenshots/metamask_7.png')
             // .scrollAndClick('[data-testid="page-container-footer-next"]')
             // .click('[data-testid="page-container-footer-next"]') // approve the tx
