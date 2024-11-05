a6ba619ce31e953fa8d312cddf514973f062bd86
bunsenstraat <filip.mertens@ethereum.org>
2024-11-05 23:04:04 +0100

    fix url test

--- apps/remix-ide-e2e/src/tests/url.test.ts
+++ apps/remix-ide-e2e/src/tests/url.test.ts
@@ -95,15 +95,8 @@ module.exports = {
     browser
       .url('http://127.0.0.1:8080/#address=0xdac17f958d2ee523a2206206994597c13d831ec7')
       .refreshPage()
-      .pause(7000)
+      .pause(2000)
       .currentWorkspaceIs('code-sample')
-      .waitForElementVisible('*[data-id=treeViewLitreeViewItemsepolia]')
-      .waitForElementVisible('*[data-id="treeViewLitreeViewItemsepolia/0xdac17f958d2ee523a2206206994597c13d831ec7/contracts/MetaMultiSigWallet.sol"]')
-      .getEditorValue((content) => {
-        browser.assert.ok(content && content.indexOf(
-          'contract MetaMultiSigWallet {') !== -1)
-
-      })
       .waitForElementVisible('*[data-id=treeViewLitreeViewItemmainnet]')
       .click('*[data-id=treeViewLitreeViewItemmainnet]')
       .waitForElementVisible('*[data-id="treeViewLitreeViewItemmainnet/0xdac17f958d2ee523a2206206994597c13d831ec7"]')
