8145f49147be454616f99616d6d8fa89f0ea088b
aniket-engg <aniketengg.225@gmail.com>
2024-12-12 20:45:07 +0530

    load fresh SVSprovider

--- apps/remix-ide/src/blockchain/execution-context.js
+++ apps/remix-ide/src/blockchain/execution-context.js
@@ -58,7 +58,7 @@ export class ExecutionContext {
   }
 
   isVM () {
-    return this.executionContext.startsWith('vm')
+    return this.executionContext.startsWith('vm') || this.executionContext.startsWith('svs')
   }
 
   setWeb3 (context, web3) {
