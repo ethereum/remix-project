3dab070460678032087902649ca14b3fdd82feef
Ocenka <testoviydiman1@gmail.com>
2024-12-17 19:44:26 +0000

    typos breakpointManager.ts

--- libs/remix-debug/src/code/breakpointManager.ts
+++ libs/remix-debug/src/code/breakpointManager.ts
@@ -64,7 +64,7 @@ export class BreakpointManager {
   }
 
   hitLine (currentStep, sourceLocation, previousSourceLocation, trace) {
-    // isJumpDestInstruction -> returning from a internal function call
+    // isJumpDestInstruction -> returning from an internal function call
     // depthChange -> returning from an external call
     // sourceLocation.start <= previousSourceLocation.start && ... -> previous src is contained in the current one
     if ((isJumpDestInstruction(trace[currentStep]) && previousSourceLocation.jump === 'o') ||
