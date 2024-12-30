3bbe5e7145deb483ec01c9bb132be344fab642f0
aniket-engg <aniketengg.225@gmail.com>
2024-12-30 12:56:18 +0530

    failing test

--- apps/remix-ide-e2e/src/tests/vm_state.test.ts
+++ apps/remix-ide-e2e/src/tests/vm_state.test.ts
@@ -19,6 +19,13 @@ const tests = {
       .assert.elementPresent('*[data-id="fork-state-icon"]')
       .assert.elementPresent('*[data-id="delete-state-icon"]')
   },
+  'Should show toaster while trying fork and delete VM state without state #group1': function (browser: NightwatchBrowser) {
+    browser
+      .assert.elementPresent('*[data-id="fork-state-icon"]')
+      .click('*[data-id="fork-state-icon"]')
+      .waitForElementVisible('*[data-shared="tooltipPopup"]', 10000)
+      .assert.containsText('*[data-shared="tooltipPopup"]', `State not available to fork, as no transactions have been made for selected environment & selected workspace.`)
+  },
 }
 
 module.exports = {
