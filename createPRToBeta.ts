e70ce424ac0e0c5207b3ca317d13712c5e89592d
futreall <86553580+futreall@users.noreply.github.com>
2025-01-17 12:33:46 +0200

    Update createPRToBeta.ts

--- createPRToBeta.ts
+++ createPRToBeta.ts
@@ -1,7 +1,7 @@
 // This script can be used to open a PR for base branch 'remix_beta' using an existing pull request
 // Pull request number should be provided while running this script
 // It will use the reference branch same as the shared PR
-// To create a new PR, Github auth token with scope 'repo' needs to be provided
+// To create a new PR, a Github auth token with scope 'repo' needs to be provided
 // Command to run this script: fromPR=4369 authToken=abc123 yarn run createPRToBeta
 
 import { Octokit } from "octokit"
