6db90daa20aecd3ba035d866de69165c292150ae
GooseMatrix <155266802+GooseMatrix@users.noreply.github.com>
2025-01-19 11:09:38 +0100

    Update normalizeContractPath.ts (#2)

--- apps/doc-gen/src/app/docgen/utils/normalizeContractPath.ts
+++ apps/doc-gen/src/app/docgen/utils/normalizeContractPath.ts
@@ -1,14 +1,19 @@
 export function joinPaths(paths: string[]): string {
-  if (!paths || paths.length === 0) {
-    return '';
-  }
+    if (!paths || paths.length === 0) {
+        return '';
+    }
+    
+    let joinedPath = paths[0];
+    
+    for (let i = 1; i < paths.length; i++) {
+        // Add path separator between elements
+        joinedPath = `${joinedPath}/${paths[i]}`;
+    }
+    
+    return joinedPath;
+}
 
-  let joinedPath = paths[0];
-
-  for (let i = 1; i < paths.length; i++) {
-    // Merging paths
-    joinedPath = `${joinedPath}/${paths[i]}`;
-  }
-
-  return joinedPath;
+export function normalizeContractPath(contractPath: string): string {
+    const paths = contractPath.split('/');
+    return joinPaths(paths);
 }
