b4218089f2cd7219c52f03533432ebaac6807290
viktorking7 <vikv4324@gmail.com>
2025-01-21 15:54:42 +0200

    fix: fix TypeScript errors in hidePopupPanel command

--- apps/remix-ide-e2e/src/commands/hidePopupPanel.ts
+++ apps/remix-ide-e2e/src/commands/hidePopupPanel.ts
@@ -1,41 +1,49 @@
 import { NightwatchBrowser } from 'nightwatch'
 import EventEmitter from 'events'
 
+interface PopupResult {
+  popupState: string | null
+  elementExists: boolean
+}
+
 class HidePopupPanel extends EventEmitter {
   command(this: NightwatchBrowser) {
     const TIMEOUT = 40000
     const SELECTOR = '*[data-id="popupPanelToggle"]'
 
-    browser
+    this
       .waitForElementPresent('body', 10000)
       .perform((done) => {
-        browser.execute(function () {
+        this.execute<PopupResult>(function () {
           return {
             popupState: localStorage.getItem('did_show_popup_panel'),
             elementExists: document.querySelector('*[data-id="popupPanelToggle"]') !== null
           }
         }, [], function (result) {
-          if (!result.value.popupState) {
-            browser
+          if (!result.value?.popupState) {
+            this
               .waitForElementPresent(SELECTOR, TIMEOUT)
               .pause(1000)
               .waitForElementVisible(SELECTOR, TIMEOUT)
               .perform(() => {
-                browser.click(SELECTOR)
+                this.click(SELECTOR)
               })
               .pause(500)
           }
           done()
-        })
+        }.bind(this))
       })
       .perform((done) => {
         done()
         this.emit('complete')
       })
-      .catch((error) => {
-        console.warn('Warning: Could not hide popup panel:', error.message)
+      .perform((client, done) => {
+        console.warn('Warning: Could not hide popup panel')
         this.emit('complete')
+        done()
       })
+
+    return this
   }
 }
 
