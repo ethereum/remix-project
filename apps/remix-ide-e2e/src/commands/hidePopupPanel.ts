c607f24922646f47bda45b10e9a581dfa09f6905
viktorking7 <vikv4324@gmail.com>
2025-01-21 15:46:33 +0200

    fix: update hidePopupPanel command to handle popup visibility correctly

--- apps/remix-ide-e2e/src/commands/hidePopupPanel.ts
+++ apps/remix-ide-e2e/src/commands/hidePopupPanel.ts
@@ -3,20 +3,37 @@ import EventEmitter from 'events'
 
 class HidePopupPanel extends EventEmitter {
   command(this: NightwatchBrowser) {
+    const TIMEOUT = 40000
+    const SELECTOR = '*[data-id="popupPanelToggle"]'
+
     browser
+      .waitForElementPresent('body', 10000)
       .perform((done) => {
         browser.execute(function () {
-          return localStorage.getItem('did_show_popup_panel')
+          return {
+            popupState: localStorage.getItem('did_show_popup_panel'),
+            elementExists: document.querySelector('*[data-id="popupPanelToggle"]') !== null
+          }
         }, [], function (result) {
-          if (!result.value) {
-            browser.waitForElementVisible('*[data-id="popupPanelToggle"]', 20000)
-            .click('*[data-id="popupPanelToggle"]')
+          if (!result.value.popupState) {
+            browser
+              .waitForElementPresent(SELECTOR, TIMEOUT)
+              .pause(1000)
+              .waitForElementVisible(SELECTOR, TIMEOUT)
+              .perform(() => {
+                browser.click(SELECTOR)
+              })
+              .pause(500)
           }
           done()
         })
       })
       .perform((done) => {
         done()
+        this.emit('complete')
+      })
+      .catch((error) => {
+        console.warn('Warning: Could not hide popup panel:', error.message)
         this.emit('complete')
       })
   }
