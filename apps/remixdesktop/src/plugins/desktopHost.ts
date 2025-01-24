47d6a8e6448c5e937a9b6b2807035b6c53d3c402
bunsenstraat <filip.mertens@ethereum.org>
2025-01-24 10:07:33 +0100

    fix server

--- apps/remixdesktop/src/plugins/desktopHost.ts
+++ apps/remixdesktop/src/plugins/desktopHost.ts
@@ -7,15 +7,14 @@ import { RequestArguments } from "../types"
 import fs from 'fs'
 import path from 'path'
 
-
 const logFilePath = path.join(__dirname, 'desktopHost.log')
 console.log('logFilePath', logFilePath)
 // Create or clear the log file
 fs.writeFileSync(logFilePath, '', { flag: 'w' })
 const profile = {
-    displayName: 'desktopHost',
-    name: 'desktopHost',
-    description: 'desktopHost',
+  displayName: 'desktopHost',
+  name: 'desktopHost',
+  description: 'desktopHost',
 }
 
 const eventEmitter = new EventEmitter()
@@ -27,85 +26,85 @@ let ports: {
 }
 
 export class DesktopHostPlugin extends ElectronBasePlugin {
-    clients: DesktopHostPluginClient[] = []
-    constructor() {
-        super(profile, clientProfile, DesktopHostPluginClient)
-        this.methods = [...super.methods]
-        this.startServer()
-        eventEmitter.on('connected', (payload) => {
-            console.log('connected', payload)
-            isConnected = payload
-            if (!isConnected) {
-                for (const client of this.clients) {
-                    client.disconnect()
-                }
-            }
-        })
-        eventEmitter.on('focus', () => {
-            console.log('focus')
-            ipcMain.emit('focus-window', 0)
-        })
-        eventEmitter.on('contextChanged', (context) => {
-            console.log('contextChanged', context)
-            for (const client of this.clients) {
-                client.emit('chainChanged', context)
-            }
-        })
-    }
+  clients: DesktopHostPluginClient[] = []
+  constructor() {
+    super(profile, clientProfile, DesktopHostPluginClient)
+    this.methods = [...super.methods]
+    this.startServer()
+    eventEmitter.on('connected', (payload) => {
+      console.log('connected', payload)
+      isConnected = payload
+      if (!isConnected) {
+        for (const client of this.clients) {
+          client.disconnect()
+        }
+      }
+    })
+    eventEmitter.on('focus', () => {
+      console.log('focus')
+      ipcMain.emit('focus-window', 0)
+    })
+    eventEmitter.on('contextChanged', (context) => {
+      console.log('contextChanged', context)
+      for (const client of this.clients) {
+        client.emit('chainChanged', context)
+      }
+    })
+  }
 
-    async startServer(): Promise<void> {
-        console.log('desktopHost activated')
-        ports = await startHostServer(eventEmitter)
-        console.log('desktopHost server started', ports)
-    }
+  async startServer(): Promise<void> {
+    console.log('desktopHost activated')
+    ports = await startHostServer(eventEmitter)
+    console.log('desktopHost server started', ports)
+  }
 }
 
 const clientProfile: Profile = {
-    name: 'desktopHost',
-    displayName: 'desktopHost',
-    description: 'desktopHost',
-    methods: ['getIsConnected', 'sendAsync', 'init'],
-    kind: 'provider',
+  name: 'desktopHost',
+  displayName: 'desktopHost',
+  description: 'desktopHost',
+  methods: ['getIsConnected', 'sendAsync', 'init'],
+  kind: 'provider',
 }
 
 export class DesktopHostPluginClient extends ElectronBasePluginClient {
-    constructor(webContentsId: number, profile: Profile) {
-        super(webContentsId, profile)
+  constructor(webContentsId: number, profile: Profile) {
+    super(webContentsId, profile)
+  }
+
+  getIsConnected() {
+    console.log('getIsConnected', isConnected)
+    return isConnected
+  }
+
+  async init() {
+    console.log('initializing destkophost plugin...', this.webContentsId)
+
+    if (!isConnected)
+      await shell.openExternal(`http://localhost:${ports.http_port}/?activate=udapp,desktopClient&desktopClientPort=${ports.websocket_port}`)
+    // wait for the connection
+    while (!isConnected) {
+      await new Promise(resolve => setTimeout(resolve, 1000))
     }
+  }
 
-    getIsConnected() {
-        console.log('getIsConnected', isConnected)
-        return isConnected
-    }
+  async disconnect() {
+    this.call('notification' as any, 'alert', {
+      id: 'Connection lost',
+      message: 'You have been disconnected from Remix on the web. Please select another environment for deploy & run.',
+    })
+  }
 
-    async init() {
-        console.log('initializing destkophost plugin...', this.webContentsId)
-        
-        if (!isConnected)
-            await shell.openExternal(`http://localhost:${ports.http_port}/?activate=udapp,desktopClient&desktopClientPort=${ports.websocket_port}`)
-        // wait for the connection
-        while (!isConnected) {
-            await new Promise(resolve => setTimeout(resolve, 1000))
-        }
-    }
+  async sendAsync(data: RequestArguments) {
+    //console.log('SEND ASYNC', data, this.webContentsId)
+    const result = await handleRequest(data, eventEmitter)
 
-    async disconnect() {
-        this.call('notification' as any, 'alert', {
-            id: 'Connection lost',
-            message: 'You have been disconnected from Remix on the web. Please select another environment for deploy & run.',
-        })
-    }
-
-    async sendAsync(data: RequestArguments) {
-        //console.log('SEND ASYNC', data, this.webContentsId)
-        const result = await handleRequest(data, eventEmitter)
-
-        const logEntry = `
+    const logEntry = `
         webContentsId: ${this.webContentsId}
         Request: ${JSON.stringify(data, (key, value) => typeof value === 'bigint' ? value.toString() : value, 2)}
         Result: ${JSON.stringify(result, (key, value) => typeof value === 'bigint' ? value.toString() : value, 2)}
         `
-        fs.appendFileSync(logFilePath, logEntry)
-        return result
-    }
+    fs.appendFileSync(logFilePath, logEntry)
+    return result
+  }
 }
