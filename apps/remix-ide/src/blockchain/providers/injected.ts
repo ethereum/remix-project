2d357ba83b55017a70530e0c9237e4f2d9c01865
yann300 <yann.levreau@gmail.com>
2025-02-11 21:58:56 +0100

    signMessage

--- apps/remix-ide/src/blockchain/providers/injected.ts
+++ apps/remix-ide/src/blockchain/providers/injected.ts
@@ -43,7 +43,7 @@ export class InjectedProvider {
     try {
       const personal = new Personal(this.executionContext.web3().currentProvider)
       message = isHexString(message) ? message : Web3.utils.utf8ToHex(message)
-      personal.sign(message, account, passphrase)
+      personal.sign(message, account, '')
         .then(signedData => cb(undefined, bytesToHex(messageHash), signedData))
         .catch(error => cb(error, bytesToHex(messageHash), undefined))
     } catch (e) {
