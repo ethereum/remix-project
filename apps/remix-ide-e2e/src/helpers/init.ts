958d2ce6e3ea4d0918b227655cea36161ac9f8bd
bunsenstraat <filip.mertens@ethereum.org>
2025-02-11 15:02:39 +0100

    img

--- apps/remix-ide-e2e/src/helpers/init.ts
+++ apps/remix-ide-e2e/src/helpers/init.ts
@@ -14,7 +14,7 @@ export default function (browser: NightwatchBrowser, callback: VoidFunction, url
     .url(url || 'http://127.0.0.1:8080')
     .pause(5000)
     .switchBrowserTab(0)
-    //.hidePopupPanel()
+    .hidePopupPanel()
     .perform((done) => {
       if (!loadPlugin) return done()
       browser
