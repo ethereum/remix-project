5a59daa473187b68bcb8b70f4356306ef7516e90
Joseph Izang <josephizang@gmail.com>
2025-02-12 10:50:16 +0100

    plugin activation should be on demand

--- apps/remix-ide/src/app.ts
+++ apps/remix-ide/src/app.ts
@@ -693,7 +693,7 @@ class AppComponent {
     })
 
     // activate solidity plugin
-    this.appManager.activatePlugin(['solidity', 'udapp', 'deploy-libraries', 'link-libraries', 'openzeppelin-proxy', 'scriptRunnerBridge'])
+    this.appManager.activatePlugin(['solidity', 'udapp', 'deploy-libraries', 'link-libraries', 'openzeppelin-proxy'])
   }
 }
 
