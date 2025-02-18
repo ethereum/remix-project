a484b253dc193cb7a58713134bd78cd6929dcff1
yann300 <yann.levreau@gmail.com>
2025-02-18 10:35:42 +0100

    fix tests

--- libs/remix-url-resolver/src/resolve.ts
+++ libs/remix-url-resolver/src/resolve.ts
@@ -275,7 +275,7 @@ function getPkg(pkg, yarnLock, packageLock, deps, url, fetchUrl) {
     } else {
       // const versionSemver = semver.minVersion(version)
       fetchUrl = url.replace(pkg, `${pkg}@${version}`)
-      return encodeURIComponent(fetchUrl)
+      return fetchUrl
     }
   }
   return null
