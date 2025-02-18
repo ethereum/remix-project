5f3c4bbd158cbaacacff928111795e2b77b8a03b
yann300 <yann.levreau@gmail.com>
2025-02-18 12:22:38 +0100

    add noir script

--- libs/remix-ws-templates/src/templates/multiplierNoir/index.ts
+++ libs/remix-ws-templates/src/templates/multiplierNoir/index.ts
@@ -5,6 +5,8 @@ export default async () => {
     // @ts-ignore
     'Nargo.toml': (await import('raw-loader!./Nargo.toml')).default,
     // @ts-ignore
-    'README.md': (await import('raw-loader!./README.md')).default
+    'README.md': (await import('raw-loader!./README.md')).default,
+    // @ts-ignore
+    'tests/multiplier.test.ts': (await import('raw-loader!./tests/multiplier.test.ts')).default
   }
 }
