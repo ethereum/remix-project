c93f8bd8119ab1bedf4faf3e09f6b23aa66dfdf6
yann300 <yann.levreau@gmail.com>
2025-02-18 12:22:38 +0100

    add noir script

--- libs/remix-ws-templates/src/templates/stealthdropNoir/index.ts
+++ libs/remix-ws-templates/src/templates/stealthdropNoir/index.ts
@@ -5,6 +5,8 @@ export default async () => {
     // @ts-ignore
     'Nargo.toml': (await import('raw-loader!./Nargo.toml')).default,
     // @ts-ignore
-    'README.md': (await import('raw-loader!./README.md')).default
+    'README.md': (await import('raw-loader!./README.md')).default,
+    // @ts-ignore
+    'tests/stealthdrop.test.ts': (await import('raw-loader!./tests/stealthdrop.test.ts')).default
   }
 }
