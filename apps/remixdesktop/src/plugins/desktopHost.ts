422e4b17a52946a8c620e4bf8cf8504019d565db
bunsenstraat <filip.mertens@ethereum.org>
2025-03-01 08:26:04 +0100

    screenshot

--- apps/remixdesktop/src/plugins/desktopHost.ts
+++ apps/remixdesktop/src/plugins/desktopHost.ts
@@ -117,7 +117,7 @@ export class DesktopHostPluginClient extends ElectronBasePluginClient {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto(`http://localhost:${ports.http_port}/?activate=udapp,desktopClient&desktopClientPort=${ports.websocket_port}`);
-    console.log('Page opened in CI');
+    await page.screenshot({ path: '/home/circleci/remix-project/apps/remixdesktop/reports/screenshots/desktophost.png' });
   }
   
 
