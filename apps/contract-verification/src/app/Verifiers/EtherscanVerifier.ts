7a48a3e9e0d9fff428de257659bf93f1924dcc5d
comfsrt <155266597+comfsrt@users.noreply.github.com>
2025-03-05 13:53:38 +0100

    Update EtherscanVerifier.ts

--- apps/contract-verification/src/app/Verifiers/EtherscanVerifier.ts
+++ apps/contract-verification/src/app/Verifiers/EtherscanVerifier.ts
@@ -44,15 +44,23 @@ export class EtherscanVerifier extends AbstractVerifier {
   }
 
   async verify(submittedContract: SubmittedContract, compilerAbstract: CompilerAbstract): Promise<VerificationResponse> {
-    // TODO: Handle version Vyper contracts. This relies on Solidity metadata.
     const metadata = JSON.parse(compilerAbstract.data.contracts[submittedContract.filePath][submittedContract.contractName].metadata)
+    const language = metadata?.compiler?.language?.toLowerCase() || 'solidity'
     const formData = new FormData()
+
     formData.append('chainId', submittedContract.chainId)
     formData.append('codeformat', 'solidity-standard-json-input')
     formData.append('sourceCode', compilerAbstract.input.toString())
     formData.append('contractaddress', submittedContract.address)
     formData.append('contractname', submittedContract.filePath + ':' + submittedContract.contractName)
-    formData.append('compilerversion', `v${metadata.compiler.version}`)
+
+    // Now handle Vyper
+    if (language === 'vyper') {
+      formData.append('compilerversion', `vyper-${metadata.compiler.version}`)
+    } else {
+      formData.append('compilerversion', `v${metadata.compiler.version}`)
+    }
+
     formData.append('constructorArguements', submittedContract.abiEncodedConstructorArgs?.replace('0x', '') ?? '')
 
     const url = new URL(this.apiUrl + '/api')
@@ -186,7 +194,7 @@ export class EtherscanVerifier extends AbstractVerifier {
 
     if (!response.ok) {
       const responseText = await response.text()
-      console.error('Error on Etherscan API check verification status at ' + this.apiUrl + '\nStatus: ' + response.status + '\nResponse: ' + responseText)
+      console.error('Error on Etherscan API check proxy verification status at ' + this.apiUrl + '\nStatus: ' + response.status + '\nResponse: ' + responseText)
       throw new Error(responseText)
     }
 
@@ -233,7 +241,11 @@ export class EtherscanVerifier extends AbstractVerifier {
 
     const lookupResponse: EtherscanGetSourceCodeResponse = await response.json()
 
-    if (lookupResponse.result[0].ABI === 'Contract source code not verified' || !lookupResponse.result[0].SourceCode || (lookupResponse.result as unknown as string) === 'Contract source code not verified') {
+    if (
+      lookupResponse.result[0].ABI === 'Contract source code not verified' ||
+      !lookupResponse.result[0].SourceCode ||
+      (lookupResponse.result as unknown as string) === 'Contract source code not verified'
+    ) {
       return { status: 'not verified' }
     }
 
@@ -257,15 +269,11 @@ export class EtherscanVerifier extends AbstractVerifier {
   processReceivedFiles(source: EtherscanSource, contractAddress: string, chainId: string): { sourceFiles: SourceFile[]; targetFilePath?: string } {
     const filePrefix = `/${this.LOOKUP_STORE_DIR}/${chainId}/${contractAddress}`
 
-    // Covers the cases:
-    // SourceFile: {[FileName]: [content]}
-    // SourceFile: {{sources: {[FileName]: [content]}}}
     let parsedFiles: any
     try {
       parsedFiles = JSON.parse(source.SourceCode)
     } catch (e) {
       try {
-        // Etherscan wraps the Object in one additional bracket
         parsedFiles = JSON.parse(source.SourceCode.substring(1, source.SourceCode.length - 1)).sources
       } catch (e) {}
     }
@@ -275,9 +283,7 @@ export class EtherscanVerifier extends AbstractVerifier {
       let targetFilePath = ''
       for (const [fileName, fileObj] of Object.entries<any>(parsedFiles)) {
         const path = `${filePrefix}/${fileName}`
-
         result.push({ path, content: fileObj.content })
-
         if (path.endsWith(`/${source.ContractName}.sol`)) {
           targetFilePath = path
         }
@@ -285,7 +291,6 @@ export class EtherscanVerifier extends AbstractVerifier {
       return { sourceFiles: result, targetFilePath }
     }
 
-    // Parsing to JSON failed, SourceCode is the code itself
     const targetFilePath = `${filePrefix}/${source.ContractName}.sol`
     const sourceFiles: SourceFile[] = [{ content: source.SourceCode, path: targetFilePath }]
     return { sourceFiles, targetFilePath }
