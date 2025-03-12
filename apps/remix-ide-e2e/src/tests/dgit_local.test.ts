1402de29fc641e1def900d212531906faf02fa76
bunsenstraat <filip.mertens@ethereum.org>
2025-03-12 11:28:58 +0100

    turn off flaky

--- apps/remix-ide-e2e/src/tests/dgit_local.test.ts
+++ apps/remix-ide-e2e/src/tests/dgit_local.test.ts
@@ -407,7 +407,7 @@ module.exports = {
         console.log(logs2)
         browser.assert.fail(logs2.includes('testcommit'))
     },
-    'switch to origin #flaky #group4': function (browser: NightwatchBrowser) {
+    'switch to origin #group4': function (browser: NightwatchBrowser) {
         browser
             .waitForElementVisible('*[data-id="remotes-panel"]')
             .pause(2000)
