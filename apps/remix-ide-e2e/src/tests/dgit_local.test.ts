b780d73f6416631969b294b238590e13933ea62a
bunsenstraat <filip.mertens@ethereum.org>
2025-03-12 11:50:08 +0100

    fix more

--- apps/remix-ide-e2e/src/tests/dgit_local.test.ts
+++ apps/remix-ide-e2e/src/tests/dgit_local.test.ts
@@ -373,7 +373,10 @@ module.exports = {
     },
     'switch to origin2 #group4': function (browser: NightwatchBrowser) {
         browser
+            .waitForElementVisible('*[data-id="remotes-panel"]')
+            .pause(2000)
             .click('*[data-id="remotes-panel"]')
+            .waitForElementVisible('*[data-id="fetch-repositories"]')
             .waitForElementVisible('*[data-id="set-as-default-origin2"]')
             .click('*[data-id="set-as-default-origin2"]')
     },
