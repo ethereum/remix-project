18756249fa9c9420cf3cb06d5ea69d2adc930813
lianahus <liana@ethereum.org>
2025-03-13 12:52:13 +0100

    fixes for sr and matomo update

--- libs/remix-ui/scriptrunner/src/lib/components/config-section.tsx
+++ libs/remix-ui/scriptrunner/src/lib/components/config-section.tsx
@@ -23,9 +23,8 @@ export default function ConfigSection(props: ConfigSectionProps) {
   }
 
   const SectionHeader = () => {
-
     return (
-      <section className="mr-1">
+      <section className="text-nowrap mr-1">
         <div className="custom-control custom-radio">
           <input
             className="custom-control-input"
@@ -34,13 +33,15 @@ export default function ConfigSection(props: ConfigSectionProps) {
             value={props.config.name}
             id={props.config.title || props.config.name}
             onChange={() => {
-              props.setActiveKey(props.config.name)
               props.loadScriptRunner(props.config)
+              if (!props.config.errorStatus) {
+                props.setActiveKey(props.config.name)
+              }
               props._paq.push(['trackEvent', 'scriptRunnerPlugin', 'loadScriptRunnerConfig', props.config.name])
             }}
-            checked={props.activeKey === props.config.name}
+            checked={(props.activeConfig && props.activeConfig.name === props.config.name)}
           />
-          <label className="form-check-label custom-control-label" htmlFor={`${props.config.title || props.config.name}`}
+          <label className="pointer form-check-label custom-control-label" htmlFor={`${props.config.title || props.config.name}`}
             data-id={`sr-load-${props.config.name}`}>
             <div data-id={`sr-loaded-${props.config.name}`} className="pl-2">{props.config.title || props.config.name}</div>
           </label>
@@ -84,50 +85,49 @@ export default function ConfigSection(props: ConfigSectionProps) {
     <section className="mb-2">
       <section className="d-flex flex-row ">
         <SectionHeader />
-        <div>
-          {!props.config.isLoading && !props.config.errorStatus && !props.config.error &&
-          <div onClick={() => props.loadScriptRunner(props.config)} className="pointer px-2 pb-1">
-            { props.activeConfig && props.activeConfig.name === props.config.name &&
-              <div className={`${!isVisible ? 'd-flex flex-row align-items-center justify-content-center pt-1' : 'd-flex flex-row pb-1 align-items-center justify-content-center'}`}>
-                <FontAwesomeIcon data-id={`sr-loaded-${props.config.name}`} className="text-success ml-3" icon={faCheck}></FontAwesomeIcon>
-                {isVisible && <span onAnimationEnd={handleAnimationEnd} className="text-success px-3" style={{ animation: 'fadeOut 5s forwards', animationFillMode: 'forwards' }}>Config loaded</span>}
+        <label htmlFor={`${props.config.title || props.config.name}`} className="pointer w-100 d-flex flex-row mb-0">
+          <div>
+            {!props.config.isLoading && !props.config.errorStatus && !props.config.error &&
+            <div onClick={() => props.loadScriptRunner(props.config)} className="pointer px-2 pb-1 mb-0 pb-0">
+              { props.activeConfig && props.activeConfig.name === props.config.name &&
+                <div className="d-flex flex-row mt-1">
+                  <FontAwesomeIcon data-id={`sr-loaded-${props.config.name}`} className="text-success ml-3" icon={faCheck}></FontAwesomeIcon>
+                  {isVisible && <span onAnimationEnd={handleAnimationEnd} className="text-success px-3 mb-0 pb-0" style={{ animation: 'fadeOut 5s forwards', animationFillMode: 'forwards' }}>Config loaded</span>}
+                </div>
+              }
+            </div>
+            }
+          </div>
+          {props.config.isLoading && <div className="d-flex flex-row mx-4">
+            <div className="d-flex flex-row pb-1 align-items-center justify-content-center">
+              <i className="fas fa-spinner fa-spin"></i><span className='pl-3'>Loading config</span>
+            </div>
+          </div>}
+          <div className="ml-4 d-flex" id="errorSection">
+            {!props.config.isLoading && props.config.errorStatus && props.config.error &&
+              <div
+                onClick={() => {
+                  props.loadScriptRunner(props.config)
+                  props._paq.push(['trackEvent', 'scriptRunnerPlugin', 'error_reloadScriptRunnerConfig', props.config.name])
+                }}
+                className="pointer text-danger d-flex flex-row"
+              >
+                <CustomTooltip tooltipText={props.config.error}>
+                  <FontAwesomeIcon className="mt-1 pr-4" data-id={`sr-error-${props.config.name}`} icon={faTimes}></FontAwesomeIcon>
+                </CustomTooltip>
+                <span className="pr-2 mt-1 text-nowrap text-danger font-weight-bold">Loading error.</span>
+                <span className="text-danger mt-1 ">We are not able to load your requested configuration for now, please try again later.</span>
               </div>
             }
           </div>
-          }
-        </div>
-        {props.config.isLoading && <div className="d-flex flex-row mx-4">
-          <div className="d-flex flex-row pb-1 align-items-center justify-content-center">
-            <i className="fas fa-spinner fa-spin"></i><span className='pl-3'>Loading config</span>
-          </div>
-        </div>}
-        <div className="ml-4 d-flex" id="errorSection">
-          {props.config.errorStatus && props.config.error && <div className="text-danger">
-            <CustomTooltip tooltipText={props.config.error}>
-              <FontAwesomeIcon data-id={`sr-error-${props.config.name}`} icon={faTimes}></FontAwesomeIcon>
-            </CustomTooltip>
-
-          </div>}
-          {!props.config.isLoading && props.config.errorStatus && props.config.error &&
-            <div
-              onClick={() => {
-                props.loadScriptRunner(props.config)
-                props._paq.push(['trackEvent', 'scriptRunnerPlugin', 'error_reloadScriptRunnerConfig', props.config.name])
-              }}
-              className="pointer px-2 text-danger d-flex flex-row"
-            >
-              <span className="pr-1 text-danger font-weight-bold">Loading error</span>
-              <span className="text-danger">We are not able to load your requested configuration for now, please try again later.</span>
-            </div>
-          }
-        </div>
+        </label>
       </section>
 
       <section className="d-flex flex-column w-100">
         <div className="mt-2 mb-4 bg-dark p-3 ">
           <p className="text-dark text-monospace">{props.config.description}</p>
           <p className="text-dark">Dependencies</p>
-          <ul className="list-unstyled ">
+          <ul className="list-unstyled m-0">
             <Dependencies />
           </ul>
         </div>
