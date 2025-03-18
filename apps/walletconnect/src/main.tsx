e34f7046849d311ec2a70915ee1da94b5d388fba
ioedeveloper <daviddisu8@gmail.com>
2025-03-18 16:52:54 +0000

    Make walletconnect an internal plugin

--- apps/walletconnect/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client';
-import App from './app/app'
-
-const container = document.getElementById('root');
-
-if (container) {
-  createRoot(container).render(<App />);
-}
