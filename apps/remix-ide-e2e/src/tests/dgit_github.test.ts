4f91fd974fb07127f629f8af669745c64d981f9a
comfsrt <155266597+comfsrt@users.noreply.github.com>
2025-03-19 15:18:04 +0100

    Update dgit_github.test.ts

--- apps/remix-ide-e2e/src/tests/dgit_github.test.ts
+++ apps/remix-ide-e2e/src/tests/dgit_github.test.ts
@@ -31,10 +31,10 @@ module.exports = {
     'login to github #group1 #group2': function (browser: NightwatchBrowser) {
         browser
             .waitForElementVisible('*[data-id="github-panel"]')
-            .waitForElementVisible('*[data-id="gitubUsername"]')
+            .waitForElementVisible('*[data-id="githubUsername"]')
             .setValue('*[data-id="githubToken"]', process.env.dgit_token)
             .pause(1000)
-            .setValue('*[data-id="gitubUsername"]', 'git')
+            .setValue('*[data-id="githubUsername"]', 'git')
             .pause(1000)
             .setValue('*[data-id="githubEmail"]', 'git@example.com')
             .pause(1000)
