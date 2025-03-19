600a7cf2a888f8da194838276377d545f3edba11
comfsrt <155266597+comfsrt@users.noreply.github.com>
2025-03-19 15:16:33 +0100

    Update validateValueInput.ts

--- apps/remix-ide-e2e/src/commands/validateValueInput.ts
+++ apps/remix-ide-e2e/src/commands/validateValueInput.ts
@@ -2,13 +2,13 @@ import { NightwatchBrowser } from 'nightwatch'
 import EventEmitter from 'events'
 
 class ValidateValueInput extends EventEmitter {
-  command (this: NightwatchBrowser, selector: string, valueTosSet: string[], expectedValue: string) {
+  command (this: NightwatchBrowser, selector: string, valueToSet: string[], expectedValue: string) {
     const browser = this.api
     browser.perform((done) => {
       browser
         .click(selector)
         .sendKeys(selector, browser.Keys.BACK_SPACE +  browser.Keys.BACK_SPACE +  browser.Keys.BACK_SPACE + browser.Keys.BACK_SPACE)
-        .sendKeys(selector, valueTosSet)
+        .sendKeys(selector, valueToSet)
         .pause(500)
         .execute(function (selector) {
           const elem = document.querySelector(selector) as HTMLInputElement
