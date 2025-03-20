66e20fd8953870eba77efb0ab3a15ffeb97734d8
bunsenstraat <filip.mertens@ethereum.org>
2025-03-20 12:02:54 +0100

    remix-dapp

--- apps/remix-dapp/webpack.config.js
+++ apps/remix-dapp/webpack.config.js
@@ -1,14 +1,11 @@
-const {composePlugins, withNx} = require('@nrwl/webpack')
-const webpack = require('webpack')
-const TerserPlugin = require('terser-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+const { composePlugins, withNx } = require('@nx/webpack');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
-// Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
-  // add fallback for node modules
+  // Add fallback for node modules
   config.resolve.fallback = {
     ...config.resolve.fallback,
     crypto: require.resolve('crypto-browserify'),
@@ -17,71 +14,86 @@ module.exports = composePlugins(withNx(), (config) => {
     http: require.resolve('stream-http'),
     https: require.resolve('https-browserify'),
     constants: require.resolve('constants-browserify'),
-    os: false, //require.resolve("os-browserify/browser"),
-    timers: false, // require.resolve("timers-browserify"),
     zlib: require.resolve('browserify-zlib'),
+    buffer: require.resolve('buffer/'),
+    vm: require.resolve('vm-browserify'),
+    os: false,
+    timers: false,
     fs: false,
     module: false,
     tls: false,
     net: false,
     readline: false,
     child_process: false,
-    buffer: require.resolve('buffer/'),
-    vm: require.resolve('vm-browserify'),
-  }
+    tty: false,
+    worker_threads: false,  // Explicitly ignore worker_threads
+  };
 
-  // add externals
-  config.externals = {
-    ...config.externals,
-    solc: 'solc',
-  }
+  // Add externals
+  config.externals = { ...config.externals, solc: 'solc' };
 
-  // add public path
-  config.output.publicPath = './'
+  // Set public path
+  config.output.publicPath = './';
 
-  // add copy & provide plugin
+  // Add ProvidePlugin for global variables
   config.plugins.push(
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
       url: ['url', 'URL'],
       process: 'process/browser',
     })
-  )
+  );
 
-  // set the define plugin to load the WALLET_CONNECT_PROJECT_ID
+  // Define WALLET_CONNECT_PROJECT_ID
   config.plugins.push(
     new webpack.DefinePlugin({
       WALLET_CONNECT_PROJECT_ID: JSON.stringify(process.env.WALLET_CONNECT_PROJECT_ID),
     })
-  )
+  );
 
+  // Fix Webpack Manifest Plugin issue by ensuring it uses compiler hooks properly
   config.plugins.push(
     new WebpackManifestPlugin({
       fileName: 'manifest.json',
       publicPath: '/',
-      generate: (seed, files, entrypoints) => {
-        const manifest = files.reduce((manifest, { name, path }) => {
+      generate: (seed, files) =>
+        files.reduce((manifest, { name, path }) => {
           manifest[name] = path;
           return manifest;
-        }, seed);
-        return manifest;
-      },
-      filter: (file) => {
-        return !(file.path.includes('assets') || file.path.includes('.map'));
-      },
+        }, seed),
+      filter: (file) => !file.path.includes('assets') && !file.path.includes('.map'),
     })
-  )
+  );
 
-  // source-map loader
+  // Add source-map loader
   config.module.rules.push({
     test: /\.js$/,
     use: ['source-map-loader'],
     enforce: 'pre',
-  })
+  });
 
-  config.ignoreWarnings = [/Failed to parse source map/] // ignore source-map-loader warnings
+  config.ignoreWarnings = [/Failed to parse source map/]; // Ignore source-map-loader warnings
 
-  // set minimizer
+  // Set CSS module rules handler
+  config.module.rules.push({
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: true, // Enables CSS modules automatically for `.module.css` files
+            localIdentName: '[name]__[local]--[hash:base64:5]', // Scoped class names
+          },
+          importLoaders: 1,
+        },
+      },
+      'postcss-loader',
+    ],
+  });
+
+  // Set minimizer
   config.optimization.minimizer = [
     new TerserPlugin({
       parallel: true,
@@ -89,20 +101,16 @@ module.exports = composePlugins(withNx(), (config) => {
         ecma: 2015,
         compress: false,
         mangle: false,
-        format: {
-          comments: false,
-        },
+        format: { comments: false },
       },
       extractComments: false,
     }),
     new CssMinimizerPlugin(),
-  ]
+  ];
 
-  config.watchOptions = {
-    ignored: /node_modules/,
-  }
+  config.watchOptions = { ignored: /node_modules/ };
 
-  config.experiments.syncWebAssembly = true
+  config.experiments.syncWebAssembly = true;
 
-  return config
-})
+  return config;
+});
