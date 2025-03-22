0801c75d78901e5369bcd0eaa83936625b560b58
yann300 <yann.levreau@gmail.com>
2025-03-22 21:03:59 +0100

    fix dumping state with latest tx

--- apps/remix-ide/src/blockchain/blockchain.tsx
+++ apps/remix-ide/src/blockchain/blockchain.tsx
@@ -1024,10 +1024,7 @@ export class Blockchain extends Plugin {
                 let stateDetails = await this.call('fileManager', 'readFile', provider.config.statePath)
                 stateDetails = JSON.parse(stateDetails)
                 state = JSON.parse(state)
-                state['stateName'] = stateDetails.stateName
-                state['forkName'] = stateDetails.forkName
-                state['savingTimestamp'] = stateDetails.savingTimestamp
-                state = JSON.stringify(state, null, 2)
+                state = JSON.stringify({...stateDetails, ...(state as any)}, null, 2)
               }
               this.call('fileManager', 'writeFile', provider.config.statePath, state)
             } else if (isBasicVMState && !isForkedRpcState && !isForkedRpcState) {
