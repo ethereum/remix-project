97e070a1c1e0949620f634df3322d59bffe1f381
ioedeveloper <daviddisu8@gmail.com>
2025-04-02 13:11:05 +0100

    Fix wallet connect modal closing issue

--- apps/remix-ide/src/blockchain/execution-context.js
+++ apps/remix-ide/src/blockchain/execution-context.js
@@ -149,9 +149,6 @@ export class ExecutionContext {
     if (this.customNetWorks[context]) {
       var network = this.customNetWorks[context]
       await network.init()
-      if (context === 'walletconnect') {
-        await network.provider.udapp.call('walletconnect', 'openModal')
-      }
       this.currentFork = network.fork
       this.executionContext = context
       // injected
