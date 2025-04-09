9a92db21846cf90eb2ff0f7db2081cc237bf51e2
bunsenstraat <filip.mertens@ethereum.org>
2025-04-09 20:22:11 +0200

    lint

--- libs/remix-lib/src/helpers/endpointHelper.ts
+++ libs/remix-lib/src/helpers/endpointHelper.ts
@@ -15,52 +15,51 @@ type EndpointUrls = {
 };
 
 const defaultUrls: EndpointUrls = {
-    corsProxy: 'https://corsproxy.remixproject.org',
-    solidityScan: 'https://solidityscan.remixproject.org',
-    ipfsGateway: 'https://jqgt.remixproject.org',
-    commonCorsProxy: 'https://common-corsproxy.remixproject.org',
-    github: 'https://github.remixproject.org',
-    solcoder: 'https://solcoder.remixproject.org',
-    ghfolderpull: 'https://ghfolderpull.remixproject.org',
-    embedly: 'https://embedly.remixproject.org',
-    gptChat: 'https://gpt-chat.remixproject.org',
-    rag: 'https://rag.remixproject.org',
-    vyper2: 'https://vyper2.remixproject.org',
-    completion: 'https://completion.remixproject.org',
-    solidityScanWebSocket: 'wss://solidityscan.remixproject.org',
+  corsProxy: 'https://corsproxy.remixproject.org',
+  solidityScan: 'https://solidityscan.remixproject.org',
+  ipfsGateway: 'https://jqgt.remixproject.org',
+  commonCorsProxy: 'https://common-corsproxy.remixproject.org',
+  github: 'https://github.remixproject.org',
+  solcoder: 'https://solcoder.remixproject.org',
+  ghfolderpull: 'https://ghfolderpull.remixproject.org',
+  embedly: 'https://embedly.remixproject.org',
+  gptChat: 'https://gpt-chat.remixproject.org',
+  rag: 'https://rag.remixproject.org',
+  vyper2: 'https://vyper2.remixproject.org',
+  completion: 'https://completion.remixproject.org',
+  solidityScanWebSocket: 'wss://solidityscan.remixproject.org',
 };
 
-
 const endpointPathMap: Record<keyof EndpointUrls, string> = {
-    corsProxy: 'corsproxy',
-    solidityScan: 'solidityscan',
-    ipfsGateway: 'jqgt',
-    commonCorsProxy: 'common-corsproxy',
-    github: 'github',
-    solcoder: 'solcoder',
-    completion: 'completion',
-    ghfolderpull: 'ghfolderpull',
-    embedly: 'embedly',
-    gptChat: 'gpt-chat',
-    rag: 'rag',
-    vyper2: 'vyper2',
-    solidityScanWebSocket: '',
-  };
+  corsProxy: 'corsproxy',
+  solidityScan: 'solidityscan',
+  ipfsGateway: 'jqgt',
+  commonCorsProxy: 'common-corsproxy',
+  github: 'github',
+  solcoder: 'solcoder',
+  completion: 'completion',
+  ghfolderpull: 'ghfolderpull',
+  embedly: 'embedly',
+  gptChat: 'gpt-chat',
+  rag: 'rag',
+  vyper2: 'vyper2',
+  solidityScanWebSocket: '',
+};
 
 const prefix = process.env.NX_ENDPOINTS_URL;
 
 const resolvedUrls: EndpointUrls = prefix
   ? Object.fromEntries(
-      Object.entries(defaultUrls).map(([key, _]) => [
-        key,
-        `${prefix}/${endpointPathMap[key as keyof EndpointUrls]}`,
-      ])
-    ) as EndpointUrls
+    Object.entries(defaultUrls).map(([key, _]) => [
+      key,
+      `${prefix}/${endpointPathMap[key as keyof EndpointUrls]}`,
+    ])
+  ) as EndpointUrls
   : defaultUrls;
 
- resolvedUrls.solidityScanWebSocket = resolvedUrls.solidityScan.replace(
-    'http://',
-    'ws://'
-  );
+resolvedUrls.solidityScanWebSocket = resolvedUrls.solidityScan.replace(
+  'http://',
+  'ws://'
+);
 
 export const endpointUrls = resolvedUrls;
