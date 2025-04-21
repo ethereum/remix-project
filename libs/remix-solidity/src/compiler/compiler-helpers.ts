6834a2e434f822bd2f365084d9df26b57d497d69
yann300 <yann.levreau@gmail.com>
2025-04-22 00:30:09 +0200

    consistent return value

--- libs/remix-solidity/src/compiler/compiler-helpers.ts
+++ libs/remix-solidity/src/compiler/compiler-helpers.ts
@@ -3,16 +3,18 @@ import { canUseWorker, urlFromVersion } from './compiler-utils'
 import { CompilerAbstract } from './compiler-abstract'
 import { Compiler } from './compiler'
 
-export const compile = (compilationTargets, settings, contentResolverCallback): Promise<CompilerAbstract> => {
+export const compile = (compilationTargets, config, contentResolverCallback): Promise<CompilerAbstract> => {
   return new Promise((resolve, reject) => {
     const compiler = new Compiler(contentResolverCallback)
-    compiler.set('evmVersion', settings.evmVersion)
-    compiler.set('optimize', settings.optimize)
-    compiler.set('language', settings.language)
-    compiler.set('runs', settings.runs)
-    compiler.loadVersion(canUseWorker(settings.version), urlFromVersion(settings.version))
+    compiler.set('evmVersion', config.settings?.evmVersion)
+    compiler.set('optimize', config.settings?.optimizer?.enabled)
+    compiler.set('language', config.language)
+    compiler.set('runs', config.settings?.optimizer?.runs)
+    compiler.set('remappings', config.settings?.remappings)
+    compiler.set('viaIR', config.settings?.viaIR)
+    compiler.loadVersion(canUseWorker(config.version), urlFromVersion(config.version))
     compiler.event.register('compilationFinished', (success, compilationData, source, input, version) => {
-      resolve(new CompilerAbstract(settings.version, compilationData, source, input))
+      resolve(new CompilerAbstract(config.version, compilationData, source, input))
     })
     compiler.event.register('compilerLoaded', _ => compiler.compile(compilationTargets, ''))
   })
