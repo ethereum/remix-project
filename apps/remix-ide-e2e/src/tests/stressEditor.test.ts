cc5728eaec9805053b8c16793eb2bd4f79911250
emmmm <155267286+eeemmmmmm@users.noreply.github.com>
2025-04-22 17:55:19 +0200

    Update stressEditor.test.ts

--- apps/remix-ide-e2e/src/tests/stressEditor.test.ts
+++ apps/remix-ide-e2e/src/tests/stressEditor.test.ts
@@ -204,7 +204,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title Ballot
- * @dev Implements voting process along with vote delegation|
+ * @dev Implements voting process along with vote delegation
  */
 contract Ballot {
    
