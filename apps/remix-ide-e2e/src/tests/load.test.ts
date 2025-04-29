76bb3e3d974be9f89719571daa050c6cccddc1a0
bunsenstraat <filip.mertens@ethereum.org>
2025-04-29 08:16:36 +0200

    connection reset test

---
+++ apps/remix-ide-e2e/src/tests/load.test.ts
@@ -0,0 +1,15 @@
+'use strict'
+import { NightwatchBrowser } from 'nightwatch'
+import init from '../helpers/init'
+
+module.exports = {
+    "@disable": true,
+    before: function (browser: NightwatchBrowser, done: VoidFunction) {
+        init(browser, done)
+    },
+
+    "test #flaky": function (browser: NightwatchBrowser) {
+        browser.pause(10000)
+    }
+
+}
