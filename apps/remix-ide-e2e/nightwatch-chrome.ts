35d9d058e268eeeccda98adf7f1f32e3d8054b6e
bunsenstraat <filip.mertens@ethereum.org>
2025-05-06 08:06:54 +0200

    enable rasterizer

--- apps/remix-ide-e2e/nightwatch-chrome.ts
+++ apps/remix-ide-e2e/nightwatch-chrome.ts
@@ -49,7 +49,6 @@ module.exports = {
             '--headless=new',
             '--verbose',
             '--disable-dev-shm-usage',
-            '--disable-software-rasterizer',
             '--disable-gpu',
             '--remote-debugging-pipe',
             '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
