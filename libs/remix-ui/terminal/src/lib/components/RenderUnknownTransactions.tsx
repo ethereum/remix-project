90ebc562ae48dcd2c917b185166ebf1bb6abfad8
aniket-engg <aniketengg.225@gmail.com>
2025-05-07 16:33:48 +0530

    update known tx too

--- libs/remix-ui/terminal/src/lib/components/RenderUnknownTransactions.tsx
+++ libs/remix-ui/terminal/src/lib/components/RenderUnknownTransactions.tsx
@@ -27,7 +27,6 @@ const RenderUnKnownTransactions = ({ tx, receipt, index, plugin, showTableHash, 
   let to = tx.to
 
   if (tx.isUserOp) {
-    console.log(receipt)
     // Track event with signature: ExecutionFromModuleSuccess (index_topic_1 address module)
     // to get sender smart account address
     const fromAddrLog = receipt.logs.find(e => e.topics[0] === "0x6895c13664aa4f67288b25d7a21d7aaa34916e355fb9b6fae0a139a9085becb8")
