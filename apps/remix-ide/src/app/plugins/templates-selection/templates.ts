857130bf4df03740100e10def46434bf344dc27f
aniket-engg <aniketengg.225@gmail.com>
2025-05-08 16:31:17 +0530

    v0.64.0 release highlights

--- apps/remix-ide/src/app/plugins/templates-selection/templates.ts
+++ apps/remix-ide/src/app/plugins/templates-selection/templates.ts
@@ -5,8 +5,8 @@ export const templates = (intl, plugin) => {
       items: [
         { value: "remixDefault", tagList: ["Solidity"], displayName: intl.formatMessage({ id: 'filePanel.basic' }), description: 'The default project' },
         { value: "blank", displayName: intl.formatMessage({ id: 'filePanel.blank' }), IsArtefact: true, description: 'A blank project' },
-        { value: "simpleEip7702", displayName: 'Simple EIP 7702', IsArtefact: true, description: 'Pectra upgrade allowing externally owner accounts (EOAs) to run contract code.' },
-        { value: "accountAbstraction", displayName: 'Account Abstraction Template', IsArtefact: true, description: 'Experiment with Account Abstraction contracts: (ERC-4337, EIP-7702)' }
+        { value: "simpleEip7702", displayName: 'Simple EIP 7702', IsArtefact: true, description: 'Pectra upgrade allowing externally owned accounts (EOAs) to run contract code.' },
+        { value: "accountAbstraction", displayName: 'Account Abstraction', IsArtefact: true, description: 'Experiment with Account Abstraction contracts: (ERC-4337, EIP-7702)' }
       ]
     },
     {
