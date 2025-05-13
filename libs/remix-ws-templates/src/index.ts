45cd6587bd5b7bc70a4eb534d0921fde2034f0d8
yann300 <yann.levreau@gmail.com>
2025-05-13 16:34:47 +0200

    add fully fledge eip7702 template

--- libs/remix-ws-templates/src/index.ts
+++ libs/remix-ws-templates/src/index.ts
@@ -1,5 +1,6 @@
 export { default as remixDefault } from './templates/remixDefault'
 export { default as simpleEip7702 } from './templates/simpleEip7702'
+export { default as fullyFledgeEIP7702 } from './templates/fullyFledgeEIP7702'
 export { default as blank } from './templates/blank'
 export { default as ozerc20 } from './templates/ozerc20'
 export { default as ozerc721 } from './templates/ozerc721'
