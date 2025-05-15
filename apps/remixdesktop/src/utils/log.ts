e192065b752c4210569f0bb112d766ffd5f454b5
bunsenstraat <filip.mertens@ethereum.org>
2025-05-15 07:09:56 +0200

    run chromedriver direct

--- apps/remixdesktop/src/utils/log.ts
+++ apps/remixdesktop/src/utils/log.ts
@@ -1,13 +1,13 @@
 const isDevPromise = import('electron-is-dev').then(module => module.default).catch(() => false);
 
 const isDev = async () => {
-    return (await isDevPromise || process.env.NODE_ENV === 'development' || process.argv.includes('--debug')) && !process.argv.includes('--no-log');
+    return (await isDevPromise || process.env.NODE_ENV === 'development' || process.argv.includes('--inspect')) && !process.argv.includes('--no-log');
 };
 
 
 (async () => {
     if (!await isDev()) {
-        console.log('Production mode, logs are disabled. Eneble with --debug');
+        console.log('Production mode, logs are disabled. Enable with --inspect');
         console.log = console.warn = console.error = console.info = () => { };
     }
 })();
