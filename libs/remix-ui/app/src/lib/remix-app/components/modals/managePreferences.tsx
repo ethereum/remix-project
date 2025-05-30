752c9e4810f009695680e72c8fae17d0680ccf62
aniket-engg <aniketengg.225@gmail.com>
2025-05-30 15:29:12 +0530

    update switcher position

--- libs/remix-ui/app/src/lib/remix-app/components/modals/managePreferences.tsx
+++ libs/remix-ui/app/src/lib/remix-app/components/modals/managePreferences.tsx
@@ -15,48 +15,36 @@ const ManagePreferencesSwitcher = (prop: {
   setParentState: (state: any) => void
 }) => {
   const [remixAISwitch, setRemixAISwitch] = useState(true)
-  const [matPerfSwitch, setMatPerfSwitch] = useState(false)
-  const [matAnonSwitch, setMatAnonSwitch] = useState(false)
+  const [matPerfSwitch, setMatPerfSwitch] = useState(true)
   
 
   useEffect(() => {
     prop.setParentState({
       remixAISwitch,
-      matPerfSwitch,
-      matAnonSwitch
+      matPerfSwitch
     })
-  }, [remixAISwitch, matPerfSwitch, matAnonSwitch])
+  }, [remixAISwitch, matPerfSwitch])
 
   return (
       <>
-        <div data-id="remixAI" className='justify-content-between d-flex'>
+        <div data-id="matomoAnonAnalytics" className='justify-content-between d-flex'>
           <div className='mt-2'>
-            <h6><FormattedMessage id="remixApp.mpOp1Title" /></h6>
-            <p className='form-check-label'><FormattedMessage id="remixApp.mpOp1Details" /></p>
-            <p className='mt-1'><FormattedMessage
-              id="remixApp.mpOp1Link"
-              values={{
-                a: (chunks) => (
-                  <a className="text-primary" href="https://matomo.org" target="_blank" rel="noreferrer">
-                    {chunks}
-                  </a>
-                ),
-              }}
-            /></p>
+            <h6 className='text-secondary'><FormattedMessage id="remixApp.mpOp1Title" /></h6>
+            <p className='form-check-label text-secondary'><FormattedMessage id="remixApp.mpOp1Details" /></p>
           </div>
           <div>
             <button
-              data-id="remixAISwitch"
-              id='remixAISwitch'
+              data-id="matomoAnonAnalyticsSwitch"
+              id='matomoAnonAnalyticsSwitch'
               className="btn text-ai"
-              onClick={() => setRemixAISwitch(!remixAISwitch)}
+              disabled
             >
-              { remixAISwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
+              <i className="fas fa-toggle-on fa-2xl"></i>
             </button>
           </div>
         </div>
-        <div data-id="matomoAnonAnalytics" className='justify-content-between d-flex'>
-          <div className='mt-2'>
+        <div data-id="matomoPerfAnalytics" className='justify-content-between d-flex'>
+          <div className='mt-3'>
             <h6><FormattedMessage id="remixApp.mpOp2Title" /></h6>
             <p className='form-check-label'><FormattedMessage id="remixApp.mpOp2Details" /></p>
             <p className='mt-1'><FormattedMessage
@@ -72,28 +60,38 @@ const ManagePreferencesSwitcher = (prop: {
           </div>
           <div>
             <button
-              data-id="matomoAnonAnalyticsSwitch"
-              id='matomoAnonAnalyticsSwitch'
-              className="btn text-ai"
-              onClick={() => setMatAnonSwitch(!matAnonSwitch)}
-            >
-              { matAnonSwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
-            </button>
-          </div>
-        </div>
-        <div data-id="matomoPerfAnalytics" className='justify-content-between d-flex'>
-          <div className='mt-2'>
-            <h6 className='text-secondary'><FormattedMessage id="remixApp.mpOp3Title" /></h6>
-            <p className='form-check-label text-secondary'><FormattedMessage id="remixApp.mpOp3Details" /></p>
-          </div>
-          <div>
-            <button
               data-id="matomoPerfAnalyticsSwitch"
               id='matomoPerfAnalyticsSwitch'
-              className="btn text-secondary"
+              className="btn text-ai mt-1"
               onClick={() => setMatPerfSwitch(!matPerfSwitch)}
             >
               { matPerfSwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
+            </button>
+          </div>
+        </div>
+        <div data-id="remixAI" className='justify-content-between d-flex'>
+          <div className='mt-2'>
+            <h6><FormattedMessage id="remixApp.mpOp3Title" /></h6>
+            <p className='form-check-label'><FormattedMessage id="remixApp.mpOp3Details" /></p>
+            <p className='mt-1'><FormattedMessage
+              id="remixApp.mpOp3Link"
+              values={{
+                a: (chunks) => (
+                  <a className="text-primary" href="https://remix-ide.readthedocs.io/en/latest/ai.html" target="_blank" rel="noreferrer">
+                    {chunks}
+                  </a>
+                ),
+              }}
+            /></p>
+          </div>
+          <div>
+            <button
+              data-id="remixAISwitch"
+              id='remixAISwitch'
+              className="btn text-ai"
+              onClick={() => setRemixAISwitch(!remixAISwitch)}
+            >
+              { remixAISwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
             </button>
           </div>
         </div>
@@ -116,8 +114,6 @@ const ManagePreferencesDialog = (props: ManagePreferencesDialogProps) => {
         }} />,
         okLabel: <FormattedMessage id="remixApp.savePreferences" />,
         okFn: savePreferences,
-        cancelLabel: <FormattedMessage id="remixApp.declineCookies" />,
-        cancelFn: () => {},
         showCancelIcon: true,
         preventBlur: true
       })
