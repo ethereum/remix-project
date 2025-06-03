e9d65faaf6d965a440fadfbdaa3c13a8d5f7e2c3
aniket-engg <aniketengg.225@gmail.com>
2025-06-03 13:53:15 +0530

    fix linting

--- libs/remix-ui/app/src/lib/remix-app/components/modals/managePreferences.tsx
+++ libs/remix-ui/app/src/lib/remix-app/components/modals/managePreferences.tsx
@@ -10,15 +10,11 @@ declare global {
 }
 const _paq = (window._paq = window._paq || [])
 
-interface ManagePreferencesDialogProps {
-}
-
 const ManagePreferencesSwitcher = (prop: {
   setParentState: (state: any) => void
 }) => {
   const [remixAISwitch, setRemixAISwitch] = useState(true)
   const [matPerfSwitch, setMatPerfSwitch] = useState(true)
-  
 
   useEffect(() => {
     prop.setParentState({
@@ -28,92 +24,92 @@ const ManagePreferencesSwitcher = (prop: {
   }, [remixAISwitch, matPerfSwitch])
 
   return (
-      <>
-        <div data-id="matomoAnonAnalytics" className='justify-content-between d-flex'>
-          <div className='mt-2'>
-            <h6 className='text-secondary'><FormattedMessage id="remixApp.mpOp1Title" /></h6>
-            <p className='form-check-label text-secondary'><FormattedMessage id="remixApp.mpOp1Details" /></p>
-          </div>
-          <div>
-            <CustomTooltip
+    <>
+      <div data-id="matomoAnonAnalytics" className='justify-content-between d-flex'>
+        <div className='mt-2'>
+          <h6 className='text-secondary'><FormattedMessage id="remixApp.mpOp1Title" /></h6>
+          <p className='form-check-label text-secondary'><FormattedMessage id="remixApp.mpOp1Details" /></p>
+        </div>
+        <div>
+          <CustomTooltip
             placement={"auto"}
             tooltipId="matomoAnonAnalyticsTooltip"
             tooltipClasses="text-nowrap"
             tooltipText={<FormattedMessage id="remixApp.mpOp1Tooltip" />}
-            >
-              <button
-                data-id="matomoAnonAnalyticsSwitch"
-                id='matomoAnonAnalyticsSwitch'
-                className="btn text-ai"
-                disabled
-              >
-                <i className="fas fa-toggle-on fa-2xl"></i>
-              </button>
-            </CustomTooltip>
-          </div>
-        </div>
-        <div data-id="matomoPerfAnalytics" className='justify-content-between d-flex'>
-          <div className='mt-3'>
-            <h6><FormattedMessage id="remixApp.mpOp2Title" /></h6>
-            <p className='form-check-label'><FormattedMessage id="remixApp.mpOp2Details" /></p>
-            <p className='mt-1'><FormattedMessage
-              id="remixApp.mpOp2Link"
-              values={{
-                a: (chunks) => (
-                  <a className="text-primary" href="https://matomo.org" target="_blank" rel="noreferrer">
-                    {chunks}
-                  </a>
-                ),
-              }}
-            /></p>
-          </div>
-          <div>
+          >
             <button
-              data-id="matomoPerfAnalyticsSwitch"
-              id='matomoPerfAnalyticsSwitch'
-              className="btn text-ai mt-1"
-              onClick={() => setMatPerfSwitch(!matPerfSwitch)}
-            >
-              { matPerfSwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
-            </button>
-          </div>
-        </div>
-        <div data-id="remixAI" className='justify-content-between d-flex'>
-          <div className='mt-2'>
-            <h6><FormattedMessage id="remixApp.mpOp3Title" /></h6>
-            <p className='form-check-label'><FormattedMessage id="remixApp.mpOp3Details" /></p>
-            <p className='mt-1'><FormattedMessage
-              id="remixApp.mpOp3Link"
-              values={{
-                a: (chunks) => (
-                  <a className="text-primary" href="https://remix-ide.readthedocs.io/en/latest/ai.html" target="_blank" rel="noreferrer">
-                    {chunks}
-                  </a>
-                ),
-              }}
-            /></p>
-          </div>
-          <div>
-            <button
-              data-id="remixAISwitch"
-              id='remixAISwitch'
+              data-id="matomoAnonAnalyticsSwitch"
+              id='matomoAnonAnalyticsSwitch'
               className="btn text-ai"
-              onClick={() => setRemixAISwitch(!remixAISwitch)}
+              disabled
             >
-              { remixAISwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
+              <i className="fas fa-toggle-on fa-2xl"></i>
             </button>
-          </div>
+          </CustomTooltip>
         </div>
-      </>
-    )
+      </div>
+      <div data-id="matomoPerfAnalytics" className='justify-content-between d-flex'>
+        <div className='mt-3'>
+          <h6><FormattedMessage id="remixApp.mpOp2Title" /></h6>
+          <p className='form-check-label'><FormattedMessage id="remixApp.mpOp2Details" /></p>
+          <p className='mt-1'><FormattedMessage
+            id="remixApp.mpOp2Link"
+            values={{
+              a: (chunks) => (
+                <a className="text-primary" href="https://matomo.org" target="_blank" rel="noreferrer">
+                  {chunks}
+                </a>
+              ),
+            }}
+          /></p>
+        </div>
+        <div>
+          <button
+            data-id="matomoPerfAnalyticsSwitch"
+            id='matomoPerfAnalyticsSwitch'
+            className="btn text-ai mt-1"
+            onClick={() => setMatPerfSwitch(!matPerfSwitch)}
+          >
+            { matPerfSwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
+          </button>
+        </div>
+      </div>
+      <div data-id="remixAI" className='justify-content-between d-flex'>
+        <div className='mt-2'>
+          <h6><FormattedMessage id="remixApp.mpOp3Title" /></h6>
+          <p className='form-check-label'><FormattedMessage id="remixApp.mpOp3Details" /></p>
+          <p className='mt-1'><FormattedMessage
+            id="remixApp.mpOp3Link"
+            values={{
+              a: (chunks) => (
+                <a className="text-primary" href="https://remix-ide.readthedocs.io/en/latest/ai.html" target="_blank" rel="noreferrer">
+                  {chunks}
+                </a>
+              ),
+            }}
+          /></p>
+        </div>
+        <div>
+          <button
+            data-id="remixAISwitch"
+            id='remixAISwitch'
+            className="btn text-ai"
+            onClick={() => setRemixAISwitch(!remixAISwitch)}
+          >
+            { remixAISwitch ? <i className="fas fa-toggle-on fa-2xl"></i> : <i className="fas fa-toggle-off fa-2xl"></i> }
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
-const ManagePreferencesDialog = (props: ManagePreferencesDialogProps) => {
+const ManagePreferencesDialog = () => {
   const { modal } = useDialogDispatchers()
-  let { settings } = useContext(AppContext)
+  const { settings } = useContext(AppContext)
   const [visible, setVisible] = useState<boolean>(true)
-  let switcherState = useRef<Record<string, any>>(null)
-  
+  const switcherState = useRef<Record<string, any>>(null)
+
   useEffect(() => {
     if (visible) {
       modal({
