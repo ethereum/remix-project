378765e177b3cec284e01b0910950db1c85f1815
bunsenstraat <filip.mertens@ethereum.org>
2025-06-16 13:31:14 +0200

    lint

--- apps/remix-ide/src/index.tsx
+++ apps/remix-ide/src/index.tsx
@@ -10,25 +10,25 @@ import { Storage } from '@remix-project/remix-lib'
 
 import { createRoot } from 'react-dom/client'
 
-  ; (async function () {
-    try {
-      const configStorage = new Storage('config-v0.8:')
-      const config = new Config(configStorage)
-      Registry.getInstance().put({ api: config, name: 'config' })
-    } catch (e) { }
-    const theme = new ThemeModule()
-    theme.initTheme()
+; (async function () {
+  try {
+    const configStorage = new Storage('config-v0.8:')
+    const config = new Config(configStorage)
+    Registry.getInstance().put({ api: config, name: 'config' })
+  } catch (e) { }
+  const theme = new ThemeModule()
+  theme.initTheme()
 
-    const container = document.getElementById('root');
-    const root = createRoot(container)
-    if (container) {
-      if (window.location.hash.includes('source=github')) {
-        root.render(
-          <GitHubPopupCallback />
-        )
-      } else {
-        root.render(
-          <Preload root={root} />)
-      }
+  const container = document.getElementById('root');
+  const root = createRoot(container)
+  if (container) {
+    if (window.location.hash.includes('source=github')) {
+      root.render(
+        <GitHubPopupCallback />
+      )
+    } else {
+      root.render(
+        <Preload root={root} />)
     }
-  })()
+  }
+})()
