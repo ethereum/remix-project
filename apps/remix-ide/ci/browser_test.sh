8e9a83c2a7ff01f6decf2ddd3a1ef31d65e95503
bunsenstraat <2787300+bunsenstraat@users.noreply.github.com>
2025-07-01 13:20:16 +0200

    fail faster

--- apps/remix-ide/ci/browser_test.sh
+++ apps/remix-ide/ci/browser_test.sh
@@ -17,11 +17,18 @@ sleep 5
 node apps/remix-ide/ci/splice_tests.js $2 $3
 TESTFILES=$(node apps/remix-ide/ci/splice_tests.js $2 $3 | grep -v 'metamask' | circleci tests split --split-by=timings)
 for TESTFILE in $TESTFILES; do
-    npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch-${1}.js dist/apps/remix-ide-e2e/src/tests/${TESTFILE}.js --env=$1 || npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch-${1}.js dist/apps/remix-ide-e2e/src/tests/${TESTFILE}.js --env=$1 || TEST_EXITCODE=1
+    if ! npx nightwatch --config dist/apps/remix-ide-e2e/nightwatch-${1}.js dist/apps/remix-ide-e2e/src/tests/${TESTFILE}.js --env=$1; then
+        TEST_EXITCODE=1
+        break
+    fi
 done
 
 echo "$TEST_EXITCODE"
-if [ "$TEST_EXITCODE" -eq 1 ]
-then
+# Fail the test early and cancel the workflow
+if [ "$TEST_EXITCODE" -eq 1 ]; then
+  echo "❌ Test failed. Attempting to cancel the workflow..."
+  curl -s -X POST "https://circleci.com/api/v2/workflow/${CIRCLE_WORKFLOW_ID}/cancel" \
+    -H "Circle-Token: $FAIL_FAST_TOKEN" \
+    -H "Content-Type: application/json"
   exit 1
 fi
