02a71023e0acb43c6ac2fa679d26c0b7e69a3080
Joseph Izang <josephizang@gmail.com>
2025-07-11 01:40:28 +0100

    add matomo

--- apps/remix-ide/src/app/plugins/remix-ai-assistant.tsx
+++ apps/remix-ide/src/app/plugins/remix-ai-assistant.tsx
@@ -103,7 +103,7 @@ export class RemixAIAssistant extends ViewPlugin {
   }
 
   async handleActivity(type: string, payload: any) {
-    _paq.push([''])
+    (window as any)._paq?.push(['trackEvent', 'remixai-assistant', `${type}-${payload}`])
   }
 
   updateComponent(state: {
