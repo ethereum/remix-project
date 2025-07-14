8a5f5fee1241b01c84b30fc048c6197febd2b7d6
STetsing <41009393+STetsing@users.noreply.github.com>
2025-07-14 12:50:31 +0200

    rm all gpt reqs from terminal

--- libs/remix-ai-core/src/prompts/promptBuilder.ts
+++ libs/remix-ai-core/src/prompts/promptBuilder.ts
@@ -3,18 +3,8 @@ import { ChatHistory } from "./chat"
 export const buildChatPrompt = (userPrompt) => {
   const history = []
   for (const [question, answer] of ChatHistory.getHistory()) {
-    if (question.startsWith('sol-gpt')) {
-      history.push({ role:'user', content:question.split('sol-gpt')[1] })
-      history.push({ role:'assistant' , content: answer })
-    }
-    else if (question.startsWith('gpt')) {
-      history.push({ role:'user', content:question.split('gpt')[1] })
-      history.push({ role:'assistant' , content: answer })
-    }
-    else {
-      history.push({ role:'user', content: question })
-      history.push({ role:'assistant' , content: answer })
-    }
+    history.push({ role:'user', content: question })
+    history.push({ role:'assistant' , content: answer })
   }
   history.push({ role: 'user', content: userPrompt })
   return history
