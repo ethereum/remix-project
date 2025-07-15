c28aa6bfeadd6f989055c41c303a43d298f0618c
aniket-engg <aniketengg.225@gmail.com>
2025-07-15 21:42:43 +0530

    keep only light and dark

--- apps/remix-dapp/src/components/SettingsUI/theme.tsx
+++ apps/remix-dapp/src/components/SettingsUI/theme.tsx
@@ -4,32 +4,7 @@ import { FormattedMessage } from 'react-intl';
 const themeMap: Record<string, any> = {
   Dark: { quality: 'dark', url: 'assets/css/themes/remix-dark_tvx1s2.css' },
   Light: { quality: 'light', url: 'assets/css/themes/remix-light_powaqg.css' },
-  Violet: { quality: 'light', url: 'assets/css/themes/remix-violet.css' },
-  Unicorn: { quality: 'light', url: 'assets/css/themes/remix-unicorn.css' },
-  Midcentury: {
-    quality: 'light',
-    url: 'assets/css/themes/remix-midcentury_hrzph3.css',
-  },
-  Black: { quality: 'dark', url: 'assets/css/themes/remix-black_undtds.css' },
-  Candy: { quality: 'light', url: 'assets/css/themes/remix-candy_ikhg4m.css' },
-  HackerOwl: { quality: 'dark', url: 'assets/css/themes/remix-hacker_owl.css' },
-  Cerulean: {
-    quality: 'light',
-    url: 'assets/css/themes/bootstrap-cerulean.min.css',
-  },
-  Flatly: {
-    quality: 'light',
-    url: 'assets/css/themes/bootstrap-flatly.min.css',
-  },
-  Spacelab: {
-    quality: 'light',
-    url: 'assets/css/themes/bootstrap-spacelab.min.css',
-  },
-  Cyborg: {
-    quality: 'dark',
-    url: 'assets/css/themes/bootstrap-cyborg.min.css',
-  },
-};
+}
 
 export function ThemeUI() {
   const [theme, setTheme] = useState(
