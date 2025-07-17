a7cec2e25d449c10bc0695db32430b36d930d12d
Joseph Izang <josephizang@gmail.com>
2025-07-17 15:09:21 +0100

    add topbar provider and inject filePanel into topbar

--- libs/remix-ui/top-bar/src/context/topbarContext.tsx
+++ libs/remix-ui/top-bar/src/context/topbarContext.tsx
@@ -1,8 +1,9 @@
+import { Topbar } from 'apps/remix-ide/src/app/components/top-bar'
 import { createContext, SyntheticEvent } from 'react'
 
-export const FileSystemContext = createContext<{
+export const TopbarContext = createContext<{
   fs: any,
-  plugin: any,
+  plugin: Topbar,
   modal:(title: string | JSX.Element, message: string | JSX.Element, okLabel: string, okFn: () => void, cancelLabel?: string, cancelFn?: () => void) => void,
   dispatchInitWorkspace:() => Promise<void>,
   dispatchFetchDirectory:(path: string) => Promise<void>,
