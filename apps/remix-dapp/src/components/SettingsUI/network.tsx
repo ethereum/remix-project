286dbce954f9975268552f76bfbadc4c3a3e2b2c
aniket-engg <aniketengg.225@gmail.com>
2025-07-17 19:12:02 +0530

    badge class

--- apps/remix-dapp/src/components/SettingsUI/network.tsx
+++ apps/remix-dapp/src/components/SettingsUI/network.tsx
@@ -35,7 +35,7 @@ export function NetworkUI() {
         </select>
       </div>
       <div className="position-relative w-100" data-id="settingsNetworkEnv">
-        <span className="badge badge-secondary">{networkName}</span>
+        <span className="badge bg-secondary">{networkName}</span>
       </div>
       {/* {provider === 'walletconnect' && (
         <div className="mt-2">
