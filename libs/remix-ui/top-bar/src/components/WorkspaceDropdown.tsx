9b67ed4d68f98286cd4c6f55ec5334ce1701f4d9
Joseph Izang <josephizang@gmail.com>
2025-07-23 22:00:35 +0100

    update Workspace dropdown component for topbar

--- libs/remix-ui/top-bar/src/components/WorkspaceDropdown.tsx
+++ libs/remix-ui/top-bar/src/components/WorkspaceDropdown.tsx
@@ -1,3 +1,4 @@
+import { CustomTopbarMenu } from '@remix-ui/helper'
 import React from 'react'
 import { Dropdown } from 'react-bootstrap'
 
@@ -12,14 +13,11 @@ export interface WorkspaceDropdownProps {
   LOCALHOST: string
   switchWorkspace: any
   ShowNonLocalHostMenuItems: () => JSX.Element
-  ShowAllMenuItems: () => JSX.Element
   CustomToggle: any
-  CustomMenu: any
   global: any
 }
 
-export function WorkspaceDropdown ({ toggleDropdown, showDropdown, selectedWorkspace, currentWorkspace, togglerText, formatNameForReadonly, NO_WORKSPACE, LOCALHOST, switchWorkspace, ShowNonLocalHostMenuItems, ShowAllMenuItems, CustomToggle, CustomMenu, global }: WorkspaceDropdownProps) {
-  console.log('what is the current workspace', global.plugin.currentWorkspaceMetadata)
+export function WorkspaceDropdown ({ toggleDropdown, showDropdown, selectedWorkspace, currentWorkspace, togglerText, formatNameForReadonly, NO_WORKSPACE, LOCALHOST, switchWorkspace, ShowNonLocalHostMenuItems, CustomToggle, global }: WorkspaceDropdownProps) {
 
   return (
     <Dropdown
@@ -27,7 +25,7 @@ export function WorkspaceDropdown ({ toggleDropdown, showDropdown, selectedWorks
       data-id="workspacesSelect"
       onToggle={toggleDropdown}
       show={showDropdown}
-      className="w-75 mx-auto"
+      className="w-75 mx-auto border"
     >
       <Dropdown.Toggle
         as={CustomToggle}
@@ -37,19 +35,21 @@ export function WorkspaceDropdown ({ toggleDropdown, showDropdown, selectedWorks
       >
         {selectedWorkspace ? selectedWorkspace.name === LOCALHOST ? togglerText : selectedWorkspace.name : currentWorkspace === LOCALHOST ? formatNameForReadonly('localhost') : NO_WORKSPACE}
       </Dropdown.Toggle>
-      <Dropdown.Menu as={CustomMenu} innerXPadding="px-2" className="w-100 custom-dropdown-items" data-id="topbar-custom-dropdown-items">
+      <Dropdown.Menu as={CustomTopbarMenu} innerXPadding="px-2" className="custom-dropdown-items w-100" data-id="topbar-custom-dropdown-items">
         {global.plugin.workspaces.length > 0 && (
           <>
             <ShowNonLocalHostMenuItems />
-            {(global.plugin.workspaces.length <= 0 || currentWorkspace === NO_WORKSPACE) && (
-              <Dropdown.Item
-                onClick={() => {
-                  switchWorkspace(NO_WORKSPACE)
-                }}
-              >
-                {<span className="pl-3">NO_WORKSPACE</span>}
-              </Dropdown.Item>
-            )}
+            <div style={{ height: '10px', overflowY: 'scroll' }} className="w-100">
+              {(global.plugin.workspaces.length <= 0 || currentWorkspace === NO_WORKSPACE) && (
+                <Dropdown.Item
+                  onClick={() => {
+                    switchWorkspace(NO_WORKSPACE)
+                  }}
+                >
+                  {<span className="pl-3">NO_WORKSPACE</span>}
+                </Dropdown.Item>
+              )}
+            </div>
             <li className="w-100 btn btn-primary font-weight-light text-decoration-none mb-2 rounded-lg">
               <span className="pl-2 ">
                 <i className="fas fa-desktop mr-2"></i>
@@ -76,7 +76,7 @@ export function WorkspaceDropdown ({ toggleDropdown, showDropdown, selectedWorks
               </span>
             </Dropdown.Item>
             <li className="w-100 btn btn-danger font-weight-light text-decoration-none">
-              <span className="pl-2 bg-danger text-white">
+              <span className="pl-2 text-white">
                 <i className="fas fa-trash-can mr-2"></i>
                 Delete all Workspaces
               </span>
