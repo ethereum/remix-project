e3af53f07a3918928a852b729e284abc442e605f
Joseph Izang <josephizang@gmail.com>
2025-07-23 21:59:54 +0100

    fix dropdown menu items

--- libs/remix-ui/top-bar/src/lib/remix-ui-topbar.tsx
+++ libs/remix-ui/top-bar/src/lib/remix-ui-topbar.tsx
@@ -3,15 +3,13 @@ import React, { useContext, useEffect, useReducer, useState } from 'react'
 import BasicLogo from '../components/BasicLogo'
 import '../css/topbar.css'
 import { Dropdown } from 'react-bootstrap'
-import { CustomMenu, CustomToggle } from 'libs/remix-ui/helper/src/lib/components/custom-dropdown'
+import { CustomToggle } from 'libs/remix-ui/helper/src/lib/components/custom-dropdown'
 import { WorkspaceMetadata } from 'libs/remix-ui/workspace/src/lib/types'
 import { platformContext } from 'libs/remix-ui/app/src/lib/remix-app/context/context'
 import { useIntl } from 'react-intl'
 import { Topbar } from 'apps/remix-ide/src/app/components/top-bar'
 import { TopbarContext } from '../context/topbarContext'
 import { WorkspaceDropdown } from '../components/WorkspaceDropdown'
-import { initWorkspace } from 'libs/remix-ui/workspace/src/lib/actions'
-import { browserInitialState, browserReducer } from 'libs/remix-ui/workspace/src/lib/reducers/workspace'
 
 export interface RemixUiTopbarProps {
   plugin: Topbar
@@ -49,12 +47,20 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
     return (
       <>
         {props.isGitRepo ? (
-          <div className="d-flex justify-content-between">
-            <span>{currentWorkspace === props.mName ? <span>&#10003; {props.mName} </span> : <span className="pl-3">{props.mName}</span>}</span>
+          <div
+            className="d-flex flex-row-reverse justify-content-end"
+          >
+            <span
+            >
+              {currentWorkspace === props.mName ? <span>&#10003; {props.mName} </span> : <span className="pl-1">{props.mName}</span>}</span>
             <i className="fas fa-code-branch pt-1"></i>
           </div>
         ) : (
-          <span>{currentWorkspace === props.mName ? <span>&#10003; {props.mName} </span> : <span className="pl-3">{props.mName}</span>}</span>
+          <div
+            className="d-flex justify-content-between"
+          >
+            <span>{currentWorkspace === props.mName ? <span>&#10003; {props.mName} </span> : <span className="pl-3">{props.mName}</span>}</span>
+          </div>
         )}
       </>
     )
@@ -81,13 +87,21 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
     return (
       <>
         { global.plugin.workspaces.map(({ name, isGitRepo }, index) => (
-          <Dropdown.Item
+          <div
             key={index}
-            onClick={() => { switchWorkspace(name) }}
-            data-id={`dropdown-item-${name}`}
+            className="d-flex justify-content-between w-100"
           >
-            <IsGitRepoDropDownMenuItem isGitRepo={isGitRepo} mName={name} />
-          </Dropdown.Item>
+            <Dropdown.Item
+              key={index}
+              onClick={() => { switchWorkspace(name) }}
+              data-id={`dropdown-item-${name}`}
+              className="text-truncate"
+              style={{ width: '90%' }}
+            >
+              <IsGitRepoDropDownMenuItem isGitRepo={isGitRepo} mName={name} />
+            </Dropdown.Item>
+            <i className="fas fa-ellipsis-vertical pt-1 top-bar-dropdownItem"></i>
+          </div>
         ))}
       </>
     )
@@ -96,7 +110,7 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
   const ShowNonLocalHostMenuItems = () => {
     const cachedFilter = global.plugin.workspaces.filter(x => !x.name.includes('localhost'))
     return (
-      <>
+      <div className="" style={{ maxHeight: '140px', overflowY: 'scroll' }}>
         {
           currentWorkspace === LOCALHOST && cachedFilter.length > 0 ? cachedFilter.map(({ name, isGitRepo }, index) => (
             <Dropdown.Item
@@ -110,22 +124,47 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
             </Dropdown.Item>
           )) : <ShowAllMenuItems />
         }
-      </>
+      </div>
     )
   }
 
   useEffect(() => {
     const run = async () => {
-      await plugin.getWorkspaces()
-      await plugin.getCurrentWorkspaceMetadata()
-      await plugin.getLatestUpdates()
       const [url, currentReleaseVersion] = await plugin.getLatestReleaseNotesUrl()
       setLatestReleaseNotesUrl(url)
       setCurrentReleaseVersion(currentReleaseVersion)
-      setCurrentWorkspace(plugin.currentWorkspaceMetadata?.name)
     }
     run()
+
+    return () => {
+      console.log('I ran the run function and I am unmounting')
+    }
   }, [])
+
+  const [currentTheme, setCurrentTheme] = useState<any>(null)
+
+  useEffect(() => {
+    const run = async () => {
+      await plugin.getWorkspaces()
+      await plugin.getCurrentWorkspaceMetadata()
+    }
+    run()
+  }, [showDropdown])
+
+  useEffect(() => {
+    plugin.on('theme', 'themeChanged', async (theme) => {
+      const currentTheme = await getCurrentTheme()
+      setCurrentTheme(currentTheme)
+    })
+  }, [])
+
+  const getCurrentTheme = async () => {
+    const theme = await plugin.call('theme', 'currentTheme')
+    return theme
+  }
+
+  const checkIfLightTheme = (themeName: string) =>
+    themeName.includes('dark') || themeName.includes('black') || themeName.includes('hackerOwl') ? false : true
 
   return (
     <section className="h-100 p-2 d-flex flex-row align-items-center justify-content-between bg-light border">
@@ -134,17 +173,24 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
           className="d-flex flex-row align-items-center justify-content-evenly"
           style={{ minWidth: '33%' }}
         >
-          <span className="d-flex align-items-center justify-content-between mr-3">
+          <span
+            className="d-flex align-items-center justify-content-between mr-3 cursor-pointer"
+            onClick={async () => {
+              await plugin.call('tabs', 'focus', 'home')
+              _paq.push(['trackEvent', 'topbar', 'header', 'Home'])
+            }}
+          >
             <span style={{ width: '35px', height: '35px' }} className="remixui_homeIcon"><BasicLogo /></span>
-            <span className="text-primary ml-2 font-weight-light text-uppercase" style={{ fontSize: '1.2rem' }}>Remix</span>
+            <span className="text-primary ml-2 font-weight-light text-uppercase cursor-pointer" style={{ fontSize: '1.2rem' }}>Remix</span>
           </span>
           <span
-            className="btn btn-sm border border-secondary text-secondary text-decoration-none font-weight-light text-light"
+            className="btn btn-sm border border-secondary text-decoration-none font-weight-light"
             onClick={() => {
               window.open(latestReleaseNotesUrl, '_blank')
             }}
             style={{
-              padding: '0.25rem 0.5rem'
+              padding: '0.25rem 0.5rem',
+              color: currentTheme && !checkIfLightTheme(currentTheme.name) ? 'var(--white)' : 'var(--text)'
             }}
           >
             {currentReleaseVersion}
@@ -162,9 +208,7 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
             LOCALHOST={LOCALHOST}
             switchWorkspace={switchWorkspace}
             ShowNonLocalHostMenuItems={ShowNonLocalHostMenuItems}
-            ShowAllMenuItems={ShowAllMenuItems}
             CustomToggle={CustomToggle}
-            CustomMenu={CustomMenu}
             global={global}
           />
         </div>
@@ -172,7 +216,14 @@ export function RemixUiTopbar ({ plugin, reducerState, dispatch }: RemixUiTopbar
           className="d-flex flex-row align-items-center justify-content-sm-end px-2"
           style={{ minWidth: '33%' }}
         >
-          <span className="btn btn-topbar mr-3" style={{ fontSize: '0.8rem' }}>
+          <span
+            className="btn btn-topbar mr-3"
+            style={{ fontSize: '0.8rem' }}
+            onClick={async () => {
+              await plugin.logInGithub()
+              _paq.push(['trackEvent', 'topbar', 'GIT', 'login'])
+            }}
+          >
             <i className="fab fa-github mr-2"></i>
             Connect to Github
           </span>
