1117cff72f8de1133b4304f110e3fe6755b67852
Joseph Izang <josephizang@gmail.com>
2025-07-24 23:51:45 +0100

    cleanup scratch code

--- libs/remix-ui/top-bar/src/components/WorkspaceDropdown.tsx
+++ libs/remix-ui/top-bar/src/components/WorkspaceDropdown.tsx
@@ -1,123 +1,7 @@
-import { CustomToggle, CustomTopbarMenu } from '@remix-ui/helper'
+import { CustomTopbarMenu } from '@remix-ui/helper'
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { Dropdown, Overlay } from 'react-bootstrap'
-import { WorkspaceDropdownSubMenu } from './WorkspaceDropdownSubMenu'
 import { remote } from '@remix-api'
-
-export interface WorkspaceDropdownProps {
-  toggleDropdown: any
-  showDropdown: boolean
-  selectedWorkspace: any
-  currentWorkspace: any
-  NO_WORKSPACE: string
-  switchWorkspace: any
-  ShowNonLocalHostMenuItems: () => JSX.Element
-  CustomToggle: any
-  global: any
-  showSubMenuFlyOut: boolean
-  setShowSubMenuFlyOut: (show: boolean) => void
-}
-
-export function WorkspaceDropdown ({ toggleDropdown, showDropdown, selectedWorkspace, currentWorkspace, NO_WORKSPACE, switchWorkspace, ShowNonLocalHostMenuItems, CustomToggle, global, showSubMenuFlyOut, setShowSubMenuFlyOut }: WorkspaceDropdownProps) {
-
-  const [subMenuFlyOutIndex, setSubMenuFlyOutIndex] = useState<number | null>(null)
-
-  const items = [
-    { label: 'Rename', onClick: () => {}, icon: 'fas fa-pencil-alt' },
-    { label: 'Copy', onClick: () => {}, icon: 'fas fa-copy' },
-    { label: 'Download', onClick: () => {}, icon: 'fas fa-download' },
-    { label: 'Delete', onClick: () => {}, icon: 'fas fa-trash' }
-  ]
-
-  const [togglerText, setTogglerText] = useState<string>(NO_WORKSPACE)
-  useEffect(() => {
-    global.plugin.on('filePanel', 'setWorkspace', (workspace) => {
-      setTogglerText(workspace.name)
-    })
-  }, [])
-
-  return (
-    <Dropdown
-      id="workspacesSelect"
-      data-id="workspacesSelect"
-      onToggle={toggleDropdown}
-      show={showDropdown}
-      className="w-75 mx-auto border"
-    >
-      <Dropdown.Toggle
-        as={CustomToggle}
-        id="dropdown-custom-components"
-        className="btn btn-light btn-block w-100 d-inline-block border border-dark form-control"
-        icon={selectedWorkspace && selectedWorkspace.isGitRepo ? 'far fa-code-branch' : null}
-      >
-        {togglerText}
-      </Dropdown.Toggle>
-      <Dropdown.Menu as={CustomTopbarMenu} innerXPadding="px-2" className="custom-dropdown-items w-100" data-id="topbar-custom-dropdown-items">
-        {global.plugin.workspaces.length > 0 && (
-          <>
-            <ShowNonLocalHostMenuItems />
-            <div style={{ height: '10px', overflowY: 'scroll' }} className="w-100">
-              {(global.plugin.workspaces.length <= 0 || currentWorkspace === NO_WORKSPACE) && (
-                <Dropdown.Item
-                  onClick={() => {
-                    switchWorkspace(NO_WORKSPACE)
-                  }}
-                >
-                  {<span className="pl-3">NO_WORKSPACE</span>}
-                </Dropdown.Item>
-              )}
-            </div>
-            <li className="w-100 btn btn-primary font-weight-light text-decoration-none mb-2 rounded-lg">
-              <span className="pl-2 ">
-                <i className="fas fa-desktop mr-2"></i>
-                Create a new workspace
-              </span>
-            </li>
-            <Dropdown.Divider className="border mb-0 mt-0 remixui_menuhr" style={{ pointerEvents: 'none' }} />
-            <Dropdown.Item>
-              <span className="pl-2" style={{ color: '#D678FF' }}>
-                <i className="far fa-desktop mr-2"></i>
-                Download Remix Desktop
-              </span>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <span className="pl-2">
-                <i className="far fa-download mr-2"></i>
-                Backup
-              </span>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <span className="pl-2">
-                <i className="fas fa-upload mr-2"></i>
-                Restore
-              </span>
-            </Dropdown.Item>
-            <li className="w-100 btn btn-danger font-weight-light text-decoration-none">
-              <span className="pl-2 text-white">
-                <i className="fas fa-trash-can mr-2"></i>
-                Delete all Workspaces
-              </span>
-            </li>
-          </>
-        )}
-      </Dropdown.Menu>
-      {showSubMenuFlyOut ? (
-        <WorkspaceDropdownSubMenu
-          menuItems={items}
-          style={{ borderRadius: '8px', position: 'absolute', left: `${62.5}dvw`,
-            right: `${28}dvw`,
-            bottom: `${50}dvh`,
-            top: `${10}dvh`,
-            height: '130px', width: '120px',
-            zIndex: 1900
-          }}
-        />
-      ) : (
-        <></>
-      )}
-    </Dropdown>
-  )
-}
 
 interface Branch {
   name: string
@@ -140,7 +24,7 @@ interface MenuItem {
   submenu: SubItem[]
 }
 
-interface NestedDropdownProps {
+interface WorkspacesDropdownProps {
   items: MenuItem[]
   toggleDropdown: any
   showDropdown: boolean
@@ -168,7 +52,7 @@ function useClickOutside(refs: React.RefObject<HTMLElement>[], handler: () => vo
   }, [refs, handler])
 }
 
-export const NestedDropdown: React.FC<NestedDropdownProps> = ({ items, toggleDropdown, showDropdown, selectedWorkspace, currentWorkspace, NO_WORKSPACE, switchWorkspace, ShowNonLocalHostMenuItems, CustomToggle, global }) => {
+export const WorkspacesDropdown: React.FC<WorkspacesDropdownProps> = ({ items, toggleDropdown, showDropdown, selectedWorkspace, currentWorkspace, NO_WORKSPACE, switchWorkspace, ShowNonLocalHostMenuItems, CustomToggle, global }) => {
   const [showMain, setShowMain] = useState(false)
   const [openSub, setOpenSub] = useState<number | null>(null)
   const mainRef = useRef<HTMLDivElement>(null)
