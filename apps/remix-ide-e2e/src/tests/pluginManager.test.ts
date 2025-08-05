c09cdfb3c4dba81c5f007151a76aa5ca3bf4401e
hsy822 <hyunsy822@gmail.com>
2025-08-05 20:50:42 +0900

    remove @disabled tag

--- apps/remix-ide-e2e/src/tests/pluginManager.test.ts
+++ apps/remix-ide-e2e/src/tests/pluginManager.test.ts
@@ -3,7 +3,6 @@ import { NightwatchBrowser } from 'nightwatch'
 import init from '../helpers/init'
 
 module.exports = {
-  '@disabled': true,
   before: function (browser: NightwatchBrowser, done: VoidFunction) {
     init(browser, done, undefined, false)
   },
