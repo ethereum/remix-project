b14c9af8fd6ddc05d445a1749c68ebe1aefba750
Aniket <30843294+Aniket-Engg@users.noreply.github.com>
2025-08-19 14:17:10 +0530

    Update web3-provider.js

--- apps/remix-ide/src/app/tabs/web3-provider.js
+++ apps/remix-ide/src/app/tabs/web3-provider.js
@@ -84,7 +84,6 @@ export class Web3ProviderModule extends Plugin {
             }
             try {
               const t = await provider.sendAsync(payload)
-              console.log(t, payload)
               resultFn(null, t)
             } catch (e) {
               resultFn(e.error ? e.error : e)
