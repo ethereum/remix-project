c42934c9cc2a2216d79a29750e514a9fcd20aae5
hsy822 <hyunsy822@gmail.com>
2025-08-21 13:51:49 +0900

    refactor: plugin manager (CI fix)

--- apps/remix-ide-e2e/src/tests/pluginManager.test.ts
+++ apps/remix-ide-e2e/src/tests/pluginManager.test.ts
@@ -2,114 +2,104 @@
 import { NightwatchBrowser } from 'nightwatch'
 import init from '../helpers/init'
 
-declare global {
-  interface Window { testmode: boolean; }
-}
-
-const testData = {
-  pluginName: 'remixIde',
-  pluginDisplayName: 'Remix IDE',
-  pluginUrl: 'https://zokrates.github.io/zokrates-remix-plugin/'
-}
-
-const localPluginData = {
-  name: testData.pluginName,
-  displayName: testData.pluginDisplayName,
-  canActivate: [],
-  url: testData.pluginUrl,
-  location: 'sidePanel'
-}
-
 module.exports = {
-  '@disabled': true,
   before: function (browser: NightwatchBrowser, done: VoidFunction) {
     init(browser, done, 'http://127.0.0.1:8080', false)
   },
 
-  'Should Load Plugin Manager #group1': function (browser: NightwatchBrowser) {
-    browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]')
-      .pause(3000)
-      .click('*[plugin="pluginManager"]')
-      .pause(3000)
-      .waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-      .assert.containsText('*[data-id="sidePanelSwapitTitle"]', 'PLUGIN MANAGER')
-  },
-
-  'Should Search for plugins #group1': function (browser: NightwatchBrowser) {
-    browser.waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-      .click('*[data-id="pluginManagerComponentSearchInput"]')
-      .keys('debugger')
-      .waitForElementVisible('*[data-id="pluginManagerComponentActivateButtondebugger"]')
-      .clearValue('*[data-id="pluginManagerComponentSearchInput"]')
-      .click('*[data-id="pluginManagerComponentSearchInput"]')
-      .keys('Vyper')
-      .waitForElementVisible('*[data-id="pluginManagerComponentActivateButtonvyper"]')
-      .clearValue('*[data-id="pluginManagerComponentSearchInput"]')
-      .click('*[data-id="pluginManagerComponentSearchInput"]')
-      .keys('ZoKrates')
-      .waitForElementVisible('*[data-id="pluginManagerComponentActivateButtonZoKrates"]')
-      .clearValue('*[data-id="pluginManagerComponentSearchInput"]')
-      .click('*[data-id="pluginManagerComponentSearchInput"]')
-      .keys(browser.Keys.SPACE)
-      .keys(browser.Keys.BACK_SPACE)
-  },
-
-  'Should activate plugins #group1': function (browser: NightwatchBrowser) {
-    browser.waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-      .click('*[data-id="pluginManagerComponentPluginManager"]')
-      .scrollAndClick('*[data-id="pluginManagerComponentActivateButtondebugger"]')
-      .pause(2000)
-      .waitForElementVisible('*[data-id="pluginManagerComponentDeactivateButtondebugger"]', 60000)
-      .scrollAndClick('*[data-id="pluginManagerComponentActivateButtonvyper"]')
-      .waitForElementVisible('*[data-id="pluginManagerComponentDeactivateButtonvyper"]', 70000)
-      .scrollAndClick('*[data-id="pluginManagerComponentActivateButtonZoKrates"]')
-      .waitForElementVisible('*[data-id="pluginManagerComponentDeactivateButtonZoKrates"]', 60000)
-  },
-
-  'Should deactivate plugins #group1': function (browser: NightwatchBrowser) {
-    browser.waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-      .click('*[data-id="pluginManagerComponentPluginManager"]')
-      .waitForElementVisible('*[data-id="pluginManagerComponentDeactivateButtondebugger"]', 60000)
-      .pause(2000)
-      .scrollAndClick('*[data-id="pluginManagerComponentDeactivateButtondebugger"]')
-      .waitForElementVisible('*[data-id="pluginManagerComponentActivateButtondebugger"]', 60000)
-      .waitForElementVisible('*[data-id="pluginManagerComponentDeactivateButtonvyper"]', 60000)
-      .scrollAndClick('*[data-id="pluginManagerComponentDeactivateButtonvyper"]')
-      .waitForElementVisible('*[data-id="pluginManagerComponentActivateButtonvyper"]', 60000)
-  },
-
-  'Should connect a local plugin #group2': function (browser: NightwatchBrowser) {
-    browser.waitForElementVisible('*[data-id="remixIdeSidePanel"]')
-      .pause(3000)
-      .click('*[plugin="pluginManager"]')
-      .clickLaunchIcon('filePanel')
-      .addLocalPlugin(localPluginData, false)
-      .pause(2000)
-  },
-
-  'Should display error message for creating already existing plugin #group2': function (browser: NightwatchBrowser) {
-    browser.waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-      .clickLaunchIcon('filePanel')
-      .addLocalPlugin(localPluginData, false)
-      .waitForElementContainsText('*[data-shared="tooltipPopup"]', 'Cannot create Plugin : This name has already been used')
-  },
-
-  'Should load back installed plugins after reload #group2': function (browser: NightwatchBrowser) {
+  'Should load Plugin Manager and search for a plugin #group1 #pr': function (browser: NightwatchBrowser) {
     browser
-      .waitForElementVisible('*[data-id="remixIdeSidePanel"]',3000)
-      .waitForElementVisible('*[data-id="pluginManagerComponentPluginManager"]')
-      .getInstalledPlugins((plugins) => {
-        browser.refreshPage()
-          .waitForElementVisible('*[data-id="remixIdeSidePanel"]')
-          .pause(3000)
-          .perform((done) => {
-            plugins.forEach(plugin => {
-              if (plugin !== testData.pluginName) {
-                browser.waitForElementVisible(`[plugin="${plugin}"`)
-              }
-            })
-            done()
-          })
+      .clickLaunchIcon('pluginManager')
+      .waitForElementVisible('[data-id="pluginManagerComponentPluginManager"]', 10000)
+      .assert.visible('[data-id="pluginManagerComponentPluginManager"]', 'Plugin Manager component is visible.')
+      .waitForElementVisible('[data-id="pluginManagerComponentSearchInput"]')
+      .setValue('[data-id="pluginManagerComponentSearchInput"]', 'debugger')
+      .assert.visible('[data-id="plugin-manager-plugin-card-debugger"]', 'Debugger plugin card is visible after search.')
+      .clearValue('[data-id="pluginManagerComponentSearchInput"]')
+  },
+
+  'Should activate and deactivate a plugin #group1 #pr': function (browser) {
+    let initialActiveCount
+    let toggleSel
+
+    browser
+      .waitForElementVisible('[data-id="pluginManagerComponentPluginManager"]', 10000)
+      .click('[data-id="pluginManagerActiveTab"]')
+      .getText('[data-id="pluginManagerActiveCount"]', (r) => {
+        initialActiveCount = parseInt(r.value)
       })
+      .click('[data-id="pluginManagerInactiveTab"]')
+      .waitForElementVisible('[data-id^="plugin-manager-plugin-card-"] [id^="toggleSwitch-"]', 10000)
+      .getAttribute('[data-id^="plugin-manager-plugin-card-"] [id^="toggleSwitch-"]', 'id', (res) => {
+        toggleSel = `#${res.value}`
+      })
+      .perform(() => {
+        browser.waitForElementVisible(toggleSel, 5000).click(toggleSel)
+      })
+      .pause(1200)
+      .click('[data-id="pluginManagerActiveTab"]')
+      .getText('[data-id="pluginManagerActiveCount"]', (r) => {
+        const newActiveCount = parseInt(r.value)
+        browser.assert.equal(newActiveCount, initialActiveCount + 1, `Active count should increase to ${initialActiveCount + 1}.`)
+      })
+      .perform(() => {
+        browser.waitForElementVisible(toggleSel, 5000).click(toggleSel)
+      })
+      .pause(1200)
+      .getText('[data-id="pluginManagerActiveCount"]', (r) => {
+        const finalActiveCount = parseInt(r.value)
+        browser.assert.equal(finalActiveCount, initialActiveCount, `Active count should return to ${initialActiveCount}.`)
+      })
+  },
+
+  'Should filter by "Only maintained by Remix" #group2 #pr': function (browser: NightwatchBrowser) {
+    let initialAllCount: number
+    let filteredCount: number
+
+    browser
+      .clickLaunchIcon('pluginManager')
+      .waitForElementVisible('[data-id="pluginManagerComponentPluginManager"]', 10000)
+      .click('[data-id="pluginManagerAllTab"]')
+      .getText('[data-id="pluginManagerAllCount"]', (result) => {
+        initialAllCount = parseInt(result.value as string)
+      })
+      .click('[data-id="filter-by-remix-switch"]')
+      .pause(1000)
+      .getText('[data-id="pluginManagerAllCount"]', (result) => {
+        filteredCount = parseInt(result.value as string)
+        browser.assert.ok(filteredCount < initialAllCount, `Filtered count (${filteredCount}) should be less than initial count (${initialAllCount}).`)
+      })
+      .click('[data-id="filter-by-remix-switch"]')
+      .pause(1000)
+      .getText('[data-id="pluginManagerAllCount"]', (result) => {
+        const finalCount = parseInt(result.value as string)
+        browser.assert.equal(finalCount, initialAllCount, `Count should return to initial count (${initialAllCount}).`)
+      })
+  },
+
+  'Should filter by category and clear filters #group2 #pr': function (browser: NightwatchBrowser) {
+    let initialAllCount: number
+
+    browser
+      .waitForElementVisible('[data-id="pluginManagerComponentPluginManager"]', 10000)
+      .click('[data-id="pluginManagerAllTab"]')
+      .getText('[data-id="pluginManagerAllCount"]', (result) => {
+        initialAllCount = parseInt(result.value as string)
+      })
+      .click('[data-id="pluginManagerComponentFilterButton"]')
+      .waitForElementVisible('[data-id="filter-panel"]')
+      .click('[data-id="filter-checkbox-5"]')
+      .pause(1000)
+      .getText('[data-id="pluginManagerAllCount"]', (result) => {
+        const filteredCount = parseInt(result.value as string)
+        browser.assert.ok(filteredCount < initialAllCount, `Category filtered count (${filteredCount}) should be less than initial count (${initialAllCount}).`)
+      })
+      .click('[data-id="clear-filters-btn"]')
+      .pause(1000)
+      .getText('[data-id="pluginManagerAllCount"]', (result) => {
+        const finalCount = parseInt(result.value as string)
+        browser.assert.equal(finalCount, initialAllCount, `Count should return to initial count after clearing filters.`)
+      })
+      .end()
   }
 }
--- libs/remix-ui/plugin-manager/src/lib/components/rootView.tsx
+++ libs/remix-ui/plugin-manager/src/lib/components/rootView.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { Fragment, ReactNode, useEffect, useState } from 'react' // eslint-disable-line no-use-before-define
+import React, { Fragment, ReactNode, useEffect, useState, useContext } from 'react' // eslint-disable-line no-use-before-define
 import { FormattedMessage, useIntl } from 'react-intl'
-import { PluginManagerComponent, PluginManagerSettings } from '../../types'
+import { PluginManagerComponent } from '../../types'
 import PermisssionsSettings from './permissionsSettings'
 import { Profile } from '@remixproject/plugin-utils'
 import LocalPluginForm from './LocalPluginForm'
+import FilterView from './FilterView'
+import { ToggleSwitch } from '@remix-ui/toggle'
 
 interface RootViewProps {
   pluginComponent: PluginManagerComponent
   children: ReactNode
+  filterByRemix: boolean
+  setFilterByRemix: (value: boolean) => void
+  categoryMap: Record<number, string>
+  selectedCategories: number[]
+  setSelectedCategories: (categories: number[]) => void
 }
 
 export interface pluginDeactivated {
@@ -21,36 +28,86 @@ export interface pluginActivated {
   profile: Profile
 }
 
-function RootView({ pluginComponent, children }: RootViewProps) {
+function RootView({ pluginComponent, children, filterByRemix, setFilterByRemix, categoryMap, selectedCategories, setSelectedCategories }: RootViewProps) {
   const intl = useIntl()
   const [visible, setVisible] = useState<boolean>(true)
   const [filterPlugins, setFilterPlugin] = useState<string>('')
-
-  const openModal = () => {
-    setVisible(false)
-  }
+  const [showFilters, setShowFilters] = useState<boolean>(false)
+  
+  const openModal = () => setVisible(false)
   const closeModal = () => setVisible(true)
 
   useEffect(() => {
     pluginComponent.getAndFilterPlugins(filterPlugins)
   }, [filterPlugins])
-  return (
+
+ return (
     <Fragment>
       <div id="pluginManager" data-id="pluginManagerComponentPluginManager">
-        <header className="mb-3 mb-0 d-flex flex-column align-items-center bg-light plugins-header pt-3 pb-0 px-3" data-id="pluginManagerComponentPluginManagerHeader">
-          <input
-            type="text"
-            onChange={(event) => {
-              setFilterPlugin(event.target.value.toLowerCase())
-            }}
-            value={filterPlugins}
-            className="mb-2 form-control"
-            placeholder={intl.formatMessage({ id: 'pluginManager.search' })}
-            data-id="pluginManagerComponentSearchInput"
-          />
-          <button onClick={openModal} className="py-1 btn bg-transparent text-dark border-0 mt-2 text-underline" data-id="pluginManagerComponentPluginSearchButton">
-            <FormattedMessage id="pluginManager.connectLocal" />
-          </button>
+        <header className="form-group mb-0 d-flex flex-column bg-light plugins-header pt-3 pb-2 px-3">
+          <div className="pb-3 mb-3 border-bottom w-100">
+            <button onClick={openModal} className="btn btn-secondary btn-sm d-flex align-items-center justify-content-center w-100" data-id="pluginManagerComponentConnectButton">
+              <img className="icon-pluginManager me-1" style={{filter: "invert(1)"}} src="assets/img/pluginManager.webp" alt="pluginManager" />
+              <FormattedMessage id="pluginManager.connectExternal" defaultMessage="Connect to an external plugin" />
+            </button>
+          </div>
+
+          <div className="d-flex w-100 mb-2">
+            <div className="search-bar-container w-100">
+              <img className="search-bar-icon" style={{filter: "invert(0.6)"}} src="assets/img/search_icon.webp" alt="Search" />
+              <input
+                type="text"
+                onChange={(event) => setFilterPlugin(event.target.value.toLowerCase())}
+                value={filterPlugins}
+                className="form-control form-control-sm"
+                placeholder={intl.formatMessage({ id: 'pluginManager.search' })}
+                data-id="pluginManagerComponentSearchInput"
+              />
+            </div>
+
+            <button 
+              onClick={() => setShowFilters(!showFilters)} 
+              className="btn btn-sm btn-secondary ml-2 d-flex align-items-center" 
+              data-id="pluginManagerComponentFilterButton"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none" className="me-1">
+                <g clipPath="url(#clip0_6001_5069)">
+                  <path d="M0.0608558 0.857813C0.163981 0.639063 0.382731 0.5 0.624918 0.5H7.37492C7.61711 0.5 7.83586 0.639063 7.93898 0.857813C8.04211 1.07656 8.01086 1.33438 7.85773 1.52188L4.99992 5.01406V7C4.99992 7.18906 4.89367 7.3625 4.72336 7.44688C4.55304 7.53125 4.35148 7.51406 4.19992 7.4L3.19992 6.65C3.07336 6.55625 2.99992 6.40781 2.99992 6.25V5.01406L0.140543 1.52031C-0.0110192 1.33438 -0.0438317 1.075 0.0608558 0.857813Z" fill="currentColor"/>
+                </g>
+                <defs>
+                  <clipPath id="clip0_6001_5069">
+                    <rect width="8" height="8" fill="white"/>
+                  </clipPath>
+                </defs>
+              </svg>
+              <span>Filters</span>
+            </button>
+          </div>
+
+          {showFilters && (
+            <FilterView 
+              categoryMap={categoryMap}
+              selectedCategories={selectedCategories}
+              setSelectedCategories={setSelectedCategories}
+            />
+          )}
+
+          <div className="d-flex align-items-center w-100">
+            <label
+              htmlFor="filter-by-remix-switch"
+              className="m-0 remixui-filter-label text-dark me-2"
+              style={{ cursor: 'pointer' }}
+            >
+              Only maintained by Remix
+            </label> 
+            <div data-id="filter-by-remix-switch">
+              <ToggleSwitch
+                id="filter-by-remix-switch"
+                isOn={filterByRemix}
+                onClick={() => setFilterByRemix(!filterByRemix)}
+              />
+            </div>
+          </div>
         </header>
         {children}
         <PermisssionsSettings />
