cd1c0f4108653e957ffee24bb55079ede5b2ac1c
STetsing <41009393+STetsing@users.noreply.github.com>
2025-08-28 11:24:36 +0200

    moved to chat completion on mistral ai

--- libs/remix-ai-core/src/prompts/promptBuilder.ts
+++ libs/remix-ai-core/src/prompts/promptBuilder.ts
@@ -1,11 +1,10 @@
 import { ChatHistory } from "./chat"
 
-export const buildChatPrompt = (userPrompt) => {
+export const buildChatPrompt = () => {
   const history = []
   for (const [question, answer] of ChatHistory.getHistory()) {
     history.push({ role:'user', content: question })
     history.push({ role:'assistant' , content: answer })
   }
-  history.push({ role: 'user', content: userPrompt })
   return history
 }
